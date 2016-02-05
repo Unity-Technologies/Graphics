20e6e7c5b3147bcbd203a8527723e4cbbb103ed6
Tim Cooper <stramit@users.noreply.github.com>
2016-02-05 11:25:05 +0100

    [Shader Graph] Add simple test framework to validate generation is consistant.

---
+++ UnityProject/Assets/UnityShaderEditor/Editor/Testing/ShaderGeneration/ShaderGenerationTest.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace UnityEditor.MaterialGraph.Tests
+{
+
+    public class ShaderGenerationTest
+    {
+        [Test]
+        public void RunGenerationTests()
+        {
+            string[] path =
+            {
+                "UnityShaderEditor",
+                "Editor",
+                "Testing",
+                "ShaderGeneration",
+                "Graphs"
+            };
+            
+            var absoluteGraphsPath = path.Aggregate(Application.dataPath, Path.Combine);
+            var prjRelativeGraphsPath = path.Aggregate("Assets", Path.Combine);
+
+            var filePaths = Directory.GetFiles(absoluteGraphsPath).Select(x => new FileInfo(x));
+
+            foreach (var file in filePaths.Where(x => x.Extension == ".ShaderGraph"))
+            {
+
+                var filePath = Path.Combine(prjRelativeGraphsPath, file.Name);
+                var graph = AssetDatabase.LoadAssetAtPath<MaterialGraph>(filePath);
+
+                if (graph == null)
+                    continue;
+
+                // Generate the shader
+                List<PropertyGenerator.TextureInfo> buff;
+                string shader = ShaderGenerator.GenerateSurfaceShader(graph, graph.name, false, out buff);
+
+                // find the 'reference' shader
+                var dumpFileLocation = string.Format("{0}.{1}", file, "dump");
+
+                if (!File.Exists(dumpFileLocation))
+                {
+                    // no reference exists, create it
+                    File.WriteAllText(dumpFileLocation, shader);
+                    Assert.Fail("FAILURE: Dump file not found for {0}, creating it.", file);
+                }
+
+                string dumpedShader = File.ReadAllText(dumpFileLocation);
+                if (string.CompareOrdinal(dumpedShader, shader) != 0)
+                    Assert.Fail("FAILURE: Shader from graph {0}, did not match .dump file.", file);
+            }
+        }
+    }
+}
