7aba975d9d311eb5c5c362a4ddbf0de3d97497b3
Julien Fryer <julienf@unity3d.com>
2016-02-12 16:59:47 +0100

    Add graph model to generate shaders

---
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/Model/VFXElementModel.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UnityEditor.Experimental
+{
+	public abstract class VFXElementModel
+	{
+		public void Add(VFXElementModel child, int index = -1,bool notify = true)
+		{
+			if (!CanAttach(child,index))
+				throw new ArgumentException();
+
+			child.Detach(notify && child.m_Owner != this); // Dont notify if the owner is already this to avoid double invalidation
+			
+			m_Children.Insert(index, child);
+			child.m_Owner = this;
+			
+			if (notify)
+				Invalidate();
+		}
+
+		public void Remove(VFXElementModel child, bool notify = true)
+		{
+			if (child.m_Owner != this)
+				return;
+
+			m_Children.Remove(child);
+			child.m_Owner = null;
+			
+			if (notify)
+				Invalidate();
+		}
+
+		public void Attach(VFXElementModel owner, bool notify = true)
+		{
+			if (owner == null)
+				throw new ArgumentNullException();
+
+			owner.Add(this, -1, notify);
+		}
+
+		public void Detach(bool notify = true)
+		{
+			if (m_Owner == null)
+				return;
+
+			m_Owner.Remove(this, notify);
+		}
+
+		public abstract bool CanAttach(VFXElementModel element,int index);
+		public abstract void Invalidate();
+
+		protected VFXElementModel m_Owner;
+		protected List<VFXElementModel> m_Children;
+	}
+
+	public abstract class VFXElementModelTyped<OwnerType, ChildrenType> : VFXElementModel
+		where OwnerType : VFXElementModel
+		where ChildrenType : VFXElementModel
+	{
+		public override bool CanAttach(VFXElementModel element,int index)
+		{
+			return index >= -1 && index <= m_Children.Count && element is ChildrenType;
+		}
+
+		public ChildrenType GetChild(int index)
+		{
+			return m_Children[index] as ChildrenType;
+		}
+
+		public OwnerType GetOwner()
+		{
+			return m_Owner as OwnerType;
+		}
+	}
+}
---
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/Model/VFXElementModelImpl.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using UnityEditor.Experimental;
+
+namespace UnityEditor.Experimental
+{
+	public class VFXAssetModel : VFXElementModelTyped<VFXElementModel, VFXSystemModel>
+	{
+		public override void Invalidate()
+		{
+			// Nothing
+		}
+
+		public void Dump()
+		{
+			// TODO log debug info
+		}
+	}
+
+	public class VFXSystemModel : VFXElementModelTyped<VFXAssetModel, VFXContextModel>
+	{
+		public override bool CanAttach(VFXElementModel element, int index)
+		{
+			if (base.CanAttach(element, index))
+				return false;
+
+			// Check if context types are inserted in the right order
+			int realIndex = index == -1 ? m_Children.Count : index;
+			if (realIndex > 0 && GetChild(realIndex - 1).GetContextType() > (element as VFXContextModel).GetContextType())
+				return false;
+			if (realIndex < m_Children.Count && GetChild(realIndex).GetContextType() < (element as VFXContextModel).GetContextType())
+				return false;
+
+			return true;
+		}
+
+		public override void Invalidate()
+		{
+			if (m_Children.Count == 0 && m_Owner != null) // If the system has no more attached contexts, remove it
+			{
+				Detach();
+				return;
+			}
+
+			// TODO
+			// gather attributes and check if attributes layout has changed
+
+			m_Dirty = true;
+		}
+
+		public void RecompileIfNeeded()
+		{
+			if (m_Dirty)
+			{
+				// TODO Recompile
+				m_Dirty = false;
+			}
+		}
+
+		private bool m_Dirty = true;
+	}
+
+
+	public class VFXContextModel : VFXElementModelTyped<VFXSystemModel, VFXBlockModel>
+	{
+		public enum Type
+		{
+			kTypeInit,
+			kTypeUpdate,
+			kTypeRender,
+		};
+
+		public const uint s_NbTypes = (uint)Type.kTypeRender + 1;
+
+		public VFXContextModel(Type type)
+		{
+			m_Type = type;
+		}
+
+		public override bool CanAttach(VFXElementModel element, int index)
+		{
+			return base.CanAttach(element, index);
+			// TODO Check if the block is comptatible with the context
+		}
+
+		public override void Invalidate()
+		{
+			// TODO
+			// Recompute parameters field
+		}
+
+		public Type GetContextType()
+		{
+			return m_Type;
+		}
+
+		private Type m_Type;
+	}
+
+	public class VFXBlockModel : VFXElementModelTyped<VFXContextModel, VFXElementModel>
+	{
+		public override void Invalidate()
+		{
+			if (m_Owner != null)
+				m_Owner.Invalidate();
+		}
+
+		VFXBlockModel(VFXBlock desc)
+		{
+			m_BlockDesc = desc;
+		}
+
+		public void SetDesc(VFXBlock blockDesc)
+		{
+			if (blockDesc == null)
+				throw new ArgumentNullException();
+
+			if (m_BlockDesc == null || !m_BlockDesc.m_Hash.Equals(blockDesc.m_Hash)) // block desc has changed
+			{
+				m_BlockDesc = blockDesc;
+				Invalidate();
+			}
+		}
+
+		public override bool CanAttach(VFXElementModel element, int index)
+		{
+			return false; // Nothing can be attached to Blocks !
+		}
+
+		private VFXBlock m_BlockDesc;
+		// TODO Store the uniform here ?
+	}
+}
