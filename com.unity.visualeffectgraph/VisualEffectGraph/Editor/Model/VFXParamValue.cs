8029e002c86371fc4a97c607fc8b44fe68106f6f
Julien Fryer <julienf@unity3d.com>
2016-02-16 12:37:42 +0100

    Allow to set/get values for VFXParamValue without downcasting

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/Model/VFXParamValue.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/Model/VFXParamValue.cs
@@ -7,7 +7,7 @@ namespace UnityEditor.Experimental
 {
     public abstract class VFXParamValue
     {
-        private VFXParam.Type m_Type;
+        protected VFXParam.Type m_Type;
         protected List<VFXBlockModel> m_BoundModels = new List<VFXBlockModel>();
 
         public VFXParam.Type ValueType
@@ -31,32 +31,39 @@ namespace UnityEditor.Experimental
                     throw new ArgumentException("Invalid parameter type");
             }
         }
+
+        //TODO: Problem is that implicit cast between types will not work here !
+        public T GetValue<T>()              { return ((VFXParamValue<T>)this).Value; }
+        public void SetValue<T>(T value)    { ((VFXParamValue<T>)this).Value = value; }
     }
 
-    public abstract class VFXParamValueTyped<T> : VFXParamValue
+    public abstract class VFXParamValue<T> : VFXParamValue
     {
         private T m_Value = default(T);
+
+        public void test() {}
 
         public T Value
         {
             get { return m_Value; }
-            set {
+            set
+            {
                 if (!m_Value.Equals(value))
                 {
                     m_Value = value;
                     foreach (var model in m_BoundModels)
                         model.Invalidate(VFXElementModel.InvalidationCause.kParamChanged);
-                } 
+                }
             }
         }
     }
 
-    public class VFXParamValueFloat : VFXParamValueTyped<float> { }
-    public class VFXParamValueFloat2 : VFXParamValueTyped<Vector2> { }
-    public class VFXParamValueFloat3 : VFXParamValueTyped<Vector3> { }
-    public class VFXParamValueFloat4 : VFXParamValueTyped<Vector4> { }
-    public class VFXParamValueInt : VFXParamValueTyped<int> { }
-    public class VFXParamValueUint : VFXParamValueTyped<uint> { }
-    public class VFXParamValueTexture2D : VFXParamValueTyped<Texture2D> { }
-    public class VFXParamValueTexture3D : VFXParamValueTyped<Texture3D> { }
+    public class VFXParamValueFloat : VFXParamValue<float>          { public VFXParamValueFloat()      { m_Type = VFXParam.Type.kTypeFloat; }}
+    public class VFXParamValueFloat2 : VFXParamValue<Vector2>       { public VFXParamValueFloat2()     { m_Type = VFXParam.Type.kTypeFloat2; }}
+    public class VFXParamValueFloat3 : VFXParamValue<Vector3>       { public VFXParamValueFloat3()     { m_Type = VFXParam.Type.kTypeFloat3; }}
+    public class VFXParamValueFloat4 : VFXParamValue<Vector4>       { public VFXParamValueFloat4()     { m_Type = VFXParam.Type.kTypeFloat4; }}
+    public class VFXParamValueInt : VFXParamValue<int>              { public VFXParamValueInt()        { m_Type = VFXParam.Type.kTypeInt; }}
+    public class VFXParamValueUint : VFXParamValue<uint>            { public VFXParamValueUint()       { m_Type = VFXParam.Type.kTypeUint; }}
+    public class VFXParamValueTexture2D : VFXParamValue<Texture2D>  { public VFXParamValueTexture2D()  { m_Type = VFXParam.Type.kTypeTexture2D; }}
+    public class VFXParamValueTexture3D : VFXParamValue<Texture3D>  { public VFXParamValueTexture3D() { m_Type = VFXParam.Type.kTypeTexture3D; }}
 }
