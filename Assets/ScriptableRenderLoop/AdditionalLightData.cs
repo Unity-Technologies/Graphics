b7337c069faf7667a9a1f2660253912e2e4bda11
Joachim Ante <joe@Joachims-MacBook-Pro.local>
2016-06-28 23:25:12 +0200

    Initial ScriptableRenderLoop

---
+++ Assets/ScriptableRenderLoop/AdditionalLightData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace UnityEngine.ScriptableRenderLoop
+{
+	//@TODO: We should continously move these values
+	// into the engine when we can see them being generally useful
+	[RequireComponent(typeof(Light))]
+	public class AdditionalLightData : MonoBehaviour
+	{
+		public const int defaultShadowResolution = 512;
+
+		public int shadowResolution = defaultShadowResolution;
+
+		[RangeAttribute(0.0F, 100.0F)]
+		public float innerSpotPercent = 0.0F;
+
+		public static float GetInnerSpotPercent01(AdditionalLightData lightData)
+		{
+			if (lightData != null)
+				return Mathf.Clamp( lightData.innerSpotPercent, 0.0f, 100.0f ) / 100.0f;
+			else
+				return 0.0F;
+		}
+
+		public static int GetShadowResolution(AdditionalLightData lightData)
+		{
+			if (lightData != null)
+				return lightData.shadowResolution;
+			else
+				return defaultShadowResolution;
+		}
+	}
+}
+
+
