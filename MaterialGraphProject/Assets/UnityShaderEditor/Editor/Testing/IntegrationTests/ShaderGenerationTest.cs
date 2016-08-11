fe2d10aba7e2250f8e091dfc92caf9230d1ff968
Tim Cooper <tim@unity3d.com>
2016-08-11 09:36:51 +0200

    [Material Graph]Tidy up integration testing.

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/Testing/IntegrationTests/ShaderGenerationTest.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Testing/IntegrationTests/ShaderGenerationTest.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using NUnit.Framework;
@@ -25,6 +26,7 @@ namespace UnityEditor.MaterialGraph.IntegrationTests
         {
             public string name;
             public FileInfo info;
+            public float threshold;
 
             public override string ToString()
             {
@@ -46,7 +48,8 @@ namespace UnityEditor.MaterialGraph.IntegrationTests
                         yield return new TestInfo
                         {
                             name = p.Name,
-                            info = p
+                            info = p,
+                            threshold = 0.02f
                         };
                     }
                 }
@@ -55,6 +58,8 @@ namespace UnityEditor.MaterialGraph.IntegrationTests
         
         private Shader m_Shader;
         private Material m_PreviewMaterial;
+        private Texture2D m_Captured;
+        private Texture2D m_FromDisk;
 
         [TearDown]
         public void CleanUp()
@@ -64,6 +69,12 @@ namespace UnityEditor.MaterialGraph.IntegrationTests
 
             if (m_PreviewMaterial != null)
                 Object.DestroyImmediate(m_PreviewMaterial);
+
+            if (m_Captured != null)
+                Object.DestroyImmediate(m_Captured);
+
+            if (m_FromDisk != null)
+                Object.DestroyImmediate(m_FromDisk);
         }
 
         [Test, TestCaseSource(typeof(CollectGraphs), "graphs")]
@@ -73,14 +84,18 @@ namespace UnityEditor.MaterialGraph.IntegrationTests
             var prjRelativeGraphsPath = s_Path.Aggregate("Assets", Path.Combine);
             var filePath = Path.Combine(prjRelativeGraphsPath, file.Name);
 
-            var graph = AssetDatabase.LoadAssetAtPath<MaterialGraphAsset>(filePath);
+            var graphAsset = AssetDatabase.LoadAssetAtPath<MaterialGraphAsset>(filePath);
 
-            Assert.IsNotNull(graph, "Graph asset not found");
+            Assert.IsNotNull(graphAsset, "Graph asset not found");
+            
+            var materialGraph = graphAsset.graph as PixelGraph;
+            Assert.IsNotNull(materialGraph);
 
             // Generate the shader
-            var shaderOutputLocation = string.Format("{0}.{1}", filePath, "shader");
-            m_Shader = GraphEditWindow.ExportShader(graph, shaderOutputLocation);
-
+            List<PropertyGenerator.TextureInfo> configuredTextures;
+            var shaderString = ShaderGenerator.GenerateSurfaceShader(materialGraph.pixelMasterNode, graphAsset.options, materialGraph.name, false, out configuredTextures);
+            m_Shader = ShaderUtil.CreateShaderAsset(shaderString);
+            m_Shader.hideFlags = HideFlags.HideAndDontSave;
             Assert.IsNotNull(m_Shader, "Shader Generation Failed");
             Assert.IsFalse(AbstractMaterialNodeUI.ShaderHasError(m_Shader), "Shader has error");
 
@@ -88,6 +103,14 @@ namespace UnityEditor.MaterialGraph.IntegrationTests
             {
                 hideFlags = HideFlags.HideAndDontSave
             };
+
+            foreach (var textureInfo in configuredTextures)
+            {
+                var texture = EditorUtility.InstanceIDToObject(textureInfo.textureId) as Texture;
+                if (texture == null)
+                    continue;
+                m_PreviewMaterial.SetTexture(textureInfo.name, texture);
+            }
 
             Assert.IsNotNull(m_PreviewMaterial, "preview material could not be created");
 
@@ -98,32 +121,81 @@ namespace UnityEditor.MaterialGraph.IntegrationTests
             Assert.IsNotNull(rendered, "Render failed");
 
             RenderTexture.active = rendered;
-            Texture2D captured = new Texture2D(rendered.width, rendered.height, TextureFormat.ARGB32, false);
-            captured.ReadPixels(new Rect(0, 0, rendered.width, rendered.height), 0, 0);
+            m_Captured = new Texture2D(rendered.width, rendered.height, TextureFormat.ARGB32, false);
+            m_Captured.ReadPixels(new Rect(0, 0, rendered.width, rendered.height), 0, 0);
             RenderTexture.active = null; //can help avoid errors 
 
-            var generated = captured.EncodeToPNG();
-           
+            var rootPath = Directory.GetParent(Directory.GetParent(Application.dataPath).ToString());
+            var templatePath = Path.Combine(rootPath.ToString(), "ImageTemplates");
+            
             // find the reference image
-            var dumpFileLocation = string.Format("{0}.template.{1}", file, "png");
-
+            var dumpFileLocation = Path.Combine(templatePath, string.Format("{0}.{1}", file.Name, "png"));
             if (!File.Exists(dumpFileLocation))
             {
                 // no reference exists, create it
+                Directory.CreateDirectory(templatePath);
+                var generated = m_Captured.EncodeToPNG();
                 File.WriteAllBytes(dumpFileLocation, generated);
                 Assert.Fail("Template file not found for {0}, creating it.", file);
             }
 
-            var saved = File.ReadAllBytes(dumpFileLocation);
+            var template = File.ReadAllBytes(dumpFileLocation);
+            m_FromDisk = new Texture2D(2,2);
+            m_FromDisk.LoadImage(template, false);
 
-            var areEqual = Enumerable.SequenceEqual(saved, generated);
+            var areEqual = CompareTextures(m_FromDisk, m_Captured, testInfo.threshold);
+
             if (!areEqual)
             {
-                var misMatchLocation = string.Format("{0}.{1}", file, "png");
-                File.WriteAllBytes(misMatchLocation, generated);
+                var failedPath = Path.Combine(rootPath.ToString(), "Failed");
+                Directory.CreateDirectory(failedPath);
+                var misMatchLocationResult = Path.Combine(failedPath, string.Format("{0}.{1}", file.Name, "png"));
+                var misMatchLocationTemplate = Path.Combine(failedPath, string.Format("{0}.template.{1}", file.Name, "png"));
+                var generated = m_Captured.EncodeToPNG();
+                File.WriteAllBytes(misMatchLocationResult, generated);
+                File.WriteAllBytes(misMatchLocationTemplate, template);
             }
 
             Assert.IsTrue(areEqual, "Shader from graph {0}, did not match .template file.", file);
+        }
+
+        private bool CompareTextures(Texture2D fromDisk, Texture2D captured, float threshold)
+        {
+            if (fromDisk == null || captured == null)
+                return false;
+
+            if (fromDisk.width != captured.width
+                || fromDisk.height != captured.height)
+                return false;
+
+            var pixels1 = fromDisk.GetPixels();
+            var pixels2 = captured.GetPixels();
+
+            if (pixels1.Length != pixels2.Length)
+                return false;
+
+            for (int i = 0; i < pixels1.Length; i++)
+            {
+                if (!CompareColor(pixels1[i], pixels2[i], threshold))
+                    return false;
+            }
+            return true;
+        }
+
+        private bool CompareColor(Vector4 left, Vector4 right, float threshold)
+        {
+            Vector4 diff = left - right;
+
+            if (Mathf.Abs(diff.x) > threshold)
+                return false;
+            if (Mathf.Abs(diff.y) > threshold)
+                return false;
+            if (Mathf.Abs(diff.z) > threshold)
+                return false;
+            if (Mathf.Abs(diff.w) > threshold)
+                return false;
+
+            return true;
         }
     }
 }
