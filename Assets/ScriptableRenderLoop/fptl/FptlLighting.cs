aea9af17d9aab3ad2f3851c0d210175b5bec1189
vlad-andreev <vladimiran@unity3d.com>
2016-08-30 13:08:59 +0200

    fptl:  fix specular in linear

--- Assets/ScriptableRenderLoop/fptl/FptlLighting.cs
+++ Assets/ScriptableRenderLoop/fptl/FptlLighting.cs
@@ -185,7 +185,7 @@ namespace UnityEngine.ScriptableRenderLoop
 				format10 = RenderTextureFormat.ARGB2101010;
 			//@TODO: GetGraphicsCaps().buggyMRTSRGBWriteFlag
 			cmd.GetTemporaryRT(kGBufferAlbedo, -1, -1, 0, FilterMode.Point, RenderTextureFormat.DefaultHDR, RenderTextureReadWrite.Default);
-			cmd.GetTemporaryRT(kGBufferSpecRough, -1, -1, 0, FilterMode.Point, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Default);
+			cmd.GetTemporaryRT(kGBufferSpecRough, -1, -1, 0, FilterMode.Point, RenderTextureFormat.DefaultHDR, RenderTextureReadWrite.Default);
 			cmd.GetTemporaryRT(kGBufferNormal, -1, -1, 0, FilterMode.Point, format10, RenderTextureReadWrite.Linear);
 			cmd.GetTemporaryRT(kGBufferEmission, -1, -1, 0, FilterMode.Point, format10, RenderTextureReadWrite.Linear); //@TODO: HDR
 			cmd.GetTemporaryRT(kGBufferZ, -1, -1, 24, FilterMode.Point, RenderTextureFormat.Depth);
