cb8e455713371e36644102c8bd73422ef970c676
vlad-andreev <vladimiran@unity3d.com>
2016-09-28 12:13:49 +0200

    menu item for the shader generator

---
+++ Assets/ShaderGenerator/ShaderGeneratorMenu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEditor;
+
+namespace UnityEngine.ScriptableRenderLoop
+{
+	public class ShaderGeneratorMenu
+	{
+		[UnityEditor.MenuItem("Renderloop/Generate Shader Includes")]
+		static void GenerateShaderIncludes()
+		{
+			CSharpToHLSL.GenerateAll();
+		}
+	}
+}
