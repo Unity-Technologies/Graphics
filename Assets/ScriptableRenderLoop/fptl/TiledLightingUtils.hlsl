ed4f517b512e5730f52a39c45d6df5c5cab895e0
mmikk <mikkelsen7@gmail.com>
2016-10-12 17:17:49 -0700

    separating out some utility functions which make sense for conventional fwd as well as tiled.
    separating out some utility functions which make sense for conventional
    fwd as well as tiled.

--- Assets/ScriptableRenderLoop/fptl/TiledLightingUtils.hlsl
+++ Assets/ScriptableRenderLoop/fptl/TiledLightingUtils.hlsl
@@ -2,18 +2,8 @@
 #define __TILEDLIGHTINGUTILS_H__
 
 
-#include "..\common\ShaderBase.h"
-#include "LightDefinitions.cs.hlsl"
+#include "LightingUtils.hlsl"
 
-
-uniform float4x4 g_mViewToWorld;
-uniform float4x4 g_mWorldToView;        // used for reflection only
-uniform float4x4 g_mScrProjection;
-uniform float4x4 g_mInvScrProjection;
-
-
-uniform uint g_widthRT;
-uniform uint g_heightRT;
 
 StructuredBuffer<SFiniteLightData> g_vLightData;
 StructuredBuffer<uint> g_vLightListGlobal;
@@ -92,74 +82,6 @@ uint FetchIndex(const uint tileOffs, const uint l)
 }
 
 #endif
-
-
-float3 GetViewPosFromLinDepth(float2 v2ScrPos, float fLinDepth)
-{
-    float fSx = g_mScrProjection[0].x;
-    //float fCx = g_mScrProjection[2].x;
-    float fCx = g_mScrProjection[0].z;
-    float fSy = g_mScrProjection[1].y;
-    //float fCy = g_mScrProjection[2].y;
-    float fCy = g_mScrProjection[1].z;
-
-#ifdef LEFT_HAND_COORDINATES
-    return fLinDepth*float3( ((v2ScrPos.x-fCx)/fSx), ((v2ScrPos.y-fCy)/fSy), 1.0 );
-#else
-    return fLinDepth*float3( -((v2ScrPos.x+fCx)/fSx), -((v2ScrPos.y+fCy)/fSy), 1.0 );
-#endif
-}
-
-float GetLinearZFromSVPosW(float posW)
-{
-#ifdef LEFT_HAND_COORDINATES
-    float linZ = posW;
-#else
-    float linZ = -posW;
-#endif
-
-    return linZ;
-}
-
-float GetLinearDepth(float zDptBufSpace)    // 0 is near 1 is far
-{
-    float3 vP = float3(0.0f,0.0f,zDptBufSpace);
-    float4 v4Pres = mul(g_mInvScrProjection, float4(vP,1.0));
-    return v4Pres.z / v4Pres.w;
-}
-
-
-
-float3 OverlayHeatMap(uint numLights, float3 c)
-{
-    /////////////////////////////////////////////////////////////////////
-    //
-    const float4 kRadarColors[12] =
-    {
-        float4(0.0,0.0,0.0,0.0),   // black
-        float4(0.0,0.0,0.6,0.5),   // dark blue
-        float4(0.0,0.0,0.9,0.5),   // blue
-        float4(0.0,0.6,0.9,0.5),   // light blue
-        float4(0.0,0.9,0.9,0.5),   // cyan
-        float4(0.0,0.9,0.6,0.5),   // blueish green
-        float4(0.0,0.9,0.0,0.5),   // green
-        float4(0.6,0.9,0.0,0.5),   // yellowish green
-        float4(0.9,0.9,0.0,0.5),   // yellow
-        float4(0.9,0.6,0.0,0.5),   // orange
-        float4(0.9,0.0,0.0,0.5),   // red
-        float4(1.0,0.0,0.0,0.9)    // strong red
-    };
-
-    float maxNrLightsPerTile = 31;
-
-
-
-    int nColorIndex = numLights==0 ? 0 : (1 + (int) floor(10 * (log2((float)numLights) / log2(maxNrLightsPerTile))) );
-    nColorIndex = nColorIndex<0 ? 0 : nColorIndex;
-    float4 col = nColorIndex>11 ? float4(1.0,1.0,1.0,1.0) : kRadarColors[nColorIndex];
-
-    return lerp(c, pow(col.xyz, 2.2), 0.3*col.w);
-}
 
 
 
