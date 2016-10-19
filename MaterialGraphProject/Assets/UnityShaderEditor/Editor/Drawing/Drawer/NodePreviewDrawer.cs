c26a46cd8ef1fae40338baa39b97884900c75bd3
Peter Bay Bastian <peter.bastian@gmail.com>
2016-10-19 10:52:00 +0200

    Extracted NodePreviewDrawer from MaterialNodeDrawer

---
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Drawer/NodePreviewDrawer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.RMGUI;
+
+namespace UnityEditor.MaterialGraph.Drawing
+{
+    public class NodePreviewDrawer : Image
+    {
+        public NodePreviewDrawData data;
+
+        public NodePreviewDrawer() {}
+
+        public override void DoRepaint(PaintContext args)
+        {
+            image = data.Render(new Vector2(200, 200));
+            base.DoRepaint(args);
+        }
+    }
+}
