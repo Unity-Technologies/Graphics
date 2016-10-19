f11e9044d3872ae48241b535ba7a00bbcdcc29fb
Peter Bay Bastian <peter.bastian@gmail.com>
2016-10-19 13:49:41 +0200

    Added UI for Vector{2,3,4}

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/DrawData/MaterialGraphDataSource.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/DrawData/MaterialGraphDataSource.cs
@@ -14,6 +14,9 @@ namespace UnityEditor.MaterialGraph.Drawing
             AddTypeMapping(typeof(ColorNode), typeof(ColorNodeDrawData));
             AddTypeMapping(typeof(TextureNode), typeof(TextureNodeDrawData));
             AddTypeMapping(typeof(Vector1Node), typeof(Vector1NodeDrawData));
+            AddTypeMapping(typeof(Vector2Node), typeof(Vector2NodeDrawData));
+            AddTypeMapping(typeof(Vector3Node), typeof(Vector3NodeDrawData));
+            AddTypeMapping(typeof(Vector4Node), typeof(Vector4NodeDrawData));
         }
     }
 }
