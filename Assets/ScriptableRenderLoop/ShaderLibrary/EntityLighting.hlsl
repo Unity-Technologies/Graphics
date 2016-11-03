1300284e6c299f515dc6c93ada88a1db08ab19fa
Sebastien Lagarde <sebastien@unity3d.com>
2016-11-03 12:52:26 -0700

    HDRenderLoop: Add more lightmap suport (need testing)

--- Assets/ScriptableRenderLoop/ShaderLibrary/EntityLighting.hlsl
+++ Assets/ScriptableRenderLoop/ShaderLibrary/EntityLighting.hlsl
@@ -58,7 +58,7 @@ float3 SampleSH9(float4 SHCoefficients[7], float3 N)
 // Volume is store as 3D texture with 4 R, G, B, X set of 4 coefficient store atlas in same 3D texture. X unused.
 // TODO: the packing here is innefficient as we will fetch values far away from each other and they may not fit into the cache
 // Suggest we pack only RGB not X and continuous
-float3 SampleProbeVolumeL0L1(TEXTURE3D_ARGS(SHVolumeTexture, SHVolumeSampler), float3 positionWS, float3 normalWS, float4x4 WorldToTexture, float texelSizeX)
+float3 SampleProbeVolumeSH4(TEXTURE3D_ARGS(SHVolumeTexture, SHVolumeSampler), float3 positionWS, float3 normalWS, float4x4 WorldToTexture, float texelSizeX)
 {
 	float3 texCoord = mul(WorldToTexture, float4(positionWS, 1.0)).xyz;
 	// Each component is store in the same texture 3D. Each use one quater on the x axis
@@ -75,6 +75,38 @@ float3 SampleProbeVolumeL0L1(TEXTURE3D_ARGS(SHVolumeTexture, SHVolumeSampler), f
 	float4 shAb = SAMPLE_TEXTURE3D(SHVolumeTexture, SHVolumeSampler, texCoord);
 
 	return SHEvalLinearL0L1(normalWS, shAr, shAg, shAb);
+}
+
+// Following functions are to sample enlighten lightmaps (or lightmaps encoded the same way as our
+// enlighten implementation). They assume use of RGB9E5 for illuminance map.
+// It is required for other platform that aren't supporting this format to implement variant of these functions
+// (But these kind of platform should use regular render loop and not news shaders).
+
+float3 SampleSingleLightmap(TEXTURE2D_ARGS(lightmapTex, lightmapSampler), float2 uv, float4 transform)
+{
+	// transform is scale and bias
+	uv = uv * transform.xy + transform.zw;
+	// Remark: Lightmap is RGB9E5
+	return SAMPLE_TEXTURE2D(lightmapTex, lightmapSampler, uv).rgb;
+}
+
+float3 SampleDirectionalLightmap(TEXTURE2D_ARGS(lightmapTex, lightmapSampler), TEXTURE2D_ARGS(lightmapDirTex, lightmapDirSampler), float2 uv, float4 transform, float3 normalWS)
+{
+	// In directional mode Enlighten bakes dominant light direction
+	// in a way, that using it for half Lambert and then dividing by a "rebalancing coefficient"
+	// gives a result close to plain diffuse response lightmaps, but normalmapped.
+
+	// Note that dir is not unit length on purpose. Its length is "directionality", like
+	// for the directional specular lightmaps.
+
+	// transform is scale and bias
+	uv = uv * transform.xy + transform.zw;
+
+	float4 direction = SAMPLE_TEXTURE2D(lightmapDirTex, lightmapDirSampler, uv);
+	// Remark: Lightmap is RGB9E5
+	float3 illuminance = SAMPLE_TEXTURE2D(lightmapTex, lightmapSampler, uv).rgb;
+	float halfLambert = dot(normalWS, direction.xyz - 0.5) + 0.5;
+	return illuminance * halfLambert / max(1e-4, direction.w);
 }
 
 #endif // UNITY_ENTITY_LIGHTING_INCLUDED
