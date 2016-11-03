5eaf465585341a25f21cfa46985ee2a085eb1f0f
Julien Fryer <julienf@unity3d.com>
2016-11-03 14:07:27 +0100

    Add a test scene for subpixel AA

---
+++ Assets/Tests/MoveCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newPos = transform.position;
+        newPos.x = -3.0f + (0.5f + Mathf.Sin(Time.realtimeSinceStartup * 0.4f) * 0.5f) * (-40.0f + 3.0f);
+        transform.position = newPos;
+	}
+}
