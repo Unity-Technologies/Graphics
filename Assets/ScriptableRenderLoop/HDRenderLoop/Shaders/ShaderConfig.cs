99126b3cab45e3c3c5ae03a39362605da4e0a47f
Sebastien Lagarde <sebastien@unity3d.com>
2016-11-09 00:29:31 +0100

    HDRenderLoop: Add velocity buffer support (C# side)

--- Assets/ScriptableRenderLoop/HDRenderLoop/Shaders/ShaderConfig.cs
+++ Assets/ScriptableRenderLoop/HDRenderLoop/Shaders/ShaderConfig.cs
@@ -4,4 +4,6 @@
 
 // #define DIFFUSE_LAMBERT_BRDF
 // #define USE_BSDF_PRE_LAMBDAV
-// #define VELOCITY_IN_GBUFFER
+
+// Caution: If you change one of the define below you need to regenerate the hlsl code (Push Generate Shader Includes)
+//#define VELOCITY_IN_GBUFFER
