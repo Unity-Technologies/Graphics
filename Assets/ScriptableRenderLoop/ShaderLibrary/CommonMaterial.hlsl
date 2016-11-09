7b6a88188817899f7e6eaf7dbc15d71a918676fe
Evgenii Golubev <evgenii@unity3d.com>
2016-11-09 17:01:05 +0100

    Set the default anisotropy direction along the tangent

--- Assets/ScriptableRenderLoop/ShaderLibrary/CommonMaterial.hlsl
+++ Assets/ScriptableRenderLoop/ShaderLibrary/CommonMaterial.hlsl
@@ -9,20 +9,31 @@
 // Convert anisotropic ratio (0->no isotropic; 1->full anisotropy in tangent direction) to roughness
 void ConvertAnisotropyToRoughness(float roughness, float anisotropy, out float roughnessT, out float roughnessB)
 {
+	// (0 <= anisotropy <= 1), therefore (0 <= anisoAspect <= 1)
+	// The 0.9 factor limits the aspect ratio to 10:1.
     float anisoAspect = sqrt(1.0 - 0.9 * anisotropy);
-    roughnessT = roughness * anisoAspect;
-    roughnessB = roughness / anisoAspect;
+
+    roughnessT = roughness / anisoAspect; // Distort along tangent (rougher)
+    roughnessB = roughness * anisoAspect; // Straighten along bitangent (smoother)
 }
 
-// Fake anisotropic by distorting the normal as suggested by:
 // Ref: Donald Revie - Implementing Fur Using Deferred Shading (GPU Pro 2)
-// anisotropic ratio (0->no isotropic; 1->full anisotropy in tangent direction)
-float3 GetAnisotropicModifiedNormal(float3 N, float3 T, float3 V, float anisotropy)
+// The grain direction (e.g. hair or brush direction) is assumed to be orthogonal to the normal.
+// The returned normal is NOT normalized.
+float3 ComputeGrainNormal(float3 grainDir, float3 V)
 {
-    float3 anisoT = cross(-V, T);
-    float3 anisoN = cross(anisoT, T);
+	float3 B = cross(-V, grainDir);
+	return cross(B, grainDir);
+}
 
-    return normalize(lerp(N, anisoN, anisotropy));
+// Fake anisotropic by distorting the normal.
+// The grain direction (e.g. hair or brush direction) is assumed to be orthogonal to N.
+// Anisotropic ratio (0->no isotropic; 1->full anisotropy in tangent direction)
+float3 GetAnisotropicModifiedNormal(float3 grainDir, float3 N, float3 V, float anisotropy)
+{
+	float3 grainNormal = ComputeGrainNormal(grainDir, V);
+	// TODO: test whether normalizing 'grainNormal' is worth it.
+	return normalize(lerp(N, grainNormal, anisotropy));
 }
 
 //-----------------------------------------------------------------------------
