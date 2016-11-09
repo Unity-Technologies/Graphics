6902886c2783452b04f6ca118c4815b491c3c422
mmikk <mikkelsen7@gmail.com>
2016-11-09 10:31:47 -0800

    revert duplicates and globals declarations
    revert duplicates and globals declarations

--- Assets/ScriptableRenderLoop/fptl/TiledReflectionTemplate.hlsl
+++ Assets/ScriptableRenderLoop/fptl/TiledReflectionTemplate.hlsl
@@ -19,16 +19,5 @@ float3 ExecuteReflectionList(out uint numReflectionProbesProcessed, uint2 pixCoo
     return ExecuteReflectionList(start, numReflectionProbes, vP, vNw, Vworld, smoothness);
 }
 
-float3 ExecuteReflectionListTile(out uint numReflectionProbesProcessed, uint2 tileIDX, float3 vP, float3 vNw, float3 Vworld, float smoothness)
-{
-    uint nrTilesX = (g_widthRT + 15) / 16; uint nrTilesY = (g_heightRT + 15) / 16;
-
-    uint start = 0, numReflectionProbes = 0;
-    GetCountAndStart(start, numReflectionProbes, tileIDX, nrTilesX, nrTilesY, vP.z, REFLECTION_LIGHT);
-
-    numReflectionProbesProcessed = numReflectionProbes;     // mainly for debugging/heat maps
-    return ExecuteReflectionList(start, numReflectionProbes, vP, vNw, Vworld, smoothness);
-}
-
 
 #endif
