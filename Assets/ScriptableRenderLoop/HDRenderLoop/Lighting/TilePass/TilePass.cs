2159ed6d0042767c125ce5da851d3f0a53050e0a
Julien Ignace <julien@unity3d.com>
2016-11-23 11:36:51 +0100

    Cleaned up profiling marker usage in HDRenderLoop (Will need PR "ignore empty named Cmd Buffer marker" to be complete)

--- Assets/ScriptableRenderLoop/HDRenderLoop/Lighting/TilePass/TilePass.cs
+++ Assets/ScriptableRenderLoop/HDRenderLoop/Lighting/TilePass/TilePass.cs
@@ -673,7 +673,7 @@ namespace UnityEngine.Experimental.ScriptableRenderLoop
                 var projscr = temp * proj;
                 var invProjscr = projscr.inverse;
 
-                var cmd = new CommandBuffer() { name = "Build light list" };
+                var cmd = new CommandBuffer() { name = "" };
 
                 // generate screen-space AABBs (used for both fptl and clustered).
                 {
