bc71a4a845fac9a41e2059376cf5065e22f9f2fc
vlad-andreev <vladimiran@unity3d.com>
2016-11-25 17:02:28 +0100

    tabs->spaces

--- Assets/ScriptableRenderLoop/HDRenderLoop/Editor/UpgradeStandardShaderMaterials.cs
+++ Assets/ScriptableRenderLoop/HDRenderLoop/Editor/UpgradeStandardShaderMaterials.cs
@@ -7,36 +7,36 @@ using NUnit.Framework;
 
 namespace UnityEditor.Experimental.ScriptableRenderLoop
 {
-	public class UpgradeStandardShaderMaterials
-	{
-		static List<MaterialUpgrader> GetHDUpgraders()
-		{
-			var upgraders = new List<MaterialUpgrader> ();
-			upgraders.Add(new StandardToHDLitMaterialUpgrader ());
+    public class UpgradeStandardShaderMaterials
+    {
+        static List<MaterialUpgrader> GetHDUpgraders()
+        {
+            var upgraders = new List<MaterialUpgrader>();
+            upgraders.Add(new StandardToHDLitMaterialUpgrader());
             upgraders.Add(new StandardSpecularToHDLitMaterialUpgrader());
             return upgraders;
-		}
+        }
 
-		[MenuItem("HDRenderLoop/Upgrade Materials - Project")]
-		static void UpgradeMaterialsProject()
-		{
-			MaterialUpgrader.UpgradeProjectFolder (GetHDUpgraders(), "Upgrade to HD Material");
-		}
-			
-		[MenuItem("HDRenderLoop/Upgrade Materials - Selection")]
-		static void UpgradeMaterialsSelection()
-		{
-			MaterialUpgrader.UpgradeSelection (GetHDUpgraders(), "Upgrade to HD Material");
-		}
+        [MenuItem("HDRenderLoop/Upgrade Materials - Project")]
+        static void UpgradeMaterialsProject()
+        {
+            MaterialUpgrader.UpgradeProjectFolder(GetHDUpgraders(), "Upgrade to HD Material");
+        }
 
-	    [MenuItem("HDRenderLoop/Modify Light Intensity for Upgrade - Scene Only")]
-	    static void UpgradeLights()
-	    {
-	        Light[] lights = Light.GetLights(LightType.Directional, 0);
-	        foreach (var l in lights)
-	        {
-	            l.intensity *= Mathf.PI;
-	        }
-	    }
-	}
+        [MenuItem("HDRenderLoop/Upgrade Materials - Selection")]
+        static void UpgradeMaterialsSelection()
+        {
+            MaterialUpgrader.UpgradeSelection(GetHDUpgraders(), "Upgrade to HD Material");
+        }
+
+        [MenuItem("HDRenderLoop/Modify Light Intensity for Upgrade - Scene Only")]
+        static void UpgradeLights()
+        {
+            Light[] lights = Light.GetLights(LightType.Directional, 0);
+            foreach (var l in lights)
+            {
+                l.intensity *= Mathf.PI;
+            }
+        }
+    }
 }
