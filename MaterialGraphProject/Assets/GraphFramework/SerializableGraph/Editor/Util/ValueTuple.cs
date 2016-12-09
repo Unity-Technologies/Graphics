fb10558ebf77a2876657773c4abf12006fb81183
Peter Bay Bastian <peter.bastian@gmail.com>
2016-12-09 11:40:14 +0100

    Created IEnumerable extension for zipping together enumerables
    (so that we can easily iterate over 2 collections using their
    IEnumerable interface, rather than having to index into one of them)

---
+++ MaterialGraphProject/Assets/GraphFramework/SerializableGraph/Editor/Util/ValueTuple.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace UnityEditor.Graphing.Util
+{
+    public struct ValueTuple<T1, T2>
+    {
+        public T1 Item1 { get; set; }
+        public T2 Item2 { get; set; }
+
+        public ValueTuple(T1 item1, T2 item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+
+        public static ValueTuple<T1, T2> Create(T1 item1, T2 item2)
+        {
+            return new ValueTuple<T1, T2>(item1, item2);
+        }
+    }
+
+    public static class ValueTuple
+    {
+        public static ValueTuple<T1, T2> Create<T1, T2>(T1 item1, T2 item2)
+        {
+            return new ValueTuple<T1, T2>(item1, item2);
+        }
+    }
+}
