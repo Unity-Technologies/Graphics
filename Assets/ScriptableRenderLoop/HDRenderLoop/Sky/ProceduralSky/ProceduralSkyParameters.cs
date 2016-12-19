b281e64c50c6b1583d257be1748f984c030c95f6
Evgenii Golubev <evgenii@unity3d.com>
2016-12-19 18:57:44 +0100

    Clean up and refactor the procedural sky

--- Assets/ScriptableRenderLoop/HDRenderLoop/Sky/ProceduralSky/ProceduralSkyParameters.cs
+++ Assets/ScriptableRenderLoop/HDRenderLoop/Sky/ProceduralSky/ProceduralSkyParameters.cs
@@ -6,47 +6,37 @@ namespace UnityEngine.Experimental.ScriptableRenderLoop
 {
     [ExecuteInEditMode]
     [DisallowMultipleComponent]
-    public class ProceduralSkyParameters
-        : SkyParameters
+    public class ProceduralSkyParameters : SkyParameters
     {
         public enum OcclusionDownscale { x1 = 1, x2 = 2, x4 = 4 }
-        public enum OcclusionSamples { x64 = 0, x164 = 1, x244 = 2 }
-        public enum DepthTexture { Enable, Disable/*, Ignore*/ } // 'Ignore' appears to be currently unused.
-        public enum ScatterDebugMode { None, Scattering, Occlusion, OccludedScattering, Rayleigh, Mie, Height }
+        public enum OcclusionSamples   { x64 = 0, x164 = 1, x244 = 2 }
+        public enum ScatterDebugMode   { None, Scattering, Occlusion, OccludedScattering, Rayleigh, Mie, Height }
 
         [Header("Global Settings")]
-        public Gradient worldRayleighColorRamp = null;
-        public float worldRayleighColorIntensity = 1f;
-        public float worldRayleighDensity = 10f;
-        public float worldRayleighExtinctionFactor = 1.1f;
-        public float worldRayleighIndirectScatter = 0.33f;
-        public Gradient worldMieColorRamp = null;
-        public float worldMieColorIntensity = 1f;
-        public float worldMieDensity = 15f;
-        public float worldMieExtinctionFactor = 0f;
-        public float worldMiePhaseAnisotropy = 0.9f;
-        public float worldNearScatterPush = 0f;
-        public float worldNormalDistance = 1000f;
+        public float    worldMieColorIntensity        = 1f;
+        public Gradient worldMieColorRamp             = null;
+        public float    worldMieDensity               = 15f;
+        public float    worldMieExtinctionFactor      = 0f;
+        public float    worldMiePhaseAnisotropy       = 0.9f;
+        public float    worldNearScatterPush          = 0f;
+        public float    worldNormalDistance           = 1000f;
+        public float    worldRayleighColorIntensity   = 1f;
+        public Gradient worldRayleighColorRamp        = null;
+        public float    worldRayleighDensity          = 10f;
+        public float    worldRayleighExtinctionFactor = 1.1f;
+        public float    worldRayleighIndirectScatter  = 0.33f;
 
         [Header("Height Settings")]
-        public Color heightRayleighColor = Color.white;
-        public float heightRayleighIntensity = 1f;
-        public float heightRayleighDensity = 10f;
-        public float heightMieDensity = 0f;
-        public float heightExtinctionFactor = 1.1f;
-        public float heightSeaLevel = 0f;
-        public float heightDistance = 50f;
-        public Vector3 heightPlaneShift = Vector3.zero;
-        public float heightNearScatterPush = 0f;
-        public float heightNormalDistance = 1000f;
-
-        [Header("Sky Dome")]
-        public Vector3 skyDomeRotation = Vector3.zero;
-        public bool skyDomeVerticalFlip = false;
-        public Cubemap skyDomeCubemap = null;
-        public float skyDomeExposure = 1f;
-        public Color skyDomeTint = Color.white;
-        public Transform skyDomeTrackedYawRotation = null;
+        public float    heightDistance                = 50f;
+        public float    heightExtinctionFactor        = 1.1f;
+        public float    heightMieDensity              = 0f;
+        public float    heightNearScatterPush         = 0f;
+        public float    heightNormalDistance          = 1000f;
+        public Vector3  heightPlaneShift              = Vector3.zero;
+        public Color    heightRayleighColor           = Color.white;
+        public float    heightRayleighDensity         = 10f;
+        public float    heightRayleighIntensity       = 1f;
+        public float    heightSeaLevel                = 0f;
 
         /*
         [Header("Scatter Occlusion")]
@@ -64,43 +54,64 @@ namespace UnityEngine.Experimental.ScriptableRenderLoop
         */
 
         [Header("Other")]
-        public Shader atmosphericShader = null;
-        // public Shader             occlusionShader       = null;
-        public float worldScaleExponent = 1.0f;
-        // public bool            forcePerPixel            = true;
-        // public bool            forcePostEffect          = true;
-        // [Tooltip("Soft clouds need depth values. Ignore means externally controlled.")]
-        public DepthTexture depthTexture = DepthTexture.Enable;
-        public ScatterDebugMode debugMode = ScatterDebugMode.None;
+        public Cubemap skyHDRI             = null;
+        // public Shader atmosphericShader = null;
+        // public Shader occlusionShader   = null;
+        public float worldScaleExponent    = 1.0f;
+        public ScatterDebugMode debugMode  = ScatterDebugMode.None;
 
         // Camera   m_currentCamera;
 
         // UnityEngine.Rendering.CommandBuffer m_occlusionCmdAfterShadows, m_occlusionCmdBeforeScreen;
 
+        void Awake()
+        {
+            if (worldRayleighColorRamp == null)
+            {
+                worldRayleighColorRamp = new Gradient();
+                worldRayleighColorRamp.SetKeys(
+                    new[] { new GradientColorKey(new Color(0.3f, 0.4f, 0.6f), 0f),
+                            new GradientColorKey(new Color(0.5f, 0.6f, 0.8f), 1f) },
+                    new[] { new GradientAlphaKey(1f, 0f),
+                            new GradientAlphaKey(1f, 1f) }
+                );
+            }
+
+            if (worldMieColorRamp == null)
+            {
+                worldMieColorRamp = new Gradient();
+                worldMieColorRamp.SetKeys(
+                    new[] { new GradientColorKey(new Color(0.95f, 0.75f, 0.5f), 0f),
+                            new GradientColorKey(new Color(1f, 0.9f, 8.0f), 1f) },
+                    new[] { new GradientAlphaKey(1f, 0f),
+                            new GradientAlphaKey(1f, 1f) }
+                );
+            }
+        }
+
         public void OnValidate()
         {
-            worldScaleExponent = Mathf.Clamp(worldScaleExponent, 1f, 2f);
-            worldNormalDistance = Mathf.Clamp(worldNormalDistance, 1f, 10000f);
-            worldNearScatterPush = Mathf.Clamp(worldNearScatterPush, -200f, 300f);
-            worldRayleighDensity = Mathf.Clamp(worldRayleighDensity, 0, 1000f);
-            worldMieDensity = Mathf.Clamp(worldMieDensity, 0f, 1000f);
+            worldMieDensity              = Mathf.Clamp(worldMieDensity, 0f, 1000f);
+            worldMiePhaseAnisotropy      = Mathf.Clamp01(worldMiePhaseAnisotropy);
+            worldNearScatterPush         = Mathf.Clamp(worldNearScatterPush, -200f, 300f);
+            worldNormalDistance          = Mathf.Clamp(worldNormalDistance, 1f, 10000f);
+            worldRayleighDensity         = Mathf.Clamp(worldRayleighDensity, 0, 1000f);
             worldRayleighIndirectScatter = Mathf.Clamp(worldRayleighIndirectScatter, 0f, 1f);
-            worldMiePhaseAnisotropy = Mathf.Clamp01(worldMiePhaseAnisotropy);
 
-            heightNormalDistance = Mathf.Clamp(heightNormalDistance, 1f, 10000f);
-            heightNearScatterPush = Mathf.Clamp(heightNearScatterPush, -200f, 300f);
-            heightRayleighDensity = Mathf.Clamp(heightRayleighDensity, 0, 1000f);
-            heightMieDensity = Mathf.Clamp(heightMieDensity, 0, 1000f);
+            heightMieDensity             = Mathf.Clamp(heightMieDensity, 0, 1000f);
+            heightNearScatterPush        = Mathf.Clamp(heightNearScatterPush, -200f, 300f);
+            heightNormalDistance         = Mathf.Clamp(heightNormalDistance, 1f, 10000f);
+            heightRayleighDensity        = Mathf.Clamp(heightRayleighDensity, 0, 1000f);
+
+            worldScaleExponent           = Mathf.Clamp(worldScaleExponent, 1f, 2f);
 
             /*
-            occlusionBias            = Mathf.Clamp01(occlusionBias);
-            occlusionBiasIndirect    = Mathf.Clamp01(occlusionBiasIndirect);
-            occlusionBiasClouds      = Mathf.Clamp01(occlusionBiasClouds);
-            occlusionBiasSkyRayleigh = Mathf.Clamp01(occlusionBiasSkyRayleigh);
-            occlusionBiasSkyMie      = Mathf.Clamp01(occlusionBiasSkyMie);
+            occlusionBias                = Mathf.Clamp01(occlusionBias);
+            occlusionBiasClouds          = Mathf.Clamp01(occlusionBiasClouds);
+            occlusionBiasIndirect        = Mathf.Clamp01(occlusionBiasIndirect);
+            occlusionBiasSkyMie          = Mathf.Clamp01(occlusionBiasSkyMie);
+            occlusionBiasSkyRayleigh     = Mathf.Clamp01(occlusionBiasSkyRayleigh);
             */
-
-            skyDomeExposure = Mathf.Clamp(skyDomeExposure, 0f, 8f);
         }
     }
 }
