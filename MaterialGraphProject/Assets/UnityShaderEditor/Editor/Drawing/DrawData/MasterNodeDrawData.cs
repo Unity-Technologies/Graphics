6a12fdd9ae840cb4853f1cdb1e951711e6aeb56d
Peter Bay Bastian <peter.bastian@gmail.com>
2016-12-20 17:08:06 +0100

    It is now possible to mark a master node as active in a shader graph (but not in others like sub graphs or remap graphs)

---
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/DrawData/MasterNodeDrawData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using RMGUI.GraphView;
+using UnityEditor.Graphing.Drawing;
+using UnityEngine.MaterialGraph;
+using UnityEngine;
+
+namespace UnityEditor.MaterialGraph.Drawing
+{
+    class MasterNodeControlDrawData : ControlDrawData
+    {
+        public override void OnGUIHandler()
+        {
+            base.OnGUIHandler();
+
+            var masterNode = node as AbstractMasterNode;
+            if (masterNode == null || !masterNode.canBeActiveMaster)
+                return;
+            
+            using (new EditorGUI.DisabledScope(masterNode.isActiveMaster))
+                masterNode.isActiveMaster |= GUILayout.Button("Set active");
+        }
+
+        public override float GetHeight()
+        {
+            var masterNode = node as AbstractMasterNode;
+            if (masterNode == null || !masterNode.canBeActiveMaster)
+                return 0f;
+            return EditorGUIUtility.singleLineHeight + 3 * EditorGUIUtility.standardVerticalSpacing;
+        }
+    }
+
+    [Serializable]
+    public class MasterNodeDrawData : MaterialNodeDrawData
+    {
+        protected override IEnumerable<GraphElementData> GetControlData()
+        {
+            var instance = CreateInstance<MasterNodeControlDrawData>();
+            instance.Initialize(node);
+            return new List<GraphElementData> { instance };
+        }
+    }
+}
--- MaterialGraphProject/Assets/UnityShaderEditor/Runtime/Nodes/AbstractMasterNode.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Runtime/Nodes/AbstractMasterNode.cs
@@ -24,6 +24,27 @@ namespace UnityEngine.MaterialGraph
             get { return false; }
         }
 
+        public bool isActiveMaster
+        {
+            get
+            {
+                var graph = owner as MaterialGraph;
+                return graph != null && graph.masterNode == this;
+            }
+            set
+            {
+                var graph = owner as MaterialGraph;
+                if (graph == null || value == isActiveMaster)
+                    return;
+                graph.masterNode = value ? this : null;
+            }
+        }
+
+        public bool canBeActiveMaster
+        {
+            get { return owner is MaterialGraph; }
+        }
+
         public abstract string GetFullShader(GenerationMode mode, out List<PropertyGenerator.TextureInfo> configuredTextures);
         public abstract string GetSubShader(GenerationMode mode, PropertyGenerator shaderPropertiesVisitor);
     }
