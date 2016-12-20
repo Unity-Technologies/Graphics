9ad8aefe1269481b937be937310f1b91d04b9693
Evgenii Golubev <evgenii@unity3d.com>
2016-12-20 19:13:05 +0100

    Use precomputed tables to store the Hammersley2D sequence

--- Assets/ScriptableRenderLoop/ShaderLibrary/Sampling.hlsl
+++ Assets/ScriptableRenderLoop/ShaderLibrary/Sampling.hlsl
@@ -5,30 +5,7 @@
 // Sample generator
 //-----------------------------------------------------------------------------
 
-// Ref: http://holger.dammertz.org/stuff/notes_HammersleyOnHemisphere.html
-uint ReverseBits32(uint bits)
-{
-#if 1 // Shader model 5
-    return reversebits(bits);
-#else
-    bits = (bits << 16) | (bits >> 16);
-    bits = ((bits & 0x00ff00ff) << 8) | ((bits & 0xff00ff00) >> 8);
-    bits = ((bits & 0x0f0f0f0f) << 4) | ((bits & 0xf0f0f0f0) >> 4);
-    bits = ((bits & 0x33333333) << 2) | ((bits & 0xcccccccc) >> 2);
-    bits = ((bits & 0x55555555) << 1) | ((bits & 0xaaaaaaaa) >> 1);
-    return bits;
-#endif
-}
-
-float RadicalInverse_VdC(uint bits)
-{
-    return float(ReverseBits32(bits)) * 2.3283064365386963e-10; // 0x100000000
-}
-
-float2 Hammersley2d(uint i, uint maxSampleCount)
-{
-    return float2(float(i) / float(maxSampleCount), RadicalInverse_VdC(i));
-}
+#include "Hammersley2D.hlsl"
 
 float Hash(uint s)
 {
