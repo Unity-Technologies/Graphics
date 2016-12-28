bbc10531df230cd16955b8c23c404af4f08c4373
Peter Bay Bastian <peter.bastian@gmail.com>
2016-12-28 12:34:24 +0100

    Extracted common parts of MaterialGraphAsset, MaterialRemapAsset and MaterialSubGraphAsset into abstract class

---
+++ MaterialGraphProject/Assets/UnityShaderEditor/Runtime/Graphs/AbstractMaterialGraphAsset.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using UnityEngine.Graphing;
+
+namespace UnityEngine.MaterialGraph
+{
+    public abstract class AbstractMaterialGraphAsset : ScriptableObject, IMaterialGraphAsset
+    {
+        public abstract IGraph graph { get; }
+
+        public bool shouldRepaint
+        {
+            get { return graph.GetNodes<AbstractMaterialNode>().OfType<IRequiresTime>().Any(); }
+        }
+
+        public ScriptableObject GetScriptableObject()
+        {
+            return this;
+        }
+
+        public void OnEnable()
+        {
+            graph.OnEnable();
+        }
+
+        [SerializeField]
+        private GraphDrawingData m_DrawingData = new GraphDrawingData();
+
+        public GraphDrawingData drawingData
+        {
+            get { return m_DrawingData; }
+        }
+    }
+}
--- MaterialGraphProject/Assets/UnityShaderEditor/Runtime/Remapper/MaterialRemapAsset.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Runtime/Remapper/MaterialRemapAsset.cs
@@ -4,12 +4,12 @@ using UnityEngine.Graphing;
 
 namespace UnityEngine.MaterialGraph
 {
-    public class MaterialRemapAsset : ScriptableObject, IMaterialGraphAsset
+    public class MaterialRemapAsset : AbstractMaterialGraphAsset
     {
         [SerializeField]
         private MasterRemapGraph m_MasterRemapGraph = new MasterRemapGraph();
 
-        public IGraph graph
+        public override IGraph graph
         {
             get { return m_MasterRemapGraph; }
         }
@@ -17,29 +17,6 @@ namespace UnityEngine.MaterialGraph
         public MasterRemapGraph masterRemapGraph
         {
             get { return m_MasterRemapGraph; }
-        }
-
-        public bool shouldRepaint
-        {
-            get { return graph.GetNodes<AbstractMaterialNode>().OfType<IRequiresTime>().Any(); }
-        }
-
-        public ScriptableObject GetScriptableObject()
-        {
-            return this;
-        }
-
-        public void OnEnable()
-        {
-            graph.OnEnable();
-        }
-
-        [SerializeField]
-        private GraphDrawingData m_DrawingData = new GraphDrawingData();
-
-        public GraphDrawingData drawingData
-        {
-            get { return m_DrawingData; }
         }
     }
 }
