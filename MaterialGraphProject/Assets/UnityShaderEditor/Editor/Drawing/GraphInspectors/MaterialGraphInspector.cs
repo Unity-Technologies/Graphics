f92577cafa53bcdcb9a10e64ac9b43c7b4225ca6
Peter Bay Bastian <peter.bastian@gmail.com>
2017-01-05 14:03:28 +0100

    Extracted abstract base class from MaterialGraphInspector

---
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/GraphInspectors/MaterialGraphInspector.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.MaterialGraph;
+
+namespace UnityEditor.MaterialGraph.Drawing
+{
+    [CustomEditor(typeof(MaterialGraphAsset))]
+    public class MaterialGraphInspector : AbstractMaterialGraphInspector
+    {
+        private UnityEngine.MaterialGraph.MaterialGraph materialGraph
+        {
+            get { return m_GraphAsset.graph as UnityEngine.MaterialGraph.MaterialGraph; }
+        }
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+            previewNode = materialGraph.masterNode as AbstractMaterialNode;
+        }
+    }
+}
