4e629b64d9ea2544c827cb5bc12a2a86ffb1d877
sebastienlagarde <sebastien@unity3d.com>
2017-01-12 19:49:07 +0100

    HDRenderPipeline: More test - Miss domain shader part

---
+++ Assets/ScriptableRenderLoop/HDRenderPipeline/Material/Lit/LitVertexShare.hlsl
@@ -0,0 +1,45 @@
+
+PackedVaryingsType Vert(Attributes input)
+{
+    VaryingsType output;
+
+#if defined(TESSELLATION_ON)
+    output.positionWS = TransformObjectToWorld(input.positionOS);
+    // TODO deal with camera center rendering and instancing (This is the reason why we always perform tow steps transform to clip space + instancing matrix)  
+    output.normalWS = TransformObjectToWorldNormal(input.normalOS);
+#if defined(VARYING_DS_WANT_TANGENT_TO_WORLD) || defined(VARYING_WANT_TANGENT_TO_WORLD)
+    output.tangentWS = float4(TransformObjectToWorldDir(input.tangentOS.xyz), input.tangentOS.w);
+#endif
+#else
+    float3 positionWS = TransformObjectToWorld(input.positionOS);
+        // TODO deal with camera center rendering and instancing (This is the reason why we always perform tow steps transform to clip space + instancing matrix)
+        output.positionCS = TransformWorldToHClip(positionWS);
+#ifdef VARYING_WANT_TANGENT_TO_WORLD
+    float3 normalWS = TransformObjectToWorldNormal(input.normalOS);
+    float4 tangentWS = float4(TransformObjectToWorldDir(input.tangentOS.xyz), input.tangentOS.w);
+
+    float3x3 tangentToWorld = CreateTangentToWorld(normalWS, tangentWS.xyz, tangentWS.w);
+    output.tangentToWorld[0] = tangentToWorld[0];
+    output.tangentToWorld[1] = tangentToWorld[1];
+    output.tangentToWorld[2] = tangentToWorld[2];
+#endif
+#endif
+
+#if defined(VARYING_WANT_TEXCOORD0) || defined(VARYING_DS_WANT_TEXCOORD0)
+    output.texCoord0 = input.uv0;
+#endif
+#if defined(VARYING_WANT_TEXCOORD1) || defined(VARYING_DS_WANT_TEXCOORD1)
+    output.texCoord1 = input.uv1;
+#endif
+#if defined(VARYING_WANT_TEXCOORD2) || defined(VARYING_DS_WANT_TEXCOORD2)
+    output.texCoord2 = input.uv2;
+#endif
+#if defined(VARYING_WANT_TEXCOORD3) || defined(VARYING_DS_WANT_TEXCOORD3)
+    output.texCoord3 = input.uv3;
+#endif
+#if defined(VARYING_WANT_COLOR) || defined(VARYING_DS_WANT_COLOR)
+    output.color = input.color;
+#endif
+
+    return PackVaryingsType(output);
+}
