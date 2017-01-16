aa37e173b33b881cf74c7eaa0370565ff41a15c0
Julien Fryer <julienf@unity3d.com>
2017-01-16 14:38:37 +0100

    Add helper function to connect/disconnect contexts + tests

--- Assets/VFXEditorNew/Editor/Models/VFXSystem.cs
+++ Assets/VFXEditorNew/Editor/Models/VFXSystem.cs
@@ -20,5 +20,58 @@ namespace UnityEditor.VFX
 
             return true;
         }
+
+        public static bool ConnectContexts(VFXContext context0, VFXContext context1, VFXModelContainer root)
+        {
+            if (context0 == context1)
+                return false;
+
+            VFXSystem system0 = context0.GetParent();
+            int context0Index = system0.GetIndex(context0);
+
+            if (system0 == context1.GetParent() && context0Index > context1.GetParent().GetIndex(context1))
+                return false;
+
+            if (!system0.AcceptChild(context1, context0Index + 1))
+                return false;
+
+            if (system0.GetNbChildren() > context0Index + 1)
+            {
+                VFXSystem newSystem = new VFXSystem();
+
+                while (system0.GetNbChildren() > context0Index + 1)
+                    system0.m_Children[context0Index + 1].Attach(newSystem, true);
+
+                root.m_Roots.Add(newSystem);
+            }
+
+            VFXSystem system1 = context1.GetParent();
+            int context1Index = system1.m_Children.IndexOf(context1);
+
+            // Then we append context1 and all following contexts to system0
+            while (system1.GetNbChildren() > context1Index)
+                system1.m_Children[context1Index].Attach(system0, true);
+
+            return true;
+        }
+
+        public static bool DisconnectContext(VFXContext context, VFXModelContainer root)
+        {
+            VFXSystem system = context.GetParent();
+            if (system == null)
+                return false;
+
+            int index = system.GetIndex(context);
+            if (index == 0)
+                return false;
+
+            VFXSystem newSystem = new VFXSystem();
+            while (system.GetNbChildren() > index)
+                system.GetChild(index).Attach(newSystem, true);
+
+            root.m_Roots.Add(newSystem);
+
+            return true;
+        }
     }
 }
---
+++ Assets/VFXEditorNew/Editor/Tests/VFXSystemTests.cs
@@ -0,0 +1,63 @@
+using System;
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+using Object = UnityEngine.Object;
+
+namespace UnityEditor.VFX.Test
+{
+    [TestFixture]
+    public class VFXSystemTests
+    {
+        private VFXModelContainer CreateAndInitContainer()
+        {
+            var modelContainer = ScriptableObject.CreateInstance<VFXModelContainer>();
+
+            var system0 = new VFXSystem();
+            system0.AddChild(new VFXContext(VFXContextDesc.CreateBasic(VFXContextType.kInit)));
+            system0.AddChild(new VFXContext(VFXContextDesc.CreateBasic(VFXContextType.kUpdate)));
+            system0.AddChild(new VFXContext(VFXContextDesc.CreateBasic(VFXContextType.kOutput)));
+            modelContainer.m_Roots.Add(system0);
+
+            var system1 = new VFXSystem();
+            system1.AddChild(new VFXContext(VFXContextDesc.CreateBasic(VFXContextType.kInit)));
+            system1.AddChild(new VFXContext(VFXContextDesc.CreateBasic(VFXContextType.kUpdate)));
+            system1.AddChild(new VFXContext(VFXContextDesc.CreateBasic(VFXContextType.kOutput)));
+            modelContainer.m_Roots.Add(system1);
+
+            return modelContainer;
+        }
+
+        [Test]
+        public void ConnectContext()
+        {
+            var modelContainer = CreateAndInitContainer();
+
+            VFXSystem.ConnectContexts(((VFXSystem)modelContainer.m_Roots[0]).GetChild(0), ((VFXSystem)modelContainer.m_Roots[1]).GetChild(1), modelContainer);
+
+            Assert.AreEqual(3, modelContainer.m_Roots.Count);
+            Assert.AreEqual(3, modelContainer.m_Roots[0].GetNbChildren());
+            Assert.AreEqual(1, modelContainer.m_Roots[1].GetNbChildren());
+            Assert.AreEqual(2, modelContainer.m_Roots[2].GetNbChildren());
+
+            Object.DestroyImmediate(modelContainer);
+        }
+
+        [Test]
+        public void DisconnectContext()
+        {
+            var modelContainer = CreateAndInitContainer();
+
+            VFXSystem.DisconnectContext(((VFXSystem)modelContainer.m_Roots[0]).GetChild(1), modelContainer);
+            VFXSystem.DisconnectContext(((VFXSystem)modelContainer.m_Roots[1]).GetChild(2), modelContainer);
+
+            Assert.AreEqual(4, modelContainer.m_Roots.Count);
+            Assert.AreEqual(1, modelContainer.m_Roots[0].GetNbChildren());
+            Assert.AreEqual(2, modelContainer.m_Roots[1].GetNbChildren());
+            Assert.AreEqual(2, modelContainer.m_Roots[2].GetNbChildren());
+            Assert.AreEqual(1, modelContainer.m_Roots[3].GetNbChildren());
+
+            Object.DestroyImmediate(modelContainer);
+        }
+    }
+}
