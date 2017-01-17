3729ea056102fa4bfa4dc26deba4ab5adaf3b292
Felipe Lira <felipe@unity3d.com>
2017-01-17 15:37:30 +0100

    Added a material upgrader to convert VikingVillage materials to LD Pipeline ones.

---
+++ Assets/LowEndRenderLoop/Editor/StandardSpecularToLDMaterialUpgrader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Experimental.Rendering;
+using UnityEngine;
+
+public class StandardSpecularToLDMaterialUpgrader : MaterialUpgrader
+{
+    [MenuItem("LDRenderPipeline/Upgrade Materials")]
+    private static void UpgradeMaterialsToLD()
+    {
+        List<MaterialUpgrader> upgraders = new List<MaterialUpgrader>();
+        upgraders.Add(new StandardSpecularToLDMaterialUpgrader("Standard (Specular setup)"));
+        upgraders.Add(new StandardSpecularToLDMaterialUpgrader("Standard"));
+        upgraders.Add(new StandardSpecularToLDMaterialUpgrader("TerrainSurface"));
+
+        MaterialUpgrader.UpgradeProjectFolder(upgraders, "Upgrade to LD Materials");
+    }
+
+    StandardSpecularToLDMaterialUpgrader(string oldShaderName)
+    {
+        RenameShader(oldShaderName, "RenderLoop/LowEnd");
+    }
+}
