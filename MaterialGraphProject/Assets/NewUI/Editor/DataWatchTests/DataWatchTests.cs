9c588f5e0f69e15712b37d80f6609a520534cc6c
joce <joce@unity3d.com>
2017-01-17 15:27:21 -0500

    Update to latest GraphView
    Signed-off-by: joce <joce@unity3d.com>

--- MaterialGraphProject/Assets/NewUI/Editor/DataWatchTests/DataWatchTests.cs
+++ MaterialGraphProject/Assets/NewUI/Editor/DataWatchTests/DataWatchTests.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.RMGUI;
-using UnityEngine.PlaymodeTests;
-using UnityEngine.Assertions;
+using UnityEngine.TestTools;
+using NUnit.Framework;
 using UnityEditor;
 using System.Collections;
 
-[EditModeTest]
+[UnityPlatform(TestPlatform.EditMode)]
 public class DataWatchTests
 {
 	class MyWindow : EditorWindow
 	{
+		public int calls;
+		public DataWatchService watch;
+
+		public MyWindow()
+		{
+			watch = new DataWatchService();
+		}
+
+		public void OnDataChanged()
+		{
+			calls++;
+		}
 	}
 
 	class MyData : ScriptableObject
@@ -18,125 +30,119 @@ public class DataWatchTests
 		public int myField;
 	}
 
-	int m_Calls;
-	MyData data;
-	DataWatchService watch;
-
-	public DataWatchTests()
-	{
-		data = ScriptableObject.CreateInstance<MyData>();
-		watch = new DataWatchService();
-	}
-
-	void OnDataChanged()
-	{
-		m_Calls++;
-	}
-
-	[EditModeTest]
+	[UnityTest]
 	public IEnumerator DataWatchCallsOnChanged()
 	{
+		var data = ScriptableObject.CreateInstance<MyData>();
+
 		var window = EditorWindow.GetWindow<MyWindow>();
 
-		IDataWatchHandle handle = watch.AddWatch(window.rootVisualContainer, data, OnDataChanged);
-		Assert.IsTrue(watch.IsActive(handle), "Expected watch to be active");
+		IDataWatchHandle handle = window.watch.AddWatch(window.rootVisualContainer, data, window.OnDataChanged);
+		Assert.IsTrue(window.watch.IsActive(handle), "Expected watch to be active");
 
 		data.myField = 1;
-		watch.ProcessNotificationQueue();
-		Assert.AreEqual(m_Calls, 1, "Expected first call after change");
+		window.watch.ProcessNotificationQueue();
+		Assert.AreEqual(window.calls, 1, "Expected first call after change");
 
-		watch.ProcessNotificationQueue();
-		Assert.AreEqual(m_Calls, 1, "Expected no new call after no change");
+		window.watch.ProcessNotificationQueue();
+		Assert.AreEqual(window.calls, 1, "Expected no new call after no change");
 
 		data.myField = 1;
-		watch.ProcessNotificationQueue();
-		Assert.AreEqual(m_Calls, 1, "Expected no new call after idempotent change");
+		window.watch.ProcessNotificationQueue();
+		Assert.AreEqual(window.calls, 1, "Expected no new call after idempotent change");
 
 		data.myField = 2;
-		watch.ProcessNotificationQueue();
-		Assert.AreEqual(m_Calls, 2, "Expect second callback after change");
+		window.watch.ProcessNotificationQueue();
+		Assert.AreEqual(window.calls, 2, "Expect second callback after change");
 
 		handle.Dispose();
 		window.Close();
 		yield return null;
 	}
 
-	[EditModeTest]
+	[UnityTest]
 	public IEnumerator DataWatchCallsOnChangedForDestroyedObject() {
 
 		var window = EditorWindow.GetWindow<MyWindow>();
 
 		yield return null;
 
-		IDataWatchHandle handle = watch.AddWatch(window.rootVisualContainer, data, OnDataChanged);
+		var data = ScriptableObject.CreateInstance<MyData>();
+
+		IDataWatchHandle handle = window.watch.AddWatch(window.rootVisualContainer, data, window.OnDataChanged);
 
 		ScriptableObject.DestroyImmediate(data);
-		Assert.IsTrue(watch.IsActive(handle), "Expected watch to still be active after destruction");
-		watch.ProcessNotificationQueue();
-		Assert.AreEqual(m_Calls, 1, "Expect a call after destruction");
+		Assert.IsTrue(window.watch.IsActive(handle), "Expected watch to still be active after destruction");
+		window.watch.ProcessNotificationQueue();
+		Assert.AreEqual(window.calls, 1, "Expect a call after destruction");
 
 		window.Close();
 		yield return null;
 	}
 
-	[EditModeTest]
+	[UnityTest]
 	public IEnumerator CanStopWatchingObject()
 	{
+		var data = ScriptableObject.CreateInstance<MyData>();
 		var window = EditorWindow.GetWindow<MyWindow>();
 
-		IDataWatchHandle handle = watch.AddWatch(window.rootVisualContainer, data, OnDataChanged);
+		IDataWatchHandle handle = window.watch.AddWatch(window.rootVisualContainer, data, window.OnDataChanged);
 
 		handle.Dispose();
-		Assert.IsFalse(watch.IsActive(handle), "Expected watch to not be active after removal");
+		Assert.IsFalse(window.watch.IsActive(handle), "Expected watch to not be active after removal");
 
 		data.myField = 1;
 
-		Assert.AreEqual(m_Calls, 0, "Expected no call after removing watch");
+		Assert.AreEqual(window.calls, 0, "Expected no call after removing watch");
 
 		window.Close();
 		yield return null;
 	}
 
-	[EditModeTest]
+	[UnityTest]
 	public IEnumerator CanStopWatchingObjectDuringCallback()
 	{
+		var data = ScriptableObject.CreateInstance<MyData>();
+
 		IDataWatchHandle handle = null;
 		var window = EditorWindow.GetWindow<MyWindow>();
 
 		Action removeOnCall = () => handle.Dispose();
 
-		handle = watch.AddWatch(window.rootVisualContainer, data, removeOnCall);
+		handle = window.watch.AddWatch(window.rootVisualContainer, data, removeOnCall);
 
 		data.myField = 1;
-		watch.ProcessNotificationQueue();
+		window.watch.ProcessNotificationQueue();
 
-		Assert.IsFalse(watch.IsActive(handle), "Expected watch to not be active after removal");
+		Assert.IsFalse(window.watch.IsActive(handle), "Expected watch to not be active after removal");
 
 		window.Close();
 		yield return null;
 	}
 
-	[EditModeTest]
+	[UnityTest]
 	public IEnumerator ReceivesCallbackForUndo()
 	{
+		var data = ScriptableObject.CreateInstance<MyData>();
+
 		var window = EditorWindow.GetWindow<MyWindow>();
-		IDataWatchHandle handle = watch.AddWatch(window.rootVisualContainer, data, OnDataChanged);
+		IDataWatchHandle handle = window.watch.AddWatch(window.rootVisualContainer, data, window.OnDataChanged);
 
 		Undo.RecordObject(data, "change");
 		data.myField = 1;
 		yield return null;
-		watch.ProcessNotificationQueue();
-		Assert.AreEqual(m_Calls, 1, "Expected first call after change");
+		window.watch.ProcessNotificationQueue();
+		Assert.AreEqual(window.calls, 1, "Expected first call after change");
 
 		Undo.PerformUndo();
 		yield return null;
-		watch.ProcessNotificationQueue();
-		Assert.AreEqual(m_Calls, 2, "Expected second call after undo");
+		window.watch.ProcessNotificationQueue();
+		Assert.AreEqual(window.calls, 2, "Expected second call after undo");
 
 		Undo.PerformRedo();
 		yield return null;
-		watch.ProcessNotificationQueue();
-		Assert.AreEqual(m_Calls, 3, "Expected third call after redo");
+		window.watch.ProcessNotificationQueue();
+		Assert.AreEqual(window.calls, 3, "Expected third call after redo");
 
 		handle.Dispose();
 		window.Close();
--- MaterialGraphProject/Assets/NewUI/Editor/GraphViewDataMapper.cs
+++ MaterialGraphProject/Assets/NewUI/Editor/GraphViewDataMapper.cs
@@ -1,55 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-
 namespace RMGUI.GraphView
 {
-	public class GraphViewDataMapper
+	public class GraphViewDataMapper : BaseDataMapper<GraphElementPresenter, GraphElement>
 	{
-		private readonly Dictionary<Type, Type> m_DataToViewDict = new Dictionary<Type, Type>();
-
-		public Type this[Type t]
+		public GraphViewDataMapper() : base(typeof(FallbackGraphElement))
 		{
-			get
-			{
-				return m_DataToViewDict[t];
-			}
-			set
-			{
-				if (!t.IsSubclassOf(typeof(GraphElementPresenter)))
-				{
-					throw new ArgumentException("The type passed as key does not derive from UnityEngine.Object.");
-				}
-
-				if (!value.IsSubclassOf(typeof(GraphElement)))
-				{
-					throw new ArgumentException("The type passed as value does not derive from DataContainer.");
-				}
-
-				m_DataToViewDict[t] = value;
-			}
 		}
 
-		public GraphElement Create(GraphElementPresenter presenter)
+		public override GraphElement Create(GraphElementPresenter key)
 		{
-			Type viewType = null;
-			Type dataType = presenter.GetType();
-
-			while (viewType == null && dataType != typeof(GraphElementPresenter))
+			GraphElement elem = base.Create(key);
+			if (elem != null)
 			{
-				if (!m_DataToViewDict.TryGetValue(dataType, out viewType))
-				{
-					dataType = dataType.BaseType;
-				}
+				elem.presenter = key;
 			}
-
-			if (viewType == null)
-			{
-				viewType = typeof(FallbackGraphElement);
-			}
-
-			var dataContainer = (GraphElement)Activator.CreateInstance(viewType);
-			dataContainer.presenter = presenter;
-			return dataContainer;
+			return elem;
 		}
 	}
 }
