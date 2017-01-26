3f3256e3579fb266fd5a588f25eb1a9492e5e7ff
runes <runes@unity3d.com>
2017-01-26 16:12:33 +0100

    Added UNITY_EDITOR #if's to fix ps4 build

--- Assets/ScriptableRenderLoop/HDRenderPipeline/HDRenderPipeline.cs
+++ Assets/ScriptableRenderLoop/HDRenderPipeline/HDRenderPipeline.cs
@@ -1,7 +1,9 @@
 using UnityEngine.Rendering;
 using System.Collections.Generic;
 using System;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 
 namespace UnityEngine.Experimental.Rendering.HDPipeline
 {
