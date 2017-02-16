4797f2f588dcc62fb5721afe63faa8f8daf9057f
Julien Ignace <julien@unity3d.com>
2017-02-16 16:27:09 +0100

    Moved "Create Scene Setting" button to the Scene Setting management window.

--- Assets/ScriptableRenderPipeline/HDRenderPipeline/Editor/HDRenderPipelineMenuItems.cs
+++ Assets/ScriptableRenderPipeline/HDRenderPipeline/Editor/HDRenderPipelineMenuItems.cs
@@ -21,23 +21,5 @@ namespace UnityEditor.Experimental.Rendering.HDPipeline
             }
         }
 
-        [MenuItem("HDRenderPipeline/Create Scene Settings")]
-        public static void CreateSceneSettings()
-        {
-            var allSettings = Object.FindObjectsOfType<SceneSettings>();
-            var activeScene = SceneManager.GetActiveScene();
-            foreach (SceneSettings setting in allSettings)
-            {
-                if (setting.gameObject.scene == activeScene)
-                {
-                    EditorUtility.DisplayDialog("Create Scene Settings", "Scene settings already exist on scene " + activeScene.name + ". \n\n If you want to create on another scene, set it active (double click) first, then select this option in the menu.", "Got It!");
-                    return;
-                }
-            }
-
-            var manager = new GameObject();
-            manager.name = "Scene Settings";
-            manager.AddComponent<SceneSettings>();
-        }
     }
 }
