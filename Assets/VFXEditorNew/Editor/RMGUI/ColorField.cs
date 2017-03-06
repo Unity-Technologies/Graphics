fbcf3a26f675c3b7dcc0e293b578af7fa4275fc7
Tristan Genevet <tristan@unity3d.com>
2017-03-06 14:09:17 -0500

    Added RMGUI ColorField

--- Assets/VFXEditorNew/Editor/RMGUI/ColorField.cs
+++ Assets/VFXEditorNew/Editor/RMGUI/ColorField.cs
@@ -6,34 +6,14 @@ using UnityEngine.RMGUI.StyleEnums;
 
 namespace UnityEditor.VFX.RMGUI
 {
-    class ColorEventListener : Manipulator
-    {
-        System.Action<Color> m_onColorChanged;
-
-        public ColorEventListener(System.Action<Color> onColorChanged)
-        {
-            m_onColorChanged = onColorChanged;
-        }
-        public override EventPropagation HandleEvent(Event evt, VisualElement finalTarget)
-		{
-            if( evt.type == EventType.ExecuteCommand && evt.commandName == "ColorPickerChanged")
-            {
-                m_onColorChanged(ColorPicker.color);
-                return EventPropagation.Stop;
-            }
-
-            return EventPropagation.Continue;
-        }
-    }
-
     class ColorField : ValueControl<Color>
     {
         VisualElement m_Label;
 
         VisualElement m_ColorDisplay;
-
+        
         VisualElement m_AlphaDisplay;
-
+        VisualElement m_NotAlphaDisplay;
 
         VisualContainer CreateColorContainer()
         {
@@ -46,21 +26,29 @@ namespace UnityEditor.VFX.RMGUI
             m_ColorDisplay = new VisualElement(){height=10};
             m_ColorDisplay.AddToClassList("colordisplay");
             
-            m_AlphaDisplay = new VisualElement(){height=3};
+            m_AlphaDisplay = new VisualElement(){height=3,backgroundColor=Color.white};
             m_AlphaDisplay.AddToClassList("alphadisplay");
+
+            m_NotAlphaDisplay = new VisualElement() {height=3,backgroundColor=Color.black};
+            m_NotAlphaDisplay.AddToClassList("notalphadisplay");
+
+            VisualContainer alphaContainer = new VisualContainer() { flexDirection = FlexDirection.Row, height = 3 };
 
             m_ColorDisplay.AddManipulator(new Clickable(OnColorClick));
             m_AlphaDisplay.AddManipulator(new Clickable(OnColorClick));
 
             container.AddChild(m_ColorDisplay);
-            container.AddChild(m_AlphaDisplay);
+            container.AddChild(alphaContainer);
+
+            alphaContainer.AddChild(m_AlphaDisplay);
+            alphaContainer.AddChild(m_NotAlphaDisplay);
 
             return container;
         }
 
         void OnColorClick()
         {
-            ColorPicker.Show(GUIView.current, m_Value, true, true, new ColorPickerHDRConfig(0.0f, 100.0f, 0.0f, 100.0f));
+            ColorPicker.Show((System.Action < Color > )OnColorChanged, m_Value, true, true, new ColorPickerHDRConfig(0.0f, 100.0f, 0.0f, 100.0f));
         }
 
         public ColorField(string label) 
@@ -86,19 +74,24 @@ namespace UnityEditor.VFX.RMGUI
             AddChild(container);
 
             m_Label = existingLabel;
-
-            AddManipulator(new ColorEventListener(OnColorChanged));
         }
 
         void OnColorChanged(Color color)
         {
             SetValue(color);
+
+            if (onValueChanged != null)
+                onValueChanged();
+
+            Dirty(ChangeType.Repaint);
+
         }
 
         protected override void ValueToGUI()
         {
-              m_ColorDisplay.backgroundColor = new Color(m_Value.r,m_Value.g,m_Value.b,1);
-              m_AlphaDisplay.backgroundColor = new Color(m_Value.a,m_Value.a,m_Value.a,1);
+            m_ColorDisplay.backgroundColor = new Color(m_Value.r,m_Value.g,m_Value.b,1);
+            m_AlphaDisplay.flex = m_Value.a;
+            m_NotAlphaDisplay.flex = 1-m_Value.a;
         }
 
     }
