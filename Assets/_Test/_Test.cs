f945e99ee70cb3224c0d953cd63ce387d6f9a230
Paul Demeulenaere <pauld@unity3d.com>
2017-03-17 17:22:58 +0100

    Add test file (this folder will be delete before merge in vfx-editor-main)

---
+++ Assets/_Test/_Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Test : MonoBehaviour {
+
+
+    public void GOOOOO()
+    {
+
+    }
+
+    public int i;
+	public Color a;
+	public Vector2 b;
+	public float c;
+	// Use this for initialization
+	void Start () {
+		
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
