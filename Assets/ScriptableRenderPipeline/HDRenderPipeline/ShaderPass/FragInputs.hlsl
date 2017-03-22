40a41ec901f50f26b07ca4498338977df7c86f8d
sebastienlagarde <sebastien@unity3d.com>
2017-03-22 19:11:57 +0100

    HDRenderPipeline: Fix one missing modification for relative path

--- Assets/ScriptableRenderPipeline/HDRenderPipeline/ShaderPass/FragInputs.hlsl
+++ Assets/ScriptableRenderPipeline/HDRenderPipeline/ShaderPass/FragInputs.hlsl
@@ -3,7 +3,7 @@
 // This structure gather all possible varying/interpolator for this shader.
 //-------------------------------------------------------------------------------------
 
-#include "../../Debug/DebugViewMaterial.cs.hlsl"
+#include "../Debug/DebugViewMaterial.cs.hlsl"
 
 struct FragInputs
 {
