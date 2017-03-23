fd5d9bf1e2ccff9f48966fdbf4b0fc2b0e7e0793
Julien Fryer <julienf@unity3d.com>
2017-03-23 16:06:13 +0100

    Fix removal of subassets

--- Assets/VFXEditorNew/Editor/Models/VFXGraphAsset.cs
+++ Assets/VFXEditorNew/Editor/Models/VFXGraphAsset.cs
@@ -18,37 +18,49 @@ namespace UnityEditor.VFX
         [SerializeField]
         private VFXGraph m_Root;
 
-        public void UpdateSubAssets()
+        public bool UpdateSubAssets()
         {
+            bool modified = false;
             if (EditorUtility.IsPersistent(this))
             {
                 Profiler.BeginSample("UpdateSubAssets");
-
+ 
                 try
                 {
                     HashSet<Object> persistentObjects = new HashSet<Object>(AssetDatabase.LoadAllAssetsAtPath(AssetDatabase.GetAssetPath(this)));
                     persistentObjects.Remove(this);
 
-                    //foreach (var o in persistentObjects)
-                    //    Debug.Log("PERSISTENT: " + o);
+                  //  foreach (var o in persistentObjects)
+                  //      Debug.Log("PERSISTENT: " + o);
 
                     HashSet<Object> currentObjects = new HashSet<Object>();
 
                     m_Root.CollectDependencies(currentObjects);
                     currentObjects.Add(m_Root);
 
-                    //foreach (var o in currentObjects)
-                    //    Debug.Log("CURRENT: " + o);
+                   // foreach (var o in currentObjects)
+                  //      Debug.Log("CURRENT: " + o);
 
                     // Add sub assets that are not already present
+                    
                     foreach (var obj in currentObjects)
                         if (!persistentObjects.Contains(obj))
+                        {
+                            //Debug.Log("ADD: " + obj);
                             AssetDatabase.AddObjectToAsset(obj, this);
+                            modified = true;
+                        }
 
                     // Remove sub assets that are not referenced anymore
                     foreach (var obj in persistentObjects)
                         if (!currentObjects.Contains(obj))
-                            ScriptableObject.DestroyImmediate(obj, true);
+                        {
+                           // Debug.Log("REMOVE: " + obj);
+                           // ScriptableObject.DestroyImmediate(obj, true);
+                            AssetDatabase.RemoveObject(obj);
+                            //AssetDatabase.AddObjectToAsset(obj, (Object)null);
+                            modified = true;
+                        }
                 }
                 catch (Exception e)
                 {
@@ -56,13 +68,21 @@ namespace UnityEditor.VFX
                 }
 
                 Profiler.EndSample();
+
+                if (modified)
+                    EditorUtility.SetDirty(this);
             }
+
+            return modified;
         }
 
         private void OnModelInvalidate(VFXModel model,VFXModel.InvalidationCause cause)
         {
             if (cause == VFXModel.InvalidationCause.kStructureChanged)
-                UpdateSubAssets();
+                if (UpdateSubAssets())
+                {
+                    //AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(this));
+                }
 
             EditorUtility.SetDirty(this);
         }
