9e9f34c2b71b9ee4799b50e343c12d1c93afedc2
Felipe Lira <felipe@unity3d.com>
2017-03-23 14:05:28 +0100

    Changed the upgrader to apply conversion parameters in the AssignNewShadertoMaterial callback so when changing the shader in the material UI it also upgrade properly.

--- Assets/LowEndMobilePipeline/Editor/LowendUpgraders.cs
+++ Assets/LowEndMobilePipeline/Editor/LowendUpgraders.cs
@@ -1,30 +1,11 @@
 ﻿using UnityEditor.Experimental.Rendering;
 using UnityEngine;
 
-public struct UpgradeParams
-{
-    public float blendMode;
-    public float specularSource;
-    public float glosinessSource;
-    public float reflectionSource;
-}
-
 public class LegacyBlinnPhongUpgrader : MaterialUpgrader
 {
-    public LegacyBlinnPhongUpgrader(string oldShaderName, UpgradeParams upgraderParams)
+    public LegacyBlinnPhongUpgrader(string oldShaderName)
     {
         RenameShader(oldShaderName, "ScriptableRenderPipeline/LowEndMobile/NonPBR");
-        SetNewFloatProperty("_Mode", upgraderParams.blendMode);
-        SetNewFloatProperty("_SpecSource", upgraderParams.specularSource);
-        SetNewFloatProperty("_GlossinessSource", upgraderParams.glosinessSource);
-        SetNewFloatProperty("_ReflectionSource", upgraderParams.reflectionSource);
-
-        if (oldShaderName.Contains("Legacy Shaders/Self-Illumin"))
-        {
-            RenameTexture("_MainTex", "_EmissionMap");
-            RemoveTexture("_MainTex");
-            SetNewColorProperty("_EmissionColor", Color.white);
-        }
     }
 }
 
@@ -58,6 +39,6 @@ public class TerrainUpgrader : MaterialUpgrader
     public TerrainUpgrader(string oldShaderName)
     {
         RenameShader(oldShaderName, "ScriptableRenderPipeline/LowEndMobile/NonPBR");
-        SetNewFloatProperty("_Shininess", 1.0f);
+        SetFloat("_Shininess", 1.0f);
     }
 }
