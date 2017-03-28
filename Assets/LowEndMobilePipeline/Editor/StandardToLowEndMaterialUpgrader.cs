4f647a16a382eeb8088498ce605b61a3ac2cdb01
Felipe Lira <felipe@unity3d.com>
2017-03-28 12:17:01 +0200

    Changes to lowend upgraders after material upgrader callback.

--- Assets/LowEndMobilePipeline/Editor/StandardToLowEndMaterialUpgrader.cs
+++ Assets/LowEndMobilePipeline/Editor/StandardToLowEndMaterialUpgrader.cs
@@ -1,31 +1,32 @@
 ﻿using System.Collections.Generic;
-using UnityEditor;
-using UnityEditor.Experimental.Rendering;
 
-public class StandardToLowEndMaterialUpgrader
+namespace UnityEditor.Experimental.Rendering.LowendMobile
 {
-    [MenuItem("RenderPipeline/LowEndMobilePipeline/Material Upgraders/Upgrade Standard Materials to Low End Mobile - Project Folder", false, 1)]
-    private static void UpgradeMaterialsToLDProject()
+    public class StandardToLowEndMaterialUpgrader
     {
-        List<MaterialUpgrader> upgraders = new List<MaterialUpgrader>();
-        GetUpgraders(ref upgraders);
+        [MenuItem("RenderPipeline/LowEndMobilePipeline/Material Upgraders/Upgrade Standard Materials to Low End Mobile - Project Folder", false, 1)]
+        private static void UpgradeMaterialsToLDProject()
+        {
+            List<MaterialUpgrader> upgraders = new List<MaterialUpgrader>();
+            GetUpgraders(ref upgraders);
 
-        MaterialUpgrader.UpgradeProjectFolder(upgraders, "Upgrade to LD Materials");
-    }
+            MaterialUpgrader.UpgradeProjectFolder(upgraders, "Upgrade to LD Materials");
+        }
 
-    [MenuItem("RenderPipeline/LowEndMobilePipeline/Material Upgraders/Upgrade Standard Materials to Low End Mobile - Selection", false, 2)]
-    private static void UpgradeMaterialsToLDSelection()
-    {
-        List<MaterialUpgrader> upgraders = new List<MaterialUpgrader>();
-        GetUpgraders(ref upgraders);
+        [MenuItem("RenderPipeline/LowEndMobilePipeline/Material Upgraders/Upgrade Standard Materials to Low End Mobile - Selection", false, 2)]
+        private static void UpgradeMaterialsToLDSelection()
+        {
+            List<MaterialUpgrader> upgraders = new List<MaterialUpgrader>();
+            GetUpgraders(ref upgraders);
 
-        MaterialUpgrader.UpgradeSelection(upgraders, "Upgrade to LD Materials");
-    }
+            MaterialUpgrader.UpgradeSelection(upgraders, "Upgrade to LD Materials");
+        }
 
-    private static void GetUpgraders(ref List<MaterialUpgrader> upgraders)
-    {
-        upgraders.Add(new StandardUpgrader("Standard (Specular setup)"));
-        upgraders.Add(new StandardUpgrader("Standard"));
-        upgraders.Add(new TerrainUpgrader("TerrainSurface"));
+        private static void GetUpgraders(ref List<MaterialUpgrader> upgraders)
+        {
+            upgraders.Add(new StandardUpgrader("Standard (Specular setup)"));
+            upgraders.Add(new StandardUpgrader("Standard"));
+            upgraders.Add(new TerrainUpgrader("TerrainSurface"));
+        }
     }
 }
