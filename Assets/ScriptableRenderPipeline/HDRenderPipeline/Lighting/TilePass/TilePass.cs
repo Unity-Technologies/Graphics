d4deb805d50c0d24da105a2f0a3497de92728d60
Evgenii Golubev <evgenii@unity3d.com>
2017-04-11 15:34:35 +0200

    Increase the maximal number of lights on screen

--- Assets/ScriptableRenderPipeline/HDRenderPipeline/Lighting/TilePass/TilePass.cs
+++ Assets/ScriptableRenderPipeline/HDRenderPipeline/Lighting/TilePass/TilePass.cs
@@ -195,8 +195,8 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
         {
             public const int k_MaxDirectionalLightsOnScreen = 4;
             public const int k_MaxPunctualLightsOnScreen    = 512;
-            public const int k_MaxAreaLightsOnScreen        = 8;
-            public const int k_MaxProjectorLightsOnScreen   = 16;
+            public const int k_MaxAreaLightsOnScreen        = 64;
+            public const int k_MaxProjectorLightsOnScreen   = 64;
             public const int k_MaxLightsOnScreen = k_MaxDirectionalLightsOnScreen + k_MaxPunctualLightsOnScreen + k_MaxAreaLightsOnScreen + k_MaxProjectorLightsOnScreen;
             public const int k_MaxEnvLightsOnScreen = 64;
             public const int k_MaxShadowOnScreen = 16;
