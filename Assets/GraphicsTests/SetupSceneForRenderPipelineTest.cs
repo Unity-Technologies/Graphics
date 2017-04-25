584bf8b6559e24dbbf30c79c683c49fb3b3a5cb7
Tim Cooper <tim@unity3d.com>
2017-04-25 20:55:01 +0200

    Initial pass at embedded gfx test framework for

---
+++ Assets/GraphicsTests/SetupSceneForRenderPipelineTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Experimental.Rendering;
+
+namespace UnityEngine.Experimental.Rendering
+{
+	[ExecuteInEditMode]
+	public class SetupSceneForRenderPipelineTest : MonoBehaviour 
+	{
+		private RenderPipelineAsset m_OriginalAsset;
+		public RenderPipelineAsset renderPipeline;
+		public Camera cameraToUse;
+
+		public int width = 1280;
+		public int height = 720;
+
+		void OnEnable()
+		{
+			m_OriginalAsset = GraphicsSettings.renderPipelineAsset;
+			GraphicsSettings.renderPipelineAsset = renderPipeline;
+		}
+
+		void OnDisable()
+		{
+			GraphicsSettings.renderPipelineAsset = m_OriginalAsset;
+		}
+	}
+}
