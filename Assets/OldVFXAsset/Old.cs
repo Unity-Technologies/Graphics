cf7d68f8f529fd2146a9aa9a1cb6f5fe26b8310c
Paul Demeulenaere <pauld@unity3d.com>
2017-04-28 11:20:36 +0200

    Simple & Basic Old VFXAsset (for reference)

---
+++ Assets/OldVFXAsset/Old.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class Old : MonoBehaviour {
+
+    void Start () 
+    {
+        var computeShader = AssetDatabase.LoadAssetAtPath<ComputeShader>("Assets/VFXShaders/VFXFillIndirectArgs.compute"); //HACK : issue with VFXManager Settings...
+        if (computeShader)
+        {
+            VFXComponent.SetIndirectCompute(computeShader);
+        }
+        else
+        {
+            Debug.LogErrorFormat("Unable to retrieve VFXFillIndirectArgs");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        
+    }
+}
