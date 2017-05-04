8b3da55e704ed1bee29f69a1e3c695836017b9db
Tristan Genevet <tristan@unity3D.com>
2017-05-04 15:07:37 +0200

    fix for paramterter initialisation

--- Assets/VFXEditorNew/Editor/Models/Parameters/VFXParameter.cs
+++ Assets/VFXEditorNew/Editor/Models/Parameters/VFXParameter.cs
@@ -52,7 +52,8 @@ namespace UnityEditor.VFX
                 VFXSlot slot = VFXSlot.Create(new VFXProperty(_type, "o"), VFXSlot.Direction.kOutput);
                 AddSlot(slot);
 
-                slot.value = System.Activator.CreateInstance(_type);
+                if( ! typeof(UnityEngine.Object).IsAssignableFrom(_type))
+                    slot.value = System.Activator.CreateInstance(_type);
             }
             else
             {
