cbe8949562c1a773293cf4e6569b6b2dfc7ea091
sebastienlagarde <sebastien@unity3d.com>
2017-05-11 14:14:15 +0200

    HDRenderPipeline: Add thin material option + Fix issue with transmission and shadow
    - Add a thin material option on SSS profile
    - Shadow was not working anymore with transmission due to merge conflict
    with shadow branch
    - Done a pass of format on file in Lit directory
    - Share constant for number of profile between c#a and hlsl

---
+++ Assets/ScriptableRenderPipeline/HDRenderPipeline/Material/Lit/SubsurfaceScatteringProfile.cs.hlsl
@@ -0,0 +1,13 @@
+//
+// This file was automatically generated from Assets/ScriptableRenderPipeline/HDRenderPipeline/Material/Lit/SubsurfaceScatteringProfile.cs.  Please don't edit by hand.
+//
+
+#ifndef SUBSURFACESCATTERINGPROFILE_CS_HLSL
+#define SUBSURFACESCATTERINGPROFILE_CS_HLSL
+//
+// UnityEngine.Experimental.Rendering.HDPipeline.SSSConstants:  static fields
+//
+#define SSS_PROFILES_MAX (8)
+
+
+#endif
