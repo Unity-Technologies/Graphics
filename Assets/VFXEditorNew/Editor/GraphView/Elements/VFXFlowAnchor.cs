fc909fc93b156acf9d092a291384996166602567
Tristan Genevet <tristan_genevet@mac.com>
2017-05-11 14:09:16 +0100

    Create and link the natural context when alt-dropping a flow edge.

--- Assets/VFXEditorNew/Editor/GraphView/Elements/VFXFlowAnchor.cs
+++ Assets/VFXEditorNew/Editor/GraphView/Elements/VFXFlowAnchor.cs
@@ -101,7 +101,50 @@ namespace UnityEditor.VFX.UI
             }
             else if (Event.current.modifiers == EventModifiers.Alt)
             {
+                VFXContextType targetContextType = VFXContextType.kNone;
                 //TODO create the most obvious context and link
+                if( presenter.direction == Direction.Input)
+                {
+
+                    switch (presenter.Owner.contextType)
+                    {
+                        case VFXContextType.kUpdate:
+                            targetContextType = VFXContextType.kInit;
+                        break;
+                        case VFXContextType.kOutput:
+                            targetContextType = VFXContextType.kUpdate;
+                        break;
+                    }
+                }
+                else
+                {
+                    switch (presenter.Owner.contextType)
+                    {
+                        case VFXContextType.kUpdate:
+                            targetContextType = VFXContextType.kOutput;
+                        break;
+                        case VFXContextType.kInit:
+                            targetContextType = VFXContextType.kUpdate;
+                        break;
+                    }
+                }
+
+                if(targetContextType != VFXContextType.kNone)
+                {
+                    var contextDesc = VFXLibrary.GetContexts().FirstOrDefault(t=>t.CreateInstance().contextType == targetContextType);
+                    if( contextDesc != null)
+                    {
+                        VFXContext newContext = viewPresenter.AddVFXContext(position - new Vector2(188,presenter.direction == Direction.Input ? 92 : 16),contextDesc);
+
+                        VFXContextPresenter newContextPresenter = viewPresenter.elements.OfType<VFXContextPresenter>().FirstOrDefault(t=>t.model == newContext);
+
+                        VFXFlowEdgePresenter edgePresenter = new VFXFlowEdgePresenter();
+                        edgePresenter.input = presenter.direction == Direction.Input ? presenter : newContextPresenter.inputAnchors[0] ;
+                        edgePresenter.output = presenter.direction == Direction.Output ? presenter : newContextPresenter.outputAnchors[0];
+
+                        viewPresenter.AddElement(edgePresenter);
+                    }
+                }
             }
         }
     }
