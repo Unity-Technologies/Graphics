d922d15ad34465cb8de30097ab2df51e37c69034
RinaldoTjan <rinaldo@unity3d.com>
2017-05-22 21:07:26 +0200

    Added BlendMode
    Soft Light need to be fixed.
    Still missing the rest blend mode. (Will add again tomorro).

---
+++ MaterialGraphProject/Assets/Rinaldo/BlendModesEnum.cs
@@ -0,0 +1,42 @@
+﻿namespace UnityEngine.MaterialGraph
+{
+    public enum BlendModesEnum
+    {
+        Burn,
+        Dodge,
+        Darken,
+        Difference,
+//        Divide,
+        Exclusion,
+        HardLight,
+//        HardMix,
+        Lighten,
+//        LinearDodge,
+//        LinearLight,
+//       LinearBurn,
+        Multiply,
+        Negation,
+        Overlay,
+//        PinLight,
+        Screen,
+        SoftLight,
+//        Substract,
+//        VividLight
+
+
+/* Old Order 
+Multiply
+Screen
+Darken
+Lighten
+Difference
+Negation
+Exclusion
+Overlay
+HardLight
+SoftLight
+Dodge (ColorDodge)
+Burn (ColorBurn)
+*/
+    }
+}
