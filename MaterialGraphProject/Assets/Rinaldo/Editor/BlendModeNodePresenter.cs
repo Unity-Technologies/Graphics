2347769bcce8e9c451ec911547263a73f0c9fda2
RinaldoTjan <rinaldo@unity3d.com>
2017-05-22 21:07:26 +0200

    Added BlendMode
    Soft Light need to be fixed.
    Still missing the rest blend mode. (Will add again tomorro).

---
+++ MaterialGraphProject/Assets/Rinaldo/Editor/BlendModeNodePresenter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using RMGUI.GraphView;
+using UnityEditor.Graphing.Drawing;
+using UnityEngine.MaterialGraph;
+
+namespace UnityEditor.MaterialGraph.Drawing
+{
+    [Serializable]
+    class BlendModeNodeControlPresenter : GraphControlPresenter
+    {
+        public override void OnGUIHandler()
+        {
+            base.OnGUIHandler();
+
+            var cNode = node as BlendModeNode;
+            if (cNode == null)
+                return;
+
+            cNode.blendMode = (BlendModesEnum)EditorGUILayout.EnumPopup("Mode", cNode.blendMode);
+        }
+
+ /*       public override float GetHeight()
+        {
+            return EditorGUIUtility.singleLineHeight + 2 * EditorGUIUtility.standardVerticalSpacing;
+        }
+ */
+        public override float GetHeight()
+        {
+            return 3 * (EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing) + EditorGUIUtility.standardVerticalSpacing;
+        }
+    }
+
+    [Serializable]
+    public class BlendModeNodePresenter : MaterialNodePresenter
+    {
+        protected override IEnumerable<GraphElementPresenter> GetControlData()
+        {
+            var instance = CreateInstance<BlendModeNodeControlPresenter>();
+            instance.Initialize(node);
+            return new List<GraphElementPresenter> { instance };
+        }
+    }
+}
