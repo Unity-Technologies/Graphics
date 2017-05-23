ebb1b092379634080fcfb618165c884c35f82e2b
Florent Guinier <florent@unity3d.com>
2017-05-23 17:15:44 +0200

    Add CombineNode

--- MaterialGraphProject/Assets/UnityShaderEditor/Runtime/Nodes/Channel/SplitNode.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Runtime/Nodes/Channel/SplitNode.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 using UnityEngine.Graphing;
 
 namespace UnityEngine.MaterialGraph
