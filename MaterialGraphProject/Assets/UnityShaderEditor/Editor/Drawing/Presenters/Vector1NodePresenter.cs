db3f8d6692a292e1d38672e55f22525ae13d5a1b
Andre McGrail <andremcgrail@Andres-MacBook-Pro.local>
2017-05-25 01:35:42 +0200

    REPO FIX

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Presenters/Vector1NodePresenter.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Presenters/Vector1NodePresenter.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using RMGUI.GraphView;
 using UnityEditor.Graphing.Drawing;
 using UnityEngine.MaterialGraph;
+using UnityEngine;
 
 namespace UnityEditor.MaterialGraph.Drawing
 {
     class Vector1ControlPresenter : GraphControlPresenter
     {
+		[SerializeField]
+		private int dynamicHeight;
+
         public override void OnGUIHandler()
         {
             base.OnGUIHandler();
@@ -16,12 +20,36 @@ namespace UnityEditor.MaterialGraph.Drawing
             if (tNode == null)
                 return;
 
-            tNode.value = EditorGUILayout.FloatField("Value:", tNode.value);
+			tNode.floatType = (FloatPropertyChunk.FloatType)EditorGUILayout.EnumPopup ("Float", tNode.floatType);
+
+			switch(tNode.floatType){
+			case FloatPropertyChunk.FloatType.Float:
+				dynamicHeight = 8;
+				tNode.value = EditorGUILayout.FloatField ("Value:", tNode.value);
+				break;
+			case FloatPropertyChunk.FloatType.Range:
+				dynamicHeight = 16;
+				Vector3 ranges = tNode.rangeValues;
+				tNode.value = EditorGUILayout.Slider (tNode.value, tNode.rangeValues.x, tNode.rangeValues.y);
+				EditorGUILayout.BeginHorizontal ();
+				ranges.x = EditorGUILayout.FloatField (ranges.x);
+				//very dirty...
+				for (int i = 0; i < 20; i++) {
+					EditorGUILayout.Space ();
+				}
+				ranges.y = EditorGUILayout.FloatField (ranges.y);
+				EditorGUILayout.EndHorizontal ();
+				tNode.rangeValues = ranges;
+				break;
+			}
+
+            
+
         }
 
         public override float GetHeight()
         {
-            return (EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing) + EditorGUIUtility.standardVerticalSpacing;
+			return (EditorGUIUtility.singleLineHeight + dynamicHeight * EditorGUIUtility.standardVerticalSpacing) + EditorGUIUtility.standardVerticalSpacing;
         }
     }
 
