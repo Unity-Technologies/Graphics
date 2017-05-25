80b6c2c52bc6ed0696aa19d59b4cb92cbd353328
ChrisTchou <ctchou@unity3d.com>
2017-05-25 13:35:56 -0700

    Custom node with switchable inputs, warp node types and AA checkerboard

---
+++ MaterialGraphProject/Assets/NewNodes/WIP/AACheckerBoardNode.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine.Graphing;
+
+namespace UnityEngine.MaterialGraph
+{
+    [Title("Procedural/AACheckerboard")]
+    public class AACheckerboardNode : AnyNode<AACheckerboardNode.Definition>
+    {
+        public class Definition : IAnyNodeDefinition
+        {
+            public string name { get { return "AACheckerboard"; } }
+
+            public AnyNodeProperty[] properties
+            {
+                get
+                {
+                    return new AnyNodeProperty[]
+                    {
+                           // slotId is the 'immutable' value we used to connect things
+                            new AnyNodeProperty { slotId= 0,    name = "inUVs",       description = "Input UV coords",          propertyType = PropertyType.Vector2,    value = Vector4.zero,                       state = AnyNodePropertyState.Slot },
+                            new AnyNodeProperty { slotId= 1,    name = "A",           description = "color A",                  propertyType = PropertyType.Vector4,    value= new Vector4(0.2f, 0.2f, 0.2f, 0.2f), state = AnyNodePropertyState.Constant },
+                            new AnyNodeProperty { slotId= 2,    name = "B",           description = "color B",                  propertyType = PropertyType.Vector4,    value= new Vector4(0.7f, 0.7f, 0.7f, 0.7f), state = AnyNodePropertyState.Constant },
+                            new AnyNodeProperty { slotId= 3,    name = "aaTweak",     description = "AA Tweak",                 propertyType = PropertyType.Vector3,    value= new Vector4(0.05f, 3.0f, 0.0f, 0.0f),state = AnyNodePropertyState.Constant },
+                            new AnyNodeProperty { slotId= 4,    name = "frequency",   description = "Frequency",                propertyType = PropertyType.Vector2,    value = Vector4.one,                        state = AnyNodePropertyState.Constant },
+                    };
+                }
+            }
+
+            public AnyNodeSlot[] outputs
+            {
+                get
+                {
+                    return new AnyNodeSlot[]
+                    {
+                            new AnyNodeSlot { slotId= 5,    name = "outColor", description = "Output color", slotValueType = SlotValueType.Vector4, value = Vector4.zero  }
+                    };
+                }
+            }
+
+            public string hlsl
+            {
+                get
+                {
+                    return
+                        "float4 derivatives = float4(ddx(inUVs), ddy(inUVs));\n" +
+                        "float2 duv_length = sqrt(float2(dot(derivatives.xz, derivatives.xz), dot(derivatives.yw, derivatives.yw)));\n" +
+                        "float width = 0.5f;\n" +
+                        "float2 distance3 = 2.0f * abs(frac(inUVs.xy * frequency) - 0.5f) - width;\n" +
+                        "float2 scale = aaTweak.x / duv_length.xy;\n" +
+                        "float2 blend_out = saturate((scale - aaTweak.zz) / (aaTweak.yy - aaTweak.zz));\n" +
+                        "float2 vector_alpha = clamp(distance3 * scale.xy * blend_out.xy, -1.0f, 1.0f);\n" +
+                        "float alpha = saturate(0.5f + 0.5f * vector_alpha.x * vector_alpha.y);\n" +
+                        "outColor= lerp(A, B, alpha.xxxx);";
+                }
+            }
+        }
+    }
+}
---
+++ MaterialGraphProject/Assets/NewNodes/WIP/RadialShearNode.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine.Graphing;
+
+namespace UnityEngine.MaterialGraph
+{
+    [Title("UV/RadialShearNode")]
+    public class RadialShearNode : AnyNode<RadialShearNode.Definition>
+    {
+        public class Definition : IAnyNodeDefinition
+        {
+            public string name { get { return "RadialShear"; } }
+
+            public AnyNodeProperty[] properties
+            {
+                get
+                {
+                    return new AnyNodeProperty[]
+                    {
+                           // slotId is the 'immutable' value we used to connect things
+                            new AnyNodeProperty { slotId= 0,    name = "inUVs",       description = "Input UV coords",          propertyType = PropertyType.Vector2,    value = Vector4.zero,                       state = AnyNodePropertyState.Slot },
+                            new AnyNodeProperty { slotId= 1,    name = "center",      description = "UV radial center point",   propertyType = PropertyType.Vector2,    value= new Vector4(0.5f, 0.5f, 0.5f, 0.5f), state = AnyNodePropertyState.Constant },
+                            new AnyNodeProperty { slotId= 2,    name = "shearAmount", description = "Shear amount",             propertyType = PropertyType.Vector2,    value= Vector4.one,                         state = AnyNodePropertyState.Constant },
+                            new AnyNodeProperty { slotId= 3,    name = "offset",      description = "UV offset",                propertyType = PropertyType.Vector2, value= Vector4.zero,                           state = AnyNodePropertyState.Constant },
+                    };
+                }
+            }
+
+            public AnyNodeSlot[] outputs
+            {
+                get
+                {
+                    return new AnyNodeSlot[]
+                    {
+                            new AnyNodeSlot { slotId= 4,    name = "outUVs", description = "Output UV texture coordinates", slotValueType = SlotValueType.Vector2, value = Vector4.zero  }
+                    };
+                }
+            }
+
+            public string hlsl
+            {
+                get
+                {
+                    return
+                        "float2 delta = inUVs - center;\n" +
+                        "float delta2 = dot(delta.xy, delta.xy);\n" +
+                        "float2 delta_offset = delta2 * shearAmount;\n" +
+                        "outUVs = inUVs + float2(delta.y, -delta.x) * delta_offset + offset;";
+                }
+            }
+        }
+    }
+
+}
