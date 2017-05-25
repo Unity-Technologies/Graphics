094b5f4704ba0f4d615bb4655ecb0e448c38a15b
Eduardo Chaves <dudurighi@gmail.com>
2017-05-25 08:19:42 -0700

    Fixing Height to Normal

--- MaterialGraphProject/Assets/Eduardo/HeightToNormalNode.cs
+++ MaterialGraphProject/Assets/Eduardo/HeightToNormalNode.cs
@@ -5,7 +5,7 @@ using System.Collections;
 namespace UnityEngine.MaterialGraph
 {
     [Title("Utility/Heightmap To Normalmap")]
-    public class HeightToNormalNode : FunctionNInNOut, IGeneratesFunction
+    public class HeightToNormalNode : FunctionNInNOut, IGeneratesFunction, IGenerateProperties
     {
 
         public HeightToNormalNode()
@@ -31,6 +31,14 @@ namespace UnityEngine.MaterialGraph
             get { return true; }
         }
 
+        public override void GeneratePropertyUsages(ShaderGenerator visitor, GenerationMode generationMode)
+        {
+            base.GeneratePropertyUsages(visitor, generationMode);
+
+            visitor.AddShaderChunk("#ifdef UNITY_COMPILER_HLSL", false);
+            visitor.AddShaderChunk("SamplerState my_linear_repeat_sampler;", false);
+            visitor.AddShaderChunk("#endif", false);
+        }
         public void GenerateNodeFunction(ShaderGenerator visitor, GenerationMode generationMode)
         {
             var outputString = new ShaderGenerator();
@@ -39,9 +47,17 @@ namespace UnityEngine.MaterialGraph
             outputString.AddShaderChunk("float2 offsetU = float2(texCoord.x + texOffset, texCoord.y);", false);
             outputString.AddShaderChunk("float2 offsetV = float2(texCoord.x, texCoord.y + texOffset);", false);
 
-            outputString.AddShaderChunk("float normalSample = heightmap.Sample(my_linear_repeat_sampler, texCoord).r;", false);
-            outputString.AddShaderChunk("float uSample = heightmap.Sample(my_linear_repeat_sampler, offsetU).r;", false);
-            outputString.AddShaderChunk("float vSample = heightmap.Sample(my_linear_repeat_sampler, offsetV).r;", false);
+            outputString.AddShaderChunk("float normalSample = 0;", false);
+            outputString.AddShaderChunk("float uSample = 0;", false);
+            outputString.AddShaderChunk("float vSample = 0;", false);
+
+            visitor.AddShaderChunk("#ifdef UNITY_COMPILER_HLSL", false);
+
+            outputString.AddShaderChunk("normalSample = heightmap.Sample(my_linear_repeat_sampler, texCoord).r;", false);
+            outputString.AddShaderChunk("uSample = heightmap.Sample(my_linear_repeat_sampler, offsetU).r;", false);
+            outputString.AddShaderChunk("vSample = heightmap.Sample(my_linear_repeat_sampler, offsetV).r;", false);
+
+            visitor.AddShaderChunk("#endif", false);
 
             outputString.AddShaderChunk("float uMinusNormal = uSample - normalSample;", false);
             outputString.AddShaderChunk("float vMinusNormal = vSample - normalSample;", false);
