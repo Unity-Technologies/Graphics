686951d000ae3af7afd6f0279549d859ea990ba8
Andre McGrail <andremcgrail@Andres-MacBook-Pro.local>
2017-05-25 21:39:38 +0200

    Fixed math in remap node Added second output for screenPos that is pure XY UVs

--- MaterialGraphProject/Assets/UnityShaderEditor/Runtime/Nodes/Input/SceneData/ScreenPosNode.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Runtime/Nodes/Input/SceneData/ScreenPosNode.cs
@@ -16,8 +16,10 @@ namespace UnityEngine.MaterialGraph
             UpdateNodeAfterDeserialization();
         }
 
-        private const int kOutputSlotId = 0;
-        private const string kOutputSlotName = "ScreenPos";
+        private const int kOutputSlot1Id = 0;
+        private const string kOutputSlot1Name = "Raw ScreenPos";
+		private const int kOutputSlot2Id = 1;
+		private const string kOutputSlot2Name = "Normalized";
 
         public override bool hasPreview { get { return true; } }
         public override PreviewMode previewMode
@@ -28,12 +30,22 @@ namespace UnityEngine.MaterialGraph
 
         public sealed override void UpdateNodeAfterDeserialization()
         {
-            AddSlot(new MaterialSlot(kOutputSlotId, kOutputSlotName, kOutputSlotName, SlotType.Output, SlotValueType.Vector4, Vector4.zero));
-            RemoveSlotsNameNotMatching(new[] { kOutputSlotId });
+            AddSlot(new MaterialSlot(kOutputSlot1Id, kOutputSlot1Name, kOutputSlot1Name, SlotType.Output, SlotValueType.Vector4, Vector4.zero));
+			AddSlot(new MaterialSlot(kOutputSlot2Id, kOutputSlot2Name, kOutputSlot2Name, SlotType.Output, SlotValueType.Vector3, Vector3.zero));
+			RemoveSlotsNameNotMatching(new[] { kOutputSlot1Id, kOutputSlot2Id });
         }
 
         public override string GetVariableNameForSlot(int slotId)
         {
+			string returnString = "";
+			switch (slotId){
+			case 0:
+				returnString = ShaderGeneratorNames.ScreenPosition;
+				break;
+			case 1:
+				returnString = "float3(" + ShaderGeneratorNames.ScreenPosition + ".xy / " + ShaderGeneratorNames.ScreenPosition + ".w, 0)";
+				break;
+			}
             return ShaderGeneratorNames.ScreenPosition;
         }
 
