d4c3a301106120340679eed14230ee9861c21aa9
Andre McGrail <andremcgrail@Andres-MacBook-Pro.local>
2017-05-25 02:16:45 +0200

    Added new modes to the Vector1node -Float(usual behaviour) -Toggle(changes to a toggle on both node and shader) -Range(adds a slider and min max values to both node and shader) -PowerSlider(same as above with the addition of a power value to give a curve growth) - DO NOT USE AS SPAMS CONSOLE ERRORS

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Presenters/Vector1NodePresenter.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Presenters/Vector1NodePresenter.cs
@@ -22,24 +22,49 @@ namespace UnityEditor.MaterialGraph.Drawing
 
 			tNode.floatType = (FloatPropertyChunk.FloatType)EditorGUILayout.EnumPopup ("Float", tNode.floatType);
 
+			Vector3 ranges = tNode.rangeValues;
+
 			switch(tNode.floatType){
 			case FloatPropertyChunk.FloatType.Float:
 				dynamicHeight = 8;
+				tNode.floatType = FloatPropertyChunk.FloatType.Float;
 				tNode.value = EditorGUILayout.FloatField ("Value:", tNode.value);
 				break;
 			case FloatPropertyChunk.FloatType.Range:
 				dynamicHeight = 16;
-				Vector3 ranges = tNode.rangeValues;
+				tNode.floatType = FloatPropertyChunk.FloatType.Range;
 				tNode.value = EditorGUILayout.Slider (tNode.value, tNode.rangeValues.x, tNode.rangeValues.y);
 				EditorGUILayout.BeginHorizontal ();
 				ranges.x = EditorGUILayout.FloatField (ranges.x);
 				//very dirty...
-				for (int i = 0; i < 20; i++) {
+				for (int i = 0; i < 15; i++) {
 					EditorGUILayout.Space ();
 				}
 				ranges.y = EditorGUILayout.FloatField (ranges.y);
 				EditorGUILayout.EndHorizontal ();
 				tNode.rangeValues = ranges;
+				break;
+			case FloatPropertyChunk.FloatType.PowerSlider:
+				dynamicHeight = 16;
+				tNode.floatType = FloatPropertyChunk.FloatType.PowerSlider;
+				tNode.value = EditorGUILayout.Slider (tNode.value, tNode.rangeValues.x, tNode.rangeValues.y);
+				EditorGUILayout.BeginHorizontal ();
+				ranges.x = EditorGUILayout.FloatField (ranges.x);
+				//very dirty...
+				for (int i = 0; i < 15; i++) {
+					EditorGUILayout.Space ();
+				}
+				ranges.y = EditorGUILayout.FloatField (ranges.y);
+				EditorGUILayout.EndHorizontal ();
+				ranges.z = EditorGUILayout.FloatField ("Power", ranges.z);
+				tNode.rangeValues = ranges;
+				break;
+			case FloatPropertyChunk.FloatType.Toggle:
+				dynamicHeight = 8;
+				bool toggleState = tNode.value == 0f ? false : true;
+				tNode.floatType = FloatPropertyChunk.FloatType.Toggle;
+				toggleState = EditorGUILayout.Toggle (toggleState);
+				tNode.value = toggleState == true ? 1f : 0f;
 				break;
 			}
 
