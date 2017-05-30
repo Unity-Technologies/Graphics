4dd6979a99b9123e13f24dfdd8c4a540988881c0
Tristan Genevet <tristan@unity3D.com>
2017-05-30 14:02:41 +0200

    edge removed from their anchor when removed. New Graphview version

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/GraphView/Elements/Presenters/VFXDataEdgePresenter.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/GraphView/Elements/Presenters/VFXDataEdgePresenter.cs
@@ -1,4 +1,4 @@
-using System.Linq;
+﻿using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Experimental.UIElements;
@@ -10,6 +10,28 @@ namespace UnityEditor.VFX.UI
     {
         public VFXDataEdgePresenter()
         {
+        }
+
+        public override UnityEngine.Object[] GetObjectsToWatch()
+        {
+            VFXDataAnchorPresenter anchorInput = input as VFXDataAnchorPresenter;
+            VFXDataAnchorPresenter anchorOutput = output as VFXDataAnchorPresenter;
+
+            return new UnityEngine.Object[] { this,anchorInput != null ? anchorInput.model : null,anchorOutput != null ? anchorOutput.model : null };
+        }
+
+        public override void OnRemoveFromGraph()
+        {
+            base.OnRemoveFromGraph();
+
+            VFXDataAnchorPresenter anchorInput = input as VFXDataAnchorPresenter;
+            VFXDataAnchorPresenter anchorOutput = output as VFXDataAnchorPresenter;
+
+            if( anchorInput != null )
+                anchorInput.Disconnect(this);
+            if( anchorOutput != null )
+                anchorOutput.Disconnect(this);
+
         }
     }
 }
