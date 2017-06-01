0febfd164bdbf97559b129246c7a60e1cd77bb65
Tristan Genevet <tristan@unity3D.com>
2017-06-01 15:48:16 +0200

    Added userfriendly name function ( ie float instead of single ).

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/GraphView/Elements/Presenters/VFXParameterPresenter.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/GraphView/Elements/Presenters/VFXParameterPresenter.cs
@@ -93,7 +93,7 @@ namespace UnityEditor.VFX.UI
         {
             if (parameter != null)
             {
-                title = node.outputSlots[0].property.type.Name + " " + node.m_OnEnabledCount;
+                title = node.outputSlots[0].property.type.UserFriendlyName() + " " + node.m_OnEnabledCount;
                 exposed = parameter.exposed;
                 exposedName = parameter.exposedName;
             }
---
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/Types/VFXTypeExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using UIElements.GraphView;
+using UnityEngine;
+
+namespace UnityEditor.VFX
+{
+    static class VFXTypeExtension 
+    {
+
+        static Dictionary<Type,string> s_FriendlyName = new Dictionary<Type, string>();
+
+        static VFXTypeExtension()
+        {
+            s_FriendlyName[typeof(float)] = "float";
+            s_FriendlyName[typeof(int)] = "int";
+            s_FriendlyName[typeof(bool)] = "bool";
+            s_FriendlyName[typeof(uint)] = "uint";
+            s_FriendlyName[typeof(char)] = "char";
+            s_FriendlyName[typeof(bool)] = "bool";
+            s_FriendlyName[typeof(double)] = "double";
+            s_FriendlyName[typeof(short)] = "short";
+            s_FriendlyName[typeof(ushort)] = "ushort";
+            s_FriendlyName[typeof(long)] = "long";
+            s_FriendlyName[typeof(ulong)] = "ulong";
+            s_FriendlyName[typeof(byte)] = "byte";
+            s_FriendlyName[typeof(sbyte)] = "sbyte";
+            s_FriendlyName[typeof(decimal)] = "decimal";
+            s_FriendlyName[typeof(char)] = "char";
+            s_FriendlyName[typeof(string)] = "string";
+        }
+
+        public static string UserFriendlyName(this Type type)
+        {
+            string result;
+            if( s_FriendlyName.TryGetValue(type,out result))
+            {
+                return result;
+            }
+            return type.Name;
+        }
+    }
+}
