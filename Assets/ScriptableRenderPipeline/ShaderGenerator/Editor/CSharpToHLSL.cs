b23b59d814957366f256c26f2b26f63c88f40fdc
sebastienlagarde <sebastien@unity3d.com>
2017-06-08 15:37:42 +0200

    Update hlsl generator for nested class and regenerate file for HD

--- Assets/ScriptableRenderPipeline/ShaderGenerator/Editor/CSharpToHLSL.cs
+++ Assets/ScriptableRenderPipeline/ShaderGenerator/Editor/CSharpToHLSL.cs
@@ -55,21 +55,13 @@ namespace UnityEditor.Experimental.Rendering
                     {
                         if (attr is GenerateHLSL)
                         {
-                            var parent = type.DeclaringType;
-                            if (parent != null)
+                            ShaderTypeGenerator gen;
+                            if (s_TypeName.TryGetValue(type.FullName, out gen))
                             {
-                                Debug.LogError("The GenerateHLSL attribute not supported on nested classes (" + type.FullName + "), skipping.");
+                                Debug.LogError( "Duplicate typename with the GenerateHLSL attribute detected: " + type.FullName +
+                                                " declared in both " + gen.type.Assembly.FullName + " and " + type.Assembly.FullName + ".  Skipping the second instance.");
                             }
-                            else
-                            {
-                                ShaderTypeGenerator gen;
-                                if (s_TypeName.TryGetValue(type.FullName, out gen))
-                                {
-                                    Debug.LogError("Duplicate typename with the GenerateHLSL attribute detected: " + type.FullName +
-                                        " declared in both " + gen.type.Assembly.FullName + " and " + type.Assembly.FullName + ".  Skipping the second instance.");
-                                }
-                                s_TypeName[type.FullName] = new ShaderTypeGenerator(type, attr as GenerateHLSL);
-                            }
+                            s_TypeName[type.FullName] = new ShaderTypeGenerator(type, attr as GenerateHLSL);
                         }
                     }
                 }
@@ -147,7 +139,7 @@ namespace UnityEditor.Experimental.Rendering
                         {
                             writer.Write(gen.EmitFunctions() + "\n");
                         }
-                    }                    
+                    }
 
                     writer.Write("\n#endif\n");
 
