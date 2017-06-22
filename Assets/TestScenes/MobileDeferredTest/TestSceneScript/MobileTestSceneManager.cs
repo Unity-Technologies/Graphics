9aa57010ccae9091d186a0c5227d4e424d74249e
Filip Iliescu <filip@unity3d.com>
2017-06-21 20:36:11 -0700

    create an iOS SceneManager script and prefab for looping through scenes

---
+++ Assets/TestScenes/MobileDeferredTest/TestSceneScript/MobileTestSceneManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MobileTestSceneManager : MonoBehaviour {
+
+	static int m_NextSceneIndex = 0;
+
+	static MobileTestSceneManager Instance;
+
+	// Use this for initialization
+	void Start () {
+		if (Instance != null) {
+			GameObject.Destroy (gameObject);
+		} else {
+			GameObject.DontDestroyOnLoad (gameObject);
+			Instance = this;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began) {
+			SceneManager.LoadScene(m_NextSceneIndex++);
+		}
+	}
+}
