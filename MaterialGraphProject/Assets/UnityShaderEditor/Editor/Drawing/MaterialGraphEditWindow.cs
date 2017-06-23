41d4f49e4f4b4b12d9693785c310a4f5c355f888
Tim Cooper <tim@unity3d.com>
2017-06-23 15:20:34 +0200

    Graph edit window now survives serialization.

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/MaterialGraphEditWindow.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/MaterialGraphEditWindow.cs
@@ -1,14 +1,31 @@
+using System;
 using RMGUI.GraphView;
 using UnityEditor.Graphing.Drawing;
+using UnityEngine;
+using UnityEngine.Graphing;
+using UnityEngine.MaterialGraph;
+using Object = UnityEngine.Object;
 
 namespace UnityEditor.MaterialGraph.Drawing
 {
     public class MaterialGraphEditWindow : AbstractGraphEditWindow
     {
-        [MenuItem("Window/Material Editor")]
-        public static void OpenMenu()
+        [SerializeField]
+        private Object m_Selected;
+
+        [SerializeField]
+        private MaterialGraphAsset m_InMemoryAsset;
+
+        public override IGraphAsset inMemoryAsset
         {
-            GetWindow<MaterialGraphEditWindow>();
+            get { return m_InMemoryAsset; }
+            set { m_InMemoryAsset = value as MaterialGraphAsset; }
+        }
+
+        public override Object selected
+        {
+            get { return m_Selected; }
+            set { m_Selected = value; }
         }
 
         public override AbstractGraphPresenter CreateDataSource()
--- MaterialGraphProject/Assets/UnityShaderEditor/Runtime/Graphs/MaterialGraphAsset.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Runtime/Graphs/MaterialGraphAsset.cs
@@ -1,6 +1,3 @@
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 #if UNITY_EDITOR
 using System.Reflection;
 using UnityEditor;
