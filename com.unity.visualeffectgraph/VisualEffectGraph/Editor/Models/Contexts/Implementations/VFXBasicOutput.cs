a37758e36e47c2cffe9e928b67da2434de65f971
Julien Fryer <julienf@unity3d.com>
2017-07-13 14:17:37 +0200

    Detect local attributes of particle data

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Contexts/Implementations/VFXBasicOutput.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Contexts/Implementations/VFXBasicOutput.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace UnityEditor.VFX
 {
     [VFXInfo]
@@ -5,5 +8,13 @@ namespace UnityEditor.VFX
     {
         public VFXBasicOutput() : base(VFXContextType.kOutput, VFXDataType.kParticle, VFXDataType.kNone) {}
         public override string name { get { return "Output"; } }
+
+        public override IEnumerable<VFXAttributeInfo> attributes
+        {
+            get
+            {
+                yield return new VFXAttributeInfo("position", VFXValueType.kFloat3, VFXAttributeMode.Read);
+            }
+        }
     }
 }
