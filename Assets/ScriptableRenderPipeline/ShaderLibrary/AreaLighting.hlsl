2bb7961161557b786cce28d23aed4776451b560a
Evgenii Golubev <evgenii@unity3d.com>
2017-07-13 13:15:27 +0200

    Remove the workaround for the PSSL compiler bug (it's been fixed)

--- Assets/ScriptableRenderPipeline/ShaderLibrary/AreaLighting.hlsl
+++ Assets/ScriptableRenderPipeline/ShaderLibrary/AreaLighting.hlsl
@@ -117,8 +117,7 @@ float DiffuseSphereLightIrradiance(float sinSqSigma, float cosOmega)
             float a = cosOmega * acos(x) - z;                       // y*ArcCos[-y*Sqrt[(1/x-1)/(1-y^2)]]-Sqrt[(1-y^2)*(x/(1-x))-y^2]*(1/x-1)
             float b = atan(y);                                      // ArcTan[Sqrt[(1-y^2)*(x/(1-x))-y^2]]
 
-            // Replacing max() with saturate() results in a 12 cycle SGPR forwarding stall on PS4.
-            return max(INV_PI * (a * sinSqSigma + b), 0);           // (a/Pi)*x+(b/Pi)
+            return saturate(INV_PI * (a * sinSqSigma + b));
         }
     #endif
 #endif
