e2d2b7e1ccf3e7901f5a7e48f6f3c701f61367c2
Tristan Genevet <tristan@unity3D.com>
2017-08-17 14:16:59 +0200

    Added PropertyIM for position.

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/GraphView/Views/Properties/VFXPropertyIM.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/GraphView/Views/Properties/VFXPropertyIM.cs
@@ -63,7 +63,8 @@ namespace UnityEditor.VFX.UI
             {typeof(int), typeof(VFXIntPropertyIM) },
             {typeof(bool), typeof(VFXBoolPropertyIM) },
             {typeof(Gradient), typeof(VFXGradientPropertyIM) },
-            {typeof(AnimationCurve), typeof(VFXAnimationCurvePropertyIM) }
+            {typeof(AnimationCurve), typeof(VFXAnimationCurvePropertyIM) },
+            {typeof(Position), typeof(VFXPositionPropertyIM) }
         };
         public static VFXPropertyIM Create(Type type, float labelWidth)
         {
@@ -148,37 +149,101 @@ namespace UnityEditor.VFX.UI
     }
 
 
-    /*
     abstract class VFXSpacedPropertyIM : VFXPropertyIM
     {
-        protected override void DoOnGUI(VFXNodeBlockPresenter presenter, ref VFXNodeBlockPresenter.PropertyInfo infos, VFXPropertyUI.GUIStyles styles)
+        protected override object DoOnGUI(VFXDataAnchorPresenter presenter)
         {
             GUILayout.BeginHorizontal();
-            Label(ref infos, styles);
+            Label(presenter, presenter.name);
 
+            Rect spaceRect = GUILayoutUtility.GetRect(24, 0, GUILayout.ExpandHeight(true));
+            SpaceButton(spaceRect, (Spaceable)presenter.value);
+
+            object result = DoOnParameterGUI(presenter);
             GUILayout.EndHorizontal();
+            return result;
         }
 
-        protected void SpaceControl(ref VFXNodeBlockPresenter.PropertyInfo infos)
+        protected override object DoOnGUI(Rect rect, string label, object value)
         {
+            Label(rect, label);
 
+            Rect spaceRect = rect;
+            spaceRect.xMin += m_LabelWidth - 24;
+            spaceRect.width = 24;
+            SpaceButton(spaceRect, (Spaceable)value);
+
+            object result = DoOnParameterGUI(rect, value, label);
+            return result;
         }
+
+        void SpaceButton(Rect rect, Spaceable value)
+        {
+            if (GUI.Button(rect, new GUIContent(Resources.Load<Texture2D>(string.Format("vfx/space{0}", value.space.ToString()))), GUIStyle.none))
+            {
+                value.space = (CoordinateSpace)((int)(value.space + 1) % (int)CoordinateSpace.SpaceCount);
+                GUI.changed = true;
+            }
+        }
+
+        protected abstract object DoOnParameterGUI(VFXDataAnchorPresenter presenter);
+        protected abstract object DoOnParameterGUI(Rect rect, object value, string label);
     }
     abstract class VFXSpacedPropertyIM<T> : VFXSpacedPropertyIM
     {
-        protected override void DoOnGUI(VFXNodeBlockPresenter presenter, ref VFXNodeBlockPresenter.PropertyInfo infos, VFXPropertyUI.GUIStyles styles)
+        protected override object DoOnParameterGUI(VFXDataAnchorPresenter presenter)
         {
-            GUILayout.BeginHorizontal();
-            Label(ref infos, styles);
-            infos.value = OnParameterGUI(ref infos, (T)infos.value, styles);
-            GUILayout.EndHorizontal();
+            return OnParameterGUI(presenter, (T)presenter.value, presenter.name);
         }
 
+        protected override object DoOnParameterGUI(Rect rect, object value, string label)
+        {
+            return OnParameterGUI(rect, (T)value, label);
+        }
 
-
-        public abstract T OnParameterGUI(ref VFXNodeBlockPresenter.PropertyInfo infos, T value, VFXPropertyUI.GUIStyles styles);
+        public abstract T OnParameterGUI(VFXDataAnchorPresenter presenter, T value, string label);
+        public abstract T OnParameterGUI(Rect rect, T value, string label);
     }
-    */
+
+
+    class VFXPositionPropertyIM : VFXSpacedPropertyIM<Position>
+    {
+        public override Position OnParameterGUI(VFXDataAnchorPresenter presenter, Position value, string label)
+        {
+            return value;
+        }
+
+        public override Position OnParameterGUI(Rect rect, Position value, string label)
+        {
+            rect.xMin += m_LabelWidth;
+
+            float paramWidth = Mathf.Floor(rect.width / 3);
+            float labelWidth = 20;
+
+            rect.width = labelWidth;
+            GUI.Label(rect, "x");
+            rect.xMin += rect.width;
+            rect.width = paramWidth - labelWidth;
+            value.position.x = EditorGUI.FloatField(rect, value.position.x);
+
+            rect.xMin += rect.width;
+            rect.width = labelWidth;
+            GUI.Label(rect, "y");
+            rect.xMin += rect.width;
+            rect.width = paramWidth - labelWidth;
+            value.position.y = EditorGUI.FloatField(rect, value.position.y);
+
+            rect.xMin += rect.width;
+            rect.width = labelWidth;
+            GUI.Label(rect, "z");
+            rect.xMin += rect.width;
+            rect.width = paramWidth - labelWidth;
+            value.position.y = EditorGUI.FloatField(rect, value.position.z);
+
+            return value;
+        }
+    }
+
 
     class VFXDefaultPropertyIM : VFXPropertyIM
     {
