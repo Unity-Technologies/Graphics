7f7392bd355f0ec5bf27498025ee52d28abd4e54
Tristan Genevet <tristan@unity3D.com>
2017-08-30 15:08:58 +0200

    NO WORKING temp version

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/GraphView/Elements/VFXEdgeControl.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/GraphView/Elements/VFXEdgeControl.cs
@@ -7,6 +7,212 @@ using UnityEngine.Experimental.UIElements.StyleSheets;
 
 namespace UnityEditor.VFX.UI
 {
+    internal class BezierSubdiv
+    {
+        public static void GetBezierSubDiv(List<Vector2> points, Vector2 start, Vector2 end, Vector2 tStart, Vector2 tEnd)
+        {
+            points.Clear();
+
+            points.Add(start);
+
+            AddBezierRecurse(points, start, end, tStart, tEnd, 0);
+
+            points.Add(end);
+        }
+
+        const float curve_collinearity_epsilon = 1e-5f;
+        const float curve_angle_tolerance_epsilon = 1e-5f;
+
+        const float distance_tolerance = 5f;
+        const float distance_tolerance_square = distance_tolerance * distance_tolerance;
+        const float angle_tolerance = 0.01f;
+        const float cusp_limit = 0.00f;
+
+        static void AddBezierRecurse(List<Vector2> points, Vector2 start, Vector2 end, Vector2 tStart, Vector2 tEnd, int level)
+        {
+            if (level > 100)
+            {
+                return;
+            }
+
+            float x1 = start.x;
+            float y1 = start.y;
+            float x2 = tStart.x;
+            float y2 = tStart.y;
+            float x3 = tEnd.x;
+            float y3 = tEnd.y;
+            float x4 = end.x;
+            float y4 = end.y;
+            // Calculate all the mid-points of the line segments
+            //----------------------
+            float x12 = (x1 + x2) / 2;
+            float y12 = (y1 + y2) / 2;
+            float x23 = (x2 + x3) / 2;
+            float y23 = (y2 + y3) / 2;
+            float x34 = (x3 + x4) / 2;
+            float y34 = (y3 + y4) / 2;
+            float x123 = (x12 + x23) / 2;
+            float y123 = (y12 + y23) / 2;
+            float x234 = (x23 + x34) / 2;
+            float y234 = (y23 + y34) / 2;
+            float x1234 = (x123 + x234) / 2;
+            float y1234 = (y123 + y234) / 2;
+
+            if (level > 0) // Enforce subdivision first time
+            {
+                // Try to approximate the full cubic curve by a single straight line
+                //------------------
+                float dx = x4 - x1;
+                float dy = y4 - y1;
+
+                float d2 = Mathf.Abs(((x2 - x4) * dy - (y2 - y4) * dx));
+                float d3 = Mathf.Abs(((x3 - x4) * dy - (y3 - y4) * dx));
+
+                float da1, da2;
+
+                //if (d2 > curve_collinearity_epsilon && d3 > curve_collinearity_epsilon)
+                {
+                    // Regular care
+                    //-----------------
+                    if ((d2 + d3) * (d2 + d3) <= distance_tolerance * (dx * dx + dy * dy))
+                    {
+                        points.Add(new Vector2(x1234, y1234));
+                        // If the curvature doesn't exceed the distance_tolerance value
+                        // we tend to finish subdivisions.
+                        //----------------------
+                        /*if (angle_tolerance < curve_angle_tolerance_epsilon)
+                        {
+                            points.Add(new Vector2(x1234, y1234));
+                            return;
+                        }
+
+                        // Angle & Cusp Condition
+                        //----------------------
+                        float a23 = Mathf.Atan2(y3 - y2, x3 - x2);
+                        da1 = Mathf.Abs(a23 - Mathf.Atan2(y2 - y1, x2 - x1));
+                        da2 = Mathf.Abs(Mathf.Atan2(y4 - y3, x4 - x3) - a23);
+                        if (da1 >= Mathf.PI) da1 = 2 * Mathf.PI - da1;
+                        if (da2 >= Mathf.PI) da2 = 2 * Mathf.PI - da2;
+
+                        if (da1 + da2 < angle_tolerance)
+                        {
+                            // Finally we can stop the recursion
+                            //----------------------
+                            points.Add(new Vector2(x1234, y1234));
+                            return;
+                        }
+
+                        if (cusp_limit != 0.0)
+                        {
+                            if (da1 > cusp_limit)
+                            {
+                                points.Add(new Vector2(x2, y2));
+                                return;
+                            }
+
+                            if (da2 > cusp_limit)
+                            {
+                                points.Add(new Vector2(x3, y3));
+                                return;
+                            }
+                        }*/
+                    }
+                }
+#if false
+                else
+                {
+                    if (d2 > curve_collinearity_epsilon)
+                    {
+                        // p1,p3,p4 are collinear, p2 is considerable
+                        //----------------------
+                        if (d2 * d2 <= distance_tolerance * (dx * dx + dy * dy))
+                        {
+                            if (angle_tolerance < curve_angle_tolerance_epsilon)
+                            {
+                                points.Add(new Vector2(x1234, y1234));
+                                return;
+                            }
+
+                            // Angle Condition
+                            //----------------------
+                            da1 = Mathf.Abs(Mathf.Atan2(y3 - y2, x3 - x2) - Mathf.Atan2(y2 - y1, x2 - x1));
+                            if (da1 >= Mathf.PI) da1 = 2 * Mathf.PI - da1;
+
+                            if (da1 < angle_tolerance)
+                            {
+                                points.Add(new Vector2(x2, y2));
+                                points.Add(new Vector2(x3, y3));
+                                return;
+                            }
+
+                            if (cusp_limit != 0.0)
+                            {
+                                if (da1 > cusp_limit)
+                                {
+                                    points.Add(new Vector2(x2, y2));
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                    else if (d3 > curve_collinearity_epsilon)
+                    {
+                        // p1,p2,p4 are collinear, p3 is considerable
+                        //----------------------
+                        if (d3 * d3 <= distance_tolerance * (dx * dx + dy * dy))
+                        {
+                            if (angle_tolerance < curve_angle_tolerance_epsilon)
+                            {
+                                points.Add(new Vector2(x1234, y1234));
+                                return;
+                            }
+
+                            // Angle Condition
+                            //----------------------
+                            da1 = Mathf.Abs(Mathf.Atan2(y4 - y3, x4 - x3) - Mathf.Atan2(y3 - y2, x3 - x2));
+                            if (da1 >= Mathf.PI) da1 = 2 * Mathf.PI - da1;
+
+                            if (da1 < angle_tolerance)
+                            {
+                                points.Add(new Vector2(x2, y2));
+                                points.Add(new Vector2(x3, y3));
+                                return;
+                            }
+
+                            if (cusp_limit != 0.0)
+                            {
+                                if (da1 > cusp_limit)
+                                {
+                                    points.Add(new Vector2(x3, y3));
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // Collinear case
+                        //-----------------
+                        dx = x1234 - (x1 + x4) / 2;
+                        dy = y1234 - (y1 + y4) / 2;
+                        if (dx * dx + dy * dy <= distance_tolerance)
+                        {
+                            points.Add(new Vector2(x1234, y1234));
+                            return;
+                        }
+                    }
+                }
+#endif
+            }
+
+            // Continue subdivision
+            //----------------------
+            AddBezierRecurse(points, new Vector2(x1, y1), new Vector2(x12, y12), new Vector2(x123, y123), new Vector2(x1234, y1234), level + 1);
+            AddBezierRecurse(points, new Vector2(x1234, y1234), new Vector2(x234, y234), new Vector2(x34, y34), new Vector2(x4, y4), level + 1);
+        }
+    }
+
+
     internal class VFXEdgeControl : EdgeControl
     {
         Vector3[] m_PrevControlPoints;
@@ -79,14 +285,11 @@ namespace UnityEditor.VFX.UI
                 Vector3 end = points[3];
                 Vector3 tEnd = points[2];
 
-                Vector3 prevPos = start;
-                Vector3 border = Vector2.zero;
-                Vector3 dir = (tStart - start).normalized;
-                Vector3 norm = new Vector2(dir.y, -dir.x);
 
-                int cpt = (int)((start - end).magnitude / 5);
-                if (cpt < 3)
-                    cpt = 3;
+                List<Vector2> pointResult = new List<Vector2>();
+                BezierSubdiv.GetBezierSubDiv(pointResult, start, end, tStart, tEnd);
+
+                int cpt = pointResult.Count;
 
                 if (m_Mesh == null)
                     m_Mesh = new Mesh();
@@ -109,42 +312,35 @@ namespace UnityEditor.VFX.UI
                 float vertexHalfWidth = halfWidth + 2;
                 int index;
 
-                for (int i = 1; i < cpt; ++i)
+                for (int i = 0; i < cpt; ++i)
                 {
-                    float t = (float)i / (float)cpt;
+                    Vector2 dir;
+                    /*if( i > 0 && i < cpt -1)
+                    {
+                        dir = (pointResult[i] - pointResult[i - 1]).normalized + (pointResult[i + 1] - pointResult[i]).normalized;
+                        dir.Normalize();
+                    }
+                    else */if (i > 0)
+                    {
+                        dir = (pointResult[i] - pointResult[i - 1]).normalized;
+                    }
+                    else
+                    {
+                        dir = (pointResult[i + 1] - pointResult[i]).normalized;
+                    }
 
-                    float minT = 1 - t;
 
-                    Vector3 pos = t * t * t * end +
-                        3 * minT * t * t * tEnd +
-                        3 * minT * minT * t * tStart +
-                        minT * minT * minT * start;
+                    Vector2 norm = new Vector3(dir.y, -dir.x, 0);
 
-                    border = norm * vertexHalfWidth;
 
-                    index = i - 1;
-                    uvs[index * 2] = new Vector2(-vertexHalfWidth, halfWidth);
-                    vertices[index * 2] = prevPos - border;
+                    Vector2 border = -norm * vertexHalfWidth;
 
-                    uvs[index * 2 + 1] = new Vector2(vertexHalfWidth, halfWidth);
-                    vertices[index * 2 + 1] = prevPos + border;
+                    uvs[i * 2] = new Vector2(-vertexHalfWidth, halfWidth);
+                    vertices[i * 2] = pointResult[i] - border;
 
-                    dir = (pos - prevPos).normalized;
-                    norm = new Vector3(dir.y, -dir.x, 0);
-
-                    prevPos = pos;
+                    uvs[i * 2 + 1] = new Vector2(vertexHalfWidth, halfWidth);
+                    vertices[i * 2 + 1] = pointResult[i] + border;
                 }
-
-                dir = (end - prevPos).normalized;
-                norm = new Vector2(dir.y, -dir.x);
-                border = norm * vertexHalfWidth;
-
-                index = cpt - 1;
-                uvs[index * 2] = new Vector2(-vertexHalfWidth, halfWidth);
-                vertices[index * 2] = prevPos - border;
-
-                uvs[index * 2 + 1] = new Vector2(vertexHalfWidth, halfWidth);
-                vertices[index * 2 + 1] = prevPos + border;
 
                 m_Mesh.vertices = vertices;
                 m_Mesh.uv = uvs;
@@ -163,7 +359,7 @@ namespace UnityEditor.VFX.UI
 
                 if (newIndices)
                 {
-                    //fill triange indices as it is a triangle strip
+                    //fill triangle indices as it is a triangle strip
                     int[] indices = new int[(wantedLength - 2) * 3];
 
 
