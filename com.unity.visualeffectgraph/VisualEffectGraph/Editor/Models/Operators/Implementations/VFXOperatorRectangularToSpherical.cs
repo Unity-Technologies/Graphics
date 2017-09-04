874cc6489749479428f6aaf9d5975e8e0238e396
Tristan Genevet <tristan@unity3D.com>
2017-09-04 16:42:32 +0200

    Naming and tooltip for RectangularToSpherical outputs.

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Operators/Implementations/VFXOperatorRectangularToSpherical.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Operators/Implementations/VFXOperatorRectangularToSpherical.cs
@@ -12,7 +12,15 @@ namespace UnityEditor.VFX
             [Tooltip("The 3D coordinate to be converted into Spherical space.")]
             public Vector3 coordinate = Vector3.zero;
         }
-
+        public class OutputProperties
+        {
+            [Tooltip("The angular coordinate (Polar angle).")]
+            public float theta = 45.0f;
+            [Tooltip("The pitch coordinate (Azimuth angle).")]
+            public float phi = 45.0f;
+            [Tooltip("The radial coordinate (Radius).")]
+            public float distance = 1.0f;
+        }
         override public string name { get { return "Rectangular to Spherical"; } }
 
         override protected VFXExpression[] BuildExpression(VFXExpression[] inputExpression)
