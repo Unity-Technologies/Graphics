8c7719bd6a4226c4958c6884db17b48e4aa7904c
Peter Bay Bastian <peter.bastian@gmail.com>
2017-09-06 13:26:53 +0200

    Fix split node and add test for it (#62)

--- MaterialGraphProject/Assets/UnityShaderEditor/Runtime/Nodes/Channel/SplitNode.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Runtime/Nodes/Channel/SplitNode.cs
@@ -8,21 +8,17 @@ namespace UnityEngine.MaterialGraph
     [Title("Channel/Split")]
     public class SplitNode : AbstractMaterialNode, IGeneratesBodyCode
     {
-        protected const string kInputSlotName = "Input";
-        protected const string kOutputSlotRName = "R";
-        protected const string kOutputSlotGName = "G";
-        protected const string kOutputSlotBName = "B";
-        protected const string kOutputSlotAName = "A";
-        protected const string kOutputSlotRGBName = "RGB";
-        protected const string kOutputSlotRGName = "RG";
+        const string kInputSlotName = "Input";
+        const string kOutputSlotRName = "R";
+        const string kOutputSlotGName = "G";
+        const string kOutputSlotBName = "B";
+        const string kOutputSlotAName = "A";
 
         public const int InputSlotId = 0;
         public const int OutputSlotRId = 1;
         public const int OutputSlotGId = 2;
         public const int OutputSlotBId = 3;
         public const int OutputSlotAId = 4;
-        public const int OutputSlotRGBId = 5;
-        public const int OutputSlotRGId = 6;
 
         public SplitNode()
         {
@@ -37,83 +33,34 @@ namespace UnityEngine.MaterialGraph
             AddSlot(new MaterialSlot(OutputSlotGId, kOutputSlotGName, kOutputSlotGName, SlotType.Output, SlotValueType.Vector1, Vector4.zero));
             AddSlot(new MaterialSlot(OutputSlotBId, kOutputSlotBName, kOutputSlotBName, SlotType.Output, SlotValueType.Vector1, Vector4.zero));
             AddSlot(new MaterialSlot(OutputSlotAId, kOutputSlotAName, kOutputSlotAName, SlotType.Output, SlotValueType.Vector1, Vector4.zero));
-            AddSlot(new MaterialSlot(OutputSlotRGBId, kOutputSlotRGBName, kOutputSlotRGBName, SlotType.Output, SlotValueType.Vector3, Vector4.zero));
-            AddSlot(new MaterialSlot(OutputSlotRGId, kOutputSlotRGName, kOutputSlotRGName, SlotType.Output, SlotValueType.Vector2, Vector4.zero));
-            RemoveSlotsNameNotMatching(validSlots);
+            RemoveSlotsNameNotMatching(s_ValidSlots);
         }
 
-        protected int[] validSlots
-        {
-            get { return new[] { InputSlotId, OutputSlotRId, OutputSlotGId, OutputSlotBId, OutputSlotAId, OutputSlotRGBId, OutputSlotRGId }; }
-        }
+        static int[] s_ValidSlots = { InputSlotId, OutputSlotRId, OutputSlotGId, OutputSlotBId, OutputSlotAId };
+        static int[] s_OutputSlots = {OutputSlotRId, OutputSlotGId, OutputSlotBId, OutputSlotAId};
 
         public void GenerateNodeCode(ShaderGenerator visitor, GenerationMode generationMode)
         {
-            NodeUtils.SlotConfigurationExceptionIfBadConfiguration(this, new[] { InputSlotId }, new[] { OutputSlotRId, OutputSlotGId, OutputSlotBId, OutputSlotRGBId, OutputSlotRGId });
-            string inputValue = GetSlotValue(InputSlotId, generationMode);
-            visitor.AddShaderChunk(precision + "4 " + GetVariableNameForNode() + " = " + GetNodeBody(inputValue) + ";", true);
-        }
+            NodeUtils.SlotConfigurationExceptionIfBadConfiguration(this, new[] { InputSlotId }, new[] { OutputSlotRId, OutputSlotGId, OutputSlotBId });
+            var inputValue = GetSlotValue(InputSlotId, generationMode);
 
-        protected string GetNodeBody(string inputValue)
-        {
-            string[] channelNames = { "r", "g", "b", "a" };
             var inputSlot = FindInputSlot<MaterialSlot>(InputSlotId);
-            if (inputSlot == null)
-                return "1.0";
-
-            int numInputChannels = (int)SlotValueHelper.GetChannelCount(inputSlot.concreteValueType);
-            if (numInputChannels > 4)
-                numInputChannels = 0;
-
-            if (owner.GetEdges(inputSlot.slotReference).ToList().Count() == 0)
-                numInputChannels = 0;
-
-            string outputString = precision + "4(";
-            if (numInputChannels == 0)
+            var numInputChannels = 0;
+            if (inputSlot != null)
             {
-                outputString += "1.0, 1.0, 1.0, 1.0)";
-            }
-            else
-            {
-                //float4(arg1, 1.0, 1.0)
-                outputString += inputValue;
-                for (int i = numInputChannels; i < 4; i++)
-                {
-                    outputString += ", 1.0";
-                }
-                outputString += ")";
-            }
-            return outputString;
-        }
+                numInputChannels = (int)SlotValueHelper.GetChannelCount(inputSlot.concreteValueType);
+                if (numInputChannels > 4)
+                    numInputChannels = 0;
 
-        public override string GetVariableNameForSlot(int slotId)
-        {
-            string slotOutput;
-            switch (slotId)
-            {
-                case OutputSlotRId:
-                    slotOutput = ".r";
-                    break;
-                case OutputSlotGId:
-                    slotOutput = ".g";
-                    break;
-                case OutputSlotBId:
-                    slotOutput = ".b";
-                    break;
-                case OutputSlotAId:
-                    slotOutput = ".a";
-                    break;
-                case OutputSlotRGBId:
-                    slotOutput = ".rgb";
-                    break;
-                case OutputSlotRGId:
-                    slotOutput = ".rg";
-                    break;
-                default:
-                    slotOutput = "";
-                    break;
+                if (!owner.GetEdges(inputSlot.slotReference).Any())
+                    numInputChannels = 0;
             }
-            return GetVariableNameForNode() + slotOutput;
+
+            for (var i = 0; i < 4; i++)
+            {
+                var outputValue = i >= numInputChannels ? "1.0" : string.Format("{0}[{1}]", inputValue, i);
+                visitor.AddShaderChunk(string.Format("{0} {1} = {2};", precision, GetVariableNameForSlot(s_OutputSlots[i]), outputValue), true);
+            }
         }
     }
 }
