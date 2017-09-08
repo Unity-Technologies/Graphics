e3eedc6f9c6b1b9297bac6ed346975546b38e107
sebastienlagarde <sebastien@unity3d.com>
2017-09-08 16:09:55 +0200

    Small fix for extreme value

--- ScriptableRenderPipeline/HDRenderPipeline/Material/Lit/LitData.hlsl
+++ ScriptableRenderPipeline/HDRenderPipeline/Material/Lit/LitData.hlsl
@@ -10,7 +10,7 @@ float GetSpecularOcclusionFromBentAO(float3 V, float3 bentNormalWS, SurfaceData 
     // Retrieve cone angle
     // Ambient occlusion is cosine weighted, thus use following equation. See slide 129
     float cosAv = sqrt(1.0 - surfaceData.ambientOcclusion);
-    float roughness = PerceptualSmoothnessToRoughness(surfaceData.perceptualSmoothness);
+    float roughness = max(PerceptualSmoothnessToRoughness(surfaceData.perceptualSmoothness), 0.01); // Clamp to 0.01 to avoid edge cases
     float cosAs = exp2(-3.32193 * Sqr(roughness));
     float cosB = dot(bentNormalWS, reflect(-V, surfaceData.normalWS));
 
