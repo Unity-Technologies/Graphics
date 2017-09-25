0c9fa5cc05684fee3b2c63102da3c790e46bfc18
thomasi <thomasi@unity3d.com>
2017-09-25 18:05:15 +0200

    Add Force Block

---
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Blocks/Implementations/Forces/Force.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEditor.VFX.BlockLibrary
+{
+    [VFXInfo(category = "Force")]
+    class Force : VFXBlock
+    {
+        public enum ForceMode
+        {
+            Absolute,
+            Relative
+        }
+
+        [VFXSetting]
+        public ForceMode Mode = ForceMode.Absolute;
+
+        public override string name { get { return "Force"; } }
+        public override VFXContextType compatibleContexts { get { return VFXContextType.kInitAndUpdateAndOutput; } }
+        public override VFXDataType compatibleData { get { return VFXDataType.kParticle; } }
+
+        public override IEnumerable<VFXNamedExpression> parameters
+        {
+            get
+            {
+                foreach (var p in GetExpressionsFromSlots(this))
+                    yield return p;
+
+                yield return new VFXNamedExpression(VFXBuiltInExpression.DeltaTime, "deltaTime");
+            }
+        }
+
+        public override IEnumerable<VFXAttributeInfo> attributes
+        {
+            get
+            {
+                yield return new VFXAttributeInfo(VFXAttribute.Velocity, VFXAttributeMode.ReadWrite);
+            }
+        }
+
+        public class InputProperties
+        {
+            public Vector Force = new Vector(0.0f, -9.81f, 0.0f);
+        }
+
+        public override string source
+        {
+            get
+            {
+                string forceVector = "0.0";
+
+                switch(Mode)
+                {
+                    case ForceMode.Absolute: forceVector = "Force_vector" ; break;
+                    case ForceMode.Relative: forceVector = "(Force_vector - velocity)"; break;
+                }
+
+                return "velocity += " + forceVector + " * deltaTime;";
+            }
+        }
+
+
+    }
+}
