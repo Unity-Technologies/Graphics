f5acd363ef927dc3812b52872257e6cf784c43f3
Jens Holm <jensgholm15@gmail.com>
2017-09-29 14:57:32 +0200

    Add preview node

---
+++ MaterialGraphProject/Assets/UnityShaderEditor/Runtime/Nodes/PreviewNode.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace UnityEngine.MaterialGraph
+{
+    [Title("Preview Node")]
+    public class PreviewNode : CodeFunctionNode
+    {
+        public const int InputSlotId = 0;
+        public const int OutputSlotId = 1;
+
+        public override bool hasPreview { get { return true; } }
+
+        protected override MethodInfo GetFunctionToConvert()
+        {
+            return GetType().GetMethod("Unity_Preview", BindingFlags.Static | BindingFlags.NonPublic);
+        }
+
+        static string Unity_Preview(
+            [Slot(0, Binding.None)] DynamicDimensionVector input,
+            [Slot(1, Binding.None)] out DynamicDimensionVector output)
+        {
+            return
+                @"
+{
+    output = input;
+}
+";
+        }
+    }
+}
