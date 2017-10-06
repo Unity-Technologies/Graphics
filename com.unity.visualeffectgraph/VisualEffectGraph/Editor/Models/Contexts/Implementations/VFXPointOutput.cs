75e7350b400f9f57a4f6a6c87447c730a1aee1de
Julien Fryer <julienf@unity3d.com>
2017-10-06 14:35:01 +0200

    Factorize some particle render stuff (soft particles, blending...)

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Contexts/Implementations/VFXPointOutput.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Contexts/Implementations/VFXPointOutput.cs
@@ -5,23 +5,11 @@ using UnityEngine.VFX;
 namespace UnityEditor.VFX
 {
     [VFXInfo]
-    class VFXPointOutput : VFXContext
+    class VFXPointOutput : VFXAbstractParticleOutput
     {
-        public VFXPointOutput() : base(VFXContextType.kOutput, VFXDataType.kParticle, VFXDataType.kNone) {}
         public override string name { get { return "Point Output"; } }
         public override string codeGeneratorTemplate { get { return "VFXShaders/VFXParticlePoints"; } }
-        public override bool codeGeneratorCompute { get { return false; } }
         public override VFXTaskType taskType { get { return VFXTaskType.kParticlePointOutput; } }
-
-        public override IEnumerable<KeyValuePair<string, VFXShaderWriter>> additionnalReplacements
-        {
-            get
-            {
-                var renderState = new VFXShaderWriter();
-                renderState.WriteLine("ZWrite On");
-                yield return new KeyValuePair<string, VFXShaderWriter>("${VFXOutputRenderState}", renderState);
-            }
-        }
 
         public override IEnumerable<VFXAttributeInfo> attributes
         {
