0c5668c09f29d1f5c0984cdf92f9afc63eb19b1d
Peter Bay Bastian <peter.bastian@gmail.com>
2017-10-12 14:54:40 +0200

    Default control attribute (so far only for floats)

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Controls/DefaultControl.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Controls/DefaultControl.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.MaterialGraph;
 
 namespace UnityEditor.MaterialGraph.Drawing.Controls
 {
-    public class DefaultControlAttribute : Attribute
+    [AttributeUsage(AttributeTargets.Property)]
+    public class DefaultControlAttribute : Attribute, IControlAttribute
     {
-        
+        public VisualElement InstantiateControl(AbstractMaterialNode node, PropertyInfo propertyInfo)
+        {
+            if (MultiFloatControlView.validTypes.Contains(propertyInfo.PropertyType))
+                return new MultiFloatControlView(null, "X", "Y", "Z", "W", node, propertyInfo);
+            return null;
+        }
     }
 }
