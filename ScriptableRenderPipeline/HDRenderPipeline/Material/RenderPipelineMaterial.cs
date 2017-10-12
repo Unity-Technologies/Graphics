129ba2d369d31aba98be91448282567d22de359b
Tim Cooper <tim@unity3d.com>
2017-10-12 10:57:09 +0200

    move HD using class data from core.

--- ScriptableRenderPipeline/HDRenderPipeline/Material/RenderPipelineMaterial.cs
+++ ScriptableRenderPipeline/HDRenderPipeline/Material/RenderPipelineMaterial.cs
@@ -1,9 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine.Rendering;
 
 namespace UnityEngine.Experimental.Rendering.HDPipeline
 {
     public class RenderPipelineMaterial : Object
     {
+        public static List<RenderPipelineMaterial> GetRenderPipelineMaterialList()
+        {
+            var baseType = typeof(RenderPipelineMaterial);
+            var assembly = baseType.Assembly;
+
+            var types = assembly.GetTypes()
+                .Where(t => t.IsSubclassOf(baseType))
+                .Select(Activator.CreateInstance)
+                .Cast<RenderPipelineMaterial>()
+                .ToList();
+
+            // Note: If there is a need for an optimization in the future of this function, user can
+            // simply fill the materialList manually by commenting the code abode and returning a
+            // custom list of materials they use in their game.
+            //
+            // return new List<RenderPipelineMaterial>
+            // {
+            //    new Lit(),
+            //    new Unlit(),
+            //    ...
+            // };
+
+            return types;
+        }
+
         // GBuffer management
         public virtual int GetMaterialGBufferCount() { return 0; }
         public virtual void GetMaterialGBufferDescription(out RenderTextureFormat[] RTFormat, out RenderTextureReadWrite[] RTReadWrite)
