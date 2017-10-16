6676e5edf67df97b21ab051536ac72bcd01e79af
Paul Demeulenaere <pauld@unity3d.com>
2017-10-16 11:38:21 +0200

    Add StringFieldPushButton class (missing file)

---
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/RMGUI/StringFieldPushButton.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.Experimental.UIElements.StyleEnums;
+using UnityEditor.Experimental.UIElements;
+
+namespace UnityEditor.VFX.UIElements
+{
+    class StringFieldPushButton : StringField
+    {
+        Action<string> m_fnOnClicked;
+
+        public StringFieldPushButton(string label, Action<string> fnClicked) : base(label)
+        {
+            m_fnOnClicked = fnClicked;
+            Add(new Button(() => fnClicked(m_TextField.text)));
+        }
+
+        public StringFieldPushButton(VisualElement existingLabel, Action<string> fnClicked) : base(existingLabel)
+        {
+            m_fnOnClicked = fnClicked;
+            Add(new Button(() => fnClicked(m_TextField.text)));
+        }
+    }
+}
