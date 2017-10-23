c9b769c8e1daed567f6435ffaca0ddb449b99957
Evgenii Golubev <evgenii@unity3d.com>
2017-10-23 19:25:45 +0200

    Improve the quality of the FDG texture

--- ScriptableRenderPipeline/Core/ShaderLibrary/ImageBasedLighting.hlsl
+++ ScriptableRenderPipeline/Core/ShaderLibrary/ImageBasedLighting.hlsl
@@ -315,7 +315,7 @@ void ImportanceSampleAnisoGGX(float2   u,
 // ----------------------------------------------------------------------------
 
 // Ref: Listing 18 in "Moving Frostbite to PBR" + https://knarkowicz.wordpress.com/2014/12/27/analytical-dfg-term-for-ibl/
-float4 IntegrateGGXAndDisneyFGD(float3 V, float3 N, float roughness, uint sampleCount = 4096)
+float4 IntegrateGGXAndDisneyFGD(float3 V, float3 N, float roughness, uint sampleCount = 8192)
 {
     float NdotV     = saturate(dot(N, V));
     float4 acc      = float4(0.0, 0.0, 0.0, 0.0);
@@ -359,6 +359,9 @@ float4 IntegrateGGXAndDisneyFGD(float3 V, float3 N, float roughness, uint sample
             acc.z += disneyDiffuse * weightOverPdf;
         }
     }
+
+    // Remap from [0, 1.5] to [0, 1] range.
+    acc.z /= 1.5;
 
     return acc / sampleCount;
 }
