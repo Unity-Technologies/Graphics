9e99073977dee89689b98921556e6569558a56c1
Evgenii Golubev <evgenii@unity3d.com>
2017-10-23 17:25:32 +0200

    Switch to Emmanuel's version of MS energy compensation

--- ScriptableRenderPipeline/Core/ShaderLibrary/ImageBasedLighting.hlsl
+++ ScriptableRenderPipeline/Core/ShaderLibrary/ImageBasedLighting.hlsl
@@ -342,10 +342,10 @@ float4 IntegrateGGXAndDisneyFGD(float3 V, float3 N, float roughness, uint sample
             // Integral{(F0 + (1 - F0) * (1 - <V,H>)^5) * (BSDF / F) * <N,L> dw} =
             // F0 * Integral{(BSDF / F) * <N,L> dw} +
             // (1 - F0) * Integral{(1 - <V,H>)^5 * (BSDF / F) * <N,L> dw} =
-            // F0 * y + (1 - F0) * x = lerp(x, y, F0)
+            // (1 - F0) * x + F0 * y = lerp(x, y, F0)
 
-            acc.x += weightOverPdf;
-            acc.y += weightOverPdf * pow(1 - VdotH, 5);
+            acc.x += weightOverPdf * pow(1 - VdotH, 5);
+            acc.y += weightOverPdf;
         }
 
         // for Disney we still use a Cosine importance sampling, true Disney importance sampling imply a look up table
