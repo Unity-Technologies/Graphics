269d068892f6afb7887aee81ac304f9a330baec8
Thomas Iché <thomasi@unity3d.com>
2017-10-23 11:23:27 +0200

    Fixed Namespaces

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Blocks/Implementations/Collision/CollisionSphere.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Blocks/Implementations/Collision/CollisionSphere.cs
@@ -2,7 +2,7 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 
-namespace UnityEditor.VFX.BlockLibrary
+namespace UnityEditor.VFX.Block
 {
     [VFXInfo(category = "Collision")]
     class CollisionSphere : VFXBlock
