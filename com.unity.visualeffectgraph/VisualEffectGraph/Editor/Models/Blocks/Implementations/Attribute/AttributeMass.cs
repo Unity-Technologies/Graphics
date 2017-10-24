e7fbaa146e939cedcd6e062f9f6d08a3cee28f33
Richard Kettlewell <richardk@unity3d.com>
2017-10-24 14:26:54 +0100

    [vfx] Move mass to Attribute, fix some contexts

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Blocks/Implementations/Attribute/AttributeMass.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Blocks/Implementations/Attribute/AttributeMass.cs
@@ -4,18 +4,18 @@ using System.Collections.Generic;
 
 namespace UnityEditor.VFX.BlockLibrary
 {
-    [VFXInfo(category = "Collision")]
-    class CollisionMass : VFXBlock
+    [VFXInfo(category = "Attribute")]
+    class AttributeMass : VFXBlock
     {
-        public override string name { get { return "Calculate Particle Mass"; } }
-        public override VFXContextType compatibleContexts { get { return VFXContextType.kInitAndUpdateAndOutput; } }
+        public override string name { get { return "Calculate Mass"; } }
+        public override VFXContextType compatibleContexts { get { return VFXContextType.kInitAndUpdate; } }
         public override VFXDataType compatibleData { get { return VFXDataType.kParticle; } }
         public override IEnumerable<VFXAttributeInfo> attributes
         {
             get
             {
                 yield return new VFXAttributeInfo(VFXAttribute.Size, VFXAttributeMode.Read);
-                yield return new VFXAttributeInfo(new VFXAttribute("mass", VFXValue.Constant(1.0f)), VFXAttributeMode.Write);
+                yield return new VFXAttributeInfo(VFXAttribute.Mass, VFXAttributeMode.Write);
             }
         }
 
--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Blocks/Implementations/Collision/CollisionBase.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Blocks/Implementations/Collision/CollisionBase.cs
@@ -17,7 +17,7 @@ namespace UnityEditor.VFX.BlockLibrary
         [VFXSetting, Tooltip("Enable random bending of the collision normal to simulate collision with a rough surface.")]
         public bool roughSurface = false;
 
-        public override VFXContextType compatibleContexts { get { return VFXContextType.kInitAndUpdateAndOutput; } }
+        public override VFXContextType compatibleContexts { get { return VFXContextType.kUpdate; } }
         public override VFXDataType compatibleData { get { return VFXDataType.kParticle; } }
         public override IEnumerable<VFXAttributeInfo> attributes
         {
@@ -29,7 +29,7 @@ namespace UnityEditor.VFX.BlockLibrary
                 yield return new VFXAttributeInfo(VFXAttribute.Lifetime, VFXAttributeMode.Read);
                 if (roughSurface)
                     yield return new VFXAttributeInfo(VFXAttribute.Seed, VFXAttributeMode.ReadWrite);
-                yield return new VFXAttributeInfo(new VFXAttribute("mass", VFXValue.Constant(1.0f)), VFXAttributeMode.Read);
+                yield return new VFXAttributeInfo(VFXAttribute.Mass, VFXAttributeMode.Read);
             }
         }
 
