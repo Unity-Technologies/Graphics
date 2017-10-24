de7e3e861dd520727a122253dccbe6c4b9d020ca
Richard Kettlewell <richardk@unity3d.com>
2017-10-24 10:09:07 +0100

    [vfx] Add AABox collider

---
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Blocks/Implementations/Collision/CollisionAABox.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+namespace UnityEditor.VFX.BlockLibrary
+{
+    [VFXInfo(category = "Collision")]
+    class CollisionAABox : CollisionBase
+    {
+        public override string name { get { return "Collider (AABox)"; } }
+
+        public class InputProperties
+        {
+            [Tooltip("The collision bounding box.")]
+            public AABox box = new AABox() { size = Vector3.one };
+        }
+
+        public override string source
+        {
+            get
+            {
+                string Source = @"
+float3 nextPos = position + velocity * deltaTime;
+float3 dir = nextPos - box_center;
+float3 absDir = abs(dir);
+float3 size = box_size * 0.5f;
+";
+
+                if (mode == Mode.Solid)
+                    Source += @"bool collision = all(absDir <= size);";
+                else
+                    Source += @"bool collision = any(absDir >= size);";
+
+                Source += @"
+if (collision)
+{
+    float3 distanceToEdge = (absDir - size);
+    float3 absDistanceToEdge = abs(distanceToEdge);
+
+    float3 n;
+
+    if (absDistanceToEdge.x < absDistanceToEdge.y && absDistanceToEdge.x < absDistanceToEdge.z)
+        n = float3(colliderSign * sign(dir.x), 0.0f, 0.0f);
+    else if (absDistanceToEdge.y < absDistanceToEdge.z)
+        n = float3(0.0f, colliderSign * sign(dir.y), 0.0f);
+    else
+        n = float3(0.0f, 0.0f, colliderSign * sign(dir.z));
+
+    position -= n * distanceToEdge * colliderSign;
+    ";
+
+                Source += collisionResponseSource;
+                Source += @"
+}";
+                return Source;
+            }
+        }
+    }
+}
