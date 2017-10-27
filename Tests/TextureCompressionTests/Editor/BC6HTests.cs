5c7d9a5cddabf95ec96ad51eea26651777b6448f
Frédéric Vauchelles <frederic.vauchelles@gmail.com>
2017-10-27 13:54:24 +0200

    Restored basic BC6H compression

--- Tests/TextureCompressionTests/Editor/BC6HTests.cs
+++ Tests/TextureCompressionTests/Editor/BC6HTests.cs
@@ -37,29 +37,4 @@ public class BC6HTests
 
         target.Release();
     }
-
-    // Test texture copy
-    [Test]
-    public void TextureCopy()
-    {
-        var shader = AssetDatabase.LoadAssetAtPath<ComputeShader>("Assets/ScriptableRenderPipeline/ScriptableRenderPipeline/Core/Resources/BC6H.compute");
-        var sourceTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/ScriptableRenderPipeline/Tests/TextureCompressionTests/Editor/Resources/graffiti_shelter_1k.hdr");
-
-        var k = shader.FindKernel("KCopy");
-        var target = new RenderTexture(sourceTexture.width, sourceTexture.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
-        target.Release();
-        target.enableRandomWrite = true;
-        target.Create();
-        var targetT2D = new Texture2D(sourceTexture.width, sourceTexture.height, TextureFormat.RGBAFloat, false, true);
-        shader.SetTexture(k, "_Source", sourceTexture);
-        shader.SetTexture(k, "_Target", target);
-        shader.Dispatch(k, targetT2D.width, targetT2D.height, 1);
-        Graphics.CopyTexture(target, targetT2D);
-
-        var targetFile = "Assets/ScriptableRenderPipeline/Tests/TextureCompressionTests/Editor/Resources/graffiti_shelter_1k_bc6h.hdr";
-
-        var bytes = targetT2D.GetRawTextureData();
-        File.WriteAllBytes(targetFile, bytes);
-        target.Release();
-    }
 }
