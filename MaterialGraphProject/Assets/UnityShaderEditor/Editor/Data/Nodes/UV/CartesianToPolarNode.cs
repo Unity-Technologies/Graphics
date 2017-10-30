b89b52512cfb7f04b71eeadf008140ee34e6bde8
Natalie Burke <natb@hq.unity3d.com>
2017-10-30 11:19:51 -0700

    changing input order to increase consistency. Added center input for rotate node

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/Data/Nodes/UV/CartesianToPolarNode.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Data/Nodes/UV/CartesianToPolarNode.cs
@@ -18,9 +18,9 @@ namespace UnityEditor.ShaderGraph
 
         static string Unity_CartesianToPolar(
             [Slot(0, Binding.MeshUV0)] Vector2 UV,
-            [Slot(1, Binding.None, 1.0f, 1.0f, 1.0f, 1.0f)] Vector1 RadialScale,
-            [Slot(2, Binding.None, 1.0f, 1.0f, 1.0f, 1.0f)] Vector1 LengthScale,
-            [Slot(3, Binding.None, 0.5f, 0.5f, 0.5f, 0.5f)] Vector2 Center,
+            [Slot(1, Binding.None, 0.5f, 0.5f, 0.5f, 0.5f)] Vector2 Center,
+            [Slot(2, Binding.None, 1.0f, 1.0f, 1.0f, 1.0f)] Vector1 RadialScale,
+            [Slot(3, Binding.None, 1.0f, 1.0f, 1.0f, 1.0f)] Vector1 LengthScale,
             [Slot(4, Binding.None)] out Vector2 Out)
         {
             Out = Vector2.zero;
--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/Data/Nodes/UV/UVRotatorNode.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Data/Nodes/UV/UVRotatorNode.cs
@@ -49,8 +49,9 @@ namespace UnityEditor.ShaderGraph
 
         static string Unity_UVRotator_Radians(
             [Slot(0, Binding.MeshUV0)] Vector2 UV,
-            [Slot(1, Binding.None)] Vector1 Rotation,
-            [Slot(2, Binding.None)] out Vector2 Out)
+            [Slot(1, Binding.None, 0.5f, 0.5f, 0.5f, 0.5f)] Vector2 Center,
+            [Slot(2, Binding.None)] Vector1 Rotation,
+            [Slot(3, Binding.None)] out Vector2 Out)
         {
             Out = Vector2.zero;
 
@@ -59,7 +60,7 @@ namespace UnityEditor.ShaderGraph
                 @"
 {
     //rotation matrix
-    UV.xy -= 0.5;
+    UV -= Center;
     {precision} s = sin(Rotation);
     {precision} c = cos(Rotation);
 
@@ -72,7 +73,7 @@ namespace UnityEditor.ShaderGraph
 
     //multiply the UVs by the rotation matrix
     UV.xy = mul(UV.xy, rMatrix);
-    UV.xy += 0.5;
+    UV += Center;
     
     Out = UV;
 }";
@@ -80,16 +81,18 @@ namespace UnityEditor.ShaderGraph
 
         static string Unity_UVRotator_Degrees(
             [Slot(0, Binding.MeshUV0)] Vector2 UV,
-            [Slot(1, Binding.None)] Vector1 Rotation,
-            [Slot(2, Binding.None)] out Vector2 Out)
+            [Slot(1, Binding.None, 0.5f, 0.5f, 0.5f, 0.5f)] Vector2 Center,
+            [Slot(2, Binding.None)] Vector1 Rotation,
+            [Slot(3, Binding.None)] out Vector2 Out)
         {
             Out = Vector2.zero;
            
             return @"
 {
     //rotation matrix
+
     Rotation = Rotation * (3.1415926f/180.0f);
-    UV.xy -= 0.5;
+    UV -= Center;
     {precision} s = sin(Rotation);
     {precision} c = cos(Rotation);
 
@@ -102,7 +105,7 @@ namespace UnityEditor.ShaderGraph
 
     //multiply the UVs by the rotation matrix
     UV.xy = mul(UV.xy, rMatrix);
-    UV.xy += 0.5;
+    UV += Center;
     
     Out = UV;
 }";
