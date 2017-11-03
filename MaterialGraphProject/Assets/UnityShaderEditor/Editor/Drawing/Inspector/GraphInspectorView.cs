44fe16b5c4b165c028490d37540752c6a088a859
Jens Holm <jensgholm15@gmail.com>
2017-11-03 14:03:33 +0100

    Store master node preview mesh in GraphInspectorView persistent data

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Inspector/GraphInspectorView.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Inspector/GraphInspectorView.cs
@@ -7,9 +7,23 @@ using UnityEngine;
 using UnityEngine.Experimental.UIElements;
 using UnityEditor.Graphing;
 using UnityEditor.ShaderGraph;
+using UnityEngine.AI;
 
 namespace UnityEditor.ShaderGraph.Drawing.Inspector
 {
+    [Serializable]
+    class  PersistentMesh
+    {
+        [SerializeField]
+        Mesh m_Mesh;
+
+        public Mesh mesh
+        {
+            get { return m_Mesh; }
+            set { m_Mesh = value; }
+        }
+    }
+
     public class GraphInspectorView : VisualElement, IDisposable
     {
         int m_SelectionHash;
@@ -30,8 +44,12 @@ namespace UnityEditor.ShaderGraph.Drawing.Inspector
 
         List<INode> m_SelectedNodes;
 
+        PersistentMesh m_PersistentMasterNodePreviewMesh;
+
         public GraphInspectorView(string assetName, PreviewManager previewManager, AbstractMaterialGraph graph)
         {
+            persistenceKey = "GraphInspector";
+
             m_Graph = graph;
             m_PreviewManager = previewManager;
             m_SelectedNodes = new List<INode>();
@@ -95,6 +113,7 @@ namespace UnityEditor.ShaderGraph.Drawing.Inspector
 
                 m_PreviewMeshPicker = new ObjectField() { objectType = typeof(Mesh) };
                 m_PreviewMeshPicker.OnValueChanged(OnPreviewMeshChanged);
+
                 bottomContainer.Add(m_PreviewMeshPicker);
             }
             Add(bottomContainer);
@@ -133,7 +152,6 @@ namespace UnityEditor.ShaderGraph.Drawing.Inspector
                 if (m_MasterNode != null)
                 {
                     m_PreviewHandle = m_PreviewManager.GetPreview(m_MasterNode);
-                    m_PreviewHandle.mesh = null;
                     m_PreviewHandle.onPreviewChanged += OnPreviewChanged;
                 }
             }
@@ -177,8 +195,7 @@ namespace UnityEditor.ShaderGraph.Drawing.Inspector
             if (changeEvent.newValue == null)
             {
                 m_PreviewHandle.mesh = null;
-
-                return;
+                m_PersistentMasterNodePreviewMesh.mesh = null;
             }
 
             Mesh changedMesh = changeEvent.newValue as Mesh;
@@ -186,7 +203,24 @@ namespace UnityEditor.ShaderGraph.Drawing.Inspector
             if (changedMesh)
             {
                 m_PreviewHandle.mesh = changedMesh;
+                m_PersistentMasterNodePreviewMesh.mesh = changedMesh;
             }
+
+            masterNode.onModified(masterNode, ModificationScope.Node);
+
+            SavePersistentData();
+        }
+
+
+        public override void OnPersistentDataReady()
+        {
+            base.OnPersistentDataReady();
+
+            string key = GetFullHierarchicalPersistenceKey();
+
+            m_PersistentMasterNodePreviewMesh = GetOrCreatePersistentData<PersistentMesh>(m_PersistentMasterNodePreviewMesh, key);
+
+            m_PreviewMeshPicker.SetValueAndNotify(m_PersistentMasterNodePreviewMesh.mesh);
         }
 
         public void UpdateSelection(IEnumerable<INode> nodes)
