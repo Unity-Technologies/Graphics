f3fe262793246a0bfa1b51153eb5f497ac351aa5
Evgenii Golubev <evgenii@unity3d.com>
2017-11-09 21:02:31 +0100

    Remove OptimizeAffineMatrix()
    Reason for removal: the function does not affect performance.
    The last row of the matrix is only used to compute the W component of the vector resulting from the matrix-vector multiplication.
    However, since we don't actually use the W component (we shouldn't...), the compiler doesn't use the last row during the multiplication.

--- ScriptableRenderPipeline/HDRenderPipeline/ShaderVariablesMatrixDefsLegacyUnity.hlsl
+++ ScriptableRenderPipeline/HDRenderPipeline/ShaderVariablesMatrixDefsLegacyUnity.hlsl
@@ -5,10 +5,10 @@
 #ifndef UNITY_SHADER_VARIABLES_MATRIX_DEFS_LEGACY_UNITY_INCLUDED
 #define UNITY_SHADER_VARIABLES_MATRIX_DEFS_LEGACY_UNITY_INCLUDED
 
-#define UNITY_MATRIX_M     OptimizeAffineMatrix(unity_ObjectToWorld)
-#define UNITY_MATRIX_I_M   OptimizeAffineMatrix(unity_WorldToObject)
-#define UNITY_MATRIX_V     OptimizeAffineMatrix(unity_MatrixV)
-#define UNITY_MATRIX_I_V   OptimizeAffineMatrix(unity_MatrixInvV)
+#define UNITY_MATRIX_M     unity_ObjectToWorld
+#define UNITY_MATRIX_I_M   unity_WorldToObject
+#define UNITY_MATRIX_V     unity_MatrixV
+#define UNITY_MATRIX_I_V   unity_MatrixInvV
 #define UNITY_MATRIX_P     OptimizeProjectionMatrix(glstate_matrix_projection)
 #define UNITY_MATRIX_I_P   inverse_glstate_matrix_projection_is_not_defined
 #define UNITY_MATRIX_VP    unity_MatrixVP
