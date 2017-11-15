210506f14a616b0db62c334cdd2640a2a9ac0b98
Matt Dean <matthewdean3d@gmail.com>
2017-11-15 15:06:05 +0000

    ChannelEnumControl fixes
    - Add modification listener
    - Add slot ID to attribute
    - Cleanup

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Controls/ChannelEnumControl.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Controls/ChannelEnumControl.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Reflection;
 using UnityEngine;
+using UnityEditor.Graphing;
 using UnityEngine.Experimental.UIElements;
 
 namespace UnityEditor.ShaderGraph.Drawing.Controls
@@ -9,30 +10,32 @@ namespace UnityEditor.ShaderGraph.Drawing.Controls
     public class ChannelEnumControlAttribute : Attribute, IControlAttribute
     {
         string m_Label;
+        int m_SlotId;
 
-        public ChannelEnumControlAttribute(string label = null)
+        public ChannelEnumControlAttribute(string label = null, int slotId = 0)
         {
             m_Label = label;
+            m_SlotId = slotId;
         }
 
         public VisualElement InstantiateControl(AbstractMaterialNode node, PropertyInfo propertyInfo)
         {
-            return new ChannelEnumControlView(m_Label, node, propertyInfo);
+            return new ChannelEnumControlView(m_Label, m_SlotId, node, propertyInfo);
         }
     }
 
-    public class ChannelEnumControlView : VisualElement
+    public class ChannelEnumControlView : VisualElement, INodeModificationListener
     {
         GUIContent m_Label;
         AbstractMaterialNode m_Node;
         PropertyInfo m_PropertyInfo;
+        int m_SlotId;
 
-        string[] popupEntries;
-
-        public ChannelEnumControlView(string label, AbstractMaterialNode node, PropertyInfo propertyInfo)
+        public ChannelEnumControlView(string label, int slotId, AbstractMaterialNode node, PropertyInfo propertyInfo)
         {
             m_Node = node;
             m_PropertyInfo = propertyInfo;
+            m_SlotId = slotId;
             if (!propertyInfo.PropertyType.IsEnum)
                 throw new ArgumentException("Property must be an enum.", "propertyInfo");
             m_Label = new GUIContent(label ?? ObjectNames.NicifyVariableName(propertyInfo.Name));
@@ -41,19 +44,33 @@ namespace UnityEditor.ShaderGraph.Drawing.Controls
 
         void OnGUIHandler()
         {
-            var entries = (Enum) m_PropertyInfo.GetValue(m_Node, null);
-            var value = (int)m_PropertyInfo.GetValue(m_Node, null);
+            UpdatePopup();
+        }
 
+        public void OnNodeModified(ModificationScope scope)
+        {
+            if(scope == ModificationScope.Graph)
+                UpdatePopup();
+        }
+
+        private void UpdatePopup()
+        {
+            var value = (int)m_PropertyInfo.GetValue(m_Node, null);
             using (var changeCheckScope = new EditorGUI.ChangeCheckScope())
             {
-                int count = (int)SlotValueHelper.GetChannelCount(m_Node.FindSlot<MaterialSlot>(0).concreteValueType);
-                popupEntries = new string[count];
-
+                int count = (int)SlotValueHelper.GetChannelCount(m_Node.FindSlot<MaterialSlot>(m_SlotId).concreteValueType);
+                if (value >= count)
+                {
+                    m_Node.owner.owner.RegisterCompleteObjectUndo("Change " + m_Node.name);
+                    m_PropertyInfo.SetValue(m_Node, 0, null);
+                    return;
+                }
+                var entries = (Enum)m_PropertyInfo.GetValue(m_Node, null);
                 string[] enumNames = Enum.GetNames(entries.GetType());
+                string[] popupEntries = new string[count];
                 for (int i = 0; i < popupEntries.Length; i++)
                     popupEntries[i] = enumNames[i];
                 value = EditorGUILayout.Popup(m_Label, value, popupEntries);
-
                 if (changeCheckScope.changed)
                 {
                     m_Node.owner.owner.RegisterCompleteObjectUndo("Change " + m_Node.name);
