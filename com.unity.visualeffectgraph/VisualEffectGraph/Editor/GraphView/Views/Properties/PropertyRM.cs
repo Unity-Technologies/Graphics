3af4a9850610c980f7df2814c156cb8bce952f73
Tristan Genevet <tristan@unity3d.com>
2017-11-17 15:09:47 +0100

    Nicifying Settings and Slot names according to unity style.

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/GraphView/Views/Properties/PropertyRM.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/GraphView/Views/Properties/PropertyRM.cs
@@ -88,7 +88,7 @@ namespace UnityEditor.VFX.UI
             m_Icon.style.backgroundImage = m_IconStates[m_Provider.expanded && m_Provider.expandable ? 1 : 0];
             SetValue(m_Provider.value);
 
-            string text = m_Provider.name;
+            string text = ObjectNames.NicifyVariableName(m_Provider.name);
             string tooltip = null;
             VFXPropertyAttribute.ApplyToGUI(m_Provider.attributes, ref text, ref tooltip);
             m_Label.text = text;
