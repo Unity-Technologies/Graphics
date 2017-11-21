ffae298807193b34006a94a72e40eee44fd0ec60
Matt Dean <matthewdean3d@gmail.com>
2017-11-21 17:13:26 +0000

    Fix Channel Mask node
    - Convert to Abstract derived
    - Move channel validation check to node side
    - Cleanup

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Controls/ChannelEnumControl.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Controls/ChannelEnumControl.cs
@@ -29,6 +29,7 @@ namespace UnityEditor.ShaderGraph.Drawing.Controls
         GUIContent m_Label;
         AbstractMaterialNode m_Node;
         PropertyInfo m_PropertyInfo;
+        IMGUIContainer m_Container;
         int m_SlotId;
 
         public ChannelEnumControlView(string label, int slotId, AbstractMaterialNode node, PropertyInfo propertyInfo)
@@ -39,7 +40,8 @@ namespace UnityEditor.ShaderGraph.Drawing.Controls
             if (!propertyInfo.PropertyType.IsEnum)
                 throw new ArgumentException("Property must be an enum.", "propertyInfo");
             m_Label = new GUIContent(label ?? ObjectNames.NicifyVariableName(propertyInfo.Name));
-            Add(new IMGUIContainer(OnGUIHandler));
+            m_Container = new IMGUIContainer(OnGUIHandler);
+            Add(m_Container);
         }
 
         void OnGUIHandler()
@@ -49,8 +51,8 @@ namespace UnityEditor.ShaderGraph.Drawing.Controls
 
         public void OnNodeModified(ModificationScope scope)
         {
-            if(scope == ModificationScope.Graph)
-                UpdatePopup();
+            if (scope == ModificationScope.Graph)
+                m_Container.Dirty(ChangeType.Repaint);
         }
 
         private void UpdatePopup()
@@ -58,19 +60,14 @@ namespace UnityEditor.ShaderGraph.Drawing.Controls
             var value = (int)m_PropertyInfo.GetValue(m_Node, null);
             using (var changeCheckScope = new EditorGUI.ChangeCheckScope())
             {
-                int count = (int)SlotValueHelper.GetChannelCount(m_Node.FindSlot<MaterialSlot>(m_SlotId).concreteValueType);
-                if (value >= count)
-                {
-                    m_Node.owner.owner.RegisterCompleteObjectUndo("Change " + m_Node.name);
-                    m_PropertyInfo.SetValue(m_Node, 0, null);
-                    return;
-                }
-                var entries = (Enum)m_PropertyInfo.GetValue(m_Node, null);
-                string[] enumNames = Enum.GetNames(entries.GetType());
-                string[] popupEntries = new string[count];
+                int channelCount = (int)SlotValueHelper.GetChannelCount(m_Node.FindSlot<MaterialSlot>(m_SlotId).concreteValueType);
+                var enumEntryCount = (Enum)m_PropertyInfo.GetValue(m_Node, null);
+                string[] enumEntryNames = Enum.GetNames(enumEntryCount.GetType());
+                string[] popupEntries = new string[channelCount];
                 for (int i = 0; i < popupEntries.Length; i++)
-                    popupEntries[i] = enumNames[i];
+                    popupEntries[i] = enumEntryNames[i];
                 value = EditorGUILayout.Popup(m_Label, value, popupEntries);
+                
                 if (changeCheckScope.changed)
                 {
                     m_Node.owner.owner.RegisterCompleteObjectUndo("Change " + m_Node.name);
