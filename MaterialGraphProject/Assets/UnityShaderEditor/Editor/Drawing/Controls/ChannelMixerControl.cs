98f13200e0be18d22b436bae94a720d8c8ddbb0f
Matt Dean <matthewdean3d@gmail.com>
2017-11-21 14:57:56 +0000

    Fixes for PR
    Also made function reusable for different node insatnces

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Controls/ChannelMixerControl.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Controls/ChannelMixerControl.cs
@@ -32,26 +32,29 @@ namespace UnityEditor.ShaderGraph.Drawing.Controls
     {
         AbstractMaterialNode m_Node;
         PropertyInfo m_PropertyInfo;
+        ChannelMixerNode.ChannelMixer m_ChannelMixer;
         int m_OutChannel;
 
-        Slider redSlider;
-        Slider greenSlider;
-        Slider blueSlider;
+        Slider m_RedSlider;
+        Slider m_GreenSlider;
+        Slider m_BlueSlider;
 
         float m_Minimum;
         float m_Maximum;
+        bool m_Initialized;
 
         public ChannelMixerControlView(string label, float minimum, float maximum, AbstractMaterialNode node, PropertyInfo propertyInfo)
         {
             m_Node = node;
             m_PropertyInfo = propertyInfo;
+            m_ChannelMixer = (ChannelMixerNode.ChannelMixer)m_PropertyInfo.GetValue(m_Node, null);
             m_OutChannel = 0;
 
             m_Minimum = minimum;
             m_Maximum = maximum;
 
             if (propertyInfo.PropertyType != typeof(ChannelMixerNode.ChannelMixer))
-                throw new ArgumentException("Property must be of type Channel Mixer.", "propertyInfo");
+                throw new ArgumentException("Property must be of type ChannelMixer.", "propertyInfo");
             label = label ?? ObjectNames.NicifyVariableName(propertyInfo.Name);
 
             if (!string.IsNullOrEmpty(label))
@@ -69,42 +72,51 @@ namespace UnityEditor.ShaderGraph.Drawing.Controls
             var outputButtonBlue = new Button(changedOutputBlue);
             Add(outputButtonBlue);
 
-            ChannelMixerNode.ChannelMixer channelMixer = (ChannelMixerNode.ChannelMixer)m_PropertyInfo.GetValue(m_Node, null);
-
             Add(new Label("Red"));
-            Action<float> changedRedIn = (s) => { OnChangeSlider(s, m_OutChannel, 0); };
-            redSlider = new Slider(m_Minimum, m_Maximum, changedRedIn) { value = channelMixer.outChannels[m_OutChannel].inChannels[0] };
-            redSlider.value = 1; // This refuses to initialize at its default value
-            Add(redSlider);
+            Action<float> changedRedIn = (s) => { OnChangeSlider(s, 0); };
+            m_RedSlider = new Slider(m_Minimum, m_Maximum, changedRedIn);
+            Add(m_RedSlider);
 
             Add(new Label("Green"));
-            Action<float> changedGreenIn = (s) => { OnChangeSlider(s, m_OutChannel, 1); };
-            greenSlider = new Slider(m_Minimum, m_Maximum, changedGreenIn) { value = channelMixer.outChannels[m_OutChannel].inChannels[1] };
-            Add(greenSlider);
+            Action<float> changedGreenIn = (s) => { OnChangeSlider(s, 1); };
+            m_GreenSlider = new Slider(m_Minimum, m_Maximum, changedGreenIn);
+            Add(m_GreenSlider);
 
             Add(new Label("Blue"));
-            Action<float> changedBlueIn = (s) => { OnChangeSlider(s, m_OutChannel, 2); };
-            blueSlider = new Slider(m_Minimum, m_Maximum, changedBlueIn) { value = channelMixer.outChannels[m_OutChannel].inChannels[2] };
-            Add(blueSlider);
+            Action<float> changedBlueIn = (s) => { OnChangeSlider(s, 2); };
+            m_BlueSlider = new Slider(m_Minimum, m_Maximum, changedBlueIn);
+            Add(m_BlueSlider);
 
+            m_Initialized = true;
             ResetSliders();
         }
 
         void ResetSliders()
         {
-            ChannelMixerNode.ChannelMixer channelMixer = (ChannelMixerNode.ChannelMixer)m_PropertyInfo.GetValue(m_Node, null);
-            redSlider.value = channelMixer.outChannels[m_OutChannel].inChannels[0];
-            greenSlider.value = channelMixer.outChannels[m_OutChannel].inChannels[1];
-            blueSlider.value = channelMixer.outChannels[m_OutChannel].inChannels[2];
+            Vector3 outputChannel = GetOutputChannel();
+            m_RedSlider.value = outputChannel[0];
+            m_GreenSlider.value = outputChannel[1];
+            m_BlueSlider.value = outputChannel[2];
         }
 
-        void OnChangeSlider(float value, int outChannel, int inChannel)
+        void OnChangeSlider(float value, int inChannel)
         {
+            if (!m_Initialized)
+                return;
             m_Node.owner.owner.RegisterCompleteObjectUndo("Slider Change");
-            ChannelMixerNode.ChannelMixer channelMixer = (ChannelMixerNode.ChannelMixer)m_PropertyInfo.GetValue(m_Node, null);
-            channelMixer.outChannels[outChannel].inChannels[inChannel] = value;
-            m_PropertyInfo.SetValue(m_Node, channelMixer, null);
-            Dirty(ChangeType.Repaint);
+            switch (m_OutChannel)
+            {
+                case 1:
+                    m_ChannelMixer.outGreen[inChannel] = value;
+                    break;
+                case 2:
+                    m_ChannelMixer.outBlue[inChannel] = value;
+                    break;
+                default:
+                    m_ChannelMixer.outRed[inChannel] = value;
+                    break;
+            }
+            m_PropertyInfo.SetValue(m_Node, m_ChannelMixer, null);
         }
 
         void OnClickButton(int outChannel)
@@ -112,7 +124,19 @@ namespace UnityEditor.ShaderGraph.Drawing.Controls
             m_Node.owner.owner.RegisterCompleteObjectUndo("Button Change");
             m_OutChannel = outChannel;
             ResetSliders();
-            Dirty(ChangeType.Repaint);
+        }
+
+        Vector3 GetOutputChannel()
+        {
+            switch (m_OutChannel)
+            {
+                case 1:
+                    return m_ChannelMixer.outGreen;
+                case 2:
+                    return m_ChannelMixer.outBlue;
+                default:
+                    return m_ChannelMixer.outRed;
+            }
         }
     }
 }
