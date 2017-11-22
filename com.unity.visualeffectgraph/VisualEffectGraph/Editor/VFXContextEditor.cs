06c4590bb220959423dae114f9c58f2ca2480130
Tristan Genevet <tristan@unity3d.com>
2017-11-22 17:17:09 +0100

    All VFXModel can appear in the inspector.

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/VFXContextEditor.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/VFXContextEditor.cs
@@ -16,66 +16,20 @@ using System.Reflection;
 
 [CustomEditor(typeof(VFXContext), true)]
 [CanEditMultipleObjects]
-public class VFXContextEditor : Editor
+public class VFXContextEditor : VFXSlotContainerEditor
 {
     SerializedProperty spaceProperty;
-    SerializedProperty[] settingsProperty;
-    void OnEnable()
+    protected new void OnEnable()
     {
         spaceProperty = serializedObject.FindProperty("m_Space");
 
-        ComputeSettings();
+        base.OnEnable();
     }
 
-    void ComputeSettings()
-    {
-        VFXContext context = targets[0] as VFXContext;
-        IEnumerable<FieldInfo> settingFields = context.GetSettings(false, VFXSettingAttribute.VisibleFlags.InInspector);
-
-        for (int i = 1; i < targets.Length; ++i)
-        {
-            IEnumerable<FieldInfo> otherSettingFields = (targets[i] as VFXContext).GetSettings(false, VFXSettingAttribute.VisibleFlags.InInspector);
-
-            settingFields = settingFields.Intersect(otherSettingFields);
-        }
-
-        settingsProperty = settingFields.Select(t => serializedObject.FindProperty(t.Name)).Where(t => t != null).ToArray();
-    }
-
-    void OnDisable()
-    {
-    }
-
-    public void OnSceneGUI()
-    {
-    }
-
-    public override void OnInspectorGUI()
+    public override void DoInspectorGUI()
     {
         EditorGUILayout.PropertyField(spaceProperty);
 
-        foreach (var prop in settingsProperty)
-        {
-            bool visibleChildren = EditorGUILayout.PropertyField(prop);
-            if (visibleChildren)
-            {
-                SerializedProperty childProp = prop.Copy();
-                while (childProp.NextVisible(visibleChildren) && childProp.propertyPath.StartsWith(prop.propertyPath + "."))
-                {
-                    visibleChildren = EditorGUILayout.PropertyField(childProp);
-                }
-            }
-        }
-
-        if (serializedObject.ApplyModifiedProperties())
-        {
-            foreach (VFXContext context in targets.OfType<VFXContext>())
-            {
-                // notify that something changed.
-                context.Invalidate(VFXModel.InvalidationCause.kSettingChanged);
-            }
-
-            ComputeSettings(); // need to recompute settings because some might show/hide
-        }
+        base.DoInspectorGUI();
     }
 }
---
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/VFXSlotContainerEditor.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Experimental;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.VFX;
+using UnityEditor.VFX;
+using UnityEditor.VFX.UI;
+
+using Object = UnityEngine.Object;
+using UnityEditorInternal;
+using System.Reflection;
+
+[CustomEditor(typeof(VFXModel), true)]
+[CanEditMultipleObjects]
+public class VFXSlotContainerEditor : Editor
+{
+    SerializedProperty[] settingsProperty;
+    protected void OnEnable()
+    {
+        ComputeSettings();
+    }
+
+    void ComputeSettings()
+    {
+        var slotContainer = targets[0] as VFXModel;
+        IEnumerable<FieldInfo> settingFields = slotContainer.GetSettings(false, VFXSettingAttribute.VisibleFlags.InInspector);
+
+        for (int i = 1; i < targets.Length; ++i)
+        {
+            IEnumerable<FieldInfo> otherSettingFields = (targets[i] as VFXModel).GetSettings(false, VFXSettingAttribute.VisibleFlags.InInspector);
+
+            settingFields = settingFields.Intersect(otherSettingFields);
+        }
+
+        settingsProperty = settingFields.Select(t => serializedObject.FindProperty(t.Name)).Where(t => t != null).ToArray();
+    }
+
+    public virtual void DoInspectorGUI()
+    {
+        foreach (var prop in settingsProperty)
+        {
+            bool visibleChildren = EditorGUILayout.PropertyField(prop);
+            if (visibleChildren)
+            {
+                SerializedProperty childProp = prop.Copy();
+                while (childProp.NextVisible(visibleChildren) && childProp.propertyPath.StartsWith(prop.propertyPath + "."))
+                {
+                    visibleChildren = EditorGUILayout.PropertyField(childProp);
+                }
+            }
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DoInspectorGUI();
+
+        if (serializedObject.ApplyModifiedProperties())
+        {
+            foreach (VFXContext context in targets.OfType<VFXContext>())
+            {
+                // notify that something changed.
+                context.Invalidate(VFXModel.InvalidationCause.kSettingChanged);
+            }
+
+            ComputeSettings(); // need to recompute settings because some might show/hide
+        }
+    }
+}
