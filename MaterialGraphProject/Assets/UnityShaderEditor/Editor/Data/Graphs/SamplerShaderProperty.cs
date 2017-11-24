92cad845641ee2529d59e92186c38e2e332b8c50
Matt Dean <matthewdean3d@gmail.com>
2017-11-24 11:09:24 +0000

    Depth Texture Node
    - SamplerShaderProperty

---
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Data/Graphs/SamplerShaderProperty.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Text;
+using UnityEngine;
+
+namespace UnityEditor.ShaderGraph
+{
+    [Serializable]
+    public class SamplerShaderProperty : AbstractShaderProperty<float>
+    {
+        public override PropertyType propertyType
+        {
+            get { return PropertyType.Float; }
+        }
+
+        public override Vector4 defaultValue
+        {
+            get { return new Vector4(value, value, value, value);}
+        }
+
+        public override string GetPropertyBlockString()
+        {
+            var result = new StringBuilder();
+            result.Append(referenceName);
+            result.Append("(\"");
+            result.Append(displayName);
+            result.Append("\", Float) = ");
+            result.Append(value);
+            return result.ToString();
+        }
+
+        public override string GetPropertyDeclarationString()
+        {
+            return "sampler2D " + referenceName + ";";
+        }
+
+        public override PreviewProperty GetPreviewMaterialProperty()
+        {
+            return new PreviewProperty()
+            {
+                m_Name = referenceName,
+                m_PropType = PropertyType.Float,
+                m_Float = value
+            };
+        }
+    }
+}
