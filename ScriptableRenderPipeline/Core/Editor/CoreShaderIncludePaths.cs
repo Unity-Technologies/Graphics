66825fb834600f8bb1d471498cd4fefab08ccee4
Peter Bay Bastian <peter.bastian@gmail.com>
2017-11-27 11:47:12 +0100

    Update `CoreShaderIncludePaths` to use `SRPMARKER`

--- ScriptableRenderPipeline/Core/Editor/CoreShaderIncludePaths.cs
+++ ScriptableRenderPipeline/Core/Editor/CoreShaderIncludePaths.cs
@@ -9,11 +9,17 @@ namespace UnityEditor.Experimental.Rendering
         [ShaderIncludePath]
         public static string[] GetPaths()
         {
-            return new[]
+            var srpMarker = Directory.GetFiles(Application.dataPath, "SRPMARKER", SearchOption.AllDirectories).FirstOrDefault();
+            var paths = new string[srpMarker == null ? 1 : 2];
+            var index = 0;
+            if (srpMarker != null)
             {
-                "Assets/ScriptableRenderPipeline/ScriptableRenderPipeline/Core",
-                Path.GetFullPath("Packages/com.unity.render-pipelines.core")
-            };
+                var rootPath = Directory.GetParent(srpMarker).ToString();
+                paths[index] = Path.Combine(rootPath, "ScriptableRenderPipeline/Core");
+                index++;
+            }
+            paths[index] = Path.GetFullPath("Packages/com.unity.render-pipelines.core");
+            return paths;
         }
     }
 }
