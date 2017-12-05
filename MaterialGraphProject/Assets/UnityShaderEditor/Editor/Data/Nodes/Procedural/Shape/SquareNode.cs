9c2d9ad26b7b5ed92cfc03c15fe4438fb2de5988
Matt Dean <matthewdean3d@gmail.com>
2017-12-05 16:39:56 +0000

    Fix Square node

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/Data/Nodes/Procedural/Shape/SquareNode.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Data/Nodes/Procedural/Shape/SquareNode.cs
@@ -1,34 +1,36 @@
 using System.Reflection;
 using UnityEngine;
 
-/*namespace UnityEditor.ShaderGraph
+namespace UnityEditor.ShaderGraph
 {
-    [Title("Procedural", "MakeSubgraph", "Square")]
-    public class BoxNode : CodeFunctionNode
+    [Title("Procedural", "Shape", "Rectangle")]
+    public class RectangleNode : CodeFunctionNode
     {
-        public BoxNode()
+        public RectangleNode()
         {
-            name = "Square";
+            name = "Rectangle";
         }
 
         protected override MethodInfo GetFunctionToConvert()
         {
-            return GetType().GetMethod("Unity_Boxnode", BindingFlags.Static | BindingFlags.NonPublic);
+            return GetType().GetMethod("Unity_Rectangle", BindingFlags.Static | BindingFlags.NonPublic);
         }
 
-        static string Unity_Boxnode(
+        static string Unity_Rectangle(
             [Slot(0, Binding.MeshUV0)] Vector2 UV,
-            [Slot(1, Binding.None, 0.2f, 0.8f, 0, 0)] Vector2 XMinAndMax,
-            [Slot(2, Binding.None, 0.2f, 0.8f, 0, 0)] Vector2 YMinAndMax,
+            [Slot(1, Binding.None, 0.5f, 0, 0, 0)] Vector1 Width,
+            [Slot(2, Binding.None, 0.5f, 0, 0, 0)] Vector1 Height,
             [Slot(3, Binding.None)] out Vector1 Out)
         {
             return
                 @"
 {
+    {precision}2 XMinAndMax = {precision}2(0.5 - Width / 2, 0.5 + Width / 2);
+    {precision}2 YMinAndMax = {precision}2(0.5 - Height / 2, 0.5 + Height / 2);
     {precision} x = step( XMinAndMax.x, UV.x ) - step( XMinAndMax.y, UV.x );
     {precision} y = step( YMinAndMax.x, UV.y ) - step( YMinAndMax.y, UV.y );
     Out = x * y;
 }";
         }
     }
-}*/
+}
