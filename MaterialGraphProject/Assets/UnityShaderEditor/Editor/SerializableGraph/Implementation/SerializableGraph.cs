a1cee6ebe227fc37d59ae6342e1a48f622ddd4e7
Peter Bay Bastian <peter.bastian@gmail.com>
2017-12-06 10:23:11 +0100

    Assembly Definition Files

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/SerializableGraph/Implementation/SerializableGraph.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/SerializableGraph/Implementation/SerializableGraph.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using NUnit.Framework;
 using UnityEngine;
 
 namespace UnityEditor.Graphing
