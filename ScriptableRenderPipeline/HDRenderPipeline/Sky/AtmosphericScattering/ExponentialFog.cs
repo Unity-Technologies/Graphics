debc4887203aba943d64f3812e46d09cda4b72d9
Julien Ignace <julien@unity3d.com>
2017-12-12 11:48:22 +0100

    Update default scene settings object creation to include exp fog and all relevant component.

--- ScriptableRenderPipeline/HDRenderPipeline/Sky/AtmosphericScattering/ExponentialFog.cs
+++ ScriptableRenderPipeline/HDRenderPipeline/Sky/AtmosphericScattering/ExponentialFog.cs
@@ -8,16 +8,13 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
 {
     public class ExponentialFog : AtmosphericScattering
     {
-        // Exp Fog
         private readonly static int m_ExpFogParam = Shader.PropertyToID("_ExpFogParameters");
 
-        // Exponential fog
-        public MinFloatParameter fogDistance = new MinFloatParameter(100.0f, 0.0f);
+        public MinFloatParameter fogDistance = new MinFloatParameter(200.0f, 0.0f);
 
         public override void PushShaderParameters(CommandBuffer cmd, RenderingDebugSettings renderingDebug)
         {
             PushShaderParametersCommon(cmd, FogType.Exponential, renderingDebug);
-
             cmd.SetGlobalVector(m_ExpFogParam, new Vector4(Mathf.Max(0.0f, fogDistance), 0.0f, 0.0f, 0.0f));
         }
     }
