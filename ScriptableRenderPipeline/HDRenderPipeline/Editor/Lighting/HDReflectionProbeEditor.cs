b8ab7185de47dddbd3c39b3db861922a9fa75426
Frédéric Vauchelles <fredericv@unity3d.com>
2017-12-13 17:05:25 +0100

    (wip) reflection probe editor

---
+++ ScriptableRenderPipeline/HDRenderPipeline/Editor/Lighting/HDReflectionProbeEditor.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using UnityEditor.AnimatedValues;
+using UnityEditor.Experimental.Rendering;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Experimental.Rendering.HDPipeline;
+using UnityEngine.Rendering;
+
+namespace UnityEditor
+{
+    [CustomEditorForRenderPipeline(typeof(ReflectionProbe), typeof(HDRenderPipelineAsset))]
+    [CanEditMultipleObjects]
+    class HDReflectionProbeEditor : Editor
+    {
+        class SerializedReflectionProbe
+        {
+            internal SerializedProperty mode;
+            internal SerializedProperty renderDynamicObjects;
+
+            public SerializedReflectionProbe(ReflectionProbe target, SerializedObject so)
+            {
+                mode = so.FindProperty("m_Mode");
+                renderDynamicObjects = so.FindProperty("m_RenderDynamicObjects");
+            }
+        }
+
+        class UIState
+        {
+            AnimBool[] m_ModeSettingsDisplays = new AnimBool[Enum.GetValues(typeof(ReflectionProbeMode)).Length];
+
+            internal UIState()
+            {
+                for (var i = 0; i < m_ModeSettingsDisplays.Length; i++)
+                    m_ModeSettingsDisplays[i] = new AnimBool();
+            }
+
+            internal void Reset(UnityAction repaint, ReflectionProbeMode mode)
+            {
+                for (var i = 0; i < m_ModeSettingsDisplays.Length; i++)
+                {
+                    m_ModeSettingsDisplays[i].valueChanged.RemoveAllListeners();
+                    m_ModeSettingsDisplays[i].valueChanged.AddListener(repaint);
+                    m_ModeSettingsDisplays[i].value = (int)mode == i;
+                }
+            }
+
+            public float GetModeFaded(ReflectionProbeMode mode)
+            {
+                return m_ModeSettingsDisplays[(int)mode].faded;
+            }
+
+            public void SetModeTarget(int value)
+            {
+                for (var i = 0; i < m_ModeSettingsDisplays.Length; i++)
+                    m_ModeSettingsDisplays[i].target = i == value;
+            }
+        }
+
+        delegate void Drawer(UIState s, SerializedReflectionProbe p);
+
+
+        SerializedReflectionProbe m_SerializedReflectionProbe;
+        ReflectionProbe m_ReflectionProbe;
+        UIState m_UIState = new UIState();
+
+        void OnEnable()
+        {
+            m_ReflectionProbe = (ReflectionProbe)target;
+            m_SerializedReflectionProbe = new SerializedReflectionProbe(m_ReflectionProbe, serializedObject);
+            m_UIState.Reset(Repaint, m_ReflectionProbe.mode);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            Drawer_ReflectionProbeMode(m_UIState, m_SerializedReflectionProbe);
+            Drawer_ModeSettings(m_UIState, m_SerializedReflectionProbe);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        static readonly GUIContent[] k_Content_ReflectionProbeMode = { new GUIContent("Baked"), new GUIContent("Custom"), new GUIContent("Realtime") };
+        static readonly int[] k_Content_ReflectionProbeModeValues = { (int)ReflectionProbeMode.Baked, (int)ReflectionProbeMode.Custom, (int)ReflectionProbeMode.Realtime };
+        static void Drawer_ReflectionProbeMode(UIState s, SerializedReflectionProbe p)
+        {
+            EditorGUILayout.IntPopup(p.mode, k_Content_ReflectionProbeMode, k_Content_ReflectionProbeModeValues, CoreEditorUtils.GetContent("Type|'Baked Cubemap' uses the 'Auto Baking' mode from the Lighting window. If it is enabled then baking is automatic otherwise manual bake is needed (use the bake button below). \n'Custom' can be used if a custom cubemap is wanted. \n'Realtime' can be used to dynamically re-render the cubemap during runtime (via scripting)."));
+            s.SetModeTarget(p.mode.intValue);
+        }
+
+        static void Drawer_NOOP(UIState s, SerializedReflectionProbe p) { }
+
+
+        static void Drawer_ModeSettings(UIState s, SerializedReflectionProbe p)
+        {
+            for (var i = 0; i < k_ModeDrawers.Length; ++i)
+            {
+                if (EditorGUILayout.BeginFadeGroup(s.GetModeFaded((ReflectionProbeMode)i)))
+                {
+                    ++EditorGUI.indentLevel;
+                    k_ModeDrawers[i](s, p);
+                    --EditorGUI.indentLevel;
+                }
+                EditorGUILayout.EndFadeGroup();
+            }
+        }
+
+        static readonly Drawer[] k_ModeDrawers = { Drawer_NOOP, Drawer_ModeRealtime , Drawer_ModeCustom };
+        static void Drawer_ModeCustom(UIState s, SerializedReflectionProbe p)
+        {
+            EditorGUILayout.PropertyField(p.renderDynamicObjects, CoreEditorUtils.GetContent("Dynamic Objects|If enabled dynamic objects are also rendered into the cubemap"));
+        }
+
+        static void Drawer_ModeRealtime(UIState s, SerializedReflectionProbe p) { }
+    }
+}
---
+++ ScriptableRenderPipeline/HDRenderPipeline/Lighting/HDAdditionalReflectionData.cs
@@ -0,0 +1,73 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace UnityEngine.Experimental.Rendering
+{
+    public enum ReflectionInfluenceShape { Box, Sphere };
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(ReflectionProbe), typeof(MeshFilter), typeof(MeshRenderer))]
+    public class HDAdditionalReflectionData : MonoBehaviour
+    {
+        public ReflectionInfluenceShape m_InfluenceShape;
+        [Range(0.0f,1.0f)]
+        public float m_Dimmer = 1.0f;
+        public float m_InfluenceSphereRadius = 3.0f;
+        public float m_SphereReprojectionVolumeRadius = 1.0f;
+        public bool m_UseSeparateProjectionVolume = false;
+        public Vector3 m_BoxReprojectionVolumeSize = Vector3.one;
+        public Vector3 m_BoxReprojectionVolumeCenter = Vector3.zero;
+        public float m_MaxSearchDistance = 8.0f;
+        private MeshRenderer m_PreviewMeshRenderer;
+        public Texture m_PreviewCubemap;
+        private MeshFilter m_PreviewMeshFilter;
+        private static Mesh m_SphereMesh;
+        private static Material m_PreviewMaterial;
+        private bool m_Visible;
+
+
+#if UNITY_EDITOR
+        private static Mesh sphereMesh
+        {
+            get { return m_SphereMesh ?? (m_SphereMesh = Resources.GetBuiltinResource(typeof(Mesh), "New-Sphere.fbx") as Mesh); }
+        }
+
+        public Material previewMaterial
+        {
+            get
+            {
+                if (m_PreviewMaterial == null )
+                {
+                    //m_PreviewMaterial = (Material)Instantiate(AssetDatabase.LoadAssetAtPath("Assets/ScriptableRenderPipeline/ScriptableRenderPipeline/HDRenderPipeline/Debug/PreviewCubemapMaterial.mat", typeof(Material)));
+                    m_PreviewMaterial = new Material(Shader.Find("Debug/ReflectionProbePreview"));
+                }
+                if(m_PreviewCubemap != null)
+                    m_PreviewMaterial.SetTexture("_Cubemap", m_PreviewCubemap);
+                m_PreviewMaterial.hideFlags = HideFlags.HideAndDontSave;
+                return m_PreviewMaterial;
+            }
+        }
+
+        private void OnEnable()
+        {
+            m_PreviewMeshFilter = gameObject.GetComponent<MeshFilter>() != null ? gameObject.GetComponent<MeshFilter>() : gameObject.AddComponent<MeshFilter>();
+            m_PreviewMeshRenderer = gameObject.GetComponent<MeshRenderer>() != null ? gameObject.GetComponent<MeshRenderer>() : gameObject.AddComponent<MeshRenderer>();
+            m_PreviewMeshFilter.sharedMesh = sphereMesh;
+            //m_PreviewMeshRenderer.sharedMaterial = previewMaterial;
+            m_PreviewMeshRenderer.lightProbeUsage = UnityEngine.Rendering.LightProbeUsage.Off;
+            m_PreviewMeshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+            m_PreviewMeshRenderer.motionVectorGenerationMode = MotionVectorGenerationMode.ForceNoMotion;
+        }
+
+        public void ChangeVisibility(bool visible)
+        {
+            if(visible != m_Visible)
+            {
+                m_PreviewMeshRenderer.sharedMaterial = previewMaterial;
+                m_PreviewMeshRenderer.enabled = visible ? true : false;
+                m_Visible = visible;
+            }
+        }
+#endif
+    }
+}
