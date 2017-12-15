7e09397101b7dcf0fe902dda3beb93f5be7f5f4a
Julien Fryer <julienf@unity3d.com>
2017-12-15 11:32:24 +0100

    Expose capacity as settings in initialize (consider using a slot?)

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Contexts/Implementations/VFXBasicInitialize.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Contexts/Implementations/VFXBasicInitialize.cs
@@ -6,12 +6,29 @@ namespace UnityEditor.VFX
     [VFXInfo]
     class VFXBasicInitialize : VFXContext
     {
+        [VFXSetting]
+        private uint capacity = 0; // not serialized here but in VFXDataParticle
+
         public VFXBasicInitialize() : base(VFXContextType.kInit, VFXDataType.kSpawnEvent, VFXDataType.kParticle) {}
         public override string name { get { return "Initialize"; } }
         public override string codeGeneratorTemplate { get { return "VFXShaders/VFXInit"; } }
         public override bool codeGeneratorCompute { get { return true; } }
         public override VFXTaskType taskType { get { return VFXTaskType.kInitialize; } }
         public override string renderLoopCommonInclude { get { return "VFXShaders/Common/VFXCommonCompute.cginc"; } }
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+            capacity = ((VFXDataParticle)GetData()).capacity;
+        }
+
+        protected override void OnInvalidate(VFXModel model, VFXModel.InvalidationCause cause)
+        {
+            if (model == this && cause == VFXModel.InvalidationCause.kSettingChanged)
+                ((VFXDataParticle)GetData()).capacity = capacity;
+
+            base.OnInvalidate(model, cause);
+        }
 
         public class InputProperties
         {
