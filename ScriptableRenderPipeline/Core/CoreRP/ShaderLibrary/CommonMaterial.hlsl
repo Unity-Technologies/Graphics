8043fed8c2658537b952f1d5cb0116827ac45f47
Felipe Lira <felipe@unity3d.com>
2017-12-21 19:31:11 +0100

    reverted real with float in GetTriplanarCoordinate

--- ScriptableRenderPipeline/Core/CoreRP/ShaderLibrary/CommonMaterial.hlsl
+++ ScriptableRenderPipeline/Core/CoreRP/ShaderLibrary/CommonMaterial.hlsl
@@ -102,13 +102,13 @@ real3 ComputeTriplanarWeights(real3 normal)
 }
 
 // Planar/Triplanar convention for Unity in world space
-void GetTriplanarCoordinate(real3 position, out real2 uvXZ, out real2 uvXY, out real2 uvZY)
+void GetTriplanarCoordinate(float3 position, out float2 uvXZ, out float2 uvXY, out float2 uvZY)
 {
     // Caution: This must follow the same rule as what is use for SurfaceGradient triplanar
     // TODO: Currently the normal mapping looks wrong without SURFACE_GRADIENT option because we don't handle corretly the tangent space
-    uvXZ = real2(position.z, position.x);
-    uvXY = real2(position.x, position.y);
-    uvZY = real2(position.z, position.y);
+    uvXZ = float2(position.z, position.x);
+    uvXY = float2(position.x, position.y);
+    uvZY = float2(position.z, position.y);
 }
 
 real LerpWhiteTo(real b, real t)
