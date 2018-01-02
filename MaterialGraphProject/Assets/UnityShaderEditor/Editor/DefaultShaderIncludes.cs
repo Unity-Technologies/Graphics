7d5a907afb5a73e34ca416335c710809bb402024
Tim Cooper <tim@unity3d.com>
2018-01-02 16:10:38 +0100

    For LW use the proper shader paths that get exported from the LW package..

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/DefaultShaderIncludes.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/DefaultShaderIncludes.cs
@@ -9,11 +9,8 @@ namespace UnityEditor
         {
             return new[]
             {
-                Path.GetFullPath("Packages/com.unity.render-pipelines.core"),
-                Path.GetFullPath("Packages/com.unity.render-pipelines.lightweight/Shaders"),
-                Path.GetFullPath("Packages/com.unity.render-pipelines.high-quality/Material/Unlit"),
-                Path.GetFullPath("Assets/UnityShaderEditor/Editor"),
-                Path.GetFullPath("Packages/com.unity.shadergraph/Editor"),
+                Path.GetFullPath("Assets/UnityShaderEditor"),
+                Path.GetFullPath("Packages/com.unity.shadergraph"),
             };
         }
     }
