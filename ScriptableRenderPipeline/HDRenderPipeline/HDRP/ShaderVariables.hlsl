dec370d68e4f8a26cdf6c9c6d57383f8f961282b
sebastienlagarde <sebastien@unity3d.com>
2018-01-03 16:48:55 +0100

    HDRenderPipeline: Move variables in correct cbuffer

--- ScriptableRenderPipeline/HDRenderPipeline/HDRP/ShaderVariables.hlsl
+++ ScriptableRenderPipeline/HDRenderPipeline/HDRP/ShaderVariables.hlsl
@@ -10,10 +10,6 @@
 // As I haven't change the variables name yet, I simply don't define anything, and I put the transform function at the end of the file outside the guard header.
 // This need to be fixed.
 
-#if defined (DIRECTIONAL_COOKIE) || defined (DIRECTIONAL)
-    #define USING_DIRECTIONAL_LIGHT
-#endif
-
 #if defined(UNITY_SINGLE_PASS_STEREO) || defined(UNITY_STEREO_INSTANCING_ENABLED) || defined(UNITY_STEREO_MULTIVIEW_ENABLED)
     #define USING_STEREO_MATRICES
 #endif
@@ -95,12 +91,7 @@ CBUFFER_END
 
 // ----------------------------------------------------------------------------
 
-CBUFFER_START(UnityPerDraw : register(b0))
-#ifdef UNITY_USE_PREMULTIPLIED_MATRICES
-    float4x4 glstate_matrix_mvp;
-    float4x4 glstate_matrix_modelview0;
-    float4x4 glstate_matrix_invtrans_modelview0;
-#endif
+CBUFFER_START(UnityPerDraw)
 
     float4x4 unity_ObjectToWorld;
     float4x4 unity_WorldToObject;
@@ -129,6 +120,14 @@ CBUFFER_START(UnityPerDraw : register(b0))
 
     // This contain occlusion factor from 0 to 1 for dynamic objects (no SH here)
     float4 unity_ProbesOcclusion;
+
+    // Velocity
+    float4x4 unity_MatrixPreviousM;
+    float4x4 unity_MatrixPreviousMI;
+    //X : Use last frame positions (right now skinned meshes are the only objects that use this
+    //Y : Force No Motion
+    //Z : Z bias value
+    float4 unity_MotionVectorsParams;
 
 CBUFFER_END
 
@@ -222,17 +221,6 @@ TEXTURE2D(unity_ShadowMask);
 // TODO: Change code here so probe volume use only one transform instead of all this parameters!
 TEXTURE3D(unity_ProbeVolumeSH);
 SAMPLER(samplerunity_ProbeVolumeSH);
-
-CBUFFER_START(UnityVelocityPass)
-    float4x4 unity_MatrixNonJitteredVP;
-    float4x4 unity_MatrixPreviousVP;
-    float4x4 unity_MatrixPreviousM;
-	float4x4 unity_MatrixPreviousMI;
-    //X : Use last frame positions (right now skinned meshes are the only objects that use this
-    //Y : Force No Motion
-    //Z : Z bias value
-    float4 unity_MotionVectorsParams;
-CBUFFER_END
 
 // ----------------------------------------------------------------------------
 
