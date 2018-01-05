52a5fbb09ebe2386fd5517f992283c1eda8e8830
Matt Dean <matthewdean3d@gmail.com>
2018-01-05 15:27:28 +0000

    Add HDR Color
    - Add HDR Color node
    - Add new constructor to ColorShaderProperty
    - Add hdr flag to ColorControl
    - Add HDR Color property to GraphInspectorView

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Inspector/GraphInspectorView.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Inspector/GraphInspectorView.cs
@@ -129,6 +129,7 @@ namespace UnityEditor.ShaderGraph.Drawing.Inspector
             gm.AddItem(new GUIContent("Vector3"), false, () => AddProperty(new Vector3ShaderProperty()));
             gm.AddItem(new GUIContent("Vector4"), false, () => AddProperty(new Vector4ShaderProperty()));
             gm.AddItem(new GUIContent("Color"), false, () => AddProperty(new ColorShaderProperty()));
+            gm.AddItem(new GUIContent("HDR Color"), false, () => AddProperty(new ColorShaderProperty(true)));
             gm.AddItem(new GUIContent("Integer"), false, () => AddProperty(new IntegerShaderProperty()));
             gm.AddItem(new GUIContent("Texture"), false, () => AddProperty(new TextureShaderProperty()));
             gm.AddItem(new GUIContent("Cubemap"), false, () => AddProperty(new CubemapShaderProperty()));
