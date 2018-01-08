511266fef115dd084ec34beee044fc105709ccf6
Frédéric Vauchelles <fredericv@unity3d.com>
2018-01-08 17:38:44 +0100

    [PlanarReflection] (wip) Palanr Reflection Editor

---
+++ ScriptableRenderPipeline/HDRenderPipeline/HDRP/Editor/Lighting/PlanarReflectionProbeEditor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEditor.Experimental.Rendering.HDPipeline;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Experimental.Rendering.HDPipeline;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+namespace UnityEditor.Experimental.Rendering.HDPipeline
+{
+    [CustomEditorForRenderPipeline(typeof(PlanarReflectionProbe), typeof(HDRenderPipelineAsset))]
+    [CanEditMultipleObjects]
+    class PlanarReflectionProbeEditor : Editor
+    {
+        SerializedPlanarReflectionProbe m_SerializedAsset;
+        PlanarReflectionProbeUI m_UIState = new PlanarReflectionProbeUI();
+        PlanarReflectionProbeUI[] m_UIHandleState;
+        PlanarReflectionProbe[] m_TypedTargets;
+
+        void OnEnable()
+        {
+            m_SerializedAsset = new SerializedPlanarReflectionProbe(serializedObject);
+            m_UIState.Reset(m_SerializedAsset, Repaint);
+
+            m_TypedTargets = new PlanarReflectionProbe[targets.Length];
+            m_UIHandleState = new PlanarReflectionProbeUI[m_TypedTargets.Length];
+            for (var i = 0; i < m_TypedTargets.Length; i++)
+            {
+                m_TypedTargets[i] = (PlanarReflectionProbe)targets[i];
+                m_UIHandleState[i] = new PlanarReflectionProbeUI();
+                m_UIHandleState[i].Reset(m_SerializedAsset, Repaint);
+            }
+        }
+
+        public override void OnInspectorGUI()
+        {
+            var s = m_UIState;
+            var d = m_SerializedAsset;
+            var o = this;
+
+            s.Update();
+            d.Update();
+
+            PlanarReflectionProbeUI.Inspector.Draw(s, d, o);
+
+            d.Apply();
+        }
+    }
+}
---
+++ ScriptableRenderPipeline/HDRenderPipeline/HDRP/Editor/Lighting/Volume/SerializedInfluenceVolume.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine.Experimental.Rendering.HDPipeline;
+
+namespace UnityEditor.Experimental.Rendering.HDPipeline
+{
+    public class SerializedInfluenceVolume
+    {
+        public SerializedProperty root;
+
+        public SerializedProperty shapeType;
+        public SerializedProperty boxBaseSize;
+        public SerializedProperty boxBaseOffset;
+        public SerializedProperty boxInfluencePositiveFade;
+        public SerializedProperty boxInfluenceNegativeFade;
+        public SerializedProperty boxInfluenceNormalPositiveFade;
+        public SerializedProperty boxInfluenceNormalNegativeFade;
+        public SerializedProperty sphereBaseRadius;
+        public SerializedProperty sphereBaseOffset;
+        public SerializedProperty sphereInfluenceRadius;
+        public SerializedProperty sphereInfluenceNormalRadius;
+
+        public SerializedInfluenceVolume(SerializedProperty root)
+        {
+            this.root = root;
+
+            shapeType = root.Find((InfluenceVolume i) => i.shapeType);
+            boxBaseSize = root.Find((InfluenceVolume i) => i.boxBaseSize);
+            boxBaseOffset = root.Find((InfluenceVolume i) => i.boxBaseOffset);
+            boxInfluencePositiveFade = root.Find((InfluenceVolume i) => i.boxInfluencePositiveFade);
+            boxInfluenceNegativeFade = root.Find((InfluenceVolume i) => i.boxInfluenceNegativeFade);
+            boxInfluenceNormalPositiveFade = root.Find((InfluenceVolume i) => i.boxInfluenceNormalPositiveFade);
+            boxInfluenceNormalNegativeFade = root.Find((InfluenceVolume i) => i.boxInfluenceNormalNegativeFade);
+            sphereBaseRadius = root.Find((InfluenceVolume i) => i.sphereBaseRadius);
+            sphereBaseOffset = root.Find((InfluenceVolume i) => i.sphereBaseOffset);
+            sphereInfluenceRadius = root.Find((InfluenceVolume i) => i.sphereInfluenceRadius);
+            sphereInfluenceNormalRadius = root.Find((InfluenceVolume i) => i.sphereInfluenceNormalRadius);
+        }
+    }
+}
