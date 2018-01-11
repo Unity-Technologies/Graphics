bc4ec40466e955cbb88989a22547aa35b0872099
Paul Melamed <paulm@unity3d.com>
2018-01-10 17:14:12 -0800

    generating decal normal to decal space matrix in vertex shader

--- ScriptableRenderPipeline/HDRenderPipeline/HDRP/Material/Decal/ShaderPass/DecalSharePass.hlsl
+++ ScriptableRenderPipeline/HDRenderPipeline/HDRP/Material/Decal/ShaderPass/DecalSharePass.hlsl
@@ -2,6 +2,9 @@
 #error Undefine_SHADERPASS
 #endif
 
+// 3 interpolators for decal normal map tangent space
+#define VARYINGS_NEED_POSITION_WS
+#define VARYINGS_NEED_TANGENT_TO_WORLD
 
 // This include will define the various Attributes/Varyings structure
 #include "../../ShaderPass/VaryingMesh.hlsl"
