29f6f0b8b00b58cadada3fece8d5ac2b69de7ea0
Frédéric Vauchelles <fredericv@unity3d.com>
2018-01-17 11:45:31 +0100

    [PlanarReflection] Capture projection

---
+++ ScriptableRenderPipeline/HDRenderPipeline/HDRP/Camera/CameraUtils.cs
@@ -0,0 +1,69 @@
+﻿namespace UnityEngine.Experimental.Rendering.HDPipeline
+{
+    public static class CameraUtils
+    {
+        public static Vector4 Plane(Vector3 position, Vector3 normal)
+        {
+            var n = normal;
+            var d = -Vector3.Dot(n, position);
+            var plane = new Vector4(n.x, n.y, n.z, d);
+            return plane;
+        }
+
+        public static Vector4 Plane(Matrix4x4 basis, Vector3 pos, Vector3 normal, float sideSign = 1, float clipPlaneOffset = 0)
+        {
+            var offsetPos = pos + normal * clipPlaneOffset;
+            var cpos = basis.MultiplyPoint(offsetPos);
+            var cnormal = basis.MultiplyVector(normal).normalized * sideSign;
+            return new Vector4(cnormal.x, cnormal.y, cnormal.z, -Vector3.Dot(cpos, cnormal));
+        }
+
+        public static Matrix4x4 CalculateObliqueMatrix(Matrix4x4 sourceProjection, Vector4 clipPlane)
+        {
+            var projection = sourceProjection;
+            var inversion = sourceProjection.inverse;
+
+            var cps = new Vector4(
+                (clipPlane.x > 0 ? 1 : 0) - (clipPlane.x < 0 ? 1 : 0), 
+                (clipPlane.y > 0 ? 1 : 0) - (clipPlane.y < 0 ? 1 : 0), 
+                1.0f, 
+                1.0f);
+            var q = inversion * cps;
+            var c = clipPlane * (2.0f / Vector4.Dot(clipPlane, q));
+
+            projection[2] = c.x - projection[3];
+            projection[6] = c.y - projection[7];
+            projection[10] = c.z - projection[11];
+            projection[14] = c.w - projection[15];
+
+            return projection;
+        }
+
+        public static Matrix4x4 CalculateReflectionMatrix(Vector4 plane)
+        {
+            var reflectionMat = new Matrix4x4();
+
+            reflectionMat.m00 = (1F - 2F * plane[0] * plane[0]);
+            reflectionMat.m01 = (-2F * plane[0] * plane[1]);
+            reflectionMat.m02 = (-2F * plane[0] * plane[2]);
+            reflectionMat.m03 = (-2F * plane[3] * plane[0]);
+
+            reflectionMat.m10 = (-2F * plane[1] * plane[0]);
+            reflectionMat.m11 = (1F - 2F * plane[1] * plane[1]);
+            reflectionMat.m12 = (-2F * plane[1] * plane[2]);
+            reflectionMat.m13 = (-2F * plane[3] * plane[1]);
+
+            reflectionMat.m20 = (-2F * plane[2] * plane[0]);
+            reflectionMat.m21 = (-2F * plane[2] * plane[1]);
+            reflectionMat.m22 = (1F - 2F * plane[2] * plane[2]);
+            reflectionMat.m23 = (-2F * plane[3] * plane[2]);
+
+            reflectionMat.m30 = 0F;
+            reflectionMat.m31 = 0F;
+            reflectionMat.m32 = 0F;
+            reflectionMat.m33 = 1F;
+
+            return reflectionMat;
+        }
+    }
+}
