c72a03f6d6707b4e1a70a79add8d9acf47f26c83
Sebastien Lagarde <sebastien@unity3d.com>
2018-01-21 14:39:27 +0100

    HDRenderPipeline: Fix misc compiler warning

--- ScriptableRenderPipeline/Core/CoreRP/MousePositionDebug.cs
+++ ScriptableRenderPipeline/Core/CoreRP/MousePositionDebug.cs
@@ -20,9 +20,9 @@ namespace UnityEngine.Experimental.Rendering
             }
         }
 
+#if UNITY_EDITOR
         private Vector2 m_mousePosition = Vector2.zero;
 
-#if UNITY_EDITOR
         private void OnSceneGUI(UnityEditor.SceneView sceneview)
         {
             m_mousePosition = Event.current.mousePosition;
