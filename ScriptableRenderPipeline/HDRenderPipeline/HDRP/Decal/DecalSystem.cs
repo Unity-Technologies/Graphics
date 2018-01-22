6d7b5398a9d32212fec992610c4b7b6d3a56bc16
Paul Melamed <paulm@unity3d.com>
2018-01-22 10:05:24 -0800

    Use decal set KeyMaterial for drawInstanced

--- ScriptableRenderPipeline/HDRenderPipeline/HDRP/Decal/DecalSystem.cs
+++ ScriptableRenderPipeline/HDRenderPipeline/HDRP/Decal/DecalSystem.cs
@@ -155,14 +155,14 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
                     if (instanceCount == kDrawIndexedBatchSize)
                     {
                         m_PropertyBlock.SetMatrixArray(m_NormalToWorldID, m_InstanceNormalToWorld);
-                        cmd.DrawMeshInstanced(m_DecalMesh, 0, m_Decals[0].m_Material, 0, m_InstanceMatrices, kDrawIndexedBatchSize, m_PropertyBlock);
+                        cmd.DrawMeshInstanced(m_DecalMesh, 0, KeyMaterial, 0, m_InstanceMatrices, kDrawIndexedBatchSize, m_PropertyBlock);
                         instanceCount = 0;
                     }
                 }
                 if (instanceCount > 0)
                 {
                     m_PropertyBlock.SetMatrixArray(m_NormalToWorldID, m_InstanceNormalToWorld);
-                    cmd.DrawMeshInstanced(m_DecalMesh, 0, m_Decals[0].m_Material, 0, m_InstanceMatrices, instanceCount, m_PropertyBlock);                    
+                    cmd.DrawMeshInstanced(m_DecalMesh, 0, KeyMaterial, 0, m_InstanceMatrices, instanceCount, m_PropertyBlock);                    
                 }
             }
 
