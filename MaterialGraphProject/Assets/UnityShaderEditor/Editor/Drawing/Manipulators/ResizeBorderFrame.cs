71c8985b5f7cf63186872eb7d69baf2adcfed15e
Jens Holm <jensgholm15@gmail.com>
2018-01-24 14:11:17 +0100

    Add callback in resize handles that notifies when the mouse button is released

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Manipulators/ResizeBorderFrame.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Manipulators/ResizeBorderFrame.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.UIElements;
@@ -7,19 +8,47 @@ using UnityEditor.ShaderGraph.Drawing;
 
 public class ResizeBorderFrame : VisualElement
 {
+    public Action OnResizeFinished;
+
     public ResizeBorderFrame(VisualElement target)
     {
         pickingMode = PickingMode.Ignore;
 
         AddToClassList("reszieBorderFrame");
 
-        Add(new ResizeSideHandle(target, ResizeHandleAnchor.TopLeft));
-        Add(new ResizeSideHandle(target, ResizeHandleAnchor.Top));
-        Add(new ResizeSideHandle(target, ResizeHandleAnchor.TopRight));
-        Add(new ResizeSideHandle(target, ResizeHandleAnchor.Right));
-        Add(new ResizeSideHandle(target, ResizeHandleAnchor.BottomRight));
-        Add(new ResizeSideHandle(target, ResizeHandleAnchor.Bottom));
-        Add(new ResizeSideHandle(target, ResizeHandleAnchor.BottomLeft));
-        Add(new ResizeSideHandle(target, ResizeHandleAnchor.Left));
+        ResizeSideHandle topLeft =  new ResizeSideHandle(target, ResizeHandleAnchor.TopLeft);
+        ResizeSideHandle top =  new ResizeSideHandle(target, ResizeHandleAnchor.Top);
+        ResizeSideHandle topRight =  new ResizeSideHandle(target, ResizeHandleAnchor.TopRight);
+        ResizeSideHandle right = new ResizeSideHandle(target, ResizeHandleAnchor.Right);
+        ResizeSideHandle bottomRight = new ResizeSideHandle(target, ResizeHandleAnchor.BottomRight);
+        ResizeSideHandle bottom = new ResizeSideHandle(target, ResizeHandleAnchor.Bottom);
+        ResizeSideHandle bottomLeft = new ResizeSideHandle(target, ResizeHandleAnchor.BottomLeft);
+        ResizeSideHandle left = new ResizeSideHandle(target, ResizeHandleAnchor.Left);
+
+        topLeft.OnResizeFinished += HandleResizefinished;
+        top.OnResizeFinished += HandleResizefinished;
+        topRight.OnResizeFinished += HandleResizefinished;
+        right.OnResizeFinished += HandleResizefinished;
+        bottomRight.OnResizeFinished += HandleResizefinished;
+        bottom.OnResizeFinished += HandleResizefinished;
+        bottomLeft.OnResizeFinished += HandleResizefinished;
+        left.OnResizeFinished += HandleResizefinished;
+
+        Add(topLeft);
+        Add(top);
+        Add(topRight);
+        Add(right);
+        Add(bottomRight);
+        Add(bottom);
+        Add(bottomLeft);
+        Add(left);
+    }
+
+    void HandleResizefinished()
+    {
+        if (OnResizeFinished != null)
+        {
+            OnResizeFinished();
+        }
     }
 }
