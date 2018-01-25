f1959a066eb2ac9d93a8ea8ddfe889b5be85a255
Tristan Genevet <tristan@unity3d.com>
2018-01-25 18:00:23 +0100

    fixed can't link to flow input.

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/GraphView/Views/Controller/VFXViewController.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/GraphView/Views/Controller/VFXViewController.cs
@@ -537,7 +537,6 @@ namespace UnityEditor.VFX.UI
                 VFXContext owner = anchorController.owner;
                 if (owner == null ||
                     startAnchorController == anchorController ||
-                    !anchorController.IsConnectable() ||
                     startAnchorController.direction == anchorController.direction ||
                     owner == startFlowAnchorController.owner)
                     continue;
