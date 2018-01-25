7db85e905622d7516c0c70483d5ce91356feda25
Julien Fryer <julienf@unity3d.com>
2018-01-25 14:14:50 +0100

    Small fixes + Add basic cube output

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Contexts/Implementations/VFXBasicCubeOutput.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Contexts/Implementations/VFXBasicCubeOutput.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor.VFX.Block;
@@ -7,11 +8,12 @@ using UnityEngine.Experimental.VFX;
 namespace UnityEditor.VFX
 {
     [VFXInfo]
-    class VFXMeshOutput : VFXAbstractParticleOutput
+    class VFXBasicCubeOutput : VFXAbstractParticleOutput
     {
-        public override string name { get { return "Mesh Output"; } }
-        public override string codeGeneratorTemplate { get { return "VFXShaders/VFXParticleMeshes"; } }
-        public override VFXTaskType taskType { get { return VFXTaskType.kParticleMeshOutput; } }
+        public override string name { get { return "Cube Output"; } }
+        public override string codeGeneratorTemplate { get { return "VFXShaders/VFXParticleBasicCube"; } }
+        public override VFXTaskType taskType { get { return VFXTaskType.kParticleHexahedronOutput; } }
+
         public override bool supportsFlipbooks { get { return true; } }
 
         public override IEnumerable<VFXAttributeInfo> attributes
@@ -38,19 +40,6 @@ namespace UnityEditor.VFX
             }
         }
 
-        protected override VFXShaderWriter renderState
-        {
-            get
-            {
-                var rs = base.renderState;
-                if (twoSided)
-                    rs.WriteLine("Cull Off");
-                else
-                    rs.WriteLine("Cull Back");
-                return rs;
-            }
-        }
-
         protected override IEnumerable<VFXNamedExpression> CollectGPUExpressions(IEnumerable<VFXNamedExpression> slotExpressions)
         {
             foreach (var exp in base.CollectGPUExpressions(slotExpressions))
@@ -59,50 +48,15 @@ namespace UnityEditor.VFX
             yield return slotExpressions.First(o => o.name == "mainTexture");
         }
 
-        [VFXSetting(VFXSettingAttribute.VisibleFlags.InInspector), SerializeField, Tooltip("Enable double-sided rendering, or use backface culling.")]
-        private bool twoSided;
-
         public class InputProperties
         {
-            [Tooltip("Texture to be applied to the mesh.")]
             public Texture2D mainTexture;
-            [Tooltip("Mesh to be used for particle rendering.")]
-            public Mesh mesh;
-            [Tooltip("Define a bitmask to control which submeshes are rendered.")]
-            public uint subMeshMask = 0xffffffff;
         }
 
         public class InputPropertiesFlipbook
         {
-            [Tooltip("Texture to be applied to the mesh.")]
             public Texture2D mainTexture;
-            [Tooltip("Mesh to be used for particle rendering.")]
-            public Mesh mesh;
-            [Tooltip("Define a bitmask to control which submeshes are rendered.")]
-            public uint subMeshMask = 0xffffffff;
-            [Tooltip("Specify the number of frames in the flipbook texture.")]
             public Vector2 flipBookSize = new Vector2(5, 5);
-        }
-
-        public override VFXExpressionMapper GetExpressionMapper(VFXDeviceTarget target)
-        {
-            var mapper = base.GetExpressionMapper(target);
-
-            switch (target)
-            {
-                case VFXDeviceTarget.CPU:
-                {
-                    mapper.AddExpression(inputSlots.First(s => s.name == "mesh").GetExpression(), "mesh", -1);
-                    mapper.AddExpression(inputSlots.First(s => s.name == "subMeshMask").GetExpression(), "subMeshMask", -1);
-                    break;
-                }
-                default:
-                {
-                    break;
-                }
-            }
-
-            return mapper;
         }
     }
 }
