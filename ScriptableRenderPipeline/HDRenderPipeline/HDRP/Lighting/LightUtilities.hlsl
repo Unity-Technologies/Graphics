07873fd8d6e520775ab7148e22137027f0b26276
Frédéric Vauchelles <fredericv@unity3d.com>
2018-01-26 13:10:15 +0100

    [PlanarReflection] Zero Initialize for EnvLightData

--- ScriptableRenderPipeline/HDRenderPipeline/HDRP/Lighting/LightUtilities.hlsl
+++ ScriptableRenderPipeline/HDRenderPipeline/HDRP/Lighting/LightUtilities.hlsl
@@ -13,32 +13,22 @@
 EnvLightData InitSkyEnvLightData(int envIndex)
 {
     EnvLightData output;
+    ZERO_INITIALIZE(EnvLightData, output);
     output.influenceShapeType = ENVSHAPETYPE_SKY;
     output.envIndex = envIndex;
-    SETTER_FLOAT3(output, capturePositionWS, float3(0.0, 0.0, 0.0));
 
     SETTER_FLOAT3(output, influenceForward, float3(0.0, 0.0, 1.0));
     SETTER_FLOAT3(output, influenceUp, float3(0.0, 1.0, 0.0));
     SETTER_FLOAT3(output, influenceRight, float3(1.0, 0.0, 0.0));
     SETTER_FLOAT3(output, influencePositionWS, float3(0.0, 0.0, 0.0));
-    SETTER_FLOAT3(output, influenceExtents, float3(0.0, 0.0, 0.0));
 
-    SETTER_FLOAT3(output, blendDistancePositive, float3(0.0, 0.0, 0.0));
-    SETTER_FLOAT3(output, blendDistanceNegative, float3(0.0, 0.0, 0.0));
-    SETTER_FLOAT3(output, blendNormalDistancePositive, float3(0.0, 0.0, 0.0));
-    SETTER_FLOAT3(output, blendNormalDistanceNegative, float3(0.0, 0.0, 0.0));
-    SETTER_FLOAT3(output, boxSideFadePositive, float3(0.0, 0.0, 0.0));
-    SETTER_FLOAT3(output, boxSideFadeNegative, float3(0.0, 0.0, 0.0));
     output.dimmer = 1.0;
-    SETTER_FLOAT3(output, sampleDirectionDiscardWS, float3(0.0, 0.0, 0.0));
 
     // proxy
     SETTER_FLOAT3(output, proxyForward, float3(0.0, 0.0, 1.0));
     SETTER_FLOAT3(output, proxyUp, float3(0.0, 1.0, 0.0));
     SETTER_FLOAT3(output, proxyRight, float3(1.0, 0.0, 0.0));
-    SETTER_FLOAT3(output, proxyPositionWS, float3(0.0, 0.0, 0.0));
     output.minProjectionDistance = 65504.0f;
-    SETTER_FLOAT3(output, proxyExtents, float3(0.0, 0.0, 0.0));
 
     return output;
 }
