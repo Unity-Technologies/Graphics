d122c19c43ea60752d729505317900f9742af4ba
Jens Holm <jensgholm15@gmail.com>
2018-01-26 11:46:48 +0100

    Add dimension check when master preview is resized
    When themaster review is resized, the new size is checked, and if it has not changed, the rendertexture is not changed.

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Inspector/MasterPreviewView.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Inspector/MasterPreviewView.cs
@@ -88,10 +88,19 @@ namespace UnityEditor.ShaderGraph.Drawing.Inspector
         {
             RenderTextureDescriptor descriptor = m_PreviewRenderHandle.renderTexture.descriptor;
 
+            var targetWidth = m_PreviewTextureView.contentRect.width;
+            var targetHeight = m_PreviewTextureView.contentRect.height;
+
+            if (Mathf.Approximately(descriptor.width, targetHeight) && Mathf.Approximately(descriptor.height, targetWidth))
+            {
+                return;
+            }
+
             descriptor.width = (int)m_PreviewTextureView.contentRect.width;
             descriptor.height = (int)m_PreviewTextureView.contentRect.height;
 
             m_PreviewRenderHandle.renderTexture.Release();
+            Object.DestroyImmediate(m_PreviewRenderHandle.renderTexture);
             m_PreviewRenderHandle.renderTexture = new RenderTexture(descriptor);
         }
 
