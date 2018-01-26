f204ab6aa5aadd647b168b547d86fa1cdc3ef5da
Frédéric Vauchelles <fredericv@unity3d.com>
2018-01-26 11:29:31 +0100

    [PlanarReflection] Refactored custom getters and setters

--- ScriptableRenderPipeline/HDRenderPipeline/HDRP/Lighting/Reflection/VolumeProjection.hlsl
+++ ScriptableRenderPipeline/HDRenderPipeline/HDRP/Lighting/Reflection/VolumeProjection.hlsl
@@ -11,16 +11,16 @@ float3x3 WorldToProxySpace(EnvLightData lightData)
 {
     return transpose(
         float3x3(
-            EnvLightData_Get_proxyRight(lightData),
-            EnvLightData_Get_proxyUp(lightData),
-            EnvLightData_Get_proxyForward(lightData)
+            GetProxyRight(lightData),
+            GetProxyUp(lightData),
+            GetProxyForward(lightData)
         )
     ); // worldToLocal assume no scaling
 }
 
 float3 WorldToProxyPosition(EnvLightData lightData, float3x3 worldToPS, float3 positionWS)
 {
-    float3 positionPS = positionWS - EnvLightData_Get_proxyPositionWS(lightData);
+    float3 positionPS = positionWS - GetProxyPositionWS(lightData);
     positionPS = mul(positionPS, worldToPS).xyz;
     return positionPS;
 }
@@ -36,7 +36,7 @@ float IntersectSphereProxy(EnvLightData lightData, float3 dirPS, float3 position
 
 float IntersectBoxProxy(EnvLightData lightData, float3 dirPS, float3 positionPS)
 {
-    float3 boxOuterDistance = EnvLightData_Get_proxyExtents(lightData);
+    float3 boxOuterDistance = GetProxyExtents(lightData);
     float projectionDistance = IntersectRayAABBSimple(positionPS, dirPS, -boxOuterDistance, boxOuterDistance);
     projectionDistance = max(projectionDistance, lightData.minProjectionDistance); // Setup projection to infinite if requested (mean no projection shape)
 
@@ -52,7 +52,7 @@ float InfluenceSphereWeight(EnvLightData lightData, BSDFData bsdfData, float3 po
 
 #if defined(ENVMAP_FEATURE_INFLUENCENORMAL)
     float insideInfluenceNormalVolume = lengthPositionLS <= (lightData.influenceExtentsX - lightData.blendNormalDistancePositiveX) ? 1.0 : 0.0;
-    float insideWeight = InfluenceFadeNormalWeight(bsdfData.normalWS, normalize(positionWS - EnvLightData_Get_capturePositionWS(lightData)));
+    float insideWeight = InfluenceFadeNormalWeight(bsdfData.normalWS, normalize(positionWS - GetCapturePositionWS(lightData)));
     alpha *= insideInfluenceNormalVolume ? 1.0 : insideWeight;
 #endif
 
@@ -61,7 +61,7 @@ float InfluenceSphereWeight(EnvLightData lightData, BSDFData bsdfData, float3 po
 
 float InfluenceBoxWeight(EnvLightData lightData, BSDFData bsdfData, float3 positionWS, float3 positionIS, float3 dirIS)
 {
-    float3 influenceExtents = EnvLightData_Get_influenceExtents(lightData);
+    float3 influenceExtents = GetInfluenceExtents(lightData);
     // 2. Process the position influence
     // Calculate falloff value, so reflections on the edges of the volume would gradually blend to previous reflection.
 #if defined(ENVMAP_FEATURE_PERFACEINFLUENCE) || defined(ENVMAP_FEATURE_INFLUENCENORMAL) || defined(ENVMAP_FEATURE_PERFACEFADE)
@@ -72,8 +72,8 @@ float InfluenceBoxWeight(EnvLightData lightData, BSDFData bsdfData, float3 posit
 
 #if defined(ENVMAP_FEATURE_PERFACEINFLUENCE)
     // Influence falloff for each face
-    float3 negativeFalloff = negativeDistance / max(0.0001, EnvLightData_Get_blendDistanceNegative(lightData));
-    float3 positiveFalloff = positiveDistance / max(0.0001, EnvLightData_Get_blendDistancePositive(lightData));
+    float3 negativeFalloff = negativeDistance / max(0.0001, GetBlendDistanceNegative(lightData));
+    float3 positiveFalloff = positiveDistance / max(0.0001, GetBlendDistancePositive(lightData));
 
     // Fallof is the min for all faces
     float influenceFalloff = min(
@@ -89,10 +89,10 @@ float InfluenceBoxWeight(EnvLightData lightData, BSDFData bsdfData, float3 posit
 #if defined(ENVMAP_FEATURE_INFLUENCENORMAL)
     // 3. Process the normal influence
     // Calculate a falloff value to discard normals pointing outward the center of the environment light
-    float3 belowPositiveInfluenceNormalVolume = positiveDistance / max(0.0001, EnvLightData_Get_blendNormalDistancePositive(lightData));
-    float3 aboveNegativeInfluenceNormalVolume = negativeDistance / max(0.0001, EnvLightData_Get_blendNormalDistanceNegative(lightData));
+    float3 belowPositiveInfluenceNormalVolume = positiveDistance / max(0.0001, GetBlendNormalDistancePositive(lightData));
+    float3 aboveNegativeInfluenceNormalVolume = negativeDistance / max(0.0001, GetBlendNormalDistanceNegative(lightData));
     float insideInfluenceNormalVolume = all(belowPositiveInfluenceNormalVolume >= 1.0) && all(aboveNegativeInfluenceNormalVolume >= 1.0) ? 1.0 : 0;
-    float insideWeight = InfluenceFadeNormalWeight(bsdfData.normalWS, normalize(positionWS - EnvLightData_Get_capturePositionWS(lightData)));
+    float insideWeight = InfluenceFadeNormalWeight(bsdfData.normalWS, normalize(positionWS - GetCapturePositionWS(lightData)));
     alpha *= insideInfluenceNormalVolume ? 1.0 : insideWeight;
 #endif
 
@@ -103,8 +103,8 @@ float InfluenceBoxWeight(EnvLightData lightData, BSDFData bsdfData, float3 posit
     // We consider R.x as cos(X) and then fade as angle from 60°(=acos(1/2)) to 75°(=acos(1/4))
     // For positive axes: axisFade = (R - 1/4) / (1/2 - 1/4)
     // <=> axisFace = 4 * R - 1;
-    float3 faceFade = saturate((4 * dirIS - 1) * EnvLightData_Get_boxSideFadePositive(lightData))
-                    + saturate((-4 * dirIS - 1) * EnvLightData_Get_boxSideFadeNegative(lightData));
+    float3 faceFade = saturate((4 * dirIS - 1) * GetBoxSideFadePositive(lightData))
+                    + saturate((-4 * dirIS - 1) * GetBoxSideFadeNegative(lightData));
     alpha *= saturate(faceFade.x + faceFade.y + faceFade.z);
 #endif
 
@@ -117,16 +117,16 @@ float3x3 WorldToInfluenceSpace(EnvLightData lightData)
 {
     return transpose(
         float3x3(
-            EnvLightData_Get_influenceRight(lightData),
-            EnvLightData_Get_influenceUp(lightData),
-            EnvLightData_Get_influenceForward(lightData)
+            GetInfluenceRight(lightData),
+            GetInfluenceUp(lightData),
+            GetInfluenceForward(lightData)
         )
     ); // worldToLocal assume no scaling
 }
 
 float3 WorldToInfluencePosition(EnvLightData lightData, float3x3 worldToIS, float3 positionWS)
 {
-    float3 positionIS = positionWS - EnvLightData_Get_influencePositionWS(lightData);
+    float3 positionIS = positionWS - GetInfluencePositionWS(lightData);
     positionIS = mul(positionIS, worldToIS).xyz;
     return positionIS;
 }
