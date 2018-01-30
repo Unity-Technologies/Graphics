5536169b477725cdb9406f46429061f6f759d85c
Sebastien Lagarde <sebastien@unity3d.com>
2018-01-30 02:04:34 +0100

    Update menu item location and label
    Based on discussion with Shawn MacClelland

--- ScriptableRenderPipeline/Core/CoreRP/Editor/Debugging/DebugMenuEditor.cs
+++ ScriptableRenderPipeline/Core/CoreRP/Editor/Debugging/DebugMenuEditor.cs
@@ -8,7 +8,7 @@ namespace UnityEngine.Experimental.Rendering
         [SerializeField]
         private DebugMenuState m_DebugMenuState;
 
-        [MenuItem("Edit/Render Pipeline/Tools/High Definition/Debug Window", priority = CoreUtils.editMenuPriority2)]
+        [MenuItem("Window/Rendering/Render Pipeline Debug", priority = CoreUtils.editMenuPriority2)]
         static void DisplayDebugMenu()
         {
             var window = EditorWindow.GetWindow<DebugMenuEditor>("Debug Window");
