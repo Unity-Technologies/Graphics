cfc9b6a9e4a81aa584866d2f022ebcd6c74a3f64
Sebastien Lagarde <sebastien@unity3d.com>
2018-01-30 01:24:57 +0100

    Pack normal on 1212bit in 888

--- ScriptableRenderPipeline/HDRenderPipeline/HDRP/Material/Lit/Lit.cs
+++ ScriptableRenderPipeline/HDRenderPipeline/HDRP/Material/Lit/Lit.cs
@@ -176,7 +176,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
 
         public override int GetMaterialGBufferCount() { return (int)GBufferMaterial.Count; }
 
-        RenderTextureFormat[] m_RTFormat4 = { RenderTextureFormat.ARGB32, RenderTextureFormat.ARGB2101010, RenderTextureFormat.ARGB32, RenderTextureFormat.RGB111110Float };
+        RenderTextureFormat[] m_RTFormat4 = { RenderTextureFormat.ARGB32, RenderTextureFormat.ARGB32, RenderTextureFormat.ARGB32, RenderTextureFormat.RGB111110Float };
         RenderTextureReadWrite[] m_RTReadWrite4 = { RenderTextureReadWrite.sRGB, RenderTextureReadWrite.Linear, RenderTextureReadWrite.Linear, RenderTextureReadWrite.Linear };
 
         public override void GetMaterialGBufferDescription(out RenderTextureFormat[] RTFormat, out RenderTextureReadWrite[] RTReadWrite)
