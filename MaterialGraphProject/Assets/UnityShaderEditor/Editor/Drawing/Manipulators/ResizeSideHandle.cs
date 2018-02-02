7d245dab2790254d39e1ce8a577c158af7d4f450
Jens Holm <jensgholm15@gmail.com>
2018-02-02 10:54:32 +0100

    Implement aspect ratio maintaining for vertical resize handles

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Manipulators/ResizeSideHandle.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Manipulators/ResizeSideHandle.cs
@@ -194,11 +194,11 @@ namespace UnityEditor.ShaderGraph.Drawing
 
             if (expandingUp)
             {
-                maxVerticalExpansion = m_ResizeTarget.layout.x;
+                maxVerticalExpansion = m_ResizeTarget.layout.y;
             }
             else
             {
-                maxVerticalExpansion =  m_ResizeTarget.parent.layout.width - m_ResizeTarget.layout.xMax;
+                maxVerticalExpansion =  m_ResizeTarget.parent.layout.height - m_ResizeTarget.layout.yMax;
             }
 
             if (maintainAspectRatio)
@@ -225,8 +225,6 @@ namespace UnityEditor.ShaderGraph.Drawing
         void OnResizeFromHorizontal(Vector2 resizeDelta, ResizeHandleAnchor anchor)
         {
             float resizeAmount = resizeDelta.x / 2f;
-            float aspectRatio = m_ResizeTarget.layout.width / m_ResizeTarget.layout.height;
-            Vector2 minSize = GetMinSize();
 
             Rect newLayout = m_ResizeTarget.layout;
 
@@ -234,7 +232,14 @@ namespace UnityEditor.ShaderGraph.Drawing
 
             if (stayWithinParentBounds)
             {
-                resizeAmount = Mathf.Clamp(resizeAmount, GetMaxContraction(), GetMaxHorizontalExpansion(resizeLeft));
+                if (resizeLeft)
+                {
+                    resizeAmount = Mathf.Clamp(resizeAmount, -GetMaxHorizontalExpansion(resizeLeft), GetMaxContraction());
+                }
+                else
+                {
+                    resizeAmount = Mathf.Clamp(resizeAmount, -GetMaxContraction(), GetMaxHorizontalExpansion(resizeLeft));
+                }
             }
 
             if (resizeLeft)
@@ -268,6 +273,51 @@ namespace UnityEditor.ShaderGraph.Drawing
 
         void OnResizeFromVertical(Vector2 resizeDelta, ResizeHandleAnchor anchor)
         {
+            float resizeAmount = resizeDelta.y / 2f;
+
+            Rect newLayout = m_ResizeTarget.layout;
+
+            bool resizeUp = anchor == ResizeHandleAnchor.Top;
+
+            if (stayWithinParentBounds)
+            {
+                if (resizeUp)
+                {
+                    resizeAmount = Mathf.Clamp(resizeAmount, -GetMaxVerticalExpansion(resizeUp), GetMaxContraction());
+                }
+                else
+                {
+                    resizeAmount = Mathf.Clamp(resizeAmount, -GetMaxContraction(), GetMaxVerticalExpansion(resizeUp));
+                }
+            }
+
+            if (resizeUp)
+            {
+                newLayout.yMin += resizeAmount;
+            }
+            else
+            {
+                newLayout.yMax += resizeAmount;
+            }
+
+            if (maintainAspectRatio)
+            {
+                if (!resizeUp)
+                {
+                    resizeAmount = -resizeAmount;
+                }
+
+                if (!m_DockingLeft)
+                {
+                    newLayout.xMin += resizeAmount;
+                }
+                else
+                {
+                    newLayout.xMax -= resizeAmount;
+                }
+            }
+
+            m_ResizeTarget.layout = newLayout;
         }
 
         void OnResizeFromCorner(Vector2 resizeDelta, ResizeHandleAnchor anchor)
@@ -276,108 +326,10 @@ namespace UnityEditor.ShaderGraph.Drawing
             Vector2 minSize = GetMinSize();
         }
 
-        void OnResize(Vector2 resizeDelta, ResizeDirection direction, ResizeHandleAnchor anchor)
-        {
-            Vector2 normalizedResizeDelta = resizeDelta / 2f;
-
-            Vector2 minSize = new Vector2(60f, 60f);
-
-            if (!Mathf.Approximately(m_ResizeTarget.style.minWidth.value, 0f))
-            {
-                minSize.x = m_ResizeTarget.style.minWidth;
-            }
-
-            if (!Mathf.Approximately(m_ResizeTarget.style.minHeight.value, 0f))
-            {
-                minSize.y = m_ResizeTarget.style.minHeight.value;
-            }
-
-            if (direction == ResizeDirection.Vertical)
-            {
-                normalizedResizeDelta.x = 0f;
-            }
-            else if (direction == ResizeDirection.Horizontal)
-            {
-                normalizedResizeDelta.y = 0f;
-            }
-
-            Rect newLayout = m_ResizeTarget.layout;
-
-            if (maintainAspectRatio)
-            {
-                bool dominantResizeAlongX = Mathf.Abs(normalizedResizeDelta.x) >= Mathf.Abs(normalizedResizeDelta.y);
-
-                float aspectRatio = m_ResizeTarget.layout.width / m_ResizeTarget.layout.height;
-
-                bool expanding;
-
-                if (dominantResizeAlongX)
-                {
-                    expanding = (IsAnchorLeft(anchor) && normalizedResizeDelta.x < 0f) || (!IsAnchorLeft(anchor) && normalizedResizeDelta.x > 0f);
-                }
-                else
-                {
-                    expanding = (IsAnchorTop(anchor) && normalizedResizeDelta.y < 0f) || (!IsAnchorTop(anchor) && normalizedResizeDelta.y > 0f);
-                }
-
-                if (dominantResizeAlongX)
-                {
-                    normalizedResizeDelta.y = 0f;
-
-                    float changeAlongY = Mathf.Abs(normalizedResizeDelta.x) / aspectRatio * .5f;
-
-                    changeAlongY = expanding ? changeAlongY : -changeAlongY;
-
-                    newLayout.yMin -= changeAlongY;
-                    newLayout.yMax += changeAlongY;
-                }
-                else
-                {
-                    normalizedResizeDelta.x = 0f;
-                    float changeAlongX = Mathf.Abs(normalizedResizeDelta.y) * aspectRatio * .5f;
-
-                    changeAlongX = expanding ? changeAlongX : -changeAlongX;
-
-                    newLayout.xMin -= changeAlongX;
-                    newLayout.xMax += changeAlongX;
-                }
-            }
-
-            if (IsAnchorLeft(anchor))
-            {
-                newLayout.xMin = Mathf.Min(newLayout.xMin + normalizedResizeDelta.x, newLayout.xMax - minSize.x);
-            }
-            else
-            {
-                newLayout.xMax = Mathf.Max(newLayout.xMax + normalizedResizeDelta.x, newLayout.xMin + minSize.x);
-            }
-
-            if (IsAnchorTop(anchor))
-            {
-                newLayout.yMin = Mathf.Min(newLayout.yMin + normalizedResizeDelta.y, newLayout.yMax - minSize.y);
-            }
-            else
-            {
-                newLayout.yMax = Mathf.Max(newLayout.yMax + normalizedResizeDelta.y, newLayout.yMin + minSize.y);
-            }
-
-            if (stayWithinParentBounds)
-            {
-                newLayout.xMin = Mathf.Max(newLayout.xMin, 0f);
-                newLayout.yMin = Mathf.Max(newLayout.yMin, 0f);
-                newLayout.xMax = Mathf.Min(newLayout.xMax, m_ResizeTarget.parent.layout.width);
-                newLayout.yMax = Mathf.Min(newLayout.yMax, m_ResizeTarget.parent.layout.height);
-            }
-
-            m_ResizeTarget.layout = newLayout;
-        }
-
         void HandleMouseDown(MouseDownEvent evt)
         {
             m_DockingLeft = m_ResizeTarget.layout.center.x / m_ResizeTarget.parent.layout.width < .5f;
             m_DockingTop = m_ResizeTarget.layout.center.y / m_ResizeTarget.parent.layout.height < .5f;
-
-            Debug.Log("docking left: " + m_DockingLeft + " docking top: " + m_DockingTop);
         }
 
         void HandleDraggableMouseUp(MouseUpEvent evt)
