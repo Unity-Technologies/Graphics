e04d774b357472220f4a65014fdc7ed8e5699ccd
Keijiro Takahashi <keijiro@gmail.com>
2018-02-04 21:47:36 +0900

    Improvements to artistic nodes.
    - Added the fuzziness slot to ReplaceColorNode and ColorMaskNode. It works like
      the fuzziness parameter in Photoshop that gives softer edges to the mask.
    - Changed the output format of ColorMaskNode from Vector3 to Vector1. It only
      outputs single channel masks.
    - Added the opacity slot to BlendNode. It's useful when one tries using
      BlendNode like layers in Photoshop.

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/Data/Nodes/Artistic/Blend/BlendNode.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Data/Nodes/Artistic/Blend/BlendNode.cs
@@ -44,210 +44,244 @@ namespace UnityEditor.ShaderGraph
         static string Unity_Blend_Burn(
             [Slot(0, Binding.None)] DynamicDimensionVector Base,
             [Slot(1, Binding.None)] DynamicDimensionVector Blend,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+            [Slot(2, Binding.None)] Vector1 Opacity,
+            [Slot(3, Binding.None)] out DynamicDimensionVector Out)
         {
             return
                 @"
 {
     Out =  1.0 - (1.0 - Blend)/Base;
+    Out = lerp(Base, Out, Opacity);
 }";
         }
 
         static string Unity_Blend_Darken(
             [Slot(0, Binding.None)] DynamicDimensionVector Base,
             [Slot(1, Binding.None)] DynamicDimensionVector Blend,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+            [Slot(2, Binding.None)] Vector1 Opacity,
+            [Slot(3, Binding.None)] out DynamicDimensionVector Out)
         {
             return
                 @"
 {
     Out = min(Blend, Base);
+    Out = lerp(Base, Out, Opacity);
 }";
         }
 
         static string Unity_Blend_Difference(
             [Slot(0, Binding.None)] DynamicDimensionVector Base,
             [Slot(1, Binding.None)] DynamicDimensionVector Blend,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+            [Slot(2, Binding.None)] Vector1 Opacity,
+            [Slot(3, Binding.None)] out DynamicDimensionVector Out)
         {
             return
                 @"
 {
     Out = abs(Blend - Base);
+    Out = lerp(Base, Out, Opacity);
 }";
         }
 
         static string Unity_Blend_Dodge(
             [Slot(0, Binding.None)] DynamicDimensionVector Base,
             [Slot(1, Binding.None)] DynamicDimensionVector Blend,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+            [Slot(2, Binding.None)] Vector1 Opacity,
+            [Slot(3, Binding.None)] out DynamicDimensionVector Out)
         {
             return
                 @"
 {
     Out = Base / (1.0 - Blend);
+    Out = lerp(Base, Out, Opacity);
 }";
         }
 
         static string Unity_Blend_Divide(
             [Slot(0, Binding.None)] DynamicDimensionVector Base,
             [Slot(1, Binding.None)] DynamicDimensionVector Blend,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+            [Slot(2, Binding.None)] Vector1 Opacity,
+            [Slot(3, Binding.None)] out DynamicDimensionVector Out)
         {
             return
                 @"
 {
     Out = Base / (Blend + 0.000000000001);
+    Out = lerp(Base, Out, Opacity);
 }";
         }
 
         static string Unity_Blend_Exclusion(
             [Slot(0, Binding.None)] DynamicDimensionVector Base,
             [Slot(1, Binding.None)] DynamicDimensionVector Blend,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+            [Slot(2, Binding.None)] Vector1 Opacity,
+            [Slot(3, Binding.None)] out DynamicDimensionVector Out)
         {
             return
                 @"
 {
     Out = Blend + Base - (2.0 * Blend * Base);
+    Out = lerp(Base, Out, Opacity);
 }";
         }
 
         static string Unity_Blend_HardLight(
             [Slot(0, Binding.None)] DynamicDimensionVector Base,
             [Slot(1, Binding.None)] DynamicDimensionVector Blend,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+            [Slot(2, Binding.None)] Vector1 Opacity,
+            [Slot(3, Binding.None)] out DynamicDimensionVector Out)
         {
             return
                 @"
 {
-    {precision}{slot2dimension} result1 = 1.0 - 2.0 * (1.0 - Base) * (1.0 - Blend);
-    {precision}{slot2dimension} result2 = 2.0 * Base * Blend;
-    {precision}{slot2dimension} zeroOrOne = step(Blend, 0.5);
+    {precision}{slot3dimension} result1 = 1.0 - 2.0 * (1.0 - Base) * (1.0 - Blend);
+    {precision}{slot3dimension} result2 = 2.0 * Base * Blend;
+    {precision}{slot3dimension} zeroOrOne = step(Blend, 0.5);
     Out = result2 * zeroOrOne + (1 - zeroOrOne) * result1;
+    Out = lerp(Base, Out, Opacity);
 }";
         }
 
         static string Unity_Blend_HardMix(
             [Slot(0, Binding.None)] DynamicDimensionVector Base,
             [Slot(1, Binding.None)] DynamicDimensionVector Blend,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+            [Slot(2, Binding.None)] Vector1 Opacity,
+            [Slot(3, Binding.None)] out DynamicDimensionVector Out)
         {
             return
                 @"
 {
     Out = step(1 - Base, Blend);
+    Out = lerp(Base, Out, Opacity);
 }";
         }
 
         static string Unity_Blend_Lighten(
             [Slot(0, Binding.None)] DynamicDimensionVector Base,
             [Slot(1, Binding.None)] DynamicDimensionVector Blend,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+            [Slot(2, Binding.None)] Vector1 Opacity,
+            [Slot(3, Binding.None)] out DynamicDimensionVector Out)
         {
             return
                 @"
 {
     Out = max(Blend, Base);
+    Out = lerp(Base, Out, Opacity);
 }";
         }
 
         static string Unity_Blend_LinearBurn(
             [Slot(0, Binding.None)] DynamicDimensionVector Base,
             [Slot(1, Binding.None)] DynamicDimensionVector Blend,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+            [Slot(2, Binding.None)] Vector1 Opacity,
+            [Slot(3, Binding.None)] out DynamicDimensionVector Out)
         {
             return
                 @"
 {
     Out = Base + Blend - 1.0;
+    Out = lerp(Base, Out, Opacity);
 }";
         }
 
         static string Unity_Blend_LinearDodge(
             [Slot(0, Binding.None)] DynamicDimensionVector Base,
             [Slot(1, Binding.None)] DynamicDimensionVector Blend,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+            [Slot(2, Binding.None)] Vector1 Opacity,
+            [Slot(3, Binding.None)] out DynamicDimensionVector Out)
         {
             return
                 @"
 {
     Out = Base + Blend;
+    Out = lerp(Base, Out, Opacity);
 }";
         }
 
         static string Unity_Blend_LinearLight(
             [Slot(0, Binding.None)] DynamicDimensionVector Base,
             [Slot(1, Binding.None)] DynamicDimensionVector Blend,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+            [Slot(2, Binding.None)] Vector1 Opacity,
+            [Slot(3, Binding.None)] out DynamicDimensionVector Out)
         {
             return
                 @"
 {
     Out = Blend < 0.5 ? max(Base + (2 * Blend) - 1, 0) : min(Base + 2 * (Blend - 0.5), 1);
+    Out = lerp(Base, Out, Opacity);
 }";
         }
 
         static string Unity_Blend_LinearLightAddSub(
             [Slot(0, Binding.None)] DynamicDimensionVector Base,
             [Slot(1, Binding.None)] DynamicDimensionVector Blend,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+            [Slot(2, Binding.None)] Vector1 Opacity,
+            [Slot(3, Binding.None)] out DynamicDimensionVector Out)
         {
             return
                 @"
 {
     Out = Blend + 2.0 * Base - 1.0;
+    Out = lerp(Base, Out, Opacity);
 }";
         }
 
         static string Unity_Blend_Multiply(
             [Slot(0, Binding.None)] DynamicDimensionVector Base,
             [Slot(1, Binding.None)] DynamicDimensionVector Blend,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+            [Slot(2, Binding.None)] Vector1 Opacity,
+            [Slot(3, Binding.None)] out DynamicDimensionVector Out)
         {
             return
                 @"
 {
     Out = Base * Blend;
+    Out = lerp(Base, Out, Opacity);
 }";
         }
 
         static string Unity_Blend_Negation(
             [Slot(0, Binding.None)] DynamicDimensionVector Base,
             [Slot(1, Binding.None)] DynamicDimensionVector Blend,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+            [Slot(2, Binding.None)] Vector1 Opacity,
+            [Slot(3, Binding.None)] out DynamicDimensionVector Out)
         {
             return
                 @"
 {
     Out = 1.0 - abs(1.0 - Blend - Base);
+    Out = lerp(Base, Out, Opacity);
 }";
         }
 
         static string Unity_Blend_Screen(
             [Slot(0, Binding.None)] DynamicDimensionVector Base,
             [Slot(1, Binding.None)] DynamicDimensionVector Blend,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+            [Slot(2, Binding.None)] Vector1 Opacity,
+            [Slot(3, Binding.None)] out DynamicDimensionVector Out)
         {
             return
                 @"
 {
     Out = 1.0 - (1.0 - Blend) * (1.0 - Base);
+    Out = lerp(Base, Out, Opacity);
 }";
         }
 
         static string Unity_Blend_Overlay(
             [Slot(0, Binding.None)] DynamicDimensionVector Base,
             [Slot(1, Binding.None)] DynamicDimensionVector Blend,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+            [Slot(2, Binding.None)] Vector1 Opacity,
+            [Slot(3, Binding.None)] out DynamicDimensionVector Out)
         {
             return
                 @"
 {
-    {precision}{slot2dimension} result1 = 1.0 - 2.0 * (1.0 - Base) * (1.0 - Blend);
-    {precision}{slot2dimension} result2 = 2.0 * Base * Blend;
-    {precision}{slot2dimension} zeroOrOne = step(Base, 0.5);
+    {precision}{slot3dimension} result1 = 1.0 - 2.0 * (1.0 - Base) * (1.0 - Blend);
+    {precision}{slot3dimension} result2 = 2.0 * Base * Blend;
+    {precision}{slot3dimension} zeroOrOne = step(Base, 0.5);
     Out = result2 * zeroOrOne + (1 - zeroOrOne) * result1;
+    Out = lerp(Base, Out, Opacity);
 }
 ";
         }
@@ -255,14 +289,16 @@ namespace UnityEditor.ShaderGraph
         static string Unity_Blend_PinLight(
             [Slot(0, Binding.None)] DynamicDimensionVector Base,
             [Slot(1, Binding.None)] DynamicDimensionVector Blend,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+            [Slot(2, Binding.None)] Vector1 Opacity,
+            [Slot(3, Binding.None)] out DynamicDimensionVector Out)
         {
             return
                 @"
 {
-    {precision}{slot2dimension} check = step (0.5, Blend);
-    {precision}{slot2dimension} result1 = check * max(2.0 * (Base - 0.5), Blend);
+    {precision}{slot3dimension} check = step (0.5, Blend);
+    {precision}{slot3dimension} result1 = check * max(2.0 * (Base - 0.5), Blend);
     Out = result1 + (1.0 - check) * min(2.0 * Base, Blend);
+    Out = lerp(Base, Out, Opacity);
 }
 ";
         }
@@ -270,15 +306,17 @@ namespace UnityEditor.ShaderGraph
         static string Unity_Blend_SoftLight(
             [Slot(0, Binding.None)] DynamicDimensionVector Base,
             [Slot(1, Binding.None)] DynamicDimensionVector Blend,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+            [Slot(2, Binding.None)] Vector1 Opacity,
+            [Slot(3, Binding.None)] out DynamicDimensionVector Out)
         {
             return
                 @"
 {
-    {precision}{slot2dimension} result1 = 2.0 * Base * Blend + Base * Base * (1.0 - 2.0 * Blend);
-    {precision}{slot2dimension} result2 = sqrt(Base) * (2.0 * Blend - 1.0) + 2.0 * Base * (1.0 - Blend);
-    {precision}{slot2dimension} zeroOrOne = step(0.5, Blend);
+    {precision}{slot3dimension} result1 = 2.0 * Base * Blend + Base * Base * (1.0 - 2.0 * Blend);
+    {precision}{slot3dimension} result2 = sqrt(Base) * (2.0 * Blend - 1.0) + 2.0 * Base * (1.0 - Blend);
+    {precision}{slot3dimension} zeroOrOne = step(0.5, Blend);
     Out = result2 * zeroOrOne + (1 - zeroOrOne) * result1;
+    Out = lerp(Base, Out, Opacity);
 }
 ";
         }
@@ -286,15 +324,17 @@ namespace UnityEditor.ShaderGraph
         static string Unity_Blend_VividLight(
             [Slot(0, Binding.None)] DynamicDimensionVector Base,
             [Slot(1, Binding.None)] DynamicDimensionVector Blend,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+            [Slot(2, Binding.None)] Vector1 Opacity,
+            [Slot(3, Binding.None)] out DynamicDimensionVector Out)
         {
             return
                 @"
 {
-    {precision}{slot2dimension} result1 = 1.0 - (1.0 - Blend) / (2.0 * Base);
-    {precision}{slot2dimension} result2 = Blend / (2.0 * (1.0 - Base));
-    {precision}{slot2dimension} zeroOrOne = step(0.5, Base);
+    {precision}{slot3dimension} result1 = 1.0 - (1.0 - Blend) / (2.0 * Base);
+    {precision}{slot3dimension} result2 = Blend / (2.0 * (1.0 - Base));
+    {precision}{slot3dimension} zeroOrOne = step(0.5, Base);
     Out = result2 * zeroOrOne + (1 - zeroOrOne) * result1;
+    Out = lerp(Base, Out, Opacity);
 }
 ";
         }
@@ -302,12 +342,14 @@ namespace UnityEditor.ShaderGraph
         static string Unity_Blend_Subtract(
             [Slot(0, Binding.None)] DynamicDimensionVector Base,
             [Slot(1, Binding.None)] DynamicDimensionVector Blend,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+            [Slot(2, Binding.None)] Vector1 Opacity,
+            [Slot(3, Binding.None)] out DynamicDimensionVector Out)
         {
             return
                 @"
 {
     Out = Base - Blend;
+    Out = lerp(Base, Out, Opacity);
 }
 ";
         }
