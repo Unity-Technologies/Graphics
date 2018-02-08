8eb445832e8a8fea52c770fb4449ea26d15c127e
Matt Dean <matthewdean3d@gmail.com>
2018-02-08 15:27:55 +0000

    Combine and Split node fixes

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/Data/Nodes/Channel/CombineNode.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Data/Nodes/Channel/CombineNode.cs
@@ -25,12 +25,18 @@ namespace UnityEditor.ShaderGraph
             [Slot(1, Binding.None)] Vector1 G,
             [Slot(2, Binding.None)] Vector1 B,
             [Slot(3, Binding.None)] Vector1 A,
-            [Slot(4, Binding.None)] out Vector4 RGBA)
+            [Slot(4, Binding.None)] out Vector4 RGBA,
+            [Slot(5, Binding.None)] out Vector3 RGB,
+            [Slot(6, Binding.None)] out Vector2 RG)
         {
             RGBA = Vector4.zero;
+            RGB = Vector3.zero;
+            RG = Vector2.zero;
             return @"
 {
     RGBA = float4(R, G, B, A);
+    RGB = float3(R, G, B);
+    RG = float2(R, G);
 }
 ";
         }
