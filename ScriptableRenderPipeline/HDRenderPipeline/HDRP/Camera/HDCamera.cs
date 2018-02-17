accf126195aaae32c0a612c6c37875c6f9fa0534
Thomas Hourdel <chman.a11w@gmail.com>
2018-02-17 21:27:51 +0100

    Fixed TAA only partially working in edit mode

--- ScriptableRenderPipeline/HDRenderPipeline/HDRP/Camera/HDCamera.cs
+++ ScriptableRenderPipeline/HDRenderPipeline/HDRP/Camera/HDCamera.cs
@@ -122,7 +122,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
         {
             // If TAA is enabled projMatrix will hold a jittered projection matrix. The original,
             // non-jittered projection matrix can be accessed via nonJitteredProjMatrix.
-            bool taaEnabled = Application.isPlaying && camera.cameraType == CameraType.Game &&
+            bool taaEnabled = camera.cameraType == CameraType.Game &&
                 CoreUtils.IsTemporalAntialiasingActive(postProcessLayer);
 
             var nonJitteredCameraProj = camera.projectionMatrix;
