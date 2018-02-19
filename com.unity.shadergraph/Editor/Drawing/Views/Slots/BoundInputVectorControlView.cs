c6b1afdbab4422c39b6bcd60e70ff66fd911254c
Jens Holm <jensgholm15@gmail.com>
2018-02-19 14:33:38 +0100

    Add control view for bound input types

---
+++ com.unity.shadergraph/Editor/Drawing/Views/Slots/BoundInputVectorControlView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEditor.Graphing;
+using UnityEditor.Experimental.UIElements;
+using UnityEngine.Experimental.UIElements;
+
+namespace UnityEditor.ShaderGraph.Drawing.Slots
+{
+    public class BoundInputVectorControlView : VisualElement
+    {
+        public BoundInputVectorControlView(string label)
+        {
+            AddStyleSheetPath("Styles/Controls/BoundInputVectorSlotControlView");
+            Add(new Label(label));
+        }
+    }
+}
