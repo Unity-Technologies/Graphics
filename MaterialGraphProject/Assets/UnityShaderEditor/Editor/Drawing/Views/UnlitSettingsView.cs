fb17e62f39ffc2435167af6c14746ce8da8d2952
Martin Thorzen <martint@unity3d.com>
2018-02-19 13:06:39 +0100

    Added a Settings Menu for Nodes.

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Views/UnlitSettingsView.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Drawing/Views/UnlitSettingsView.cs
@@ -7,41 +7,26 @@ using UnityEngine.Experimental.UIElements;
 
 namespace UnityEditor.ShaderGraph.Drawing
 {
-    public class PBRSettingsView : VisualElement
+    public class UnlitSettingsView : VisualElement
     {
         VisualElement m_Container;
 
-        EnumField m_Model;
         EnumField m_AlphaMode;
-        PBRMasterNode m_Node;
-        public PBRSettingsView(INode node)
+        UnlitMasterNode m_Node;
+        public UnlitSettingsView(INode node)
         {
-            m_Node = (PBRMasterNode)node;
-            var uxml = Resources.Load<VisualTreeAsset>("UXML/PBRSettings");
+            m_Node = (UnlitMasterNode)node;
+            var uxml = Resources.Load<VisualTreeAsset>("UXML/UnlitSettings");
             uxml.CloneTree(this, null);
 
             m_Container = this.Q("container");
 
-            m_Model = new EnumField(m_Node.model);
             m_AlphaMode = new EnumField(m_Node.alphaMode);
-
-            m_Model.AddToClassList("enumcontainer");
             m_AlphaMode.AddToClassList("enumcontainer");
 
-            m_Container.Add(m_Model);
             m_Container.Add(m_AlphaMode);
-            
-            m_Model.OnValueChanged(ChangeModel);
+
             m_AlphaMode.OnValueChanged(ChangeAlphaMode);
-        }
-
-        void ChangeModel(ChangeEvent<Enum> evt)
-        {
-            if (Equals(m_Node.model, evt.newValue))
-                return;
-
-            m_Node.owner.owner.RegisterCompleteObjectUndo("Enum Change");
-            m_Node.model = (PBRMasterNode.Model)evt.newValue;
         }
 
         void ChangeAlphaMode(ChangeEvent<Enum> evt)
