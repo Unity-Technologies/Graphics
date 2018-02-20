7acfc3bc644488103d1b103cad222b64e51d0990
Tristan Genevet <tristan@unity3d.com>
2018-02-20 14:19:38 +0100

    allow seeing generated shaders.

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/Inspector/VFXAssetEditor.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/Inspector/VFXAssetEditor.cs
@@ -9,9 +9,17 @@ using UnityEngine;
 using UnityEngine.Experimental.VFX;
 using UnityEditor.VFX;
 using UnityEditor.VFX.UI;
+using System.IO;
 
 using Object = UnityEngine.Object;
 using UnityEditorInternal;
+
+
+public class VisualEffectAssetEditorStyles
+{
+    public static Texture2D errorIcon = EditorGUIUtility.LoadIcon("console.erroricon.sml");
+    public static Texture2D warningIcon = EditorGUIUtility.LoadIcon("console.warnicon.sml");
+}
 
 [CustomEditor(typeof(VisualEffectAsset))]
 public class VisualEffectAssetEditor : Editor
@@ -33,13 +41,30 @@ public class VisualEffectAssetEditor : Editor
         {
             if (shaderSource.shader != null)
             {
+                GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                GUILayout.Label(shaderSource.name);
+                if (GUILayout.Button("Reveal"))
+                {
+                    OpenTempFile(shaderSource);
+                }
+                GUILayout.EndHorizontal();
                 var errors = shaderSource.compute ? ShaderUtil.GetComputeShaderErrors(shaderSource.shader as ComputeShader) : ShaderUtil.GetShaderErrors(shaderSource.shader as Shader);
 
                 foreach (var error in errors)
                 {
-                    GUILayout.Label(new GUIContent(error.message, string.Format("{0} line:{1} shader:{2}", error.messageDetails, error.line, shaderSource.name)));
+                    GUILayout.Label(new GUIContent(error.message, error.warning != 0 ? VisualEffectAssetEditorStyles.warningIcon : VisualEffectAssetEditorStyles.errorIcon, string.Format("{0} line:{1} shader:{2}", error.messageDetails, error.line, shaderSource.name)));
                 }
             }
         }
+    }
+
+    void OpenTempFile(VFXShaderSourceDesc shader)
+    {
+        string path = AssetDatabase.GetAssetPath(target);
+        string name = Path.GetFileNameWithoutExtension(path);
+        string fileName = "Temp/" + name + "_" + shader.name;
+        File.WriteAllText(fileName, shader.source);
+        EditorUtility.RevealInFinder(fileName);
     }
 }
