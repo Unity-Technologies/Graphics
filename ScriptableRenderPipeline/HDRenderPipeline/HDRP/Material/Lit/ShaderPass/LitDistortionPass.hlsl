003eee3e366b1b8830493c2dc85dd2201e03bb36
Evgenii Golubev <evgenii@unity3d.com>
2018-02-28 17:29:45 +0100

    Make sure passes that require UV2 and UV3 actually have access to them

--- ScriptableRenderPipeline/HDRenderPipeline/HDRP/Material/Lit/ShaderPass/LitDistortionPass.hlsl
+++ ScriptableRenderPipeline/HDRenderPipeline/HDRP/Material/Lit/ShaderPass/LitDistortionPass.hlsl
@@ -22,21 +22,17 @@
 #endif
 
 // About UV
-// If we have a lit shader, only the UV0 is available for opacity or heightmap
-// If we have a layered shader, any UV can be use for this. To reduce the number of variant we groupt UV0/UV1 and UV2/UV3 instead of having variant for UV0/UV1/UV2/UV3
 // When UVX is present, we assume that UVX - 1 ... UV0 is present
 
 #define ATTRIBUTES_NEED_TEXCOORD0
 
 #if REQUIRE_UV_FOR_TESSELATION || REQUIRE_TANGENT_TO_WORLD || defined(_ALPHATEST_ON)
-    #ifdef LAYERED_LIT_SHADER
     #define ATTRIBUTES_NEED_TEXCOORD1
-        #if defined(_REQUIRE_UV2) || defined(_REQUIRE_UV3)
+    #if defined(_REQUIRE_UV2) || defined(_REQUIRE_UV3)
         #define ATTRIBUTES_NEED_TEXCOORD2
-        #endif
-        #if defined(_REQUIRE_UV3)
+    #endif
+    #if defined(_REQUIRE_UV3)
         #define ATTRIBUTES_NEED_TEXCOORD3
-        #endif
     #endif
 #endif
 
