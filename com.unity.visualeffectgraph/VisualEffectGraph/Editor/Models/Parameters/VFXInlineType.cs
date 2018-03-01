b9d25caabe9e923a02b67ccf24dbd79e40fae28e
Paul Demeulenaere <pauld@unity3d.com>
2018-03-01 13:50:18 +0100

    - Create branch & Add inline value provider (like shadergraph) - Fix SetOutputExpressions which wasn't considering composed type

---
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Parameters/VFXInlineType.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+using Object = System.Object;
+
+namespace UnityEditor.VFX
+{
+    class InlineTypeProvider : IVariantProvider
+    {
+        public Dictionary<string, object[]> variants
+        {
+            get
+            {
+                return new Dictionary<string, object[]>
+                {
+                    { "m_Type", VFXLibrary.GetSlotsType().Select(o => new SerializableType(o)).ToArray() }
+                };
+            }
+        }
+    }
+
+    [VFXInfo(category = "Inline", variantProvider = typeof(InlineTypeProvider))]
+    class VFXInlineType : VFXOperator
+    {
+        [SerializeField, VFXSetting(VFXSettingAttribute.VisibleFlags.None)]
+        private SerializableType m_Type;
+
+        public override string name
+        {
+            get
+            {
+                var type = (Type)m_Type;
+                return type == null ? string.Empty : VFXTypeExtension.UserFriendlyName(type);
+            }
+        }
+
+        private IEnumerable<VFXPropertyWithValue> property
+        {
+            get
+            {
+                var type = (Type)m_Type;
+                if (type != null)
+                    yield return new VFXPropertyWithValue(new VFXProperty(type, string.Empty));
+            }
+        }
+
+        protected override IEnumerable<VFXPropertyWithValue> inputProperties { get { return property; } }
+        protected override IEnumerable<VFXPropertyWithValue> outputProperties { get { return property; } }
+
+        protected override VFXExpression[] BuildExpression(VFXExpression[] inputExpression)
+        {
+            return inputExpression;
+        }
+    }
+}
