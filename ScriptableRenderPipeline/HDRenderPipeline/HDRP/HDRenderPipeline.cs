de280251bedbe1e7b48a9046c340ae97f29b7438
Julien Ignace <julien@unity3d.com>
2018-03-06 11:36:40 +0100

    Fixed various issues with debug window - Fixed Shadow Atlas index. - Fixed DebugManager.instance.displayRuntimeUI breaking after domain reload - Removed wrong "Last" entries in material debug. - Fixed Frame Settings debug not being removed correctly on camera disable. - Frame Settings debug layout is now the same as the regular UI.

--- ScriptableRenderPipeline/HDRenderPipeline/HDRP/HDRenderPipeline.cs
+++ ScriptableRenderPipeline/HDRenderPipeline/HDRP/HDRenderPipeline.cs
@@ -433,6 +433,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
 
 #if UNITY_EDITOR
             SceneViewDrawMode.ResetDrawMode();
+            FrameSettings.UnRegisterDebug("Default Camera");
 #endif
         }
 
