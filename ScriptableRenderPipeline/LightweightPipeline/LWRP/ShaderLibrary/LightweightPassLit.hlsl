914a6156067ab196af1b246cc99abeda4e6a6484
Aleksandr Kirillov <aleksandrk@unity3d.com>
2018-03-07 18:24:16 +0100

    Some shuffling around reduces frame time on older devices.

--- ScriptableRenderPipeline/LightweightPipeline/LWRP/ShaderLibrary/LightweightPassLit.hlsl
+++ ScriptableRenderPipeline/LightweightPipeline/LWRP/ShaderLibrary/LightweightPassLit.hlsl
@@ -19,7 +19,7 @@ struct LightweightVertexOutput
 {
     float2 uv                       : TEXCOORD0;
     DECLARE_LIGHTMAP_OR_SH(lightmapUV, vertexSH, 1);
-    float3 posWS                    : TEXCOORD2;
+    float4 posWSShininess           : TEXCOORD2;    // xyz: posWS, w: Shininess * 128
 
 #ifdef _NORMALMAP
     half4 normal                    : TEXCOORD3;    // xyz: normal, w: viewDir.x
@@ -41,7 +41,7 @@ struct LightweightVertexOutput
 
 void InitializeInputData(LightweightVertexOutput IN, half3 normalTS, out InputData inputData)
 {
-    inputData.positionWS = IN.posWS.xyz;
+    inputData.positionWS = IN.posWSShininess.xyz;
 
 #ifdef _NORMALMAP
     half3 viewDir = half3(IN.tangent.w, IN.binormal.w, IN.normal.w);
@@ -85,10 +85,11 @@ LightweightVertexOutput LitPassVertex(LightweightVertexInput v)
 
     o.uv = TRANSFORM_TEX(v.texcoord, _MainTex);
 
-    o.posWS = TransformObjectToWorld(v.vertex.xyz);
-    o.clipPos = TransformWorldToHClip(o.posWS);
+    o.posWSShininess.xyz = TransformObjectToWorld(v.vertex.xyz);
+    o.posWSShininess.w = _Shininess * 128.0;
+    o.clipPos = TransformWorldToHClip(o.posWSShininess.xyz);
 
-    half3 viewDir = GetCameraPositionWS() - o.posWS;
+    half3 viewDir = GetCameraPositionWS() - o.posWSShininess.xyz;
 #if !SHADER_HINT_NICE_QUALITY
     // Normalize in vertex and avoid renormalizing it in frag to save ALU.
     viewDir = SafeNormalize(viewDir);
@@ -112,7 +113,7 @@ LightweightVertexOutput LitPassVertex(LightweightVertexInput v)
     OUTPUT_LIGHTMAP_UV(v.lightmapUV, unity_LightmapST, o.lightmapUV);
     OUTPUT_SH(o.normal.xyz, o.vertexSH);
 
-    half3 vertexLight = VertexLighting(o.posWS, o.normal.xyz);
+    half3 vertexLight = VertexLighting(o.posWSShininess.xyz, o.normal.xyz);
     half fogFactor = ComputeFogFactor(o.clipPos.z);
     o.fogFactorAndVertexLight = half4(fogFactor, vertexLight);
     o.shadowCoord = ComputeShadowCoord(o.clipPos);
@@ -167,7 +168,7 @@ half4 LitPassFragmentSimple(LightweightVertexOutput IN) : SV_Target
 
     half3 emission = Emission(uv);
     half4 specularGloss = SpecularGloss(uv, diffuseAlpha.a);
-    half shininess = _Shininess * 128.0h;
+    half shininess = IN.posWSShininess.w;
 
     InputData inputData;
     InitializeInputData(IN, normalTS, inputData);
