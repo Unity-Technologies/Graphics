4d6e936cf8bfe47a8f18cd9e0adea3621235a139
thomasi <devnull@localhost>
2018-03-08 17:35:28 +0100

    Optims for PR

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Blocks/Implementations/Forces/Turbulence.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Blocks/Implementations/Forces/Turbulence.cs
@@ -14,19 +14,16 @@ namespace UnityEditor.VFX.Block
             public Transform FieldTransform = Transform.defaultValue;
 
             [Tooltip("Number of Octaves of the noise (Max 10)")]
-            public int NumOctaves = 3;
+            public uint NumOctaves = 3;
 
             [Range(0.0f, 1.0f), Tooltip("The roughness of the turbulence")]
             public float Roughness = 0.5f;
 
             [Tooltip("Intensity of the motion vectors")]
-            public float Intensity;
+            public float Intensity = 1.0f;
             [Tooltip("The drag coefficient used to drive particles")]
-            public float DragCoefficient;
+            public float DragCoefficient = 1.0f;
         }
-
-        [VFXSetting(VFXSettingAttribute.VisibleFlags.InInspector), Tooltip("Texture Data Layout : Actual Values, or Unsigned Normalized (Centered on Gray)")]
-        public TextureDataEncoding DataEncoding = TextureDataEncoding.Signed;
 
         public override string name { get { return "Turbulence"; } }
         public override VFXContextType compatibleContexts { get { return VFXContextType.kUpdate; } }
@@ -47,12 +44,19 @@ namespace UnityEditor.VFX.Block
             {
                 foreach (var input in GetExpressionsFromSlots(this))
                 {
+                    if (input.name == "NumOctaves") continue;
+                    if (input.name == "DragCoefficient") continue;
+
                     if (input.name == "FieldTransform")
                         yield return new VFXNamedExpression(new VFXExpressionInverseMatrix(input.exp), "InvFieldTransform");
                     yield return input;
+
                 }
 
-                yield return new VFXNamedExpression(VFXBuiltInExpression.DeltaTime, "deltaTime");
+                // Clamp (1..10) for octaves
+                yield return new VFXNamedExpression(new VFXExpressionCastFloatToUint(VFXOperatorUtility.Clamp(new VFXExpressionCastUintToFloat(inputSlots[1].GetExpression()), VFXValue.Constant(1.0f), VFXValue.Constant(10.0f))), "octaves");
+
+                yield return new VFXNamedExpression(inputSlots[4].GetExpression() * VFXBuiltInExpression.DeltaTime, "deltaDragCoefficient");
             }
         }
 
@@ -63,10 +67,10 @@ namespace UnityEditor.VFX.Block
                 return @"
 float3 vectorFieldCoord = mul(InvFieldTransform, float4(position,1.0f)).xyz;
 
-float3 value = Noise3D(vectorFieldCoord + 0.5f, min(NumOctaves,10), Roughness);
+float3 value = Noise3D(vectorFieldCoord + 0.5f, octaves, Roughness);
 value = mul(FieldTransform,float4(value,0.0f)).xyz * Intensity;
 float3 relativeForce = value - velocity;
-velocity += relativeForce * min(1.0,(DragCoefficient * deltaTime) / mass);
+velocity += relativeForce * min(1.0, deltaDragCoefficient / mass);
 ";
             }
         }
