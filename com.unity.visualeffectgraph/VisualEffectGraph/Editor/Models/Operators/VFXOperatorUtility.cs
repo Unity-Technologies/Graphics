d36858115ec52c45fba7746783ea5d0e1a0ab19c
Paul Demeulenaere <pauld@unity3d.com>
2018-03-08 17:58:41 +0100

    Minor : Add bitwise operator helper & VanDerCorputSequence utility (use only in test for now)

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Operators/VFXOperatorUtility.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Operators/VFXOperatorUtility.cs
@@ -128,6 +128,17 @@ namespace UnityEditor.VFX
             return (one - E_minusTwoX) / (one + E_minusTwoX);
         }
 
+        static public VFXExpression VanDerCorputSequence(VFXExpression bits) //expect an uint return a float
+        {
+            bits = bits << 16 | bits >> 16;
+            bits = ((bits & 0x55555555u) << 1) | ((bits & 0xAAAAAAAA) >> 1);
+            bits = ((bits & 0x33333333u) << 2) | ((bits & 0xCCCCCCCC) >> 2);
+            bits = ((bits & 0x0F0F0F0Fu) << 4) | ((bits & 0xF0F0F0F0) >> 4);
+            bits = ((bits & 0x00FF00FFu) << 8) | ((bits & 0xFF00FF00) >> 8);
+
+            return new VFXExpressionCastUintToFloat(bits) * new VFXValue<float>(2.3283064365386963e-10f); // / 0x100000000;
+        }
+
         static public VFXExpression Sqrt(VFXExpression input)
         {
             //pow(x, 0.5f)
