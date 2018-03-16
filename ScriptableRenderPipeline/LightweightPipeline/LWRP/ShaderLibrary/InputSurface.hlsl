ec96b0976a6efdb18eb3e86b23fcf61ffded5756
Felipe Lira <felipe@unity3d.com>
2018-03-16 17:08:52 +0100

    replaced bump_scaled_not_supported with quality hint define

--- ScriptableRenderPipeline/LightweightPipeline/LWRP/ShaderLibrary/InputSurface.hlsl
+++ ScriptableRenderPipeline/LightweightPipeline/LWRP/ShaderLibrary/InputSurface.hlsl
@@ -77,7 +77,7 @@ half4 MainTexture(float2 uv)
 half3 Normal(float2 uv)
 {
 #if _NORMALMAP
-    #if BUMP_SCALE_NOT_SUPPORTED
+    #if !SHADER_HINT_NICE_QUALITY
         return UnpackNormal(SAMPLE_TEXTURE2D(_BumpMap, sampler_BumpMap, uv));
     #else
         return UnpackNormalScale(SAMPLE_TEXTURE2D(_BumpMap, sampler_BumpMap, uv), _BumpScale);
