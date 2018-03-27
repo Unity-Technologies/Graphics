2d24c473f6428c05031f005963a3e297a1cdec43
Paul Demeulenaere <pauld@unity3d.com>
2018-03-27 14:26:53 +0200

    Bring back VFXCopyBufferTest.cs

---
+++ TestProjects/VisualEffectGraph/Assets/AllTests/Editor/Tests/VFXCopyBufferTest.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEditor.VFX;
+using UnityEngine.Experimental.VFX;
+
+namespace UnityEditor.VFX.Test
+{
+    [TestFixture]
+    class VFXCopyBufferTest
+    {
+        [Test]
+        public void ProcessBasicTest()
+        {
+            Assert.IsTrue(VisualEffect.DebugCopyBufferComputeTest());
+        }
+    }
+}
