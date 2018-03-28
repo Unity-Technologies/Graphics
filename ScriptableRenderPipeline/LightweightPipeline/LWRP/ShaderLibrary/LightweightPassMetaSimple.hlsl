e57d9201b7156bf24d99bced2e5ddab26e2a9510
Aleksandr Kirillov <aleksandrk@unity3d.com>
2018-03-28 18:12:53 +0200

    Refactored some leftovers missed in the previous refactor. (SampleSpecularGloss)

--- ScriptableRenderPipeline/LightweightPipeline/LWRP/ShaderLibrary/LightweightPassMetaSimple.hlsl
+++ ScriptableRenderPipeline/LightweightPipeline/LWRP/ShaderLibrary/LightweightPassMetaSimple.hlsl
@@ -8,7 +8,7 @@ half4 LightweightFragmentMetaSimple(MetaVertexOuput i) : SV_Target
     float2 uv = i.uv;
     MetaInput o;
     o.Albedo = _Color.rgb * SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv).rgb;
-    o.SpecularColor = SampleSpecularGloss(uv, 1.0).xyz;
+    o.SpecularColor = SampleSpecularGloss(uv, 1.0h, _SpecColor, TEXTURE2D_PARAM(_SpecGlossMap, sampler_SpecGlossMap)).xyz;
     o.Emission = SampleEmission(uv, _EmissionColor.rgb, TEXTURE2D_PARAM(_EmissionMap, sampler_EmissionMap));
 
     return MetaFragment(o);
