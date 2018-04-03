df9a0c7de65c069f1cc99e69ff835697e0d755be
Martin Thorzen <martint@unity3d.com>
2018-04-03 10:16:39 +0200

    Updated with new property sheet and row functionality plus added the .css files for them

---
+++ com.unity.shadergraph/Editor/Drawing/Views/PropertySheet.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using UnityEditorInternal;
+using UnityEngine.Experimental.UIElements;
+
+namespace UnityEditor.ShaderGraph.Drawing
+{
+    public class PropertySheet : VisualElement
+    {
+        VisualElement m_ContentContainer;
+        VisualElement m_HeaderContainer;
+        Label m_Header;
+        public override VisualElement contentContainer
+        {
+            get { return m_ContentContainer; }
+        }
+
+        public VisualElement headerContainer
+        {
+            get { return m_HeaderContainer.FirstOrDefault(); }
+            set
+            {
+                var first = m_HeaderContainer.FirstOrDefault();
+                if( first != null )
+                    first.RemoveFromHierarchy();
+
+                m_HeaderContainer.Add(value);
+            }
+        }
+
+//        public Label header
+//        {
+//            get { return m_Header; }
+//            set { m_Header = value; }
+//        }
+
+        public PropertySheet(Label header = null)
+        {
+            AddStyleSheetPath("Styles/PropertySheet");
+            m_ContentContainer = new VisualElement { name = "content" };
+            m_HeaderContainer = new VisualElement { name = "header" };
+            if( header != null)
+                m_HeaderContainer.Add(header);
+//            header.AddToClassList("header");
+            m_ContentContainer.Add(m_HeaderContainer);
+            shadow.Add(m_ContentContainer);
+        }
+    }
+}
