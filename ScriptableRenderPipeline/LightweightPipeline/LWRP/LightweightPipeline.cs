4efe173310dea90d6bd41be3e27de0b5e5ddf6a5
Thomas <chman.a11w@gmail.com>
2018-04-09 16:22:11 +0200

    Fixed Y-flip in LW when a camera is rendering to a RT

--- ScriptableRenderPipeline/LightweightPipeline/LWRP/LightweightPipeline.cs
+++ ScriptableRenderPipeline/LightweightPipeline/LWRP/LightweightPipeline.cs
@@ -1487,7 +1487,7 @@ namespace UnityEngine.Experimental.Rendering.LightweightPipeline
             m_PostProcessRenderContext.sourceFormat = m_ColorFormat;
             m_PostProcessRenderContext.destination = dest;
             m_PostProcessRenderContext.command = cmd;
-            m_PostProcessRenderContext.flip = true;
+            m_PostProcessRenderContext.flip = m_CurrCamera.targetTexture == null;
 
             if (opaqueOnly)
             {
