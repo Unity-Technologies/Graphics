16e9cfe761986c44b037c5642873c41299f3d1c0
Julien Fryer <julienf@unity3d.com>
2018-04-18 19:50:27 +0200

    SW line renderer

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Contexts/Implementations/VFXLineOutput.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Contexts/Implementations/VFXLineOutput.cs
@@ -10,11 +10,14 @@ namespace UnityEditor.VFX
     class VFXLineOutput : VFXAbstractParticleOutput
     {
         public override string name { get { return "Line Output"; } }
-        public override string codeGeneratorTemplate { get { return RenderPipeTemplate("VFXParticleLines"); } }
-        public override VFXTaskType taskType { get { return VFXTaskType.ParticleLineOutput; } }
+        public override string codeGeneratorTemplate { get { return RenderPipeTemplate(native ? "VFXParticleLinesHW" : "VFXParticleLinesSW"); } }
+        public override VFXTaskType taskType { get { return native ? VFXTaskType.ParticleLineOutput : VFXTaskType.ParticleQuadOutput; } }
 
         [VFXSetting, SerializeField]
         protected bool targetFromAttributes = true;
+
+        [VFXSetting, SerializeField]
+        protected bool native = false;
 
         protected override IEnumerable<string> filteredOutSettings
         {
