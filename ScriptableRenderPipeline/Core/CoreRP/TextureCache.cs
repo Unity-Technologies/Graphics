2acc0043fd151ccf2e4ab5fa431f01f3ae9876cc
Tim Cooper <tim@unity3d.com>
2018-04-19 12:54:54 +0200

    Tizen does not exist in 2018.2+ so hide warning.

--- ScriptableRenderPipeline/Core/CoreRP/TextureCache.cs
+++ ScriptableRenderPipeline/Core/CoreRP/TextureCache.cs
@@ -210,7 +210,9 @@ namespace UnityEngine.Experimental.Rendering
                 {
                     case BuildTarget.iOS:
                     case BuildTarget.Android:
+#if !UNITY_2018_2_OR_NEWER
                     case BuildTarget.Tizen:
+#endif
                     case BuildTarget.WSAPlayer:
                         // Note: We return true on purpose even if Windows Store Apps are running on Desktop.
                         return true;
