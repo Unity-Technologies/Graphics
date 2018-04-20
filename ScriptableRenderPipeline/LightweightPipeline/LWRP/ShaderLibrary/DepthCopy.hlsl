a9b2907bc141abd11985041feb0eb0b9da19718e
Aleksandr Kirillov <aleksandrk@unity3d.com>
2018-04-20 13:32:26 +0200

    Added stereo rendering support to CopyDepth shaders.

--- ScriptableRenderPipeline/LightweightPipeline/LWRP/ShaderLibrary/DepthCopy.hlsl
+++ ScriptableRenderPipeline/LightweightPipeline/LWRP/ShaderLibrary/DepthCopy.hlsl
@@ -7,34 +7,52 @@ struct VertexInput
 {
     float4 vertex   : POSITION;
     float2 uv       : TEXCOORD0;
+    UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
 struct VertexOutput
 {
     float4 position : SV_POSITION;
     float2 uv       : TEXCOORD0;
+    UNITY_VERTEX_INPUT_INSTANCE_ID
+    UNITY_VERTEX_OUTPUT_STEREO
 };
 
 VertexOutput vert(VertexInput i)
 {
     VertexOutput o;
+    UNITY_SETUP_INSTANCE_ID(i);
+    UNITY_TRANSFER_INSTANCE_ID(i, o);
+    UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(o);
     o.uv = i.uv;
     o.position = TransformObjectToHClip(i.vertex.xyz);
     return o;
 }
 
-#if MSAA_DEPTH
-    Texture2DMS<float> _CameraDepthTexture;
+#if defined(UNITY_STEREO_INSTANCING_ENABLED) || defined(UNITY_STEREO_MULTIVIEW_ENABLED)
+#define USE_ARRAY_TEXTURE 1
+#endif
+
+#ifdef MSAA_DEPTH
+    #ifdef USE_ARRAY_TEXTURE
+        Texture2DMSArray<float> _CameraDepthTexture;
+    #else
+        Texture2DMS<float> _CameraDepthTexture;
+    #endif
     float _SampleCount;
     float4 _CameraDepthTexture_TexelSize;
 #else
-    TEXTURE2D(_CameraDepthTexture);
+    #ifdef USE_ARRAY_TEXTURE
+        TEXTURE2D_ARRAY(_CameraDepthTexture);
+    #else
+        TEXTURE2D(_CameraDepthTexture);
+    #endif
     SAMPLER(sampler_CameraDepthTexture);
 #endif
 
 float SampleDepth(float2 uv)
 {
-#if MSAA_DEPTH
+#ifdef MSAA_DEPTH
     int2 coord = int2(uv * _CameraDepthTexture_TexelSize.zw);
     int samples = (int)_SampleCount;
     #if UNITY_REVERSED_Z
@@ -46,12 +64,26 @@ float SampleDepth(float2 uv)
     #endif
 
     for (int i = 0; i < samples; ++i)
-        outDepth = DEPTH_OP(LOAD_TEXTURE2D_MSAA(_CameraDepthTexture, coord, i), outDepth);
+        #ifdef USE_ARRAY_TEXTURE
+            outDepth = DEPTH_OP(LOAD_TEXTURE2D_ARRAY_MSAA(_CameraDepthTexture, uv, unity_StereoEyeIndex, i), outDepth);
+        #else
+            outDepth = DEPTH_OP(LOAD_TEXTURE2D_MSAA(_CameraDepthTexture, uv, i), outDepth);
+        #endif
 
     return outDepth;
 #else
-    return SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, sampler_CameraDepthTexture, uv);
+    #ifdef USE_ARRAY_TEXTURE
+        return SAMPLE_TEXTURE2D_ARRAY(_CameraDepthTexture, sampler_CameraDepthTexture, uv, unity_StereoEyeIndex).r;
+    #else
+        return SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, sampler_CameraDepthTexture, uv);
+    #endif
 #endif
+}
+
+float frag(VertexOutput i) : SV_Depth
+{
+    UNITY_SETUP_INSTANCE_ID(i);
+    return SampleDepth(i.uv);
 }
 
 #endif // LIGHTWEIGHT_DEPTH_COPY_INCLUDED
