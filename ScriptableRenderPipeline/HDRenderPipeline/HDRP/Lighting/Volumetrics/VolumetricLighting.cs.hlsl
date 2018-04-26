91a128f292d24be74274aa52ec0ca9f573b235a2
Stephane Laroche <stephanel@unity3d.com>
2018-04-26 17:52:40 -0400

    Cosmetics, auto-merge cleanup (VolumetricLighting.cs vs compute should be fixed though)

--- ScriptableRenderPipeline/HDRenderPipeline/HDRP/Lighting/Volumetrics/VolumetricLighting.cs.hlsl
+++ ScriptableRenderPipeline/HDRenderPipeline/HDRP/Lighting/Volumetrics/VolumetricLighting.cs.hlsl
@@ -4,22 +4,22 @@
 
 #ifndef VOLUMETRICLIGHTING_CS_HLSL
 #define VOLUMETRICLIGHTING_CS_HLSL
-// Generated from UnityEngine.Experimental.Rendering.HDPipeline.DensityVolumeData
+// Generated from UnityEngine.Experimental.Rendering.HDPipeline.DensityVolumeProperties
 // PackingRules = Exact
-struct DensityVolumeData
+struct DensityVolumeProperties
 {
     float3 scattering;
     float extinction;
 };
 
 //
-// Accessors for UnityEngine.Experimental.Rendering.HDPipeline.DensityVolumeData
+// Accessors for UnityEngine.Experimental.Rendering.HDPipeline.DensityVolumeProperties
 //
-float3 GetScattering(DensityVolumeData value)
+float3 GetScattering(DensityVolumeProperties value)
 {
 	return value.scattering;
 }
-float GetExtinction(DensityVolumeData value)
+float GetExtinction(DensityVolumeProperties value)
 {
 	return value.extinction;
 }
