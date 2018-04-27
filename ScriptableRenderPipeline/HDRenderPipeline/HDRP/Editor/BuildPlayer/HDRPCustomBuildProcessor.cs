e61659590e7e8008c05dd34ee50ad1e0ba8d61a6
sebastienlagarde <sebastien@unity3d.com>
2018-04-27 11:30:21 +0200

    Fix issue with preprocessor

--- ScriptableRenderPipeline/HDRenderPipeline/HDRP/Editor/BuildPlayer/HDRPCustomBuildProcessor.cs
+++ ScriptableRenderPipeline/HDRenderPipeline/HDRP/Editor/BuildPlayer/HDRPCustomBuildProcessor.cs
@@ -2,6 +2,8 @@ using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Experimental.Rendering.HDPipeline;
 
 class HDRPCustomBuildProcessor : IPreprocessBuildWithReport
 {
@@ -9,6 +11,11 @@ class HDRPCustomBuildProcessor : IPreprocessBuildWithReport
 
     public void OnPreprocessBuild(BuildReport report)
     {
+        // Don't execute the preprocess if we are not HDRenderPipeline
+        HDRenderPipeline hdrp = RenderPipelineManager.currentPipeline as HDRenderPipeline;
+        if (hdrp == null)
+            return;
+
         // Note: If you add new platform in this function, think about adding support in IsSupportedPlatform() function in HDRenderPipeline.cs
 
         // If platform is supported all good
