14bceda3a34b4bde1229c4cc19a55eae3630c5a7
Julien Fryer <julienf@unity3d.com>
2018-05-03 18:23:50 +0200

    Add emissive

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Contexts/Implementations/HDRP/VFXAbstractParticleHDRPLitOutput.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Contexts/Implementations/HDRP/VFXAbstractParticleHDRPLitOutput.cs
@@ -16,6 +16,15 @@ namespace UnityEditor.VFX
             Translucent,
         }
 
+        [Flags]
+        public enum ColorMode
+        {
+            None = 0,
+            BaseColor = 1 << 0,
+            Emissive = 1 << 1,
+            BaseColorAndEmissive = BaseColor | Emissive,
+        }
+
         private readonly string[] kMaterialTypeToName = new string[] {
             "StandardProperties",
             "SpecularColorProperties",
@@ -36,6 +45,15 @@ namespace UnityEditor.VFX
 
         [VFXSetting(VFXSettingAttribute.VisibleFlags.InInspector), SerializeField]
         protected bool useNormalMap = false;
+
+        [VFXSetting(VFXSettingAttribute.VisibleFlags.InInspector), SerializeField]
+        protected bool useEmissiveMap = false;
+
+        [VFXSetting(VFXSettingAttribute.VisibleFlags.InInspector), SerializeField]
+        protected ColorMode colorMode = ColorMode.BaseColor;
+
+        [VFXSetting(VFXSettingAttribute.VisibleFlags.InInspector), SerializeField]
+        protected bool useEmissive = false;
 
         protected virtual bool allowTextures { get { return true; }}
 
@@ -65,7 +83,7 @@ namespace UnityEditor.VFX
         public class BaseColorMapProperties
         {
             [Tooltip("Base Color (RGB) Opacity (A)")]
-            public Texture2D baseColor = VFXResources.defaultResources.particleTexture;
+            public Texture2D baseColorMap = VFXResources.defaultResources.particleTexture;
         }
 
         public class MaskMapProperties
@@ -78,6 +96,25 @@ namespace UnityEditor.VFX
         {
             [Tooltip("Normal in tangent space")]
             public Texture2D normalMap;
+            [Range(0, 2)]
+            public float normalScale = 1.0f;
+        }
+
+        public class EmissiveMapProperties
+        {
+            [Tooltip("Normal in tangent space")]
+            public Texture2D emissiveMap;
+            public float emissiveScale = 1.0f;
+        }
+
+        public class BaseColorProperties
+        {
+            public Color baseColor = Color.black;
+        }
+
+        public class EmissiveColorProperties
+        {
+            public Color emissiveColor = Color.black;
         }
 
         protected override IEnumerable<VFXPropertyWithValue> inputProperties
@@ -87,15 +124,28 @@ namespace UnityEditor.VFX
                 var properties = base.inputProperties;
                 properties = properties.Concat(PropertiesFromType("HDRPLitInputProperties"));
                 properties = properties.Concat(PropertiesFromType(kMaterialTypeToName[(int)materialType]));
+
                 if (allowTextures)
                 {
                     if (useBaseColorMap)
                         properties = properties.Concat(PropertiesFromType("BaseColorMapProperties"));
+                }
+
+                if ((colorMode & ColorMode.BaseColor) == 0) // particle color is not used as base color so add a slot
+                    properties = properties.Concat(PropertiesFromType("BaseColorProperties"));
+
+                if (allowTextures)
+                {
                     if (useMaskMap)
                         properties = properties.Concat(PropertiesFromType("MaskMapProperties"));
                     if (useNormalMap)
                         properties = properties.Concat(PropertiesFromType("NormalMapProperties"));
+                    if (useEmissiveMap)
+                        properties = properties.Concat(PropertiesFromType("EmissiveMapProperties"));
                 }
+
+                if (((colorMode & ColorMode.Emissive) == 0) && useEmissive)
+                    properties = properties.Concat(PropertiesFromType("EmissiveColorProperties"));
 
                 return properties;
             }
@@ -129,12 +179,26 @@ namespace UnityEditor.VFX
             if (allowTextures)
             {
                 if (useBaseColorMap)
-                    yield return slotExpressions.First(o => o.name == "baseColor");
+                    yield return slotExpressions.First(o => o.name == "baseColorMap");
                 if (useMaskMap)
                     yield return slotExpressions.First(o => o.name == "maskMap");
                 if (useNormalMap)
+                {
                     yield return slotExpressions.First(o => o.name == "normalMap");
+                    yield return slotExpressions.First(o => o.name == "normalScale");
+                }
+                if (useEmissiveMap)
+                {
+                    yield return slotExpressions.First(o => o.name == "emissiveMap");
+                    yield return slotExpressions.First(o => o.name == "emissiveScale");
+                }
             }
+
+            if ((colorMode & ColorMode.BaseColor) == 0)
+                yield return slotExpressions.First(o => o.name == "baseColor");
+
+            if (((colorMode & ColorMode.Emissive) == 0) && useEmissive)
+                yield return slotExpressions.First(o => o.name == "emissiveColor");
         }
 
         public override IEnumerable<string> additionalDefines
@@ -169,7 +233,19 @@ namespace UnityEditor.VFX
                         yield return "HDRP_USE_MASK_MAP";
                     if (useNormalMap)
                         yield return "HDRP_USE_NORMAL_MAP";
+                    if (useEmissiveMap)
+                        yield return "HDRP_USE_EMISSIVE_MAP";
                 }
+
+                if ((colorMode & ColorMode.BaseColor) != 0)
+                    yield return "HDRP_USE_BASE_COLOR";
+                else
+                    yield return "HDRP_USE_ADDITIONAL_BASE_COLOR";
+
+                if ((colorMode & ColorMode.Emissive) != 0)
+                    yield return "HDRP_USE_EMISSIVE_COLOR";
+                else if (useEmissive)
+                    yield return "HDRP_USE_ADDITIONAL_EMISSIVE_COLOR";
             }
         }
 
@@ -189,6 +265,9 @@ namespace UnityEditor.VFX
                     yield return "useMaskMap";
                     yield return "useNormalMap";
                 }
+
+                if ((colorMode & ColorMode.Emissive) != 0)
+                    yield return "useEmissive";
             }
         }
     }
