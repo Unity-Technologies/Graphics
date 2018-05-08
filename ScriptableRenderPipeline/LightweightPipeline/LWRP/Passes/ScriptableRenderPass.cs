148839654c7298b2b67c1250ad475c017cdb490b
Felipe Lira <felipe@unity3d.com>
2018-05-08 10:51:39 +0200

    Enabled Dynamic batching.

--- ScriptableRenderPipeline/LightweightPipeline/LWRP/Passes/ScriptableRenderPass.cs
+++ ScriptableRenderPipeline/LightweightPipeline/LWRP/Passes/ScriptableRenderPass.cs
@@ -51,6 +51,7 @@ namespace UnityEngine.Experimental.Rendering.LightweightPipeline
             DrawRendererSettings settings = new DrawRendererSettings(camera, m_ShaderPassNames[0]);
             for (int i = 1; i < m_ShaderPassNames.Count; ++i)
                 settings.SetShaderPassName(i, m_ShaderPassNames[i]);
+            settings.flags = DrawRendererFlags.EnableDynamicBatching | DrawRendererFlags.EnableInstancing;
             settings.sorting.flags = sortFlags;
             settings.rendererConfiguration = rendererConfiguration;
             return settings;
