bdfb34f6d410933fa16e84f6f9670c3d55801d07
thomasi <devnull@localhost>
2018-05-25 08:25:26 +0200

    Support for SizeZ

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Blocks/Implementations/Size/ScreenSpaceSize.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/Models/Blocks/Implementations/Size/ScreenSpaceSize.cs
@@ -17,6 +17,16 @@ namespace UnityEditor.VFX.Block
             RatioRelativeToHeightAndWidth
         }
 
+        public enum SizeZMode
+        {
+            Ignore,
+            SameAsSizeX,
+            SameAsSizeY,
+            MinOfSizeXY,
+            MaxOfSizeXY,
+            AverageOfSizeXY
+        }
+
         public class InputPropertiesAbsolute
         {
             public float PixelSize = 16.0f;
@@ -34,6 +44,9 @@ namespace UnityEditor.VFX.Block
 
         [SerializeField, VFXSetting]
         protected SizeMode sizeMode = SizeMode.PixelAbsolute;
+
+        [SerializeField, VFXSetting(VFXSettingAttribute.VisibleFlags.InInspector)]
+        protected SizeZMode sizeZMode = SizeZMode.SameAsSizeX;
 
         public override string name { get { return "Screen Space Size"; } }
         public override VFXContextType compatibleContexts { get { return VFXContextType.kOutput; } }
@@ -57,6 +70,18 @@ namespace UnityEditor.VFX.Block
             }
         }
 
+        protected override IEnumerable<string> filteredOutSettings
+        {
+            get
+            {
+                foreach(var setting in base.filteredOutSettings)
+                    yield return setting;
+
+                if (!GetData().IsCurrentAttributeRead(VFXAttribute.SizeZ))
+                    yield return "sizeZMode";
+            }
+        }
+
 
         public override IEnumerable<VFXAttributeInfo> attributes
         {
@@ -68,6 +93,10 @@ namespace UnityEditor.VFX.Block
 
                 if (GetData().IsCurrentAttributeWritten(VFXAttribute.SizeY) || sizeMode == SizeMode.RatioRelativeToHeightAndWidth || sizeMode == SizeMode.PixelRelativeToResolution)
                     yield return new VFXAttributeInfo(VFXAttribute.SizeY, VFXAttributeMode.ReadWrite);
+
+                // if SizeZ is used, we need to scale it too, in an uniform way.
+                if(GetData().IsCurrentAttributeRead(VFXAttribute.SizeZ) && sizeZMode != SizeZMode.Ignore)
+                    yield return new VFXAttributeInfo(VFXAttribute.SizeZ, VFXAttributeMode.ReadWrite);
             }
         }
 
@@ -86,16 +115,31 @@ namespace UnityEditor.VFX.Block
                     default: throw new NotImplementedException(string.Format("Not Implemented SizeMode: {0}", sizeMode));
                 }
 
-                return string.Format(@"
+                string Source = string.Format(@"
 float2 size = {0};
 float clipPosW = TransformPositionVFXToClip(position).w;
 float minSize = clipPosW / (0.5f * min(UNITY_MATRIX_P[0][0] * _ScreenParams.x,-UNITY_MATRIX_P[1][1] * _ScreenParams.y)); // max size in one pixel
 float2 scale = {2};
 size = minSize * scale;
-{1}",
+{1}
+",
                     VFXBlockUtility.GetSizeVector(GetParent(), 2),
                     VFXBlockUtility.SetSizesFromVector(GetParent(), "size", 2),
                     sizeString);
+
+                if(GetData().IsCurrentAttributeRead(VFXAttribute.SizeZ) && sizeZMode != SizeZMode.Ignore)
+                {
+                    switch(sizeZMode)
+                    {
+                        case SizeZMode.Ignore: break; // should not happen
+                        case SizeZMode.SameAsSizeX:     Source += "sizeZ = size.x;"; break;
+                        case SizeZMode.SameAsSizeY:     Source += "sizeZ = size.y;"; break;
+                        case SizeZMode.MinOfSizeXY:     Source += "sizeZ = min(size.x,size.y);"; break;
+                        case SizeZMode.MaxOfSizeXY:     Source += "sizeZ = max(size.x,size.y);"; break;
+                        case SizeZMode.AverageOfSizeXY: Source += "sizeZ = (size.x + size.y) * 0.5;"; break;
+                    }
+                }
+                return Source;
             }
         }
     }
