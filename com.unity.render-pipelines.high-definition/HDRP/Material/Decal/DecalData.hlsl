dcc1f8aa1515fea8f164c48c8bfa247b6aedb9b1
Paul Melamed <paulm@unity3d.com>
2018-05-30 19:54:59 -0700

    mesh decals somewhat working

--- com.unity.render-pipelines.high-definition/HDRP/Material/Decal/DecalData.hlsl
+++ com.unity.render-pipelines.high-definition/HDRP/Material/Decal/DecalData.hlsl
@@ -4,6 +4,7 @@
 #include "CoreRP/ShaderLibrary/Packing.hlsl"
 #include "CoreRP/ShaderLibrary/Sampling/SampleUVMapping.hlsl"
 
+#if (SHADERPASS == SHADERPASS_DBUFFER)
 void GetSurfaceData(float2 texCoordDS, float4x4 normalToWorld, out DecalSurfaceData surfaceData)
 {
     surfaceData.baseColor = float4(0,0,0,0);
@@ -32,3 +33,31 @@ void GetSurfaceData(float2 texCoordDS, float4x4 normalToWorld, out DecalSurfaceD
     surfaceData.HTileMask |= DBUFFERHTILEBIT_MASK;
 #endif
 }
+#endif
+#if (SHADERPASS == SHADERPASS_MESHDECALS)
+void GetSurfaceData(float2 texCoordDS, out DecalSurfaceData surfaceData)
+{
+	surfaceData.baseColor = float4(0, 0, 0, 0);
+	surfaceData.normalWS = float4(0, 0, 0, 0);
+	surfaceData.mask = float4(0, 0, 0, 0);
+	surfaceData.HTileMask = 0;	
+	float totalBlend = 1.0f;
+#if _COLORMAP
+	surfaceData.baseColor = SAMPLE_TEXTURE2D(_BaseColorMap, sampler_BaseColorMap, texCoordDS.xy);
+	surfaceData.baseColor.w *= totalBlend;
+	totalBlend = surfaceData.baseColor.w;   // base alpha affects all other channels;
+	surfaceData.HTileMask |= DBUFFERHTILEBIT_DIFFUSE;
+#endif
+#if _NORMALMAP
+	surfaceData.normalWS.xyz = UnpackNormalmapRGorAG(SAMPLE_TEXTURE2D(_NormalMap, sampler_NormalMap, texCoordDS)) * 0.5f + 0.5f;
+	surfaceData.normalWS.w = totalBlend;
+	surfaceData.HTileMask |= DBUFFERHTILEBIT_NORMAL;
+#endif
+#if _MASKMAP
+	surfaceData.mask = SAMPLE_TEXTURE2D(_MaskMap, sampler_MaskMap, texCoordDS.xy);
+	surfaceData.mask.z = surfaceData.mask.w;
+	surfaceData.mask.w = totalBlend;
+	surfaceData.HTileMask |= DBUFFERHTILEBIT_MASK;
+#endif
+}
+#endif
--- com.unity.render-pipelines.high-definition/HDRP/Material/Decal/ShaderPass/DecalSharePass.hlsl
+++ com.unity.render-pipelines.high-definition/HDRP/Material/Decal/ShaderPass/DecalSharePass.hlsl
@@ -2,5 +2,13 @@
 #error Undefine_SHADERPASS
 #endif
 
+#define ATTRIBUTES_NEED_NORMAL
+#define ATTRIBUTES_NEED_TANGENT // Always present as we require it also in case of anisotropic lighting
+#define ATTRIBUTES_NEED_TEXCOORD0
+
+#define VARYINGS_NEED_POSITION_WS
+#define VARYINGS_NEED_TANGENT_TO_WORLD
+#define VARYINGS_NEED_TEXCOORD0
+
 // This include will define the various Attributes/Varyings structure
 #include "../../ShaderPass/VaryingMesh.hlsl"
