31109f03c4c62bb0fb96ae71d85b75abf9640ccb
Paul Melamed <paulm@unity3d.com>
2018-05-31 14:28:55 -0700

    Mesh decals working

--- com.unity.render-pipelines.high-definition/HDRP/Material/Decal/DecalData.hlsl
+++ com.unity.render-pipelines.high-definition/HDRP/Material/Decal/DecalData.hlsl
@@ -3,6 +3,8 @@
 //-------------------------------------------------------------------------------------
 #include "CoreRP/ShaderLibrary/Packing.hlsl"
 #include "CoreRP/ShaderLibrary/Sampling/SampleUVMapping.hlsl"
+#include "CoreRP/ShaderLibrary/EntityLighting.hlsl"
+#include "../MaterialUtilities.hlsl"
 
 #if (SHADERPASS == SHADERPASS_DBUFFER)
 void GetSurfaceData(float2 texCoordDS, float4x4 normalToWorld, out DecalSurfaceData surfaceData)
@@ -35,26 +37,29 @@ void GetSurfaceData(float2 texCoordDS, float4x4 normalToWorld, out DecalSurfaceD
 }
 #endif
 #if (SHADERPASS == SHADERPASS_MESHDECALS)
-void GetSurfaceData(float2 texCoordDS, out DecalSurfaceData surfaceData)
+void GetSurfaceData(FragInputs input, out DecalSurfaceData surfaceData)
 {
 	surfaceData.baseColor = float4(0, 0, 0, 0);
 	surfaceData.normalWS = float4(0, 0, 0, 0);
 	surfaceData.mask = float4(0, 0, 0, 0);
 	surfaceData.HTileMask = 0;	
-	float totalBlend = 1.0f;
+	float totalBlend = _DecalBlend;
 #if _COLORMAP
-	surfaceData.baseColor = SAMPLE_TEXTURE2D(_BaseColorMap, sampler_BaseColorMap, texCoordDS.xy);
+	surfaceData.baseColor = SAMPLE_TEXTURE2D(_BaseColorMap, sampler_BaseColorMap, input.texCoord0);
 	surfaceData.baseColor.w *= totalBlend;
 	totalBlend = surfaceData.baseColor.w;   // base alpha affects all other channels;
 	surfaceData.HTileMask |= DBUFFERHTILEBIT_DIFFUSE;
 #endif
 #if _NORMALMAP
-	surfaceData.normalWS.xyz = UnpackNormalmapRGorAG(SAMPLE_TEXTURE2D(_NormalMap, sampler_NormalMap, texCoordDS)) * 0.5f + 0.5f;
+	float3 normalTS = UnpackNormalmapRGorAG(SAMPLE_TEXTURE2D(_NormalMap, sampler_NormalMap, input.texCoord0));
+	float3 normalWS;
+	GetNormalWS(input, 0, normalTS, normalWS);
+	surfaceData.normalWS.xyz = normalWS * 0.5f + 0.5f;	
 	surfaceData.normalWS.w = totalBlend;
 	surfaceData.HTileMask |= DBUFFERHTILEBIT_NORMAL;
 #endif
 #if _MASKMAP
-	surfaceData.mask = SAMPLE_TEXTURE2D(_MaskMap, sampler_MaskMap, texCoordDS.xy);
+	surfaceData.mask = SAMPLE_TEXTURE2D(_MaskMap, sampler_MaskMap, input.texCoord0);
 	surfaceData.mask.z = surfaceData.mask.w;
 	surfaceData.mask.w = totalBlend;
 	surfaceData.HTileMask |= DBUFFERHTILEBIT_MASK;
