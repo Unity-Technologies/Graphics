6548a1e8ff08c8fdba3e61639841d0a4abf35b67
Sebastien Lagarde <sebastien@unity3d.com>
2018-06-04 00:53:22 +0200

    refactor shader variant per material + Add variant stripper for stacklit and selection

--- com.unity.render-pipelines.high-definition/HDRP/Editor/RenderPipeline/HDEditorUtils.cs
+++ com.unity.render-pipelines.high-definition/HDRP/Editor/RenderPipeline/HDEditorUtils.cs
@@ -71,5 +71,19 @@ namespace UnityEditor.Experimental.Rendering.HDPipeline
             }
             return false;
         }
+
+        public static List<ShaderPreprocessorMaterial> GetShaderPreprocessorMaterialList()
+        {
+            var baseType = typeof(ShaderPreprocessorMaterial);
+            var assembly = baseType.Assembly;
+
+            var types = assembly.GetTypes()
+                .Where(t => t.IsSubclassOf(baseType))
+                .Select(Activator.CreateInstance)
+                .Cast<ShaderPreprocessorMaterial>()
+                .ToList();
+
+            return types;
+        }
     }
 }
