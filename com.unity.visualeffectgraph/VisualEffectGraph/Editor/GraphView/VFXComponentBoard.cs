1639d669ce3f7117102ed5816c9956bf766c30f8
Tristan Genevet <tristan@unity3d.com>
2018-06-04 15:10:18 +0200

    renamed ComponentBoard uxml, uss to the right name.

--- com.unity.visualeffectgraph/VisualEffectGraph/Editor/GraphView/VFXComponentBoard.cs
+++ com.unity.visualeffectgraph/VisualEffectGraph/Editor/GraphView/VFXComponentBoard.cs
@@ -145,11 +145,11 @@ namespace  UnityEditor.VFX.UI
         public VFXComponentBoard(VFXView view)
         {
             m_View = view;
-            var tpl = EditorGUIUtility.Load(UXMLHelper.GetUXMLPath("uxml/VFXComponentBlackboardSection.uxml")) as VisualTreeAsset;
+            var tpl = EditorGUIUtility.Load(UXMLHelper.GetUXMLPath("uxml/VFXComponentBoard.uxml")) as VisualTreeAsset;
 
             tpl.CloneTree(contentContainer, new Dictionary<string, VisualElement>());
 
-            contentContainer.AddStyleSheetPath("VFXComponentBlackboardSection");
+            contentContainer.AddStyleSheetPath("VFXComponentBoard");
 
             m_AttachButton = this.Query<Button>("attach");
             m_AttachButton.clickable.clicked += ToggleAttach;
