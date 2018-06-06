b5ed8b332df8ea710d07a046a6662dd8922e9fcc
Thomas <chman.a11w@gmail.com>
2018-06-06 10:45:36 +0200

    Gfx test framework bootstrap

---
+++ com.unity.testframework.graphics/Editor/CreateSceneListFileFromBuildSettings.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Linq;
+using UnityEngine.TestTools;
+
+namespace UnityEditor.TestTools.Graphics
+{
+    internal class CreateSceneListFileFromBuildSettings : IPrebuildSetup
+    {
+        public void Setup()
+        {
+            File.WriteAllLines("Assets/StreamingAssets/SceneList.txt",
+                EditorBuildSettings.scenes.Select(s => s.path).ToArray());
+        }
+    }
+}
