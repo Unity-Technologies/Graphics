bbaf19cad63d99759ba776c93478da9328cafee8
Alex Lindman <alexandral@unity3d.com>
2018-06-14 09:41:24 -0700

    fixing spacing

--- com.unity.shadergraph/Editor/Data/Nodes/Math/Vector/SphereMaskNode.cs
+++ com.unity.shadergraph/Editor/Data/Nodes/Math/Vector/SphereMaskNode.cs
@@ -24,8 +24,8 @@ namespace UnityEditor.ShaderGraph
         static string SphereMask(
             [Slot(0, Binding.None)] DynamicDimensionVector Coords,
             [Slot(1, Binding.None, 0.5f, 0.5f, 0.5f, 0.5f)] DynamicDimensionVector Center,
-			[Slot(2, Binding.None, 0.1f, 0.1f, 0.1f, 0.1f)] Vector1 Radius,
-			[Slot(3, Binding.None, 0.8f, 0.8f, 0.8f, 0.8f)] Vector1 Hardness,
+            [Slot(2, Binding.None, 0.1f, 0.1f, 0.1f, 0.1f)] Vector1 Radius,
+            [Slot(3, Binding.None, 0.8f, 0.8f, 0.8f, 0.8f)] Vector1 Hardness,
             [Slot(4, Binding.None)] out DynamicDimensionVector Out)
         {
             return
