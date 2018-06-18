67c7365a0c229fd2f0b7ed4ae1f55a8a876d3f43
Peter Bay Bastian <peter.bastian@gmail.com>
2018-06-18 11:34:29 +0200

    stuff

--- TestProjects/ShaderGraph/Assets/Testing/Editor/UnitTests/Serialization/UnityStructTests.cs
+++ TestProjects/ShaderGraph/Assets/Testing/Editor/UnitTests/Serialization/UnityStructTests.cs
@@ -10,15 +10,87 @@ namespace UnityEditor.ShaderGraph.UnitTests.Serialization
     public class UnityStructTests
     {
         [Test]
-        public void CanSerializeAndDeserializeVector4()
+        public void CanSerializeAndDeserializeVector2()
         {
-            var originalVector = new Vector4(1.123f, 655f, 43.344f, 985885.34445f);
+            var originalValue = new Vector2(1.123f, 655f);
 
             var jsonSerializerSettings = new JsonSerializerSettings { ContractResolver = new ContractResolver() };
-            var json = JsonConvert.SerializeObject(originalVector, jsonSerializerSettings);
-            var deserializedVector = JsonConvert.DeserializeObject<Vector4>(json, jsonSerializerSettings);
+            var json = JsonConvert.SerializeObject(originalValue, jsonSerializerSettings);
+            var deserializedValue = JsonConvert.DeserializeObject<Vector2>(json, jsonSerializerSettings);
 
-            Assert.AreEqual(originalVector, deserializedVector);
+            Assert.AreEqual(originalValue, deserializedValue);
+        }
+
+        [Test]
+        public void CanSerializeAndDeserializeVector3()
+        {
+            var originalValue = new Vector3(1.123f, 655f, 43.344f);
+
+            var jsonSerializerSettings = new JsonSerializerSettings { ContractResolver = new ContractResolver() };
+            var json = JsonConvert.SerializeObject(originalValue, jsonSerializerSettings);
+            var deserializedValue = JsonConvert.DeserializeObject<Vector3>(json, jsonSerializerSettings);
+
+            Assert.AreEqual(originalValue, deserializedValue);
+        }
+
+        [Test]
+        public void CanSerializeAndDeserializeVector4()
+        {
+            var originalValue = new Vector4(1.123f, 655f, 43.344f, 985885.34445f);
+
+            var jsonSerializerSettings = new JsonSerializerSettings { ContractResolver = new ContractResolver() };
+            var json = JsonConvert.SerializeObject(originalValue, jsonSerializerSettings);
+            var deserializedValue = JsonConvert.DeserializeObject<Vector4>(json, jsonSerializerSettings);
+
+            Assert.AreEqual(originalValue, deserializedValue);
+        }
+
+        [Test]
+        public void CanSerializeAndDeserializeQuaternion()
+        {
+            var originalValue = new Quaternion(1.123f, 655f, 43.344f, 985885.34445f);
+
+            var jsonSerializerSettings = new JsonSerializerSettings { ContractResolver = new ContractResolver() };
+            var json = JsonConvert.SerializeObject(originalValue, jsonSerializerSettings);
+            var deserializedValue = JsonConvert.DeserializeObject<Quaternion>(json, jsonSerializerSettings);
+
+            Assert.AreEqual(originalValue, deserializedValue);
+        }
+
+        [Test]
+        public void CanSerializeAndDeserializeColor()
+        {
+            var originalValue = new Color(1.123f, 655f, 43.344f, 985885.34445f);
+
+            var jsonSerializerSettings = new JsonSerializerSettings { ContractResolver = new ContractResolver() };
+            var json = JsonConvert.SerializeObject(originalValue, jsonSerializerSettings);
+            var deserializedValue = JsonConvert.DeserializeObject<Color>(json, jsonSerializerSettings);
+
+            Assert.AreEqual(originalValue, deserializedValue);
+        }
+
+        [Test]
+        public void CanSerializeAndDeserializeBounds()
+        {
+            var originalValue = new Bounds(new Vector3(1.123f, 655f, 43.344f), new Vector3(23.3f, 0.4553f, 985885.34445f));
+
+            var jsonSerializerSettings = new JsonSerializerSettings { ContractResolver = new ContractResolver() };
+            var json = JsonConvert.SerializeObject(originalValue, jsonSerializerSettings);
+            var deserializedValue = JsonConvert.DeserializeObject<Bounds>(json, jsonSerializerSettings);
+
+            Assert.AreEqual(originalValue, deserializedValue);
+        }
+
+        [Test]
+        public void CanSerializeAndDeserializeRect()
+        {
+            var originalValue = new Rect(1.123f, 655f, 43.344f, 985885.34445f);
+
+            var jsonSerializerSettings = new JsonSerializerSettings { ContractResolver = new ContractResolver() };
+            var json = JsonConvert.SerializeObject(originalValue, jsonSerializerSettings);
+            var deserializedValue = JsonConvert.DeserializeObject<Rect>(json, jsonSerializerSettings);
+
+            Assert.AreEqual(originalValue, deserializedValue);
         }
     }
 }
