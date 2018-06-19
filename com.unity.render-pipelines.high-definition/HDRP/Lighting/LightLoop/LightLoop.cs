e0fc090d78266187c3ac42caa17c35fa4e655024
Antoine Lelievre <antoinel@unity3d.com>
2018-06-19 16:16:02 +0200

    Reverted LightLoop to the original version

--- com.unity.render-pipelines.high-definition/HDRP/Lighting/LightLoop/LightLoop.cs
+++ com.unity.render-pipelines.high-definition/HDRP/Lighting/LightLoop/LightLoop.cs
@@ -123,9 +123,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
 
             m_ShadowMgr = new ShadowManager(shadowSettings, ref scInit, ref budgets, m_Shadowmaps);
             // set global overrides - these need to match the override specified in LightLoop/Shadow.hlsl
-
             bool useGlobalOverrides = true;
-
             m_ShadowMgr.SetGlobalShadowOverride(GPUShadowType.Point        , ShadowAlgorithm.PCF, ShadowVariant.V2, ShadowPrecision.High, useGlobalOverrides);
             m_ShadowMgr.SetGlobalShadowOverride(GPUShadowType.Spot         , ShadowAlgorithm.PCF, ShadowVariant.V2, ShadowPrecision.High, useGlobalOverrides);
             m_ShadowMgr.SetGlobalShadowOverride(GPUShadowType.Directional  , ShadowAlgorithm.PCF, ShadowVariant.V3, ShadowPrecision.High, useGlobalOverrides);
