e5390d412594f2b9e1934fc00c5f93205fa8a767
Kay Chang <kaychang@unity3d.com>
2018-06-20 16:12:11 -0700

    Switch compilation fixes.

--- com.unity.render-pipelines.lightweight/LWRP/LightweightPipeline.cs
+++ com.unity.render-pipelines.lightweight/LWRP/LightweightPipeline.cs
@@ -201,11 +201,13 @@ namespace UnityEngine.Experimental.Rendering.LightweightPipeline
 
             if (camera.cameraType == CameraType.Game)
             {
+#if !UNITY_SWITCH
                 if (cameraData.isStereoEnabled)
                 {
                     cameraData.renderScale = XRSettings.eyeTextureResolutionScale;
                 }
                 else
+#endif
                 {
                     cameraData.renderScale = pipelineAsset.renderScale;
                 }
