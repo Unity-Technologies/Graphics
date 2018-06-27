28a089042f15cf1e64c52c3887ef373e4f4c7c71
Antoine Lelievre <antoinel@unity3d.com>
2018-06-27 19:51:56 +0200

    Begin scripts to import IES files

---
+++ com.unity.render-pipelines.high-definition/HDRP/Editor/ScriptedImporters/IesAsset.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEditor.Experimental.Rendering.HDPipeline
+{
+    public enum PhotometricType
+    {
+        A = 1,
+        B = 2,
+        C = 3,
+    }
+
+    public enum UnitType
+    {
+        Feet    = 1,
+        Meter   = 2,
+    }
+
+    public class IesAsset : ScriptableObject
+    {
+        public string[]         keywords;
+        public int              clampCount;
+        public float            lumenPerLamp;
+        public float            candelaMultiplier;
+        public int              verticalAnglesCount;
+        public int              horizontalAnglesCount;
+        public PhotometricType  photometricType;
+        public UnitType         unitType;
+        public Vector3          luminousDimensions;
+
+        public float            ballastFactor;
+        public float            inputWatts;
+
+        public List<float>      verticalAngles;
+        public List<float>      horizontalAngles;
+        public List<float>      intensityValues; // In candella
+    }
+}
---
+++ com.unity.render-pipelines.high-definition/HDRP/Editor/ScriptedImporters/IesScriptedImporter.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Experimental.AssetImporters;
+
+namespace UnityEditor.Experimental.Rendering.HDPipeline
+{
+    [ScriptedImporter(1, "ies")]
+    public class IesScriptedImporter : ScriptedImporter
+    {
+        public override void OnImportAsset(AssetImportContext ctx)
+        {
+            string[] lines = File.ReadAllLines(ctx.assetPath);
+            
+        }
+    }
+}
