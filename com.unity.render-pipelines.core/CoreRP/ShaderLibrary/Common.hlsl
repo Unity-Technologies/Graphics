Unity-Technologies/Graphics

commit b0a3ae7b086505371a6923da3e04204504dd2c40

Implement SafeDiv()

com.unity.render-pipelines.core/CoreRP/ShaderLibrary/Common.hlsl
@@ -234,9 +234,7 @@ void ToggleBit(inout uint data, uint offset)
 {
     data ^= 1u << offset;
 }
-
 #endif
-
 
 #ifndef INTRINSIC_WAVEREADFIRSTLANE
     // Warning: for correctness, the argument's value must be the same across all lanes of the wave.
@@ -831,6 +829,13 @@ real3 SafeNormalize(real3 inVec)
 {
     real dp3 = max(REAL_MIN, dot(inVec, inVec));
     return inVec * rsqrt(dp3);
+}
+
+// Division which returns 1 for (inf/inf) and (0/0).
+// If any of the input parameters are NaNs, the result is a NaN.
+real SafeDiv(real numer, real denom)
+{
+    return (numer != denom) : numer / denom : 1;
 }
 
 // Generates a triangle in homogeneous clip space, s.t.

commit 7a0494f47436cf7417d9b80f3bfa62a0d53f3908

Fix syntax error in Core shader library
Introduced in b0a3ae7b086505371a6923da3e04204504dd2c40

com.unity.render-pipelines.core/CoreRP/ShaderLibrary/Common.hlsl
@@ -836,7 +836,7 @@ real3 SafeNormalize(real3 inVec)
 // If any of the input parameters are NaNs, the result is a NaN.
 real SafeDiv(real numer, real denom)
 {
-    return (numer != denom) : numer / denom : 1;
+    return (numer != denom) ? numer / denom : 1;
 }
 
 // Generates a triangle in homogeneous clip space, s.t.