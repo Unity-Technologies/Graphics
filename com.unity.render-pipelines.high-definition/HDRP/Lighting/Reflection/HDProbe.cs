e861451e16052ddc5f55d50eaefe7fa5e687a009
RSlysz <remi@unity3d.com>
2018-07-13 10:58:11 +0200

    merge multiplier field in HDProbe

--- com.unity.render-pipelines.high-definition/HDRP/Lighting/Reflection/HDProbe.cs
+++ com.unity.render-pipelines.high-definition/HDRP/Lighting/Reflection/HDProbe.cs
@@ -8,6 +8,10 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
         [SerializeField, FormerlySerializedAs("proxyVolumeComponent"), FormerlySerializedAs("m_ProxyVolumeReference")]
         ReflectionProxyVolumeComponent m_ProxyVolume = null;
 
+        [SerializeField, FormerlySerializedAsAttribute("dimmer"), FormerlySerializedAsAttribute("m_Dimmer"), FormerlySerializedAsAttribute("multiplier")]
+        float m_Multiplier = 1.0f;
+
         public ReflectionProxyVolumeComponent proxyVolume { get { return m_ProxyVolume; } }
+        public float multiplier { get { return m_Multiplier; } }
     }
 }
