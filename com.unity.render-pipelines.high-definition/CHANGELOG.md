3d64338d14fbdff83ce091a936d2f068021fe986
Julien Ignace <julien@unity3d.com>
2018-07-13 15:14:04 +0200

    PostProcess input buffers are not copied anymore on PC if the viewport size matches the final render target size

--- com.unity.render-pipelines.high-definition/CHANGELOG.md
+++ com.unity.render-pipelines.high-definition/CHANGELOG.md
@@ -9,6 +9,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ### Fixed
 - Fixed an issue with distortion that was using previous frame instead of current frame
 - Fixed an issue where disabled light where not upgrade correctly to the new physical light unit system introduce in 2.0.5-preview
+- PostProcess input buffers are not copied anymore on PC if the viewport size matches the final render target size
 
 ## [2.0.5-preview]
 
--- com.unity.render-pipelines.high-definition/HDRP/RenderPipeline/HDRenderPipeline.cs
+++ com.unity.render-pipelines.high-definition/HDRP/RenderPipeline/HDRenderPipeline.cs
@@ -1750,7 +1750,9 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
                 RenderTargetIdentifier source = m_CameraColorBuffer;
 
                 // For console we are not allowed to resize the windows, so don't use our hack.
-                bool tempHACK = !IsConsolePlatform();
+                // We also don't do the copy if viewport size and render texture size match.
+                bool viewportAndRTSameSize = (hdcamera.actualWidth == m_CameraColorBuffer.rt.width && hdcamera.actualHeight == m_CameraColorBuffer.rt.height);
+                bool tempHACK = !IsConsolePlatform() && !viewportAndRTSameSize;
 
                 if (tempHACK)
                 {
