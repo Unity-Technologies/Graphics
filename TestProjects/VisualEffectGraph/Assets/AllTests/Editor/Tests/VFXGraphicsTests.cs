a911cff9bcdebf46a73b761f826cd0836144d20b
Tristan Genevet <tristan@unity3d.com>
2018-07-16 18:41:37 +0200

    reset VFXGraphicTests to the one in staging.

--- TestProjects/VisualEffectGraph/Assets/AllTests/Editor/Tests/VFXGraphicsTests.cs
+++ TestProjects/VisualEffectGraph/Assets/AllTests/Editor/Tests/VFXGraphicsTests.cs
@@ -242,8 +242,7 @@ namespace UnityEditor.VFX.Test
                 int expectedFrameIndex = startFrameIndex + waitFrameCount;
                 while (Time.frameCount != expectedFrameIndex)
                 {
-                    //EditorWindow.GetWindow(typeof(GameView)).Focus();
-                    UpdateScene(scene, (VFXManager.frameIndex - startFrameIndex) * frequency);
+                    UpdateScene(scene, (Time.frameCount - startFrameIndex) * frequency);
                     Time.captureFramerate = 20;
                     scene.camera.Render();
                     yield return null;
