5729cb3e057d5858f7a41b29ca999c0119e422fc
Julien Ignace <julien@unity3d.com>
2018-07-18 17:32:46 +0200

    Added an explicit option in SkySettings (only supported for now in Procedural Sky) to enable or disable the rendering of the sun disk when baking light and reflection probes. This allows us to fix some bugs and discrepancy with the current framework.

--- com.unity.render-pipelines.high-definition/HDRP/Editor/Sky/SkySettingsEditor.cs
+++ com.unity.render-pipelines.high-definition/HDRP/Editor/Sky/SkySettingsEditor.cs
@@ -7,11 +7,22 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
 {
     public abstract class SkySettingsEditor : VolumeComponentEditor
     {
+        [System.Flags]
+        protected enum SkySettingsUIElement
+        {
+            Exposure = 1 << 0,
+            Multiplier = 1 << 1,
+            Rotation = 1 << 2,
+            UpdateMode = 1 << 3,
+            IncludeSunInBaking = 1 << 4
+        }
+
         SerializedDataParameter m_SkyExposure;
         SerializedDataParameter m_SkyMultiplier;
         SerializedDataParameter m_SkyRotation;
         SerializedDataParameter m_EnvUpdateMode;
         SerializedDataParameter m_EnvUpdatePeriod;
+        SerializedDataParameter m_IncludeSunInBaking;
 
         public override void OnEnable()
         {
@@ -22,22 +33,31 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
             m_SkyRotation = Unpack(o.Find(x => x.rotation));
             m_EnvUpdateMode = Unpack(o.Find(x => x.updateMode));
             m_EnvUpdatePeriod = Unpack(o.Find(x => x.updatePeriod));
+            m_IncludeSunInBaking = Unpack(o.Find(x => x.includeSunInBaking));
+
         }
 
-        protected void CommonSkySettingsGUI(bool enableRotation = true)
+        protected void CommonSkySettingsGUI(uint uiElementMask = 0xFFFFFFFF)
         {
-            PropertyField(m_SkyExposure);
-            PropertyField(m_SkyMultiplier);
-            if (enableRotation)
+            if ((uiElementMask & (uint)SkySettingsUIElement.Exposure) != 0)
+                PropertyField(m_SkyExposure);
+            if ((uiElementMask & (uint)SkySettingsUIElement.Multiplier) != 0)
+                PropertyField(m_SkyMultiplier);
+            if ((uiElementMask & (uint)SkySettingsUIElement.Rotation) != 0)
                 PropertyField(m_SkyRotation);
 
-            PropertyField(m_EnvUpdateMode);
-            if (!m_EnvUpdateMode.value.hasMultipleDifferentValues && m_EnvUpdateMode.value.intValue == (int)EnvironementUpdateMode.Realtime)
+            if ((uiElementMask & (uint)SkySettingsUIElement.UpdateMode) != 0)
             {
-                EditorGUI.indentLevel++;
-                PropertyField(m_EnvUpdatePeriod);
-                EditorGUI.indentLevel--;
+                PropertyField(m_EnvUpdateMode);
+                if (!m_EnvUpdateMode.value.hasMultipleDifferentValues && m_EnvUpdateMode.value.intValue == (int)EnvironementUpdateMode.Realtime)
+                {
+                    EditorGUI.indentLevel++;
+                    PropertyField(m_EnvUpdatePeriod);
+                    EditorGUI.indentLevel--;
+                }
             }
+            if ((uiElementMask & (uint)SkySettingsUIElement.IncludeSunInBaking) != 0)
+                PropertyField(m_IncludeSunInBaking);
         }
     }
 }
