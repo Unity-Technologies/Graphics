Joytis/ScriptableRenderPipeline

merge proxyVolume field in HDProbe

com.unity.render-pipelines.high-definition/HDRP/Editor/Lighting/Reflection/SerializedHDReflectionProbe.cs
@@ -133,7 +133,7 @@ namespace UnityEditor.Experimental.Rendering
                 Apply();
             }
 
-            proxyVolumeComponent = addso.Find((HDReflectionProbe d) => d.proxyVolumeComponent);
+            proxyVolumeComponent = addso.Find((HDReflectionProbe d) => d.proxyVolume);
         }
 
         public void Update()

Unity-Technologies/Graphics

com.unity.render-pipelines.high-definition/HDRP/Editor/Lighting/Reflection/SerializedHDReflectionProbe.cs
@@ -133,7 +133,7 @@ namespace UnityEditor.Experimental.Rendering.HDPipeline
                 Apply();
             }
 
-            proxyVolumeComponent = addso.Find((HDAdditionalReflectionData d) => d.proxyVolumeComponent);
+            proxyVolumeComponent = addso.Find((HDAdditionalReflectionData d) => d.proxyVolume);
         }
 
         public void Update()