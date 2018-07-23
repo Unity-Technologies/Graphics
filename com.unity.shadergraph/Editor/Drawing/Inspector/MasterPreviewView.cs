895e4975cc475060e999ab29b6640b01daaedcbb
Tristan Genevet <tristan@unity3d.com>
2018-07-23 18:48:12 +0200

    Reverted incorrect change in Shadergraph

--- com.unity.shadergraph/Editor/Drawing/Inspector/MasterPreviewView.cs
+++ com.unity.shadergraph/Editor/Drawing/Inspector/MasterPreviewView.cs
@@ -64,7 +64,7 @@ namespace UnityEditor.ShaderGraph.Drawing.Inspector
 
         List<string> m_DoNotShowPrimitives = new List<string>(new string[] {PrimitiveType.Plane.ToString()});
 
-        static Type s_DropdownMenuManipulator = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypesOrNothing()).FirstOrDefault(t => t.FullName == "UnityEngine.Experimental.UIElements.ContextualMenuManipulator");
+        static Type s_ContextualMenuManipulator = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypesOrNothing()).FirstOrDefault(t => t.FullName == "UnityEngine.Experimental.UIElements.ContextualMenuManipulator");
         static Type s_ObjectSelector = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypesOrNothing()).FirstOrDefault(t => t.FullName == "UnityEditor.ObjectSelector");
 
         public string assetName
@@ -170,7 +170,7 @@ namespace UnityEditor.ShaderGraph.Drawing.Inspector
         {
             var image = new Image { name = "preview", image = StyleValue<Texture>.Create(m_PreviewRenderHandle.texture ?? texture) };
             image.AddManipulator(new Draggable(OnMouseDragPreviewMesh, true));
-            image.AddManipulator((IManipulator)Activator.CreateInstance(s_DropdownMenuManipulator, (Action<ContextualMenuPopulateEvent>)BuildContextualMenu));
+            image.AddManipulator((IManipulator)Activator.CreateInstance(s_ContextualMenuManipulator, (Action<ContextualMenuPopulateEvent>)BuildContextualMenu));
             return image;
         }
 
