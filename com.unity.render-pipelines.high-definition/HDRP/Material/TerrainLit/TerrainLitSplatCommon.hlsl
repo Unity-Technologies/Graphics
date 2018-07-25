836570aba84fc6ff1ae2eb3c262dc3cb751cd28d
Yao Xiaoling <zero.xlyao@gmail.com>
2018-07-25 10:53:54 -0700

    Make sure UnityPerMaterial CB stays the same for all variants.

--- com.unity.render-pipelines.high-definition/HDRP/Material/TerrainLit/TerrainLitSplatCommon.hlsl
+++ com.unity.render-pipelines.high-definition/HDRP/Material/TerrainLit/TerrainLitSplatCommon.hlsl
@@ -59,11 +59,8 @@ CBUFFER_START(UnityPerMaterial)
     #endif
 
     float _HeightTransition;
-
-    #ifdef UNITY_INSTANCING_ENABLED
-        float4 _TerrainHeightmapRecipSize;   // float4(1.0f/width, 1.0f/height, 1.0f/(width-1), 1.0f/(height-1))
-        float4 _TerrainHeightmapScale;       // float4(hmScale.x, hmScale.y / (float)(kMaxHeight), hmScale.z, 0.0f)
-    #endif
+    float4 _TerrainHeightmapRecipSize;   // float4(1.0f/width, 1.0f/height, 1.0f/(width-1), 1.0f/(height-1))
+    float4 _TerrainHeightmapScale;       // float4(hmScale.x, hmScale.y / (float)(kMaxHeight), hmScale.z, 0.0f)
 
 CBUFFER_END
 
