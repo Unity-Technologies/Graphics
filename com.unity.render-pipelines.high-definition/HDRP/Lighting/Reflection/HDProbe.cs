96460a646366b171893663605ac3a1a5439bdcf9
sebastienlagarde <sebastien@unity3d.com>
2018-08-08 21:08:08 +0200

    Revert "Hdrp/refactor reflection probe/merge probe editors"

--- com.unity.render-pipelines.high-definition/HDRP/Lighting/Reflection/HDProbe.cs
+++ com.unity.render-pipelines.high-definition/HDRP/Lighting/Reflection/HDProbe.cs
@@ -8,14 +8,9 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
     {
         [SerializeField, FormerlySerializedAs("proxyVolumeComponent"), FormerlySerializedAs("m_ProxyVolumeReference")]
         ReflectionProxyVolumeComponent m_ProxyVolume = null;
-        [SerializeField]
-        bool m_InfiniteProjection = true; //usable when no proxy set
 
         [SerializeField]
         InfluenceVolume m_InfluenceVolume;
-
-        [SerializeField]
-        FrameSettings m_FrameSettings = null;
 
         [SerializeField, FormerlySerializedAsAttribute("dimmer"), FormerlySerializedAsAttribute("m_Dimmer"), FormerlySerializedAsAttribute("multiplier")]
         float m_Multiplier = 1.0f;
@@ -27,8 +22,6 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
         ReflectionProbeMode m_Mode = ReflectionProbeMode.Baked;
         [SerializeField]
         ReflectionProbeRefreshMode m_RefreshMode = ReflectionProbeRefreshMode.OnAwake;
-        
-        RenderTexture m_RealtimeTexture = null;
 
         /// <summary>ProxyVolume currently used by this probe.</summary>
         public ReflectionProxyVolumeComponent proxyVolume { get { return m_ProxyVolume; } }
@@ -36,17 +29,11 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
         /// <summary>InfluenceVolume of the probe.</summary>
         public InfluenceVolume influenceVolume { get { return m_InfluenceVolume; } private set { m_InfluenceVolume = value; } }
 
-        /// <summary>Frame settings in use with this probe.</summary>
-        public FrameSettings frameSettings { get { return m_FrameSettings; } }
-
         /// <summary>Multiplier factor of reflection (non PBR parameter).</summary>
         public float multiplier { get { return m_Multiplier; } set { m_Multiplier = value; } }
 
         /// <summary>Weight for blending amongst probes (non PBR parameter).</summary>
         public float weight { get { return m_Weight; } set { m_Weight = value; } }
-
-        /// <summary>Get the realtime acquired Render Texture</summary>
-        public RenderTexture realtimeTexture { get { return m_RealtimeTexture; } internal set { m_RealtimeTexture = value; } }
 
         /// <summary>The capture mode.</summary>
         public virtual ReflectionProbeMode mode
@@ -60,18 +47,6 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
         {
             get { return m_RefreshMode; }
             set { m_RefreshMode = value; }
-        }
-
-        public virtual bool infiniteProjection
-        {
-            get
-            {
-                return proxyVolume != null && proxyVolume.proxyVolume.shape == ProxyShape.Infinite || m_InfiniteProjection;
-            }
-            set
-            {
-                m_InfiniteProjection = value;
-            }
         }
 
         internal void Awake()
