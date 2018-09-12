e4ba03b39c886c0b64039f50651eda788475c87b
anisunity <42026998+anisunity@users.noreply.github.com>
2018-09-12 10:23:50 +0200

    Fixing a bug in the specular term of the silk variant of the fabric shader (#1884)
    Purpose of this PR
    Fixing a bug in the specular term of the Fabric Shader (reported by QA)
    
    * Fixing a bug in the specular term of the silk variant of the fabric shader
    
    * Making the SSS and transmisison work in the fabric shader
    
    * Correcting the Detail AO and Fuzz Detail now affects the base color
    
    * Renaming the Fuzz Tint to Specular Tint and fixing the Fuzz detail map and AO term
    
    * Detail to Thread rename and Fuzz mul to add
    
    * Changing the mechanic of the specular Color
    Updating the test scene
    Updating the test image

--- com.unity.render-pipelines.high-definition/HDRP/Material/Fabric/Fabric.cs
+++ com.unity.render-pipelines.high-definition/HDRP/Material/Fabric/Fabric.cs
@@ -14,9 +14,8 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
         public enum MaterialFeatureFlags
         {
             FabricCottonWool = 1 << 0,
-            FabricSilk = 1 << 1,
-            FabricSubsurfaceScattering = 1 << 2,
-            FabricTransmission = 1 << 3
+            FabricSubsurfaceScattering = 1 << 1,
+            FabricTransmission = 1 << 2
         };
 
         //-----------------------------------------------------------------------------
@@ -48,9 +47,9 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
             [SurfaceDataAttributes("Ambient Occlusion")]
             public float ambientOcclusion;
 
-            // Fuzz Tint
-            [SurfaceDataAttributes("Fuzz Tint", false, true)]
-            public Vector3 fuzzTint;
+            // Specular Tint
+            [SurfaceDataAttributes("Specular Tint", false, true)]
+            public Vector3 specularColor;
 
             // MaterialFeature dependent attribute
 
@@ -88,7 +87,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
             public float ambientOcclusion;
             public float specularOcclusion;
 
-            public Vector3 fuzzTint;
+            public Vector3 specularColor;
 
             [SurfaceDataAttributes(new string[] { "Normal WS", "Normal View Space" }, true)]
             public Vector3 normalWS;
