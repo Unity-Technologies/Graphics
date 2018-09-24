75fa17363d644eba52dec863b0360ed8540c624d
RemyUnity <32760367+RemyUnity@users.noreply.github.com>
2018-09-24 19:04:45 +0200

    Tests/hdrp/staging (#1908)
    * Add spot angle test
    
    * Change spot angle distance test to use sphere sections as "receptors"
    
    * Add Spot Angle and Distance test
    
    * Change Post Process package to use released one
    Update Project settings.
    
    * Add missing reference image
    
    * Fix test 2201
    
    * Add SSS Mask test
    
    * Fix reflection probes values
    
    * Update SSS test scene.
    
    * Add oblique matrix light culling test
    
    * Add auto naming script
    
    * Update project settigns
    
    * Add new distortion test scene
    
    * Change test camera default resolution to 720p
    
    * Finish new distor test scene
    
    * Add vulkan as window api
    
    * Change Font to have point sampling
    
    * Update default material
    
    * Update material
    
    * Add code to automatically resize the game view resolution to mimic test settings
    
    * Optimize test scenes
    
    * Add script to auto resize test for pixel perfect font
    
    * Delete TR scene
    
    * Update and optimize test scenes.
    
    * Update/Optimize test scenes
    
    * Change color to HDR color in material parameter variation
    
    * Move code to auto set game window size to editor
    
    * Add mid-grey with srgb texture
    
    * Update/Optimize test scenes.
    
    * Update / Optimize Tests
    
    * Optimize Tests scenes
    
    * Add missing packages in manifest
    
    * Update/optimize tests
    
    * Update / optimize tests
    
    * Merge with office desktop
    
    * Optimize density volumes scene
    
    * Prepare for PR
    
    * remove systematicly failing tests
    
    * Fix failing tests
    
    * re-add sss tests after evegeni fix
    
    * Remove Light baked data
    
    * Fix tests for katana
    
    * Showrendering settings MSAA control in UI and change "DBuffer" label to "decals"
    
    * fix duplicate scope creation (#1977)
    
    * Fixed contact shadows (use the new functions to sample the camera depth) (#2002)
    
    * Fixed contact shadows (use the new functions to sample the camera depth)
    
    * Fixed contact shadow uv scale
    
    * Simpler SampleCameraDepth uv function
    
    * Fix contact shadow load out of bouds in lightloop (#2008)
    
    * Add needed missing packages
    
    * fix value used for cube probe cache (#2020)
    
    * Fix test errors
    
    * Update Reference Images
    
    * Update reflection probes in bent normal test
    
    * Update and correct tests from last merge.
    
    * Fix SSS_Max radius scene.
    Update ref images
    
    * Remove contact shadows test until their rework is finished
    
    * Update reflection probes in test 2103
    
    * Higher wait frames in scene 2103 to allow reflection probes to be applied.
    
    * Remove unstable tests scenes
    
    * Remove unecessary HDRP asset.
    Add text prefab.

---
+++ TestProjects/HDRP_Tests/Assets/GraphicTests/Common/Scripts/Editor/ReplaceObjects.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ReplaceObjects : EditorWindow
+{
+    public GameObject newObject;
+
+    [MenuItem("Internal/GraphicTest Tools/Replace Objects")]
+    public static void OpenReplaceObjects()
+    {
+        GetWindow<ReplaceObjects>();
+    }
+
+    void OnGUI()
+    {
+        newObject = EditorGUILayout.ObjectField("New Object", newObject, typeof(GameObject), false) as GameObject;
+
+        if(GUILayout.Button("Replace selection with object")) ReplaceSelectionWithPrefab();
+    }
+
+    void ReplaceSelectionWithPrefab()
+    {
+        List<GameObject> selected = new List<GameObject>( Selection.gameObjects );
+
+        if (selected.Count == 0 || newObject == null) return;
+
+        for (int i=0 ; i<selected.Count ; ++i)
+        {
+            GameObject go = selected[i];
+            GameObject newGo = PrefabUtility.InstantiatePrefab(newObject) as GameObject;
+            newGo.transform.parent = go.transform.parent;
+            newGo.transform.localPosition = go.transform.localPosition;
+            newGo.transform.localRotation = go.transform.localRotation;
+            newGo.transform.localScale = go.transform.localScale;
+            newGo.name = go.name;
+
+            DestroyImmediate(go);
+            selected[i] = newGo;
+        }
+
+        Selection.objects = selected.ToArray() as Object[];
+    }
+}
