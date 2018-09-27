0c7064ee4d65da05e561b980421a59df30b8c06a
Felipe Lira <felipedrl@gmail.com>
2018-09-27 15:20:16 +0200

    Move space convertion functions (#2067)
    *  Move Space Convertion functions to core.
    
    *  updated shadergraph to use the space convertion funcitons in core.
    
    *   Renamed file to SpaceTransforms according to PR review.
    
    *  added release notes
    
    *  Added Core as dependency to Shader Graph (requested by @pbbastian)
    
    *  updated ifdef to match filename

--- com.unity.shadergraph/ShaderGraphLibrary/ShaderVariablesFunctions.hlsl
+++ com.unity.shadergraph/ShaderGraphLibrary/ShaderVariablesFunctions.hlsl
@@ -1,76 +1,7 @@
 #ifndef UNITY_SHADER_VARIABLES_FUNCTIONS_INCLUDED
 #define UNITY_SHADER_VARIABLES_FUNCTIONS_INCLUDED
 
-float4x4 GetWorldToViewMatrix()
-{
-    return UNITY_MATRIX_V;
-}
-
-float4x4 GetObjectToWorldMatrix()
-{
-    return UNITY_MATRIX_M;
-}
-
-float4x4 GetWorldToObjectMatrix()
-{
-    return UNITY_MATRIX_I_M;
-}
-
-// Transform to homogenous clip space
-float4x4 GetWorldToHClipMatrix()
-{
-    return UNITY_MATRIX_VP;
-}
-
-float GetOddNegativeScale()
-{
-    return unity_WorldTransformParams.w;
-}
-
-float3 TransformWorldToView(float3 positionWS)
-{
-    return mul(GetWorldToViewMatrix(), float4(positionWS, 1.0)).xyz;
-}
-
-float3 TransformObjectToWorld(float3 positionOS)
-{
-    return mul(GetObjectToWorldMatrix(), float4(positionOS, 1.0)).xyz;
-}
-
-float3 TransformWorldToObject(float3 positionWS)
-{
-    return mul(GetWorldToObjectMatrix(), float4(positionWS, 1.0)).xyz;
-}
-
-float3 TransformObjectToWorldDir(float3 dirOS)
-{
-    // Normalize to support uniform scaling
-    return normalize(mul((float3x3)GetObjectToWorldMatrix(), dirOS));
-}
-
-float3 TransformWorldToObjectDir(float3 dirWS)
-{
-    // Normalize to support uniform scaling
-    return normalize(mul((float3x3)GetWorldToObjectMatrix(), dirWS));
-}
-
-// Transforms normal from object to world space
-float3 TransformObjectToWorldNormal(float3 normalOS)
-{
-#ifdef UNITY_ASSUME_UNIFORM_SCALING
-    return UnityObjectToWorldDir(normalOS);
-#else
-    // Normal need to be multiply by inverse transpose
-    // mul(IT_M, norm) => mul(norm, I_M) => {dot(norm, I_M.col0), dot(norm, I_M.col1), dot(norm, I_M.col2)}
-    return normalize(mul(normalOS, (float3x3)GetWorldToObjectMatrix()));
-#endif
-}
-
-// Tranforms position from world space to homogenous space
-float4 TransformWorldToHClip(float3 positionWS)
-{
-    return mul(GetWorldToHClipMatrix(), float4(positionWS, 1.0));
-}
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/SpaceTransforms.hlsl"
 
 float3 GetAbsolutePositionWS(float3 positionWS)
 {
@@ -143,38 +74,6 @@ float3 GetWorldSpaceNormalizeViewDir(float3 positionWS)
         // Orthographic
         return -GetViewForwardDir();
     }
-}
-
-float3x3 CreateWorldToTangent(float3 normal, float3 tangent, float flipSign)
-{
-    // For odd-negative scale transforms we need to flip the sign
-    float sgn = flipSign * GetOddNegativeScale();
-    float3 bitangent = cross(normal, tangent) * sgn;
-
-    return float3x3(tangent, bitangent, normal);
-}
-
-float3 TransformTangentToWorld(float3 dirTS, float3x3 worldToTangent)
-{
-    // Use transpose transformation to go from tangent to world as the matrix is orthogonal
-    return mul(dirTS, worldToTangent);
-}
-
-float3 TransformWorldToTangent(float3 dirWS, float3x3 worldToTangent)
-{
-    return mul(worldToTangent, dirWS);
-}
-
-float3 TransformTangentToObject(float3 dirTS, float3x3 worldToTangent)
-{
-    // Use transpose transformation to go from tangent to world as the matrix is orthogonal
-    float3 normalWS = mul(dirTS, worldToTangent);
-    return mul((float3x3)GetWorldToObjectMatrix(), normalWS);
-}
-
-float3 TransformObjectToTangent(float3 dirOS, float3x3 worldToTangent)
-{
-    return mul(worldToTangent, TransformObjectToWorldDir(dirOS));
 }
 
 // UNITY_MATRIX_V defines a right-handed view space with the Z axis pointing towards the viewer.
