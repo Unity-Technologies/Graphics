fa93c53648bd7c792ab46a93971fd044be03e90a
Zachary Diller <zackd44@gmail.com>
2018-09-28 03:07:30 -0700

    Sg/hdrp automated tests (#2075)
    * shader graph hdrp automated test first push
    
    * more readme info

---
+++ TestProjects/HDRP_Tests/Assets/GraphicTests/Common/TestRunner/HDRP_ShaderGraph_TestSettings.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+using UnityEngine.TestTools.Graphics;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Events;
+
+public class HDRP_ShaderGraph_TestSettings : HDRP_TestSettings
+{
+    public bool compareSGtoBI = true;
+    public GameObject sgObjs;
+    public GameObject biObjs;
+}
