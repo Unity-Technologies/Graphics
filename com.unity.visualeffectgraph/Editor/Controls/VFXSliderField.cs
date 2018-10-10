56929cbea18b7d2127ebdf6fb3b7ba36a32f72d2
Tristan Genevet <tristan@unity3d.com>
2018-10-10 15:11:49 +0200

    fix Slider Field sometime beeing overriden even if it has the focus.

--- com.unity.visualeffectgraph/Editor/Controls/VFXSliderField.cs
+++ com.unity.visualeffectgraph/Editor/Controls/VFXSliderField.cs
@@ -108,7 +108,8 @@ namespace UnityEditor.VFX.UIElements
         {
             m_IgnoreNotification = true;
             m_Value = newValue;
-            m_Field.value = newValue;
+            if( !(m_Field as VisualElement).HasFocus())
+                m_Field.value = newValue;
             m_Slider.value = ValueToFloat(value);
             m_IgnoreNotification = false;
         }
