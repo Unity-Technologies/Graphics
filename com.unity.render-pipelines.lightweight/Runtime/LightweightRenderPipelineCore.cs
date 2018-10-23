5bfb57092e4b05e0e760acbcf456fc3aba63321e
Felipe Lira <felipedrl@gmail.com>
2018-10-23 08:35:58 +0200

    Lw/gfx test defaults (#2228)
    * Tweaked scene threshold values and set the default in the script to be 0.005(avg) and 0.001(per pix) as per discussions with Thomas/Remy
    
    * Updated test images
    
    *  fixed normalmap sampling in unlit shader
    
    *  Fixed shadowmap issues when no shadow casters were present in the scene. Fixed shadows for multiple lights.
    
    *  Fixed issue that was breaking shadows if more than 4 shadow cast lights were visible.
    
    *  updated reference images
    
    *   Fixed shadow cascade keyword. Removed keyword setup from resolve pass as it was redundant.
    
    *  Fixed test 027
    
    *  fix shadows for additional lights
    
    *  disabled test 053
    
    *  Added release notes
    
    *  updated image reference for scene 040
    
    *  updated images and scene
    
    * Update CHANGELOG.md
    
    *  fixed test 040

--- com.unity.render-pipelines.lightweight/Runtime/LightweightRenderPipelineCore.cs
+++ com.unity.render-pipelines.lightweight/Runtime/LightweightRenderPipelineCore.cs
@@ -22,11 +22,11 @@ namespace UnityEngine.Experimental.Rendering.LightweightPipeline
 
     public struct LightData
     {
-        public int additionalLightsCount;
-        public bool shadeAdditionalLightsPerVertex;
         public int mainLightIndex;
+        public int additionalLightsCount;
+        public int maxPerObjectAdditionalLightsCount;
         public List<VisibleLight> visibleLights;
-        public List<int> additionalLightIndices;
+        public bool shadeAdditionalLightsPerVertex;
         public bool supportsMixedLighting;
     }
 
