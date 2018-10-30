056d37e7f97bd7df35d433d1c449a41be210f505
Remi Slysz <40034005+RSlysz@users.noreply.github.com>
2018-10-30 11:52:52 +0100

    Hdrp/rename line to tube light (#2302)
    * rename line light to tube light
    
    * update changelog
    
    * revert line to tube on luminence computation function
    
    * fix changelog

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/Light/HDAdditionalLightData.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/Light/HDAdditionalLightData.cs
@@ -13,7 +13,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
     {
         Punctual, // Fallback on LightShape type
         Rectangle,
-        Line,
+        Tube,
         // Sphere,
         // Disc,
     };
@@ -586,7 +586,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
             MeshRenderer emissiveMeshRenderer = GetComponent<MeshRenderer>();
             MeshFilter emissiveMeshFilter = GetComponent<MeshFilter>();
 
-            bool displayEmissiveMesh = IsAreaLight(lightTypeExtent) && lightTypeExtent != LightTypeExtent.Line && displayAreaLightEmissiveMesh;
+            bool displayEmissiveMesh = IsAreaLight(lightTypeExtent) && lightTypeExtent != LightTypeExtent.Tube && displayAreaLightEmissiveMesh;
 
             // Ensure that the emissive mesh components are here
             if (displayEmissiveMesh)
@@ -772,7 +772,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
                                 break;
                         }
                         break;
-                    case LightTypeExtent.Line:
+                    case LightTypeExtent.Tube:
                     case LightTypeExtent.Rectangle:
                         lightUnit = LightUnit.Lumen;
                         intensity = areaIntensity;
