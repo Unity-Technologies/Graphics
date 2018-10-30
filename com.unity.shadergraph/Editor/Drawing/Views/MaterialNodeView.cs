0c65760eb09ae373c0eb21a42c79163540e8e2e1
martint-unity <martint@unity3d.com>
2018-10-30 11:07:07 +0100

    Removed preview
    Removed preview from the node.

--- com.unity.shadergraph/Editor/Drawing/Views/MaterialNodeView.cs
+++ com.unity.shadergraph/Editor/Drawing/Views/MaterialNodeView.cs
@@ -67,7 +67,7 @@ namespace UnityEditor.ShaderGraph.Drawing
             if (m_ControlItems.childCount > 0)
                 contents.Add(controlsContainer);
 
-            if (node.hasPreview && !(node is IMasterNode))
+            if (node.hasPreview)
             {
                 // Add actual preview which floats on top of the node
                 m_PreviewContainer = new VisualElement
