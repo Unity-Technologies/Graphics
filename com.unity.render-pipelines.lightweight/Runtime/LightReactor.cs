6428482dac167ac72eaa73227e2cd7599d751349
chris.chu <chris.chu@unity3d.com>
2018-10-31 17:47:34 +0800

    In progress setup work for 2d. Added dependencies.

---
+++ com.unity.render-pipelines.lightweight/Runtime/LightReactor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightReactor : MonoBehaviour
+{
+    [SerializeField]
+    private bool m_RecievesShadows;
+    [SerializeField]
+    private bool m_CastsShadows;
+
+    [SerializeField]
+    private int[]     m_ShadowMeshTriangles;
+    [SerializeField]
+    private Vector3[] m_ShadowMeshVertices;
+    [SerializeField]
+    private Vector3[] m_ShadowMeshNormals;
+
+    private Mesh m_ShadowMesh;
+
+    //public void CreateShadowMesh()
+    //{
+                
+    //}
+
+    //public void Awake()
+    //{
+        
+    //}
+}
