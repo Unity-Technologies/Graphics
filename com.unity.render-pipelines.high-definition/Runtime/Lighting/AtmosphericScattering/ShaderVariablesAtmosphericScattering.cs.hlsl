e548ef52748dc17dded60e9c7fc85bf3438a3d0f
Mikko Strandborg <strandborg@users.noreply.github.com>
2018-11-07 14:47:20 +0200

    C# shader variables (#2338)
    * Add omitStructDeclaration parameter to [GenerateHLSL] to allow generating struct fragments
    
    * C#-ize ShaderVariablesLightLoop
    
    * LightLoop: Actually use the generated .cs.hlsl file for variables
    
    * Update shader code to use GPULIGHTTYPE_TUBE instead of LINE; .cs and .hlsl code had gotten out of sync
    
    * Remove leftovers
    
    * Hoist ScreenSpaceLighting to CS
    
    * Csharp-ize ShaderVariablesAtmosphericScattering
    
    * Csharp-ize ShaderVariablesSubsurfaceScattering
    
    * Decals to C#
    
    * regenerate include
    
    * update include generation
    
    * miss a public keyword

---
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/AtmosphericScattering/ShaderVariablesAtmosphericScattering.cs.hlsl
@@ -0,0 +1,18 @@
+//
+// This file was automatically generated. Please don't edit by hand.
+//
+
+#ifndef SHADERVARIABLESATMOSPHERICSCATTERING_CS_HLSL
+#define SHADERVARIABLESATMOSPHERICSCATTERING_CS_HLSL
+// Generated from UnityEngine.Experimental.Rendering.HDPipeline.ShaderVariablesAtmosphericScattering
+// PackingRules = Exact
+    float4 _FogColorDensity;
+    float4 _MipFogParameters;
+    float4 _LinearFogParameters;
+    float4 _ExpFogParameters;
+    int _AtmosphericScatteringType;
+    float _FogColorMode;
+    float _SkyTextureMipCount;
+
+
+#endif
