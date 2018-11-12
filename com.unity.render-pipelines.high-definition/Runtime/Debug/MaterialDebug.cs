3720e2f095a9493d88f50ceb85454b79f705590c
Remi Slysz <40034005+RSlysz@users.noreply.github.com>
2018-11-12 17:16:47 +0100

    Hdrp/better fix EnumField in DebugMenu (#2388)
    * Revert previous material enum fix
    
    * Add multiple same value support on EnumField for debug menu
    
    * Remove old warning as it is now supported

--- com.unity.render-pipelines.high-definition/Runtime/Debug/MaterialDebug.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Debug/MaterialDebug.cs
@@ -76,7 +76,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
         }
 
         // className include the additional "/"
-        int FillWithProperties(Type type, ref List<GUIContent> debugViewMaterialStringsList, ref List<int> debugViewMaterialValuesList, string className, Type overrideTypeForIndex = null, int baseIndex = 0)
+        void FillWithProperties(Type type, ref List<GUIContent> debugViewMaterialStringsList, ref List<int> debugViewMaterialValuesList, string className)
         {
             var attributes = type.GetCustomAttributes(true);
             // Get attribute to get the start number of the value for the enum
@@ -84,17 +84,12 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
 
             if (!attr.needParamDebug)
             {
-                return baseIndex;
+                return;
             }
 
             var fields = type.GetFields();
 
-            var localIndex = baseIndex;
-            if(overrideTypeForIndex != null)
-            {
-                var attr2 = overrideTypeForIndex.GetCustomAttributes(true)[0] as GenerateHLSL;
-                localIndex += attr2.paramDefinesStart - attr.paramDefinesStart;
-            }
+            var localIndex = 0;
             foreach (var field in fields)
             {
                 // Note: One field can have multiple name. This is to allow to have different debug view mode for the same field
@@ -121,12 +116,6 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
                     localIndex++;
                 }
             }
-            if (overrideTypeForIndex != null)
-            {
-                var attr2 = overrideTypeForIndex.GetCustomAttributes(true)[0] as GenerateHLSL;
-                localIndex -= attr2.paramDefinesStart - attr.paramDefinesStart;
-            }
-            return localIndex;
         }
 
         void FillWithPropertiesEnum(Type type, ref List<GUIContent> debugViewMaterialStringsList, ref List<int> debugViewMaterialValuesList, string prefix)
@@ -214,8 +203,8 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
                     // BuiltinData are duplicated for each material
                     // Giving the material specific types allow to move iterator at a separate range for each material
                     // Otherwise, all BuiltinData will be at same offset and will broke the enum
-                    int nbElement = FillWithProperties(typeof(Builtin.BuiltinData), ref debugViewMaterialStringsList, ref debugViewMaterialValuesList, item.className, overrideTypeForIndex: item.surfaceDataType);
-                    FillWithProperties(item.surfaceDataType, ref debugViewMaterialStringsList, ref debugViewMaterialValuesList, item.className, baseIndex: nbElement);
+                    FillWithProperties(typeof(Builtin.BuiltinData), ref debugViewMaterialStringsList, ref debugViewMaterialValuesList, item.className);
+                    FillWithProperties(item.surfaceDataType, ref debugViewMaterialStringsList, ref debugViewMaterialValuesList, item.className);
                 }
 
                 // Engine properties debug
@@ -261,31 +250,6 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
                 debugViewMaterialGBufferValues = debugViewMaterialGBufferValuesList.ToArray();
 
                 isDebugViewMaterialInit = true;
-
-                //raise warning if indexes collide
-                unsafe
-                {
-                    if(CoreUnsafeUtils.HaveDuplicates(debugViewMaterialValues))
-                    {
-                        Debug.LogError("DebugMenu: debugViewMaterialValues have duplicate values. This will broke the runtime UI.");
-                    }
-                    if (CoreUnsafeUtils.HaveDuplicates(debugViewEngineValues))
-                    {
-                        Debug.LogError("DebugMenu: debugViewEngineValues have duplicate values. This will broke the runtime UI.");
-                    }
-                    if (CoreUnsafeUtils.HaveDuplicates(debugViewMaterialVaryingValues))
-                    {
-                        Debug.LogError("DebugMenu: debugViewMaterialVaryingValues have duplicate values. This will broke the runtime UI.");
-                    }
-                    if (CoreUnsafeUtils.HaveDuplicates(debugViewMaterialPropertiesValues))
-                    {
-                        Debug.LogError("DebugMenu: debugViewMaterialPropertiesValues have duplicate values. This will broke the runtime UI.");
-                    }
-                    if (CoreUnsafeUtils.HaveDuplicates(debugViewMaterialGBufferValues))
-                    {
-                        Debug.LogError("DebugMenu: debugViewMaterialGBufferValues have duplicate values. This will broke the runtime UI.");
-                    }
-                }
             }
         }
 
