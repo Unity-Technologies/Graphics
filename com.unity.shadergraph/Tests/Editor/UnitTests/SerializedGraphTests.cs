b9476932497ea67aa06f1f2bac28a0bd49eb9399
Martin Thorzen <35328557+martint-unity@users.noreply.github.com>
2018-11-21 12:40:19 +0100

    Group node (#2376)
    ### Purpose of this PR
    Adding the long lost son of shader graph; Group Node.
    With this you can now finally group your nodes and keep your Graphs organized and nice.
    
    ---
    ### Release Notes
    
    ---
    ### Testing status
    Running Katana
    https://katana.bf.unity3d.com/projects/com.unity.render-pipelines/builders/proj57-All%20Tests/builds/1365?automation-tools_branch=add-platform-filter&ScriptableRenderLoop_branch=sg/group-node&unity_branch=trunk
    
    ---
    ### Overall Product Risks
    Medium
    
    **Halo Effect**: None, Low, Medium, High?
    Medium
    
    ---
    ### Comments to reviewers
    Notes for the reviewers you have assigned.

--- com.unity.shadergraph/Tests/Editor/UnitTests/SerializedGraphTests.cs
+++ com.unity.shadergraph/Tests/Editor/UnitTests/SerializedGraphTests.cs
@@ -648,7 +648,7 @@ namespace UnityEditor.Graphing.UnitTests
             graph.Connect(outputNode.GetSlotReference(TestableNode.Output0), inputNode.GetSlotReference(TestableNode.Input0));
             Assert.AreEqual(1, graph.edges.Count());
 
-            graph.RemoveElements(graph.GetNodes<INode>(), graph.edges);
+            graph.RemoveElements(graph.GetNodes<INode>(), graph.edges, Enumerable.Empty<GroupData>());
             Assert.AreEqual(0, graph.GetNodes<INode>().Count());
             Assert.AreEqual(0, graph.edges.Count());
         }
