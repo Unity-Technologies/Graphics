d73cc50831df1c36a995e76cb013270f05fa6850
Paul Demeulenaere <pauld@unity3d.com>
2018-11-22 14:51:15 +0100

    Fix invalidation of exposed parameter (#49)
    * Fix invalidation of exposed parameter
    
    * Minor update comment
    
    * Use a public API instead of reflection (after discuss with @tristan)
    
    * Fix Undo/Redo
    
    * Fix ExposedParameter construction from array
    
    * Reset unecessary change

--- com.unity.visualeffectgraph/Utilities/ParameterBinding/ExposedParameter.cs
+++ com.unity.visualeffectgraph/Utilities/ParameterBinding/ExposedParameter.cs
@@ -7,12 +7,11 @@ using UnityEngine.Experimental.VFX;
 namespace UnityEngine.VFX.Utils
 {
     [Serializable]
-    public struct ExposedParameter
+    public class ExposedParameter
     {
         [SerializeField]
         private string m_Name;
         private int m_Id;
-        private bool m_Init;
 
         public static implicit operator ExposedParameter(string name)
         {
@@ -26,9 +25,23 @@ namespace UnityEngine.VFX.Utils
 
         public static implicit operator int(ExposedParameter parameter)
         {
-            if (!parameter.m_Init)
+#if UNITY_EDITOR
+            //In Editor, m_Id cached cannot be used for several reasons :
+            // - m_Name is modified thought a SerializedProperty
+            // -ExposedParameter are stored in array, when we modify it, m_Id is reset to zero
+            // -Undo /Redo is restoring m_Name
+            // Could be resolved modifying directly object reference in inspector, but for Undo/Redo, we have to invalid everything
+            //In Runtime, there isn't any undo/redo and SerializedObject is only available in UnityEditor namespace
+            return Shader.PropertyToID(parameter.m_Name);
+#else
+            if (parameter.m_Id == 0)
+                throw new InvalidOperationException("Unexpected constructor has been called");
+
+            if (parameter.m_Id == -1)
                 parameter.m_Id = Shader.PropertyToID(parameter.m_Name);
+
             return parameter.m_Id;
+#endif
         }
 
         public static ExposedParameter operator+(ExposedParameter self, ExposedParameter other)
@@ -36,11 +49,15 @@ namespace UnityEngine.VFX.Utils
             return new ExposedParameter(self.m_Name + other.m_Name);
         }
 
+        public ExposedParameter()
+        {
+            m_Id = -1;
+        }
+
         private ExposedParameter(string name)
         {
             m_Name = name;
-            m_Id = 0; //Cannot be -1 because ExposedParameter is a structure, otherwise, instancing from SerializedProperty will be problematic
-            m_Init = false;
+            m_Id = -1;
         }
 
         public override string ToString()
