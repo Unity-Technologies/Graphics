b2defdef2b81b0a42bdae91d78459b037bf6bfd7
Evgenii Golubev <EvgeniiG@users.noreply.github.com>
2018-11-22 08:45:42 +0100

    Backport: Change VBuffer depth parametrization #2472

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/VolumetricLighting/VolumetricLightingController.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/VolumetricLighting/VolumetricLightingController.cs
@@ -4,9 +4,8 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
 {
     public class VolumetricLightingController : VolumeComponent
     {
-        [Tooltip("Range from the front to the back of the camera's volumetric lighting buffers (in meters).")]
-        [FormerlySerializedAs("depthRange")]
-        public FloatRangeParameter distanceRange = new FloatRangeParameter(new Vector2(0.5f, 64.0f), 0.01f, 10000.0f);
+        [Tooltip("Distance from the near plane of the camera to the back of the camera's volumetric lighting buffer (in meters).")]
+        public MinFloatParameter depthExtent = new MinFloatParameter(64.0f, 0.1f);
         [Tooltip("Controls the slice distribution: 0 = exponential (more slices near the camera, fewer slices far away), 1 = linear (uniform spacing).")]
         [FormerlySerializedAs("depthDistributionUniformity")]
         public ClampedFloatParameter sliceDistributionUniformity = new ClampedFloatParameter(0.75f, 0, 1);
