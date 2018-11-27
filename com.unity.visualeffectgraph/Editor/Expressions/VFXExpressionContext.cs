6a05c544f484024a0948f6723098ee7156eed959
Julien Fryer <julienf@unity3d.com>
2018-11-27 16:37:05 +0100

    Fix bug with gradient data transformation in expression context

--- com.unity.visualeffectgraph/Editor/Expressions/VFXExpressionContext.cs
+++ com.unity.visualeffectgraph/Editor/Expressions/VFXExpressionContext.cs
@@ -121,7 +121,9 @@ namespace UnityEditor.VFX
                     {
                         var parent = Compile(e);
 
-                        if (Has(VFXExpressionContextOption.GPUDataTransformation) && expression.Is(VFXExpression.Flags.PerElement) && !parent.Is(VFXExpression.Flags.PerElement))
+                        if (Has(VFXExpressionContextOption.GPUDataTransformation)
+                            && expression.IsAny(VFXExpression.Flags.NotCompilableOnCPU)
+                            && !parent.IsAny(VFXExpression.Flags.NotCompilableOnCPU))
                             parent = InsertGPUTransformation(parent);
 
                         return parent;
