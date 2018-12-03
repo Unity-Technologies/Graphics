91b727dc6d9c8340207d946cd7514843c3c4e452
Tristan Genevet <tristan@unity3d.com>
2018-12-03 09:54:01 +0100

    remove call to isLayoutManual which seems to be 2019.1 only.

--- com.unity.visualeffectgraph/Editor/GraphView/Blackboard/VFXBlackboard.cs
+++ com.unity.visualeffectgraph/Editor/GraphView/Blackboard/VFXBlackboard.cs
@@ -101,12 +101,7 @@ namespace  UnityEditor.VFX.UI
             subTitle = "Parameters";
 
             resizer.RemoveFromHierarchy();
-
-
-            s_LayoutManual.SetValue(this, false);
         }
-
-        static System.Reflection.PropertyInfo s_LayoutManual = typeof(VisualElement).GetProperty("isLayoutManual",System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
 
         void OnKeyDown(KeyDownEvent e)
         {
