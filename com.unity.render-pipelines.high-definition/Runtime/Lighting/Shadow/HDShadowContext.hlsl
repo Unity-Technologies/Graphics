15d3ac3afd488f62bb9729a9638fa4190e68ba1f
Antti Tapaninen <aet@aet.fi>
2018-12-05 07:34:11 -0800

    fix typo (#2551)
    avoid hitting metal sampler limits
    
    add OSX test scripts, similar to WindowsBatchMode.cmd and RunAllTests_Windows.cmd
    
    add PLATFORM_NEEDS_UNORM_UAV_SPECIFIER to fix DBuffer issues
    
    update changelog

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/Shadow/HDShadowContext.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/Shadow/HDShadowContext.hlsl
@@ -15,10 +15,7 @@ struct HDShadowContext
 #include "Packages/com.unity.render-pipelines.high-definition/Runtime/Lighting/Shadow/HDShadowAlgorithms.hlsl"
 
 TEXTURE2D(_ShadowmapAtlas);
-SamplerComparisonState sampler_ShadowmapAtlas;
-
 TEXTURE2D(_ShadowmapCascadeAtlas);
-SamplerComparisonState sampler_ShadowmapCascadeAtlas;
 
 StructuredBuffer<HDShadowData>              _HDShadowDatas;
 // Only the first element is used since we only support one directional light
