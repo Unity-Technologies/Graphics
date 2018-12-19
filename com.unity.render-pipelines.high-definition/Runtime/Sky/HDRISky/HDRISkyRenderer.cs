dad1be98292c2fdf39887c72704fcec401a148dd
Fabien Houlmann <44069206+fabien-unity@users.noreply.github.com>
2018-12-19 16:32:58 -0500

    Small sky refactor to fix stereo issues (#2640)
    * Small sky refactor to fix stereo issues
    
    * change back real to float
    
    * add entry to changelog
    
    * remove useless dir rotation code in procedural sky shader

--- com.unity.render-pipelines.high-definition/Runtime/Sky/HDRISky/HDRISkyRenderer.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Sky/HDRISky/HDRISkyRenderer.cs
@@ -42,9 +42,10 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
             float luxMultiplier = m_HdriSkyParams.desiredLuxValue.value / m_HdriSkyParams.upperHemisphereLuxValue.value;
             float multiplier = (m_HdriSkyParams.skyIntensityMode == SkyIntensityMode.Exposure) ? m_HdriSkyParams.multiplier.value : luxMultiplier;
             float exposure = (m_HdriSkyParams.skyIntensityMode == SkyIntensityMode.Exposure) ? GetExposure(m_HdriSkyParams, builtinParams.debugSettings) : 0;
+            float phi = Mathf.Deg2Rad * -m_HdriSkyParams.rotation; // -rotation to match Legacy...
 
             m_SkyHDRIMaterial.SetTexture(HDShaderIDs._Cubemap, m_HdriSkyParams.hdriSky);
-            m_SkyHDRIMaterial.SetVector(HDShaderIDs._SkyParam, new Vector4(exposure, multiplier, -m_HdriSkyParams.rotation, 0.0f)); // -rotation to match Legacy...
+            m_SkyHDRIMaterial.SetVector(HDShaderIDs._SkyParam, new Vector4(exposure, multiplier, Mathf.Cos(phi), Mathf.Sin(phi)));
 
             using (new ProfilingSample(builtinParams.commandBuffer, "Draw sky"))
             {
