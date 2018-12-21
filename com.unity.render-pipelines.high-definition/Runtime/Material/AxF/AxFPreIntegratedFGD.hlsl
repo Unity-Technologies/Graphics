f07adf993da2bfd0c031ca49b20739fe5ad22d52
slunity <37302815+slunity@users.noreply.github.com>
2018-12-21 12:39:12 -0500

    backport Hdrp axf v0.1.5 preview (#2673)

--- com.unity.render-pipelines.high-definition/Runtime/Material/AxF/AxFPreIntegratedFGD.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Material/AxF/AxFPreIntegratedFGD.hlsl
@@ -2,7 +2,7 @@ TEXTURE2D(_PreIntegratedFGD_Ward);
 
 // For image based lighting, a part of the BSDF is pre-integrated.
 // This is done both for specular Ward and Lambert
-// reflectivity is  Integral{(BSDF_GGX / F) - used for multiscattering
+// reflectivity is  Integral{(BSDF_Ward / F) - used for multiscattering
 void GetPreIntegratedFGDWardAndLambert(float NdotV, float perceptualRoughness, float3 F0, out float3 specularFGD, out float diffuseFGD, out float reflectivity)
 {
     float2 preFGD = SAMPLE_TEXTURE2D_LOD(_PreIntegratedFGD_Ward, s_linear_clamp_sampler, float2(NdotV, perceptualRoughness), 0).xy;
@@ -17,7 +17,7 @@ TEXTURE2D(_PreIntegratedFGD_CookTorrance);
 
 // For image based lighting, a part of the BSDF is pre-integrated.
 // This is done both for specular Ward and Lambert
-// reflectivity is  Integral{(BSDF_GGX / F) - used for multiscattering
+// reflectivity is  Integral{(BSDF_CookTorrance / F) - used for multiscattering
 void GetPreIntegratedFGDCookTorranceAndLambert(float NdotV, float perceptualRoughness, float3 F0, out float3 specularFGD, out float diffuseFGD, out float reflectivity)
 {
     float2 preFGD = SAMPLE_TEXTURE2D_LOD(_PreIntegratedFGD_CookTorrance, s_linear_clamp_sampler, float2(NdotV, perceptualRoughness), 0).xy;
