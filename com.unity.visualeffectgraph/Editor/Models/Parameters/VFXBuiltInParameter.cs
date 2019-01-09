e0a90ac982373c63d57824181a6f343c9e2ca8ca
Paul Demeulenaere <pauld@unity3d.com>
2019-01-09 10:08:28 +0100

    Handle undefined space and let user specify them (LocalToWorld & WorldToLocal node concerned) (#76)

--- com.unity.visualeffectgraph/Editor/Models/Parameters/VFXBuiltInParameter.cs
+++ com.unity.visualeffectgraph/Editor/Models/Parameters/VFXBuiltInParameter.cs
@@ -28,32 +28,6 @@ namespace UnityEditor.VFX
 
         override public string name { get { return m_expressionOp.ToString(); } }
 
-        public override void Sanitize(int version)
-        {
-            if (VFXBuiltInExpression.Find(m_expressionOp) == null)
-            {
-                switch (m_expressionOp)
-                {
-                    // Due to reorganization, some indices have changed
-                    case VFXExpressionOperation.Tan:         m_expressionOp = VFXExpressionOperation.DeltaTime; break;
-                    case VFXExpressionOperation.ASin:        m_expressionOp = VFXExpressionOperation.TotalTime; break;
-                    case VFXExpressionOperation.ACos:        m_expressionOp = VFXExpressionOperation.SystemSeed; break;
-                    case VFXExpressionOperation.RGBtoHSV:    m_expressionOp = VFXExpressionOperation.LocalToWorld; break;
-                    case VFXExpressionOperation.HSVtoRGB:    m_expressionOp = VFXExpressionOperation.WorldToLocal; break;
-
-                    default:
-                        Debug.LogWarning(string.Format("Expression operator for the BuiltInParameter is invalid ({0}). Reset to none", m_expressionOp));
-                        m_expressionOp = VFXExpressionOperation.None;
-                        break;
-                }
-            }
-
-            if (outputSlots.Count > 0 && outputSlots[0].GetType() == typeof(Matrix4x4))
-                outputSlots[0].Detach(); // In order not to have a bad conversion
-
-            base.Sanitize(version); // Will call ResyncSlots
-        }
-
         private Type GetOutputType()
         {
             switch (m_expressionOp)
