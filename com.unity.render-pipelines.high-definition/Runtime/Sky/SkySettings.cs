7f2b34e97a11462e8b8c83c86888297a126ef2f9
JulienIgnace-Unity <julien@unity3d.com>
2019-01-25 16:33:20 +0100

    Various bugfixes (#2815)
    * Fixed an issue where sky cubemap would not be cleared correctly when setting the current sky to None
    
    * Fixed a typo in Staitc Lighting Sky component UI.
    
    * Added HDRenderPipeline.RequestSkyEnvironmentUpdate to force an update from script when sky is set to OnDemand
    
    * Changelog Update
    
    * Update CHANGELOG.md
    
    * Updated test screenshot

--- com.unity.render-pipelines.high-definition/Runtime/Sky/SkySettings.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Sky/SkySettings.cs
@@ -59,7 +59,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
         public MinFloatParameter        upperHemisphereLuxValue = new MinFloatParameter(1.0f, 0.0f);
         [Tooltip("Lux intensity multiplier for the sky")]
         public FloatParameter           desiredLuxValue = new FloatParameter(20000);
-        [Tooltip("Specify how the environment lighting should be updated.")]
+        [Tooltip("Specify how the environment lighting should be updated. When set to OnDemand, use HDRenderPipeline.RequestSkyEnvironmentUpdate() to request an update.")]
         public EnvUpdateParameter       updateMode = new EnvUpdateParameter(EnvironementUpdateMode.OnChanged);
         [Tooltip("If environment update is set to realtime, period in seconds at which it is updated (0.0 means every frame).")]
         public MinFloatParameter        updatePeriod = new MinFloatParameter(0.0f, 0.0f);
