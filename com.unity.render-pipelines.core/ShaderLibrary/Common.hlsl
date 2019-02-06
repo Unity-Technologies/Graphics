42abe4aaa2b1bd8d14a92dc022669c70524ea292
Evgenii Golubev <EvgeniiG@users.noreply.github.com>
2019-02-06 06:01:11 -0800

    Implement fast portable versions of IsNaN() and IsInf() (#2816)

--- com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl
+++ com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl
@@ -304,26 +304,69 @@ float CubeMapFaceID(float3 dir)
 }
 #endif // INTRINSIC_CUBEMAP_FACE_ID
 
+#if !defined(SHADER_API_GLES)
 // Intrinsic isnan can't be used because it require /Gic to be enabled on fxc that we can't do. So use AnyIsNan instead
-bool IsNan(float n)
+bool IsNaN(float x)
 {
-    return (n < 0.0 || n > 0.0 || n == 0.0) ? false : true;
+    return (asuint(x) & 0x7FFFFFFF) > 0x7F800000;
 }
 
-bool AnyIsNan(float2 v)
+bool AnyIsNaN(float2 v)
 {
-    return (IsNan(v.x) || IsNan(v.y));
+    return (IsNaN(v.x) || IsNaN(v.y));
 }
 
-bool AnyIsNan(float3 v)
+bool AnyIsNaN(float3 v)
 {
-    return (IsNan(v.x) || IsNan(v.y) || IsNan(v.z));
+    return (IsNaN(v.x) || IsNaN(v.y) || IsNaN(v.z));
 }
 
-bool AnyIsNan(float4 v)
+bool AnyIsNaN(float4 v)
 {
-    return (IsNan(v.x) || IsNan(v.y) || IsNan(v.z) || IsNan(v.w));
+    return (IsNaN(v.x) || IsNaN(v.y) || IsNaN(v.z) || IsNaN(v.w));
 }
+
+bool IsInf(float x)
+{
+    return (asuint(x) & 0x7FFFFFFF) == 0x7F800000;
+}
+
+bool AnyIsInf(float2 v)
+{
+    return (IsInf(v.x) || IsInf(v.y));
+}
+
+bool AnyIsInf(float3 v)
+{
+    return (IsInf(v.x) || IsInf(v.y) || IsInf(v.z));
+}
+
+bool AnyIsInf(float4 v)
+{
+    return (IsInf(v.x) || IsInf(v.y) || IsInf(v.z) || IsInf(v.w));
+}
+
+bool IsFinite(float x)
+{
+    return (asuint(x) & 0x7F800000) != 0x7F800000;
+}
+
+float SanitizeFinite(float x)
+{
+    return IsFinite(x) ? x : 0;
+}
+
+bool IsPositiveFinite(float x)
+{
+    return asuint(x) < 0x7F800000;
+}
+
+float SanitizePositiveFinite(float x)
+{
+    return IsPositiveFinite(x) ? x : 0;
+}
+
+#endif
 
 // ----------------------------------------------------------------------------
 // Common math functions
