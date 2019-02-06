e85efd8aa64b6976eeb959da0a8d635bf0e0d283
Frédéric Vauchelles <frederic.vauchelles@gmail.com>
2019-02-06 13:45:22 +0100

    Added OnDemand realtime render for realtime probes (#2904)

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/Reflection/HDAdditionalReflectionData.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/Reflection/HDAdditionalReflectionData.cs
@@ -9,4 +9,22 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
             k_ReflectionProbeMigration.Migrate(this);
         }
     }
+
+    public static class HDAdditionalReflectionDataExtensions
+    {
+        /// <summary>
+        /// Request to render this probe next update.
+        ///
+        /// Call this method with the mode <see cref="ProbeSettings.RealtimeMode.OnDemand"/> and the probe will
+        /// be rendered the next time it will influence a camera rendering.
+        ///
+        /// If the probe don't have a <see cref="HDAdditionalReflectionData"/> component, nothing is done.
+        /// </summary>
+        public static void RequestRenderNextUpdate(this ReflectionProbe probe)
+        {
+            var add = probe.GetComponent<HDAdditionalReflectionData>();
+            if (add != null && !add.Equals(null))
+                add.RequestRenderNextUpdate();
+        }
+    }
 }
