2385b321ed0ac4f26fbf2f156b9237b0b09aac11
chris.chu <chris.chu@unity3d.com>
2019-02-08 08:42:38 +0800

    Fixed a few number of issues with menus and ux

--- com.unity.render-pipelines.lightweight/Editor/2D/Lightweight2DMenus.cs
+++ com.unity.render-pipelines.lightweight/Editor/2D/Lightweight2DMenus.cs
@@ -5,11 +5,41 @@ namespace UnityEditor.Experimental.Rendering.LWRP
 {
     public class Lightweight2DMenus : MonoBehaviour
     {
-        [MenuItem("GameObject/2D Object/Light 2d")]
-        static void CreateLight2D()
+        [MenuItem("GameObject/Light/2D/Freeform Light2D", false, -100)]
+        static void CreateFreeformLight2D()
         {
-            GameObject go = new GameObject("New Light 2d");
-            go.AddComponent<Light2D>();
+            GameObject go = new GameObject("Freeform Light2D");
+            Light2D light2D = go.AddComponent<Light2D>();
+            light2D.LightProjectionType = Light2D.LightProjectionTypes.Shape;
+            light2D.m_ShapeLightStyle = Light2D.CookieStyles.Parametric;
+            light2D.m_ParametricShape = Light2D.ParametricShapes.Freeform;
+        }
+
+        [MenuItem("GameObject/Light/2D/Sprite Light2D", false, -100)]
+        static void CreateSpriteLight2D()
+        {
+            GameObject go = new GameObject("Sprite Light2D");
+            Light2D light2D = go.AddComponent<Light2D>();
+            light2D.LightProjectionType = Light2D.LightProjectionTypes.Shape;
+            light2D.m_ShapeLightStyle = Light2D.CookieStyles.Sprite;
+        }
+
+        [MenuItem("GameObject/Light/2D/Parametric Light2D", false, -100)]
+        static void CreateParametricLight2D()
+        {
+            GameObject go = new GameObject("Parametric Light2D");
+            Light2D  light2D = go.AddComponent<Light2D>();
+            light2D.LightProjectionType = Light2D.LightProjectionTypes.Shape;
+            light2D.m_ShapeLightStyle = Light2D.CookieStyles.Parametric;
+            light2D.m_ParametricShape = Light2D.ParametricShapes.Circle;
+        }
+
+        [MenuItem("GameObject/Light/2D/Point Light2D", false, -100)]
+        static void CreatePointLight2D()
+        {
+            GameObject go = new GameObject("Point Light2D");
+            Light2D light2D = go.AddComponent<Light2D>();
+            light2D.LightProjectionType = Light2D.LightProjectionTypes.Point;
         }
     }
 }
