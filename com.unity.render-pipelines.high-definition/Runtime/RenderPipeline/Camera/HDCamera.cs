8de87b92c13421653bfddf28ac75534deea58d4a
sebastienlagarde <sebastien@unity3d.com>
2019-02-11 17:41:28 +0100

    Revert "Revert "Hdrp/hide camera clear depth (#2920)" (#2940)" (#2946)
    This reverts commit a7d8bd72dce98399732631a3f8de1257a87975d7.

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/Camera/HDCamera.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/Camera/HDCamera.cs
@@ -158,7 +158,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
                 if (camera.clearFlags == CameraClearFlags.Skybox)
                     return HDAdditionalCameraData.ClearColorMode.Sky;
                 else if (camera.clearFlags == CameraClearFlags.SolidColor)
-                    return HDAdditionalCameraData.ClearColorMode.BackgroundColor;
+                    return HDAdditionalCameraData.ClearColorMode.Color;
                 else // None
                     return HDAdditionalCameraData.ClearColorMode.None;
             }
