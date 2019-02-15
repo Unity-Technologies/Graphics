0779c375d5c95b00237d7644eb84f5b65f634054
Raymond Graham <raymond.graham4087@gmail.com>
2019-02-15 14:09:42 -0800

    Disable the emissive forward pass if the decal is not emissive.

--- com.unity.render-pipelines.high-definition/Editor/Material/Decal/DecalUI.cs
+++ com.unity.render-pipelines.high-definition/Editor/Material/Decal/DecalUI.cs
@@ -200,6 +200,7 @@ namespace UnityEditor.Experimental.Rendering.HDPipeline
             material.SetShaderPassEnabled(HDShaderPassNames.s_MeshDecalsAOSStr, false);
             material.SetShaderPassEnabled(HDShaderPassNames.s_MeshDecalsMAOSStr, false);
             material.SetShaderPassEnabled(HDShaderPassNames.s_MeshDecals3RTStr, true);
+            material.SetShaderPassEnabled(HDShaderPassNames.s_MeshDecalsForwardEmissive, (material.GetFloat(kEmissive) == 1.0f));
             switch (blendMode)
             {
                 case Decal.MaskBlendFlags.Metal:
