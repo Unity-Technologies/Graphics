11bde344a3cc7d4aaa7d7eaf7659e773ec0aaaa0
Martin Thorzen <35328557+martint-unity@users.noreply.github.com>
2019-02-18 11:58:12 +0100

    Use saturate to avoid nan when negative values (#2952)
    * Use saturate to avoid nan when negative values
    
    * Updated changelog
    
    * Update CHANGELOG.md

--- com.unity.shadergraph/CHANGELOG.md
+++ com.unity.shadergraph/CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [6.3.0] - 2019-XX-XX
+### Fixed
+- Fixed an issue where the Normal Reconstruct Z Node sometimes caused Not a Number (NaN) errors when using negative values.
 
 ## [6.2.0] - 2019-02-15
 ### Fixed
--- com.unity.shadergraph/Editor/Data/Nodes/Artistic/Normal/NormalReconstructZNode.cs
+++ com.unity.shadergraph/Editor/Data/Nodes/Artistic/Normal/NormalReconstructZNode.cs
@@ -11,7 +11,6 @@ namespace UnityEditor.ShaderGraph
             name = "Normal Reconstruct Z";
         }
 
-
         protected override MethodInfo GetFunctionToConvert()
         {
             return GetType().GetMethod("NormalReconstructZ", BindingFlags.Static | BindingFlags.NonPublic);
@@ -25,7 +24,7 @@ namespace UnityEditor.ShaderGraph
             return
                 @"
 {
-    {precision} reconstructZ = sqrt(1 - ( In.x * In.x + In.y * In.y));
+    {precision} reconstructZ = sqrt(1.0 - saturate(dot(In.xy, In.xy)));
     {precision}3 normalVector = {precision}3(In.x, In.y, reconstructZ);
     Out = normalize(normalVector);
 }";
