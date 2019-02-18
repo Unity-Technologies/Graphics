62ffa4e38fd016c89d8afd2eacf8c83c99e9e828
Felipe Lira <felipedrl@gmail.com>
2019-02-18 15:23:22 +0100

    Removed IBeforeCameraRender callback. This is exposed in SRP core now.

--- com.unity.render-pipelines.lightweight/Runtime/LightweightRenderPipeline.cs
+++ com.unity.render-pipelines.lightweight/Runtime/LightweightRenderPipeline.cs
@@ -10,11 +10,6 @@ using Lightmapping = UnityEngine.Experimental.GlobalIllumination.Lightmapping;
 
 namespace UnityEngine.Rendering.LWRP
 {
-    public interface IBeforeCameraRender
-    {
-        void ExecuteBeforeCameraRender(ScriptableRenderContext context, Camera camera);
-    }
-
     public sealed partial class LightweightRenderPipeline : RenderPipeline
     {
         static class PerFrameBuffer
@@ -119,12 +114,7 @@ namespace UnityEngine.Rendering.LWRP
             foreach (Camera camera in cameras)
             {
                 BeginCameraRendering(renderContext, camera);
-
-                foreach (var beforeCamera in camera.GetComponents<IBeforeCameraRender>())
-                    beforeCamera.ExecuteBeforeCameraRender(renderContext, camera);
-
                 RenderSingleCamera(renderContext, camera);
-
                 EndCameraRendering(renderContext, camera);
             }
 
