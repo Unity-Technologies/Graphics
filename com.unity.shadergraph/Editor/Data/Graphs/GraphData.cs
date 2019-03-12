ae6b2981ff5f5c01a9205321c0ab8e55f6baeb7e
Martin Thorzen <35328557+martint-unity@users.noreply.github.com>
2019-03-12 14:31:05 +0100

    group node fixes (#3164)
    * Create group with ctrl + g. Selects title
    
    so that the user can rename the title directly
    
    * FIxes for group nodes. Deleting changes, save pos
    
    * renamed Group to group
    
    * renaming again
    
    * Calling the new method for editing title
    
    * Moved the deletion of the groups after node delete
    
    * cleanup of code
    
    * Cleanup code

--- com.unity.shadergraph/Editor/Data/Graphs/GraphData.cs
+++ com.unity.shadergraph/Editor/Data/Graphs/GraphData.cs
@@ -106,7 +106,6 @@ namespace UnityEditor.ShaderGraph
         {
             get { return m_PastedNodes; }
         }
-        
         #endregion
 
         #region Group Data
@@ -150,6 +149,11 @@ namespace UnityEditor.ShaderGraph
         {
             get { return m_NodeGroupChanges; }
         }
+
+        [NonSerialized]
+        GroupData m_MostRecentlyCreatedGroup;
+
+        public GroupData mostRecentlyCreatedGroup => m_MostRecentlyCreatedGroup;
 
         [NonSerialized]
         Dictionary<Guid, List<AbstractMaterialNode>> m_GroupNodes = new Dictionary<Guid, List<AbstractMaterialNode>>();
@@ -226,9 +230,8 @@ namespace UnityEditor.ShaderGraph
         }
 
         public MessageManager messageManager { get; set; }
-        
         public bool isSubGraph { get; set; }
-        
+
         [NonSerialized]
         private AbstractMaterialNode m_OutputNode;
 
@@ -248,7 +251,6 @@ namespace UnityEditor.ShaderGraph
                         m_OutputNode = (AbstractMaterialNode)GetNodes<IMasterNode>().FirstOrDefault();
                     }
                 }
-                
 
                 return m_OutputNode;
             }
@@ -273,6 +275,7 @@ namespace UnityEditor.ShaderGraph
             m_AddedProperties.Clear();
             m_RemovedProperties.Clear();
             m_MovedProperties.Clear();
+            m_MostRecentlyCreatedGroup = null;
         }
 
         public void AddNode(AbstractMaterialNode node)
@@ -284,7 +287,7 @@ namespace UnityEditor.ShaderGraph
                     Debug.LogWarningFormat("Attempting to add {0} to Sub Graph. This is not allowed.", materialNode.GetType());
                     return;
                 }
-                
+
                 AddNodeNoValidate(materialNode);
                 ValidateGraph();
             }
@@ -294,13 +297,24 @@ namespace UnityEditor.ShaderGraph
             }
         }
 
-        public void AddGroupData(GroupData groupData)
+        public void CreateGroup(GroupData groupData)
+        {
+            if (AddGroup(groupData))
+            {
+                m_MostRecentlyCreatedGroup = groupData;
+            }
+        }
+
+        bool AddGroup(GroupData groupData)
         {
             if (m_Groups.Contains(groupData))
-                return;
+                return false;
+
             m_Groups.Add(groupData);
             m_AddedGroups.Add(groupData);
             m_GroupNodes.Add(groupData.guid, new List<AbstractMaterialNode>());
+
+            return true;
         }
 
         public void RemoveGroup(GroupData groupData)
@@ -318,11 +332,12 @@ namespace UnityEditor.ShaderGraph
 
             if (m_GroupNodes.TryGetValue(group.guid, out var nodes))
             {
-                m_GroupNodes.Remove(group.guid);
-                foreach (AbstractMaterialNode node in nodes)
+                foreach (AbstractMaterialNode node in nodes.ToList())
                 {
-                    RemoveNodeNoValidate(node);
+                    SetNodeGroup(node, null);
                 }
+
+                m_GroupNodes.Remove(group.guid);
             }
         }
 
@@ -340,11 +355,6 @@ namespace UnityEditor.ShaderGraph
 
             var oldGroupNodes = m_GroupNodes[groupChange.oldGroupGuid];
             oldGroupNodes.Remove(node);
-
-            if (groupChange.oldGroupGuid != Guid.Empty && !oldGroupNodes.Any())
-            {
-                RemoveGroupNoValidate(m_Groups.First(x => x.guid == groupChange.oldGroupGuid));
-            }
 
             m_GroupNodes[groupChange.newGroupGuid].Add(node);
             m_NodeGroupChanges.Add(groupChange);
@@ -403,10 +413,6 @@ namespace UnityEditor.ShaderGraph
             if (m_GroupNodes.TryGetValue(materialNode.groupGuid, out var nodes))
             {
                 nodes.Remove(materialNode);
-                if (materialNode.groupGuid != Guid.Empty && !nodes.Any())
-                {
-                    RemoveGroupNoValidate(m_Groups.First(x => x.guid == materialNode.groupGuid));
-                }
             }
         }
 
@@ -483,15 +489,18 @@ namespace UnityEditor.ShaderGraph
         public void RemoveElements(IEnumerable<AbstractMaterialNode> nodes, IEnumerable<IEdge> edges, IEnumerable<GroupData> groups)
         {
             foreach (var edge in edges.ToArray())
+            {
                 RemoveEdgeNoValidate(edge);
+            }
 
             foreach (var serializableNode in nodes.ToArray())
+            {
                 RemoveNodeNoValidate(serializableNode);
+            }
 
             foreach (var groupData in groups)
             {
-                if (m_GroupNodes.ContainsKey(groupData.guid))
-                    RemoveGroupNoValidate(groupData);
+                RemoveGroupNoValidate(groupData);
             }
 
             ValidateGraph();
@@ -568,7 +577,7 @@ namespace UnityEditor.ShaderGraph
                     foundEdges.Add(edge);
             }
         }
-        
+
         public IEnumerable<IEdge> GetEdges(SlotReference s)
         {
             var edges = new List<IEdge>();
@@ -761,13 +770,21 @@ namespace UnityEditor.ShaderGraph
                     m_AddedEdges.Remove(edge);
                 }
             }
+
+            foreach (var groupChange in m_NodeGroupChanges.ToList())
+            {
+                if (!ContainsNodeGuid(groupChange.nodeGuid))
+                {
+                    m_NodeGroupChanges.Remove(groupChange);
+                }
+            }
         }
 
         public void AddValidationError(Identifier id, string errorMessage)
         {
             messageManager?.AddOrAppendError(this, id, new ShaderMessage(errorMessage));;
         }
-        
+
         public void ReplaceWith(GraphData other)
         {
             if (other == null)
@@ -822,7 +839,7 @@ namespace UnityEditor.ShaderGraph
             ValidateGraph();
 
             foreach (GroupData groupData in other.groups)
-                AddGroupData(groupData);
+                AddGroup(groupData);
 
             foreach (var node in other.GetNodes<AbstractMaterialNode>())
                 AddNodeNoValidate(node);
@@ -848,7 +865,7 @@ namespace UnityEditor.ShaderGraph
                 var newGuid = newGroup.guid;
                 groupGuidMap[oldGuid] = newGuid;
 
-                AddGroupData(newGroup);
+                AddGroup(newGroup);
                 m_PastedGroups.Add(newGroup);
             }
 
@@ -939,6 +956,12 @@ namespace UnityEditor.ShaderGraph
             var nodes = SerializationHelper.Deserialize<AbstractMaterialNode>(m_SerializableNodes, GraphUtil.GetLegacyTypeRemapping());
             m_Nodes = new List<AbstractMaterialNode>(nodes.Count);
             m_NodeDictionary = new Dictionary<Guid, AbstractMaterialNode>(nodes.Count);
+
+            foreach (var group in m_Groups)
+            {
+                m_GroupNodes.Add(group.guid, new List<AbstractMaterialNode>());
+            }
+
             foreach (var node in nodes.OfType<AbstractMaterialNode>())
             {
                 node.owner = this;
@@ -946,11 +969,7 @@ namespace UnityEditor.ShaderGraph
                 node.tempId = new Identifier(m_Nodes.Count);
                 m_Nodes.Add(node);
                 m_NodeDictionary.Add(node.guid, node);
-
-                if(m_GroupNodes.ContainsKey(node.groupGuid))
-                    m_GroupNodes[node.groupGuid].Add(node);
-                else
-                    m_GroupNodes.Add(node.groupGuid, new List<AbstractMaterialNode>(){node});
+                m_GroupNodes[node.groupGuid].Add(node);
             }
 
             m_SerializableNodes = null;
