c86ebe2f8e9cdad132df48d8d8078383498e6bc0
Fabien Houlmann <44069206+fabien-unity@users.noreply.github.com>
2019-03-19 10:16:15 -0400

    Restructure code from HDCamera.Update() (#3257)
    * reorganized code from HDCamera Update()
    
    added UpdateAntialiasing() and UpdateViewConstants() to HDCamera
    also removed unused invViewProjMatrix from SkyRenderingContext
    
    * update comment on _DeferredShadowTexture
    
    * added entry to changelog

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/ShaderVariablesLightLoop.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/ShaderVariablesLightLoop.hlsl
@@ -32,7 +32,7 @@
     TEXTURECUBE_ARRAY_ABSTRACT(_EnvCubemapTextures);
     TEXTURE2D_ARRAY(_Env2DTextures);
 
-    // XRTODO: Need to stereo-ize access
+    // Contact shadows
     TEXTURE2D_X(_DeferredShadowTexture);
 
 #if SHADEROPTIONS_RAYTRACING
