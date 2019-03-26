642a610c51ea0dbc4678628c538c97b022dff8ca
chris.chu <chris.chu@unity3d.com>
2019-03-26 16:53:10 +0800

    Started work on _2D renderer editor. Checking in empty class that seems to let us use the class in the editor again

---
+++ com.unity.render-pipelines.lightweight/Editor/2D/_2DRendererEditor.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+using UnityEngine.Rendering.LWRP;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEditor.Experimental.Rendering.LWRP;
+using UnityEditor;
+
+namespace UnityEngine.Experimental.Rendering.LWRP
+{
+    [CustomEditor(typeof(_2DRendererData))]
+    [CanEditMultipleObjects]
+    internal class _2DRendererEditor : Editor
+    {
+    }
+}
