5273cd7dfc7ed245736f742e8aec060624bbe2e1
Yuanxing Cai <yuanxing@unity3d.com>
2019-03-29 18:04:47 +0800

    _2DRendererEditor.cs -> _2DRendererDataEditor.cs

---
+++ com.unity.render-pipelines.lightweight/Editor/2D/_2DRendererDataEditor.cs
@@ -0,0 +1,14 @@
+using UnityEditor.Rendering.LWRP;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+namespace UnityEditor.Experimental.Rendering.LWRP
+{
+    [CustomEditor(typeof(_2DRendererData), true)]
+    internal class _2DRendererDataEditor : Editor
+    {
+        //public override void OnInspectorGUI()
+        //{
+        //    //base.OnInspectorGUI(); // Draw the base UI, contains ScriptableRenderFeatures list
+        //}
+    }
+}
