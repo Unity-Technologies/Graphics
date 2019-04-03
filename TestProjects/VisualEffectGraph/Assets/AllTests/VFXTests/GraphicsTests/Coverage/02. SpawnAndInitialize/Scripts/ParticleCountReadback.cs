5c6d029cd3e47f22741a2513b6105d5407c23ea8
Vlad Neykov <vlad@vladimirneykov.com>
2019-04-03 18:30:35 +0100

    Initial push of test coverage

---
+++ TestProjects/VisualEffectGraph/Assets/AllTests/VFXTests/GraphicsTests/Coverage/02. SpawnAndInitialize/Scripts/ParticleCountReadback.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+[ExecuteInEditMode]
+public class ParticleCountReadback : MonoBehaviour
+{
+
+    public VisualEffect vf;
+    public float particlesAlive = 0f;
+    public TextMesh text;
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            if (!vf)
+                vf = GetComponent<VisualEffect>();
+
+            particlesAlive = vf.aliveParticleCount;
+
+            if(text)
+                text.text = particlesAlive.ToString();
+            yield return null;
+        }
+    }
+}
