7c3e58160a4f2c64cd30f5639fcba1426e9a638b
Roberto Bertini Renzetti (Unity) <robertor@unity3d.com>
2019-04-04 16:05:26 -0700

    memory allocation test test suite with one test for checking memory allocation on motion blur

---
+++ TestProjects/HDRP_Memory_Tests/Assets/HDRP_Memory_Tests/MemoryTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using Is = UnityEngine.TestTools.Constraints.Is;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine.TestTools.Constraints;
+
+namespace Tests
+{
+    public class MemoryTests
+    {
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator MemoryTests_RenderMotionBlurSceneToCubeMap_CheckThatNoGCMemoryWasAllocated()
+        {
+            SceneManager.LoadScene("Assets/Scenes/MotionBlur.unity");
+
+            // Arbitrary wait for 5 frames for the scene to load, and other stuff to happen (like Realtime GI to appear ...)
+            for (int i = 0; i < 5; ++i)
+                yield return null;
+
+            Camera cam = GameObject.FindObjectOfType<Camera>();
+            Cubemap cubemap =  new Cubemap(128, TextureFormat.RGBA32, false);
+
+            // render into cubemap while checking for memory allocation
+            Assert.That(() => { bool b = cam.RenderToCubemap(cubemap); Assert.IsTrue(b); }, Is.Not.AllocatingGCMemory(), "The method tested allocated some memory");
+
+            yield return null;
+        }
+    }
+}
---
+++ TestProjects/HDRP_Memory_Tests/Assets/Scripts/MoveMe.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMe : MonoBehaviour
+{
+    public float _speed = 10;
+    public float _direction = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > 15)
+        {
+            _direction = -1;
+        }
+        if (transform.position.x < 0)
+        {
+            _direction = 1;
+        }
+        transform.Translate(new Vector3(_direction, 0, 0) * _speed * Time.deltaTime);
+    }
+}
