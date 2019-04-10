Unity-Technologies/Graphics

commit 31dc554f9bbf1ba2c3ee1d128d9d6e2469abf5d2

Fixed checkout of file when baking reflection probes (#3344)

com.unity.render-pipelines.high-definition/Editor/Lighting/Reflection/HDBakedReflectionSystem.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using Unity.Collections.LowLevel.Unsafe;
 using UnityEditor;
 using UnityEditor.Experimental.Rendering;
+using UnityEditor.Experimental.Rendering.HDPipeline;
 using UnityEditor.VersionControl;
 using UnityEngine.Assertions;
 using UnityEngine.Rendering;
@@ -217,7 +218,14 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
 
                     var bakedTexturePath = HDBakingUtilities.GetBakedTextureFilePath(probe);
                     HDBakingUtilities.CreateParentDirectoryIfMissing(bakedTexturePath);
-                    File.Copy(cacheFile, bakedTexturePath, true);
+                    if (Provider.isActive && File.Exists(bakedTexturePath))
+                    {
+                        Checkout(bakedTexturePath, CheckoutMode.Both);
+                        // Checkout will make those file writeable, but this is not immediate,
+                        // so we retries when this fails.
+                        if (!HDEditorUtils.CopyFileWithRetryOnUnauthorizedAccess(cacheFile, bakedTexturePath))
+                            return;
+                    }
                 }
                 // AssetPipeline bug
                 // Sometimes, the baked texture reference is destroyed during 'AssetDatabase.StopAssetEditing()'
@@ -513,7 +521,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
                             (uint)StaticEditorFlags.ReflectionProbeStatic
                         );
                         HDBakingUtilities.CreateParentDirectoryIfMissing(targetFile);
-                        if (Provider.isActive)
+                        if (Provider.isActive && HDEditorUtils.IsAssetPath(targetFile))
                             Checkout(targetFile, CheckoutMode.Both);
                         HDTextureUtilities.WriteTextureFileToDisk(cubeRT, targetFile);
                         break;
@@ -533,12 +541,12 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
                             out CameraSettings cameraSettings, out CameraPositionSettings cameraPositionSettings
                         );
                         HDBakingUtilities.CreateParentDirectoryIfMissing(targetFile);
-                        if (Provider.isActive)
+                        if (Provider.isActive && HDEditorUtils.IsAssetPath(targetFile))
                             Checkout(targetFile, CheckoutMode.Both);
                         HDTextureUtilities.WriteTextureFileToDisk(planarRT, targetFile);
                         var renderData = new HDProbe.RenderData(cameraSettings, cameraPositionSettings);
                         var targetRenderDataFile = targetFile + ".renderData";
-                        if (Provider.isActive)
+                        if (Provider.isActive && HDEditorUtils.IsAssetPath(targetRenderDataFile))
                             Checkout(targetRenderDataFile, CheckoutMode.Both);
                         HDBakingUtilities.TrySerializeToDisk(renderData, targetRenderDataFile);
                         break;

com.unity.render-pipelines.high-definition/Editor/RenderPipeline/HDEditorUtils.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using UnityEditorInternal;
 using UnityEditor.Rendering;
@@ -79,6 +80,40 @@ namespace UnityEditor.Experimental.Rendering.HDPipeline
             var rect = GUILayoutUtility.GetRect(11, 17, GUILayout.ExpandWidth(false));
             rect.y += 4;
             return rect;
+        }
+
+        public static bool IsAssetPath(string path)
+        {
+            var isPathRooted = Path.IsPathRooted(path);
+            return isPathRooted && path.StartsWith(Application.dataPath)
+                   || !isPathRooted && path.StartsWith("Assets");
+        }
+
+        // Copy texture from cache
+        public static bool CopyFileWithRetryOnUnauthorizedAccess(string s, string path)
+        {
+            UnauthorizedAccessException exception = null;
+            for (var k = 0; k < 20; ++k)
+            {
+                try
+                {
+                    File.Copy(s, path, true);
+                    exception = null;
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    exception = e;
+                }
+            }
+
+            if (exception != null)
+            {
+                Debug.LogException(exception);
+                // Abort the update, something else is preventing the copy
+                return false;
+            }
+
+            return true;
         }
 
         public static void PropertyFieldWithOptionalFlagToggle<TEnum>(

commit 3764e04ee8204cffa58712cdea1f811fb4e587de

Fixed an issue where baked probe file was not copied correctly  (introduced by PR https://github.com/Unity-Technologies/ScriptableRenderPipeline/pull/3344 (#3357)

com.unity.render-pipelines.high-definition/Editor/Lighting/Reflection/HDBakedReflectionSystem.cs
@@ -221,11 +221,11 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
                     if (Provider.isActive && File.Exists(bakedTexturePath))
                     {
                         Checkout(bakedTexturePath, CheckoutMode.Both);
-                        // Checkout will make those file writeable, but this is not immediate,
-                        // so we retries when this fails.
-                        if (!HDEditorUtils.CopyFileWithRetryOnUnauthorizedAccess(cacheFile, bakedTexturePath))
-                            return;
                     }
+                    // Checkout will make those file writeable, but this is not immediate,
+                    // so we retries when this fails.
+                    if (!HDEditorUtils.CopyFileWithRetryOnUnauthorizedAccess(cacheFile, bakedTexturePath))
+                        return;
                 }
                 // AssetPipeline bug
                 // Sometimes, the baked texture reference is destroyed during 'AssetDatabase.StopAssetEditing()'
@@ -252,8 +252,6 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
                     var index = addIndices[i];
                     var instanceId = states[index].instanceID;
                     var probe = (HDProbe)EditorUtility.InstanceIDToObject(instanceId);
-                    var cacheFile = GetGICacheFileForHDProbe(states[index].probeBakingHash);
-
                     var bakedTexturePath = HDBakingUtilities.GetBakedTextureFilePath(probe);
                     var bakedTexture = AssetDatabase.LoadAssetAtPath<Texture>(bakedTexturePath);
                     Assert.IsNotNull(bakedTexture, "The baked texture was imported before, " +