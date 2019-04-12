1b48f0384bb192565e0b8eea1c1b33261874f551
Thomas <chman.a11w@gmail.com>
2019-04-12 15:07:53 +0200

    Fixed volume blending pass breaking the loop depending on the pipeline config

--- com.unity.render-pipelines.lightweight/Runtime/Passes/VolumeBlendingPass.cs
+++ com.unity.render-pipelines.lightweight/Runtime/Passes/VolumeBlendingPass.cs
@@ -3,18 +3,19 @@ namespace UnityEngine.Rendering.LWRP
     // TODO: xmldoc
     internal class VolumeBlendingPass : ScriptableRenderPass
     {
-        const string k_RenderPostProcessingTag = "Volume Blending";
+        const string k_VolumeBlendingTag = "Volume Blending";
 
         public VolumeBlendingPass(RenderPassEvent evt)
         {
             renderPassEvent = evt;
+            overrideCameraTarget = true;
         }
 
         /// <inheritdoc/>
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
             ref var cameraData = ref renderingData.cameraData;
-            var cmd = CommandBufferPool.Get(k_RenderPostProcessingTag);
+            var cmd = CommandBufferPool.Get(k_VolumeBlendingTag);
 
             // TODO: Send a command buffer for texture blending ops
             VolumeManager.instance.Update(cameraData.volumeTrigger, cameraData.volumeLayerMask);
