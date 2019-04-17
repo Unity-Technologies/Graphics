ada06d28a939779d67a099f340614e05d9dc6fb0
Yuanxing Cai <yuanxing@unity3d.com>
2019-04-17 16:28:49 +0800

    Added warning dialogs before upgrading.

--- com.unity.render-pipelines.lightweight/Editor/2D/Lightweight2DUpgrader.cs
+++ com.unity.render-pipelines.lightweight/Editor/2D/Lightweight2DUpgrader.cs
@@ -1,66 +1,86 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityEditor;
 
-internal class Lightweight2DUpgrader : MonoBehaviour
+namespace UnityEditor.Experimental.Rendering.LWRP
 {
-    static Material s_SpriteLitDefault = UnityEditor.AssetDatabase.LoadAssetAtPath<Material>("Packages/com.unity.render-pipelines.lightweight/Runtime/Materials/Sprite-Lit-Default.mat");
-
-    public delegate void Upgrader<T>(T toUpgrade) where T : Object;
-
-    static void ProcessAssetDatabaseObjects<T>(string searchString, Upgrader<T> upgrader) where T : Object
+    static class Lightweight2DUpgrader
     {
-        string[] prefabNames = AssetDatabase.FindAssets(searchString);
-        foreach (string prefabName in prefabNames)
+        static Material s_SpriteLitDefault = AssetDatabase.LoadAssetAtPath<Material>("Packages/com.unity.render-pipelines.lightweight/Runtime/Materials/Sprite-Lit-Default.mat");
+
+        delegate void Upgrader<T>(T toUpgrade) where T : Object;
+
+        static void ProcessAssetDatabaseObjects<T>(string searchString, Upgrader<T> upgrader) where T : Object
         {
-            string path = AssetDatabase.GUIDToAssetPath(prefabName);
-            if (path.StartsWith("Assets"))
+            string[] prefabNames = AssetDatabase.FindAssets(searchString);
+            foreach (string prefabName in prefabNames)
             {
-                T obj = AssetDatabase.LoadAssetAtPath<T>(path);
-                if(obj != null)
+                string path = AssetDatabase.GUIDToAssetPath(prefabName);
+                if (path.StartsWith("Assets"))
                 {
-                    upgrader(obj);
+                    T obj = AssetDatabase.LoadAssetAtPath<T>(path);
+                    if (obj != null)
+                    {
+                        upgrader(obj);
+                    }
                 }
             }
         }
-    }
 
-    static void UpgradeGameObject(GameObject go)
-    {
-        SpriteRenderer[] spriteRenderers = go.GetComponentsInChildren<SpriteRenderer>(true);
-        foreach (SpriteRenderer renderer in spriteRenderers)
+        static void UpgradeGameObject(GameObject go)
         {
-            renderer.sharedMaterial = s_SpriteLitDefault;
-        }
-    }
+            SpriteRenderer[] spriteRenderers = go.GetComponentsInChildren<SpriteRenderer>(true);
 
-    static void UpgradeMaterial(Material mat)
-    {
-        if (mat.shader.name == "Sprites/Default")
-        {
-            mat.shader = s_SpriteLitDefault.shader;
-        }
-    }
-
-    [MenuItem("Edit/Render Pipeline/2D Renderer/Upgrade Scene to 2D Renderer")]
-    static void UpgradeSceneTo2DRenderer()
-    {
-        GameObject[] gameObjects = GameObject.FindObjectsOfType<GameObject>();
-        if (gameObjects != null && gameObjects.Length > 0)
-        {
-            foreach (GameObject go in gameObjects)
+            bool upgraded = false;
+            foreach (SpriteRenderer renderer in spriteRenderers)
             {
-                UpgradeGameObject(go);
+                if (renderer.sharedMaterial != null && renderer.sharedMaterial.shader.name == "Sprites/Default")
+                {
+                    renderer.sharedMaterial = s_SpriteLitDefault;
+                    upgraded = true;
+                }
+            }
+
+            if (upgraded)
+            {
+                Debug.Log(go.name + " was upgraded.", go);
+                EditorSceneManager.MarkSceneDirty(go.scene);
             }
         }
-    }
 
-    [MenuItem("Edit/Render Pipeline/2D Renderer/Upgrade Project to 2D Renderer")]
-    static void UpgradeProjectTo2DRenderer()
-    {
-        ProcessAssetDatabaseObjects<GameObject>("t: Prefab", UpgradeGameObject);
-        ProcessAssetDatabaseObjects<Material>("t: Material", UpgradeMaterial);
-        AssetDatabase.SaveAssets();
+        static void UpgradeMaterial(Material mat)
+        {
+            if (mat.shader.name == "Sprites/Default")
+            {
+                mat.shader = s_SpriteLitDefault.shader;
+            }
+        }
+
+        [MenuItem("Edit/Render Pipeline/LWRP 2D Renderer/Upgrade Scene to 2D Renderer")]
+        static void UpgradeSceneTo2DRenderer()
+        {
+            if (!EditorUtility.DisplayDialog("2D Renderer Upgrader", "The upgrade will change the material references of Sprite Renderers in currently open scene(s) to a lit material. You can't undo this operation. Make sure you save the scene(s) before proceeding.", "Proceed", "Cancel"))
+                return;
+
+            GameObject[] gameObjects = Object.FindObjectsOfType<GameObject>();
+            if (gameObjects != null && gameObjects.Length > 0)
+            {
+                foreach (GameObject go in gameObjects)
+                {
+                    UpgradeGameObject(go);
+                }
+            }
+        }
+
+        [MenuItem("Edit/Render Pipeline/LWRP 2D Renderer/Upgrade Project to 2D Renderer")]
+        static void UpgradeProjectTo2DRenderer()
+        {
+            if (!EditorUtility.DisplayDialog("2D Renderer Upgrader", "The upgrade will search for all prefabs in your project that use Sprite Renderers and change the material references of those Sprite Renderers to a lit material. You can't undo this operation. It's highly recommended to backup your project before proceeding.", "Proceed", "Cancel"))
+                return;
+
+            ProcessAssetDatabaseObjects<GameObject>("t: Prefab", UpgradeGameObject);
+            //ProcessAssetDatabaseObjects<Material>("t: Material", UpgradeMaterial);
+            AssetDatabase.SaveAssets();
+            Resources.UnloadUnusedAssets();
+        }
     }
 }
