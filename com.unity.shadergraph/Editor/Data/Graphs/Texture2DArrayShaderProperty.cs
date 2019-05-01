2bf205b761d1bb7e43083182c6e7e5dbb9866524
Matt Dean <matthewdean3d@gmail.com>
2019-05-01 16:51:01 -0700

    Backport: Fix FB#1141553 - Sampler State properties are treated as duplicate when parameters are different (#3331)
    * Fix FB#1141553
    
    * Update CHANGELOG.md

--- com.unity.shadergraph/Editor/Data/Graphs/Texture2DArrayShaderProperty.cs
+++ com.unity.shadergraph/Editor/Data/Graphs/Texture2DArrayShaderProperty.cs
@@ -43,6 +43,11 @@ namespace UnityEditor.ShaderGraph
             get { return true; }
         }
 
+        public override bool isRenamable
+        {
+            get { return true; }
+        }
+
         public override string GetPropertyBlockString()
         {
             var result = new StringBuilder();
