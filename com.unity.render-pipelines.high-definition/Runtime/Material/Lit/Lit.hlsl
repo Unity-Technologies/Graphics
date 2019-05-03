6338fb544f8c17bd879a9a3d24074b4374b44747
Jesse Barker <jesseb@unity3d.com>
2019-05-03 14:38:47 -0700

    Revert the Inf protection when computing energy compensation

--- com.unity.render-pipelines.high-definition/Runtime/Material/Lit/Lit.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Material/Lit/Lit.hlsl
@@ -1052,7 +1052,7 @@ PreLightData GetPreLightData(float3 V, PositionInputs posInput, inout BSDFData b
     // We deem the intensity difference of a couple of percent for high values of roughness
     // to not be worth the cost of another precomputed table.
     // Note: this formulation bakes the BSDF non-symmetric!
-    preLightData.energyCompensation = 1.0 / max(specularReflectivity, 0.01) - 1.0;
+    preLightData.energyCompensation = 1.0 / specularReflectivity - 1.0;
 #else
     preLightData.energyCompensation = 0.0;
 #endif // LIT_USE_GGX_ENERGY_COMPENSATION
