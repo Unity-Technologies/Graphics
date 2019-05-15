f7b55f3f090085ce654df92e50258d49bef33e53
Julien Fryer <julienf@unity3d.com>
2019-05-15 17:17:30 +0200

    Add exposure weight to HDRP lit outputs

--- com.unity.visualeffectgraph/Editor/Models/Contexts/Implementations/HDRP/VFXAbstractParticleHDRPLitOutput.cs
+++ com.unity.visualeffectgraph/Editor/Models/Contexts/Implementations/HDRP/VFXAbstractParticleHDRPLitOutput.cs
@@ -154,6 +154,12 @@ namespace UnityEditor.VFX
             public Color emissiveColor = Color.black;
         }
 
+        public class ExposureWeightProperties
+        {
+            [Tooltip("The proportion of emission to expose"), Range(0, 1)]
+            public float exposureWeight = 1.0f;
+        }
+
         protected override IEnumerable<VFXPropertyWithValue> inputProperties
         {
             get
@@ -183,6 +189,9 @@ namespace UnityEditor.VFX
 
                 if (((colorMode & ColorMode.Emissive) == 0) && useEmissive)
                     properties = properties.Concat(PropertiesFromType("EmissiveColorProperties"));
+
+                if ((colorMode & ColorMode.Emissive) != 0 || useEmissive || useEmissiveMap)
+                    properties = properties.Concat(PropertiesFromType("ExposureWeightProperties"));
 
                 return properties;
             }
@@ -244,6 +253,9 @@ namespace UnityEditor.VFX
 
             if (((colorMode & ColorMode.Emissive) == 0) && useEmissive)
                 yield return slotExpressions.First(o => o.name == "emissiveColor");
+
+            if ((colorMode & ColorMode.Emissive) != 0 || useEmissive || useEmissiveMap)
+                yield return slotExpressions.First(o => o.name == "exposureWeight");
         }
 
         public override IEnumerable<string> additionalDefines
