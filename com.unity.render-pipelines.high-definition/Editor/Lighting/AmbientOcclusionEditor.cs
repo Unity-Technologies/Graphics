22df49d497f21e9faea002c65d2addcac45c54f3
anisunity <42026998+anisunity@users.noreply.github.com>
2019-05-17 18:23:35 +0200

    Moving ray tracing parameters to Volume system, HDAddditionalLightData and fixing compilation errors (#3658)
    * Correcting Raytracing Errors due to depth beging a texture array
    Moving Ambient occlusion parameters to the volume system
    Adding the tier option in the HDRP Asset
    
    * Indenting parameters of raytraced ambient occlusion
    Moving reflection parameters from raytracing environement to volume system
    Supporting the reflect sky parameter in raytraced reflections
    Splitting raytraced reflections in tiers
    
    * Moving Area Shadow parameters from environement to hdadditionalLightData
    
    * Fixing radius used for shadow filtering
    
    * Moving Ray Tracing Resources into a different scriptable object
    
    * Supporting properly the hot reload for the ray tracing resource

--- com.unity.render-pipelines.high-definition/Editor/Lighting/AmbientOcclusionEditor.cs
+++ com.unity.render-pipelines.high-definition/Editor/Lighting/AmbientOcclusionEditor.cs
@@ -8,16 +8,57 @@ namespace UnityEditor.Experimental.Rendering.HDPipeline
     [VolumeComponentEditor(typeof(AmbientOcclusion))]
     public class AmbientOcclusionEditor : VolumeComponentEditor
     {
+        SerializedDataParameter m_Intensity;
+        SerializedDataParameter m_ThicknessModifier;
+        SerializedDataParameter m_DirectLightingStrength;
+
+        SerializedDataParameter m_EnableRaytracing;
+        SerializedDataParameter m_RayLength;
+        SerializedDataParameter m_EnableFilter;
+        SerializedDataParameter m_NumSamples;
+        SerializedDataParameter m_FilterRadius;
+
+        public override void OnEnable()
+        {
+            var o = new PropertyFetcher<AmbientOcclusion>(serializedObject);
+
+            m_Intensity = Unpack(o.Find(x => x.intensity));
+            m_ThicknessModifier = Unpack(o.Find(x => x.thicknessModifier));
+            m_DirectLightingStrength = Unpack(o.Find(x => x.directLightingStrength));
+
+            m_EnableRaytracing = Unpack(o.Find(x => x.enableRaytracing));
+            m_RayLength = Unpack(o.Find(x => x.rayLength));
+            m_EnableFilter = Unpack(o.Find(x => x.enableFilter));
+            m_NumSamples = Unpack(o.Find(x => x.numSamples));
+            m_FilterRadius = Unpack(o.Find(x => x.filterRadius));
+        }
+
         public override void OnInspectorGUI()
         {
-            base.OnInspectorGUI();
-
             if (!(GraphicsSettings.renderPipelineAsset as HDRenderPipelineAsset)
                 ?.currentPlatformRenderPipelineSettings.supportSSAO ?? false)
             {
                 EditorGUILayout.Space();
                 EditorGUILayout.HelpBox("The current HDRP Asset does not support Ambient Occlusion.", MessageType.Error, wide: true);
+                return;
             }
+
+            PropertyField(m_Intensity);
+            PropertyField(m_ThicknessModifier);
+            PropertyField(m_DirectLightingStrength);
+
+#if ENABLE_RAYTRACING
+            PropertyField(m_EnableRaytracing);
+            if (m_EnableRaytracing.overrideState.boolValue && m_EnableRaytracing.value.boolValue)
+            {
+                EditorGUI.indentLevel++;
+                PropertyField(m_RayLength);
+                PropertyField(m_EnableFilter);
+                PropertyField(m_NumSamples);
+                PropertyField(m_FilterRadius);
+                EditorGUI.indentLevel--;
+            }
+#endif
         }
     }
 }
