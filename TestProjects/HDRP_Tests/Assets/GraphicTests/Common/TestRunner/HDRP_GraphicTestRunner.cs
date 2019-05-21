80899c65358a86cfef354ebac285a39766d876af
Roberto Bertini Renzetti (Unity) <38924810+roberto-renzetti-unity@users.noreply.github.com>
2019-05-21 02:03:47 -0700

    Added check for GCAlloc when rendering what's on camera, based on CHECK_ALLOCATIONS_WHEN_RENDERING define (#3670)

--- TestProjects/HDRP_Tests/Assets/GraphicTests/Common/TestRunner/HDRP_GraphicTestRunner.cs
+++ TestProjects/HDRP_Tests/Assets/GraphicTests/Common/TestRunner/HDRP_GraphicTestRunner.cs
@@ -48,6 +48,21 @@ public class HDRP_GraphicTestRunner
         {
             // Standard Test
             ImageAssert.AreEqual(testCase.ReferenceImage, camera, (settings != null)?settings.ImageComparisonSettings:null);
+
+#if CHECK_ALLOCATIONS_WHEN_RENDERING
+            // Does it allocate memory when it renders what's on camera?
+            bool allocatesMemory = false;
+            try
+            {
+                ImageAssert.AllocatesMemory(camera, 512, 512); // 512 used for height and width to render
+            }
+            catch (AssertionException)
+            {
+                allocatesMemory = true;
+            }
+            if (allocatesMemory)
+                Assert.Fail("Allocated memory when rendering what is on camera");
+#endif
         }
         else
         {
