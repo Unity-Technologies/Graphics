384fb4ac78e992d5190d63fc67552566a1526a34
Kink3d <matthewdean3d@gmail.com>
2019-06-03 10:43:29 -0700

    Fix Property node name

--- com.unity.shadergraph/Editor/Data/Nodes/Input/PropertyNode.cs
+++ com.unity.shadergraph/Editor/Data/Nodes/Input/PropertyNode.cs
@@ -11,6 +11,7 @@ namespace UnityEditor.ShaderGraph
     {
         public PropertyNode()
         {
+            name = "Property";
             UpdateNodeAfterDeserialization();
         }
         
