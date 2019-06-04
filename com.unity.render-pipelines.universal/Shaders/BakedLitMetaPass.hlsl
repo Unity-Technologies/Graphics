fa52a490b5389fb6bd2265f9935ccf53b4bc9574
Tim Cooper <stramit@gmail.com>
2019-06-04 11:41:37 +0200

    Refactoring

---
+++ com.unity.render-pipelines.universal/Shaders/BakedLitMetaPass.hlsl
@@ -0,0 +1,23 @@
+#ifndef LIGHTWEIGHT_BAKEDLIT_META_PASS_INCLUDED
+#define LIGHTWEIGHT_BAKEDLIT_META_PASS_INCLUDED
+
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/MetaInput.hlsl"
+
+Varyings LightweightVertexMeta(Attributes input)
+{
+    Varyings output;
+    output.positionCS = MetaVertexPosition(input.positionOS, input.uv1, input.uv2,
+        unity_LightmapST, unity_DynamicLightmapST);
+    output.uv = TRANSFORM_TEX(input.uv0, _BaseMap);
+    return output;
+}
+
+half4 LightweightFragmentMetaBakedLit(Varyings input) : SV_Target
+{
+    MetaInput metaInput = (MetaInput)0;
+    metaInput.Albedo = _BaseColor.rgb * SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, input.uv).rgb;
+
+    return MetaFragment(metaInput);
+}
+
+#endif
