c47497301583a92832b7a5fdb0a3dc92024ea0e3
Tim Cooper <stramit@gmail.com>
2019-06-06 15:37:25 +0200

    Missed a graph node

--- com.unity.shadergraph/Editor/Importers/ShaderGraphImporter.cs
+++ com.unity.shadergraph/Editor/Importers/ShaderGraphImporter.cs
@@ -9,7 +9,7 @@ using UnityEditor.Graphing.Util;
 
 namespace UnityEditor.ShaderGraph
 {
-    [ScriptedImporter(28, Extension, 3)]
+    [ScriptedImporter(30, Extension, 3)]
     class ShaderGraphImporter : ScriptedImporter
     {
         public const string Extension = "shadergraph";
