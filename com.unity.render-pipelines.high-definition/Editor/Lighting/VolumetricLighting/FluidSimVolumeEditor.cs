4249fc2bcba58a01492902ab4815995f2b02a80a
Seongdae Kim <37893618+Seongdae@users.noreply.github.com>
2019-06-07 16:51:24 +0900

    WIP for fluid sim editor

--- com.unity.render-pipelines.high-definition/Editor/Lighting/VolumetricLighting/FluidSimVolumeEditor.cs
+++ com.unity.render-pipelines.high-definition/Editor/Lighting/VolumetricLighting/FluidSimVolumeEditor.cs
@@ -1,3 +1,4 @@
+using UnityEditorInternal;
 using UnityEngine.Experimental.Rendering.HDPipeline;
 
 namespace UnityEditor.Experimental.Rendering.HDPipeline
@@ -6,5 +7,34 @@ namespace UnityEditor.Experimental.Rendering.HDPipeline
     [CustomEditor(typeof(FluidSimVolume))]
     public class FluidSimVolumeEditor : Editor
     {
+        internal const EditMode.SceneViewEditMode k_EditShape = EditMode.SceneViewEditMode.ReflectionProbeBox;
+        internal const EditMode.SceneViewEditMode k_EditBlend = EditMode.SceneViewEditMode.GridBox;
+
+        static HierarchicalBox s_ShapeBox;
+        internal static HierarchicalBox s_BlendBox;
+
+        void OnEnable()
+        {
+            //m_SerializedDensityVolume = new SerializedDensityVolume(serializedObject);
+
+            if (s_ShapeBox == null || s_ShapeBox.Equals(null))
+            {
+                s_ShapeBox = new HierarchicalBox(DensityVolumeUI.Styles.k_GizmoColorBase, DensityVolumeUI.Styles.k_BaseHandlesColor);
+                s_ShapeBox.monoHandle = false;
+            }
+            if (s_BlendBox == null || s_BlendBox.Equals(null))
+            {
+                s_BlendBox = new HierarchicalBox(DensityVolumeUI.Styles.k_GizmoColorBase, InfluenceVolumeUI.k_HandlesColor, parent: s_ShapeBox);
+            }
+        }
+
+        public override void OnInspectorGUI()
+        {
+            //serializedObject.Update();
+
+            //FluidSimVolumeUI.Inspector.Draw(m_SerializedDensityVolume, this);
+
+            //m_SerializedDensityVolume.Apply();
+        }
     }
 }
