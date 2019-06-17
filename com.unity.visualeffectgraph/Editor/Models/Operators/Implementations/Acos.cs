f7e5059c57ec81a3c1535ba660f6a6c04b17bb58
Arno Galvez <arno.galve@unity3d.com>
2019-06-17 18:01:22 +0200

    Added nodes Atan, Asin, Acos

--- com.unity.visualeffectgraph/Editor/Models/Operators/Implementations/Acos.cs
+++ com.unity.visualeffectgraph/Editor/Models/Operators/Implementations/Acos.cs
@@ -1,21 +1,20 @@
 namespace UnityEditor.VFX.Operator
 {
     [VFXInfo(category = "Math/Trigonometry")]
-    class Atan2 : VFXOperatorNumericUniform
+    class Acos : VFXOperatorNumericUniform
     {
         public class InputProperties
         {
-            public float x = 1.0f;
-            public float y = 0.0f;
+            public float x = 0.0f;
         }
 
-        protected override sealed string operatorName { get { return "Atan2"; } }
+        protected override sealed string operatorName { get { return "Acos"; } }
 
         protected override sealed ValidTypeRule typeFilter { get { return ValidTypeRule.allowEverythingExceptInteger; } }
 
         protected override sealed VFXExpression[] BuildExpression(VFXExpression[] inputExpression)
         {
-            return new[] { new VFXExpressionATan2(inputExpression[1], inputExpression[0]) };
+            return new[] { new VFXExpressionACos(inputExpression[0]) };
         }
     }
 }
