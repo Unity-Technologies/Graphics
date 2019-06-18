samermurad/ScriptableRenderPipeline

commit d55189c623e8162786c614f0981d4417dce4884a

fix ExecuteCaptureAction incorrect blit material source
This commit changes the source of the blit material texture dimension used to
copy the final pass to a temp render target, this has changed from
a Tex2D to a Tex2DArray between version 2019.1 to 2019.2

com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/Camera/HDCamera.cs
@@ -872,7 +872,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
             // Since recorder does not know about this, we need to send a texture of the right size.
             cmd.GetTemporaryRT(m_RecorderTempRT, actualWidth, actualHeight, 0, FilterMode.Point, input.rt.graphicsFormat);
 
-            var blitMaterial = HDUtils.GetBlitMaterial(TextureDimension.Tex2D);
+            var blitMaterial = HDUtils.GetBlitMaterial(TextureDimension.Tex2DArray);
 
             var rtHandleScale = RTHandles.rtHandleProperties.rtHandleScale;
             Vector2 viewportScale = new Vector2(rtHandleScale.x, rtHandleScale.y);

Unity-Technologies/Graphics

commit 454fd0b9e193abf24c63698293d2d5fd42dcb6c8

Query the RenderTexture.dimension to set the blit material source inside ExecuteCaptureAction
This commit fix a problem introduced with commit d55189c623e8162786c614f0981d4417dce4884a
where we set the BlitMaterial to TextureDimension.Tex2DArray
On OSX this is not the case, the input render texture dimension is still a Tex2D on OSX.

This commit correctly query the dimension of the input texture.

com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/Camera/HDCamera.cs
@@ -872,7 +872,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
             // Since recorder does not know about this, we need to send a texture of the right size.
             cmd.GetTemporaryRT(m_RecorderTempRT, actualWidth, actualHeight, 0, FilterMode.Point, input.rt.graphicsFormat);
 
-            var blitMaterial = HDUtils.GetBlitMaterial(TextureDimension.Tex2DArray);
+            var blitMaterial = HDUtils.GetBlitMaterial(input.rt.dimension);
 
             var rtHandleScale = RTHandles.rtHandleProperties.rtHandleScale;
             Vector2 viewportScale = new Vector2(rtHandleScale.x, rtHandleScale.y);