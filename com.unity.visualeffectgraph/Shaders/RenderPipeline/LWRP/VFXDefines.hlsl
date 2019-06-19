a47e50468d9db69807313552839dee24b8da293f
Fabien Houlmann <44069206+fabien-unity@users.noreply.github.com>
2019-06-19 08:53:20 -0400

    added shader macros for XR single-pass instancing (#3723)

--- com.unity.visualeffectgraph/Shaders/RenderPipeline/LWRP/VFXDefines.hlsl
+++ com.unity.visualeffectgraph/Shaders/RenderPipeline/LWRP/VFXDefines.hlsl
@@ -1,4 +1,4 @@
-#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
+#include "Packages/com.unity.render-pipelines.lightweight/ShaderLibrary/Core.hlsl"
 #if defined(FOG_LINEAR) || defined(FOG_EXP) || defined(FOG_EXP2)
 #define USE_FOG 1
 #endif
