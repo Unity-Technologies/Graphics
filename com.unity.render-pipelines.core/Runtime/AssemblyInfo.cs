f6accd4bb49579bf01590f4cd3021e83d7110352
Felipe Lira <felipe@unity3d.com>
2019-06-24 11:22:13 +0200

    Moved DebugWindow from HDRP to Core.

---
+++ com.unity.render-pipelines.core/Runtime/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Runtime")]
+[assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Editor")]
+[assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Editor.Tests")]
