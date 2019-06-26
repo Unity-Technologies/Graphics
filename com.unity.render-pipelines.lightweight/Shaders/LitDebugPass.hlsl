5bcc376398e07b38144abad46491ad73c053b1ca
Wilfrid Girard <wilfrid@unity3d.com>
2019-06-26 09:05:22 +0200

    Remove emissive component from the "Reflections..." debug views

--- com.unity.render-pipelines.lightweight/Shaders/LitDebugPass.hlsl
+++ com.unity.render-pipelines.lightweight/Shaders/LitDebugPass.hlsl
@@ -248,11 +248,13 @@ half4 LitPassFragment(Varyings input) : SV_Target
     {
         surfaceData.albedo = half3(0.0h, 0.0h, 0.0h);
         surfaceData.smoothness = 1.0;
+        surfaceData.emission = half3(0.0h, 0.0h, 0.0h);
     }
     if (_DebugLightingIndex == DEBUG_LIGHTING_REFLECTIONS_WITH_SMOOTHNESS)
     {
         surfaceData.albedo = half3(0.0h, 0.0h, 0.0h);
         surfaceData.metallic = 1.0;
+        surfaceData.emission = half3(0.0h, 0.0h, 0.0h);
     }
 
     InputData inputData;
