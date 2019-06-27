bf43cfa10e3f5e0cebe409ca7db9fda55328bb71
Wilfrid Girard <wilfrid@unity3d.com>
2019-06-27 15:54:10 +0200

    Add debug view for Metallic material component

--- com.unity.render-pipelines.lightweight/ShaderLibrary/Debugging.hlsl
+++ com.unity.render-pipelines.lightweight/ShaderLibrary/Debugging.hlsl
@@ -15,7 +15,7 @@
 #define DEBUG_NORMAL_TANGENT_SPACE 9
 #define DEBUG_LIGHTING_COMPLEXITY 10
 #define DEBUG_LOD 11
-
+#define DEBUG_METALLIC 12
 int _DebugMaterialIndex;
 
 #define DEBUG_LIGHTING_SHADOW_CASCADES 1
@@ -292,6 +292,10 @@ bool CalculateColorForDebugMaterial(InputData inputData, SurfaceData surfaceData
 
         case DEBUG_LOD:
             surfaceData.albedo = GetLODDebugColor().rgb;
+            return true;
+
+        case DEBUG_METALLIC:
+            color.rgb = surfaceData.metallic.xxx;
             return true;
 
         default:
