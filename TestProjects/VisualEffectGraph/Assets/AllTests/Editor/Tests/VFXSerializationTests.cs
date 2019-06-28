7783a91988e0bb0cbea95367858fd7a70cd75e09
Julien Fryer <julienf@unity3d.com>
2019-06-28 17:41:03 +0200

    Workaround to fix serialization tests

--- TestProjects/VisualEffectGraph/Assets/AllTests/Editor/Tests/VFXSerializationTests.cs
+++ TestProjects/VisualEffectGraph/Assets/AllTests/Editor/Tests/VFXSerializationTests.cs
@@ -10,6 +10,7 @@ using UnityEditor.VFX;
 
 
 using Object = UnityEngine.Object;
+using System.IO;
 
 namespace UnityEditor.VFX.Test
 {
@@ -175,7 +176,9 @@ namespace UnityEditor.VFX.Test
 
                 asset = null;
                 EditorUtility.UnloadUnusedAssetsImmediate();
-                AssetDatabase.CopyAsset(kTempAssetPathA, kTempAssetPathB);
+                //AssetDatabase.CopyAsset(kTempAssetPathA, kTempAssetPathB); // TODO Deactivated because a regression makes it fail when load the copy
+                File.Copy(kTempAssetPathA, kTempAssetPathB);
+
                 if (asset != null)
                     AssetDatabase.RemoveObjectFromAsset(asset);
             }
