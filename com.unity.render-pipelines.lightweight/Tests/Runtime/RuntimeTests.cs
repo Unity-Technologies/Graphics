fbc6659bbe7015df7abf0d45ea86613796dcb8b4
Yuanxing Cai <yuanxing@unity3d.com>
2019-07-02 17:27:26 +0800

    Fix failing tests for Yamato (#3772)
    * Made two Light2D culling tests run in a cleaner way.
    
    * Updated the two runtime tests to use a TestFixture so that cleanup code is called even when a test fails and throws an exception.
    
    * Updated conflicting or missing meta files.

--- com.unity.render-pipelines.lightweight/Tests/Runtime/RuntimeTests.cs
+++ com.unity.render-pipelines.lightweight/Tests/Runtime/RuntimeTests.cs
@@ -5,27 +5,41 @@ using UnityEngine;
 using UnityEngine.Rendering.LWRP;
 using UnityEngine.Rendering;
 
+[TestFixture]
 class RuntimeTests
 {
+    GameObject go;
+    Camera camera;
+    RenderPipelineAsset prevAsset;
+    LightweightRenderPipelineAsset asset;
+
+    [SetUp]
+    public void Setup()
+    {
+        go = new GameObject();
+        camera = go.AddComponent<Camera>();
+        prevAsset = GraphicsSettings.renderPipelineAsset;
+        asset = ScriptableObject.CreateInstance<LightweightRenderPipelineAsset>();
+    }
+
+    [TearDown]
+    public void Cleanup()
+    {
+        GraphicsSettings.renderPipelineAsset = prevAsset;
+        Object.DestroyImmediate(asset);
+        Object.DestroyImmediate(go);
+    }
+
     // When LWRP pipeline is active, lightsUseLinearIntensity must match active color space.
     [UnityTest]
     public IEnumerator PipelineHasCorrectColorSpace()
     {
-        GameObject go = new GameObject();
-        Camera camera = go.AddComponent<Camera>();
-        RenderPipelineAsset prevAsset = GraphicsSettings.renderPipelineAsset;
-
-        LightweightRenderPipelineAsset asset = ScriptableObject.CreateInstance<LightweightRenderPipelineAsset>();
         GraphicsSettings.renderPipelineAsset = asset;
         camera.Render();
         yield return null;
         
         Assert.AreEqual(QualitySettings.activeColorSpace == ColorSpace.Linear, GraphicsSettings.lightsUseLinearIntensity,
             "GraphicsSettings.lightsUseLinearIntensity must match active color space.");
-
-        GraphicsSettings.renderPipelineAsset = prevAsset;
-        ScriptableObject.DestroyImmediate(asset);
-        GameObject.DestroyImmediate(go);
     }
 
     // When switching to LWRP it sets "LightweightPipeline" as global shader tag.
@@ -33,10 +47,6 @@ class RuntimeTests
     [UnityTest]
     public IEnumerator PipelineSetsAndRestoreGlobalShaderTagCorrectly()
     {
-        GameObject go = new GameObject();
-        Camera camera = go.AddComponent<Camera>();
-        RenderPipelineAsset prevAsset = GraphicsSettings.renderPipelineAsset;
-        LightweightRenderPipelineAsset asset = ScriptableObject.CreateInstance<LightweightRenderPipelineAsset>();
         GraphicsSettings.renderPipelineAsset = asset;
         camera.Render();
         yield return null;
@@ -48,9 +58,5 @@ class RuntimeTests
         yield return null;
 
         Assert.AreEqual("", Shader.globalRenderPipeline, "Render Pipeline shader tag is not restored.");
-        GraphicsSettings.renderPipelineAsset = prevAsset;
-
-        ScriptableObject.DestroyImmediate(asset);
-        GameObject.DestroyImmediate(go);
     }
 }
