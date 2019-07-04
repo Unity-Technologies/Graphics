86d79cc915487febc679c7b2628428055d03b71c
anisunity <42026998+anisunity@users.noreply.github.com>
2019-07-04 06:19:37 -0700

    Adding a standard Lit model that is our go to for indirect lighting for tier1 (#3866)

--- com.unity.render-pipelines.high-definition/Runtime/Material/Lit/Lit.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Material/Lit/Lit.hlsl
@@ -68,15 +68,6 @@ TEXTURE2D_X(_ShadowMaskTexture); // Alias for shadow mask, so we don't need to k
 
 #define HAS_REFRACTION (defined(_REFRACTION_PLANE) || defined(_REFRACTION_SPHERE))
 
-// Enum for materialFeatureId (only use for encode/decode GBuffer)
-#define GBUFFER_LIT_STANDARD         0
-// we have not enough space (3bit) to store mat feature to have SSS and Transmission as bitmask, such why we have all variant
-#define GBUFFER_LIT_SSS              1
-#define GBUFFER_LIT_TRANSMISSION     2
-#define GBUFFER_LIT_TRANSMISSION_SSS 3
-#define GBUFFER_LIT_ANISOTROPIC      4
-#define GBUFFER_LIT_IRIDESCENCE      5 // TODO
-
 #define SUPPORTS_RAYTRACED_AREA_SHADOWS (SHADEROPTIONS_RAYTRACING && (SHADERPASS == SHADERPASS_DEFERRED_LIGHTING))
 
 // It is safe to include this file after the G-Buffer macros above.
