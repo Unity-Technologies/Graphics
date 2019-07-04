31c9fe2326b2e9bc0ac486e6475d0710ec4f21bf
Antoine Lelievre <alelievr@student.42.fr>
2019-07-04 13:57:50 +0200

    Add Light Scripting API and deprecate the AdditionalShadowData component (#3940)
    * Begin to clean the HDAdditionalLight script API
    
    * Finished to change fields in properties for HDAdditionalLightData
    
    * Continue to add functions for the light scripting API
    
    * Begin to add graphic test for light scripting API
    
    * Finished first draft of the light scripting API
    
    * Change log to LogError
    
    * Continue to add more docs and scripting functions
    
    * Update the test scene for light scripting
    
    * Moved shadow properties to HDAdditionalLightData
    
    * Added HDLightType extension
    
    * Marked obsolete the AdditionalShadowDarta component and moved its properties to HDAdditionalLightData
    
    * Added a function to remove the hd light components
    
    * Add shadow biases in the light data init function
    
    * Added doc for customSpotLightShadowCone and useCustomSpotLightShadowCone
    
    * Added light scripting reference image
    
    * Fix lightloop compilation error

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/Utility/HDUtils.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/Utility/HDUtils.cs
@@ -13,7 +13,6 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
         static public HDAdditionalReflectionData s_DefaultHDAdditionalReflectionData { get { return ComponentSingleton<HDAdditionalReflectionData>.instance; } }
         static public HDAdditionalLightData s_DefaultHDAdditionalLightData { get { return ComponentSingleton<HDAdditionalLightData>.instance; } }
         static public HDAdditionalCameraData s_DefaultHDAdditionalCameraData { get { return ComponentSingleton<HDAdditionalCameraData>.instance; } }
-        static public AdditionalShadowData s_DefaultAdditionalShadowData { get { return ComponentSingleton<AdditionalShadowData>.instance; } }
 
         static Texture3D m_ClearTexture3D;
         public static Texture3D clearTexture3D
