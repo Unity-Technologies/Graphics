e1089621db4786146824035f1594c80fe278dbd3
sebastien lagarde <sebastien@unity3d.com>
2019-07-05 14:31:51 +0200

    updte version in script for locql pqckqge

--- Tools/CreateLocalPackages.cmd
+++ Tools/CreateLocalPackages.cmd
@@ -1,5 +1,5 @@
-set version_preview=6.8.1-preview
-set version=6.8.1
+set version_preview=6.9.0-preview
+set version=6.9.0
 call CreateOneLocalPackage.bat com.unity.render-pipelines.core %version%
 call CreateOneLocalPackage.bat com.unity.render-pipelines.high-definition %version_preview%
 call CreateOneLocalPackage.bat com.unity.render-pipelines.lightweight %version%
