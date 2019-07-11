4ed1a897bcb34eb59a368b79ccfd51262daa9ad9
unity-cwells <47453839+unity-cwells@users.noreply.github.com>
2019-07-11 11:39:53 +0100

    Backport specular fp16 overflow fix on Switch (#4020)

--- com.unity.render-pipelines.lightweight/CHANGELOG.md
+++ com.unity.render-pipelines.lightweight/CHANGELOG.md
@@ -6,8 +6,8 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [6.9.1] - 2019-07-25
 
-Version Updated
-The version number for this package has increased due to a version update of a related graphics package.
+### Fixed
+- Fixed fp16 overflow in Switch in specular calculation
 
 ## [6.9.0] - 2019-07-05
 ### Fixed
--- com.unity.render-pipelines.lightweight/ShaderLibrary/Lighting.hlsl
+++ com.unity.render-pipelines.lightweight/ShaderLibrary/Lighting.hlsl
@@ -287,10 +287,10 @@ half3 DirectBDRF(BRDFData brdfData, half3 normalWS, half3 lightDirectionWS, half
     half LoH2 = LoH * LoH;
     half specularTerm = brdfData.roughness2 / ((d * d) * max(0.1h, LoH2) * brdfData.normalizationTerm);
 
-    // on mobiles (where half actually means something) denominator have risk of overflow
+    // On platforms where half actually means something, the denominator has a risk of overflow
     // clamp below was added specifically to "fix" that, but dx compiler (we convert bytecode to metal/gles)
     // sees that specularTerm have only non-negative terms, so it skips max(0,..) in clamp (leaving only min(100,...))
-#if defined (SHADER_API_MOBILE)
+#if defined (SHADER_API_MOBILE) || defined (SHADER_API_SWITCH)
     specularTerm = specularTerm - HALF_MIN;
     specularTerm = clamp(specularTerm, 0.0, 100.0); // Prevent FP16 overflow on mobiles
 #endif
