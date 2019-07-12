61c2a1c783d9ec838c81e0291a05d2727cb46b60
Remi Slysz <40034005+RSlysz@users.noreply.github.com>
2019-07-12 10:39:52 +0200

    Hdrp/move menu renderpipeline (#4065)
    * Move Render Pipeline Menus
    
    * Update documentation

--- com.unity.render-pipelines.high-definition/Editor/DefaultScene/HDRPWizard.cs
+++ com.unity.render-pipelines.high-definition/Editor/DefaultScene/HDRPWizard.cs
@@ -201,7 +201,7 @@ namespace UnityEditor.Rendering.HighDefinition
             GetSupportedGraphicsAPIs = getSupportedGraphicsAPIsLambda.Compile();
         }
 
-        [MenuItem("Window/General/HD Render Pipeline Wizard", priority = 2000)]
+        [MenuItem("Window/Render Pipeline/HD Render Pipeline Wizard", priority = 5)]
         static void OpenWindow()
         {
             GetWindow<HDWizard>("Render Pipeline Wizard");
