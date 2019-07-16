d04978da25d1753b0c92ddaed6a979106a0ace66
Sophia <16596228+sophiaaar@users.noreply.github.com>
2019-07-16 15:23:21 +0200

    Adding files needed by the package validation suite (#4134)

---
+++ com.unity.render-pipelines.lightweight/Tests/Editor/LightweightEditorTests.cs
@@ -0,0 +1,13 @@
+using System;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+class LightweightEditorTests
+{
+    [Test]
+    public void DummyTest()
+    {
+        Assert.IsTrue(true);
+    }
+}
