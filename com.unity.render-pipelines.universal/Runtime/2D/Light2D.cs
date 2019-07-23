5663871394029fdb5ceae20e95d679fe06f8637c
Yuanxing Cai <yuanxing@unity3d.com>
2019-07-23 15:39:46 +0800

    Moved the stub Light2D class to a new file.

--- com.unity.render-pipelines.universal/Runtime/2D/Light2D.cs
+++ com.unity.render-pipelines.universal/Runtime/2D/Light2D.cs
@@ -2,18 +2,9 @@ using System;
 using System.Collections.Generic;
 using UnityEngine.Serialization;
 using UnityEngine.Rendering;
-using UnityEngine.Scripting.APIUpdating;
 #if UNITY_EDITOR
 using UnityEditor.Experimental.SceneManagement;
 #endif
-
-namespace UnityEngine.Experimental.Rendering.LWRP
-{
-    [Obsolete("LWRP -> Universal (UnityUpgradable) -> UnityEngine.Experimental.Rendering.Universal.Light2D", true)]
-    public class Light2D
-    {}
-}
-
 
 namespace UnityEngine.Experimental.Rendering.Universal
 {
