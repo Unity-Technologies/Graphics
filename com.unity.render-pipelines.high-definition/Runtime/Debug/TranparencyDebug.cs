7b0387f4737ca5f162169bc8fbb4f6207a2ec809
Arno Galvez <arno.galvez@unity3d.com>
2019-07-30 17:43:49 +0200

    Untracked file
    Added TransparencyDebug

---
+++ com.unity.render-pipelines.high-definition/Runtime/Debug/TranparencyDebug.cs
@@ -0,0 +1,9 @@
+using System;
+namespace UnityEngine.Rendering.HighDefinition
+{
+    [Serializable]
+    public class TransparencyDebugSettings
+    {
+        public float maxPixelCost = 10.0f;
+    }
+}
