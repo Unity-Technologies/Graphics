unity-gdc-public/ScriptableRenderPipeline

Various occlusions

com.unity.render-pipelines.high-definition/Runtime/Material/Lit/LitBuiltinData.hlsl
@@ -1,9 +1,12 @@
 #include "Packages/com.unity.render-pipelines.high-definition/Runtime/Material/BuiltinUtilities.hlsl"
-
-void GetBuiltinData(FragInputs input, float3 V, inout PositionInputs posInput, SurfaceData surfaceData, float alpha, float3 bentNormalWS, float depthOffset, out BuiltinData builtinData)
+//forest-begin: sky occlusion
+void GetBuiltinData(FragInputs input, float3 V, inout PositionInputs posInput, SurfaceData surfaceData, float alpha, float3 bentNormalWS, float depthOffset, float grassOcclusion, out BuiltinData builtinData)
+//forest-end:
 {
     // For back lighting we use the oposite vertex normal
-    InitBuiltinData(posInput, alpha, bentNormalWS, -input.worldToTangent[2], input.texCoord1, input.texCoord2, builtinData);
+//forest-begin: sky occlusion / Tree Occlusion
+    InitBuiltinData(posInput, alpha, bentNormalWS, -input.worldToTangent[2], input.texCoord1, input.texCoord2, surfaceData.skyOcclusion, grassOcclusion, surfaceData.treeOcclusion, builtinData);
+//forest-end:
 
     builtinData.emissiveColor = _EmissiveColor * lerp(float3(1.0, 1.0, 1.0), surfaceData.baseColor.rgb, _AlbedoAffectEmissive);
 #ifdef _EMISSIVE_COLOR_MAP

Unity-Technologies/Graphics

com.unity.render-pipelines.high-definition/Runtime/Material/Lit/LitBuiltinData.hlsl
@@ -1,9 +1,12 @@
 #include "Packages/com.unity.render-pipelines.high-definition/Runtime/Material/BuiltinUtilities.hlsl"
-
-void GetBuiltinData(FragInputs input, float3 V, inout PositionInputs posInput, SurfaceData surfaceData, float alpha, float3 bentNormalWS, float depthOffset, out BuiltinData builtinData)
+//forest-begin: sky occlusion
+void GetBuiltinData(FragInputs input, float3 V, inout PositionInputs posInput, SurfaceData surfaceData, float alpha, float3 bentNormalWS, float depthOffset, float grassOcclusion, out BuiltinData builtinData)
+//forest-end:
 {
     // For back lighting we use the oposite vertex normal
-    InitBuiltinData(posInput, alpha, bentNormalWS, -input.tangentToWorld[2], input.texCoord1, input.texCoord2, builtinData);
+//forest-begin: sky occlusion / Tree Occlusion
+    InitBuiltinData(posInput, alpha, bentNormalWS, -input.tangentToWorld[2], input.texCoord1, input.texCoord2, surfaceData.skyOcclusion, grassOcclusion, surfaceData.treeOcclusion, builtinData);
+//forest-end:
 
     builtinData.emissiveColor = _EmissiveColor * lerp(float3(1.0, 1.0, 1.0), surfaceData.baseColor.rgb, _AlbedoAffectEmissive);
 #ifdef _EMISSIVE_COLOR_MAP