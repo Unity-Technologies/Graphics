b8d224651d45e1473d02f453b819fed1fb03385e
Felipe Lira <felipedrl@gmail.com>
2019-08-01 16:40:02 +0200

    UniversalRP increase light limits (#4074)
    *   Added support for storing lights in StructuredBuffer
    
    *  Added StructuredBuffer shadow data.
    
    *  fixed light indexing
    
    *  Moved buffer data to rendering utils.
    
    *  Moved data to Singleton shader data. Disposing buffers correctly.
    
    *  increased light per-object limits
    
    *  Pushing interim changes to additional shadow casting lights
    
    *  added shader data
    
    *  added test scene
    
    *  fixed shadow caster indices
    
    *  Added DisposeBuffer helper to dispose ComputeBuffers.
    
    *  minor tweaks to optimize light looping.
    
    *  optimized shadow constant setup to only setup when required by the shader
    
    *  Fixed shadow issues.
    
    *  Optimized data storate for shadow matrices and strenghts in cpu.
    
    *  optimized shadow atlas packing.
    
    *  Minor improvements
    
    *  using [flatten] to remove branching.
    
    *  conditional are now attributes defined in gles2
    
    *  StructuredBuffers not being used in mobile. Bumped light limits for mobile: per-camera 256, per-object 8.
    
    *  fixed indexing issue when main light was present.
    
    *  skip all directional lights as they are not packed in shadowmap atlas.
    
    *  added support to 64 shadows in uniform array path
    
    *  Fixed compiler error
    
    *  Implement the shader cast for GetPerObjectLightIndex as well so we get 8 indices in custom user shaders using that.
    
    *  addressed PR feedbacks
    
    *  added changelog
    
    *  Optimized usage for shadowmap atlas. We don't take space for invalid shadow casting lights (ubo path) and also reduce shadowmap altas by half when only needing half or less max allocated slices.
    
    *  made shaderdata class internal
    
    *  64 per-object lights exceeds the 8k SP limit. Reduced to 32.
    
    *  Added autogenerated ShaderTypes.
    
    *  Forgot to adjust max pixels lights in c#.
    
    *  removed CBUFFER declaration that are not required.
    
    *  Doing bit mask ops to sample ubo lighting. gles2 fallback to 4 per-object lights as bitfields are not supported. added comments.
    
    *  updated changelog.
    
    *  Added tests for per-object realtime lights and shadows. (8 lights)
    
    *  Added reference images for Windows and enabled test 08 and 09.
    
    *  updated scene 008 and 009 to wait a few frames.
    
    *  Disabled StructuredBuffer path in OpenGL Core.
    
    *  fix issue that pipeline tried to create a buffer with 0 lights upon starting unity. updates comments.
    
    * Update universalrp-builtin-feature-comparison.md
    
    Update  light limits in Docs
    
    * Update universalrp-builtin-feature-comparison.md
    
    Update light limits as per review by Felipe
    
    *  Fixed issue that causes UBO path to not work when increasing light buffer.
    
    *  Split data of SSBO vs UBO to avoid data copies.
    
    *  Don't set unity_LightIndices when using structuredbuffe.r
    
    *  Fixed crash when using SSBO. Switch uses UBO performance tests show it's faster.
    
    *  Added support for packing shadow soft data for lights.
    
    *  updated test images
    
    *   Removed dx11 from set of platforms supporting SSBO due to not being able to check if using DX11 feature level dx10.
    
    *  fixed issues that caused pipeline to access invalid light indices.
    
    *   updated limits on non-mobile to 256. (due to switch and d3d10 not supporting SSBO)
    
    *  Updated test to catch light index issue fixed earlier.
    
    *  updated test images for MacOs

--- com.unity.render-pipelines.universal/Runtime/UniversalRenderPipeline.cs
+++ com.unity.render-pipelines.universal/Runtime/UniversalRenderPipeline.cs
@@ -64,17 +64,37 @@ namespace UnityEngine.Rendering.Universal
         }
 
         // Amount of Lights that can be shaded per object (in the for loop in the shader)
-        // This uses unity_4LightIndices to store an array of 4 light indices
         public static int maxPerObjectLights
         {
-            get => 4;
+            // No support to bitfield mask and int[] in gles2. Can't index fast more than 4 lights.
+            // Check Lighting.hlsl for more details.
+            get => (SystemInfo.graphicsDeviceType == GraphicsDeviceType.OpenGLES2) ? 4 : 8;
         }
 
-        // Light data is stored in a constant buffer (uniform array)
-        // This value has to match MAX_VISIBLE_LIGHTS in Input.hlsl
+        // These limits have to match same limits in Input.hlsl
+        const int k_MaxVisibleAdditionalLightsSSBO  = 256;
+        const int k_MaxVisibleAdditionalLightsUBO   = 32;
         public static int maxVisibleAdditionalLights
         {
-            get => 16;
+            get
+            {
+                // There are some performance issues by using SSBO in mobile.
+                // Also some GPUs don't supports SSBO in vertex shader.
+                if (RenderingUtils.useStructuredBuffer)
+                    return k_MaxVisibleAdditionalLightsSSBO;
+
+                // We don't use SSBO in D3D because we can't figure out without adding shader variants if platforms is D3D10.
+                // We don't use SSBO on Nintendo Switch as UBO path is faster.
+                // However here we use same limits as SSBO path. 
+                var deviceType = SystemInfo.graphicsDeviceType;
+                if (deviceType == GraphicsDeviceType.Direct3D11 || deviceType == GraphicsDeviceType.Direct3D12 ||
+                    deviceType == GraphicsDeviceType.Switch)
+                    return k_MaxVisibleAdditionalLightsSSBO;
+
+                // We use less limits for mobile as some mobile GPUs have small SP cache for constants
+                // Using more than 32 might cause spilling to main memory.
+                return k_MaxVisibleAdditionalLightsUBO;
+            }
         }
 
         public static UniversalRenderPipelineAsset asset
@@ -122,6 +142,7 @@ namespace UnityEngine.Rendering.Universal
             base.Dispose(disposing);
             Shader.globalRenderPipeline = "";
             SupportedRenderingFeatures.active = new SupportedRenderingFeatures();
+            ShaderData.instance.Dispose();
 
 #if UNITY_EDITOR
             SceneViewDrawMode.ResetDrawMode();
@@ -474,7 +495,13 @@ namespace UnityEngine.Rendering.Universal
             var configuration = PerObjectData.ReflectionProbes | PerObjectData.Lightmaps | PerObjectData.LightProbe | PerObjectData.LightData | PerObjectData.OcclusionProbe;
 
             if (additionalLightsCount > 0)
-                configuration |= PerObjectData.LightIndices;
+            {
+                configuration |= PerObjectData.LightData;
+
+                // In this case we also need per-object indices (unity_LightIndices)
+                if (!RenderingUtils.useStructuredBuffer)
+                    configuration |= PerObjectData.LightIndices;
+            }
 
             return configuration;
         }
