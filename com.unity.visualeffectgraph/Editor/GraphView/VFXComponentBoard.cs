0c3b4b5888311d2264da3dcda08f0fcc9f267d71
iTris666 <tristan@unity3d.com>
2019-08-07 16:46:04 +0200

    Resources folder content moved to Editor Default Resources (#4241)

--- com.unity.visualeffectgraph/Editor/GraphView/VFXComponentBoard.cs
+++ com.unity.visualeffectgraph/Editor/GraphView/VFXComponentBoard.cs
@@ -151,7 +151,7 @@ namespace UnityEditor.VFX.UI
         public VFXComponentBoard(VFXView view)
         {
             m_View = view;
-            var tpl = Resources.Load<VisualTreeAsset>("uxml/VFXComponentBoard");
+            var tpl = VFXView.LoadUXML("VFXComponentBoard");
 
             tpl.CloneTree(contentContainer);
 
@@ -570,7 +570,7 @@ namespace UnityEditor.VFX.UI
 
                 foreach (var added in eventNames.Except(m_Events.Keys).ToArray())
                 {
-                    var tpl = Resources.Load<VisualTreeAsset>("uxml/VFXComponentBoard-event");
+                    var tpl = VFXView.LoadUXML("VFXComponentBoard-event");
 
                     tpl.CloneTree(m_EventsContainer);
 
