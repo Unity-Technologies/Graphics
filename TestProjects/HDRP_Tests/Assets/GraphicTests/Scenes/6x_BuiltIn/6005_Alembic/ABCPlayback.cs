9390894c4d8ddfbcc7bc2765e409a6bfff555f6f
JansMargevics <jans.margevics@unity3d.com>
2019-08-09 15:25:20 +0300

    Add alembic + additional motion vector data test

---
+++ TestProjects/HDRP_Tests/Assets/GraphicTests/Scenes/6x_BuiltIn/6005_Alembic/ABCPlayback.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Formats.Alembic;
+using UnityEngine.Formats.Alembic.Sdk;
+using UnityEngine.Formats.Alembic.Importer;
+
+public class ABCPlayback : MonoBehaviour
+{
+    public GameObject[] ListOfThings;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject obj in ListOfThings )
+        {
+            //obj.GetComponent<AlembicStreamPlayer>().Time = 1;
+        }
+        
+    }
+}
