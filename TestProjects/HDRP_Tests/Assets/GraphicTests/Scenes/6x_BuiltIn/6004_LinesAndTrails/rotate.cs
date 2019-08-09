bbc0e23dab60a402672b41a05ee47fbeda985e97
JansMargevics <jans.margevics@unity3d.com>
2019-08-09 12:49:38 +0300

    Added a test for Line and Trail Renderers

---
+++ TestProjects/HDRP_Tests/Assets/GraphicTests/Scenes/6x_BuiltIn/6004_LinesAndTrails/rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(0,0,90);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
