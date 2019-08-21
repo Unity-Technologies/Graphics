0a1aafa121124a204142f36ab6523293145bda7d
Kay Chang <kaychang@unity3d.com>
2019-08-21 11:59:36 -0700

    Moved to temporary allocations.

--- com.unity.render-pipelines.universal/Runtime/DeferredPass.cs
+++ com.unity.render-pipelines.universal/Runtime/DeferredPass.cs
@@ -11,15 +11,20 @@ namespace UnityEngine.Rendering.Universal
 {
     internal struct BitArray
     {
-        uint[] m_Mem; // ulong not supported in il2cpp???
+        NativeArray<uint> m_Mem; // ulong not supported in il2cpp???
         int m_BitCount;
         int m_IntCount;
 
-        public BitArray(int bitCount)
+        public BitArray(int bitCount, Allocator allocator, NativeArrayOptions options = NativeArrayOptions.ClearMemory)
         {
             m_BitCount = bitCount;
             m_IntCount = (bitCount + 31) >> 5;
-            m_Mem = new uint[m_IntCount];
+            m_Mem = new NativeArray<uint>(m_IntCount, allocator, options);
+        }
+
+        public void Dispose()
+        {
+            m_Mem.Dispose();
         }
 
         public void Clear()
@@ -51,6 +56,17 @@ namespace UnityEngine.Rendering.Universal
         public float radius;
         // Index into visibleLight native array.
         public ushort visLightIndex;
+    }
+
+    internal struct DrawCall
+    {
+        public ComputeBuffer tileIDBuffer;
+        public ComputeBuffer _TileRelLightBuffer;
+        public ComputeBuffer _PointLightBuffer;
+        public ComputeBuffer _RelLightIndexBuffer;
+        public int tileCount;
+        public int lightCount;
+        public int relLightIndices;
     }
 
     // Render all tiled-based deferred lights.
@@ -116,7 +132,7 @@ namespace UnityEngine.Rendering.Universal
                 PrecomputeTiles(out m_PreTiles, renderingData.cameraData.camera.projectionMatrix, renderingData.cameraData.camera.orthographic, m_RenderWidth, m_RenderHeight);
             }
 
-            m_Tiles = new NativeArray<ushort>(m_TileXCount * m_TileYCount * m_TileSize, Allocator.Temp);
+            m_Tiles = new NativeArray<ushort>(m_TileXCount * m_TileYCount * m_TileSize, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
 
             NativeArray<PrePointLight> prePointLights;
             PrecomputeLights(out prePointLights, renderingData.lightData.visibleLights, renderingData.cameraData.camera.worldToCameraMatrix);
@@ -164,19 +180,18 @@ namespace UnityEngine.Rendering.Universal
                 int relLightIndices = 0;
 
                 // Acceleration structure to quickly find if a light has already been added to the uniform block data for the current draw call.
-                ushort[] trimmedLights = new ushort[maxLightPerTile]; // TODO use temp allocation
-                ushort[] visLightToRelLights = new ushort[renderingData.lightData.visibleLights.Length];
-                BitArray usedLights = new BitArray(renderingData.lightData.visibleLights.Length);
-                usedLights.Clear();
+                NativeArray<ushort> trimmedLights = new NativeArray<ushort>(maxLightPerTile, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
+                NativeArray<ushort> visLightToRelLights = new NativeArray<ushort>(renderingData.lightData.visibleLights.Length, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
+                BitArray usedLights = new BitArray(renderingData.lightData.visibleLights.Length, Allocator.Temp, NativeArrayOptions.ClearMemory);
 
-                uint[] tileIDBuffer = new uint[m_MaxTilesPerBatch]; // TODO use temp allocation
-                uint[] tileRelLightBuffer = new uint[m_MaxTilesPerBatch];
+                NativeArray<uint> tileIDBuffer = new NativeArray<uint>(m_MaxTilesPerBatch, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
+                NativeArray<uint> tileRelLightBuffer = new NativeArray<uint>(m_MaxTilesPerBatch, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
 #if UNITY_SUPPORT_STRUCT_IN_CBUFFER
-                PointLightData[] pointLightBuffer = new PointLightData[m_MaxPointLightPerBatch];
+                NativeArray<PointLightData> pointLightBuffer = new NativeArray<PointLightData>(m_MaxPointLightPerBatch, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
 #else
-                Vector4[] pointLightBuffer = new Vector4[m_MaxPointLightPerBatch * sizeof_vec4_PointLightData];
+                NativeArray<Vector4> pointLightBuffer = new NativeArray<Vector4>(m_MaxPointLightPerBatch * sizeof_vec4_PointLightData, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
 #endif
-                uint[] relLightIndexBuffer = new uint[m_MaxRelLightIndicesPerBatch];
+                NativeArray<uint> relLightIndexBuffer = new NativeArray<uint>(m_MaxRelLightIndicesPerBatch, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
 
                 for (int j = 0; j < m_TileYCount; ++j)
                 {
@@ -278,7 +293,16 @@ namespace UnityEngine.Rendering.Universal
                 }
 
                 DeferredShaderData.instance.ResetBuffers();
+
+                trimmedLights.Dispose();
+                visLightToRelLights.Dispose();
+                usedLights.Dispose();
+                tileIDBuffer.Dispose();
+                tileRelLightBuffer.Dispose();
+                pointLightBuffer.Dispose();
+                relLightIndexBuffer.Dispose();
             }
+
             context.ExecuteCommandBuffer(cmd);
             CommandBufferPool.Release(cmd);
         }
@@ -359,9 +383,7 @@ namespace UnityEngine.Rendering.Universal
         void PrecomputeLights(out NativeArray<PrePointLight> prePointLights, NativeArray<VisibleLight> visibleLights, Matrix4x4 view)
         {
             int lightTypeCount = 5;
-            int[] lightCount = new int[lightTypeCount];
-            for (int i = 0; i < lightCount.Length; ++i)
-                lightCount[i] = 0;
+            NativeArray<int> lightCount = new NativeArray<int>(lightTypeCount, Allocator.Temp, NativeArrayOptions.ClearMemory);
 
             // Count the number of lights per type.
             for (ushort visLightIndex = 0; visLightIndex < visibleLights.Length; ++visLightIndex)
@@ -370,7 +392,7 @@ namespace UnityEngine.Rendering.Universal
                 ++lightCount[(int)vl.lightType];
             }
 
-            prePointLights = new NativeArray<PrePointLight>(lightCount[(int)LightType.Point], Allocator.Temp);
+            prePointLights = new NativeArray<PrePointLight>(lightCount[(int)LightType.Point], Allocator.Temp, NativeArrayOptions.UninitializedMemory);
 
             for (int i = 0; i < lightCount.Length; ++i)
                 lightCount[i] = 0;
@@ -391,6 +413,7 @@ namespace UnityEngine.Rendering.Universal
                     prePointLights[i] = ppl;
                 }
             }
+            lightCount.Dispose();
         }
 
         void CullLights(NativeArray<PrePointLight> visPointLights)
@@ -429,7 +452,7 @@ namespace UnityEngine.Rendering.Universal
             }
         }
 
-        int TrimLights(ushort[] trimmedLights, NativeArray<ushort> tiles, int offset, int lightCount, BitArray usedLights)
+        int TrimLights(NativeArray<ushort> trimmedLights, NativeArray<ushort> tiles, int offset, int lightCount, BitArray usedLights)
         {
             int trimCount = 0;
             for (int i = 0; i < lightCount; ++i)
@@ -450,14 +473,10 @@ namespace UnityEngine.Rendering.Universal
             pointLightBuffer[storeIndex].Color = visibleLights[index].light.color;
         }
 #else
-        void StorePointLightData(Vector4[] pointLightBuffer, int storeIndex, NativeArray<VisibleLight> visibleLights, int index)
+        void StorePointLightData(NativeArray<Vector4> pointLightBuffer, int storeIndex, NativeArray<VisibleLight> visibleLights, int index)
         {
             Vector3 wsPos = visibleLights[index].light.transform.position;
-            pointLightBuffer[storeIndex].x = wsPos.x;
-            pointLightBuffer[storeIndex].y = wsPos.y;
-            pointLightBuffer[storeIndex].z = wsPos.z;
-            pointLightBuffer[storeIndex].w = visibleLights[index].range;
-
+            pointLightBuffer[                           0 + storeIndex] = new Vector4(wsPos.x, wsPos.y, wsPos.z, visibleLights[index].range);
             pointLightBuffer[this.m_MaxPointLightPerBatch + storeIndex] = visibleLights[index].light.color;
         }
 #endif
@@ -467,18 +486,6 @@ namespace UnityEngine.Rendering.Universal
             return ((s + alignment - 1) / alignment) * alignment;
         }
 
-        /*
-        static Vector4 MakePlane(float x1, float y1, float z1, float x2, float y2, float z2)
-        {
-            Vector3 v0 = new Vector3(x1, y1, z1);
-            Vector3 v1 = new Vector3(x2, y2, z2);
-            Vector3 n = Vector3.Cross(v0, v1);
-            n = Vector3.Normalize(n);
-
-            // The planes pass all by the origin.
-            return new Vector4(n.x, n.y, n.z, 0.0f);
-        }
-        */
         static Vector4 MakePlane(Vector3 pb, Vector3 pc)
         {
             Vector3 v0 = pb;
