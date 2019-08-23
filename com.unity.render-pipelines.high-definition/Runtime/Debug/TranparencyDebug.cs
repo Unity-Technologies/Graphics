d4836c943fc84d9d1edfdb3a826f06b05d9b852a
Arno Galvez <arno.galvez@unity3d.com>
2019-08-23 10:05:56 +0200

    Modifications according to review

--- com.unity.render-pipelines.high-definition/Runtime/Debug/TranparencyDebug.cs
+++
@@ -1,9 +0,0 @@
-using System;
-namespace UnityEngine.Rendering.HighDefinition
-{
-    [Serializable]
-    public class TransparencyDebugSettings
-    {
-        public float maxPixelCost = 10.0f;
-    }
-}
