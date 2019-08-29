f613ea304e961ec5d4410a133904c31b7f45a47a
Julien Fryer <julienf@unity3d.com>
2019-08-29 17:20:34 +0200

    Fix camera buffer access

--- com.unity.visualeffectgraph/Editor/Expressions/VFXExpressionLoadTexture.cs
+++ com.unity.visualeffectgraph/Editor/Expressions/VFXExpressionLoadTexture.cs
@@ -24,4 +24,23 @@ namespace UnityEditor.VFX
             return string.Format("LoadTexture(VFX_SAMPLER({0}),(int3){1})", parents[0], parents[1]);
         }
     }
+
+    class VFXExpressionLoadTexture2DArray : VFXExpression
+    {
+        public VFXExpressionLoadTexture2DArray() : this(VFXTexture2DArrayValue.Default, VFXValue<Vector4>.Default)
+        {
+        }
+
+        public VFXExpressionLoadTexture2DArray(VFXExpression texture, VFXExpression location)
+            : base(Flags.InvalidOnCPU, new VFXExpression[2] { texture, location })
+        { }
+
+        sealed public override VFXExpressionOperation operation { get { return VFXExpressionOperation.None; } }
+        sealed public override VFXValueType valueType { get { return VFXValueType.Float4; } }
+
+        public sealed override string GetCodeString(string[] parents)
+        {
+            return string.Format("LoadTexture(VFX_SAMPLER({0}),(int4){1})", parents[0], parents[1]);
+        }
+    }
 }
