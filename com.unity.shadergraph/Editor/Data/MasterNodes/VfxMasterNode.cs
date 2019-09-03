abf5bfc1a9666f7986515da1e97d6a71db734b7b
Peter Bay Bastian <peter.bastian@gmail.com>
2019-09-03 11:23:17 +0200

    Add VFX master to search menu

--- com.unity.shadergraph/Editor/Data/MasterNodes/VfxMasterNode.cs
+++ com.unity.shadergraph/Editor/Data/MasterNodes/VfxMasterNode.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor.Graphing;
@@ -11,6 +12,8 @@ using UnityEditor.Graphing.Util;
 
 namespace UnityEditor.ShaderGraph
 {
+    [Serializable]
+    [Title("Master", "Visual Effect")]
     sealed class VfxMasterNode : MasterNode, IMayRequirePosition
     {
         const string BaseColorSlotName = "Base Color";
