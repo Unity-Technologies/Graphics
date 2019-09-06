2938e962124787d57eaa0eb3c05f7a7db27ed398
sebastienlagarde <sebastien@unity3d.com>
2019-09-06 11:22:48 +0200

    Fix custom postprocess initiatilization of volume (#4521)

--- com.unity.render-pipelines.high-definition/Runtime/PostProcessing/CustomPostProcessing/CustomPostProcessVolumeComponent.cs
+++ com.unity.render-pipelines.high-definition/Runtime/PostProcessing/CustomPostProcessing/CustomPostProcessVolumeComponent.cs
@@ -37,6 +37,8 @@ namespace UnityEngine.Rendering.HighDefinition
         {
             if (isInitialized)
                 Cleanup();
+
+            isInitialized = false;
         }
 
         internal void SetupIfNeeded()
