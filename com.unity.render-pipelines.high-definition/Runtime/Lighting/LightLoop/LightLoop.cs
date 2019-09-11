81ff969e342937711e02b582b1be3f31aaadac7b
FrancescoC-unity <43168857+FrancescoC-unity@users.noreply.github.com>
2019-09-11 13:50:45 +0100

    Fix for area lights counted as point lights for max count (#4578)
    * Don't count point lights and area lights together
    
    * Update changelog

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/LightLoop.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/LightLoop.cs
@@ -2032,27 +2032,6 @@ namespace UnityEngine.Rendering.HighDefinition
                         HDRenderPipeline.EvaluateGPULightType(light.lightType, additionalData.lightTypeExtent, additionalData.spotLightShape, 
                                                                 ref lightCategory, ref gpuLightType, ref lightVolumeType);
 
-                        bool typeIsFull = false;
-                        if (lightCategory == LightCategory.Punctual)
-                        {
-                            if (gpuLightType == GPULightType.Directional)
-                            {
-                                typeIsFull = (directionalLightcount >= m_MaxDirectionalLightsOnScreen);
-                            }
-                            else
-                            {
-                                typeIsFull = (punctualLightcount >= m_MaxPunctualLightsOnScreen);
-                            }
-                        }
-                        else
-                        {
-                            typeIsFull = (areaLightCount >= m_MaxAreaLightsOnScreen);
-                        }
-
-                        // If no slot is left for the target light type, we continue
-                        if (typeIsFull)
-                            continue;
-
                         if (hasDebugLightFilter
                             && !debugLightFilter.IsEnabledFor(gpuLightType, additionalData.spotLightShape))
                             continue;
@@ -2097,6 +2076,20 @@ namespace UnityEngine.Rendering.HighDefinition
 
                         var light = cullResults.visibleLights[lightIndex];
                         var lightComponent = light.light;
+
+                        switch(lightCategory)
+                        {
+                            case LightCategory.Punctual:
+                                if (punctualLightcount >= m_MaxPunctualLightsOnScreen)
+                                    continue;
+                                break;
+                            case LightCategory.Area:
+                                if (areaLightCount >= m_MaxAreaLightsOnScreen)
+                                    continue;
+                                break;
+                            default:
+                                break;
+                        }
 
                         m_enableBakeShadowMask = m_enableBakeShadowMask || IsBakedShadowMaskLight(lightComponent);
 
