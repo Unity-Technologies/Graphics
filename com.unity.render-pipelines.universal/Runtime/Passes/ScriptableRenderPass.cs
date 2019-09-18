470c98318fdf366840373bab4a8cd7285f767a49
Martin Thorzen <35328557+martint-unity@users.noreply.github.com>
2019-09-18 10:34:27 +0200

    UI UX Camera for Universal (#4589)
    * added UI for the different cameras in LWRP
    
    * Added gizmo icons and reorderable list
    
    * Added functionality to the reorderable list
    
    Added ifdef unity 19.2
    
    * Change game to base and added foldable headers
    
    * clean-up and  add null check to stop an errror
    
    * Updated the tooltip
    
    * Update tooltips
    
    Updated tooltips for Headers and a few features in camera inspector
    
    *  updated from LWRP -> UniversalRP
    
    *  CameraData is now initialized before starting rendering a camera.
    
    *  added stacked rendering.
    
    *  udpated FPS cam scene to test stacking.
    
    *  Fixed issue with scene view and stacking.
    
    *  Fix redundant UI caused by merge.
    
    * Added camera stacking icons and PP awareness
    
    * Post process icon and Cleaned up code
    
    * Removed volume setting. Not needed anymore
    
    * ui-ux changes for camera
    
    * removed legacy folder
    
    * removed legacy code
    
    * Removed offscreen camera added output targets
    
    * Updated with fixes for issues found
    
    * Fixed issue with accidently rendering empty gizmo

--- com.unity.render-pipelines.universal/Runtime/Passes/ScriptableRenderPass.cs
+++ com.unity.render-pipelines.universal/Runtime/Passes/ScriptableRenderPass.cs
@@ -29,7 +29,7 @@ namespace UnityEngine.Rendering.Universal
     }
 
     /// <summary>
-    /// <c>ScriptableRenderPass</c> implements a logical rendering pass that can be used to extend LWRP renderer.
+    /// <c>ScriptableRenderPass</c> implements a logical rendering pass that can be used to extend Universal RP renderer.
     /// </summary>
     [MovedFrom("UnityEngine.Rendering.LWRP")] public abstract class ScriptableRenderPass
     {
