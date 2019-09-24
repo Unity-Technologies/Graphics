3ae5f753b277b6994b4cb487328679120c360937
Alex Lindman <alexandral@unity3d.com>
2019-09-24 09:42:28 -0700

    add descriptors

---
+++ com.unity.shadergraph/Editor/Internal/Data/StructDescriptor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace UnityEditor.ShaderGraph.Internal
+{
+    public struct StructDescriptor
+    {
+        public string name;
+        public SubscriptDescriptor[] subscripts;
+    }
+}
---
+++ com.unity.shadergraph/Editor/Internal/Data/SubscriptDescriptor.cs
@@ -0,0 +1,59 @@
+﻿namespace UnityEditor.ShaderGraph.Internal
+{
+    public class SubscriptDescriptor : IField
+    {
+        public enum SubscriptOptions
+        {
+            Static = 0,
+            Optional = 1 << 0, 
+            Generated = 1 << 1
+        }
+
+        public string tag { get; }
+        public string name { get; }
+        public string define { get; }
+        public string type { get; }
+        public string semantic { get; }
+        public string preprocessor { get; }
+        public SubscriptOptions subcriptOptions { get; }
+
+        public SubscriptDescriptor(string tag, string name, string define, ShaderValueType type,
+                string semantic = "", string preprocessor = "", SubscriptOptions subcriptOptions = SubscriptOptions.Static)
+        {
+            this.tag = tag;
+            this.name = name;
+            this.define = define;
+            this.type = type.ToString(); //TODO: implement proper to string get here
+            this.semantic = semantic;            
+            this.preprocessor = preprocessor;
+            this.subcriptOptions = subcriptOptions;
+        }
+
+        public SubscriptDescriptor(string tag, string name, string define, string type,
+                string semantic = "", string preprocessor = "", SubscriptOptions subcriptOptions = SubscriptOptions.Static)
+        {
+            this.tag = tag;
+            this.name = name;
+            this.define = define;
+            this.type = type;
+            this.semantic = semantic;            
+            this.preprocessor = preprocessor;
+            this.subcriptOptions = subcriptOptions;
+        }
+
+        public bool hasPreprocessor()
+        {
+            return (this.preprocessor.Length > 0);
+        }
+
+        public bool hasSemantic()
+        {
+            return (this.semantic.Length > 0);
+        }
+
+        public bool hasFlag(SubscriptOptions options)
+        {
+            return (this.subcriptOptions & options) == options;
+        }
+    }
+}
