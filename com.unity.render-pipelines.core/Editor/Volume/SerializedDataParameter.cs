4a66843411f212e9e28d2abdb03780fa8ecc679d
Thomas <Chman@users.noreply.github.com>
2019-09-30 16:28:10 +0200

    First draft of xmldoc for the volume framework (#4635)
    * First draft of xmldoc for the volume framework
    
    * Update VolumeComponentEditor.cs
    
    * Update VolumeComponentListEditor.cs
    
    * Update VolumeParameterDrawer.cs
    
    * Update VolumeProfileFactory.cs
    
    * Update Volume.cs
    
    * Update VolumeComponent.cs
    
    * Update VolumeManager.cs
    
    * Update VolumeProfile.cs
    
    * Update VolumeParameter.cs
    
    * Update VolumeStack.cs
    
    * Added missing xmldoc
    
    * Update VolumeComponent.cs
    
    * Added missing xmldoc
    
    * Update VolumeComponentEditor.cs

--- com.unity.render-pipelines.core/Editor/Volume/SerializedDataParameter.cs
+++ com.unity.render-pipelines.core/Editor/Volume/SerializedDataParameter.cs
@@ -2,23 +2,42 @@ using System;
 using System.Linq;
 using System.Reflection;
 using UnityEngine.Assertions;
+using UnityEngine.Rendering;
 
 namespace UnityEditor.Rendering
 {
+    /// <summary>
+    /// A serialization wrapper for <see cref="VolumeParameter{T}"/>.
+    /// </summary>
     public sealed class SerializedDataParameter
     {
+        /// <summary>
+        /// The serialized property for <see cref="VolumeParameter.overrideState"/>.
+        /// </summary>
         public SerializedProperty overrideState { get; private set; }
+
+        /// <summary>
+        /// The serialized property for <see cref="VolumeParameter{T}.value"/>
+        /// </summary>
         public SerializedProperty value { get; private set; }
+
+        /// <summary>
+        /// A pre-fetched list of all the attributes applied on the <see cref="VolumeParameter{T}"/>.
+        /// </summary>
         public Attribute[] attributes { get; private set; }
+
+        /// <summary>
+        /// The actual type of the serialized <see cref="VolumeParameter{T}"/>.
+        /// </summary>
         public Type referenceType { get; private set; }
 
         SerializedProperty m_BaseProperty;
         object m_ReferenceValue;
 
-        public string displayName
-        {
-            get { return m_BaseProperty.displayName; }
-        }
+        /// <summary>
+        /// The generated display name of the <see cref="VolumeParameter{T}"/> for the inspector.
+        /// </summary>
+        public string displayName => m_BaseProperty.displayName;
 
         internal SerializedDataParameter(SerializedProperty property)
         {
@@ -43,12 +62,22 @@ namespace UnityEditor.Rendering
             m_ReferenceValue = obj;
         }
 
+        /// <summary>
+        /// Gets and casts an attribute applied on the base <see cref="VolumeParameter{T}"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public T GetAttribute<T>()
             where T : Attribute
         {
             return (T)attributes.FirstOrDefault(x => x is T);
         }
 
+        /// <summary>
+        /// Gets and casts the underlying reference of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type to cast to</typeparam>
+        /// <returns>The reference to the serialized <see cref="VolumeParameter{T}"/> type</returns>
         public T GetObjectRef<T>()
         {
             return (T)m_ReferenceValue;
