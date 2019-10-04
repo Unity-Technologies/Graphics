632ed14fdfbaa3ee212e8c1a01f21243c2f8e004
Antoine Lelievre <antoinel@unity3d.com>
2019-10-04 12:48:58 +0200

    Fix planar probe fov 0 breaking projection matrix (#4713)
    * Fixed 0 scale on planar probe causing invalid projection matrix
    
    * Updated changelog
    
    * Fov clamp
    
    * Fixed typo

--- com.unity.render-pipelines.high-definition/Runtime/Utilities/ProbeSettings.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Utilities/ProbeSettings.cs
@@ -159,11 +159,13 @@ namespace UnityEngine.Rendering.HighDefinition
             /// </summary>
             public FOVMode fieldOfViewMode;
             /// <summary>Value to use when FOV is fixed.</summary>
-            [Range(0, 180)]
+            [Range(0, 179f)]
             public float fixedValue;
             /// <summary>The automatic value of the FOV is multiplied by this factor at the end.</summary>
+            [Min(0)]
             public float automaticScale;
             /// <summary>The viewer's FOV is multiplied by this factor at the end.</summary>
+            [Min(0)]
             public float viewerScale;
         }
 
