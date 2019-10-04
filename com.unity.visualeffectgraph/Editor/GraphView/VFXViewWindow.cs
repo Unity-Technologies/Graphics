a9d37008ad2eaa0c1b364fcf658ef78b529eed6f
Tristan Genevet <tristan@unity3d.com>
2019-10-04 15:11:03 +0200

    more fixes.

--- com.unity.visualeffectgraph/Editor/GraphView/VFXViewWindow.cs
+++ com.unity.visualeffectgraph/Editor/GraphView/VFXViewWindow.cs
@@ -258,6 +258,9 @@ namespace  UnityEditor.VFX.UI
 
                         if (autoCompile && graph.IsExpressionGraphDirty())
                             AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(graphView.controller.model));
+                        else
+                            graph.RecompileIfNeeded(true, true);
+                        
                         controller.RecompileExpressionGraphIfNeeded();
                     }
                 }
