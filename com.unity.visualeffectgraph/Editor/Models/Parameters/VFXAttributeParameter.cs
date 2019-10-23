d09293b41ed1c0aae2aec42707cf195d6e72d547
Vlad Neykov <vlad@vladimirneykov.com>
2019-10-23 13:24:55 +0100

    Tooltips /Clean-up (#4805)
    * Tooltips for:
    Preferences / VFX
    Spawn, Initialize
    
    * First pass of Update block tooltips
    
    * Remaining Update contextual node tooltips
    
    * Outputs and output blocks
    
    * Added Cone and Torus types to ArcCone and ArcTorus types, updated all relevant gizmos and blocks. Added attribute parameter tooltips, wrapped up some lose ends (set curve/map blocks), first pass on node tooltips.
    
    * Icon for particle strips
    
    * Added tooltips for the remaining nodes, cleaned up a few tooltips.
    
    * Missing tooltip for Asset template
    
    * Renamed the manual integration blocks to Update Position / Rotation
    
    * Add FormerlySerializedAs for useTargetOffset
    
    * Revert VFXType & VFXUpdate (#4821)
    
    * Revert VFXType structure change
    
    * Custom inspector for VFXBasicUpdate : keep integration mode, display a toggle
    
    * Update changelog.md

--- com.unity.visualeffectgraph/Editor/Models/Parameters/VFXAttributeParameter.cs
+++ com.unity.visualeffectgraph/Editor/Models/Parameters/VFXAttributeParameter.cs
@@ -66,13 +66,13 @@ namespace UnityEditor.VFX
     [VFXInfo(category = "Attribute", variantProvider = typeof(AttributeVariant))]
     class VFXAttributeParameter : VFXOperator
     {
-        [VFXSetting(VFXSettingAttribute.VisibleFlags.InInspector), StringProvider(typeof(AttributeProvider))]
+        [VFXSetting(VFXSettingAttribute.VisibleFlags.InInspector), StringProvider(typeof(AttributeProvider)), Tooltip("Specifies which attribute to use.")]
         public string attribute = VFXAttribute.AllIncludingVariadicExceptWriteOnly.First();
 
-        [VFXSetting, Tooltip("Select the version of this parameter that is used.")]
+        [VFXSetting, Tooltip("Specifies which version of the parameter to use. It can return the current value, or the source value derived from a GPU event or a spawn attribute.")]
         public VFXAttributeLocation location = VFXAttributeLocation.Current;
 
-        [VFXSetting, Regex("[^x-zX-Z]", 3)]
+        [VFXSetting, Regex("[^x-zX-Z]", 3), Tooltip("Sets the axes and the order in which they are derived. The input can be only the letters x, y, and z, in any combination, up to a length of 3 (i.e. xyz).")]
         public string mask = "xyz";
 
         protected override IEnumerable<string> filteredOutSettings
