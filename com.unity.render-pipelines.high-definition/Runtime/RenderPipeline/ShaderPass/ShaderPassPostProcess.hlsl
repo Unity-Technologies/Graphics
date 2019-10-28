76092ce2df79454aa59b1010665cc0ece6572403
Paul Melamed <paulm@unity3d.com>
2019-10-28 13:47:27 -0700

    update to work for world view direction

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/ShaderPass/ShaderPassPostProcess.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/ShaderPass/ShaderPassPostProcess.hlsl
@@ -4,7 +4,6 @@
 
 #include "Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/ShaderPass/VertMesh.hlsl"
 
-
 PackedVaryingsType Vert(AttributesMesh inputMesh)
 {
     VaryingsType varyingsType;
@@ -12,52 +11,16 @@ PackedVaryingsType Vert(AttributesMesh inputMesh)
     return PackVaryingsType(varyingsType);
 }
 
-
-/*
-struct Attributes
-{
-    uint vertexID : SV_VertexID;
-    UNITY_VERTEX_INPUT_INSTANCE_ID
-};
-
-struct VSOutput
-{
-    float4 positionCS : SV_POSITION;
-    float2 texcoord   : TEXCOORD0;
-    UNITY_VERTEX_OUTPUT_STEREO
-};
-
-VSOutput Vert(Attributes input)
-{
-    VSOutput output;
-    UNITY_SETUP_INSTANCE_ID(input);
-    UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(output);
-    output.positionCS = GetFullScreenTriangleVertexPosition(input.vertexID);
-    output.texcoord = GetFullScreenTriangleTexCoord(input.vertexID) * _ScreenSize.xy;
-    return output;
-}
-
-void Frag(VSOutput input, out float4 outColor : SV_Target)
-{
-    UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
-    FragInputs fragInputs;
-    PositionInputs posInputs;
-    float3 V = float3(0, 0, 0);
-    SurfaceData surfaceData;
-    fragInputs.texCoord0.xy = input.texcoord;   
-    GetSurfaceData(fragInputs, V, posInputs, surfaceData);
-    outColor = surfaceData.output;    
-}
-
-*/
-
 void Frag(PackedVaryingsToPS packedInput, out float4 outColor : SV_Target)
 {
     UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(packedInput);
     FragInputs fragInputs = UnpackVaryingsMeshToFragInputs(packedInput.vmesh);
+
+    float depth = LoadCameraDepth(fragInputs.positionSS.xy);
+    PositionInputs posInputs = GetPositionInput(fragInputs.positionSS.xy, _ScreenSize.zw, depth, UNITY_MATRIX_I_VP, UNITY_MATRIX_V);
+
+    float3 V = GetWorldSpaceNormalizeViewDir(posInputs.positionWS);
     fragInputs.texCoord0.xy *= _ScreenSize.xy;
-    PositionInputs posInputs;
-    float3 V = float3(0, 0, 0);
     SurfaceData surfaceData;
     GetSurfaceData(fragInputs, V, posInputs, surfaceData);
     outColor = surfaceData.output;
