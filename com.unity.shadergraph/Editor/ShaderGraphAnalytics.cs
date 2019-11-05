7c360271cd90d37e23939b85c6f487c98e418e8f
martint-unity <martint@unity3d.com>
2019-11-05 10:47:55 +0100

    Started the analytics class

---
+++ com.unity.shadergraph/Editor/ShaderGraphAnalytics.cs
@@ -0,0 +1,14 @@
+using UnityEditor.Analytics;
+
+namespace UnityEditor.ShaderGraph
+{
+    class ShaderGraphAnalytics
+    {
+        static bool s_EnableAnalytics = false;
+
+        public static void EnableAnalytics()
+        {
+
+        }
+    }
+}
