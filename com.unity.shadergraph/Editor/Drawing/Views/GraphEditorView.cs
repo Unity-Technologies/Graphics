9c1e7537c44a01dc9d4b4e164a3d10e6a763a985
Martin Thorzen <35328557+martint-unity@users.noreply.github.com>
2019-11-05 15:05:23 +0100

    Group node fixes (#4625)
    * Fixed copy nodes inside group pastes properly
    
    * Fix for sticky notes in group
    
    * Updated changelog
    
    * Fixed the ungrouping of sticky note
    
    * Added ungroup as last contextual menu item
    
    * Updated with PR feedback
    
    * commit for safety
    
    Cleaned out and moved functionality from shadergroup into Material graphview
    
    * Added some shortcuts to the menus
    
    Fixed up the menu to look  better,
    
    * transparency added
    
    * Fixed so that group can move inside another group
    
    * removed grouping for none nodes, groups, props
    
    * reset selection if not already selected contextual

--- com.unity.shadergraph/Editor/Drawing/Views/GraphEditorView.cs
+++ com.unity.shadergraph/Editor/Drawing/Views/GraphEditorView.cs
@@ -326,11 +326,19 @@ namespace UnityEditor.ShaderGraph.Drawing
                 }
             }
 
-            if (evt.ctrlKey && evt.keyCode == KeyCode.G)
+            if (evt.actionKey && evt.keyCode == KeyCode.G)
             {
-                if (m_GraphView.selection.OfType<MaterialNodeView>().Any())
+                if (m_GraphView.selection.OfType<GraphElement>().Any())
                 {
                     m_GraphView.GroupSelection();
+                }
+            }
+
+            if (evt.actionKey && evt.keyCode == KeyCode.U)
+            {
+                if (m_GraphView.selection.OfType<GraphElement>().Any())
+                {
+                    m_GraphView.RemoveFromGroupNode();
                 }
             }
         }
@@ -787,7 +795,7 @@ namespace UnityEditor.ShaderGraph.Drawing
 
         void AddGroup(GroupData groupData)
         {
-            ShaderGroup graphGroup = new ShaderGroup(m_Graph);
+            ShaderGroup graphGroup = new ShaderGroup();
 
             graphGroup.userData = groupData;
             graphGroup.title = groupData.title;
