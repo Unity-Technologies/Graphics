93ea628022abdadde57527e149f103cd02094756
Fabien Houlmann <44069206+fabien-unity@users.noreply.github.com>
2019-11-05 12:30:06 -0500

    Add XR single-pass setting to HDRP asset (#4954)
    * add XR single-pass setting into HDRP asset
    
    * renormalize file for EOL

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/XR/XRSystem.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/XR/XRSystem.cs
@@ -95,7 +95,7 @@ namespace UnityEngine.Rendering.HighDefinition
             return maxViews;
         }
 
-        internal List<(Camera, XRPass)> SetupFrame(Camera[] cameras)
+        internal List<(Camera, XRPass)> SetupFrame(Camera[] cameras, bool singlePassAllowed)
         {
             bool xrSdkActive = RefreshXrSdk();
 
@@ -130,7 +130,7 @@ namespace UnityEngine.Rendering.HighDefinition
 
                     if (xrSdkActive)
                     {
-                        CreateLayoutFromXrSdk(camera);
+                        CreateLayoutFromXrSdk(camera, singlePassAllowed);
                     }
                     else
                     {
@@ -214,7 +214,7 @@ namespace UnityEngine.Rendering.HighDefinition
         }
 
 #if ENABLE_VR && ENABLE_XR_MODULE
-        void CreateLayoutFromXrSdk(Camera camera)
+        void CreateLayoutFromXrSdk(Camera camera, bool singlePassAllowed)
         {
             bool CanUseSinglePass(XRDisplaySubsystem.XRRenderPass renderPass)
             {
@@ -241,7 +241,7 @@ namespace UnityEngine.Rendering.HighDefinition
                 display.GetRenderPass(renderPassIndex, out var renderPass);
                 display.GetCullingParameters(camera, renderPass.cullingPassIndex, out var cullingParams);
 
-                if (CanUseSinglePass(renderPass))
+                if (singlePassAllowed && CanUseSinglePass(renderPass))
                 {
                     var xrPass = XRPass.Create(renderPass, multipassId: framePasses.Count, textureArraySlice: -1, cullingParams, occlusionMeshMaterial);
 
