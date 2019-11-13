8e5762348009dcce8ab39cfc697d9ed0b35a2e61
Tristan Genevet <tristan@unity3d.com>
2019-11-13 15:46:20 +0100

    Copy Paste of sot values.

--- com.unity.visualeffectgraph/Editor/GraphView/Elements/VFXDataAnchor.cs
+++ com.unity.visualeffectgraph/Editor/GraphView/Elements/VFXDataAnchor.cs
@@ -72,7 +72,7 @@ namespace UnityEditor.VFX.UI
             Profiler.EndSample();
         }
 
-        public void BuildContextualMenu(ContextualMenuPopulateEvent evt)
+        public virtual void BuildContextualMenu(ContextualMenuPopulateEvent evt)
         {
             var op = controller.sourceNode.model as VFXOperatorNumericCascadedUnified;
 
