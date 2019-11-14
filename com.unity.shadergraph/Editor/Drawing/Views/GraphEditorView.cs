bd9040dfc5942f370f72f813910193a0934d76a5
Martin Thorzen <35328557+martint-unity@users.noreply.github.com>
2019-11-14 16:03:23 +0100

    Updated the zoom level (#4984)
    * Updated max zoom scale
    
    * updated change log
    
    * Updated CHANGELOG.md
    
    * Update GraphEditorView.cs
    
    Removed comment in code
    
    
    Co-authored-by: Sharlene Tan <43795986+sharlenet@users.noreply.github.com>

--- com.unity.shadergraph/Editor/Drawing/Views/GraphEditorView.cs
+++ com.unity.shadergraph/Editor/Drawing/Views/GraphEditorView.cs
@@ -226,7 +226,7 @@ namespace UnityEditor.ShaderGraph.Drawing
             var content = new VisualElement { name = "content" };
             {
                 m_GraphView = new MaterialGraphView(graph) { name = "GraphView", viewDataKey = "MaterialGraphView" };
-                m_GraphView.SetupZoom(0.05f, ContentZoomer.DefaultMaxScale);
+                m_GraphView.SetupZoom(0.05f, 8);
                 m_GraphView.AddManipulator(new ContentDragger());
                 m_GraphView.AddManipulator(new SelectionDragger());
                 m_GraphView.AddManipulator(new RectangleSelector());
