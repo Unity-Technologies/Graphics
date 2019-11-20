5fd74ff2e43669f286d2abd1f6a99cd75e6e137b
Remi Slysz <40034005+RSlysz@users.noreply.github.com>
2019-11-20 11:26:07 +0100

    CoreRP/Catch srp loading error while assetdatabase not available [Need C++ PR] (#5011)
    * Check if path is available or not to detect if the issue is caused by AssetDatabase or wrong path
    
    * Remove unneeded Exception catching as this will be handled in InternalCreatePipeline
    
    * Remove Try-Catche in URP preventing explicite error for debugging
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipelineAsset.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipelineAsset.cs
@@ -23,25 +23,8 @@ namespace UnityEngine.Rendering.HighDefinition
 
         void Reset() => OnValidate();
 
-        protected override UnityEngine.Rendering.RenderPipeline CreatePipeline()
-        {
-            // safe: When we return a null render pipline it will do nothing in the rendering
-            HDRenderPipeline pipeline = null;
-
-            // We need to do catch every errors that happend during the HDRP build, when we upgrade the
-            // HDRP package, some required assets are not yet imported by the package manager when the
-            // pipeline is created so in that case, we just return a null pipeline. Some error may appear
-            // when we upgrade the pipeline but it's better than breaking HDRP resources an causing more
-            // errors.
-            try
-            {
-                pipeline = new HDRenderPipeline(this, HDRenderPipeline.defaultAsset);
-            } catch (Exception e) {
-                UnityEngine.Debug.LogError(e);
-            }
-
-            return pipeline;
-        }
+        protected override RenderPipeline CreatePipeline()
+            => new HDRenderPipeline(this, HDRenderPipeline.defaultAsset);
 
         protected override void OnValidate()
         {
