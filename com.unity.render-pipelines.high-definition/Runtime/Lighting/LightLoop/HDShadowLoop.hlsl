ab89fbe9c68b543e9dfb3741b428cedbf0f482d9
sebastienlagarde <sebastien@unity3d.com>
2019-11-22 12:08:06 +0100

    Fix issue with shadow matte (spot was disabled) (#5134)
    * Reenable code for Spot
    
    * Update unit test
    
    * fix shadowmatte

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/HDShadowLoop.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/HDShadowLoop.hlsl
@@ -114,7 +114,7 @@ void ShadowLoopMin(HDShadowContext shadowContext, PositionInputs posInput, float
                                                             s_lightData.angleScale,            s_lightData.angleOffset) > 0.0 &&
                         L.y > 0.0)
                     {
-                        shadowP = GetPunctualShadowAttenuation(shadowContext, posInput.positionSS, posInput.positionWS, normalWS, s_lightData.shadowIndex, L, distances.x, /*s_lightData.lightType == GPULIGHTTYPE_POINT*/ true, s_lightData.lightType != GPULIGHTTYPE_PROJECTOR_BOX);
+                        shadowP = GetPunctualShadowAttenuation(shadowContext, posInput.positionSS, posInput.positionWS, normalWS, s_lightData.shadowIndex, L, distances.x, s_lightData.lightType == GPULIGHTTYPE_POINT, s_lightData.lightType != GPULIGHTTYPE_PROJECTOR_BOX);
                         shadowP = s_lightData.nonLightMappedOnly ? min(1.0f, shadowP) : shadowP;
                         shadowP = lerp(1.0f, shadowP, s_lightData.shadowDimmer);
 
