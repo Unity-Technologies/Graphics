94729c1bb60dae3939eef3a684de825ca7706ef6
sebastienlagarde <sebastien@unity3d.com>
2019-11-25 08:41:37 +0100

    Fix specular occlusion issue with upgrade + update various label to preview (#5150)
    * Revert all material file to previous version before specular occlusion PR
    
    * Revert more material for fix specular occlusion
    
    * update specular occlusion mode to not required any udpate
    
    * Update component menu item label + fix order of renderpipelineUI
    
    * Fix migration path for _ENABLESPECULAROCCLUSION
    
    * Update scene 1210 with latest binary version
    
    * Update 1210_Lit_BentNormal.unity
    
    * increase slightly threashold of distortion to have less error locally
    
    * update terrain 1601 screenshots
    
    * Fix display of shadow resolution tiers

--- com.unity.render-pipelines.high-definition/Editor/Material/LayeredLit/LayeredLitGUI.cs
+++ com.unity.render-pipelines.high-definition/Editor/Material/LayeredLit/LayeredLitGUI.cs
@@ -208,8 +208,12 @@ namespace UnityEditor.Rendering.HighDefinition
             CoreUtils.SetKeyword(material, "_EMISSIVE_MAPPING_TRIPLANAR", ((UVBaseMapping)material.GetFloat(kUVEmissive)) == UVBaseMapping.Triplanar && material.GetTexture(kEmissiveColorMap));
             CoreUtils.SetKeyword(material, "_EMISSIVE_COLOR_MAP", material.GetTexture(kEmissiveColorMap));
 
+            // For migration of specular occlusion to specular mode we remove previous keyword
+            // _ENABLESPECULAROCCLUSION is deprecated
+            CoreUtils.SetKeyword(material, "_ENABLESPECULAROCCLUSION", false);
+
             int specOcclusionMode = material.GetInt(kSpecularOcclusionMode);
-            CoreUtils.SetKeyword(material, "_SPECULAR_OCCLUSION_FROM_AMBIENT_OCCLUSION", specOcclusionMode == 1);
+            CoreUtils.SetKeyword(material, "_SPECULAR_OCCLUSION_NONE", specOcclusionMode == 0);
             CoreUtils.SetKeyword(material, "_SPECULAR_OCCLUSION_FROM_BENT_NORMAL_MAP", specOcclusionMode == 2);
             CoreUtils.SetKeyword(material, "_MAIN_LAYER_INFLUENCE_MODE", material.GetFloat(kkUseMainLayerInfluence) != 0.0f);
 
