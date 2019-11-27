6dd109fd03da074bafc6ed6d5027caae0a125ce7
Thomas <chman.a11w@gmail.com>
2019-11-27 16:44:48 +0100

    Added a threshold to bloom

--- com.unity.render-pipelines.high-definition/Runtime/PostProcessing/Components/Bloom.cs
+++ com.unity.render-pipelines.high-definition/Runtime/PostProcessing/Components/Bloom.cs
@@ -12,6 +12,9 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
     [Serializable, VolumeComponentMenu("Post-processing/Bloom")]
     public sealed class Bloom : VolumeComponent, IPostProcessComponent
     {
+        [Tooltip("Set the level of brightness to filter out pixels under this level. This value is expressed in gamma-space. A value above 0 will disregard energy conservation rules.")]
+        public MinFloatParameter threshold = new MinFloatParameter(0f, 0f);
+
         [Tooltip("Controls the strength of the bloom filter.")]
         public ClampedFloatParameter intensity = new ClampedFloatParameter(0f, 0f, 1f);
 
