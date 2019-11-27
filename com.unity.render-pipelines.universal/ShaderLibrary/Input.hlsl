22d06495df46f5ce62c83946fd1faf16896a82fb
Kay Chang <kaychang@unity3d.com>
2019-11-27 14:35:10 -0800

    Fixed matrices for XR rendering mode.

--- com.unity.render-pipelines.universal/ShaderLibrary/Input.hlsl
+++ com.unity.render-pipelines.universal/ShaderLibrary/Input.hlsl
@@ -65,9 +65,13 @@ half4 _AdditionalLightsOcclusionProbes[MAX_VISIBLE_LIGHTS];
 #define UNITY_MATRIX_V     unity_MatrixV
 #define UNITY_MATRIX_I_V   unity_MatrixInvV
 #define UNITY_MATRIX_P     OptimizeProjectionMatrix(glstate_matrix_projection)
-#define UNITY_MATRIX_I_P   ERROR_UNITY_MATRIX_I_P_IS_NOT_DEFINED
+#define UNITY_MATRIX_I_P   unity_CameraInvProjection
 #define UNITY_MATRIX_VP    unity_MatrixVP
+#if defined(USING_STEREO_MATRICES)
+#define UNITY_MATRIX_I_VP  unity_MatrixInvVP
+#else
 #define UNITY_MATRIX_I_VP  _InvCameraViewProj
+#endif
 #define UNITY_MATRIX_MV    mul(UNITY_MATRIX_V, UNITY_MATRIX_M)
 #define UNITY_MATRIX_T_MV  transpose(UNITY_MATRIX_MV)
 #define UNITY_MATRIX_IT_MV transpose(mul(UNITY_MATRIX_I_M, UNITY_MATRIX_I_V))
