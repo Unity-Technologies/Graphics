fce3410967687075c738273141c86a309de82f06
Thomas <chman.a11w@gmail.com>
2019-11-27 16:44:48 +0100

    Added a threshold to bloom

--- com.unity.render-pipelines.high-definition/Editor/PostProcessing/BloomEditor.cs
+++ com.unity.render-pipelines.high-definition/Editor/PostProcessing/BloomEditor.cs
@@ -6,6 +6,7 @@ namespace UnityEditor.Experimental.Rendering.HDPipeline
     [VolumeComponentEditor(typeof(Bloom))]
     sealed class BloomEditor : VolumeComponentEditor
     {
+        SerializedDataParameter m_Threshold;
         SerializedDataParameter m_Intensity;
         SerializedDataParameter m_Scatter;
         SerializedDataParameter m_Tint;
@@ -24,12 +25,12 @@ namespace UnityEditor.Experimental.Rendering.HDPipeline
         {
             var o = new PropertyFetcher<Bloom>(serializedObject);
 
+            m_Threshold = Unpack(o.Find(x => x.threshold));
             m_Intensity = Unpack(o.Find(x => x.intensity));
             m_Scatter = Unpack(o.Find(x => x.scatter));
             m_Tint = Unpack(o.Find(x => x.tint));
             m_DirtTexture = Unpack(o.Find(x => x.dirtTexture));
             m_DirtIntensity = Unpack(o.Find(x => x.dirtIntensity));
-
             m_HighQualityFiltering = Unpack(o.Find(x => x.highQualityFiltering));
             m_Resolution = Unpack(o.Find(x => x.resolution));
             m_Prefilter = Unpack(o.Find(x => x.prefilter));
@@ -39,6 +40,7 @@ namespace UnityEditor.Experimental.Rendering.HDPipeline
         public override void OnInspectorGUI()
         {
             EditorGUILayout.LabelField("Bloom", EditorStyles.miniLabel);
+            PropertyField(m_Threshold);
             PropertyField(m_Intensity);
             PropertyField(m_Scatter);
             PropertyField(m_Tint);
