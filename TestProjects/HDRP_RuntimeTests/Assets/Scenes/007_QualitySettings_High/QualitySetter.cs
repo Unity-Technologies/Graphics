d4020e974579e44895642a80743da9124240e9ba
JansMargevics <jans.margevics@unity3d.com>
2019-11-29 16:20:26 +0200

    Add quality settings test, fix default quality level to avoid breaking other tets

---
+++ TestProjects/HDRP_RuntimeTests/Assets/Scenes/007_QualitySettings_High/QualitySetter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QualitySetter : MonoBehaviour
+{
+    public void SetHighQuality()
+    {
+    	QualitySettings.SetQualityLevel(1, true);
+    }
+
+    public void SetMediumQuality()
+    {
+    	QualitySettings.SetQualityLevel(2, true);
+    }
+
+    public void SetLowQuality()
+    {
+    	QualitySettings.SetQualityLevel(3, true);
+    }
+
+}
