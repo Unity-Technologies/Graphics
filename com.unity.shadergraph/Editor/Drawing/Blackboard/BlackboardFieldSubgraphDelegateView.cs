1d86929ae9df6f8b2afa6490d59ccb0b517b927d
LandonTownsendUnity <landon.townsend@unity3d.com>
2019-12-03 10:20:13 -0800

    Fixed bug where input and output lists can contain the same reference or display names

--- com.unity.shadergraph/Editor/Drawing/Blackboard/BlackboardFieldSubgraphDelegateView.cs
+++ com.unity.shadergraph/Editor/Drawing/Blackboard/BlackboardFieldSubgraphDelegateView.cs
@@ -203,19 +203,27 @@ namespace UnityEditor.ShaderGraph.Drawing
             DirtyNodes();
         }
 
-        public string GetDuplicateSafeDisplayName(int id, string name, ReorderableList reorderableList)
+        public string GetDuplicateSafeDisplayName(int id, string name, ReorderableList list)
         {
             name = name.Trim();
-            var entryList = reorderableList.list as List<SubgraphDelegateEntry>;
-            return GraphUtil.SanitizeName(entryList.Where(p => p.id != id).Select(p => p.displayName), "{0} ({1})", name);
+            var entryListIn = m_ReorderableList_Input.list as List<SubgraphDelegateEntry>;
+            var entryListOut = m_ReorderableList_Output.list as List<SubgraphDelegateEntry>;
+            var matchingIn = entryListIn.Where(p => p.id != id || list != m_ReorderableList_Input);
+            var matchingOut = entryListOut.Where(p => p.id != id || list != m_ReorderableList_Output);
+            var matchingString = matchingIn.Concat<SubgraphDelegateEntry>(matchingOut).Select(p => p.displayName);
+            return GraphUtil.SanitizeName(matchingString, "{0} ({1})", name);
         }
 
-        public string GetDuplicateSafeReferenceName(int id, string name, ReorderableList reorderableList)
+        public string GetDuplicateSafeReferenceName(int id, string name, ReorderableList list)
         {
             name = name.Trim();
             name = Regex.Replace(name, @"(?:[^A-Za-z_0-9])|(?:\s)", "_");
-            var entryList = reorderableList.list as List<SubgraphDelegateEntry>;
-            return GraphUtil.SanitizeName(entryList.Where(p => p.id != id).Select(p => p.referenceName), "{0}_{1}", name);
+            var entryListIn = m_ReorderableList_Input.list as List<SubgraphDelegateEntry>;
+            var entryListOut = m_ReorderableList_Output.list as List<SubgraphDelegateEntry>;
+            var matchingIn = entryListIn.Where(p => p.id != id || list != m_ReorderableList_Input);
+            var matchingOut = entryListOut.Where(p => p.id != id || list != m_ReorderableList_Output);
+            var matchingString = matchingIn.Concat<SubgraphDelegateEntry>(matchingOut).Select(p => p.referenceName);
+            return GraphUtil.SanitizeName(matchingString, "{0}_{1}", name);
         }
 
         public override void DirtyNodes(ModificationScope modificationScope = ModificationScope.Node)
