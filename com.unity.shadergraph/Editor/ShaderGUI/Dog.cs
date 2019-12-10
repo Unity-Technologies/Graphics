1116bbf72d0e1967c8a472ffda597758567a4914
zadi0648 <zackd44@gmail.com>
2019-12-10 23:02:43 +0000

    commitin the dog

---
+++ com.unity.shadergraph/Editor/ShaderGUI/Dog.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace UnityEditor.ShaderGraph
+{
+    public class Dog
+    {
+        private string bigBoy = "raf raf";             
+          
+        public void Bark()        
+        {
+            Debug.Log("Boof boof");
+            return;         
+        }
+    }
+}               
