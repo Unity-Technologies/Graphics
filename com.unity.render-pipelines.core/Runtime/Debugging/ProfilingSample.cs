f672f4ba346dc6e050b58d9aa38085802512b26c
JulienIgnace-Unity <julien@unity3d.com>
2019-12-16 12:12:07 +0100

    Hdrp/api doc update 4 [Skip CI] (#5345)
    * Observable List API doc
    
    * Removed duplicate RectInt
    
    * MousePositionDebug API Doc
    
    * Bit array.
    
    * Texture XR
    
    * Removed Mini Profiler
    
    * Various small API doc
    
    * WIP (XR, profiling samples, ...)
    
    * Material Upgrader API doc.

--- com.unity.render-pipelines.core/Runtime/Debugging/ProfilingSample.cs
+++ com.unity.render-pipelines.core/Runtime/Debugging/ProfilingSample.cs
@@ -3,6 +3,9 @@ using UnityEngine.Profiling;
 
 namespace UnityEngine.Rendering
 {
+    /// <summary>
+    /// Scoped profiling sample.
+    /// </summary>
     public struct ProfilingSample : IDisposable
     {
         readonly CommandBuffer m_Cmd;
@@ -11,6 +14,12 @@ namespace UnityEngine.Rendering
         bool m_Disposed;
         CustomSampler m_Sampler;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="cmd">Command Buffer used for setting up profiling samples.</param>
+        /// <param name="name">Name of the profiling sample.</param>
+        /// <param name="sampler">Optional CustomSampler for CPU profiling.</param>
         public ProfilingSample(CommandBuffer cmd, string name, CustomSampler sampler = null)
         {
             m_Cmd = cmd;
@@ -23,16 +32,31 @@ namespace UnityEngine.Rendering
         }
 
         // Shortcut to string.Format() using only one argument (reduces Gen0 GC pressure)
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="cmd">Command Buffer used for setting up profiling samples.</param>
+        /// <param name="format">Format string.</param>
+        /// <param name="arg">Format string arguments.</param>
         public ProfilingSample(CommandBuffer cmd, string format, object arg) : this(cmd, string.Format(format, arg))
         {
         }
 
         // Shortcut to string.Format() with variable amount of arguments - for performance critical
         // code you should pre-build & cache the marker name instead of using this
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="cmd">Command Buffer used for setting up profiling samples.</param>
+        /// <param name="format">Format string.</param>
+        /// <param name="args">Format string arguments.</param>
         public ProfilingSample(CommandBuffer cmd, string format, params object[] args) : this(cmd, string.Format(format, args))
         {
         }
 
+        /// <summary>
+        /// Disposable pattern implementation.
+        /// </summary>
         public void Dispose()
         {
             Dispose(true);
