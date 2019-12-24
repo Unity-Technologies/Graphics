040dd4fdca34f454126f6874ead8e2158270de6c
Paul Demeulenaere <pauld@unity3d.com>
2019-12-24 11:40:30 +0100

    Fill VFXDataOutputEvent

--- com.unity.visualeffectgraph/Editor/Data/VFXDataOutputEvent.cs
+++ com.unity.visualeffectgraph/Editor/Data/VFXDataOutputEvent.cs
@@ -17,9 +17,53 @@ namespace UnityEditor.VFX
             //There is nothing serialized here
         }
 
-        public override void FillDescs(List<VFXGPUBufferDesc> outBufferDescs, List<VFXTemporaryGPUBufferDesc> outTemporaryBufferDescs, List<VFXEditorSystemDesc> outSystemDescs, VFXExpressionGraph expressionGraph, Dictionary<VFXContext, VFXContextCompiledData> contextToCompiledData, Dictionary<VFXContext, int> contextSpawnToBufferIndex, VFXDependentBuffersData dependentBuffers, Dictionary<VFXContext, List<VFXContextLink>[]> effectiveFlowInputLinks, VFXSystemNames systemNames = null)
+        public override void FillDescs(
+            List<VFXGPUBufferDesc> outBufferDescs,
+            List<VFXTemporaryGPUBufferDesc> outTemporaryBufferDescs,
+            List<VFXEditorSystemDesc> outSystemDescs,
+            VFXExpressionGraph expressionGraph,
+            Dictionary<VFXContext, VFXContextCompiledData> contextToCompiledData,
+            Dictionary<VFXContext, int> contextSpawnToBufferIndex,
+            VFXDependentBuffersData dependentBuffers,
+            Dictionary<VFXContext, List<VFXContextLink>[]> effectiveFlowInputLinks,
+            VFXSystemNames systemNames = null)
         {
-            throw new InvalidOperationException("Success !!");
+            if (m_Contexts.Count != 1)
+                throw new InvalidOperationException("VFXDataOutputEvent unexpected context count : " + m_Contexts.Count);
+
+            if (m_Contexts[0].contextType != VFXContextType.OutputEvent)
+                throw new InvalidOperationException("VFXDataOutputEvent unexpected context type : " + m_Contexts[0].contextType);
+
+            var flowInputLinks = effectiveFlowInputLinks[m_Contexts[0]];
+            var inputSpawnerContext = flowInputLinks.SelectMany(o => o.Select(p => p.context));
+
+            var systemBufferMappings = new List<VFXMapping>();
+            foreach (var spawner in inputSpawnerContext)
+            {
+                if (spawner.contextType != VFXContextType.Spawner)
+                    throw new InvalidOperationException("VFXDataOutputEvent unexpected link on Output event");
+
+                systemBufferMappings.Add(new VFXMapping()
+                {
+                    name = "spawner_input",
+                    index = contextSpawnToBufferIndex[spawner]
+                });
+            }
+
+            string nativeName = string.Empty;
+            if (systemNames != null)
+                nativeName = systemNames.GetUniqueSystemName(this);
+            else
+                throw new InvalidOperationException("system names manager cannot be null");
+
+            outSystemDescs.Add(new VFXEditorSystemDesc()
+            {
+                flags = VFXSystemFlag.SystemDefault,
+                name = nativeName,
+                buffers = systemBufferMappings.ToArray(),
+                type = VFXSystemType.OutputEvent,
+                layer = m_Layer
+            });
         }
 
         public override void GenerateAttributeLayout(Dictionary<VFXContext, List<VFXContextLink>[]> effectiveFlowInputLinks)
