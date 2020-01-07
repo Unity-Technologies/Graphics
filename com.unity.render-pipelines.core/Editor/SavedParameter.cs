8b88213f2fd939f002a71b94422743d583c4d6bc
Thomas <chman.a11w@gmail.com>
2020-01-07 16:52:26 +0100

    Added missing XML doc

--- com.unity.render-pipelines.core/Editor/SavedParameter.cs
+++ com.unity.render-pipelines.core/Editor/SavedParameter.cs
@@ -3,10 +3,30 @@ using UnityEngine.Assertions;
 
 namespace UnityEditor.Rendering
 {
+    /// <summary>
+    /// An utility class to save and load data to a custom location, usually in editor preferences.
+    /// </summary>
+    /// <typeparam name="T">The type of data to save</typeparam>
+    /// <seealso cref="SavedBool"/>
+    /// <seealso cref="SavedInt"/>
+    /// <seealso cref="SavedFloat"/>
+    /// <seealso cref="SavedString"/>
     public class SavedParameter<T>
         where T : IEquatable<T>
     {
+        /// <summary>
+        /// The method called when the data is being stored.
+        /// </summary>
+        /// <param name="key">The string key to associate the data with.</param>
+        /// <param name="value">The value to store.</param>
         public delegate void SetParameter(string key, T value);
+
+        /// <summary>
+        /// The method called when the data is being retrieved.
+        /// </summary>
+        /// <param name="key">The string key associated with the data to retrieve.</param>
+        /// <param name="defaultValue">A default value to return if the key doesn't exist.</param>
+        /// <returns>The value associated to the key, <paramref name="defaultValue"/> otherwise.</returns>
         public delegate T GetParameter(string key, T defaultValue);
 
         readonly string m_Key;
@@ -16,6 +36,13 @@ namespace UnityEditor.Rendering
         readonly SetParameter m_Setter;
         readonly GetParameter m_Getter;
 
+        /// <summary>
+        /// Creates a new instance of a <see cref="SavedParameter{T}"/>.
+        /// </summary>
+        /// <param name="key">The string key to associate the data with.</param>
+        /// <param name="value">The initial value to set the parameter to if the key doesn't exist yet.</param>
+        /// <param name="getter">The method called when the data is being retrieved.</param>
+        /// <param name="setter">The method called when the data is being stored.</param>
         public SavedParameter(string key, T value, GetParameter getter, SetParameter setter)
         {
             Assert.IsNotNull(setter);
@@ -37,6 +64,9 @@ namespace UnityEditor.Rendering
             m_Value = m_Getter(m_Key, m_Value);
         }
 
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
         public T value
         {
             get
@@ -58,26 +88,59 @@ namespace UnityEditor.Rendering
     }
 
     // Pre-specialized class for easier use and compatibility with existing code
+
+    /// <summary>
+    /// A boolean value stored in editor preferences.
+    /// </summary>
     public sealed class SavedBool : SavedParameter<bool>
     {
+        /// <summary>
+        /// Creates a new instance of a <see cref="SavedBool"/>.
+        /// </summary>
+        /// <param name="key">The string key to associate the data with.</param>
+        /// <param name="value">The initial value to set the parameter to if the key doesn't exist yet.</param>
         public SavedBool(string key, bool value)
             : base(key, value, EditorPrefs.GetBool, EditorPrefs.SetBool) { }
     }
 
+    /// <summary>
+    /// An integer value stored in editor preferences.
+    /// </summary>
     public sealed class SavedInt : SavedParameter<int>
     {
+        /// <summary>
+        /// Creates a new instance of a <see cref="SavedInt"/>.
+        /// </summary>
+        /// <param name="key">The string key to associate the data with.</param>
+        /// <param name="value">The initial value to set the parameter to if the key doesn't exist yet.</param>
         public SavedInt(string key, int value)
             : base(key, value, EditorPrefs.GetInt, EditorPrefs.SetInt) { }
     }
 
+    /// <summary>
+    /// A floating point value stored in editor preferences.
+    /// </summary>
     public sealed class SavedFloat : SavedParameter<float>
     {
+        /// <summary>
+        /// Creates a new instance of a <see cref="SavedFloat"/>.
+        /// </summary>
+        /// <param name="key">The string key to associate the data with.</param>
+        /// <param name="value">The initial value to set the parameter to if the key doesn't exist yet.</param>
         public SavedFloat(string key, float value)
             : base(key, value, EditorPrefs.GetFloat, EditorPrefs.SetFloat) { }
     }
 
+    /// <summary>
+    /// A string value stored in editor preferences.
+    /// </summary>
     public sealed class SavedString : SavedParameter<string>
     {
+        /// <summary>
+        /// Creates a new instance of a <see cref="SavedString"/>.
+        /// </summary>
+        /// <param name="key">The string key to associate the data with.</param>
+        /// <param name="value">The initial value to set the parameter to if the key doesn't exist yet.</param>
         public SavedString(string key, string value)
             : base(key, value, EditorPrefs.GetString, EditorPrefs.SetString) { }
     }
