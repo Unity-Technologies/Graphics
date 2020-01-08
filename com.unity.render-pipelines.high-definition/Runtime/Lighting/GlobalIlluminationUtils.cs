8b603f70f32fa32016c7b973647236dc38bdb67f
uygar <uygar@unity3d.com>
2020-01-08 17:38:54 +0100

    Added support for cookies on rectangular area lights.

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/GlobalIlluminationUtils.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/GlobalIlluminationUtils.cs
@@ -194,6 +194,7 @@ namespace UnityEngine.Rendering.HighDefinition
                             // TEMP: for now, if we bake a rectangle type this will disable the light for runtime, need to speak with GI team about it!
                             lightDataGI.type = UnityEngine.Experimental.GlobalIllumination.LightType.Rectangle;
                             lightDataGI.falloff = add.applyRangeAttenuation ? FalloffType.InverseSquared : FalloffType.InverseSquaredNoRangeAttenuation;
+                            lightDataGI.cookieID = add.areaLightCookie ? add.areaLightCookie.GetInstanceID() : 0;
                             break;
 
                         case AreaLightShape.Tube:
