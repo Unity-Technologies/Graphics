766136fabfa4aae60ab6e2f4abd5c055029ea814
Mark Gehan <markg@unity3d.com>
2020-01-14 22:16:33 -0800

    reverting other Universal test project assets

--- TestProjects/UniversalGraphicsTest/Assets/Test/Runtime/UniversalGraphicsTests.cs
+++ TestProjects/UniversalGraphicsTest/Assets/Test/Runtime/UniversalGraphicsTests.cs
@@ -9,10 +9,7 @@ using UnityEngine.SceneManagement;
 
 public class UniversalGraphicsTests
 {
-#if UNITY_ANDROID
-    static bool wasFirstSceneRan = false;
-    const int firstSceneAdditionalFrames = 3;
-#endif
+
     public const string universalPackagePath = "Assets/ReferenceImages";
 
     [UnityTest, Category("UniversalRP")]
@@ -35,7 +32,6 @@ public class UniversalGraphicsTests
 
         if (scene.name.Substring(3, 4).Equals("_xr_"))
         {
-#if ENABLE_VR && ENABLE_VR_MODULE
             Assume.That((Application.platform != RuntimePlatform.OSXEditor && Application.platform != RuntimePlatform.OSXPlayer), "Stereo Universal tests do not run on MacOSX.");
 
             XRSettings.LoadDeviceByName("MockHMD");
@@ -49,33 +45,15 @@ public class UniversalGraphicsTests
 
             foreach (var camera in cameras)
                 camera.stereoTargetEye = StereoTargetEyeMask.Both;
-#else
-            yield return null;
-#endif
         }
         else
         {
-#if ENABLE_VR && ENABLE_VR_MODULE
             XRSettings.enabled = false;
-#endif
             yield return null;
         }
 
         for (int i = 0; i < settings.WaitFrames; i++)
             yield return null;
-
-#if UNITY_ANDROID
-        // On Android first scene often needs a bit more frames to load all the assets
-        // otherwise the screenshot is just a black screen
-        if (!wasFirstSceneRan)
-        {
-            for(int i = 0; i < firstSceneAdditionalFrames; i++)
-            {
-                yield return null;
-            }
-            wasFirstSceneRan = true;
-        }
-#endif
 
         ImageAssert.AreEqual(testCase.ReferenceImage, cameras.Where(x => x != null), settings.ImageComparisonSettings);
 
@@ -85,7 +63,7 @@ public class UniversalGraphicsTests
         var mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         try
         {
-            ImageAssert.AllocatesMemory(mainCamera, settings?.ImageComparisonSettings);
+            ImageAssert.AllocatesMemory(mainCamera, 512, 512); // 512 used for height and width to render
         }
         catch (AssertionException)
         {
