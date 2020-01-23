51292352bb192d6c1bdf9d9127039016d606ce6f
RSlysz <remi@unity3d.com>
2020-01-23 19:21:19 +0100

    pass by ExtensibleCamera now

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/Camera/HDCamera.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/Camera/HDCamera.cs
@@ -130,7 +130,7 @@ namespace UnityEngine.Rendering.HighDefinition
     
     [HelpURL(Documentation.baseURL + Documentation.version + Documentation.subURL + "HDRP-Camera" + Documentation.endURL)]
     [DisallowMultipleComponent, ExecuteAlways]
-    public partial class HDCamera : Camera, IFrameSettingsHistoryContainer
+    public partial class HDCamera : ExtensibleCamera, IFrameSettingsHistoryContainer
     {
         public enum FlipYMode
         {
