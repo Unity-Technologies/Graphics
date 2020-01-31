92de021613c7aa11fbb290bc01a4226907afe8d7
Thomasch-unity3d <30902625+Thomasch-unity3d@users.noreply.github.com>
2020-01-31 11:38:45 -0500

    Dcc/Arnold material description preprocessor (#5573)
    * Fixed issue with FBX Standard Material preprocessor not run for collada files.
    
    * Added MaterialDescription postProcessor and shadergraph handling Arnold Standard Surface Material import from FBX.
    
    * Added MaterialDescription postProcessor and shadergraph for URP, small fixes to HDRP version.
    
    * Adding 3DSmax PhysicalMaterial shader and postprocessor
    
    * Added missing namespace in AssetPostprocessor, now handling anisotropy rotation in shader.
    
    * Edit some PhysicalMaterial nodes to match what's available in 3dsmax
    
    * Small fixes
    
    * Fixed typo in SpecularIOR shader property name.
    
    * Added DependsOnImportedAsset dependencies on shaders, renamed PhysicalMaterial as PhysicalMaterial3DsMax
    
    * Added entries to Changelog, using reflection to add import dependency to shaders in AssetPostprocessors.
    
    * Fixed compilation issue in URP assetPostprocessor.
    
    * Made AssetPostprocessors private.
    
    * Replaced reflection calls to DependsOnImportedAssets by CollectImportedDependenciesForModelImporter implementation.
    
    * Fixed issue with FBX files being reimported as many times as they contain materials in MaterialPostprocessor.
    
    * Removed subgraphs used in 3DsPhysicalMaterial graphs because they would be available to users otherwise.
    
    * Fixed wrong and missing namespaces.
    
    * Added transparent versions of graphs in URP, cleaned up unused includes.
    
    * Reverted changes making AssetPostprocessors public in URP and added Obsolete attributes on them.
    
    Co-authored-by: Bastien Humeau <bastienh@unity3d.com>

--- com.unity.render-pipelines.universal/Editor/AssetPostProcessors/SketchupMaterialDescriptionPostprocessor.cs
+++ com.unity.render-pipelines.universal/Editor/AssetPostProcessors/SketchupMaterialDescriptionPostprocessor.cs
@@ -1,9 +1,11 @@
-﻿using System.IO;
+using System;
+using System.IO;
 using UnityEngine;
 using UnityEditor.AssetImporters;
 
 namespace UnityEditor.Rendering.Universal
 {
+    [Obsolete("SketchupMaterialDescriptionPreprocessor is deprecated, consider creating a new AsserPostProcessor rather than overriding it.")]
     public class SketchupMaterialDescriptionPreprocessor : AssetPostprocessor
     {
         static readonly uint k_Version = 1;
