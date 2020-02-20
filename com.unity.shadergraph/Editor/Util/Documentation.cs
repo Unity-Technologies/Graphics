dc992bd23e5defcce6cda6acd17f03b02868b115
Tim Cooper <stramit@gmail.com>
2020-02-20 12:49:54 +0100

    8.x.x/fix api validation [Skip CI] (#5935)
    * fix pt1
    
    * updating Unity branch to 2020.1/staging
    
    * Update HDRP versions
    
    Co-authored-by: Liis Kivistik <kivistik.liis@gmail.com>

--- com.unity.shadergraph/Editor/Util/Documentation.cs
+++ com.unity.shadergraph/Editor/Util/Documentation.cs
@@ -5,14 +5,16 @@ using System.Runtime.CompilerServices;
 namespace UnityEngine.Rendering.ShaderGraph
 {
     //Need to live in Runtime as Attribute of documentation is on Runtime classes \o/
-    class Documentation : DocumentationInfo
+    internal class Documentation
     {
         //This must be used like
         //[HelpURL(Documentation.baseURL + Documentation.version + Documentation.subURL + "some-page" + Documentation.endURL)]
         //It cannot support String.Format nor string interpolation
         internal const string baseURL = "https://docs.unity3d.com/Packages/com.unity.shadergraph@";
+        internal const string version = "8.0";
         internal const string subURL = "/manual/";
         internal const string endURL = ".html";
+
 
         internal static string GetPageLink(string pageName)
         {
