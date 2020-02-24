b20fd570350e70e91a918a96a8c77f47811f6878
hdb-unity <hakeem.bux@unity3d.com>
2020-02-24 20:01:37 +0200

    Remove sample scenes, add assets from monolithic project

---
+++ TestProjects/UniversalGraphicsTest_2D/Assets/Test/Runtime/UniversalGraphicsTests.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Linq;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.XR;
+using UnityEngine.TestTools.Graphics;
+using UnityEngine.SceneManagement;
+
+public class UniversalGraphicsTests
+{
+
+    public const string universalPackagePath = "Assets/ReferenceImages";
+
+    [UnityTest, Category("UniversalRP")]
+    [PrebuildSetup("SetupGraphicsTestCases")]
+    [UseGraphicsTestCases(universalPackagePath)]
+
+
+    public IEnumerator Run(GraphicsTestCase testCase)
+    {
+        SceneManager.LoadScene(testCase.ScenePath);
+
+        // Always wait one frame for scene load
+        yield return null;
+
+        var cameras = GameObject.FindGameObjectsWithTag("MainCamera").Select(x=>x.GetComponent<Camera>());
+        var settings = Object.FindObjectOfType<UniversalGraphicsTestSettings>();
+        Assert.IsNotNull(settings, "Invalid test scene, couldn't find UniversalGraphicsTestSettings");
+
+        Scene scene = SceneManager.GetActiveScene();
+
+        if (scene.name.Substring(3, 4).Equals("_xr_"))
+        {
+#if ENABLE_VR && ENABLE_VR_MODULE
+            Assume.That((Application.platform != RuntimePlatform.OSXEditor && Application.platform != RuntimePlatform.OSXPlayer), "Stereo Universal tests do not run on MacOSX.");
+
+            XRSettings.LoadDeviceByName("MockHMD");
+            yield return null;
+
+            XRSettings.enabled = true;
+            yield return null;
+
+            XRSettings.gameViewRenderMode = GameViewRenderMode.BothEyes;
+            yield return null;
+
+            foreach (var camera in cameras)
+                camera.stereoTargetEye = StereoTargetEyeMask.Both;
+#else
+            yield return null;
+#endif
+        }
+        else
+        {
+#if ENABLE_VR && ENABLE_VR_MODULE
+            XRSettings.enabled = false;
+#endif
+            yield return null;
+        }
+
+        for (int i = 0; i < settings.WaitFrames; i++)
+            yield return null;
+
+        ImageAssert.AreEqual(testCase.ReferenceImage, cameras.Where(x => x != null), settings.ImageComparisonSettings);
+
+#if CHECK_ALLOCATIONS_WHEN_RENDERING
+        // Does it allocate memory when it renders what's on the main camera?
+        bool allocatesMemory = false;
+        var mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        try
+        {
+            ImageAssert.AllocatesMemory(mainCamera, settings?.ImageComparisonSettings);
+        }
+        catch (AssertionException)
+        {
+            allocatesMemory = true;
+        }
+        if (allocatesMemory)
+            Assert.Fail("Allocated memory when rendering what is on main camera");
+#endif
+
+    }
+
+#if UNITY_EDITOR
+    [TearDown]
+    public void DumpImagesInEditor()
+    {
+        UnityEditor.TestTools.Graphics.ResultsUtility.ExtractImagesFromTestProperties(TestContext.CurrentContext.Test);
+    }
+#endif
+}
---
+++ TestProjects/UniversalGraphicsTest_Lighting/Assets/Test/Runtime/UniversalGraphicsTestSettings.cs
@@ -0,0 +1,14 @@
+using UnityEngine.TestTools.Graphics;
+
+public class UniversalGraphicsTestSettings : GraphicsTestSettings
+{
+    public int WaitFrames = 0;
+
+    public UniversalGraphicsTestSettings()
+    {
+        ImageComparisonSettings.TargetWidth = 512;
+        ImageComparisonSettings.TargetHeight = 512;
+        ImageComparisonSettings.AverageCorrectnessThreshold = 0.005f;
+        ImageComparisonSettings.PerPixelCorrectnessThreshold = 0.001f;
+    }
+}
