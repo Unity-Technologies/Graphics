bc6374c6ad7981f4503df4bebc85b0eeb4aa7a70
Frédéric Vauchelles <fredericv@unity3d.com>
2020-02-26 10:40:25 +0100

    added performance test attribute

--- TestProjects/HDRP_PerformanceTests/Assets/PerformanceTests/Editor/NDA_PS4/EditorStaticAnalysisTestPS4.cs
+++ TestProjects/HDRP_PerformanceTests/Assets/PerformanceTests/Editor/NDA_PS4/EditorStaticAnalysisTestPS4.cs
@@ -7,6 +7,6 @@ public class EditorStaticAnalysisTestXboxOne
 {
     static IEnumerable<EditorStaticAnalysisTests.StaticAnalysisEntry> GetStaticAnalysisEntriesPS4() => EditorStaticAnalysisTests.GetStaticAnalysisEntries(BuildTarget.PS4);
 
-    [Test, Version("1")]
+    [Test, Version("1"), Performance]
     public void StaticAnalysisPS4([ValueSource(nameof(GetStaticAnalysisEntriesPS4))] EditorStaticAnalysisTests.StaticAnalysisEntry entries) => EditorStaticAnalysisTests.StaticAnalysisExecute(entries);
 }
--- TestProjects/HDRP_PerformanceTests/Assets/PerformanceTests/Editor/NDA_XboxOne/EditorStaticAnalysisTestXboxOne.cs
+++ TestProjects/HDRP_PerformanceTests/Assets/PerformanceTests/Editor/NDA_XboxOne/EditorStaticAnalysisTestXboxOne.cs
@@ -7,6 +7,6 @@ public class EditorStaticAnalysisTestXboxOne
 {
     static IEnumerable<EditorStaticAnalysisTests.StaticAnalysisEntry> GetStaticAnalysisEntriesXboxOne() => EditorStaticAnalysisTests.GetStaticAnalysisEntries(BuildTarget.XboxOne);
 
-    [Test, Version("1")]
+    [Test, Version("1"), Performance]
     public void StaticAnalysisXboxOne([ValueSource(nameof(GetStaticAnalysisEntriesXboxOne))] EditorStaticAnalysisTests.StaticAnalysisEntry entry) => EditorStaticAnalysisTests.StaticAnalysisExecute(entry);
 }
