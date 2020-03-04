c664f396f694ba5bc4c544bf1c400f143ab023a7
Soufiane KHIAT <soufiane.khiat@unity3d.com>
2020-03-04 13:03:57 +0100

    Backup

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/AtmosphericScattering/ShaderVariablesAtmosphericScattering.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/AtmosphericScattering/ShaderVariablesAtmosphericScattering.hlsl
@@ -3,6 +3,10 @@
 #else
     TEXTURE3D(_VBufferLighting);
     TEXTURECUBE_ARRAY(_SkyTexture);
+    bool            _SkyTextureImportanceSamplerReady;
+    TEXTURE2D_ARRAY(_SkyTextureIntegrals);
+    TEXTURE2D_ARRAY(_SkyTextureMarginals);
+    TEXTURE2D_ARRAY(_SkyTextureConditionalMarginals);
 
     #define _MipFogNear                     _MipFogParameters.x
     #define _MipFogFar                      _MipFogParameters.y
