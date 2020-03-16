bbe04c0dffcea971af01c7485ba2bba24fbdbfd8
Alex Lindman <alexandral@unity3d.com>
2020-03-16 10:59:13 -0700

    first pass extracting visual overlays from stacks branch

--- com.unity.shadergraph/Editor/Drawing/Views/GraphEditorView.cs
+++ com.unity.shadergraph/Editor/Drawing/Views/GraphEditorView.cs
@@ -678,6 +678,9 @@ namespace UnityEditor.ShaderGraph.Drawing
                     if (nodeView?.node != null)
                     {
                         nodesToUpdate.Add(nodeView);
+
+                        // Update active state for connected Nodes
+                        NodeUtils.UpdateNodeActiveOnEdgeChange(nodeView?.node);
                     }
 
                     edgeView.output.Disconnect(edgeView);
@@ -694,7 +697,13 @@ namespace UnityEditor.ShaderGraph.Drawing
             {
                 var edgeView = AddEdge(edge);
                 if (edgeView != null)
-                    nodesToUpdate.Add((IShaderNodeView)edgeView.input.node);
+                {
+                    var outputNodeView = (IShaderNodeView)edgeView.output.node;
+                    nodesToUpdate.Add(outputNodeView);
+
+                    // Update active state for connected Nodes
+                    NodeUtils.UpdateNodeActiveOnEdgeChange(outputNodeView?.node);
+                }
             }
 
             foreach (var node in nodesToUpdate)
