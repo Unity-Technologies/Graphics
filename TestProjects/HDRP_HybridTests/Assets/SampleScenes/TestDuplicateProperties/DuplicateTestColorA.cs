741084e884832500b8e80062b8c581441c7f2b8a
MingWai <cmwdes@gmail.com>
2020-03-18 15:49:25 +0100

    [HDRP_HybridTests] Adding subscene cache for Win standalone test, fixed missing script objects

--- TestProjects/HDRP_HybridTests/Assets/SampleScenes/TestDuplicateProperties/DuplicateTestColorA.cs
+++ TestProjects/HDRP_HybridTests/Assets/SampleScenes/TestDuplicateProperties/DuplicateTestColorA.cs
@@ -2,7 +2,7 @@
 using Unity.Mathematics;
 using Unity.Rendering;
 
-#if ENABLE_HYBRID_RENDERER_V2 && UNITY_2020_1_OR_NEWER && (HDRP_9_0_0_OR_NEWER || URP_9_0_0_OR_NEWER)
+//#if ENABLE_HYBRID_RENDERER_V2 && UNITY_2020_1_OR_NEWER && (HDRP_9_0_0_OR_NEWER || URP_9_0_0_OR_NEWER)
 namespace Scenes.TestDuplicateProperties
 {
     [GenerateAuthoringComponent]
@@ -12,4 +12,4 @@ namespace Scenes.TestDuplicateProperties
         public float4 Value;
     }
 }
-#endif
+//#endif
