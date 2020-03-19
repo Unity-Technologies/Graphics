57c097a1d55b7fac0c544580fef19560d60c1f44
Paul Demeulenaere <pauld@unity3d.com>
2020-03-19 16:24:56 +0100

    *Update OutputEventTest.cs to check Test_Output_Event

--- com.unity.testing.visualeffectgraph/Runtime/OutputEventTest.cs
+++ com.unity.testing.visualeffectgraph/Runtime/OutputEventTest.cs
@@ -6,7 +6,8 @@ namespace Unity.Testing.VisualEffectGraph
 {
     public class OutputEventTest : MonoBehaviour
     {
-        static private int s_outputEventNameId = Shader.PropertyToID("Test_Output_Event");
+        static private string s_outputEventName = "Test_Output_Event";
+        static private int s_outputEventNameId = Shader.PropertyToID(s_outputEventName);
         static private int s_positionNameId = Shader.PropertyToID("position");
         static private int s_colorNameId = Shader.PropertyToID("color");
         static private int s_texIndexId = Shader.PropertyToID("texIndex");
@@ -15,29 +16,45 @@ namespace Unity.Testing.VisualEffectGraph
         public GameObject m_ObjectReference;
 
         private VisualEffect m_vfx;
-        private List<VFXEventAttribute> m_cachedVFXEventAttribute;
-        private List<VFXEventAttribute> m_currentVFXEventAttribute;
 
         void Start()
         {
             m_vfx = GetComponent<VisualEffect>();
-            if (m_vfx)
-            {
-                m_cachedVFXEventAttribute = new List<VFXEventAttribute>(3);
-                m_currentVFXEventAttribute = new List<VFXEventAttribute>(3);
-                for (int i = 0; i<3; ++i)
-                    m_cachedVFXEventAttribute.Add(m_vfx.CreateVFXEventAttribute());
 
-                m_vfx.outputEventReceived += o => ProcessVisualEffectEvent(o.eventAttribute);
+            if (!m_vfx)
+            {
+                Debug.LogError("Cannot find the VisualEffect component");
+                return;
             }
+
+            var outputEventNames = new List<string>();
+            m_vfx.GetOutputEventNames(outputEventNames);
+            if (outputEventNames.Count != 1)
+            {
+                Debug.LogErrorFormat("Unexpected output event count : {0} (expected : 1)", outputEventNames.Count);
+                return;
+            }
+
+            if (outputEventNames[0] != s_outputEventName)
+            {
+                Debug.LogErrorFormat("Unexpected output event name : {0} (expected : {1})", outputEventNames.Count);
+                return;
+            }
+
+            m_vfx.outputEventReceived += ProcessVisualEffectEvent;
         }
 
         static private float[] s_acceptableTexIndexValues = new [] { 10.0f, 20.0f, 30.0f };
 
-        void ProcessVisualEffectEvent(VFXEventAttribute eventAttribute)
+        void ProcessVisualEffectEvent(VisualEffect.OutputEventArgs eventArgs)
         {
+            if (eventArgs.nameId != s_outputEventNameId)
+            {
+                Debug.LogErrorFormat("Incorrect output event name : {0} (expected:{1})", eventArgs.nameId, s_outputEventNameId);
+            }
+
             //texIndex isn't used in particle system but it should be reachable in these vfx event attribute.
-            var texIndex = eventAttribute.GetFloat(s_texIndexId);
+            var texIndex = eventArgs.eventAttribute.GetFloat(s_texIndexId);
             bool correct = false;
             foreach (var value in s_acceptableTexIndexValues)
             {
@@ -55,11 +72,11 @@ namespace Unity.Testing.VisualEffectGraph
             }
 
             var newObject = GameObject.Instantiate(m_ObjectReference);
-            newObject.GetComponent<Transform>().position = eventAttribute.GetVector3(s_positionNameId);
+            newObject.GetComponent<Transform>().position = eventArgs.eventAttribute.GetVector3(s_positionNameId);
             var renderer = newObject.GetComponent<Renderer>();
             var currentMaterial = renderer.material;
             currentMaterial = new Material(currentMaterial);
-            currentMaterial.SetVector(s_customEmissionNameId, eventAttribute.GetVector3(s_colorNameId));
+            currentMaterial.SetVector(s_customEmissionNameId, eventArgs.eventAttribute.GetVector3(s_colorNameId));
             renderer.material = currentMaterial;
             newObject.SetActive(true);
         }
