2d17377fe2ff693b1a422e27ed73e68624238819
Tristan Genevet <tristan@unity3d.com>
2020-03-20 14:00:06 +0100

    more work

--- com.unity.visualeffectgraph/Editor/GraphView/Views/Properties/SimplePropertiesRM.cs
+++ com.unity.visualeffectgraph/Editor/GraphView/Views/Properties/SimplePropertiesRM.cs
@@ -7,7 +7,7 @@ using EnumField = UnityEditor.VFX.UIElements.VFXEnumField;
 
 namespace UnityEditor.VFX.UI
 {
-    class ListPropertyRM<T, U> : PropertyRM<List<T>> where U : PropertyRM<T>, new()
+    abstract class ListPropertyRM<T, U> : PropertyRM<List<T>> where U : PropertyRM<T>
     {
         public ListPropertyRM(IPropertyRMProvider controller, float labelWidth) : base(controller, labelWidth)
         {
@@ -21,6 +21,90 @@ namespace UnityEditor.VFX.UI
             return 150;
         }
         public override void UpdateGUI(bool force)
+        {
+            List<T> list = (List<T>)m_Provider.value;
+
+            while(m_List.itemCount < list.Count)
+            {
+                m_List.AddItem(CreateNewItem(m_List.itemCount));
+            }
+            while(m_List.itemCount > list.Count)
+            {
+                m_List.RemoveItemAt(m_List.itemCount - 1);
+            }
+
+            for(int i = 0; i < list.Count; ++i)
+            {
+                (m_List.ItemAt(i) as U).UpdateGUI(force);
+            }
+        }
+
+
+        class ItemProvider : IPropertyRMProvider
+        {
+            PropertyRM<List<T>> m_List;
+            int m_Index;
+
+            public ItemProvider(PropertyRM<List<T>> list, int index)
+            {
+                m_List = list;
+                m_Index = index;
+            }
+
+            bool IPropertyRMProvider.expanded => false;
+
+            bool IPropertyRMProvider.expandable => false;
+
+            bool IPropertyRMProvider.expandableIfShowsEverything => false;
+
+            object IPropertyRMProvider.value { get => ((List<T>)m_List.GetValue())[m_Index]; set => ((List<T>)m_List.GetValue())[m_Index] = (T)value; }
+
+            bool IPropertyRMProvider.spaceableAndMasterOfSpace => false;
+
+            VFXCoordinateSpace IPropertyRMProvider.space { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            string IPropertyRMProvider.name => m_Index.ToString();
+
+            VFXPropertyAttribute[] IPropertyRMProvider.attributes => null;
+
+            object[] IPropertyRMProvider.customAttributes => null;
+
+            Type IPropertyRMProvider.portType => typeof(T);
+
+            int IPropertyRMProvider.depth => 0;
+
+            bool IPropertyRMProvider.editable => m_List.provider.editable;
+
+            void IPropertyRMProvider.ExpandPath()
+            {
+                throw new NotImplementedException();
+            }
+
+            bool IPropertyRMProvider.IsSpaceInherited()
+            {
+                return false;
+            }
+
+            void IPropertyRMProvider.RetractPath()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        protected abstract U CreateField(IPropertyRMProvider provider);
+
+        protected U CreateNewItem(int index)
+        {
+            U item = CreateField(new ItemProvider(this,index));
+
+            return item;
+        }
+
+        protected override void UpdateEnabled()
+        {
+        }
+
+        protected override void UpdateIndeterminate()
         {
         }
 
