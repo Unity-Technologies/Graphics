c228d3f52a33cf604399d2fa4f9a503c7492c96d
Julien Ignace <julien@unity3d.com>
2020-03-20 18:03:32 +0100

    Fixed XR global constant name

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDStringConstants.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDStringConstants.cs
@@ -269,7 +269,7 @@ namespace UnityEngine.Rendering.HighDefinition
         };
 
         public static readonly int _ShaderVariablesGlobal = Shader.PropertyToID("ShaderVariablesGlobal");
-        public static readonly int _ShaderVariablesXR = Shader.PropertyToID("_ShaderVariablesXR");
+        public static readonly int _ShaderVariablesXR = Shader.PropertyToID("ShaderVariablesXR");
         public static readonly int _ShaderVariablesVolumetric = Shader.PropertyToID("ShaderVariablesVolumetric");
         public static readonly int _ShaderVariablesLightList = Shader.PropertyToID("ShaderVariablesLightList");
 
