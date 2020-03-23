dc1b65ff32bad6b6677753c8cac12988672b5a8f
Tim Cooper <tim@unity3d.com>
2020-03-23 09:52:11 +0100

    Bump package version

--- com.unity.render-pipelines.high-definition/Runtime/Documentation.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Documentation.cs
@@ -11,7 +11,7 @@ namespace UnityEngine.Rendering.HighDefinition
         //[HelpURL(Documentation.baseURL + Documentation.version + Documentation.subURL + "some-page" + Documentation.endURL)]
         //It cannot support String.Format nor string interpolation
         internal const string baseURL = "https://docs.unity3d.com/Packages/com.unity.render-pipelines.high-definition@";
-        internal const string version = "8.0";
+        internal const string version = "8.1";
         internal const string subURL = "/manual/";
         internal const string endURL = ".html";
 
