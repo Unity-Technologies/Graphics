8015510ea6bcd4590945d415166ec9329996fb47
sebastienlagarde <sebastien@unity3d.com>
2020-03-25 23:37:10 +0100

    Revert: Deprecate Utilities namespace #6219 (#6374)

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/Camera/HDCamera.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/Camera/HDCamera.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Utilities;
 using UnityEngine.Experimental.Rendering;
 using UnityEngine.Experimental.Rendering.RenderGraphModule;
 
