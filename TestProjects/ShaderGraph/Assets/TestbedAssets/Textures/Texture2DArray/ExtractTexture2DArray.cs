0b1b0a68709ac4b138549624920c0f80267349d6
Peter Bay Bastian <peter.bastian@gmail.com>
2020-04-03 11:52:09 +0200

    Create a simple asset importer for Texture2DArray that just references an array of Texture2Ds

---
+++ TestProjects/ShaderGraph/Assets/TestbedAssets/Textures/Texture2DArray/ExtractTexture2DArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+static class ExtractTexture2DArray
+{
+    private const string AssetsExtractTextureDArray = "Assets/Extract Texture 2D Array";
+
+    [MenuItem(AssetsExtractTextureDArray)]
+    static void Execute()
+    {
+        var textureArray = (Texture2DArray) Selection.activeObject;
+        var assetPath = AssetDatabase.GetAssetPath(textureArray);
+        var basePath = Path.Combine(Path.GetDirectoryName(assetPath), Path.GetFileNameWithoutExtension(assetPath));
+        for (var arrayElement = 0; arrayElement < textureArray.depth; arrayElement++)
+        {
+            var texture = new Texture2D(textureArray.width, textureArray.height, textureArray.format, false);
+            texture.SetPixels(textureArray.GetPixels(arrayElement));
+            var tgaBytes = texture.EncodeToTGA();
+            var tgaPath = $"{basePath}{arrayElement}.tga";
+            File.WriteAllBytes(tgaPath, tgaBytes);
+        }
+        AssetDatabase.Refresh();
+    }
+
+    [MenuItem(AssetsExtractTextureDArray, true)]
+    static bool Validate()
+    {
+        return Selection.activeObject is Texture2DArray;
+    }
+}
---
+++ TestProjects/ShaderGraph/Assets/TestbedAssets/Textures/Texture2DArray/Texture2DArrayImporter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Experimental.AssetImporters;
+using UnityEditor.ProjectWindowCallback;
+using UnityEngine;
+
+[ScriptedImporter(0, "texture2darray")]
+[Serializable]
+class Texture2DArrayImporter : ScriptedImporter
+{
+    [SerializeField] private Texture2D[] m_Textures = null;
+
+    public override void OnImportAsset(AssetImportContext ctx)
+    {
+        if (m_Textures == null || m_Textures.Length < 1)
+        {
+            return;
+        }
+
+        var first = m_Textures[0];
+        var textureArray = new Texture2DArray(first.width, first.height, m_Textures.Length, TextureFormat.ARGB32, false);
+        for (var arrayElement = 0; arrayElement < textureArray.depth; arrayElement++)
+        {
+            textureArray.SetPixels(m_Textures[arrayElement].GetPixels(), arrayElement);
+        }
+
+        ctx.AddObjectToAsset("MainObject", textureArray);
+        ctx.SetMainObject(textureArray);
+    }
+
+    [MenuItem("Assets/Create/Texture 2D Array")]
+    static void CreateAsset()
+    {
+        ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0, ScriptableObject.CreateInstance<EditAction>(), "New Texture 2D Array.texture2darray", null, null);
+    }
+
+    class EditAction : EndNameEditAction
+    {
+        public override void Action(int instanceId, string pathName, string resourceFile)
+        {
+            File.WriteAllText(pathName, "");
+            AssetDatabase.Refresh();
+        }
+    }
+}
