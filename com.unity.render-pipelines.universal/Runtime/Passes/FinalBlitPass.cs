d5a8c9b86ce2a85e457a4cf91f72841866bf53d3
Jonas Sideravičius <jonass@unity3d.com>
2020-04-06 17:17:29 +0300

    fixing material preview to properly set render target

--- com.unity.render-pipelines.universal/Runtime/Passes/FinalBlitPass.cs
+++ com.unity.render-pipelines.universal/Runtime/Passes/FinalBlitPass.cs
@@ -77,7 +77,7 @@ namespace UnityEngine.Rendering.Universal.Internal
                 // TODO: Final blit pass should always blit to backbuffer. The first time we do we don't need to Load contents to tile.
                 // We need to keep in the pipeline of first render pass to each render target to propertly set load/store actions.
                 // meanwhile we set to load so split screen case works.
-                if (!useNativeRenderPass)
+                if (!useNativeRenderPass || cameraData.camera.cameraType == CameraType.Preview)
                 {
                     SetRenderTarget(
                         cmd,
