6e7902f8d9ebd1dad5502ccedac96a494578115b
Remi Chapelain <remi.chapelain@unity3d.com>
2020-04-08 14:29:20 +0200

    Enabling profiling and access to internal in HDRP

--- com.unity.render-pipelines.high-definition/Runtime/PackageInfo.cs
+++ com.unity.render-pipelines.high-definition/Runtime/PackageInfo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition-Tests.Runtime")]
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition-Tests.Editor")]
+[assembly: InternalsVisibleTo("TestRuntime")]	//To have access to internal HDPRofileId and HDCamera.profilingSampler 
