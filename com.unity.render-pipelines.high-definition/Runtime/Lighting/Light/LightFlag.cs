6e6effd8506603b853470697187d6deb5001705c
John Parsaie <johnpa@unity3d.com>
2020-04-16 12:32:57 -0400

    Add light flag plane

---
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/Light/LightFlag.cs
@@ -0,0 +1,51 @@
+﻿namespace UnityEngine.Rendering
+{
+    public class LightFlag : MonoBehaviour
+    {
+        [Tooltip("How much to feather the clipped edge")]
+        public float m_Feather = 1.0f;
+
+        [GenerateHLSL]
+        public struct LightFlagData
+        {
+            public Vector4 plane;
+
+            public float feather;
+            public Vector3 unused;
+        }
+
+        public LightFlagData FlagParams
+        {
+            get
+            {
+                return new LightFlagData
+                {
+                    plane   = GetFlagPlane(),
+                    feather = m_Feather * 0.1f
+                };
+            }
+        }
+
+        Vector4 GetFlagPlane()
+        {
+            Transform t = transform;
+            Vector3 v = t.forward;
+            float d = Vector3.Dot(t.position, v);
+            return new Vector4(v.x, v.y, v.z, d);
+        }
+
+        private void OnValidate()
+        {
+            m_Feather = Mathf.Max(0, m_Feather);
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            Matrix4x4 m = Matrix4x4.zero;
+            Transform t = transform;
+            m.SetTRS(t.position, t.rotation, new Vector3(1, 1, 0));
+            Gizmos.matrix = m;
+            Gizmos.DrawWireSphere(Vector3.zero, 1);
+        }
+    }
+}
