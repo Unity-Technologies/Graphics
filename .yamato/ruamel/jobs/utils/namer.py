9a12df5c88a9d03e4215c89c51f963eb6c40585d
Liis Kivistik <liis.kivistik@unity3d.com>
2020-04-20 13:45:11 +0200

    added hybrids

--- .yamato/ruamel/jobs/utils/namer.py
+++ .yamato/ruamel/jobs/utils/namer.py
@@ -10,7 +10,7 @@ def editor_job_id(editor_version, platform_os):
 
 # package specific
 def packages_filepath():
-    return f'{parent_dir}/packages.yml'.lower()
+    return f'{parent_dir}/_packages.yml'.lower()
 
 def package_job_id_test(package_id, platform_name, editor_version):
     return f'test_{ package_id }_{ platform_name }_{editor_version}'
@@ -50,7 +50,7 @@ def project_job_id_all(project_name, editor_version):
 
 # abv specific
 def abv_filepath():
-    return f'{parent_dir}/abv.yml'.lower()
+    return f'{parent_dir}/_abv.yml'.lower()
 
 def abv_job_id_all_project_ci(editor_version):
     return f'all_project_ci_{editor_version}'
