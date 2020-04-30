e40b57c606e23643aa3ea2442674533b17a893e5
Elvar Örn Unnþórsson <ellioman@ellioman.com>
2020-04-30 17:59:05 +0200

    Reverting PR #146 (#334)

--- com.unity.render-pipelines.universal/Shaders/Nature/SpeedTree8Passes.hlsl
+++ com.unity.render-pipelines.universal/Shaders/Nature/SpeedTree8Passes.hlsl
@@ -147,11 +147,7 @@ void InitializeData(inout SpeedTreeVertexInput input, float lodValue)
                 globalWindTime += UNITY_ACCESS_INSTANCED_PROP(STWind, _GlobalWindTime);
             #endif
 
-            // Disabling "pow(f,e) will not work for negative f"; warnings.
-            #pragma warning (disable : 3571)
             windyPosition = GlobalWind(windyPosition, treePos, true, rotatedWindVector, globalWindTime);
-            #pragma warning (enable : 3571)
-
             input.vertex.xyz = windyPosition;
         }
     #endif
