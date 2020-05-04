3202cdf80debab79f88b977988918f403f5ae18c
Liis Kivistik <liis.kivistik@unity3d.com>
2020-05-04 12:50:50 +0200

    add upm-ci back

--- .yamato/ruamel/jobs/packages/package_pack.py
+++ .yamato/ruamel/jobs/packages/package_pack.py
@@ -1,7 +1,7 @@
 from ruamel.yaml.scalarstring import DoubleQuotedScalarString as dss
 from ..shared.namer import package_job_id_pack
 from ..shared.yml_job import YMLJob
-from ..shared.constants import NPM_UPMCI_INSTALL_URL, PATH_PACKAGES
+from ..shared.constants import NPM_UPMCI_INSTALL_URL
 
 class Package_PackJob():
     
@@ -19,7 +19,7 @@ class Package_PackJob():
         job.set_agent(agent)
         job.add_commands( [
                 f'npm install upm-ci-utils@stable -g --registry {NPM_UPMCI_INSTALL_URL}',
-                f'upm-ci package pack --artifacts-path packages --package-path {package["packagename"]}'])
+                f'upm-ci package pack --package-path {package["packagename"]}'])
         job.add_artifacts_packages()
         return job
     
