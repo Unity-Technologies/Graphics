cb392e8b34b4172b3e82f0442eb5e9d79b8b0e2b
Erik Hakala <erik.hakala@unity3d.com>
2020-05-05 17:27:12 +0300

    Fix valid fields.

--- com.unity.render-pipelines.universal/Editor/ShaderGraph/Targets/UniversalLitSubTarget.cs
+++ com.unity.render-pipelines.universal/Editor/ShaderGraph/Targets/UniversalLitSubTarget.cs
@@ -450,6 +450,8 @@ namespace UnityEditor.Rendering.Universal.ShaderGraph
                 BlockFields.SurfaceDescription.Occlusion,
                 BlockFields.SurfaceDescription.Alpha,
                 BlockFields.SurfaceDescription.AlphaClipThreshold,
+                BlockFields.SurfaceDescription.CoatMask,
+                BlockFields.SurfaceDescription.CoatSmoothness,
             };
 
             public static BlockFieldDescriptor[] FragmentMeta = new BlockFieldDescriptor[]
