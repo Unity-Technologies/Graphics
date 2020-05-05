a11e1c16335deb12e6c21b82b678f003657c1c99
Alex Lindman <alexandral@unity3d.com>
2020-05-05 13:10:05 -0700

    add null check for blackboard selections when blackboard is hidden

--- com.unity.shadergraph/Editor/Drawing/Blackboard/BlackboardProvider.cs
+++ com.unity.shadergraph/Editor/Drawing/Blackboard/BlackboardProvider.cs
@@ -255,7 +255,9 @@ namespace UnityEditor.ShaderGraph.Drawing
 
         public void HandleGraphChanges(bool wasUndoRedoPerformed)
         {
-            var selection = new List<ISelectable>(blackboard.selection);
+            var selection = new List<ISelectable>();
+            if(blackboard.selection != null)
+                selection.Concat(blackboard.selection);
 
             foreach (var inputGuid in m_Graph.removedInputs)
             {
@@ -275,7 +277,7 @@ namespace UnityEditor.ShaderGraph.Drawing
                 foreach (var item in selection)
                 {
                     if (item is BlackboardFieldView blackboardFieldView)
-            {
+                    {
                         var guid = blackboardFieldView.shaderInput.referenceName;
                         oldSelectionPersistenceData.Add(guid, blackboardFieldView.viewDataKey);
                     }
