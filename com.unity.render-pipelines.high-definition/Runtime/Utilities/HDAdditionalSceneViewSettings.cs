38d7a017133f2ca194a4bd6087cf9ac738329669
Pavlos Mavridis <pavlos.mavridis@unity3d.com>
2020-05-13 13:16:43 +0200

    Small refactor to allow easily adding more prefs in the future

--- com.unity.render-pipelines.high-definition/Runtime/Utilities/HDAdditionalSceneViewSettings.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Utilities/HDAdditionalSceneViewSettings.cs
@@ -18,41 +18,95 @@ namespace UnityEngine.Rendering.HighDefinition
 #endif
         }
 
-        static readonly string k_SceneViewAntialiasingKey = "HDRP:SceneViewCamera:Antialiasing";
-        static AntialiasingMode s_SceneViewAntialiasing = AntialiasingMode.None;
-
-        public static AntialiasingMode sceneViewAntialiasing
+        // Helper class to manage editor preferences with local caching.
+        // Only supports bools, floats and ints/enums for now.
+        class CachedEditorPref<T>
         {
-            // We update the Editor prefs only when writing. Reading goes through the cached local var to ensure that reads have no overhead.
-            get => s_SceneViewAntialiasing;
-            set
+            T m_Storage;
+            string m_Key;
+
+            public T value
             {
-                s_SceneViewAntialiasing = value;
-                SetPref<AntialiasingMode>(k_SceneViewAntialiasingKey, value);
+                // We update the Editor prefs only when writing. Reading goes through the cached local var to ensure that reads have no overhead.
+                get => m_Storage;
+                set
+                {
+                    m_Storage = value;
+                    SetPref(value);
+                }
+            }
+
+            // Creates a cached editor preference using the specified key and default value
+            // Only supports floats, ints/enum or bools, so keeping it local for now
+            public CachedEditorPref(string key, T dafaultValue)
+            {
+                m_Key = key;
+                m_Storage = GetOrCreatePref(dafaultValue);
+            }
+
+            T GetOrCreatePref(T defaultValue)
+            {
+                if (EditorPrefs.HasKey(m_Key))
+                {
+                    if (typeof(T) == typeof(bool))
+                    {
+                        return (T)(object)EditorPrefs.GetBool(m_Key);
+                    }
+                    else if (typeof(T) == typeof(float))
+                    {
+                        return (T)(object)EditorPrefs.GetFloat(m_Key);
+                    }
+                    return (T)(object)EditorPrefs.GetInt(m_Key);
+                }
+                else
+                {
+                    if (typeof(T) == typeof(bool))
+                    {
+                        EditorPrefs.SetBool(m_Key, (bool)(object)defaultValue);
+                    }
+                    else if (typeof(T) == typeof(float))
+                    {
+                        EditorPrefs.SetFloat(m_Key, (float)(object)defaultValue);
+                    }
+                    else
+                    {
+                        EditorPrefs.SetInt(m_Key, (int)(object)defaultValue);
+                    }
+                    return defaultValue;
+                }
+            }
+
+            void SetPref(T value)
+            {
+                if (typeof(T) == typeof(bool))
+                    EditorPrefs.SetBool(m_Key, (bool)(object)value);
+                else if (typeof(T) == typeof(float))
+                    EditorPrefs.SetFloat(m_Key, (float)(object)value);
+                else
+                    EditorPrefs.SetInt(m_Key, (int)(object)value);
             }
         }
 
-        static readonly string k_SceneViewStopNaNsKey = "HDRP:SceneViewCamera:StopNaNs";
-        static bool s_SceneViewStopNaNs = false;
+        static CachedEditorPref<AntialiasingMode> s_SceneViewAntialiasing = new CachedEditorPref<AntialiasingMode>("HDRP:SceneViewCamera:Antialiasing", AntialiasingMode.None);
+
+        public static AntialiasingMode sceneViewAntialiasing
+        {
+            get => s_SceneViewAntialiasing.value;
+            set => s_SceneViewAntialiasing.value = value;
+        }
+
+        static CachedEditorPref<bool> s_SceneViewStopNaNs = new CachedEditorPref<bool>("HDRP:SceneViewCamera:StopNaNs", false);
 
         public static bool sceneViewStopNaNs
         {
             // We update the Editor prefs only when writing. Reading goes through the cached local var to ensure that reads have no overhead.
-            get => s_SceneViewStopNaNs;
-            set
-            {
-                s_SceneViewStopNaNs = value;
-                SetPref<bool>(k_SceneViewStopNaNsKey, value);
-            } 
+            get => s_SceneViewStopNaNs.value;
+            set => s_SceneViewStopNaNs.value = value;
         }
 
         static HDAdditionalSceneViewSettings()
         {
             SceneViewCameraWindow.additionalSettingsGui += DoAdditionalSettings;
-
-            // Initialize the scene view settings from the saved editor preferences (or register new preferences if necessary) 
-            sceneViewAntialiasing = GetOrCreatePref<AntialiasingMode>(k_SceneViewAntialiasingKey, s_SceneViewAntialiasing);
-            sceneViewStopNaNs = GetOrCreatePref<bool>(k_SceneViewStopNaNsKey, s_SceneViewStopNaNs);
         }
 
         static void DoAdditionalSettings(SceneView sceneView)
@@ -65,38 +119,6 @@ namespace UnityEngine.Rendering.HighDefinition
                 EditorGUILayout.HelpBox(Styles.HelpBox, MessageType.Info);
 
             sceneViewStopNaNs = EditorGUILayout.Toggle(Styles.StopNaNs, sceneViewStopNaNs);
-        }
-
-        static T GetOrCreatePref<T>(string key, T defaultValue)
-        {
-            if (EditorPrefs.HasKey(key))
-            {
-                if (typeof(T) == typeof(bool))
-                {
-                    return (T)(object)EditorPrefs.GetBool(key);
-                }
-                return (T)(object)EditorPrefs.GetInt(key);
-            }
-            else
-            {
-                if (typeof(T) == typeof(bool))
-                {
-                    EditorPrefs.SetBool(key, (bool)(object)defaultValue);
-                }
-                else
-                {
-                    EditorPrefs.SetInt(key, (int)(object)defaultValue);
-                }
-                return defaultValue;
-            }
-        }
-
-        static void SetPref<T>(string key, T value )
-        {
-            if (typeof(T) == typeof(bool))
-                EditorPrefs.SetBool(key, (bool)(object)value);
-            else
-                EditorPrefs.SetInt(key, (int)(object)value);
         }
     }
 #endif
