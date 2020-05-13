9e0b604799aa6f3ef0792b3689b4a28690f6a7f2
Pavlos Mavridis <pavlos.mavridis@unity3d.com>
2020-05-13 11:45:26 +0200

    Sceneview camera settings are now saved in Editor prefs

--- com.unity.render-pipelines.high-definition/CHANGELOG.md
+++ com.unity.render-pipelines.high-definition/CHANGELOG.md
@@ -585,6 +585,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - Fix error when removing DecalProjector from component contextual menu (case 1243960)
 - Fixed issue with post process when running in RGBA16 and an object with additive blending is in the scene.
 - Fixed corrupted values on LayeredLit when using Vertex Color multiply mode to multiply and MSAA is activated. 
+- Fixed issue with sceneview camera settings not being saved after Editor restart.
 
 ### Changed
 - Improve MIP selection for decals on Transparents
--- com.unity.render-pipelines.high-definition/Runtime/Utilities/HDAdditionalSceneViewSettings.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Utilities/HDAdditionalSceneViewSettings.cs
@@ -11,28 +11,46 @@ namespace UnityEngine.Rendering.HighDefinition
         {
             public static readonly GUIContent AAMode = EditorGUIUtility.TrTextContent("Camera Anti-aliasing", "The anti-alising mode that will be used in the scene view camera.");
             public static readonly GUIContent StopNaNs = EditorGUIUtility.TrTextContent("Camera Stop NaNs", "When enabled, any NaNs in the color buffer of the scene view camera will be suppressed.");
+#if UNITY_2020_2_OR_NEWER
+            public static readonly string HelpBox = "Temporal Anti - aliasing in the Scene View is only supported when Always Refresh is enabled.";
+#else
             public static readonly string HelpBox = "Temporal Anti - aliasing in the Scene View is only supported when Animated Materials are enabled.";
+#endif
         }
 
+        static readonly string k_SceneViewAntialiasingKey = "HDRP:SceneViewCamera:Antialiasing";
         static AntialiasingMode s_SceneViewAntialiasing = AntialiasingMode.None;
 
         public static AntialiasingMode sceneViewAntialiasing
         {
+            // We update the Editor prefs only when writing. Reading goes through the cached local var to ensure that reads have no overhead.
             get => s_SceneViewAntialiasing;
-            set => s_SceneViewAntialiasing = value;
+            set
+            {
+                s_SceneViewAntialiasing = value;
+                SetPref<AntialiasingMode>(k_SceneViewAntialiasingKey, value);
+            }
         }
 
+        static readonly string k_SceneViewStopNaNsKey = "HDRP:SceneViewCamera:StopNaNs";
         static bool s_SceneViewStopNaNs = false;
 
         public static bool sceneViewStopNaNs
         {
+            // We update the Editor prefs only when writing. Reading goes through the cached local var to ensure that reads have no overhead.
             get => s_SceneViewStopNaNs;
-            set => s_SceneViewStopNaNs = value;
+            set
+            {
+                s_SceneViewStopNaNs = value;
+                SetPref<bool>(k_SceneViewStopNaNsKey, value);
+            } 
         }
 
         static HDAdditionalSceneViewSettings()
         {
             SceneViewCameraWindow.additionalSettingsGui += DoAdditionalSettings;
+            sceneViewAntialiasing = GetOrCreatePref<AntialiasingMode>(k_SceneViewAntialiasingKey, AntialiasingMode.None);
+            sceneViewStopNaNs = GetOrCreatePref<bool>(k_SceneViewStopNaNsKey, false);
         }
 
         static void DoAdditionalSettings(SceneView sceneView)
@@ -40,11 +58,43 @@ namespace UnityEngine.Rendering.HighDefinition
             EditorGUILayout.Space();
             EditorGUILayout.LabelField("HD Render Pipeline", EditorStyles.boldLabel);
 
-            s_SceneViewAntialiasing = (AntialiasingMode)EditorGUILayout.EnumPopup(Styles.AAMode, s_SceneViewAntialiasing);
-            if (s_SceneViewAntialiasing == AntialiasingMode.TemporalAntialiasing)
+            sceneViewAntialiasing = (AntialiasingMode)EditorGUILayout.EnumPopup(Styles.AAMode, s_SceneViewAntialiasing);
+            if (sceneViewAntialiasing == AntialiasingMode.TemporalAntialiasing)
                 EditorGUILayout.HelpBox(Styles.HelpBox, MessageType.Info);
 
-            s_SceneViewStopNaNs = EditorGUILayout.Toggle(Styles.StopNaNs, s_SceneViewStopNaNs);
+            sceneViewStopNaNs = EditorGUILayout.Toggle(Styles.StopNaNs, s_SceneViewStopNaNs);
+        }
+
+        static T GetOrCreatePref<T>(string key, T defaultValue)
+        {
+            if (EditorPrefs.HasKey(key))
+            {
+                if (typeof(T) == typeof(bool))
+                {
+                    return (T)(object)EditorPrefs.GetBool(key);
+                }
+                return (T)(object)EditorPrefs.GetInt(key);
+            }
+            else
+            {
+                if (typeof(T) == typeof(bool))
+                {
+                    EditorPrefs.SetBool(key, (bool)(object)defaultValue);
+                }
+                else
+                {
+                    EditorPrefs.SetInt(key, (int)(object)defaultValue);
+                }
+                return defaultValue;
+            }
+        }
+
+        static void SetPref<T>(string key, T value )
+        {
+            if (typeof(T) == typeof(bool))
+                EditorPrefs.SetBool(key, (bool)(object)value);
+            else
+                EditorPrefs.SetInt(key, (int)(object)value);
         }
     }
 #endif
