366dd23224e95f1900e5d4782055fe81131f3093
Ricardas Jonaitis <richard.jonaitis@gmail.com>
2020-05-15 15:26:15 +0300

    Update adaptive performance package version and defines

--- com.unity.render-pipelines.universal/Runtime/UniversalRenderPipeline.cs
+++ com.unity.render-pipelines.universal/Runtime/UniversalRenderPipeline.cs
@@ -184,7 +184,7 @@ namespace UnityEngine.Rendering.Universal
             }
 
             InitializeCameraData(camera, additionalCameraData, true, out var cameraData);
-#if ADAPTIVE_PERFORMANCE_2_1_0_OR_NEWER
+#if ADAPTIVE_PERFORMANCE_2_0_0_OR_NEWER
             ApplyAdaptivePerformance(ref cameraData);
 #endif
             RenderSingleCamera(context, cameraData, cameraData.postProcessEnabled);
@@ -233,7 +233,7 @@ namespace UnityEngine.Rendering.Universal
                 var cullResults = context.Cull(ref cullingParameters);
                 InitializeRenderingData(asset, ref cameraData, ref cullResults, anyPostProcessingEnabled, out var renderingData);
 
-#if ADAPTIVE_PERFORMANCE_2_1_0_OR_NEWER
+#if ADAPTIVE_PERFORMANCE_2_0_0_OR_NEWER
                 ApplyAdaptivePerformance(ref renderingData);
 #endif
 
@@ -327,7 +327,7 @@ namespace UnityEngine.Rendering.Universal
 #endif
             UpdateVolumeFramework(baseCamera, baseCameraAdditionalData);
             InitializeCameraData(baseCamera, baseCameraAdditionalData, !isStackedRendering, out var baseCameraData);
-#if ADAPTIVE_PERFORMANCE_2_1_0_OR_NEWER
+#if ADAPTIVE_PERFORMANCE_2_0_0_OR_NEWER
             ApplyAdaptivePerformance(ref baseCameraData);
 #endif
             RenderSingleCamera(context, baseCameraData, anyPostProcessingEnabled);
@@ -818,7 +818,7 @@ namespace UnityEngine.Rendering.Universal
             Shader.SetGlobalVector(PerFrameBuffer._SubtractiveShadowColor, CoreUtils.ConvertSRGBToActiveColorSpace(RenderSettings.subtractiveShadowColor));
         }
 
-#if ADAPTIVE_PERFORMANCE_2_1_0_OR_NEWER
+#if ADAPTIVE_PERFORMANCE_2_0_0_OR_NEWER
         static void ApplyAdaptivePerformance(ref CameraData cameraData)
         {
             var noFrontToBackOpaqueFlags = SortingCriteria.SortingLayer | SortingCriteria.RenderQueue | SortingCriteria.OptimizeStateChanges | SortingCriteria.CanvasOrder;
