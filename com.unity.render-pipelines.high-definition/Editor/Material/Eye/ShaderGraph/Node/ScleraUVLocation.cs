fad8047efa8b136d949404e0d0097b72e5e0b0db
Anis <anis@unity3d.com>
2020-05-19 17:03:16 +0200

    Added shader graph nodes for rendering a complex eye shader as well as a sample shader graph (and a test scene)

---
+++ com.unity.render-pipelines.high-definition/Editor/Material/Eye/ShaderGraph/Node/ScleraUVLocation.cs
@@ -0,0 +1,37 @@
+using System.Reflection;
+using UnityEngine;
+
+namespace UnityEditor.ShaderGraph
+{
+    [Title("Utility", "High Definition Render Pipeline", "Eye", "ScleraUVLocation")]
+    class ScleraUVLocation : CodeFunctionNode
+    {
+        public ScleraUVLocation()
+        {
+            name = "Sclera UV Location";
+        }
+
+        public override bool hasPreview
+        {
+            get { return false; }
+        }
+
+        protected override MethodInfo GetFunctionToConvert()
+        {
+            return GetType().GetMethod("Unity_ScleraUVLocation", BindingFlags.Static | BindingFlags.NonPublic);
+        }
+
+        static string Unity_ScleraUVLocation(
+            [Slot(0, Binding.None, 0, 0, 0, 0)] Vector3 PositionOS,
+            [Slot(2, Binding.None)] out Vector2 ScleraUV)
+        {
+            ScleraUV = Vector2.zero;
+            return
+                @"
+                {
+                    ScleraUV =  PositionOS.xy + $precision2(0.5, 0.5);
+                }
+                ";
+        }
+    }
+}
