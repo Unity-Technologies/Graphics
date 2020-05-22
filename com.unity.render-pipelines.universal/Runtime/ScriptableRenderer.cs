8c9eecd8ef22998043252d6cc8b190bdee89dd24
Aras Pranckevičius <aras@unity3d.com>
2020-05-22 15:53:16 -0700

    Enable "Shaded Wireframe" scene view mode in HDRP & URP (#356)
    * Enable "Shaded Wireframe" scene view mode in HDRP & URP
    
    Will need graphics/srp/textured-wire codebase branch to work
    
    * Use new DrawWireOverlay API point to render wireframe
    
    This way it's not coupled to Gizmos rendering or not
    
    Co-authored-by: Sebastien Lagarde <sebastien@unity3d.com>

--- com.unity.render-pipelines.universal/Runtime/ScriptableRenderer.cs
+++ com.unity.render-pipelines.universal/Runtime/ScriptableRenderer.cs
@@ -440,6 +440,7 @@ namespace UnityEngine.Rendering.Universal
                 EndXRRendering(context, renderingData, eyeIndex);
             }
 
+            DrawWireOverlay(context, camera);
             DrawGizmos(context, camera, GizmoSubset.PostImageEffects);
 
             InternalFinishRendering(context, cameraData.resolveFinalTarget);
@@ -852,6 +853,14 @@ namespace UnityEngine.Rendering.Universal
 #if UNITY_EDITOR
             if (UnityEditor.Handles.ShouldRenderGizmos())
                 context.DrawGizmos(camera, gizmoSubset);
+#endif
+        }
+
+        [Conditional("UNITY_EDITOR")]
+        void DrawWireOverlay(ScriptableRenderContext context, Camera camera)
+        {
+#if UNITY_EDITOR
+            context.DrawWireOverlay(camera);
 #endif
         }
 
