8aa226a1d9ca8dd387cb4305bef553f2ea9077c3
Nicolaj Zøllner <nicolaj.zollner@unity3d.com>
2020-05-25 15:01:50 +0200

    Added define flag for calculating positionWS when shadow fade is applied

--- com.unity.render-pipelines.universal/ShaderLibrary/Shadows.hlsl
+++ com.unity.render-pipelines.universal/ShaderLibrary/Shadows.hlsl
@@ -22,7 +22,7 @@
     #endif
 #endif
 
-#if defined(_ADDITIONAL_LIGHTS) || defined(_MAIN_LIGHT_SHADOWS_CASCADE)
+#if defined(_ADDITIONAL_LIGHTS) || defined(_MAIN_LIGHT_SHADOWS_CASCADE) || defined(FADE_SHADOWS)
     #define REQUIRES_WORLD_SPACE_POS_INTERPOLATOR
 #endif
 
