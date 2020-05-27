b2b46acb69e6d040272ee40d15c31f6606d2e640
Martin Thorzen <35328557+martint-unity@users.noreply.github.com>
2020-05-27 13:55:00 +0200

    fix for case 1241218 (#432)
    Added list of serialized objects instead of a bool to check if it is needing to go into the Init method or not.

--- com.unity.render-pipelines.universal/CHANGELOG.md
+++ com.unity.render-pipelines.universal/CHANGELOG.md
@@ -189,6 +189,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - Fixed SceneView Draw Modes not being properly updated after opening new scene view panels or changing the editor layout.
 - Fixed GLES shaders compilation failing on Windows platform (not a mobile platform) due to uniform count limit.
 - Fixed an issue that caused the inverse view and projection matrix to output wrong values in some platforms. [case 1243990](https://issuetracker.unity3d.com/issues/urp-8-dot-1-breaks-unity-matrix-i-vp)
+- Fixed an issue where Stencil settings wasn't serialized properly in sub object [case 1241218](https://issuetracker.unity3d.com/issues/stencil-overrides-in-urp-7-dot-3-1-render-objects-does-not-save-or-apply)
 - Fixed an issue with not being able to remove Light Mode Tags [case 1240895](https://issuetracker.unity3d.com/issues/urp-unable-to-remove-added-lightmode-tags-of-filters-property-in-render-object)
 - Fixed an issue where preset button could still be used, when it is not supposed to. [case 1246261](https://issuetracker.unity3d.com/issues/urp-reset-functionality-does-not-work-for-renderobject-preset-asset)
 - Fixed issue with Model Importer materials using the Legacy standard shader instead of URP's Lit shader when import happens at Editor startup.
--- com.unity.render-pipelines.universal/Editor/RenderStateDataEditor.cs
+++ com.unity.render-pipelines.universal/Editor/RenderStateDataEditor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
 namespace UnityEditor.Rendering.Universal
@@ -27,8 +28,6 @@ namespace UnityEditor.Rendering.Universal
                 new GUIContent("Z Fail", "What happens the the stencil value when failing Z testing.");
         }
 
-        private bool firstTime = true;
-
         //Stencil rendering
         private const int stencilBits = 4;
         private const int minStencilValue = 0;
@@ -41,7 +40,7 @@ namespace UnityEditor.Rendering.Universal
         private SerializedProperty m_StencilPass;
         private SerializedProperty m_StencilFail;
         private SerializedProperty m_StencilZFail;
-
+        private List<SerializedObject> m_properties = new List<SerializedObject>();
         void Init(SerializedProperty property)
         {
             //Stencil
@@ -52,12 +51,12 @@ namespace UnityEditor.Rendering.Universal
             m_StencilFail = property.FindPropertyRelative("failOperation");
             m_StencilZFail = property.FindPropertyRelative("zFailOperation");
 
-            firstTime = false;
+            m_properties.Add(property.serializedObject);
         }
 
         public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
         {
-            if(firstTime)
+            if(!m_properties.Contains(property.serializedObject))
                 Init(property);
 
             rect.height = EditorGUIUtility.singleLineHeight;
