5cae6d4989ad8944d62349c0dc5138e86ebc73ce
martint-unity <martint@unity3d.com>
2020-06-02 13:33:30 +0200

    Added tests for cascades and removed old test

---
+++ TestProjects/UniversalGraphicsTest/Assets/CommonAssets/Scripts/ChangeShadowCascadeSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class ChangeShadowCascadeSettings : MonoBehaviour
+{
+    public ShadowCascadesOption cascadeOption;
+    ShadowCascadesOption prevCascadeOption;
+
+    void Awake()
+    {
+        UniversalRenderPipelineAsset asset = GraphicsSettings.renderPipelineAsset as UniversalRenderPipelineAsset;
+        prevCascadeOption = asset.shadowCascadeOption;
+        asset.shadowCascadeOption = cascadeOption;
+    }
+
+    void OnDestroy()
+    {
+        UniversalRenderPipelineAsset asset = GraphicsSettings.renderPipelineAsset as UniversalRenderPipelineAsset;
+        asset.shadowCascadeOption = prevCascadeOption;
+    }
+}
