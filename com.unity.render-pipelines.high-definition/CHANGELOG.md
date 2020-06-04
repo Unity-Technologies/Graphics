f43c99367c8f4eff246055f85426b956ed26c9fa
Antoine Lelievre <antoinel@unity3d.com>
2020-06-04 11:12:39 +0200

    Custom Pass API part 2 (#227)
    * Begin to write the new custom pass API
    
    * Begin to add custom pass utils shader
    
    * Begin to add custom pass API test scenes
    
    * Fixes for the Gaussian blur of the custom pass API
    
    * Fix blur release
    
    * Added custom pass copy test and fixed blur
    
    * Added more custom pass documentation
    
    * Added custom pass API graphic test reference image
    
    * Added more obsolete things
    
    * fix function visibility
    
    * Added the API to override camera rendering
    
    * Updated changelog
    
    * Updated changelog
    
    * Fixed compilation issue
    
    * Added custom pass API test scene
    
    * Begin to add custom pass override test
    
    * Factorized protection function
    
    * Update custom pass test scene
    
    * Fixed custom pass API doc
    
    * Begin to add camera override tests
    
    * Fixed camera override in scene view
    
    * Finished RenderFrom API fucntions
    
    * Fixed XR disable class
    
    * Added isPersistant bool in HDCamera and allocateHistoryBuffers param in Update
    
    * Fix MSAA for before pre refraction and added errors for fullscreen MSAA operations
    
    * Finished to fix the RenderFrom* functions
    
    * Moved obsolete execute function into the new one
    
    * Update reference images
    
    * Added all the API doc
    
    * Updated changelog
    
    * revert mats
    
    * Updated custom clear pass to use the new API
    
    * Fixed depth buffer random read/write error
    
    * Disable custom pass blur test part
    
    * Revert camera pixel rect changes
    
    * Rename multi-pass to single pass
    
    * Disable custom pass test on vulkan and XR
    
    Co-authored-by: Sebastien Lagarde <sebastien@unity3d.com>

--- com.unity.render-pipelines.high-definition/CHANGELOG.md
+++ com.unity.render-pipelines.high-definition/CHANGELOG.md
@@ -132,6 +132,9 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - Added few exposure debug modes.
 - Added support for multiple path-traced views at once (e.g., scene and game views).
 - Added custom target mid grey for auto exposure.
+- Added CustomPassUtils API to simplify Blur, Copy and DrawRenderers custom passes.
+- Added an API in HDRP to override the camera within the rendering of a frame (mainly for custom pass).
+- Added more custom pass API functions, mainly to render objects from another camera.
 
 ### Fixed
 - Fix when rescale probe all direction below zero (1219246)
--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.cs
@@ -649,7 +649,7 @@ namespace UnityEngine.Rendering.HighDefinition
             m_CameraSssDiffuseLightingBuffer = RTHandles.Alloc(Vector2.one, TextureXR.slices, dimension: TextureXR.dimension, colorFormat: GraphicsFormat.B10G11R11_UFloatPack32, enableRandomWrite: true, useDynamicScale: true, name: "CameraSSSDiffuseLighting");
 
             m_CustomPassColorBuffer = new Lazy<RTHandle>(() => RTHandles.Alloc(Vector2.one, TextureXR.slices, dimension: TextureXR.dimension, colorFormat: GetCustomBufferFormat(), enableRandomWrite: true, useDynamicScale: true, name: "CustomPassColorBuffer"));
-            m_CustomPassDepthBuffer = new Lazy<RTHandle>(() => RTHandles.Alloc(Vector2.one, TextureXR.slices, dimension: TextureXR.dimension, colorFormat: GraphicsFormat.R32_UInt, enableRandomWrite: true, useDynamicScale: true, name: "CustomPassDepthBuffer", depthBufferBits: DepthBits.Depth32));
+            m_CustomPassDepthBuffer = new Lazy<RTHandle>(() => RTHandles.Alloc(Vector2.one, TextureXR.slices, dimension: TextureXR.dimension, colorFormat: GraphicsFormat.R32_UInt, useDynamicScale: true, name: "CustomPassDepthBuffer", depthBufferBits: DepthBits.Depth32));
 
             m_DistortionBuffer = RTHandles.Alloc(Vector2.one, TextureXR.slices, dimension: TextureXR.dimension, colorFormat: Builtin.GetDistortionBufferFormat(), useDynamicScale: true, name: "Distortion");
 
