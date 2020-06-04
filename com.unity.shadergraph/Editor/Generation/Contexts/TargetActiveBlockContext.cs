1b8100c0f7b74d302a9d7aac134fcdd46982d912
Antoine Lelièvre <antoinel@unity3d.com>
2020-06-04 12:15:41 +0200

    Added pass descriptor in target active block context

--- com.unity.shadergraph/Editor/Generation/Contexts/TargetActiveBlockContext.cs
+++ com.unity.shadergraph/Editor/Generation/Contexts/TargetActiveBlockContext.cs
@@ -7,11 +7,13 @@ namespace UnityEditor.ShaderGraph
     {
         public List<BlockFieldDescriptor> activeBlocks { get; private set; }
         public List<BlockFieldDescriptor> currentBlocks { get; private set; }
+        public PassDescriptor? pass { get; private set; }
 
-        public TargetActiveBlockContext(List<BlockFieldDescriptor> currentBlocks)
+        public TargetActiveBlockContext(List<BlockFieldDescriptor> currentBlocks, PassDescriptor? pass)
         {
             activeBlocks = new List<BlockFieldDescriptor>();
             this.currentBlocks = currentBlocks;
+            this.pass = pass;
         }
 
         public void AddBlock(BlockFieldDescriptor block, bool conditional = true)
