170e059cd67c3658c18deb6e8bf9551cbfa8a44e
Chris Tchou <ctchou@unity3d.com>
2020-06-08 10:22:24 -0700

    Cherry picking VTPropertyChange branch changes from 10.x.x/sg/VTPropertyChange into sg/master-stack
    Cherry pick of diff between these commits and 10.x.x/sg/master-stack2
    
    commit 67031d0363fdb2c5f52a81c6b3b25896665844a2
    Author: Chris Tchou <ctchou@unity3d.com>
    Date:   Fri Jun 5 13:47:49 2020 -0700
    
        Hooked up TextureType to VTProperty setting, removed old SampleVT settings
    
        verified works in both VT enabled and VT disabled scenarios
    
    commit ff95056ada5f393ab5a8292685376634fd1dd4ef
    Author: Chris Tchou <ctchou@unity3d.com>
    Date:   Fri Jun 5 10:13:47 2020 -0700
    
        Reorganized VT property UI, added Texture Type

--- com.unity.shadergraph/Editor/Data/Graphs/SerializableVirtualTexture.cs
+++ com.unity.shadergraph/Editor/Data/Graphs/SerializableVirtualTexture.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 
 namespace UnityEditor.ShaderGraph.Internal
 {
+    // TODO: make TextureType work like this, combine the two enums (needs versioning for old data)
+    enum LayerTextureType
+    {
+        Default,
+        NormalTangentSpace,
+        NormalObjectSpace
+    };
+
     [Serializable]
-    public class SerializableVirtualTextureLayer
+    internal class SerializableVirtualTextureLayer
     {
         public string layerName;
         public string layerRefName;
         public SerializableTexture layerTexture;
+        public LayerTextureType layerTextureType;
 
         public SerializableVirtualTextureLayer(string name, string refName, SerializableTexture texture)
         {
             this.layerName = name;
             this.layerRefName = refName;
             this.layerTexture = texture;
+            this.layerTextureType = LayerTextureType.Default;
         }
     }
 
     [Serializable]
-    public sealed class SerializableVirtualTexture : ISerializationCallbackReceiver
+    internal sealed class SerializableVirtualTexture : ISerializationCallbackReceiver
     {
         [SerializeField]
         public List<SerializableVirtualTextureLayer> layers = new List<SerializableVirtualTextureLayer>();
