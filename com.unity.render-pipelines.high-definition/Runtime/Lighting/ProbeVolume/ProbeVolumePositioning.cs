12b43cc83398d2bb8ecc22cd388ddceac6f3b4b6
Rasmus Rønn Nielsen <rasmusn@unity3d.com>
2020-06-08 14:04:13 +0200

    Port over Adaptive Probe Volume code from branch lighting/adaptive-probevolumes-draft.
    This reorganization lets us work on Adaptive Probe Volumes in context of Env2.0 without having to wait for Env2.0 to update to HDRP master.

---
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/ProbeVolume/ProbeVolumePositioning.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+
+namespace UnityEngine.Rendering.HighDefinition
+{
+    using Brick = ProbeBrickIndex.Brick;
+    using Flags = ProbeReferenceVolume.BrickFlags;
+    using Volume = ProbeReferenceVolume.Volume;
+    using RefTrans = ProbeReferenceVolume.RefVolTransform;
+
+    internal static class ProbeVolumePositioning
+    {
+        internal static Vector3[] m_Axes = new Vector3[6];
+
+        internal static void SubdivisionAlgorithm(RefTrans refTrans, List<Brick> inBricks, List<Flags> outFlags)
+        {
+            Flags f = new Flags();
+            for( int i = 0; i < inBricks.Count; i++ )
+            {
+                if( ShouldKeepBrick( ref refTrans, inBricks[i] ) )
+                {
+                    f.discard = false;
+                    f.subdivide = true;
+                }
+                else
+                {
+                    f.discard = true;
+                    f.subdivide = false;
+                }
+                outFlags.Add(f);
+            }
+        }
+
+        // TODO: Add subdivision criteria here,
+        // currently just keeps subdividing inside probe volumes
+        internal static bool ShouldKeepBrick(ref RefTrans refTrans, Brick brick)
+        {
+            return true;
+        }
+
+        // TODO: Take refvol translation and rotation into account
+        internal static bool OBBIntersect(ref RefTrans refTrans, Brick brick, ref Volume volume)
+        {
+            float scaledSize = Mathf.Pow(3, brick.size);
+            Vector3 scaledPos = refTrans.refSpaceToWS.MultiplyPoint(brick.position);
+
+            Volume bounds;
+            bounds.Corner = scaledPos;
+            bounds.X = refTrans.refSpaceToWS.GetColumn(0) * scaledSize;
+            bounds.Y = refTrans.refSpaceToWS.GetColumn(1) * scaledSize;
+            bounds.Z = refTrans.refSpaceToWS.GetColumn(2) * scaledSize;
+
+            return OBBIntersect(ref bounds, ref volume);
+        }
+
+        internal static bool OBBIntersect(ref Volume a, ref Volume b)
+        {
+            m_Axes[0] = a.X.normalized;
+            m_Axes[1] = a.Y.normalized;
+            m_Axes[2] = a.Z.normalized;
+            m_Axes[3] = b.X.normalized;
+            m_Axes[4] = b.Y.normalized;
+            m_Axes[5] = b.Z.normalized;
+
+            foreach (Vector3 axis in m_Axes)
+            {
+                Vector2 aProj = ProjectOBB(ref a, axis);
+                Vector2 bProj = ProjectOBB(ref b, axis);
+
+                if (aProj.y < bProj.x || bProj.y < aProj.x)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static Vector2 ProjectOBB(ref Volume a, Vector3 axis)
+        {
+            float min = Vector3.Dot(axis, a.Corner);
+            float max = min;
+
+            for (int x = 0; x < 2; x++)
+            {
+                for (int y = 0; y < 2; y++)
+                {
+                    for (int z = 0; z < 2; z++)
+                    {
+                        Vector3 vert = a.Corner + a.X * x + a.Y * y + a.Z * z;
+
+                        float proj = Vector3.Dot(axis, vert);
+
+                        if (proj < min)
+                        {
+                            min = proj;
+                        }
+                        else if (proj > max)
+                        {
+                            max = proj;
+                        }
+                    }
+                }
+            }
+
+            return new Vector2(min, max);
+        }
+    }
+}
---
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/ProbeVolume/SphericalHarmonics.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace UnityEngine.Rendering.HighDefinition
+{
+    public struct SphericalHarmonicsL1
+    {
+        public Vector4 shAr;
+        public Vector4 shAg;
+        public Vector4 shAb;
+
+        public static SphericalHarmonicsL1 GetNeutralValues()
+        {
+            SphericalHarmonicsL1 sh;
+            sh.shAr = Vector4.zero;
+            sh.shAg = Vector4.zero;
+            sh.shAb = Vector4.zero;
+            return sh;
+        }
+    }
+}
