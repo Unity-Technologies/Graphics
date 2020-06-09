6fa7747f5e83d9a26fe921338cc13a3dbf2e30f8
Sebastien Lagarde <sebastien@unity3d.com>
2020-06-09 20:44:55 +0200

    Revert "Fix vulkan and metal"
    This reverts commit 367ad170d80681701f789f0ef9cbb89f5a420601.

--- com.unity.render-pipelines.high-definition/Editor/Material/ShaderGraph/HDShaderPasses.cs
+++ com.unity.render-pipelines.high-definition/Editor/Material/ShaderGraph/HDShaderPasses.cs
@@ -776,7 +776,7 @@ namespace UnityEditor.Rendering.HighDefinition.ShaderGraph
             { Pragma.Target(ShaderModel.Target45) },
             { Pragma.Vertex("Vert") },
             { Pragma.Fragment("Frag") },
-            { Pragma.OnlyRenderers(new Platform[] {Platform.D3D11, Platform.Playstation, Platform.XboxOne, Platform.Vulkan, Platform.Metal, Platform.Switch}) },
+            { Pragma.OnlyRenderers(new Platform[] {Platform.D3D11}) },
         };
 
         public static IncludeCollection RayTracingPrepassIncludes = new IncludeCollection
