86a3a0b177c05a081a2a892b4b9b2988e7fb7330
Sophia <sophia@unity3d.com>
2020-06-10 14:58:23 +0200

    fix dependencies for promote_all

--- .yamato/ruamel/jobs/preview_publish/pb_promote_all_preview.py
+++ .yamato/ruamel/jobs/preview_publish/pb_promote_all_preview.py
@@ -11,10 +11,15 @@ class PreviewPublish_PromoteAllPreviewJob():
 
     def get_job_definition(self, packages, target_branch, auto_publish):
 
+        dependencies = [f'{pb_filepath()}#{pb_job_id_publish_all_preview()}']
+
+        for package in packages:
+            dependencies.append(f'{pb_filepath()}#{pb_job_id_promote(package["name"])}')
+
         # construct job
         job = YMLJob()
         job.set_name(f'Promote all preview packages - nightly')
-        job.add_dependencies([f'{pb_filepath()}#{pb_job_id_promote(package["name"])}' for package in packages])
+        job.add_dependencies(dependencies)
         # if auto_publish is True:
         #     job.add_trigger_recurrent(target_branch, 'daily')
         return job
