d8b7cfa1fb9f75195fb703693389f80badb9ebfb
Sophia <sophia@unity3d.com>
2020-06-10 15:01:41 +0200

    Revert "fix dependencies for promote_all"
    This reverts commit 86a3a0b177c05a081a2a892b4b9b2988e7fb7330.

--- .yamato/ruamel/jobs/preview_publish/pb_promote_all_preview.py
+++ .yamato/ruamel/jobs/preview_publish/pb_promote_all_preview.py
@@ -11,15 +11,10 @@ class PreviewPublish_PromoteAllPreviewJob():
 
     def get_job_definition(self, packages, target_branch, auto_publish):
 
-        dependencies = [f'{pb_filepath()}#{pb_job_id_publish_all_preview()}']
-
-        for package in packages:
-            dependencies.append(f'{pb_filepath()}#{pb_job_id_promote(package["name"])}')
-
         # construct job
         job = YMLJob()
         job.set_name(f'Promote all preview packages - nightly')
-        job.add_dependencies(dependencies)
+        job.add_dependencies([f'{pb_filepath()}#{pb_job_id_promote(package["name"])}' for package in packages])
         # if auto_publish is True:
         #     job.add_trigger_recurrent(target_branch, 'daily')
         return job
