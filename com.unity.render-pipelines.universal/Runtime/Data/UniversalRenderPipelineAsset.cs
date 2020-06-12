03e3f44821af0f1f6b47d671f16bd87ff00b764d
Ricardas Jonaitis <richard.jonaitis@gmail.com>
2020-06-12 10:11:11 +0300

    [7.x.x] backport #375 Add adaptive performance as optional dependency (#514)
    * Adding adaptive performance as optional dependency
    
    * Fix adaptive performance version and defines
    
    * Add enable flag for adaptive performance in URP asset
    
    * Add adaptive performance documentation
    
    * Update changelog.md
    
    * Fix spelling error
    
    Co-authored-by: Lukas Chodosevicius <lukasc@unity3d.com>

--- com.unity.render-pipelines.universal/Runtime/Data/UniversalRenderPipelineAsset.cs
+++ com.unity.render-pipelines.universal/Runtime/Data/UniversalRenderPipelineAsset.cs
@@ -190,6 +190,9 @@ namespace UnityEngine.Rendering.Universal
         [SerializeField] bool m_MixedLightingSupported = true;
         [SerializeField] PipelineDebugLevel m_DebugLevel = PipelineDebugLevel.Disabled;
 
+        // Adaptive performance settings
+        [SerializeField] bool m_UseAdaptivePerformance = false;
+
         // Post-processing settings
 #pragma warning disable 414 // 'field' is assigned but never used
         [SerializeField] PostProcessingFeatureSet m_PostProcessingFeatureSet = PostProcessingFeatureSet.Integrated;
@@ -685,6 +688,16 @@ namespace UnityEngine.Rendering.Universal
         {
             get { return m_ColorGradingLutSize; }
             set { m_ColorGradingLutSize = Mathf.Clamp(value, k_MinLutSize, k_MaxLutSize); }
+        }
+
+       /// <summary>
+       /// Set to true to allow Adaptive performance to modify graphics quality settings during runtime.
+       /// Only applicable when Adaptive performance package is available.
+       /// </summary>
+        public bool useAdaptivePerformance
+        {
+            get { return m_UseAdaptivePerformance; }
+            set { m_UseAdaptivePerformance = value; }
         }
 
         public override Material defaultMaterial
