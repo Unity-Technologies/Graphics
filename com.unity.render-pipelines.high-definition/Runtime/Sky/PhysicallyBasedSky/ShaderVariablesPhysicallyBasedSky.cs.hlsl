38516696702fdef86e340d212a9088e3530721ce
Sebastien Lagarde <sebastien@unity3d.com>
2020-06-15 19:11:30 +0200

    Update ShaderVariablesPhysicallyBasedSky.cs.hlsl

--- com.unity.render-pipelines.high-definition/Runtime/Sky/PhysicallyBasedSky/ShaderVariablesPhysicallyBasedSky.cs.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Sky/PhysicallyBasedSky/ShaderVariablesPhysicallyBasedSky.cs.hlsl
@@ -2,10 +2,10 @@
 // This file was automatically generated. Please don't edit by hand.
 //
 
-#ifndef PHYSICALLYBASEDSKYRENDERER_CS_HLSL
-#define PHYSICALLYBASEDSKYRENDERER_CS_HLSL
+#ifndef SHADERVARIABLESPHYSICALLYBASEDSKY_CS_HLSL
+#define SHADERVARIABLESPHYSICALLYBASEDSKY_CS_HLSL
 //
-// UnityEngine.Rendering.HighDefinition.PhysicallyBasedSkyRenderer+PbrSkyConfig:  static fields
+// UnityEngine.Rendering.HighDefinition.PbrSkyConfig:  static fields
 //
 #define PBRSKYCONFIG_GROUND_IRRADIANCE_TABLE_SIZE (256)
 #define PBRSKYCONFIG_IN_SCATTERED_RADIANCE_TABLE_SIZE_X (128)
@@ -13,7 +13,7 @@
 #define PBRSKYCONFIG_IN_SCATTERED_RADIANCE_TABLE_SIZE_Z (16)
 #define PBRSKYCONFIG_IN_SCATTERED_RADIANCE_TABLE_SIZE_W (64)
 
-// Generated from UnityEngine.Rendering.HighDefinition.PhysicallyBasedSkyRenderer+ShaderVariablesPhysicallyBasedSky
+// Generated from UnityEngine.Rendering.HighDefinition.ShaderVariablesPhysicallyBasedSky
 // PackingRules = Exact
 GLOBAL_CBUFFER_START(ShaderVariablesPhysicallyBasedSky, b2)
     float _PlanetaryRadius;
