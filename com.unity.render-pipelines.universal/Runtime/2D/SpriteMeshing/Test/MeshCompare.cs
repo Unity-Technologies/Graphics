a9b6e5dfd33864bd694c05261b475ad578625495
chris.chu <chris.chu@unity3d.com>
2020-06-18 14:15:59 +0800

    Updated mesh compare to support sprite mesh comparing

--- com.unity.render-pipelines.universal/Runtime/2D/SpriteMeshing/Test/MeshCompare.cs
+++ com.unity.render-pipelines.universal/Runtime/2D/SpriteMeshing/Test/MeshCompare.cs
@@ -7,22 +7,22 @@ namespace UnityEngine.Experimental.Rendering.Universal
     [ExecuteInEditMode]
     public class MeshCompare : MonoBehaviour
     {
-        public SpriteRenderer m_SpriteRenderer;
-        public MeshRenderer m_MeshRenderer;
+        public SpriteRenderer m_SpriteRenderer0;
+        public SpriteRenderer m_SpriteRenderer1;
         public bool m_Compare;
-        public int m_SpriteVertices;
-        public int m_SpriteTriangles;
-        public float m_SpriteArea;
-        public int m_MeshVertices;
-        public int m_MeshTriangles;
-        public float m_MeshArea;
+        public int m_SpriteVertices0;
+        public int m_SpriteTriangles0;
+        public float m_SpriteArea0;
+        public int m_SpriteVertices1;
+        public int m_SpriteTriangles1;
+        public float m_SpriteArea1;
 
         public float m_TrianglesReducedTo;
         public float m_VerticesReducedTo;
         public float m_AreaReducedTo;
 
-        SpriteRenderer m_PrevSpriteRenderer;
-        MeshRenderer m_PrevMeshRenderer;
+        SpriteRenderer m_PrevSpriteRenderer0;
+        SpriteRenderer m_PrevSpriteRenderer1;
 
 
         //area += (int) prevPoint.x * (int) curPoint.y - (int) curPoint.x* (int) prevPoint.y;
@@ -40,10 +40,10 @@ namespace UnityEngine.Experimental.Rendering.Universal
 
 
 
-        void GetSpriteStats(out int spriteVertices, out int spriteTriangles, out float spriteArea)
+        void GetSpriteStats(SpriteRenderer spriteRenderer, out int spriteVertices, out int spriteTriangles, out float spriteArea)
         {
-            Vector2[] vertices = m_SpriteRenderer.sprite.vertices;
-            ushort[] triangles = m_SpriteRenderer.sprite.triangles;
+            Vector2[] vertices = spriteRenderer.sprite.vertices;
+            ushort[] triangles = spriteRenderer.sprite.triangles;
 
             spriteVertices = vertices.Length;
             spriteTriangles = triangles.Length / 3;
@@ -55,38 +55,22 @@ namespace UnityEngine.Experimental.Rendering.Universal
             }
         }
 
-        void GetMeshStats(out int meshVertices, out int meshTriangles, out float meshArea)
-        {
-            MeshFilter mf = m_MeshRenderer.GetComponent<MeshFilter>();
-            Vector3[] vertices = mf.sharedMesh.vertices;
-            int[] triangles = mf.sharedMesh.triangles;
-
-            meshVertices = vertices.Length;
-            meshTriangles = triangles.Length / 3;
-
-            meshArea = 0;
-            for (int i = 0; i < triangles.Length; i += 3)
-            {
-                meshArea += ComputeArea(vertices[triangles[i]], vertices[triangles[i + 1]], vertices[triangles[i + 2]]);
-            }
-        }
-
         void Update()
         {
-            if (m_SpriteRenderer != null && m_MeshRenderer != null && (m_SpriteRenderer != m_PrevSpriteRenderer || m_MeshRenderer != m_PrevMeshRenderer || m_Compare))
+            if (m_SpriteRenderer0 != null && m_SpriteRenderer1 != null && (m_SpriteRenderer0 != m_PrevSpriteRenderer0 || m_SpriteRenderer1 != m_PrevSpriteRenderer1 || m_Compare))
             {
                 m_Compare = false;
 
-                GetSpriteStats(out m_SpriteVertices, out m_SpriteTriangles, out m_SpriteArea);
-                GetMeshStats(out m_MeshVertices, out m_MeshTriangles, out m_MeshArea);
+                GetSpriteStats(m_SpriteRenderer0, out m_SpriteVertices0, out m_SpriteTriangles0, out m_SpriteArea0);
+                GetSpriteStats(m_SpriteRenderer1, out m_SpriteVertices1, out m_SpriteTriangles1, out m_SpriteArea1);
 
-                m_VerticesReducedTo = (float)m_MeshVertices / (float)m_SpriteVertices;
-                m_TrianglesReducedTo = (float)m_MeshTriangles / (float)m_SpriteTriangles;
-                m_AreaReducedTo = m_MeshArea / m_SpriteArea;
+                m_VerticesReducedTo = (float)m_SpriteVertices1 / (float)m_SpriteVertices0;
+                m_TrianglesReducedTo = (float)m_SpriteTriangles1 / (float)m_SpriteTriangles0;
+                m_AreaReducedTo = m_SpriteArea1 / m_SpriteArea0;
             }
 
-            m_PrevSpriteRenderer = m_SpriteRenderer;
-            m_PrevMeshRenderer = m_MeshRenderer;
+            m_PrevSpriteRenderer0 = m_SpriteRenderer0;
+            m_PrevSpriteRenderer1 = m_SpriteRenderer1;
 
         }
     }
