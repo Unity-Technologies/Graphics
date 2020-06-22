d360cea8a3b5ca0f7ee90b3eeec674ff35b59ddd
Liis Kivistik <liis.kivistik@unity3d.com>
2020-06-22 11:48:26 +0200

    change utr install command

--- .yamato/ruamel/jobs/projects/commands/win.py
+++ .yamato/ruamel/jobs/projects/commands/win.py
@@ -2,7 +2,8 @@ from ...shared.constants import TEST_PROJECTS_DIR, PATH_UNITY_REVISION, PATH_TES
 
 def _cmd_base(project_folder, components):
     return [
-        f'git clone git@github.cds.internal.unity3d.com:unity/utr.git {TEST_PROJECTS_DIR}/{project_folder}/utr',
+        f'curl -s https://artifactory.internal.unity3d.com/core-automation/tools/utr-standalone/utr.bat --output {TEST_PROJECTS_DIR}/{project_folder}/utr/utr.bat',
+        #f'git clone git@github.cds.internal.unity3d.com:unity/utr.git {TEST_PROJECTS_DIR}/{project_folder}/utr',
         f'pip install unity-downloader-cli --extra-index-url https://artifactory.internal.unity3d.com/api/pypi/common-python/simple --upgrade',
         f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-downloader-cli --source-file ../../{PATH_UNITY_REVISION} {"".join([f"-c {c} " for c in components])} --wait --published-only'
     ]
