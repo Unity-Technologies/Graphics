75e77aeb8abc8bd884876fcaf45ed56f7400d4fd
Samuel Siltanen <samuel.siltanen@unity3d.com>
2020-06-22 17:23:00 +0300

    Created ShaderDebugPrintManager.

---
+++ com.unity.render-pipelines.universal/Runtime/ShaderDebugPrintManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace UnityEngine.Rendering.Universal
+{
+    public class ShaderDebugPrintManager
+    {
+        private const int DebugUAVSlot = 7;
+        private const int FramesInFlight = 4;
+
+        private List<GraphicsBuffer> m_outputBuffers = new List<GraphicsBuffer>(FramesInFlight);
+
+        private int m_FrameCounter = 0;
+
+        public ShaderDebugPrintManager()
+        {
+
+        }
+
+        void SetShaderDebugPrintBindings(CommandBuffer cmd)
+        {
+            cmd.SetRandomWriteTarget(DebugUAVSlot, m_outputBuffers[m_FrameCounter]);
+        }
+
+        void EndFrame()
+        {
+            // TODO: Read back
+
+            m_FrameCounter++;
+        }
+    }
+}
