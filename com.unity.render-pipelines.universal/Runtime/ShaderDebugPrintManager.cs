816733f131d393a350c16491f8475076834dcaf6
Samuel Siltanen <samuel.siltanen@unity3d.com>
2020-06-23 15:23:09 +0300

    Added HLSL header. Fixed buffer clearing.

--- com.unity.render-pipelines.universal/Runtime/ShaderDebugPrintManager.cs
+++ com.unity.render-pipelines.universal/Runtime/ShaderDebugPrintManager.cs
@@ -11,9 +11,12 @@ namespace UnityEngine.Rendering.Universal
 
         private const int DebugUAVSlot = 7;
         private const int FramesInFlight = 4;
-        private const int MaxBufferElements = 1024 * 1024; // 1M
+        private const int MaxBufferElements = 1024 * 1024; // 1M - must match the shader size definition
 
         private List<GraphicsBuffer> m_outputBuffers = new List<GraphicsBuffer>();
+
+        private List<Rendering.AsyncGPUReadbackRequest> m_readbackRequests =
+            new List<Rendering.AsyncGPUReadbackRequest>();
 
         private int m_FrameCounter = 0;
         private bool m_FrameCleared = false;
@@ -27,6 +30,7 @@ namespace UnityEngine.Rendering.Universal
             for (int i = 0; i < FramesInFlight; i++)
             {
                 m_outputBuffers.Add(new GraphicsBuffer(GraphicsBuffer.Target.Structured, MaxBufferElements, 4));
+                m_readbackRequests.Add(new Rendering.AsyncGPUReadbackRequest());
             }
         }
 
@@ -37,12 +41,27 @@ namespace UnityEngine.Rendering.Universal
 
         public void SetShaderDebugPrintBindings(CommandBuffer cmd)
         {
-            cmd.SetRandomWriteTarget(DebugUAVSlot, m_outputBuffers[m_FrameCounter % FramesInFlight]);
+            int index = m_FrameCounter % FramesInFlight;
+            if (!m_readbackRequests[index].done)
+            {
+                // We shouldn't end up here too often
+                m_readbackRequests[index].WaitForCompletion();
+            }
 
+            cmd.SetRandomWriteTarget(DebugUAVSlot, m_outputBuffers[index]);
+
+            ClearShaderDebugPrintBuffer();
+        }
+
+        private void ClearShaderDebugPrintBuffer()
+        {
             // Only clear the buffer the first time this is called in each frame
             if (!m_FrameCleared)
             {
-                cmd.ClearRandomWriteTargets();
+                int index = m_FrameCounter % FramesInFlight;
+                NativeArray<uint> data = new NativeArray<uint>(1, Allocator.Temp);
+                data[0] = 0;
+                m_outputBuffers[index].SetData(data, 0, 0, 1);
                 m_FrameCleared = true;
             }
         }
@@ -54,7 +73,13 @@ namespace UnityEngine.Rendering.Universal
             if (!request.hasError)
             {
                 NativeArray<uint> data = request.GetData<uint>(0);
-                Debug.Log("Frame number #" + m_FrameCounter + " debug value: "  + data[0]);
+                string msg = "Frame #" + m_FrameCounter + " (" + m_FrameCounter % FramesInFlight + "): ";
+                // TODO: This is just for testing...
+                for (int i = 0; i < 10; i++)
+                {
+                    msg += data[i] + ", ";
+                }
+                Debug.Log(msg);
             }
             else
             {
@@ -64,8 +89,8 @@ namespace UnityEngine.Rendering.Universal
 
         public void EndFrame()
         {
-            // TODO: Read back
-            Rendering.AsyncGPUReadback.Request(m_outputBuffers[m_FrameCounter % FramesInFlight], BufferReadComplete);
+            int index = m_FrameCounter % FramesInFlight;
+            m_readbackRequests[index] = Rendering.AsyncGPUReadback.Request(m_outputBuffers[index], BufferReadComplete);
 
             m_FrameCounter++;
             m_FrameCleared = false;
---
+++ com.unity.render-pipelines.universal/ShaderLibrary/ShaderDebugPrint.hlsl
@@ -0,0 +1,22 @@
+﻿#ifndef SHADER_DEBUG_PRINT_INCLUDED
+#define SHADER_DEBUG_PRINT_INCLUDED
+
+RWStructuredBuffer<uint> shaderDebugOutputData : register(u7);
+
+static const int MaxShaderDebugOutputElements = 1024 * 1024; // 1M - must match the C# side buffer size
+
+void ShaderDebugPrint(uint value)
+{
+    if (shaderDebugOutputData[0] < MaxShaderDebugOutputElements)
+    {
+        uint index;
+        InterlockedAdd(shaderDebugOutputData[0], 1, index);
+        index++; // Skip the counter in the beginning
+        if (index < MaxShaderDebugOutputElements)
+        {
+            shaderDebugOutputData[index] = value;
+        }
+    }
+}
+
+#endif // SHADER_DEBUG_PRINT_INCLUDED
