5e9d9d5bf10d22c0d9bd994ff6ce2c7c7d73272b
Pavlos Mavridis <pavlos.mavridis@unity3d.com>
2020-06-23 13:38:11 +0200

    Fixes in the math formula

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/PathTracing/Shaders/AdaptiveSampling.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/PathTracing/Shaders/AdaptiveSampling.hlsl
@@ -1,18 +1,20 @@
+// Storage format:
+// x: the mean value
+// y: the squared distance to the mean
+// z: sample count
 RW_TEXTURE2D_X(float4, _AccumulatedVariance);
 
-void UpdatePerPixelVariance(uint2 pixelCoords, uint sampleCount, float4 rad)
+void UpdatePerPixelVariance(uint2 pixelCoords, uint iteration, float4 rad)
 {
     // Radiance is measured in gamma/perceptual space
     float L = Luminance(LinearToGamma22(rad));
 
     // Welford's algorithm
-    if (sampleCount > 0)
-    {
-        float4 accVariance = _AccumulatedVariance[COORD_TEXTURE2D_X(pixelCoords)];
-        float delta = L - accVariance.x;
-        accVariance.x += delta / sampleCount;
-        float delta2 = L - accVariance.x;
-        accVariance.y += delta * delta2;
-        _AccumulatedVariance[COORD_TEXTURE2D_X(pixelCoords)] = accVariance;
-    }
+    float4 accVariance = (iteration > 0) ? _AccumulatedVariance[COORD_TEXTURE2D_X(pixelCoords)] : 0;
+    float delta = L - accVariance.x;
+    accVariance.x += delta / (accVariance.z + 1);
+    float delta2 = L - accVariance.x;
+    accVariance.y += delta * delta2;
+    accVariance.z += 1;
+    _AccumulatedVariance[COORD_TEXTURE2D_X(pixelCoords)] = accVariance;
 }
