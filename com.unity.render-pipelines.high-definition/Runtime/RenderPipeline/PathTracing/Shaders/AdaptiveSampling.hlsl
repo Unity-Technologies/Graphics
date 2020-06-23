4c34b6d672445f49a3b3c0de8860d2e30cdf71ae
Pavlos Mavridis <pavlos.mavridis@unity3d.com>
2020-06-23 22:43:01 +0200

    Fix variance computation

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/PathTracing/Shaders/AdaptiveSampling.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/PathTracing/Shaders/AdaptiveSampling.hlsl
@@ -4,24 +4,30 @@
 // x: the mean value
 // y: the squared distance to the mean
 // z: sample count
-// w: counts how many successive times this pixel passed the variance test
+// w: luminance 
+// TODO: count how many successive times this pixel passed the variance test
 RW_TEXTURE2D_X(float4, _AccumulatedVariance);
 
-bool UpdatePerPixelVariance(uint2 pixelCoords, uint iteration, float exposure, float4 radiance)
+bool UpdatePerPixelVariance(uint2 pixelCoords, uint iteration, float exposureMultiplier, float4 radiance)
 {
     if (radiance.w == CONVERGED_ALPHA)
     {
         return true;
     }
 
-    float L = exposure * Luminance(radiance.xyz);
+    float L = exposureMultiplier * Luminance(radiance.xyz);
 
-    // Welford's online algorithm
     float4 accVariance = (iteration > 0) ? _AccumulatedVariance[COORD_TEXTURE2D_X(pixelCoords)] : 0;
     accVariance.z += 1.0;
-    float delta = L - accVariance.x;
+
+    // first compute the accumulated luminance
+    float deltaL = L - accVariance.w;
+    accVariance.w += deltaL / accVariance.z;
+
+    // The pass this to Welford's online algorithm for variance computation
+    float delta = accVariance.w - accVariance.x;
     accVariance.x += delta / accVariance.z;
-    float delta2 = L - accVariance.x;
+    float delta2 = accVariance.w - accVariance.x;
     accVariance.y += delta * delta2;
 
     _AccumulatedVariance[COORD_TEXTURE2D_X(pixelCoords)] = accVariance;
@@ -38,17 +44,17 @@ bool CheckVariance(uint2 pixelCoords, uint iteration, float2 threshold)
     if (variance < threshold.x)
     {
         // update history
-        accVariance.w += 1.0;
+        // accVariance.w += 1.0;
         _AccumulatedVariance[COORD_TEXTURE2D_X(pixelCoords)] = accVariance;
-        if (accVariance.w > threshold.y)
+        // if (accVariance.w > threshold.y)
         {
             return true;
         }
-        return false;
+        //return false;
     }
     // reset history
-    accVariance.w = 0;
-    _AccumulatedVariance[COORD_TEXTURE2D_X(pixelCoords)] = accVariance;
+    //accVariance.w = 0;
+    //_AccumulatedVariance[COORD_TEXTURE2D_X(pixelCoords)] = accVariance;
     return false;
 }
 
@@ -58,7 +64,8 @@ float3 VisualizeVariance(uint2 pixelCoords, float minVariance, float maxVariance
 
     float variance = (accVariance.z > 0) ? accVariance.y / accVariance.z : 0;
     variance = clamp(variance, minVariance, maxVariance);
-    float hue = (0.6 * variance - minVariance) / (maxVariance - minVariance);
+    float hue = (variance - minVariance) / (maxVariance - minVariance);
+    hue *= 0.6f;
     hue = 0.6 - hue;
     return HsvToRgb(float3(hue, 1.0, 1.0));
 }
