33520393e8df11dd8eb70281178336f7fe757365
Paul Demeulenaere <pauld@unity3d.com>
2020-06-24 14:57:53 +0200

    Change isVFXTarget rule : if one vfx, should be considered as VFX

--- com.unity.shadergraph/Editor/Data/Graphs/GraphData.cs
+++ com.unity.shadergraph/Editor/Data/Graphs/GraphData.cs
@@ -319,7 +319,8 @@ namespace UnityEditor.ShaderGraph
         public DataValueEnumerable<Target> activeTargets => m_ActiveTargets.SelectValue();
 
         // TODO: Need a better way to handle this
-        public bool isVFXTarget => !isSubGraph && activeTargets.Count() > 0 && activeTargets.ElementAt(0).GetType() == typeof(VFXTarget);
+        public bool isVFXTarget => !isSubGraph && activeTargets.Any(o => o is VFXTarget);
+
         #endregion
 
         private Comparison<Target> targetComparison = new Comparison<Target>((a, b) => string.Compare(a.displayName, b.displayName));
