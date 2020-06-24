e7eb4fc81c1cf61dd9de0b72820759a1da62483f
Sebastien Lagarde <sebastien@unity3d.com>
2020-06-24 17:58:44 +0200

    Fix all hierarchy dependency update

--- com.unity.render-pipelines.high-definition/Editor/Material/MaterialVariant/MaterialVariantImporter.cs
+++ com.unity.render-pipelines.high-definition/Editor/Material/MaterialVariant/MaterialVariantImporter.cs
@@ -5,9 +5,63 @@ using UnityEngine;
 
 namespace Unity.Assets.MaterialVariant.Editor
 {
-    [ScriptedImporter(6, ".matVariant", 5)] // importQueueOffset must be higher than ShaderGraphImporter value
+    [ScriptedImporter(1, ".matVariant", 5)] // importQueueOffset must be higher than ShaderGraphImporter value
     public class MaterialVariantImporter : ScriptedImporter
     {
+        protected Material GetMaterialFromRoot(AssetImportContext ctx, string rootPath)
+        {
+            // As only a write of a file trigger the OnImportAsset we need to add
+            // all member of the hierarchy as a dependency so we can detect changes
+            // (Change of shader graph write a file, change of Material change a file, change of a MaterialVariant write a file)
+            // If we were only adding dependency on the Parent we will not catch the change of a GrandParent as it will only
+            // trigger OnImportAsset for the Parent but not propagate it for child (as it don't write on disk)
+            ctx.DependsOnSourceAsset(rootPath);
+
+            // When we call LoadAssetAtPath on a MaterialVariant or a ShaderGraph it return respectively a Material or a Shader
+            // because they are set as MainObject in their respective importer.
+            // For a Material or a Shader it return what you expect
+            Object subAsset = AssetDatabase.LoadAssetAtPath<Object>(rootPath);
+
+            // We need to know if we are a MaterialVariant so we can dig into the hierarchy
+            // When a Root (Material or Shader) write on disk, it invalidate all the child that depends on it (i.e the on the fly
+            // Material created is invalided, so LoadAssetAtPath will return null as we haven't yet call OnImportAsset.
+            // We can't store MaterialVariant as a subasset (i.e AddObjectToAsset) as OnImportAsset may not be call before we try to
+            // access it.
+            // In order to work around this dependency order issue we check if subAsset is null (Which must mean that a MaterialVariant
+            // have been invalidated). Otherwise if OnImportAsset have been call correctly, then (importer is MaterialVariantImporter) will be true.
+            var importer = AssetImporter.GetAtPath(rootPath);
+            if (subAsset == null || importer is MaterialVariantImporter)
+            {
+                // We use LoadSerializedFileAndForget to load the MaterialVariant, any other asset database function will try to Load
+                // the Material as it is setup as a MainObject and thus will return null. Even if you used LoadAllAssetsAtPath (it still return 0 asset).
+                var assets = InternalEditorUtility.LoadSerializedFileAndForget(rootPath);
+                subAsset = assets[0]; // Here we assume we are a MaterialVariant - Don't know yet if there is failure possible by doing this.
+            }
+
+            Material material = null;
+
+            if (subAsset is MaterialVariant)
+            {
+                MaterialVariant rootMatVariant = subAsset as MaterialVariant;
+                material = GetMaterialFromRoot(ctx, AssetDatabase.GUIDToAssetPath(rootMatVariant.rootGUID));
+
+                // Apply root modification
+                MaterialPropertyModification.ApplyPropertyModificationsToMaterial(material, rootMatVariant.overrides);
+            }
+            else if (subAsset is Shader)
+            {
+                Shader rootShader = subAsset as Shader;
+                material = new Material(rootShader);
+            }
+            else if (subAsset is Material)
+            {
+                Material rootMaterial = subAsset as Material;
+                material = new Material(rootMaterial);
+            }
+
+            return material;
+        }
+
         public override void OnImportAsset(AssetImportContext ctx)
         {
             var assets = InternalEditorUtility.LoadSerializedFileAndForget(ctx.assetPath);
@@ -16,44 +70,13 @@ namespace Unity.Assets.MaterialVariant.Editor
                 var matVariant = assets[0] as MaterialVariant;
                 if (matVariant != null)
                 {
-                    string rootPath = null;
-                    Material material = null;
+                    Material material = GetMaterialFromRoot(ctx, AssetDatabase.GUIDToAssetPath(matVariant.rootGUID));
 
-                    // Reinit Material + Build dependency chain
-                    if (matVariant.isShader)
-                    {
-                        rootPath = AssetDatabase.GUIDToAssetPath(matVariant.rootGUID);
-                        Shader shader = AssetDatabase.LoadAssetAtPath<Shader>(rootPath);
-                        material = new Material(shader);
-                        ctx.DependsOnSourceAsset(AssetDatabase.GetAssetPath(shader));
-                    }
-                    else
-                    {
-                        rootPath = AssetDatabase.GUIDToAssetPath(matVariant.rootGUID);
-                        Material mat = AssetDatabase.LoadAssetAtPath<Material>(rootPath);
-                        mat = AssetDatabase.LoadAssetAtPath<Material>(rootPath);
-                        material = new Material(mat);
-                        ctx.DependsOnSourceAsset(AssetDatabase.GetAssetPath(mat));
-                    }
+                    // Apply local modification
+                    MaterialPropertyModification.ApplyPropertyModificationsToMaterial(material, matVariant.overrides);
 
-                    // Setup as main replacement object
                     ctx.AddObjectToAsset("Material", material);
                     ctx.SetMainObject(material);
-
-                    int hash = material.ComputeCRC();
-
-                    // Force a save of this MaterialVariant to force an update of the asset database
-                    // and propagate to children
-                    if (matVariant.hash != hash)
-                    {
-                        matVariant.hash = hash;
-                        //InternalEditorUtility.SaveToSerializedFileAndForget(assets, ctx.assetPath, true);
-                    }
-                    else
-                    {
-                        // Apply change again
-                        MaterialPropertyModification.ApplyPropertyModificationsToMaterial(material, matVariant.overrides);
-                    }                   
                 }
             }
         }
