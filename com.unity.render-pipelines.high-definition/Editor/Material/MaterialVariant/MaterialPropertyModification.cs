9636f62b825e02e80a5009c2bf9aaf4282c5cc0d
RSlysz <remi@unity3d.com>
2020-06-24 15:16:27 +0200

    Add Color/Vector distinction as it is handled differently in MaterialProperty

--- com.unity.render-pipelines.high-definition/Editor/Material/MaterialVariant/MaterialPropertyModification.cs
+++ com.unity.render-pipelines.high-definition/Editor/Material/MaterialVariant/MaterialPropertyModification.cs
@@ -15,6 +15,7 @@ namespace Unity.Assets.MaterialVariant.Editor
         enum SerializedType
         {
             Scalar,
+            Color, // will be decomposed as r g b a scalars
             Vector, // will be decomposed as r g b a scalars
             Texture // will be decomposed as m_Texture ObjectReference and m_Scale and m_Offset Vector2s, and each vector2 will be split into x and y
         }
@@ -42,13 +43,21 @@ namespace Unity.Assets.MaterialVariant.Editor
             {
                 case SerializedType.Scalar:
                     return new[] { new MaterialPropertyModification(property.name, property.floatValue, null) };
+                case SerializedType.Color:
+                    return new[]
+                    {
+                        new MaterialPropertyModification(property.name + ".r", property.colorValue.r, null),
+                        new MaterialPropertyModification(property.name + ".g", property.colorValue.g, null),
+                        new MaterialPropertyModification(property.name + ".b", property.colorValue.b, null),
+                        new MaterialPropertyModification(property.name + ".a", property.colorValue.a, null)
+                    };
                 case SerializedType.Vector:
                     return new[]
                     {
-                        new MaterialPropertyModification(property.name + ".r", property.vectorValue.x, null),
-                        new MaterialPropertyModification(property.name + ".g", property.vectorValue.y, null),
-                        new MaterialPropertyModification(property.name + ".b", property.vectorValue.z, null),
-                        new MaterialPropertyModification(property.name + ".a", property.vectorValue.w, null)
+                        new MaterialPropertyModification(property.name + ".x", property.vectorValue.x, null),
+                        new MaterialPropertyModification(property.name + ".y", property.vectorValue.y, null),
+                        new MaterialPropertyModification(property.name + ".z", property.vectorValue.z, null),
+                        new MaterialPropertyModification(property.name + ".w", property.vectorValue.w, null)
                     };
                 case SerializedType.Texture:
                     return new[]
@@ -89,7 +98,7 @@ namespace Unity.Assets.MaterialVariant.Editor
             {
                 case MaterialProperty.PropType.Float:   return SerializedType.Scalar;
                 case MaterialProperty.PropType.Range:   return SerializedType.Scalar;
-                case MaterialProperty.PropType.Color:   return SerializedType.Vector;
+                case MaterialProperty.PropType.Color:   return SerializedType.Color;
                 case MaterialProperty.PropType.Vector:  return SerializedType.Vector;
                 case MaterialProperty.PropType.Texture: return SerializedType.Texture;
                 default:
@@ -105,9 +114,22 @@ namespace Unity.Assets.MaterialVariant.Editor
 
             if (propertyModification.m_ObjectReference != null)
                 return (SerializedType.Texture, parts);
-            
+
             if (parts.Length == 2)
-                return (SerializedType.Vector, parts);
+            {
+                string sub = parts[1];
+                if (sub == "r" || sub == "g" || sub == "b" || sub == "a")
+                    return (SerializedType.Color, parts);
+                else
+                {
+                    // replace on the fly the sub path as in YAML vector are stored as r g b a
+                    if (sub == "x")         { parts[1] = "r"; return (SerializedType.Vector, parts); }
+                    else if (sub == "y")    { parts[1] = "g"; return (SerializedType.Vector, parts); }
+                    else if (sub == "z")    { parts[1] = "b"; return (SerializedType.Vector, parts); }
+                    else if (sub == "w")    { parts[1] = "a"; return (SerializedType.Vector, parts); }
+                    // else it is a texture object name
+                }
+            }
 
             return (SerializedType.Texture, parts);  //could be length 2 only if object reference, else length is 3
         }
@@ -122,6 +144,7 @@ namespace Unity.Assets.MaterialVariant.Editor
                     propertyBase = propertyBase.FindPropertyRelative("m_Floats");
                     break;
                 case SerializedType.Vector:
+                case SerializedType.Color:
                     propertyBase = propertyBase.FindPropertyRelative("m_Colors");
                     break;
                 case SerializedType.Texture:
