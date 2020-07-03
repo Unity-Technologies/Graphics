12189e02f3af347be32ef2cb733d0c635c051af4
Sophia <sophia@unity3d.com>
2020-07-03 13:59:51 +0200

    Revert "get project name"
    This reverts commit 32b7ae96b785983b612f343d3247fa22dfbe9588.

--- .yamato/ruamel/jobs/projects/commands/internal.py
+++ .yamato/ruamel/jobs/projects/commands/internal.py
@@ -1,36 +1,37 @@
 from ...shared.constants import TEST_PROJECTS_DIR, PATH_UNITY_REVISION, PATH_TEST_RESULTS, PATH_PLAYERS
 
-def _cmd_base(project, components):
+def _cmd_base(project_folder, components):
     return [
-        f'git clone https://github.cds.internal.unity3d.com/sophia/{project["folder"]}.git TestProjects/{project["folder"]}',
-        f'curl -s https://artifactory.internal.unity3d.com/core-automation/tools/utr-standalone/utr.bat --output {TEST_PROJECTS_DIR}/{project["folder"]}/{project["name"]}/utr.bat',
+        f'git clone https://github.cds.internal.unity3d.com/sophia/URP-Update-testing.git TestProjects/URP-Update-testing',
+        f'curl -s https://artifactory.internal.unity3d.com/core-automation/tools/utr-standalone/utr.bat --output {TEST_PROJECTS_DIR}/{project_folder}/URP-Update-testing/utr.bat',
         f'pip install unity-downloader-cli --extra-index-url https://artifactory.internal.unity3d.com/api/pypi/common-python/simple --upgrade',
-        f'cd {TEST_PROJECTS_DIR}/{project["folder"]}/{project["name"]}  && unity-downloader-cli --source-file ../../../{PATH_UNITY_REVISION} {"".join([f"-c {c} " for c in components])} --wait --published-only'
+        f'cd {TEST_PROJECTS_DIR}/URP-Update-testing/{project_folder} && unity-downloader-cli --source-file ../../../{PATH_UNITY_REVISION} {"".join([f"-c {c} " for c in components])} --wait --published-only'
     ]
 
 
-def cmd_not_standalone(project, platform, api, test_platform_args):
-    base = _cmd_base(project["folder"], project["name"], platform["components"])
+def cmd_not_standalone(project_folder, platform, api, test_platform_args):
+    base = _cmd_base(project_folder, platform["components"])
     base.extend([
-        f'cd {TEST_PROJECTS_DIR}/{project["folder"]}/{project["name"]} && utr {test_platform_args} --testproject=. --editor-location=.Editor --artifacts_path={PATH_TEST_RESULTS}'
+        f'cd {TEST_PROJECTS_DIR}/URP-Update-testing/{project_folder} && utr {test_platform_args} --testproject=. --editor-location=.Editor --artifacts_path={PATH_TEST_RESULTS}'
     ])
     base[-1] += f' --extra-editor-arg="{platform["apis"][api]}"' if (api != "" and platform["apis"][api] != None)  else ''
     return base
 
-def cmd_standalone(project, platform, api, test_platform_args):
-    base = _cmd_base(project["folder"], project["name"], platform["components"])
-    base.append('git clone https://github.cds.internal.unity3d.com/sophia/{project["folder"]}.git TestProjects')
+def cmd_standalone(project_folder, platform, api, test_platform_args):
+    base = _cmd_base(project_folder, platform["components"])
+
+    if project_folder.lower() == 'URP-Update-Testing'.lower():
+        base.append('git clone https://github.cds.internal.unity3d.com/sophia/URP-Update-testing.git TestProjects')
 
     base.extend([
-        f'cd {TEST_PROJECTS_DIR}/{project["folder"]}/{project["name"]}   && utr {test_platform_args}Windows64 --artifacts_path={PATH_TEST_RESULTS} --timeout=1200 --player-load-path=../../{PATH_PLAYERS} --player-connection-ip=auto'
+        f'cd {TEST_PROJECTS_DIR}/URP-Update-testing/{project_folder} && utr {test_platform_args}Windows64 --artifacts_path={PATH_TEST_RESULTS} --timeout=1200 --player-load-path=../../{PATH_PLAYERS} --player-connection-ip=auto'
     ])
     return base
 
 
-def cmd_standalone_build(project, platform, api, test_platform_args):
-    base = _cmd_base(project["folder"], project["name"], platform["components"])
-
+def cmd_standalone_build(project_folder, platform, api, test_platform_args):
+    base = _cmd_base(project_folder, platform["components"])
     base.extend([
-        f'cd {TEST_PROJECTS_DIR}/{project["folder"]}/{project["name"]}   && utr {test_platform_args}Windows64 --extra-editor-arg="-executemethod" --extra-editor-arg="CustomBuild.BuildWindows{api}Linear" --testproject=. --editor-location=.Editor --artifacts_path={PATH_TEST_RESULTS} --timeout=1200 --player-save-path=../../{PATH_PLAYERS} --build-only'
+        f'cd {TEST_PROJECTS_DIR}/URP-Update-testing/{project_folder} && utr {test_platform_args}Windows64 --extra-editor-arg="-executemethod" --extra-editor-arg="CustomBuild.BuildWindows{api}Linear" --testproject=. --editor-location=.Editor --artifacts_path={PATH_TEST_RESULTS} --timeout=1200 --player-save-path=../../{PATH_PLAYERS} --build-only'
     ])
     return base
