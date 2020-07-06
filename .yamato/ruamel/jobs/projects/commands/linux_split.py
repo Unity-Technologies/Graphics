82ac5954078aa30820d9c3ee82011d32feaecd59
Liis Kivistik <liis.kivistik@unity3d.com>
2020-07-06 10:58:10 +0200

    [backport] 7.x.x/backports/yamato/ruamel-refactor-2 (#1031)
    * take all yamato code from old backport branch
    
    * comment out incoming hdrp_all dependencies from master

---
+++ .yamato/ruamel/jobs/projects/commands/linux_split.py
@@ -0,0 +1,33 @@
+from ...shared.constants import TEST_PROJECTS_DIR,PATH_UNITY_REVISION, PATH_TEST_RESULTS, PATH_PLAYERS
+
+def _cmd_base(project_folder, components):
+    return [ 
+        f'sudo -H pip install --upgrade pip',
+        f'sudo -H pip install unity-downloader-cli --extra-index-url https://artifactory.internal.unity3d.com/api/pypi/common-python/simple --upgrade',
+        f'git clone git@github.cds.internal.unity3d.com:unity/utr.git {TEST_PROJECTS_DIR}/{project_folder}/utr',
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-downloader-cli --source-file ../../{PATH_UNITY_REVISION} {"".join([f"-c {c} " for c in components])} --wait --published-only'
+    ]
+
+
+def cmd_not_standalone(project_folder, platform, api, test_platform_args):
+    base = _cmd_base(project_folder, platform["components"])
+    base.extend([ 
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && DISPLAY=:0.0 utr/utr {test_platform_args} --testproject=. --editor-location=.Editor --artifacts_path={PATH_TEST_RESULTS}'
+     ])
+    base[-1] += f' --extra-editor-arg="{platform["apis"][api]}"' if (api != "" and platform["apis"][api] != None)  else ''
+    return base
+
+def cmd_standalone(project_folder, platform, api, test_platform_args):
+    base = _cmd_base(project_folder, platform["components"])
+    base.extend([
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && DISPLAY=:0.0 utr/utr {test_platform_args}Linux64 --artifacts_path={PATH_TEST_RESULTS} --timeout=1200 --player-load-path=.{PATH_PLAYERS} --player-connection-ip=auto'
+      ])
+    return base
+
+def cmd_standalone_build(project_folder, platform, api, test_platform_args):
+    base = _cmd_base(project_folder, platform["components"])
+    base.extend([
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && DISPLAY=:0.0 utr/utr {test_platform_args}Linux64 --extra-editor-arg="-executemethod" --extra-editor-arg="CustomBuild.BuildLinux{api["name"]}Linear" --testproject=. --editor-location=.Editor --artifacts_path={PATH_TEST_RESULTS} --timeout=1200 --player-save-path=.{PATH_PLAYERS} --build-only'
+      ])
+    return base
+
