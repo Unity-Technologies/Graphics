4fc752118c77c51821b3df20ff48688739174956
sebastienlagarde <sebastien@unity3d.com>
2020-07-07 14:29:45 +0200

    Fix API breaking change: Add Public quality to Raytracing volume effect

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/Raytracing/RayTracingSettings.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/Raytracing/RayTracingSettings.cs
@@ -14,7 +14,7 @@ namespace UnityEngine.Rendering.HighDefinition
         [Tooltip("Controls the bias for all real-time ray tracing effects.")]
         public ClampedFloatParameter rayBias = new ClampedFloatParameter(0.001f, 0.0f, 0.1f);
 
-        RayTracingSettings()
+        public RayTracingSettings()
         {
             displayName = "Ray Tracing Settings (Preview)";
         }
