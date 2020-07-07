ba05618efac0430bf22faede6454516976a0ae99
Julien Ignace <julien@unity3d.com>
2020-07-07 16:58:22 +0200

    Fixed creation of Decal Buffer (should not have been bindMS)

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.Prepass.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.Prepass.cs
@@ -103,7 +103,7 @@ namespace UnityEngine.Rendering.HighDefinition
         TextureHandle CreateDecalPrepassBuffer(RenderGraph renderGraph, bool msaa)
         {
             TextureDesc decalDesc = new TextureDesc(Vector2.one, true, true)
-                { colorFormat = GraphicsFormat.R8G8B8A8_UNorm, clearBuffer = true, clearColor = Color.black, bindTextureMS = msaa, enableMSAA = msaa, enableRandomWrite = !msaa, name = msaa ? "DecalPrepassBufferMSAA" : "DecalPrepassBuffer" };
+                { colorFormat = GraphicsFormat.R8G8B8A8_UNorm, clearBuffer = true, clearColor = Color.black, bindTextureMS = false, enableMSAA = msaa, enableRandomWrite = !msaa, name = msaa ? "DecalPrepassBufferMSAA" : "DecalPrepassBuffer" };
             return renderGraph.CreateTexture(decalDesc);
         }
 
@@ -304,7 +304,7 @@ namespace UnityEngine.Rendering.HighDefinition
                 if (decalLayersEnabled)
                     decalBuffer = passData.decalBuffer;
                 else
-                    decalBuffer = new TextureHandle();
+                    decalBuffer = renderGraph.defaultResources.blackTextureXR;
 
                 builder.SetRenderFunc(
                 (DepthPrepassData data, RenderGraphContext context) =>
