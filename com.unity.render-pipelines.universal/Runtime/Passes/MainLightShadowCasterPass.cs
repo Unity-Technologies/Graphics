90f36f644e40c6bfa4c0eb8ee659ae1161858904
Lukas Chodosevičius <lukasc@unity3d.com>
2020-07-09 17:47:25 +0300

    Changing main light soft shadows properties to be passed when soft shadows are supported.

--- com.unity.render-pipelines.universal/Runtime/Passes/MainLightShadowCasterPass.cs
+++ com.unity.render-pipelines.universal/Runtime/Passes/MainLightShadowCasterPass.cs
@@ -181,16 +181,17 @@ namespace UnityEngine.Rendering.Universal.Internal
                 CoreUtils.SetKeyword(cmd, ShaderKeywordStrings.MainLightShadowCascades, shadowData.mainLightShadowCascadesCount > 1);
                 CoreUtils.SetKeyword(cmd, ShaderKeywordStrings.SoftShadows, softShadows);
 
-                SetupMainLightShadowReceiverConstants(cmd, shadowLight, softShadows);
+                SetupMainLightShadowReceiverConstants(cmd, shadowLight, shadowData.supportsSoftShadows);
             }
 
             context.ExecuteCommandBuffer(cmd);
             CommandBufferPool.Release(cmd);
         }
 
-        void SetupMainLightShadowReceiverConstants(CommandBuffer cmd, VisibleLight shadowLight, bool softShadows)
+        void SetupMainLightShadowReceiverConstants(CommandBuffer cmd, VisibleLight shadowLight, bool supportsSoftShadows)
         {
             Light light = shadowLight.light;
+            bool softShadows = shadowLight.light.shadows == LightShadows.Soft && supportsSoftShadows;
 
             int cascadeCount = m_ShadowCasterCascadesCount;
             for (int i = 0; i < cascadeCount; ++i)
@@ -232,8 +233,9 @@ namespace UnityEngine.Rendering.Universal.Internal
 
             // Inside shader soft shadows are controlled through global keyword.
             // If any additional light has soft shadows it will force soft shadows on main light too.
-            // TODO: make this conditional once we will support soft shadows per light inside shader.
-            // if (softShadows)
+            // As it is not trivial finding out which additional light has soft shadows, we will pass main light properties if soft shadows are supported.
+            // This workaround will be removed once we will support soft shadows per light.
+            if (supportsSoftShadows)
             {
                 if (m_SupportsBoxFilterForShadows)
                 {
