449be5e60842294fa4b36294de156bbbe62f7dca
Pavlos Mavridis <pavlos.mavridis@unity3d.com>
2020-07-10 10:43:22 +0200

    Fix for adaptive search radius

--- com.unity.render-pipelines.high-definition/Runtime/PostProcessing/PostProcessSystem.cs
+++ com.unity.render-pipelines.high-definition/Runtime/PostProcessing/PostProcessSystem.cs
@@ -2265,7 +2265,7 @@ namespace UnityEngine.Rendering.HighDefinition
 
             // Map the old "max radius" parameters to a bigger range, so we can work on more challenging scenes
             float maxRadius = Mathf.Max(m_DepthOfField.farMaxBlur, m_DepthOfField.nearMaxBlur);
-            float cocLimit = Mathf.Clamp(2 * maxRadius, 1, 32);
+            float cocLimit = Mathf.Clamp(4 * maxRadius, 1, 32);
 
             ComputeShader cs;
             int kernel;
