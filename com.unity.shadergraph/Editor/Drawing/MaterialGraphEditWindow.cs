331bc0204e8f403ea04367a3c0832b05f70e9372
Elizabeth LeGros <elizabeth.legros@unity3d.com>
2020-07-16 17:05:23 -0500

    code review requested changes

--- com.unity.shadergraph/Editor/Drawing/MaterialGraphEditWindow.cs
+++ com.unity.shadergraph/Editor/Drawing/MaterialGraphEditWindow.cs
@@ -322,15 +322,7 @@ namespace UnityEditor.ShaderGraph.Drawing
 
         void OnDestroy()
         {
-            if (graphObject != null)
-            {
-                string nameOfFile = AssetDatabase.GUIDToAssetPath(selectedGuid);
-                if (IsDirty() && EditorUtility.DisplayDialog("Shader Graph Has Been Modified", "Do you want to save the changes you made in the Shader Graph?\n" + nameOfFile + "\n\nYour changes will be lost if you don't save them.", "Save", "Don't Save"))
-                    UpdateAsset();
-                Undo.ClearUndo(graphObject);
-                graphObject = null;
-            }
-
+            PromptSaveIfDirtyOnQuit();
             graphEditorView = null;
         }
 
@@ -812,6 +804,7 @@ namespace UnityEditor.ShaderGraph.Drawing
         {
             try
             {
+                EditorApplication.wantsToQuit -= PromptSaveIfDirtyOnQuit;
                 m_ColorSpace = PlayerSettings.colorSpace;
                 m_RenderPipelineAsset = GraphicsSettings.renderPipelineAsset;
 
