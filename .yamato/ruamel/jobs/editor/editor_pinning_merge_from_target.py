321c00d422a403febf6d1c921a9748802349b086
Liis Kivistik <liis.kivistik@unity3d.com>
2020-07-21 11:31:19 +0200

    checkout target branch

--- .yamato/ruamel/jobs/editor/editor_pinning_merge_from_target.py
+++ .yamato/ruamel/jobs/editor/editor_pinning_merge_from_target.py
@@ -23,6 +23,7 @@ class Editor_PinningMergeFromTargetJob():
                 exit 1
             fi'''),
             f'git fetch',
+            f'git checkout {target_branch}',
             f'git checkout {target_branch_editor_ci}',
             f'git merge --ff-only {target_branch }',
             f'git config --global user.name "noreply@unity3d.com"',
