fb3a9fc89b77befa2ac2d3077754bf1badf7a5f6
thomas-zeng <49886741+thomas-zeng@users.noreply.github.com>
2020-07-21 01:15:44 -0700

    [10.x.x] Fixed camera stack tests not working with XR test framework (#1186)
    * * Enabled XR SRP regression test framework to handle URP camera stack.
    
    * Enabled LayoutSinglePassTestMode for cameras that are not main.
    Made XR test mode's intermediate texture size matching back buffer size.
    
    * Updated logic to handle culling params for stacked camera.
    Added XRSDK native integration.
    
    * Fixed test framework mirrorview srgb logic.
    Fixed test framework using target texture from camera. (cameraData.targetTexture should be used instead because URP camera stack only updates cameraData's target)
    
    * Fixed pixelRect not being respected in postFX XR.
    Fixed XR test framework not being treated as eye texture type.
    
    * Fixed more cameraData usage in test framework.
    Fixed update XR view not actually updating it.
    
    * Added readonly back to XRView's fields.
    
    * Fixed cameraData.pixelRect in XR.
    Fixed mirrorview to not setViewport to pixelRect(instead mirror to full screen)
    
    * Fixed XRView's viewport to mimic values returned by a XR provider instead of using camera's pixelrect.
    
    * Fixed up merge.

--- com.unity.render-pipelines.universal/Runtime/UniversalRenderPipeline.cs
+++ com.unity.render-pipelines.universal/Runtime/UniversalRenderPipeline.cs
@@ -248,7 +248,7 @@ namespace UnityEngine.Rendering.Universal
                 renderer.Execute(context, ref renderingData);
             } // When ProfilingSample goes out of scope, an "EndSample" command is enqueued into CommandBuffer cmd
 
-            cameraData.xr.EndCamera(cmd, camera);
+            cameraData.xr.EndCamera(cmd, cameraData);
             context.ExecuteCommandBuffer(cmd); // Sends to ScriptableRenderContext all the commands enqueued since cmd.Clear, i.e the "EndSample" command
             CommandBufferPool.Release(cmd);
             context.Submit(); // Actually execute the commands that we previously sent to the ScriptableRenderContext context
@@ -345,9 +345,19 @@ namespace UnityEngine.Rendering.Universal
                         baseCameraData.cameraTargetDescriptor.graphicsFormat = originalTargetDesc.graphicsFormat;
                     }
                     baseCameraData.cameraTargetDescriptor.msaaSamples = originalTargetDesc.msaaSamples;
+                    // Update cameraData for XR
+                    Rect cameraRect = baseCamera.rect;
+                    Rect xrViewport = baseCameraData.xr.GetViewport();
+                    baseCameraData.pixelRect = new Rect(cameraRect.x * xrViewport.width + xrViewport.x,
+                                                        cameraRect.y * xrViewport.height + xrViewport.y,
+                                                        cameraRect.width * xrViewport.width,
+                                                        cameraRect.height * xrViewport.height);
+                    baseCameraData.pixelWidth  = (int)(cameraRect.width * xrViewport.width);
+                    baseCameraData.pixelHeight = (int)(cameraRect.height * xrViewport.height);
+                    baseCameraData.aspectRatio = (float)baseCameraData.pixelWidth / (float)baseCameraData.pixelHeight;
                 }
 #endif
-                BeginCameraRendering(context, baseCamera);
+            BeginCameraRendering(context, baseCamera);
 #if VISUAL_EFFECT_GRAPH_0_0_1_OR_NEWER
                 //It should be called before culling to prepare material. When there isn't any VisualEffect component, this method has no effect.
                 VFX.VFXManager.PrepareCamera(baseCamera);
@@ -375,10 +385,6 @@ namespace UnityEngine.Rendering.Universal
                             // Copy base settings from base camera data and initialize initialize remaining specific settings for this camera type.
                             CameraData overlayCameraData = baseCameraData;
                             bool lastCamera = i == lastActiveOverlayCameraIndex;
-#if ENABLE_VR && ENABLE_XR_MODULE
-                            if (baseCameraData.xr.enabled)
-                                m_XRSystem.UpdateFromCamera(ref overlayCameraData.xr, currCamera);
-#endif
                             BeginCameraRendering(context, currCamera);
 #if VISUAL_EFFECT_GRAPH_0_0_1_OR_NEWER
                             //It should be called before culling to prepare material. When there isn't any VisualEffect component, this method has no effect.
@@ -386,6 +392,10 @@ namespace UnityEngine.Rendering.Universal
 #endif
                             UpdateVolumeFramework(currCamera, currCameraData);
                             InitializeAdditionalCameraData(currCamera, currCameraData, lastCamera, ref overlayCameraData);
+#if ENABLE_VR && ENABLE_XR_MODULE
+                            if (baseCameraData.xr.enabled)
+                                m_XRSystem.UpdateFromCamera(ref overlayCameraData.xr, overlayCameraData);
+#endif
                             RenderSingleCamera(context, overlayCameraData, anyPostProcessingEnabled);
                             EndCameraRendering(context, currCamera);
                         }
