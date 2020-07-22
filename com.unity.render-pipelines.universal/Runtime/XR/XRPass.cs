Unity-Technologies/Graphics

commit fb3a9fc89b77befa2ac2d3077754bf1badf7a5f6

[10.x.x] Fixed camera stack tests not working with XR test framework (#1186)
* * Enabled XR SRP regression test framework to handle URP camera stack.

* Enabled LayoutSinglePassTestMode for cameras that are not main.
Made XR test mode's intermediate texture size matching back buffer size.

* Updated logic to handle culling params for stacked camera.
Added XRSDK native integration.

* Fixed test framework mirrorview srgb logic.
Fixed test framework using target texture from camera. (cameraData.targetTexture should be used instead because URP camera stack only updates cameraData's target)

* Fixed pixelRect not being respected in postFX XR.
Fixed XR test framework not being treated as eye texture type.

* Fixed more cameraData usage in test framework.
Fixed update XR view not actually updating it.

* Added readonly back to XRView's fields.

* Fixed cameraData.pixelRect in XR.
Fixed mirrorview to not setViewport to pixelRect(instead mirror to full screen)

* Fixed XRView's viewport to mimic values returned by a XR provider instead of using camera's pixelrect.

* Fixed up merge.

com.unity.render-pipelines.universal/Runtime/XR/XRPass.cs
@@ -66,7 +66,7 @@ namespace UnityEngine.Rendering.Universal
 
     class XRPass
     {
-        readonly List<XRView> views = new List<XRView>(2);
+        internal List<XRView> views = new List<XRView>(2);
 
         internal bool enabled      { get => views.Count > 0; }
         internal bool xrSdkEnabled { get; private set; }
@@ -152,6 +152,28 @@ namespace UnityEngine.Rendering.Universal
             passInfo.copyDepth = false;
 
             return passInfo;
+        }
+
+        internal void UpdateView(int viewId, XRDisplaySubsystem.XRRenderPass xrSdkRenderPass, XRDisplaySubsystem.XRRenderParameter xrSdkRenderParameter)
+        {
+            if (viewId >= views.Count)
+                throw new NotImplementedException($"Invalid XR setup to update, trying to update non-existing xr view.");
+
+            views[viewId] = new XRView(xrSdkRenderPass, xrSdkRenderParameter);
+        }
+
+        internal void UpdateView(int viewId, Matrix4x4 proj, Matrix4x4 view, Rect vp, int textureArraySlice = -1)
+        {
+            if (viewId >= views.Count)
+                throw new NotImplementedException($"Invalid XR setup to update, trying to update non-existing xr view.");
+
+            views[viewId] = new XRView(proj, view, vp, textureArraySlice);
+        }
+
+        internal void UpdateCullingParams(int cullingPassId, ScriptableCullingParameters cullingParams)
+        {
+            this.cullingPassId = cullingPassId;
+            this.cullingParams = cullingParams;
         }
 
         internal void AddView(Matrix4x4 proj, Matrix4x4 view, Rect vp, int textureArraySlice = -1)
@@ -268,7 +290,7 @@ namespace UnityEngine.Rendering.Universal
             }
         }
 
-        internal void EndCamera(CommandBuffer cmd, Camera camera)
+        internal void EndCamera(CommandBuffer cmd, CameraData cameraData)
         {
             if (!enabled)
                 return;
@@ -280,7 +302,7 @@ namespace UnityEngine.Rendering.Universal
             {
                 using (new ProfilingScope(cmd, _XRCustomMirrorProfilingSampler))
                 {
-                    customMirrorView(this, cmd, camera.targetTexture, camera.pixelRect);
+                    customMirrorView(this, cmd, cameraData.targetTexture, cameraData.pixelRect);
                 }
             }
         }

TestProjects/UniversalGraphicsTest/Assets/Scenes/123_CameraStackingClear.unity
@@ -232,7 +232,7 @@ MonoBehaviour:
     UseBackBuffer: 1
     ImageResolution: 4
   WaitFrames: 2
-  XRCompatible: 0
+  XRCompatible: 1
 --- !u!114 &763890391
 MonoBehaviour:
   m_ObjectHideFlags: 0

TestProjects/UniversalGraphicsTest/Assets/Scenes/124_CameraStackingClearRT.unity
@@ -1193,7 +1193,7 @@ MonoBehaviour:
     UseBackBuffer: 1
     ImageResolution: 4
   WaitFrames: 2
-  XRCompatible: 0
+  XRCompatible: 1
 --- !u!20 &1203454921
 Camera:
   m_ObjectHideFlags: 0

com.unity.render-pipelines.universal/Runtime/Passes/FinalBlitPass.cs
@@ -69,7 +69,7 @@ namespace UnityEngine.Rendering.Universal.Internal
                         ClearFlag.None,
                         Color.black);
 
-                    cmd.SetViewport(cameraData.xr.GetViewport());
+                    cmd.SetViewport(cameraData.pixelRect);
 
                     // We y-flip if
                     // 1) we are bliting from render texture to back buffer(UV starts at bottom) and

com.unity.render-pipelines.universal/Runtime/Passes/PostProcessPass.cs
@@ -426,6 +426,8 @@ namespace UnityEngine.Rendering.Universal.Internal
                          colorLoadAction, RenderBufferStoreAction.Store, RenderBufferLoadAction.DontCare, RenderBufferStoreAction.DontCare);
 
                     bool isRenderToBackBufferTarget = cameraTarget == cameraData.xr.renderTarget && !cameraData.xr.renderTargetIsRenderTexture;
+                    if (isRenderToBackBufferTarget)
+                        cmd.SetViewport(cameraData.pixelRect);
                     // We y-flip if
                     // 1) we are bliting from render texture to back buffer and
                     // 2) renderTexture starts UV at top
@@ -1156,6 +1158,8 @@ namespace UnityEngine.Rendering.Universal.Internal
 
                 cmd.SetRenderTarget(new RenderTargetIdentifier(cameraTarget, 0, CubemapFace.Unknown, -1),
                     colorLoadAction, RenderBufferStoreAction.Store, RenderBufferLoadAction.DontCare, RenderBufferStoreAction.DontCare);
+                // XRTODO: Investigate this case: Must call SetViewport after SetRenderTarget. SetRenderTarget calls setViewport internally which invalidates SetViewport
+                cmd.SetViewport(cameraData.pixelRect); 
                 cmd.SetGlobalVector(ShaderPropertyId.scaleBias, scaleBias);
                 cmd.DrawProcedural(Matrix4x4.identity, material, 0, MeshTopology.Quads, 4, 1, null);
             }

com.unity.render-pipelines.universal/Runtime/UniversalRenderPipeline.cs
@@ -248,7 +248,7 @@ namespace UnityEngine.Rendering.Universal
                 renderer.Execute(context, ref renderingData);
             } // When ProfilingSample goes out of scope, an "EndSample" command is enqueued into CommandBuffer cmd
 
-            cameraData.xr.EndCamera(cmd, camera);
+            cameraData.xr.EndCamera(cmd, cameraData);
             context.ExecuteCommandBuffer(cmd); // Sends to ScriptableRenderContext all the commands enqueued since cmd.Clear, i.e the "EndSample" command
             CommandBufferPool.Release(cmd);
             context.Submit(); // Actually execute the commands that we previously sent to the ScriptableRenderContext context
@@ -345,9 +345,19 @@ namespace UnityEngine.Rendering.Universal
                         baseCameraData.cameraTargetDescriptor.graphicsFormat = originalTargetDesc.graphicsFormat;
                     }
                     baseCameraData.cameraTargetDescriptor.msaaSamples = originalTargetDesc.msaaSamples;
+                    // Update cameraData for XR
+                    Rect cameraRect = baseCamera.rect;
+                    Rect xrViewport = baseCameraData.xr.GetViewport();
+                    baseCameraData.pixelRect = new Rect(cameraRect.x * xrViewport.width + xrViewport.x,
+                                                        cameraRect.y * xrViewport.height + xrViewport.y,
+                                                        cameraRect.width * xrViewport.width,
+                                                        cameraRect.height * xrViewport.height);
+                    baseCameraData.pixelWidth  = (int)(cameraRect.width * xrViewport.width);
+                    baseCameraData.pixelHeight = (int)(cameraRect.height * xrViewport.height);
+                    baseCameraData.aspectRatio = (float)baseCameraData.pixelWidth / (float)baseCameraData.pixelHeight;
                 }
 #endif
-                BeginCameraRendering(context, baseCamera);
+            BeginCameraRendering(context, baseCamera);
 #if VISUAL_EFFECT_GRAPH_0_0_1_OR_NEWER
                 //It should be called before culling to prepare material. When there isn't any VisualEffect component, this method has no effect.
                 VFX.VFXManager.PrepareCamera(baseCamera);
@@ -375,10 +385,6 @@ namespace UnityEngine.Rendering.Universal
                             // Copy base settings from base camera data and initialize initialize remaining specific settings for this camera type.
                             CameraData overlayCameraData = baseCameraData;
                             bool lastCamera = i == lastActiveOverlayCameraIndex;
-#if ENABLE_VR && ENABLE_XR_MODULE
-                            if (baseCameraData.xr.enabled)
-                                m_XRSystem.UpdateFromCamera(ref overlayCameraData.xr, currCamera);
-#endif
                             BeginCameraRendering(context, currCamera);
 #if VISUAL_EFFECT_GRAPH_0_0_1_OR_NEWER
                             //It should be called before culling to prepare material. When there isn't any VisualEffect component, this method has no effect.
@@ -386,6 +392,10 @@ namespace UnityEngine.Rendering.Universal
 #endif
                             UpdateVolumeFramework(currCamera, currCameraData);
                             InitializeAdditionalCameraData(currCamera, currCameraData, lastCamera, ref overlayCameraData);
+#if ENABLE_VR && ENABLE_XR_MODULE
+                            if (baseCameraData.xr.enabled)
+                                m_XRSystem.UpdateFromCamera(ref overlayCameraData.xr, overlayCameraData);
+#endif
                             RenderSingleCamera(context, overlayCameraData, anyPostProcessingEnabled);
                             EndCameraRendering(context, currCamera);
                         }

com.unity.render-pipelines.universal/Runtime/XR/XRSystem.cs
@@ -138,7 +138,6 @@ namespace UnityEngine.Rendering.Universal
             // Enable XR layout only for game camera
             bool isGameCamera = (camera.cameraType == CameraType.Game || camera.cameraType == CameraType.VR);
             bool xrSupported = isGameCamera && camera.targetTexture == null;
-
             if (XRGraphicsAutomatedTests.enabled && XRGraphicsAutomatedTests.running && isGameCamera && LayoutSinglePassTestMode(cameraData, new XRLayout() { camera = camera, xrSystem = this }))
             {
                 // test layout in used
@@ -203,32 +202,45 @@ namespace UnityEngine.Rendering.Universal
         }
 
         // Used for camera stacking where we need to update the parameters per camera
-        internal void UpdateFromCamera(ref XRPass xrPass, Camera camera)
+        internal void UpdateFromCamera(ref XRPass xrPass, CameraData cameraData)
         {
-            if (xrPass.enabled && display != null)
+            bool isGameCamera = (cameraData.camera.cameraType == CameraType.Game || cameraData.camera.cameraType == CameraType.VR);
+            if (XRGraphicsAutomatedTests.enabled && XRGraphicsAutomatedTests.running && isGameCamera)
+            {
+                // XR test framework code path. Update 2nd view with camera's view projection data
+                Matrix4x4 projMatrix = cameraData.camera.projectionMatrix;
+                Matrix4x4 viewMatrix = cameraData.camera.worldToCameraMatrix;
+                Rect      viewport = new Rect(0, 0, testRenderTexture.width, testRenderTexture.height);
+                int       textureArraySlice = -1;
+                xrPass.UpdateView(1, projMatrix, viewMatrix, viewport, textureArraySlice);
+
+                // Update culling params for this xr pass using camera's culling params
+                cameraData.camera.TryGetCullingParameters(false, out var cullingParams);
+                //// Disable legacy stereo culling path
+                cullingParams.cullingOptions &= ~CullingOptions.Stereo;
+                xrPass.UpdateCullingParams(0, cullingParams);
+            }
+            else if (xrPass.enabled && display != null)
             {
                 display.GetRenderPass(xrPass.multipassId, out var renderPass);
-                display.GetCullingParameters(camera, renderPass.cullingPassIndex, out var cullingParams);
-
+                display.GetCullingParameters(cameraData.camera, renderPass.cullingPassIndex, out var cullingParams);
                 // Disable legacy stereo culling path
                 cullingParams.cullingOptions &= ~CullingOptions.Stereo;
 
+                xrPass.UpdateCullingParams(cullingPassId: renderPass.cullingPassIndex, cullingParams);
                 if (xrPass.singlePassEnabled)
                 {
-                    xrPass = XRPass.Create(renderPass, multipassId: xrPass.multipassId, cullingParams, occlusionMeshMaterial);
 
                     for (int renderParamIndex = 0; renderParamIndex < renderPass.GetRenderParameterCount(); ++renderParamIndex)
                     {
-                        renderPass.GetRenderParameter(camera, renderParamIndex, out var renderParam);
-                        xrPass.AddView(renderPass, renderParam);
+                        renderPass.GetRenderParameter(cameraData.camera, renderParamIndex, out var renderParam);
+                        xrPass.UpdateView(renderParamIndex, renderPass, renderParam);
                     }
                 }
                 else
                 {
-                    renderPass.GetRenderParameter(camera, 0, out var renderParam);
-
-                    xrPass = XRPass.Create(renderPass, multipassId: xrPass.multipassId, cullingParams, occlusionMeshMaterial);
-                    xrPass.AddView(renderPass, renderParam);
+                    renderPass.GetRenderParameter(cameraData.camera, 0, out var renderParam);
+                    xrPass.UpdateView(0, renderPass, renderParam);
                 }
             }
         }
@@ -319,7 +331,6 @@ namespace UnityEngine.Rendering.Universal
             using (new ProfilingScope(cmd, _XRMirrorProfilingSampler))
             {
                 cmd.SetRenderTarget(camera.targetTexture != null  ? camera.targetTexture : new RenderTargetIdentifier(BuiltinRenderTextureType.CameraTarget));
-                cmd.SetViewport(camera.pixelRect);
                 bool yflip = camera.targetTexture != null || camera.cameraType == CameraType.SceneView || camera.cameraType == CameraType.Preview;
                 int mirrorBlitMode = display.GetPreferredMirrorBlitMode();
                 if (display.GetMirrorViewBlitDesc(null, out var blitDesc, mirrorBlitMode))
@@ -338,7 +349,7 @@ namespace UnityEngine.Rendering.Universal
                                                         new Vector4(blitParam.srcRect.width, blitParam.srcRect.height, blitParam.srcRect.x, blitParam.srcRect.y);
                             Vector4 scaleBiasRt = new Vector4(blitParam.destRect.width, blitParam.destRect.height, blitParam.destRect.x, blitParam.destRect.y);
 
-                            mirrorViewMaterialProperty.SetInt(XRShaderIDs._SRGBRead, (!display.sRGB || blitParam.srcTex.sRGB) ? 0 : 1);
+                            mirrorViewMaterialProperty.SetInt(XRShaderIDs._SRGBRead, (blitParam.srcTex.sRGB) ? 0 : 1);
                             mirrorViewMaterialProperty.SetTexture(ShaderPropertyId.sourceTex, blitParam.srcTex);
                             mirrorViewMaterialProperty.SetVector(ShaderPropertyId.scaleBias, scaleBias);
                             mirrorViewMaterialProperty.SetVector(ShaderPropertyId.scaleBiasRt, scaleBiasRt);
@@ -360,7 +371,7 @@ namespace UnityEngine.Rendering.Universal
         {
             Camera camera = frameLayout.camera;
 
-            if (camera == null || camera != Camera.main)
+            if (camera == null)
                 return false;
 
             if (camera.TryGetCullingParameters(false, out var cullingParams))
@@ -374,7 +385,12 @@ namespace UnityEngine.Rendering.Universal
                     RenderTextureDescriptor rtDesc = cameraData.cameraTargetDescriptor;
                     rtDesc.dimension = TextureDimension.Tex2DArray;
                     rtDesc.volumeDepth = 2;
-
+                    // If camera renders to subrect and it renders to backbuffer, we adjust size to match back buffer
+                    if(!cameraData.isDefaultViewport && cameraData.targetTexture == null)
+                    {
+                        rtDesc.width = (int)(rtDesc.width / cameraData.camera.rect.width);
+                        rtDesc.height = (int)(rtDesc.height / cameraData.camera.rect.height);
+                    }
                     testRenderTexture = RenderTexture.GetTemporary(rtDesc);
                 }
 
@@ -392,7 +408,7 @@ namespace UnityEngine.Rendering.Universal
                         scaleBias.w = 1.0f;
                     }
 
-                    mirrorViewMaterialProperty.SetInt(XRShaderIDs._SRGBRead, (rt != null && rt.sRGB) ? 0 : 1);
+                    mirrorViewMaterialProperty.SetInt(XRShaderIDs._SRGBRead, (testRenderTexture.sRGB) ? 0 : 1);
                     mirrorViewMaterialProperty.SetTexture(ShaderPropertyId.sourceTex, testRenderTexture);
                     mirrorViewMaterialProperty.SetVector(ShaderPropertyId.scaleBias, scaleBias);
                     mirrorViewMaterialProperty.SetVector(ShaderPropertyId.scaleBiasRt, scaleBiasRT);
@@ -417,7 +433,7 @@ namespace UnityEngine.Rendering.Universal
                 {
                     projMatrix = camera.projectionMatrix,
                     viewMatrix = camera.worldToCameraMatrix,
-                    viewport = new Rect(camera.pixelRect.x, camera.pixelRect.y, camera.pixelWidth, camera.pixelHeight),
+                    viewport = new Rect(0, 0, testRenderTexture.width, testRenderTexture.height),
                     textureArraySlice = -1
                 };
 

commit d61c9fdb389bcf9554617b6c6551491cf3b07669

Fixed XBox regression introduced in PR #1186

com.unity.render-pipelines.universal/Runtime/XR/XRPass.cs
@@ -364,7 +364,7 @@ namespace UnityEngine.Rendering.Universal
         internal bool enabled { get => false; }
         internal void StartSinglePass(CommandBuffer cmd) { }
         internal void StopSinglePass(CommandBuffer cmd) { }
-        internal void EndCamera(CommandBuffer cmd, Camera camera) { }
+        internal void EndCamera(CommandBuffer cmd, CameraData camera) { }
     }
 }
 #endif