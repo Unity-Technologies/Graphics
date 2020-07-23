76f2dce7cb5de23a49291cddeb5028998d96a692
Paul Tham <paul@unity3d.com>
2020-07-23 23:25:12 +0800

    more review fixes

--- com.unity.render-pipelines.universal/Runtime/2D/Passes/IRenderPass2D.cs
+++ com.unity.render-pipelines.universal/Runtime/2D/Passes/IRenderPass2D.cs
@@ -1,7 +1,3 @@
-using System.Collections.Generic;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
-
 namespace UnityEngine.Experimental.Rendering.Universal
 {
     internal interface IRenderPass2D
