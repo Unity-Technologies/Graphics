37b2c3a9457d9e588002fe1125e6d390623da060
FrancescoC-Unity <francescoc@unity3d.com>
2020-07-23 13:20:19 +0200

    Fix xbox

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/XR/XRSystem.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/XR/XRSystem.cs
@@ -130,6 +130,8 @@ namespace UnityEngine.Rendering.HighDefinition
                 {
                     // custom layout in used
                 }
+#if ENABLE_VR && ENABLE_XR_MODULE
+
                 else if (xrActive && xrSupported)
                 {
                     // Disable vsync on the main display when rendering to a XR device
@@ -143,6 +145,7 @@ namespace UnityEngine.Rendering.HighDefinition
 
                     CreateLayoutFromXrSdk(camera, singlePassAllowed);
                 }
+#endif
                 else
                 {
                     AddPassToFrame(camera, emptyPass);
