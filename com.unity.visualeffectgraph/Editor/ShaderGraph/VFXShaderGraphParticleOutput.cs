2ee231887997e22850a4e13ed70045940e78b6b8
Paul Demeulenaere <pauld@unity3d.com>
2020-07-24 10:23:27 +0200

    Fix display of alphaTreshold slider with shaderGraph : only if transparent using motionVector or shadowCasting

--- com.unity.visualeffectgraph/Editor/ShaderGraph/VFXShaderGraphParticleOutput.cs
+++ com.unity.visualeffectgraph/Editor/ShaderGraph/VFXShaderGraphParticleOutput.cs
@@ -133,8 +133,13 @@ namespace UnityEditor.VFX
                 }
                 else
                 {
-                    if (!shaderGraph.HasOutput(ShaderGraphVfxAsset.AlphaThresholdSlotId)) //alpha threshold isn't controlled by shadergraph
-                        return true;
+                    
+                    if (!shaderGraph.HasOutput(ShaderGraphVfxAsset.AlphaThresholdSlotId))
+                    {
+                        //alpha clipping isn't enabled in shaderGraph, we implicitly allow clipping for shadow & motion vector passes.
+                        if (!isBlendModeOpaque && (hasMotionVector || hasShadowCasting))
+                            return true;
+                    }
                 }
                 return false;
             }
