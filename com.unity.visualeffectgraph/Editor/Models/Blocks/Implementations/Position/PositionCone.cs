6d951a8c8f64e36dd65ea9ea170c24bbdb10a13c
Paul Demeulenaere <pauld@unity3d.com>
2020-07-30 18:04:44 +0200

    Some *WIP*

--- com.unity.visualeffectgraph/Editor/Models/Blocks/Implementations/Position/PositionCone.cs
+++ com.unity.visualeffectgraph/Editor/Models/Blocks/Implementations/Position/PositionCone.cs
@@ -26,6 +26,18 @@ namespace UnityEditor.VFX.Block
             public ArcCone ArcCone = ArcCone.defaultValue;
         }
 
+        public class InputPropertiesBis
+        {
+            [Tooltip("Sets the cone used for positioning the particles.")]
+            public ArcConeBis ArcCone = ArcConeBis.defaultValue;
+        }
+
+        public class InputPropertiesTer
+        {
+            [Tooltip("Sets the cone used for positioning the particles.")]
+            public ArcConeTer ArcCone = ArcConeTer.defaultValue;
+        }
+
         public class CustomProperties
         {
             [Range(0, 1), Tooltip("Sets the position along the height to emit particles from when ‘Custom Emission’ is used.")]
@@ -33,6 +45,70 @@ namespace UnityEditor.VFX.Block
             [Range(0, 1), Tooltip("Sets the position on the arc to emit particles from when ‘Custom Emission’ is used.")]
             public float ArcSequencer = 0.0f;
         }
+
+        public enum Type_Of_Transform_For_ArcCone
+        {
+            TwoVectorsDirection,
+            AxisUpAndRotation,
+            EulerAngle
+        }
+
+        [VFXSetting]
+        public Type_Of_Transform_For_ArcCone arcCone_ModeTest;
+
+        /* Remove this*/
+        protected override IEnumerable<VFXPropertyWithValue> inputProperties
+        {
+            get
+            {
+                var properties = Enumerable.Empty<VFXPropertyWithValue>();
+                if (arcCone_ModeTest == Type_Of_Transform_For_ArcCone.TwoVectorsDirection)
+                {
+                    properties = PropertiesFromType(GetInputPropertiesTypeName());
+                }
+                else if (arcCone_ModeTest == Type_Of_Transform_For_ArcCone.AxisUpAndRotation)
+                {
+                    properties = PropertiesFromType("InputPropertiesBis");
+                }
+                else if (arcCone_ModeTest == Type_Of_Transform_For_ArcCone.EulerAngle)
+                {
+                    properties = PropertiesFromType("InputPropertiesTer");
+                }
+
+                if (supportsVolumeSpawning)
+                {
+                    if (positionMode == PositionMode.ThicknessAbsolute || positionMode == PositionMode.ThicknessRelative)
+                        properties = properties.Concat(PropertiesFromType("ThicknessProperties"));
+                }
+
+                if (spawnMode == SpawnMode.Custom)
+                    properties = properties.Concat(PropertiesFromType("CustomProperties"));
+
+                return properties;
+            }
+        }
+
+        static void ApplyRotationAroundYAxis(ref VFXExpression i, ref VFXExpression j, ref VFXExpression k, VFXExpression rotation)
+        {
+            var cosTheta = new VFXExpressionCos(rotation);
+            var sinTheta = new VFXExpressionSin(rotation);
+            var one = VFXOperatorUtility.OneExpression[VFXValueType.Float];
+            var minusOne = VFXOperatorUtility.MinusOneExpression[VFXValueType.Float];
+            var zero = VFXOperatorUtility.ZeroExpression[VFXValueType.Float];
+
+            var matrix = new VFXExpressionVector4sToMatrix(
+                new VFXExpressionCombine(cosTheta,              zero,       sinTheta,   zero),
+                new VFXExpressionCombine(zero,                  one,        zero,       zero),
+                new VFXExpressionCombine(sinTheta * minusOne,   zero,       cosTheta,   zero),
+                new VFXExpressionCombine(zero,                  zero,       zero,       one)
+            );
+
+            //Not sure it's ideal for constant folding
+            i = new VFXExpressionTransformDirection(matrix, i);
+            j = new VFXExpressionTransformDirection(matrix, j);
+            k = new VFXExpressionTransformDirection(matrix, k);
+        }
+
 
         public override IEnumerable<VFXNamedExpression> parameters
         {
@@ -44,25 +120,95 @@ namespace UnityEditor.VFX.Block
 
                 yield return new VFXNamedExpression(CalculateVolumeFactor(positionMode, 0, 1), "volumeFactor");
 
-                VFXExpression center = inputSlots[0][0].GetExpression();
-                VFXExpression direction = inputSlots[0][1].GetExpression();
-                VFXExpression up = inputSlots[0][2].GetExpression();
-
-                var left = VFXOperatorUtility.Cross(direction, up);
-                left = VFXOperatorUtility.Normalize(left);
-                up = VFXOperatorUtility.Cross(left, direction);
-
-                yield return new VFXNamedExpression(direction, "transformMatrix_a");
-                yield return new VFXNamedExpression(up, "transformMatrix_b");
-                yield return new VFXNamedExpression(left, "transformMatrix_c");
-                yield return new VFXNamedExpression(center, "transformMatrix_d");
-
                 VFXExpression radius0 = inputSlots[0][3].GetExpression();
                 VFXExpression radius1 = inputSlots[0][4].GetExpression();
                 VFXExpression height = inputSlots[0][5].GetExpression();
                 VFXExpression tanSlope = (radius1 - radius0) / height;
                 VFXExpression slope = new VFXExpressionATan(tanSlope);
                 yield return new VFXNamedExpression(new VFXExpressionCombine(new VFXExpression[] { new VFXExpressionSin(slope), new VFXExpressionCos(slope) }), "sincosSlope");
+
+                VFXExpression center = inputSlots[0][0].GetExpression();
+                if (arcCone_ModeTest == Type_Of_Transform_For_ArcCone.EulerAngle)
+                {
+                    VFXExpression eulerAngle = inputSlots[0][1].GetExpression();
+
+                    var slotSpace = inputSlots[0].space;
+                    var systemSpace = ((VFXDataParticle)GetData()).space;
+                    if (slotSpace != systemSpace)
+                    {
+                        //Revert center transform
+                        //TODO
+                    }
+
+                    var zeroF3 = VFXOperatorUtility.ZeroExpression[VFXValueType.Float3];
+                    var oneF3 = VFXOperatorUtility.OneExpression[VFXValueType.Float3];
+
+                    VFXExpression rotationMatrix = new VFXExpressionTRSToMatrix(zeroF3, eulerAngle, oneF3);
+                    /*VFXExpression axisInverter = VFXValue.Constant(new Matrix4x4(   new Vector4(1, 0, 0, 0),
+                                                                                    new Vector4(0, 0, 1, 0),
+                                                                                    new Vector4(0, 1, 0, 0),
+                                                                                    new Vector4(0, 0, 0, 1)));*/
+                    VFXExpression axisInverter = VFXValue.Constant(new Matrix4x4(   new Vector4(1, 0, 0, 0),
+                                                                                    new Vector4(0, 1, 0, 0),
+                                                                                    new Vector4(0, 0, 1, 0),
+                                                                                    new Vector4(0, 0, 0, 1)));
+                    rotationMatrix = new VFXExpressionTransformMatrix(rotationMatrix, axisInverter);
+
+                    if (slotSpace != systemSpace)
+                    {
+                        if (systemSpace == VFXCoordinateSpace.World)
+                        {
+                            rotationMatrix = new VFXExpressionTransformMatrix(rotationMatrix, VFXBuiltInExpression.LocalToWorld);
+                        }
+                        else
+                        {
+                            rotationMatrix = new VFXExpressionTransformMatrix(rotationMatrix, VFXBuiltInExpression.WorldToLocal);
+                        }
+                    }
+
+                    var translateMatrix = new VFXExpressionTRSToMatrix(center, zeroF3, oneF3); //Can be simplified
+                    var matrix = new VFXExpressionTransformMatrix(rotationMatrix, translateMatrix);
+                    yield return new VFXNamedExpression(matrix, "transformMatrix");
+                }
+                else
+                {
+                    VFXExpression upAxis = inputSlots[0][1].GetExpression();
+                    VFXExpression leftAxis;
+                    if (arcCone_ModeTest == Type_Of_Transform_For_ArcCone.AxisUpAndRotation)
+                    {
+                        leftAxis = VFXValue.Constant(new Vector3(1, 0, 0)); //Local space or is depending of space of slot ? TODO
+
+                        var upAxisRotation = inputSlots[0][2].GetExpression();
+
+                        var cosTheta = new VFXExpressionCos(upAxisRotation);
+                        var sinTheta = new VFXExpressionSin(upAxisRotation);
+                        var zero = VFXOperatorUtility.ZeroExpression[VFXValueType.Float];
+                        leftAxis = new VFXExpressionCombine(sinTheta, zero, cosTheta);
+
+                        VFXCoordinateSpace systemSpace = ((VFXDataParticle)GetData()).space;
+                        if (systemSpace != VFXCoordinateSpace.Local)
+                        {
+                            leftAxis = new VFXExpressionTransformDirection(VFXBuiltInExpression.WorldToLocal, leftAxis);
+                        }
+                    }
+                    else
+                    {
+                        leftAxis = inputSlots[0][2].GetExpression();
+                    }
+
+                    var directionAxis = VFXOperatorUtility.Cross(upAxis, leftAxis);
+                    directionAxis = VFXOperatorUtility.Normalize(directionAxis);
+                    leftAxis = VFXOperatorUtility.Cross(directionAxis, upAxis);
+
+                    var matrix = new VFXExpressionVector3sToMatrix(directionAxis, upAxis, leftAxis, center);
+                    yield return new VFXNamedExpression(matrix, "transformMatrix");
+                    //TODO : Could reduce number of cbuffer, not sure it's relevant.
+                    //yield return new VFXNamedExpression(directionAxis, "transformMatrix_a");
+                    //yield return new VFXNamedExpression(upAxis, "transformMatrix_b");
+                    //yield return new VFXNamedExpression(leftAxis, "transformMatrix_c");
+                    //yield return new VFXNamedExpression(center, "transformMatrix_d");
+                }
+
             }
         }
 
@@ -125,16 +271,14 @@ float hNorm = HeightSequencer;
 
                 outSource += @"
 direction.xzy = normalize(float3(pos * sincosSlope.x, sincosSlope.y));
-float3 finalPos = lerp(float3(pos * ArcCone_radius0, 0.0f), float3(pos * ArcCone_radius1, ArcCone_height), hNorm).xzy;
+float3 finalPos = lerp(float3(pos * ArcCone_radius0, 0.0f), float3(pos * ArcCone_radius1, ArcCone_height), hNorm);
+";
+                //Maybe revert this xzy for all cases, could be always embedded in transformMatrix
+                if (arcCone_ModeTest != Type_Of_Transform_For_ArcCone.EulerAngle)
+                    outSource += "finalPos = finalPos.xzy;";
 
-float3x4 trsMatrix =
-{
-    transformMatrix_a.x, transformMatrix_b.x, transformMatrix_c.x, transformMatrix_d.x,
-    transformMatrix_a.y, transformMatrix_b.y, transformMatrix_c.y, transformMatrix_d.y,
-    transformMatrix_a.z, transformMatrix_b.z, transformMatrix_c.z, transformMatrix_d.z
-};
-
-finalPos = mul(trsMatrix, float4(finalPos, 1.0f)).xyz;
+                outSource += @"
+finalPos = mul(transformMatrix, float4(finalPos, 1.0f)).xyz;
 position += finalPos;
 ";
                 return outSource;
