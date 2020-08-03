a57279ec10bc4016fb6eae1557f0be19a8810d65
Liis Kivistik <liis.kivistik@unity3d.com>
2020-08-03 12:55:24 +0300

    change pip cmd

--- .yamato/ruamel/jobs/editor/editor_pinning_update.py
+++ .yamato/ruamel/jobs/editor/editor_pinning_update.py
@@ -15,8 +15,8 @@ class Editor_PinningUpdateJob():
     def get_job_definition(self, agent, target_branch, target_branch_editor_ci):
 
         commands = [
-            f'sudo pip3 install pipenv',
-            f'pipenv install --dev', 
+            f'sudo pip3 install pipenv --index-url https://artifactory.prd.cds.internal.unity3d.com/artifactory/api/pypi/pypi/simple',# TODO(henrik.kjellander): Remove when the image has this preinstalled.
+            f'python3 -m pipenv install --dev', 
             f'curl -L https://artifactory.prd.it.unity3d.com/artifactory/api/gpg/key/public | sudo apt-key add -',
             f'sudo sh -c "echo \'deb https://artifactory.prd.it.unity3d.com/artifactory/unity-apt-local bionic main\' > /etc/apt/sources.list.d/unity.list"',
             f'sudo apt-get update',
