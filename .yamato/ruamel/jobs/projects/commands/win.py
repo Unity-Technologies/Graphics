2c1050f0de6adddae653d15bd8e8df8354b4b113
Sophia <sophia@unity3d.com>
2020-08-04 15:21:48 +0200

    [WIP] add perf to win
    (cherry picked from commit c17f74e449d877d847e3eb217dff04b74f5989a9)

--- .yamato/ruamel/jobs/projects/commands/win.py
+++ .yamato/ruamel/jobs/projects/commands/win.py
@@ -36,3 +36,31 @@ def cmd_standalone_build(project_folder, platform, api, test_platform_args):
         f'cd {TEST_PROJECTS_DIR}/{project_folder} && utr {test_platform_args}Windows64 --extra-editor-arg="-executemethod" --extra-editor-arg="CustomBuild.BuildWindows{api["name"]}Linear" --testproject=. --editor-location=.Editor --artifacts_path={PATH_TEST_RESULTS} --timeout=1200 --player-save-path=../../{PATH_PLAYERS} --build-only'
     ])
     return base
+
+def cmd_not_standalone_performance(project_folder, platform, api, test_platform_args):
+    base = _cmd_base(project_folder, platform["components"])
+    base.extend([
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && utr {test_platform_args} --platform=StandaloneWindows64 --report-performance-data --performance-project-id=URP_Performance --testproject=. --editor-location=.Editor --artifacts_path={PATH_TEST_RESULTS}'
+    ])
+    base[-1] += f' --extra-editor-arg="{api["cmd"]}"' if api["name"] != ""  else ''
+    return base
+
+def cmd_standalone_performance(project_folder, platform, api, test_platform_args):
+    base = [
+        f'curl -s https://artifactory.internal.unity3d.com/core-automation/tools/utr-standalone/utr.bat --output {TEST_PROJECTS_DIR}/{project_folder}/utr.bat'
+    ]
+
+    if project_folder.lower() == 'UniversalGraphicsTest'.lower():
+        base.append('cd Tools && powershell -command ". .\\Unity.ps1; Set-ScreenResolution -width 1920 -Height 1080"')
+
+    base.extend([
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && utr {test_platform_args} --platform=StandaloneWindows64 --report-performance-data --performance-project-id=URP_Performance --artifacts_path={PATH_TEST_RESULTS} --timeout=1200 --player-load-path=../../{PATH_PLAYERS} --player-connection-ip=auto'
+    ])
+    return base
+
+def cmd_standalone_build_performance(project_folder, platform, api, test_platform_args):
+    base = _cmd_base(project_folder, platform["components"])
+    base.extend([
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && utr --suite=playmode --platform=StandaloneWindows64 --extra-editor-arg="-executemethod" --extra-editor-arg="CustomBuild.BuildWindows{api["name"]}Linear" --testproject=. --editor-location=.Editor --artifacts_path={PATH_TEST_RESULTS} --timeout=1200 --player-save-path=../../{PATH_PLAYERS} --build-only'
+    ])
+    return base
