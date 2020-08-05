de98fb096ffd980cc62f77d3e519090b60463686
Sophia <sophia@unity3d.com>
2020-08-05 14:22:09 +0200

    remove unneeded arg

--- .yamato/ruamel/jobs/projects/commands/android.py
+++ .yamato/ruamel/jobs/projects/commands/android.py
@@ -83,7 +83,7 @@ def cmd_standalone_build_performance(project_folder, platform, api, test_platfor
         ])
     
     if api["name"].lower() =='vulkan':
-        base.append(f'utr --suite=playmode --platform=Android --testproject={TEST_PROJECTS_DIR}\{project_folder} --extra-editor-arg="-executemethod" --extra-editor-arg="CustomBuild.BuildAndroidVulkanLinear" --extra-editor-arg="vulkan" --editor-location=WindowsEditor --artifacts_path={PATH_TEST_RESULTS} --player-save-path={PATH_PLAYERS} --scripting-backend=il2cpp --timeout=1800 --build-only')
+        base.append(f'utr --suite=playmode --platform=Android --testproject={TEST_PROJECTS_DIR}\{project_folder} --extra-editor-arg="-executemethod" --extra-editor-arg="CustomBuild.BuildAndroidVulkanLinear" --editor-location=WindowsEditor --artifacts_path={PATH_TEST_RESULTS} --player-save-path={PATH_PLAYERS} --scripting-backend=il2cpp --timeout=1800 --build-only')
     else:
         base.append(f'utr --suite=playmode --platform=Android --testproject={TEST_PROJECTS_DIR}\{project_folder} --editor-location=WindowsEditor --extra-editor-arg="-executemethod" --extra-editor-arg="CustomBuild.BuildAndroidGLES3Linear" --artifacts_path={PATH_TEST_RESULTS} --player-save-path={PATH_PLAYERS} --scripting-backend=il2cpp --timeout=1800 --build-only')
     return base
