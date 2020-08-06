1fc36f68e3df0538ee4be3686ef86843c1a52f6d
chris.chu <chris.chu@unity3d.com>
2020-08-07 06:08:54 +0800

    Removed extraneous code, made names more consistant with URP naming conventions. Added Universal Forward pass

--- com.unity.render-pipelines.universal/Shaders/2D/Include/SpriteMaskShared.hlsl
+++ com.unity.render-pipelines.universal/Shaders/2D/Include/SpriteMaskShared.hlsl
@@ -1,45 +1,40 @@
 // alpha below which a mask should discard a pixel, thereby preventing the stencil buffer from being marked with the Mask's presence
 half  _Cutoff;
-half4 _Color;
-half4 _RendererColor;
-half2 _Flip;
 
 TEXTURE2D(_MainTex);
 SAMPLER(sampler_MainTex);
 
-struct appdata_masking
+struct Attributes
 {
-    float4 vertex : POSITION;
-    half2 texcoord : TEXCOORD0;
-    UNITY_VERTEX_INPUT_INSTANCE_ID
+    float4 positionOS : POSITION;
+    half2  texcoord : TEXCOORD0;
 };
 
-struct v2f_masking
+struct Varyings
 {
-    float4 pos : SV_POSITION;
-    half2 uv : TEXCOORD0;
+    float4 positionCS : SV_POSITION;
+    half2  uv : TEXCOORD0;
     UNITY_VERTEX_OUTPUT_STEREO
 };
 
 
-v2f_masking vert(appdata_masking IN)
+Varyings vert(Attributes input)
 {
     v2f_masking OUT;
 
-    UNITY_SETUP_INSTANCE_ID(IN);
     UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(OUT);
 
-    OUT.pos = TransformObjectToHClip(IN.vertex);
-    OUT.uv = IN.texcoord;
+    OUT.positionCS = TransformObjectToHClip(attributes.positionOS);
+    OUT.uv = v.texcoord;
 
     return OUT;
 }
 
-half4 frag(v2f_masking IN) : SV_Target
+half4 frag(Varyings input) : SV_Target
 {
-    half4 c = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, IN.uv);
+    half4 c = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, input.uv);
     // for masks: discard pixel if alpha falls below MaskingCutoff
     clip(c.a - _Cutoff);
-    return _Color;
+    return 0;
 }
 
--- com.unity.render-pipelines.universal/Shaders/2D/Sprite-Lit-Mask.shader
+++ com.unity.render-pipelines.universal/Shaders/2D/Sprite-Lit-Mask.shader
@@ -1,13 +1,9 @@
-Shader "Universal Render Pipeline/Sprite-Lit-Mask"
+Shader "Universal Render Pipeline/2D/Sprite-Mask"
 {
     Properties
     {
         [PerRendererData] _MainTex ("Sprite Texture", 2D) = "white" {}
-        [PerRendererData] _AlphaTex ("External Alpha", 2D) = "white" {}
         [HideInInspector] _Cutoff ("Mask alpha cutoff", Range(0.0, 1.0)) = 0.0
-        _Color ("Tint", Color) = (1,1,1,0.2)
-        [PerRendererData] _EnableExternalAlpha ("Enable External Alpha", Float) = 0
-        [MaterialToggle] PixelSnap ("Pixel snap", Float) = 0
     }
 
     HLSLINCLUDE
@@ -41,10 +37,19 @@ Shader "Universal Render Pipeline/Sprite-Lit-Mask"
             #include "Packages/com.unity.render-pipelines.universal/Shaders/2D/Include/SpriteMaskShared.hlsl"
             ENDHLSL
         }
-
         Pass
         {
             Tags{ "LightMode" = "NormalsRendering" }
+            HLSLPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "Packages/com.unity.render-pipelines.universal/Shaders/2D/Include/SpriteMaskShared.hlsl"
+            ENDHLSL
+        }
+        Pass
+        {
+            Tags{ "LightMode" = "UniversalForward" }
             HLSLPROGRAM
             #pragma vertex vert
             #pragma fragment frag
