950dc1c158c23fd1833842c020228948260baad3
John Parsaie <johnpa@unity3d.com>
2020-08-06 13:43:48 -0400

    Move drawing functionality into the light unit levels, to handle the placement of markers

--- com.unity.render-pipelines.high-definition/Editor/Lighting/HDLightUnitSliderUIDrawer.cs
+++ com.unity.render-pipelines.high-definition/Editor/Lighting/HDLightUnitSliderUIDrawer.cs
@@ -8,37 +8,78 @@ namespace UnityEditor.Rendering.HighDefinition
 {
     internal class HDLightUnitSliderUIDrawer
     {
-        private class LightUnitPresets
+        private struct LightUnitLevel
         {
-            public void AddPreset(Texture2D icon, string tooltip, Vector2 range)
+            public GUIContent content;
+            public Vector2    range;
+        }
+
+        private class LightUnitLevels
+        {
+            public LightUnitLevels(string cautionTooltip)
             {
-                presets.Add(new GUIContent(icon, tooltip));
-                ranges.Add(range);
+                // Load builtin caution icon.
+                m_CautionContent = EditorGUIUtility.IconContent("console.warnicon.sml");
+                m_CautionContent.tooltip = cautionTooltip;
             }
 
-            public void GetPreset(float value, out GUIContent preset)
+            public void AddLevel(Texture2D icon, string tooltip, Vector2 range)
             {
-                for (int i = 0; i < ranges.Count; ++i)
-                {
-                    var range = ranges[i];
+                LightUnitLevel level;
+                level.content = new GUIContent(icon, tooltip);
+                level.range = range;
+                m_Levels.Add(level);
 
-                    if (value >= range.x && value <= range.y)
+                // Update the slider ranges.
+                if (range.y > m_RangeMax)
+                    m_RangeMax = range.y;
+                else if (range.x < m_RangeMin)
+                    m_RangeMin = range.x;
+            }
+
+            private void CurrentLevel(float value, out GUIContent level)
+            {
+                foreach (var l in m_Levels)
+                {
+                    if (value >= l.range.x && value <= l.range.y)
                     {
-                        preset = presets[i];
+                        level = l.content;
                         return;
                     }
                 }
 
                 // If value out of range, indicate caution. (For now assume caution feedback is last)
-                preset = presets[presets.Count - 1];
+                level = m_CautionContent;
             }
 
-            private List<GUIContent> presets = new List<GUIContent>();
-            private List<Vector2> ranges = new List<Vector2>();
+            public void Draw(Rect rect, SerializedProperty value)
+            {
+                // Fetch the rects
+                GetRects(rect, out var sliderRect, out var iconRect);
+
+                // Slider
+                DoSlider(sliderRect, value, m_RangeMin, m_RangeMax);
+
+                // Markers
+                foreach (var l in m_Levels)
+                {
+                    var markerValue = l.range.y / m_RangeMax;
+                    DoMarker(sliderRect, markerValue, l.content.tooltip);
+                }
+
+                // Icon
+                CurrentLevel(value.floatValue, out var level);
+                DoIcon(iconRect, level);
+            }
+
+            private readonly GUIContent m_CautionContent;
+            private readonly List<LightUnitLevel> m_Levels = new List<LightUnitLevel>();
+            private float m_RangeMin = float.MaxValue;
+            private float m_RangeMax = float.MinValue;
         }
 
-        private static readonly Dictionary<LightUnit, LightUnitPresets> s_LightUnitPresets = new Dictionary<LightUnit, LightUnitPresets>();
-        private static GUIContent s_MarkerContent;
+        private static readonly Dictionary<LightUnit, LightUnitLevels> s_LightUnitLevelMap = new Dictionary<LightUnit, LightUnitLevels>();
+        private static readonly GUIContent s_MarkerContent;
 
         static HDLightUnitSliderUIDrawer()
         {
@@ -46,34 +87,56 @@ namespace UnityEditor.Rendering.HighDefinition
             var editorTextures = HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures;
 
             // TODO: Fill a table of light unit presets, containing their icon, tooltip, and range
-            var luxPresets = new LightUnitPresets();
-            luxPresets.AddPreset(editorTextures.lightUnitVeryBrightSun,   "Very Bright Sun",   new Vector2(80000, 120000));
-            luxPresets.AddPreset(editorTextures.lightUnitOvercastSky,     "Overcast Sky",      new Vector2(10000, 80000));
-            luxPresets.AddPreset(editorTextures.lightUnitSunriseOrSunset, "Sunrise or Sunset", new Vector2(1,     10000));
-            luxPresets.AddPreset(editorTextures.lightUnitMoonLight,       "Moon Light",        new Vector2(0,     1));;
-            luxPresets.AddPreset(editorTextures.lightUnitCautionValue,    "Higher than Sunlight", Vector2.positiveInfinity);
-            s_LightUnitPresets.Add(LightUnit.Lux, luxPresets);
+            var luxPresets = new LightUnitLevels("Higher than Sunlight");
+            luxPresets.AddLevel(editorTextures.lightUnitVeryBrightSun,   "Very Bright Sun",   new Vector2(80000, 120000));
+            luxPresets.AddLevel(editorTextures.lightUnitOvercastSky,     "Overcast Sky",      new Vector2(10000, 80000));
+            luxPresets.AddLevel(editorTextures.lightUnitSunriseOrSunset, "Sunrise or Sunset", new Vector2(1,     10000));
+            luxPresets.AddLevel(editorTextures.lightUnitMoonLight,       "Moon Light",        new Vector2(0,     1));;
+            s_LightUnitLevelMap.Add(LightUnit.Lux, luxPresets);
 
             s_MarkerContent = new GUIContent(string.Empty);
         }
 
-        private static void GetLightUnitRects(Rect baseRect, out Rect sliderRect, out Rect iconRect)
+        public void OnGUI(LightUnit unit, SerializedProperty value)
         {
+            OnGUI(unit, value, EditorGUILayout.GetControlRect());
+        }
+
+        public void OnGUI(LightUnit unit, SerializedProperty value, Rect rect)
+        {
+            if (!s_LightUnitLevelMap.TryGetValue(unit, out var lightUnitLevels))
+                return;
+
+            // TODO: Disable indentation scope?
+            // Disable indentation (causes issues with manually rect management).
+            var prevLevel = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+
+            lightUnitLevels.Draw(rect, value);
+
+            // Restore indentation
+            EditorGUI.indentLevel = prevLevel;
+        }
+
+        private static void GetRects(Rect baseRect, out Rect sliderRect, out Rect iconRect)
+        {
+            const int k_IconSeparator = 6;
+
             sliderRect = baseRect;
-            sliderRect.width -= EditorGUIUtility.singleLineHeight;
+            sliderRect.width -= EditorGUIUtility.singleLineHeight + k_IconSeparator;
 
             iconRect = baseRect;
-            iconRect.x += sliderRect.width;
+            iconRect.x += sliderRect.width + k_IconSeparator;
             iconRect.width = EditorGUIUtility.singleLineHeight;
         }
 
-        private static void DrawLightUnitSlider(Rect rect, SerializedProperty value)
+        private static void DoSlider(Rect rect, SerializedProperty value, float leftValue, float rightValue)
         {
             // TODO: Look into compiling a lambda to access internal slider function for background (markers) + logarithmic values.
-            value.floatValue = GUI.HorizontalSlider(rect, value.floatValue, 0f, 150000f, GUI.skin.horizontalSlider, GUI.skin.horizontalSliderThumb);
+            value.floatValue = GUI.HorizontalSlider(rect, value.floatValue, leftValue, rightValue, GUI.skin.horizontalSlider, GUI.skin.horizontalSliderThumb);
         }
 
-        private static void DrawLightUnitMarker(Rect sliderRect, float x, string tooltip, float width = 4, float height = 2)
+        private static void DoMarker(Rect sliderRect, float x, string tooltip, float width = 4, float height = 2)
         {
             var markerRect = sliderRect;
             markerRect.width  = width;
@@ -88,6 +151,7 @@ namespace UnityEditor.Rendering.HighDefinition
             // Center the marker on value.
             markerRect.x -= markerRect.width / 2;
 
+            // Draw marker by manually drawing a rect, and an empty label with the tooltip.
             EditorGUI.DrawRect(markerRect, Color.white);
 
             // TODO: Consider enlarging this tooltip rect so that it's easier to discover?
@@ -95,7 +159,7 @@ namespace UnityEditor.Rendering.HighDefinition
             EditorGUI.LabelField(markerRect, s_MarkerContent, EditorStyles.inspectorDefaultMargins);
         }
 
-        private static void DrawLightUnitIcon(Rect iconRect, GUIContent icon)
+        private static void DoIcon(Rect iconRect, GUIContent icon)
         {
             var oldColor = GUI.color;
             GUI.color = Color.clear;
@@ -103,36 +167,6 @@ namespace UnityEditor.Rendering.HighDefinition
             GUI.color = oldColor;
 
             EditorGUI.LabelField(iconRect, new GUIContent(string.Empty, icon.tooltip));
-        }
-
-        public void OnGUI(LightUnit unit, SerializedProperty value)
-        {
-            OnGUI(unit, value, EditorGUILayout.GetControlRect());
-        }
-
-        public void OnGUI(LightUnit unit, SerializedProperty value, Rect rect)
-        {
-            if (!s_LightUnitPresets.TryGetValue(unit, out var unitPreset))
-                return;
-
-            // Disable indentation (causes issues with manually rect management).
-            var prevLevel = EditorGUI.indentLevel;
-            EditorGUI.indentLevel = 0;
-
-            // Fetch the presets for this light unit
-            unitPreset.GetPreset(value.floatValue, out var preset);
-
-            // Fetch the rects
-            GetLightUnitRects(rect, out var sliderRect, out var iconRect);
-
-            // Draw
-            DrawLightUnitSlider(sliderRect, value);
-            DrawLightUnitMarker(sliderRect,   0f, "Here's a Marker");
-            DrawLightUnitMarker(sliderRect, 0.5f, "Here's another one");
-            DrawLightUnitIcon(iconRect, preset);
-
-            // Restore indentation
-            EditorGUI.indentLevel = prevLevel;
         }
     }
 }
