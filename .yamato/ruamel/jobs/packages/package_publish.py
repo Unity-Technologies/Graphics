5cbf4f2540e30db8c6fb8a345f1fa683971af8d3
Liis Kivistik <liis.kivistik@unity3d.com>
2020-08-07 14:11:55 +0300

    add copy to publish

--- .yamato/ruamel/jobs/packages/package_publish.py
+++ .yamato/ruamel/jobs/packages/package_publish.py
@@ -23,8 +23,9 @@ class Package_PublishJob():
         job.set_agent(agent)
         job.add_dependencies(dependencies)
         job.add_commands([
+                f'copy packages_temp\\{package["id"]}\\upm-ci~\\packages\\* upm-ci~\\packages',
                 f'npm install upm-ci-utils@stable -g --registry {NPM_UPMCI_INSTALL_URL}',
-                f'upm-ci package publish --package-path {package["packagename"]} --artifacts-path packages_temp/{package["id"]}'])
+                f'upm-ci package publish --package-path {package["packagename"]}'])
         job.add_artifacts_packages()
         return job
     
