37fb1a8d7991afc2bfe39ae2ebe4657c794afa31
Théo Penavaire <theo-pnv@users.noreply.github.com>
2020-08-07 13:45:12 +0200

    Cherry pick 4b8c69f

--- .yamato/ruamel/jobs/projects/commands/iphone.py
+++ .yamato/ruamel/jobs/projects/commands/iphone.py
@@ -1,4 +1,4 @@
-from ...shared.constants import TEST_PROJECTS_DIR, PATH_UNITY_REVISION, PATH_TEST_RESULTS, PATH_PLAYERS
+from ...shared.constants import TEST_PROJECTS_DIR, PATH_UNITY_REVISION, PATH_TEST_RESULTS, PATH_PLAYERS, UTR_INSTALL_URL, UNITY_DOWNLOADER_CLI_URL
 from ruamel.yaml.scalarstring import PreservedScalarString as pss
 
 def _cmd_base(project_folder, components):
@@ -10,7 +10,7 @@ def cmd_not_standalone(project_folder, platform, api, test_platform_args):
 
 def cmd_standalone(project_folder, platform, api, test_platform_args):
     return [
-        f'curl -s https://artifactory.internal.unity3d.com/core-automation/tools/utr-standalone/utr --output utr',        
+        f'curl -s {UTR_INSTALL_URL} --output utr',        
         f'chmod +x ./utr',
         f'./utr --suite=playmode --platform=iOS --player-load-path={PATH_PLAYERS} --artifacts_path={PATH_TEST_RESULTS}'
     ]
@@ -19,9 +19,9 @@ def cmd_standalone(project_folder, platform, api, test_platform_args):
 def cmd_standalone_build(project_folder, platform, api, test_platform_args):
     components = platform["components"]
     return [
-        f'pip install unity-downloader-cli --index-url https://artifactory.prd.it.unity3d.com/artifactory/api/pypi/pypi/simple --upgrade',
+        f'pip install unity-downloader-cli --index-url {UNITY_DOWNLOADER_CLI_URL} --upgrade',
         f'unity-downloader-cli --source-file $YAMATO_SOURCE_DIR/{PATH_UNITY_REVISION} {"".join([f"-c {c} " for c in components])}  --wait --published-only',
-        f'curl -s https://artifactory.internal.unity3d.com/core-automation/tools/utr-standalone/utr --output utr',
+        f'curl -s {UTR_INSTALL_URL} --output utr',
         f'chmod +x ./utr',
         f'./utr --suite=playmode --platform=iOS --editor-location=.Editor --testproject={TEST_PROJECTS_DIR}/{project_folder} --player-save-path={PATH_PLAYERS} --artifacts_path={PATH_TEST_RESULTS} --build-only'
      ]
