1fb2aa491b606136c3fd48b249557e5aa6381216
Liis Kivistik <liis.kivistik@unity3d.com>
2020-08-10 14:05:39 +0300

    [Yamato] Package publish_all with and without tagging (#1555)
    * publish_all with and without tagging
    
    * Update _packages.yml
    
    * add tagging/no-tagging jobs for project context
    
    * edit job names and remove agent from publish job
    
    Co-authored-by: Sophia <sophia@unity3d.com>

--- .yamato/ruamel/jobs/packages/yml_project.py
+++ .yamato/ruamel/jobs/packages/yml_project.py
@@ -3,6 +3,7 @@ from .project_pack import Project_PackJob
 from .project_publish import Project_PublishJob
 from .project_test import Project_TestJob
 from .project_publish_all import Project_PublishAllJob
+from .project_publish_all_tag import Project_PublishAllTagJob
 from .project_test_all import Project_AllPackageCiJob
 from .project_publish_dry import Project_PublishJob_DryRun
 
@@ -30,6 +31,9 @@ def create_projectcontext_ymls(metafile):
         yml[job.job_id] = job.yml
 
     job = Project_PublishAllJob(metafile["packages"], metafile["target_branch"], metafile["agent_publish_all"])
+    yml[job.job_id] = job.yml
+
+    job = Project_PublishAllTagJob(metafile["packages"], metafile["target_branch"], metafile["agent_publish_all"])
     yml[job.job_id] = job.yml
 
     yml_files[projectcontext_filepath()] = yml
