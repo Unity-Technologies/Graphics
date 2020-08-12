4d29835d0fce701d10aab233b8e8745f4add3fef
Liis Kivistik <liis.kivistik@unity3d.com>
2020-08-12 11:07:22 +0300

    [8.x.x] [backport] Yamato joint backports (#1567)
    * [Yamato] pass UPM_REGISTRY var to mac minis (#1354)
    
    * [Yamato] add promote/publish dry run jobs (#1480)
    
    * promote dry run jobs
    
    * publish dryrun jobs
    
    * let promote dryrun use publish dryrun
    
    * [Packages][Yamato] Use project context for package jobs (#1546)
    
    * project context jobs
    
    * fix platform names
    
    * fix names
    
    * bump package versions
    
    * refactor jobs
    
    * publish_all job
    
    * promote jobs [project context]
    
    * turn off nightly trigger for old package publish jobs
    
    * all 'test all' job and turn off trigger for old job
    
    * fix duplicated dependencies
    
    * add publish dry runs
    
    * add promotion dry run jobs
    
    * [Yamato][Packages] Use unique package artifact paths (#1557)
    
    * cherrypick: packages path fix (#1045)
    
    (missing backport from release branches to master)
    
    * try copy
    
    * change path
    
    * mkdir
    
    * copy artifacts together
    
    * remove quotes
    
    * use %%x as in batch file
    
    * add temp publish_shadergraph --dry-run to test publishing with new paths
    
    * mac cmd
    
    * fix macos mkdir
    
    * add artifacts-path to publish and promote, add shadergraph dryrun publish/promote
    
    * add copy to publish
    
    * mkdir publish
    
    * add missing copycmd
    
    * test dependencies cmd
    
    * remove artifacts path from promote
    
    * add promote copy .json back
    
    * use constant for temp path, update templates pack path
    
    * undo artifact path *.tgz endings
    
    * [Yamato] add missing commands to core package test (#1561)
    
    * add missing commands to core
    
    * let abv use project context

--- .yamato/ruamel/jobs/preview_publish/pb_promote_project.py
+++ .yamato/ruamel/jobs/preview_publish/pb_promote_project.py
@@ -1,12 +1,12 @@
 from ruamel.yaml.scalarstring import DoubleQuotedScalarString as dss
 from ..shared.namer import *
 from ..shared.yml_job import YMLJob
-from ..shared.constants import NPM_UPMCI_INSTALL_URL,PATH_PACKAGES_temp
+from ..shared.constants import NPM_UPMCI_INSTALL_URL
 
-class PreviewPublish_PromoteJob():
+class PreviewPublish_ProjectContext_PromoteJob():
     
     def __init__(self, agent, package, platforms, target_editor, dry_run=False):
-        self.job_id = pb_job_id_promote(package["name"])
+        self.job_id = pb_projectcontext_job_id_promote(package["name"])
         self.yml = self.get_job_definition(agent, package, platforms, target_editor, dry_run).get_yml()
 
 
@@ -15,25 +15,23 @@ class PreviewPublish_PromoteJob():
             raise Exception('Tried to promote package for which "publish_source" set to false.')
 
         # define dependencies
-        dependencies = [f'{packages_filepath()}#{package_job_id_pack(package["name"])}']
+        dependencies = [f'{projectcontext_filepath()}#{projectcontext_job_id_pack()}']
 
         if dry_run:
-            dependencies.append(f'{packages_filepath()}#{package_job_id_publish_dry(package["name"])}')
+            dependencies.append(f'{projectcontext_filepath()}#{projectcontext_job_id_publish_dry(package["name"])}')
         else:
-            dependencies.append(f'{packages_filepath()}#{package_job_id_publish(package["name"])}')
-
+            dependencies.append(f'{projectcontext_filepath()}#{projectcontext_job_id_publish(package["name"])}')
             
         for platform in platforms:
-            dependencies.append(f'{packages_filepath()}#{package_job_id_test(package["name"],  platform["os"], target_editor)}')
+            dependencies.append(f'{projectcontext_filepath()}#{projectcontext_job_id_test(platform["os"], target_editor)}')
         
         # construct job
         job = YMLJob()
-        job.set_name(f'[{package["name"]}] Preview - Production Promote')
+        job.set_name(f'[{package["name"]}] Preview - Production Promote [project context]')
         job.set_agent(agent)
         job.add_var_custom('UPMCI_PROMOTION', 1)
         job.add_dependencies(dependencies)
         job.add_commands([
-                f'copy {PATH_PACKAGES_temp}\\{package["name"]}\\upm-ci~\\packages\\packages.json upm-ci~\\packages',
                 f'npm install upm-ci-utils@stable -g --registry {NPM_UPMCI_INSTALL_URL}',
                 f'upm-ci {package["type"]} promote --{package["type"]}-path {package["path"]}'])
         job.add_artifacts_packages()
