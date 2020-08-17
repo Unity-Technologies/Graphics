3dd00944004c63e97092c66faa3c6e4ba5b49036
Theo Penavaire <theo.penavaire@gmail.com>
2020-08-17 10:50:36 +0200

    Update manifest updater to bump templates

--- Tools/standalone/templates_auto_bumper.py
+++ Tools/standalone/templates_auto_bumper.py
@@ -1,22 +1,32 @@
-import sys
-import os
-import json
+#!/usr/bin/env python3
 
-# place this file in Graphics/TestProjects/ before running
+import sys, os, json, argparse
 
-# enter package to update as first arg
-package = sys.argv[1]
-# enter new version as second arg
-new_version = sys.argv[2]
 
-for subdir, dirs, files in os.walk('.'):
-    for file in files:
-        if file == "manifest.json":
-                manifest_file = os.path.join(subdir, file)
-                with open(manifest_file, "r") as f:
-                    manifest_json = json.load(f)
-                dependencies = manifest_json['dependencies']
-                if package in dependencies:
-                    dependencies[package] = new_version
-                    with open(manifest_file, "w") as json_file:
-                        json.dump(manifest_json, json_file, indent=4)
+def update_dependencies(project, file_name, target_dependency, target_version):
+    for subdir, dirs, files in os.walk(project):
+        for file in files:
+            if file == file_name:
+                path = os.path.join(subdir, file)
+                with open(path, "r") as f:
+                    json_content = json.load(f)
+                dependencies = json_content['dependencies']
+                if target_dependency in dependencies:
+                    print(f'{file_name} - {file}')
+                    dependencies[target_dependency] = target_version
+                    with open(path, "w") as json_file:
+                        json.dump(json_content, json_file, indent=2)
+
+
+if __name__ == "__main__":
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--template-name', help='Name of the template to bump dependencies of')
+    parser.add_argument('--target-dependency', help='Name of the target dependency')
+    parser.add_argument('--target-version', help='Name of the target version')
+    args=parser.parse_args()
+    if not args.template_name or not args.target_dependency or not args.target_version:
+        parser.print_usage()
+        exit(0)
+    else:
+        update_dependencies(args.template_name, "manifest.json", args.target_dependency, args.target_version)
+        update_dependencies(args.template_name, "package.json", args.target_dependency, args.target_version)
