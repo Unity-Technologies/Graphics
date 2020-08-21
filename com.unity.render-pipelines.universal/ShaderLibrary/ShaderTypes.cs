36c3908411ce9956d9c8b457c3f2eccc8974d494
Wilfrid Girard <wilfrid@unity3d.com>
2020-08-21 16:09:32 +0900

    Remove unused structure ShadowData from hlsl declarations
    Addresses comment https://github.com/Unity-Technologies/Graphics/pull/1616#discussion_r473898811

--- com.unity.render-pipelines.universal/ShaderLibrary/ShaderTypes.cs
+++ com.unity.render-pipelines.universal/ShaderLibrary/ShaderTypes.cs
@@ -11,16 +11,5 @@ namespace UnityEngine.Rendering.Universal
             public Vector4 spotDirection;
             public Vector4 occlusionProbeChannels;
         }
-
-        //[GenerateHLSL(PackingRules.Exact, false)]
-        // Currently we do not use this struct in the hlsl side, because worldToShadowMatrix and shadowParams must be stored in arrays of different sizes
-        public struct ShadowData // ShadowSliceData
-        {
-            public Matrix4x4 worldToShadowMatrix; // per-shadow-slice
-
-            // x: shadow strength
-            // y: 1 if soft shadows, 0 otherwise
-            public Vector4 shadowParams;          // per-casting-light
-        }
     }
 }
