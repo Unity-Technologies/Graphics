0d098a06c71c01c10c73423b28dd7bb998341ea1
Theo Penavaire <theo.penavaire@gmail.com>
2020-08-25 18:16:48 +0200

    Split build and execution for linux standalone tests

--- .yamato/ruamel/jobs/projects/commands/linux_split.py
+++ .yamato/ruamel/jobs/projects/commands/linux_split.py
@@ -6,17 +6,18 @@ def _cmd_base(project_folder, components):
         f'sudo -H pip install unity-downloader-cli --index-url {UNITY_DOWNLOADER_CLI_URL} --upgrade',
         f'curl -s {UTR_INSTALL_URL} --output {TEST_PROJECTS_DIR}/{project_folder}/utr',
         f'chmod +x {TEST_PROJECTS_DIR}/{project_folder}/utr',
-        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-downloader-cli --source-file ../../{PATH_UNITY_REVISION} {"".join([f"-c {c} " for c in components])} --wait --published-only'
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && sudo unity-downloader-cli --source-file ../../{PATH_UNITY_REVISION} {"".join([f"-c {c} " for c in components])} --wait --published-only'
     ]
 
 
 def cmd_not_standalone(project_folder, platform, api, test_platform_args):
     base = _cmd_base(project_folder, platform["components"])
     base.extend([ 
-        f'cd {TEST_PROJECTS_DIR}/{project_folder} && DISPLAY=:0.0 utr {test_platform_args} --testproject=. --editor-location=.Editor --artifacts_path={PATH_TEST_RESULTS}'
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && DISPLAY=:0.0 ./utr {test_platform_args} --testproject=. --editor-location=.Editor --artifacts_path={PATH_TEST_RESULTS}{_get_extra_utr_arg(project_folder)}'
      ])
-    base[-1] += f' --extra-editor-arg="{platform["apis"][api]}"' if (api != "" and platform["apis"][api] != None)  else ''
+    base[-1] += f' --extra-editor-arg="{api["cmd"]}"' if api["name"] != ""  else ''
     return base
+
 
 def cmd_standalone(project_folder, platform, api, test_platform_args):
     base = [
@@ -24,14 +25,18 @@ def cmd_standalone(project_folder, platform, api, test_platform_args):
         f'chmod +x {TEST_PROJECTS_DIR}/{project_folder}/utr',
     ]
     base.extend([
-        f'cd {TEST_PROJECTS_DIR}/{project_folder} && DISPLAY=:0.0 utr {test_platform_args}Linux64 --artifacts_path={PATH_TEST_RESULTS} --timeout=1200 --player-load-path=.{PATH_PLAYERS} --player-connection-ip=auto'
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && DISPLAY=:0.0 ./utr {test_platform_args}Linux64 --artifacts_path={PATH_TEST_RESULTS} --timeout=1200 --player-load-path=.{PATH_PLAYERS} --player-connection-ip=auto'
       ])
     return base
+
 
 def cmd_standalone_build(project_folder, platform, api, test_platform_args):
     base = _cmd_base(project_folder, platform["components"])
     base.extend([
-        f'cd {TEST_PROJECTS_DIR}/{project_folder} && DISPLAY=:0.0 utr {test_platform_args}Linux64 --extra-editor-arg="-executemethod" --extra-editor-arg="CustomBuild.BuildLinux{api["name"]}Linear" --testproject=. --editor-location=.Editor --artifacts_path={PATH_TEST_RESULTS} --timeout=1200 --player-save-path=.{PATH_PLAYERS} --build-only'
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && DISPLAY=:0.0 ./utr {test_platform_args}Linux64 --extra-editor-arg="-executemethod" --extra-editor-arg="CustomBuild.BuildLinux{api["name"]}Linear" --testproject=. --editor-location=.Editor --artifacts_path={PATH_TEST_RESULTS} --timeout=1200 --player-save-path=.{PATH_PLAYERS} --build-only'
       ])
     return base
 
+
+def _get_extra_utr_arg(project_folder):
+    return ' --compilation-errors-as-warnings' if project_folder.lower() in ['universalhybridtest', 'hdrp_hybridtests'] else ''
