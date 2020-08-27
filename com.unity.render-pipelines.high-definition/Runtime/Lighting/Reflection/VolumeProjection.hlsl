5d01f072505d21fb677e4e97495a5b2df6169eaf
FrancescoC-Unity <francescoc@unity3d.com>
2020-08-27 18:21:57 +0200

    Add comment

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/Reflection/VolumeProjection.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/Reflection/VolumeProjection.hlsl
@@ -27,7 +27,10 @@ float IntersectSphereProxy(EnvLightData lightData, float3 dirPS, float3 position
 {
     float sphereOuterDistance = lightData.proxyExtents.x;
     float projectionDistance = IntersectRaySphereSimple(positionPS, dirPS, sphereOuterDistance);
-    projectionDistance = max(IsNaN(projectionDistance) ? 0 : projectionDistance, lightData.minProjectionDistance); // Setup projection to infinite if requested (mean no projection shape)
+    projectionDistance = IsNaN(projectionDistance) ? -1.0f : projectionDistance; // Note that because we use IntersectRaySphereSimple, in case of a ill-set proxy, it could be that
+                                                                                // the determinant in the ray-sphere intersection code ends up negative, leading to a NaN.
+                                                                                // Rather than complicating the IntersectRaySphereSimple or switching to a more complex case, we cover that case this way.
+    projectionDistance = max(projectionDistance, lightData.minProjectionDistance); // Setup projection to infinite if requested (mean no projection shape)
 
     return projectionDistance;
 }
