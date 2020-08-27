90c4145feaa71b74cc9d39d47eec067f12a248c2
Guillermo Gijon <guillermo.gijon@unity3d.com>
2020-08-27 15:58:09 +0100

    Created custom editor for the GradientSky volume and exposed its parameters (Commit: 31e9c1fa3894c425eef1eaf6789d9d82d0073aaa)

---
+++ com.unity.render-pipelines.universal/Editor/Sky/SkySettingsEditor.cs
@@ -0,0 +1,99 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace UnityEditor.Rendering.Universal
+{
+    public abstract class SkySettingsEditor : VolumeComponentEditor
+    {
+        [System.Flags]
+        protected enum SkySettingsUIElement
+        {
+            SkyIntensity = 1 << 0,
+            //Rotation = 1 << 1, // TODO
+            UpdateMode = 1 << 2,
+            //IncludeSunInBaking = 1 << 3, // TODO
+        }
+        protected uint m_CommonUIElementsMask = 0xFFFFFFFF;
+        protected bool m_EnableLuxIntensityMode = false;
+
+        GUIContent m_SkyIntensityModeLabel = new UnityEngine.GUIContent("Intensity Mode");
+        GUIContent[] m_IntensityModes = { new GUIContent("Exposure"), new GUIContent("Multiplier"), new GUIContent("Lux") };
+        int[] m_IntensityModeValues = { (int)SkyIntensityMode.Exposure, (int)SkyIntensityMode.Multiplier, (int)SkyIntensityMode.Lux };
+        GUIContent[] m_IntensityModesNoLux = { new GUIContent("Exposure"), new GUIContent("Multiplier") };
+        int[] m_IntensityModeValuesNoLux = { (int)SkyIntensityMode.Exposure, (int)SkyIntensityMode.Multiplier };
+
+        // TODO SerializedDataParameter
+        SerializedDataParameter m_IntensityMode;
+        SerializedDataParameter m_SkyExposure;
+        SerializedDataParameter m_SkyMultiplier;
+        SerializedDataParameter m_DesiredLuxValue;
+        SerializedDataParameter m_EnvUpdateMode;
+        SerializedDataParameter m_EnvUpdatePeriod;
+
+        public override void OnEnable()
+        {
+            var o = new PropertyFetcher<SkySettings>(serializedObject);
+            // TODO SerializedDataParameter
+            m_IntensityMode = Unpack(o.Find(x => x.skyIntensityMode));
+            m_SkyExposure = Unpack(o.Find(x => x.exposure));
+            m_SkyMultiplier = Unpack(o.Find(x => x.multiplier));
+            m_DesiredLuxValue = Unpack(o.Find(x => x.desiredLuxValue));
+            m_EnvUpdateMode = Unpack(o.Find(x => x.updateMode));
+            m_EnvUpdatePeriod = Unpack(o.Find(x => x.updatePeriod));
+        }
+
+        protected void CommonSkySettingsGUI()
+        {
+            if ((m_CommonUIElementsMask & (uint)SkySettingsUIElement.SkyIntensity) != 0)
+            {
+                using (new EditorGUILayout.HorizontalScope())
+                {
+                    DrawOverrideCheckbox(m_IntensityMode);
+                    using (new EditorGUI.DisabledScope(!m_IntensityMode.overrideState.boolValue))
+                    {
+                        if (m_EnableLuxIntensityMode)
+                        {
+                            m_IntensityMode.value.intValue = EditorGUILayout.IntPopup(m_SkyIntensityModeLabel, (int)m_IntensityMode.value.intValue, m_IntensityModes, m_IntensityModeValues);
+                        }
+                        else
+                        {
+                            m_IntensityMode.value.intValue = EditorGUILayout.IntPopup(m_SkyIntensityModeLabel, (int)m_IntensityMode.value.intValue, m_IntensityModesNoLux, m_IntensityModeValuesNoLux);
+                        }
+                    }
+                }
+
+                EditorGUI.indentLevel++;
+                switch (m_IntensityMode.value.GetEnumValue<SkyIntensityMode>())
+                {
+                    case SkyIntensityMode.Exposure:
+                        PropertyField(m_SkyExposure);
+                        break;
+                    case SkyIntensityMode.Multiplier:
+                        PropertyField(m_SkyMultiplier);
+                        break;
+                    case SkyIntensityMode.Lux:
+                        PropertyField(m_DesiredLuxValue);
+                        // TODO Helpbox
+                        break;
+                }
+                EditorGUI.indentLevel--;
+            }
+
+            // TODO Rotation
+
+            if ((m_CommonUIElementsMask & (uint)SkySettingsUIElement.UpdateMode) != 0)
+            {
+                PropertyField(m_EnvUpdateMode);
+
+                if (!m_EnvUpdateMode.value.hasMultipleDifferentValues && m_EnvUpdateMode.value.intValue == (int)EnvironmentUpdateMode.Realtime)
+                {
+                    EditorGUI.indentLevel++;
+                    PropertyField(m_EnvUpdatePeriod);
+                    EditorGUI.indentLevel--;
+                }
+            }
+
+            // TODO Sun in baking
+        }
+    }
+}
--- com.unity.render-pipelines.universal/Runtime/Sky/GradientSky/GradientSkyRenderer.cs
+++ com.unity.render-pipelines.universal/Runtime/Sky/GradientSky/GradientSkyRenderer.cs
@@ -5,6 +5,11 @@ namespace UnityEngine.Rendering.Universal
         Material m_GradientSkyMaterial;
         MaterialPropertyBlock m_PropertyBlock = new MaterialPropertyBlock();
 
+        readonly int _GradientBottom = Shader.PropertyToID("_GradientBottom");
+        readonly int _GradientMiddle = Shader.PropertyToID("_GradientMiddle");
+        readonly int _GradientTop = Shader.PropertyToID("_GradientTop");
+        readonly int _GradientDiffusion = Shader.PropertyToID("_GradientDiffusion");
+
         public override void Build()
         {
             var urpRendererData = UniversalRenderPipeline.asset.scriptableRendererData;
@@ -25,9 +30,17 @@ namespace UnityEngine.Rendering.Universal
 
             cmd.SetViewProjectionMatrices(Matrix4x4.identity, Matrix4x4.identity);
 
+            var gradientSky = (cameraData.visualSky.skySettings as GradientSky);
+            m_GradientSkyMaterial.SetColor(_GradientBottom, gradientSky.bottom.value);
+            m_GradientSkyMaterial.SetColor(_GradientMiddle, gradientSky.middle.value);
+            m_GradientSkyMaterial.SetColor(_GradientTop, gradientSky.top.value);
+            m_GradientSkyMaterial.SetFloat(_GradientDiffusion, gradientSky.gradientDiffusion.value);
+            //m_GradientSkyMaterial.SetFloat(SkyShaderConstants._SkyIntensity, GetSkyIntensity(gradientSky)); // TODO Enable again
+            m_GradientSkyMaterial.SetFloat(SkyShaderConstants._SkyIntensity, 1.0f);
+
             m_PropertyBlock.SetMatrix(SkyShaderConstants._PixelCoordToViewDirWS, cameraData.pixelCoordToViewDirMatrix);
 
-            CoreUtils.DrawFullScreen(cmd, m_GradientSkyMaterial, m_PropertyBlock, 1);
+            CoreUtils.DrawFullScreen(cmd, m_GradientSkyMaterial, m_PropertyBlock, 0);
 
             cmd.SetViewProjectionMatrices(camera.worldToCameraMatrix, camera.projectionMatrix);
         }
