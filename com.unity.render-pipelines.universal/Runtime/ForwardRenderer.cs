36233ebc9858c93258e861a74603a2ccfb652ccf
Manuele Bonanno <manuele.bonanno@unity3d.com>
2020-09-02 13:45:25 +0200

    renamed PlatformRequiresExplicitAutoResolve to PlatformRequiresExplicitMsaaResolve

--- com.unity.render-pipelines.universal/Runtime/ForwardRenderer.cs
+++ com.unity.render-pipelines.universal/Runtime/ForwardRenderer.cs
@@ -426,7 +426,7 @@ namespace UnityEngine.Rendering.Universal
             CommandBufferPool.Release(cmd);
         }
 
-        bool PlatformRequiresExplicitAutoResolve()
+        bool PlatformRequiresExplicitMsaaResolve()
         {
             return !SystemInfo.supportsMultisampleAutoResolve &&
                    SystemInfo.graphicsDeviceType != GraphicsDeviceType.Metal;
@@ -451,7 +451,7 @@ namespace UnityEngine.Rendering.Universal
             bool isStereoEnabled = cameraData.isStereoEnabled;
             bool isScaledRender = !Mathf.Approximately(cameraData.renderScale, 1.0f) && !cameraData.isStereoEnabled;
             bool isCompatibleBackbufferTextureDimension = cameraTargetDescriptor.dimension == TextureDimension.Tex2D;
-            bool requiresExplicitMsaaResolve = msaaSamples > 1 && PlatformRequiresExplicitAutoResolve();
+            bool requiresExplicitMsaaResolve = msaaSamples > 1 && PlatformRequiresExplicitMsaaResolve();
             bool isOffscreenRender = cameraData.targetTexture != null && !isSceneViewCamera;
             bool isCapturing = cameraData.captureActions != null;
 
