bfba82ea76f16c14e3a21f5b7cf7c4dd2c44fcab
John Parsaie <johnpa@unity3d.com>
2020-09-03 18:40:20 -0400

    Fill in the remaining light value tables, implement light unit slider descriptor, implement the exponential light unit slider

---
+++ com.unity.render-pipelines.high-definition/Editor/Lighting/LightUnit/LightUnitSlider.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.UIElements;
+
+namespace UnityEditor.Rendering.HighDefinition
+{
+    class LightUnitSlider
+    {
+        static class SliderConfig
+        {
+            public const float k_IconSeparator      = 6;
+            public const float k_MarkerWidth        = 4;
+            public const float k_MarkerHeight       = 2;
+            public const float k_MarkerTooltipScale = 4;
+            public const float k_ThumbTooltipSize   = 10;
+        }
+
+        protected LightUnitSliderUIDescriptor m_Descriptor;
+
+        public LightUnitSlider(LightUnitSliderUIDescriptor descriptor)
+        {
+            m_Descriptor = descriptor;
+        }
+
+        public void Draw(Rect rect, SerializedProperty value)
+        {
+            BuildRects(rect, out var sliderRect, out var iconRect);
+
+            DoSlider(sliderRect, value, m_Descriptor.sliderRange);
+
+            foreach (var r in m_Descriptor.valueRanges)
+            {
+                var markerValue = r.value.y;
+                var markerPosition = GetPositionOnSlider(markerValue);
+                var markerTooltip = r.content.tooltip;
+                DoSliderMarker(sliderRect, markerPosition, markerValue, markerTooltip);
+            }
+
+            var level = CurrentRange(value.floatValue);
+            var levelIconContent = level.content;
+            var levelRange = level.value;
+            DoIcon(iconRect, levelIconContent, levelRange.y);
+
+            var thumbValue = value.floatValue;
+            var thumbPosition = GetPositionOnSlider(thumbValue);
+            var thumbTooltip = levelIconContent.tooltip;
+            DoThumbTooltip(sliderRect, thumbPosition, thumbValue, thumbTooltip);
+        }
+
+        LightUnitSliderUIRange CurrentRange(float value)
+        {
+            foreach (var l in m_Descriptor.valueRanges)
+            {
+                if (value >= l.value.x && value <= l.value.y)
+                {
+                    return l;
+                }
+            }
+
+            return LightUnitSliderUIRange.CautionRange(m_Descriptor.cautionTooltip, value);
+        }
+
+        void BuildRects(Rect baseRect, out Rect sliderRect, out Rect iconRect)
+        {
+            sliderRect = baseRect;
+            sliderRect.width -= EditorGUIUtility.singleLineHeight + SliderConfig.k_IconSeparator;
+
+            iconRect = baseRect;
+            iconRect.x += sliderRect.width + SliderConfig.k_IconSeparator;
+            iconRect.width = EditorGUIUtility.singleLineHeight;
+        }
+
+        void DoSliderMarker(Rect rect, float position, float value, string tooltip)
+        {
+            const float width  = SliderConfig.k_MarkerWidth;
+            const float height = SliderConfig.k_MarkerHeight;
+
+            var markerRect = rect;
+            markerRect.width  = width;
+            markerRect.height = height;
+
+            // Vertically align with slider.
+            markerRect.y += (EditorGUIUtility.singleLineHeight / 2f) - 1;
+
+            // Horizontally place on slider.
+            const float halfWidth = width * 0.5f;
+            markerRect.x = rect.x + rect.width * position;
+
+            // Center the marker on value.
+            markerRect.x -= halfWidth;
+
+            // Clamp to the slider edges.
+            float min = rect.x;
+            float max = (rect.x + rect.width) - width;
+            markerRect.x = Mathf.Clamp(markerRect.x, min, max);
+
+            // Draw marker by manually drawing the rect, and an empty label with the tooltip.
+            EditorGUI.DrawRect(markerRect, Color.white);
+
+            // Scale the marker tooltip for easier discovery
+            const float markerTooltipRectScale = SliderConfig.k_MarkerTooltipScale;
+            var markerTooltipRect = markerRect;
+            markerTooltipRect.width  *= markerTooltipRectScale;
+            markerTooltipRect.height *= markerTooltipRectScale;
+            markerTooltipRect.x      -= (markerTooltipRect.width  * 0.5f) - 1;
+            markerTooltipRect.y      -= (markerTooltipRect.height * 0.5f) - 1;
+            EditorGUI.LabelField(markerTooltipRect, GetLightUnitTooltip(tooltip, value));
+        }
+
+        void DoIcon(Rect rect, GUIContent icon, float range)
+        {
+            var oldColor = GUI.color;
+            GUI.color = Color.clear;
+            EditorGUI.DrawTextureTransparent(rect, icon.image);
+            GUI.color = oldColor;
+
+            EditorGUI.LabelField(rect, GetLightUnitTooltip(icon.tooltip, range));
+        }
+
+        void DoThumbTooltip(Rect rect, float position, float value, string tooltip)
+        {
+            const float size = SliderConfig.k_ThumbTooltipSize;
+            const float halfSize = SliderConfig.k_ThumbTooltipSize * 0.5f;
+
+            var thumbMarkerRect = rect;
+            thumbMarkerRect.width  = size;
+            thumbMarkerRect.height = size;
+
+            // Vertically align with slider
+            thumbMarkerRect.y += halfSize - 1f;
+
+            // Horizontally place tooltip on the wheel,
+            thumbMarkerRect.x  = rect.x + (rect.width - size) * position;
+
+            EditorGUI.LabelField(thumbMarkerRect, GetLightUnitTooltip(tooltip, value));
+        }
+
+        GUIContent GetLightUnitTooltip(string baseTooltip, float value)
+        {
+            string formatValue;
+
+            if (value >= 100000)
+                formatValue = (value / 1000).ToString("#,0K");
+            else if (value >= 10000)
+                formatValue = (value / 1000).ToString("0.#") + "K";
+            else
+                formatValue = value.ToString("#.0");
+
+            string tooltip = baseTooltip + " - " + formatValue + " " + m_Descriptor.unitName;
+
+            return new GUIContent(string.Empty, tooltip);
+        }
+
+        protected virtual void DoSlider(Rect rect, SerializedProperty value, Vector2 range)
+        {
+            value.floatValue = GUI.HorizontalSlider(rect, value.floatValue, range.x, range.y, GUI.skin.horizontalSlider, GUI.skin.horizontalSliderThumb);
+        }
+
+        protected virtual float GetPositionOnSlider(float value)
+        {
+            return value / m_Descriptor.sliderRange.y;
+        }
+    }
+
+    class ExponentialLightUnitSlider : LightUnitSlider
+    {
+        private Vector3 m_ExponentialConstraints;
+
+        /// <summary>
+        /// Exponential slider modeled to set a f(0.5) value.
+        /// ref: https://stackoverflow.com/a/17102320
+        /// </summary>
+        void PrepareExponentialConstraints(float lo, float mi, float hi)
+        {
+            float x = lo;
+            float y = mi;
+            float z = hi;
+
+            // https://www.desmos.com/calculator/yx2yf4huia
+            m_ExponentialConstraints.x = ((x * z) - (y * y)) / (x - (2 * y) + z);
+            m_ExponentialConstraints.y = ((y - x) * (y - x)) / (x - (2 * y) + z);
+            m_ExponentialConstraints.z = 2 * Mathf.Log((z - y) / (y - x));
+        }
+
+        float ValueToSlider(float x) => Mathf.Log((x - m_ExponentialConstraints.x) / m_ExponentialConstraints.y) / m_ExponentialConstraints.z;
+        float SliderToValue(float x) => m_ExponentialConstraints.x + m_ExponentialConstraints.y * Mathf.Exp(m_ExponentialConstraints.z * x);
+
+        public ExponentialLightUnitSlider(LightUnitSliderUIDescriptor descriptor) : base(descriptor)
+        {
+            var halfValue = 10000f; // TODO: Compute the median
+            PrepareExponentialConstraints(m_Descriptor.sliderRange.x, halfValue, m_Descriptor.sliderRange.y);
+        }
+
+        protected override float GetPositionOnSlider(float value)
+        {
+            return ValueToSlider(value);
+        }
+
+        protected override void DoSlider(Rect rect, SerializedProperty value, Vector2 range)
+        {
+            value.floatValue = ExponentialSlider(rect, value.floatValue);
+        }
+
+        float ExponentialSlider(Rect rect, float value)
+        {
+            float internalValue = GUI.HorizontalSlider(rect, ValueToSlider(value), 0f, 1f);
+
+            return SliderToValue(internalValue);
+        }
+    }
+
+    class TemperatureSlider : LightUnitSlider
+    {
+        public TemperatureSlider(LightUnitSliderUIDescriptor descriptor) : base(descriptor) {}
+    }
+
+    internal class LightUnitSliderUIDrawer
+    {
+        static Dictionary<LightUnit, LightUnitSlider> s_LightUnitSliderMap;
+        static LightUnitSlider s_ExposureSlider;
+        static TemperatureSlider s_TemperatureSlider;
+
+        static LightUnitSliderUIDrawer()
+        {
+            s_LightUnitSliderMap = new Dictionary<LightUnit, LightUnitSlider>
+            {
+                { LightUnit.Lux,     new ExponentialLightUnitSlider(LightUnitSliderDescriptors.k_LuxDescriptor)     },
+                { LightUnit.Lumen,   new ExponentialLightUnitSlider(LightUnitSliderDescriptors.k_LumenDescriptor)   },
+                { LightUnit.Candela, new ExponentialLightUnitSlider(LightUnitSliderDescriptors.k_CandelaDescriptor) },
+                { LightUnit.Ev100,   new ExponentialLightUnitSlider(LightUnitSliderDescriptors.k_EV100Descriptor)   },
+                { LightUnit.Nits,    new ExponentialLightUnitSlider(LightUnitSliderDescriptors.k_NitsDescriptor)    },
+            };
+
+            // Exposure is in EV100, but we load a separate due to the different icon set.
+            s_ExposureSlider = new LightUnitSlider(LightUnitSliderDescriptors.k_LuxDescriptor);
+
+            // Kelvin is not classified internally as a light unit so we handle it independently as well.
+            s_TemperatureSlider = new TemperatureSlider(LightUnitSliderDescriptors.k_TemperatureDescriptor);
+        }
+
+        public void Draw(LightUnit unit, SerializedProperty value, Rect rect)
+        {
+            if (!s_LightUnitSliderMap.TryGetValue(unit, out var lightUnitSlider))
+                return;
+
+            var prevIndentLevel = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+            {
+                lightUnitSlider.Draw(rect, value);
+            }
+            EditorGUI.indentLevel = prevIndentLevel;
+        }
+
+        public void DrawExposureSlider(SerializedProperty value, Rect rect)
+        {
+            var prevIndentLevel = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+            {
+                s_ExposureSlider.Draw(rect, value);
+            }
+            EditorGUI.indentLevel = prevIndentLevel;
+        }
+
+        public void DrawTemperatureSlider(SerializedProperty value, Rect rect)
+        {
+            var prevIndentLevel = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+            {
+                s_TemperatureSlider.Draw(rect, value);
+            }
+            EditorGUI.indentLevel = prevIndentLevel;
+        }
+    }
+}
---
+++ com.unity.render-pipelines.high-definition/Editor/Lighting/LightUnit/LightUnitSliderSettings.cs
@@ -0,0 +1,205 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+
+namespace UnityEditor.Rendering.HighDefinition
+{
+    struct LightUnitSliderUIDescriptor
+    {
+        public LightUnitSliderUIDescriptor(LightUnitSliderUIRange[] valueRanges, string cautionTooltip, string unitName)
+        {
+            this.valueRanges = valueRanges;
+            this.cautionTooltip = cautionTooltip;
+            this.unitName = unitName;
+
+            sliderRange = new Vector2(
+                this.valueRanges.Min(x => x.value.x),
+                this.valueRanges.Max(x => x.value.y)
+            );
+        }
+
+        public readonly LightUnitSliderUIRange[] valueRanges;
+        public readonly Vector2 sliderRange;
+        public readonly string cautionTooltip;
+        public readonly string unitName;
+    }
+
+    struct LightUnitSliderUIRange
+    {
+        public LightUnitSliderUIRange(Texture2D icon, string tooltip, Vector2 value)
+        {
+            this.content = new GUIContent(icon, tooltip);
+            this.value = value;
+        }
+
+        public static LightUnitSliderUIRange CautionRange(string tooltip, float value) => new LightUnitSliderUIRange
+        {
+            // Load the buildin caution icon with provided tooltip.
+            content = new GUIContent( EditorGUIUtility.IconContent("console.warnicon.sml").image, tooltip),
+            value = new Vector2(-1, value)
+        };
+
+        public GUIContent content;
+        public Vector2    value;
+    }
+
+    static class LightUnitSliderDescriptors
+    {
+        // Lux
+        public static LightUnitSliderUIDescriptor k_LuxDescriptor = new LightUnitSliderUIDescriptor(
+        LightUnitSliderRanges.k_LuxValueTable,
+        LightUnitTooltips.k_SunCaution,
+           "Lux"
+        );
+
+        // Lumen
+        public static LightUnitSliderUIDescriptor k_LumenDescriptor = new LightUnitSliderUIDescriptor(
+            LightUnitSliderRanges.k_LumenValueTable,
+            LightUnitTooltips.k_PunctualCaution,
+            "Lumen"
+        );
+
+        // Candela
+        public static LightUnitSliderUIDescriptor k_CandelaDescriptor = new LightUnitSliderUIDescriptor(
+            LightUnitSliderRanges.k_CandelaValueTable,
+            LightUnitTooltips.k_PunctualCaution,
+            "Candela"
+        );
+
+        // EV100
+        public static LightUnitSliderUIDescriptor k_EV100Descriptor = new LightUnitSliderUIDescriptor(
+            LightUnitSliderRanges.k_EV100ValueTable,
+            LightUnitTooltips.k_PunctualCaution,
+            "EV"
+        );
+
+        // Nits
+        public static LightUnitSliderUIDescriptor k_NitsDescriptor = new LightUnitSliderUIDescriptor(
+            LightUnitSliderRanges.k_NitsValueTable,
+            LightUnitTooltips.k_PunctualCaution,
+            "Nits"
+        );
+
+        // Exposure
+        public static LightUnitSliderUIDescriptor k_ExposureDescriptor = new LightUnitSliderUIDescriptor(
+            LightUnitSliderRanges.k_LumenValueTable,
+            LightUnitTooltips.k_ExposureCaution,
+            "EV"
+        );
+
+        // Temperature
+        public static LightUnitSliderUIDescriptor k_TemperatureDescriptor = new LightUnitSliderUIDescriptor(
+            LightUnitSliderRanges.k_KelvinValueTable,
+            LightUnitTooltips.k_TemperatureCaution,
+            "Kelvin"
+        );
+
+        private static class LightUnitTooltips
+        {
+            // Caution
+            public const string k_SunCaution         = "Higher than sunlight.";
+            public const string k_PunctualCaution    = "Very high intensity light.";
+            public const string k_ExposureCaution    = "Higher than sunlight.";
+            public const string k_TemperatureCaution = "";
+
+            // Lux / Directional
+            public const string k_LuxBrightSky       = "Very Bright Sun";
+            public const string k_LuxOvercastSky     = "Overcast Sky";
+            public const string k_LuxSunriseSunset   = "Sunrise or Sunset";
+            public const string k_LuxMoonlight       = "Moon Light";
+
+            // Punctual
+            public const string k_PunctualExterior   = "Exterior";
+            public const string k_PunctualInterior   = "Interior";
+            public const string k_PunctualDecorative = "Decorative";
+            public const string k_PunctualCandle     = "Candle";
+
+            // Exposure
+            public const string k_ExposureBrightSky     = "Bright Sky";
+            public const string k_ExposureOvercastSky   = "Overcast Sky";
+            public const string k_ExposureSunriseSunset = "Sunrise or Sunset";
+            public const string k_ExposureInterior      = "Interior";
+            public const string k_ExposureMoonlitSky    = "Moonlit Sky";
+            public const string k_ExposureMoonlessNight = "Moonless Night";
+
+            // Temperature
+            public const string k_TemperatureBlueSky        = "Blue Sky";
+            public const string k_TemperatureCloudySky      = "Cloudy Sky";
+            public const string k_TemperatureDirectSunlight = "Direct Sunlight";
+            public const string k_TemperatureArtificial     = "Artificial";
+            public const string k_TemperatureCandle         = "Candle";
+        }
+
+        private static class LightUnitSliderRanges
+        {
+            // Shorthand helper for converting the pre-defined ranges into other units (Nits, EV, Candela).
+            static float LuxToEV(float x) => LightUtils.ConvertLuxToEv(x, 1f);
+            static float LuxToCandela(float x) => LightUtils.ConvertLuxToCandela(x, 1f);
+
+            // Note: In case of area light, the intensity is scaled by the light size. How should this be reconciled in the UI?
+            static float LumenToNits(float x) => LightUtils.ConvertRectLightLumenToLuminance(x, 1f, 1f);
+
+            public static readonly LightUnitSliderUIRange[] k_LumenValueTable =
+            {
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconExterior,   LightUnitTooltips.k_PunctualExterior,   new Vector2(3000, 40000)),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconInterior,   LightUnitTooltips.k_PunctualInterior,   new Vector2(300,  3000)),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconDecorative, LightUnitTooltips.k_PunctualDecorative, new Vector2(15,   300)),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconCandle,     LightUnitTooltips.k_PunctualCandle,     new Vector2(0,    15)),
+            };
+
+            public static readonly LightUnitSliderUIRange[] k_NitsValueTable =
+            {
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconExterior,   LightUnitTooltips.k_PunctualExterior,   new Vector2(LumenToNits(3000), LumenToNits(40000))),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconInterior,   LightUnitTooltips.k_PunctualInterior,   new Vector2(LumenToNits(300),  LumenToNits(3000))),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconDecorative, LightUnitTooltips.k_PunctualDecorative, new Vector2(LumenToNits(15),   LumenToNits(300))),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconCandle,     LightUnitTooltips.k_PunctualCandle,     new Vector2(0,               LumenToNits(15))),
+            };
+
+            public static readonly LightUnitSliderUIRange[] k_LuxValueTable =
+            {
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconBrightSky,     LightUnitTooltips.k_LuxBrightSky,     new Vector2(80000, 120000)),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconOvercastSky,   LightUnitTooltips.k_LuxOvercastSky,   new Vector2(10000, 80000)),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconSunriseSunset, LightUnitTooltips.k_LuxSunriseSunset, new Vector2(1,     10000)),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconMoonlitSky,    LightUnitTooltips.k_LuxMoonlight,     new Vector2(0,     1)),
+            };
+
+            public static readonly LightUnitSliderUIRange[] k_CandelaValueTable =
+            {
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconExterior,   LightUnitTooltips.k_PunctualExterior,   new Vector2(LuxToCandela(80000),  LuxToCandela(120000))),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconInterior,   LightUnitTooltips.k_PunctualInterior,   new Vector2(LuxToCandela(10000),  LuxToCandela(80000))),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconDecorative, LightUnitTooltips.k_PunctualDecorative, new Vector2(LuxToCandela(1),      LuxToCandela(10000))),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconCandle,     LightUnitTooltips.k_PunctualCandle,     new Vector2(0,                       LuxToCandela(1))),
+            };
+
+            public static readonly LightUnitSliderUIRange[] k_EV100ValueTable =
+            {
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconExterior,   LightUnitTooltips.k_PunctualExterior,   new Vector2(LuxToEV(80000),  LuxToEV(120000))),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconInterior,   LightUnitTooltips.k_PunctualInterior,   new Vector2(LuxToEV(10000),  LuxToEV(80000))),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconDecorative, LightUnitTooltips.k_PunctualDecorative, new Vector2(LuxToEV(1),      LuxToEV(10000))),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconCandle,     LightUnitTooltips.k_PunctualCandle,     new Vector2(0,                  LuxToEV(1))),
+            };
+
+            // Same units as EV100, but we declare a new table since we use different icons in the exposure context.
+            public static readonly LightUnitSliderUIRange[] k_ExposureValueTable =
+            {
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconBrightSky,      LightUnitTooltips.k_ExposureBrightSky,     new Vector2(12, 16)),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconOvercastSky,    LightUnitTooltips.k_ExposureOvercastSky,   new Vector2(8,  12)),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconSunriseSunset,  LightUnitTooltips.k_ExposureSunriseSunset, new Vector2(6,   8)),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconInterior,       LightUnitTooltips.k_ExposureInterior,      new Vector2(3,   6)),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconMoonlitSky,     LightUnitTooltips.k_ExposureMoonlitSky,    new Vector2(0,   3)),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconMoonlessNight,  LightUnitTooltips.k_ExposureMoonlessNight, new Vector2(-3,  0)),
+            };
+
+            public static readonly LightUnitSliderUIRange[] k_KelvinValueTable =
+            {
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconClearSky,      LightUnitTooltips.k_TemperatureBlueSky,        new Vector2(10000, 20000)),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconOvercastSky,   LightUnitTooltips.k_TemperatureCloudySky,      new Vector2(6500,  10000)),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconDirectSunlight,LightUnitTooltips.k_TemperatureDirectSunlight, new Vector2(3500,   6500)),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconExterior,      LightUnitTooltips.k_TemperatureArtificial,     new Vector2(2500,   3500)),
+                new LightUnitSliderUIRange(HDRenderPipeline.defaultAsset.renderPipelineEditorResources.textures.iconCandle,        LightUnitTooltips.k_ExposureMoonlitSky,        new Vector2(1500,   2500)),
+            };
+        }
+    }
+}
