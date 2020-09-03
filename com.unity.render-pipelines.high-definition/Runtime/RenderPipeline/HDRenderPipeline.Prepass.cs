0fb04b16c90ffe243e9522c092c44fbd1934e7e7
Sebastien Lagarde <sebastien@unity3d.com>
2020-09-03 11:56:00 +0200

    Revert "Remove Decal Property mask generation (#1676)"
    This reverts commit 96a28ae1ce6cba1704000fcaf081329a6ce31361.

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.Prepass.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.Prepass.cs
@@ -467,6 +467,7 @@ namespace UnityEngine.Rendering.HighDefinition
         {
             for (int i = 0; i < dBufferOutput.dBufferCount; ++i)
                 ctx.cmd.SetGlobalTexture(HDShaderIDs._DBufferTexture[i], dBufferOutput.mrt[i]);
+            ctx.cmd.SetGlobalBuffer(HDShaderIDs._DecalPropertyMaskBufferSRV, dBufferOutput.decalPropertyMaskBuffer);
         }
 
         static void BindProbeVolumeGlobalData(in FrameSettings frameSettings, GBufferPassData data, in RenderGraphContext ctx)
@@ -674,6 +675,7 @@ namespace UnityEngine.Rendering.HighDefinition
             public RendererListHandle       meshDecalsRendererList;
             public TextureHandle            depthStencilBuffer;
             public TextureHandle            depthTexture;
+            public ComputeBufferHandle      propertyMaskBuffer;
             public TextureHandle            decalBuffer;
         }
 
@@ -710,12 +712,14 @@ namespace UnityEngine.Rendering.HighDefinition
             propertyMaskBufferSize = ((propertyMaskBufferSize + 63) / 64) * 64; // round off to nearest multiple of 64 for ease of use in CS
 
             passData.depthStencilBuffer = builder.UseDepthBuffer(output.resolvedDepthBuffer, DepthAccess.Write);
+            passData.propertyMaskBuffer = builder.WriteComputeBuffer(renderGraph.CreateComputeBuffer(new ComputeBufferDesc(propertyMaskBufferSize, 4) { name = "DecalPropertyMask" }));
 
             output.dbuffer.dBufferCount = passData.dBufferCount;
             for (int i = 0; i < passData.dBufferCount; ++i)
             {
                 output.dbuffer.mrt[i] = passData.mrt[i];
             }
+            output.dbuffer.decalPropertyMaskBuffer = passData.propertyMaskBuffer;
         }
 
         static DBufferOutput ReadDBuffer(DBufferOutput dBufferOutput, RenderGraphBuilder builder)
@@ -779,6 +783,7 @@ namespace UnityEngine.Rendering.HighDefinition
                                     data.depthStencilBuffer,
                                     data.depthTexture,
                                     data.meshDecalsRendererList,
+                                    data.propertyMaskBuffer,
                                     data.decalBuffer,
                                     context.renderContext,
                                     context.cmd);
