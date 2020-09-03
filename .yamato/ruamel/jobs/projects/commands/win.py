f1d88d860b11b172b1de741669811a6c67357cef
Sophia <sophia@unity3d.com>
2020-09-03 17:54:18 +0200

    tidy up code

--- .yamato/ruamel/jobs/projects/commands/win.py
+++ .yamato/ruamel/jobs/projects/commands/win.py
@@ -40,13 +40,9 @@ def cmd_standalone_build(project_folder, platform, api, test_platform_args):
 def cmd_not_standalone_performance(project_folder, platform, api, test_platform_args):
     base = _cmd_base(project_folder, platform["components"])
 
+    extra_cmds = extra_perf_cmd(project_folder)
     if project_folder.lower() == "BoatAttack".lower():
-        base.insert(0, [
-        f'git clone https://github.com/seanstolberg-unity/BoatAttack.git -b seans/add-perf-tests TestProjects/{project_folder}', #placeholder for now
-        f'Xcopy /E /I \"com.unity.render-pipelines.core\" \"{TEST_PROJECTS_DIR}/{project_folder}/Packages/com.unity.render-pipelines.core\" /Y',
-        f'Xcopy /E /I \"com.unity.render-pipelines.universal\" \"{TEST_PROJECTS_DIR}/{project_folder}/Packages/com.unity.render-pipelines.universal\" /Y',
-        f'Xcopy /E /I \"com.unity.shadergraph\" \"{TEST_PROJECTS_DIR}/{project_folder}/Packages/com.unity.shadergraph\" /Y'
-        ])
+        base.insert(0, extra_cmds)
  
     base.extend([
         f'cd {TEST_PROJECTS_DIR}/{project_folder} && utr {test_platform_args} --platform=StandaloneWindows64 --report-performance-data --performance-project-id=URP_Performance --testproject=. --editor-location=.Editor --artifacts_path={PATH_TEST_RESULTS}'
@@ -59,13 +55,9 @@ def cmd_standalone_performance(project_folder, platform, api, test_platform_args
         f'curl -s https://artifactory.internal.unity3d.com/core-automation/tools/utr-standalone/utr.bat --output {TEST_PROJECTS_DIR}/{project_folder}/utr.bat'
     ]
 
+    extra_cmds = extra_perf_cmd(project_folder)
     if project_folder.lower() == "BoatAttack".lower():
-        base.insert(0, [
-        f'git clone https://github.com/seanstolberg-unity/BoatAttack.git -b seans/add-perf-tests TestProjects/{project_folder}', #placeholder for now
-        f'Xcopy /E /I \"com.unity.render-pipelines.core\" \"{TEST_PROJECTS_DIR}/{project_folder}/Packages/com.unity.render-pipelines.core\" /Y',
-        f'Xcopy /E /I \"com.unity.render-pipelines.universal\" \"{TEST_PROJECTS_DIR}/{project_folder}/Packages/com.unity.render-pipelines.universal\" /Y',
-        f'Xcopy /E /I \"com.unity.shadergraph\" \"{TEST_PROJECTS_DIR}/{project_folder}/Packages/com.unity.shadergraph\" /Y'
-        ])
+        base.insert(0, extra_cmds)
 
     if project_folder.lower() == 'UniversalGraphicsTest'.lower():
         base.append('cd Tools && powershell -command ". .\\Unity.ps1; Set-ScreenResolution -width 1920 -Height 1080"')
@@ -77,15 +69,9 @@ def cmd_standalone_performance(project_folder, platform, api, test_platform_args
 
 def cmd_standalone_build_performance(project_folder, platform, api, test_platform_args):
     base = _cmd_base(project_folder, platform["components"])
-
+    extra_cmds = extra_perf_cmd(project_folder)
     if project_folder.lower() == "BoatAttack".lower():
-        base.insert(0, [
-        f'git clone https://github.com/seanstolberg-unity/BoatAttack.git -b seans/add-perf-tests-2 TestProjects/{project_folder}', #placeholder for now
-        f'Xcopy /E /I \"com.unity.render-pipelines.core\" \"{TEST_PROJECTS_DIR}/{project_folder}/Packages/com.unity.render-pipelines.core\" /Y',
-        f'Xcopy /E /I \"com.unity.render-pipelines.universal\" \"{TEST_PROJECTS_DIR}/{project_folder}/Packages/com.unity.render-pipelines.universal\" /Y',
-        f'Xcopy /E /I \"com.unity.shadergraph\" \"{TEST_PROJECTS_DIR}/{project_folder}/Packages/com.unity.shadergraph\" /Y',
-        f'Xcopy /E /I \"com.unity.shaderanalysis\" \"{TEST_PROJECTS_DIR}/{project_folder}/Packages/com.unity.shaderanalysis\" /Y'
-        ])
+        base.insert(0, extra_cmds)
 
     base.extend([
         f'cd {TEST_PROJECTS_DIR}/{project_folder} && utr --suite=playmode --platform=StandaloneWindows64 --extra-editor-arg="-executemethod" --extra-editor-arg="CustomBuild.BuildWindows{api["name"]}Linear" --testproject=. --editor-location=.Editor --artifacts_path={PATH_TEST_RESULTS} --timeout=1200 --player-save-path=../../{PATH_PLAYERS} --build-only'
@@ -94,3 +80,13 @@ def cmd_standalone_build_performance(project_folder, platform, api, test_platfor
 
 def _get_extra_utr_arg(project_folder):
     return ' --compilation-errors-as-warnings' if project_folder.lower() in ['universalhybridtest', 'hdrp_hybridtests'] else ''
+
+def extra_perf_cmd(project_folder):
+    perf_list = [
+        f'git clone https://github.com/seanstolberg-unity/BoatAttack.git -b seans/add-perf-tests-2 TestProjects/{project_folder}',
+        f'Xcopy /E /I \"com.unity.render-pipelines.core\" \"{TEST_PROJECTS_DIR}/{project_folder}/Packages/com.unity.render-pipelines.core\" /Y',
+        f'Xcopy /E /I \"com.unity.render-pipelines.universal\" \"{TEST_PROJECTS_DIR}/{project_folder}/Packages/com.unity.render-pipelines.universal\" /Y',
+        f'Xcopy /E /I \"com.unity.shadergraph\" \"{TEST_PROJECTS_DIR}/{project_folder}/Packages/com.unity.shadergraph\" /Y',
+        f'Xcopy /E /I \"com.unity.shaderanalysis\" \"{TEST_PROJECTS_DIR}/{project_folder}/Packages/com.unity.shaderanalysis\" /Y'
+        ]
+    return perf_list
