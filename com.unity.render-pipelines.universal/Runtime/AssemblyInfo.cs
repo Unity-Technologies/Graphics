fa8eed418205cccadfb91f18b79cd861255e2673
Sean Stolberg <seans@unity3d.com>
2020-09-04 12:58:25 -0700

    Make internals visible to perf test assemblies.

--- com.unity.render-pipelines.universal/Runtime/AssemblyInfo.cs
+++ com.unity.render-pipelines.universal/Runtime/AssemblyInfo.cs
@@ -5,3 +5,5 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.Universal.Editor.Tests")]
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.Universal.Runtime.Tests")]
 [assembly: InternalsVisibleTo("Unity.GraphicTests.Performance.Universal.Runtime")]
+[assembly: InternalsVisibleTo("Unity.RenderPipelines.Universal.Editor.Performance.Tests")]
+[assembly: InternalsVisibleTo("Unity.RenderPipelines.Universal.Runtime.Performance.Tests")]
