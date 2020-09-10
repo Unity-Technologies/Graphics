d5bb32a8476b0ca50d033c4185b990878b127961
Paul Demeulenaere <pauld@unity3d.com>
2020-09-10 15:39:01 +0200

    Simply generated code verifying at http://shader-playground.timjones.io/d7df4e7dbec05529d6943aacca2812b9

--- com.unity.visualeffectgraph/Shaders/VFXCommon.hlsl
+++ com.unity.visualeffectgraph/Shaders/VFXCommon.hlsl
@@ -344,7 +344,7 @@ float FixedRand(uint seed)
 ///////////////////
 // Mesh sampling //
 ///////////////////
-uint   FetchBuffer(ByteAddressBuffer buffer, int offset) { return buffer.Load(offset << 2); }
+uint   FetchBuffer(ByteAddressBuffer buffer, int offset)  { return buffer.Load(offset << 2); }
 uint2  FetchBuffer2(ByteAddressBuffer buffer, int offset) { return buffer.Load2(offset << 2); }
 uint3  FetchBuffer3(ByteAddressBuffer buffer, int offset) { return buffer.Load3(offset << 2); }
 uint4  FetchBuffer4(ByteAddressBuffer buffer, int offset) { return buffer.Load4(offset << 2); }
@@ -358,18 +358,17 @@ float4 SampleMeshReadFloat(ByteAddressBuffer vertices, uint offset, uint channel
         uint format = channelFormatAndDimension & 0xff;
         uint dimension = (channelFormatAndDimension >> 8) & 0xff;
 
-        [branch]
         if (format == VERTEXATTRIBUTEFORMAT_FLOAT32)
         {
-			//TODOPAUL : Double check if this code cannot be simplified.
-			
-            //for (uint i = 0u; i < maxRead && i < dimension; ++i)
-            //    r[i] = vertices[offset + i];
-            //^ Equivalent be less branch in generated IL (maxRead evaluation are constand folded)
-            r.x = asfloat(FetchBuffer(vertices, offset + 0u));
-            if (maxRead > 1u) r.y = dimension > 1 ? asfloat(FetchBuffer(vertices, offset + 1u)) : 0.0f;
-            if (maxRead > 2u) r.z = dimension > 2 ? asfloat(FetchBuffer(vertices, offset + 2u)) : 0.0f;
-            if (maxRead > 3u) r.w = dimension > 3 ? asfloat(FetchBuffer(vertices, offset + 3u)) : 0.0f;
+            uint readSize = min(dimension, maxRead);
+            uint4 readValue = (uint4)r;
+
+            if      (readSize == 4u)    readValue.xyzw  = FetchBuffer4(vertices, offset);
+            else if (readSize == 3u)    readValue.xyz   = FetchBuffer3(vertices, offset);
+            else if (readSize == 2u)    readValue.xy    = FetchBuffer2(vertices, offset);
+            else                        readValue.x     = FetchBuffer(vertices, offset);
+
+            r = asfloat(readValue);
         }
         else
         {
