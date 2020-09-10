7eb8c8e7a6f4d685c9dcc98b7338569bd98bbb7d
Paul Demeulenaere <pauld@unity3d.com>
2020-09-10 11:07:05 +0200

    Clean multiple place where "ByteAddressBuffer" is declared

--- com.unity.visualeffectgraph/Editor/Expressions/VFXExpressionAbstract.cs
+++ com.unity.visualeffectgraph/Editor/Expressions/VFXExpressionAbstract.cs
@@ -132,14 +132,10 @@ namespace UnityEditor.VFX
                 case VFXValueType.TextureCube: return "TextureCube";
                 case VFXValueType.TextureCubeArray: return "TextureCubeArray";
                 case VFXValueType.Matrix4x4: return "float4x4";
+                case VFXValueType.Mesh:
+                case VFXValueType.SkinnedMeshRenderer:
                 case VFXValueType.Buffer: return "ByteAddressBuffer";
                 case VFXValueType.Boolean: return "bool";
-            }
-
-            //TODOPAUL : Remove this and handle correctly Mesh exposed but not used within the graph
-            if (type == VFXValueType.Mesh)
-            {
-                return "ByteAddressBuffer";
             }
 
             throw new NotImplementedException(type.ToString());
