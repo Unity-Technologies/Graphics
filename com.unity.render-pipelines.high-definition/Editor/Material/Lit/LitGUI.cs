929a38e88661ebe513aa2feeb6e1671c3149b295
FrancescoC-Unity <francescoc@unity3d.com>
2020-09-16 14:20:21 +0200

    Deleted too many keyword sets.

--- com.unity.render-pipelines.high-definition/Editor/Material/Lit/LitGUI.cs
+++ com.unity.render-pipelines.high-definition/Editor/Material/Lit/LitGUI.cs
@@ -119,6 +119,7 @@ namespace UnityEditor.Rendering.HighDefinition
 
                 if (material.HasProperty(kEmissiveColorMap))
                 {
+                    CoreUtils.SetKeyword(material, "_EMISSIVE_COLOR_MAP", material.GetTexture(kEmissiveColorMap));
                     material.SetFloat(kUVEmissive, (float)emissiveMapping); 
                 }
                 else if ((int)emissiveMapping > (int)UVEmissiveMapping.UV3) // Meaning it was set as a non UV-set option and we don't have an emissive map, so ...
