76a83ce72c76a5143de6e3d773853e393c9b9f77
Liis Kivistik <liis.kivistik@unity3d.com>
2020-09-18 17:18:22 +0200

    fix unity revision file path

--- .yamato/ruamel/jobs/projects/commands/linux.py
+++ .yamato/ruamel/jobs/projects/commands/linux.py
@@ -8,7 +8,7 @@ def _cmd_base(project_folder, platform, utr_flags, editor):
         f'sudo -H pip install unity-downloader-cli --index-url {UNITY_DOWNLOADER_CLI_URL} --upgrade',
         f'curl -s {UTR_INSTALL_URL} --output {TEST_PROJECTS_DIR}/{project_folder}/utr',
         f'chmod +x {TEST_PROJECTS_DIR}/{project_folder}/utr',
-        f'cd {TEST_PROJECTS_DIR}/{project_folder} && sudo unity-downloader-cli { get_unity_downloader_cli_cmd(editor, platform["os"]) } {"".join([f"-c {c} " for c in platform["components"]])} --wait --published-only',
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && sudo unity-downloader-cli { get_unity_downloader_cli_cmd(editor, platform["os"], cd=True) } {"".join([f"-c {c} " for c in platform["components"]])} --wait --published-only',
         f'cd {TEST_PROJECTS_DIR}/{project_folder} && DISPLAY=:0.0 ./utr {" ".join(utr_flags)}'
     ]
 
