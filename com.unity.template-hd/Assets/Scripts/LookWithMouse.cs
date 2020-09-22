92e2682f193f0c610f4723759a53089c11c9bb80
Antoine Lelièvre <antoinel@unity3d.com>
2020-09-22 15:22:55 +0200

    Added input system suppoort to SimpleCameraController.cs

--- com.unity.template-hd/Assets/Scripts/LookWithMouse.cs
+++ com.unity.template-hd/Assets/Scripts/LookWithMouse.cs
@@ -28,7 +28,7 @@ public class LookWithMouse : MonoBehaviour
 
         if (Mouse.current != null)
         {
-            var delta = Mouse.current.delta.ReadValue() / 5.0f;
+            var delta = Mouse.current.delta.ReadValue() / 15.0f;
             mouseX += delta.x;
             mouseY += delta.y;
         }
