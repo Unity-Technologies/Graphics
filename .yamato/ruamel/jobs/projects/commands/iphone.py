8d6aa156e97d80c67b8c90cead8b78240eeedf06
Sophia <sophia@unity3d.com>
2020-09-28 18:09:53 +0200

    add ios commands back in

--- .yamato/ruamel/jobs/projects/commands/iphone.py
+++ .yamato/ruamel/jobs/projects/commands/iphone.py
@@ -7,7 +7,16 @@ def _cmd_base(project_folder, platform, editor):
     return []
 
 def cmd_editmode(project_folder, platform, api, test_platform, editor):
-    return []
+    utr_args = utr_standalone_build_flags(platform_spec='', platform='iOS', testproject=f'{TEST_PROJECTS_DIR}/{project_folder}', player_save_path=PATH_PLAYERS)
+    utr_args.extend(test_platform["extra_utr_flags"])
+
+    return [
+        f'pip install unity-downloader-cli --index-url {UNITY_DOWNLOADER_CLI_URL} --upgrade',
+        f'unity-downloader-cli { get_unity_downloader_cli_cmd(editor, platform["os"]) } {"".join([f"-c {c} " for c in platform["components"]])}  --wait --published-only',
+        f'curl -s {UTR_INSTALL_URL} --output utr',
+        f'chmod +x ./utr',
+        f'./utr {" ".join(utr_args)}'
+     ]
 
 def cmd_playmode(project_folder, platform, api, test_platform, editor):
     return []
