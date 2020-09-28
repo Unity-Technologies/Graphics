60ec8c2c06e6a5981a6e741da3d54866954164ec
Sebastien Lagarde <sebastien@unity3d.com>
2020-09-28 10:16:01 +0200

    Revert "Fix hair shader compilation issue with Debug Display mode (#2023)"
    This reverts commit 16989c6867ff5c288dbb8913a120bf514ade5783.

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/LightLoop.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/LightLoop.hlsl
@@ -163,12 +163,10 @@ void ApplyDebug(LightLoopContext context, PositionInputs posInput, BSDFData bsdf
 
         lightLoopOutput.diffuseLighting = SAMPLE_TEXTURE2D_LOD(_DebugMatCapTexture, s_linear_repeat_sampler, UV, 0).rgb * (_MatcapMixAlbedo > 0  ? defaultColor.rgb * _MatcapViewScale : 1.0f);
 
-    #ifdef OUTPUT_SPLIT_LIGHTING
-        if (_EnableSubsurfaceScattering != 0 && ShouldOutputSplitLighting(bsdfData))
+        if (ShouldOutputSplitLighting(bsdfData) && _EnableSubsurfaceScattering != 0)
         {
             lightLoopOutput.specularLighting = lightLoopOutput.diffuseLighting;
         }
-    #endif
 
     }
 #endif
