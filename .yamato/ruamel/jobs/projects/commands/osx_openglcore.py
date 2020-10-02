e4196b44d52952adf830be4f92f24315d90b06c5
Sophia <sophia@unity3d.com>
2020-10-02 14:54:13 +0200

    fix openglcore parse issue

--- .yamato/ruamel/jobs/projects/commands/osx_openglcore.py
+++ .yamato/ruamel/jobs/projects/commands/osx_openglcore.py
@@ -1,5 +1,5 @@
 from ...shared.constants import TEST_PROJECTS_DIR, PATH_UNITY_REVISION, PATH_TEST_RESULTS, PATH_PLAYERS, UNITY_DOWNLOADER_CLI_URL, UTR_INSTALL_URL,get_unity_downloader_cli_cmd
-from ...shared.utr_utils import utr_editmode_flags, utr_playmode_flags, utr_standalone_split_flags, utr_standalone_build_flags
+from ...shared.utr_utils import utr_editmode_flags, utr_playmode_flags, utr_standalone_split_flags, utr_standalone_build_flags, utr_standalone_not_split_flags
 
 def _cmd_base(project_folder, platform, utr_flags, editor):
     return [
@@ -25,7 +25,10 @@ def cmd_playmode(project_folder, platform, api, test_platform, editor, build_con
     return  _cmd_base(project_folder, platform, utr_args, editor)
 
 def cmd_standalone(project_folder, platform, api, test_platform, editor, build_config, color_space):
-    return []
+    utr_args = utr_standalone_not_split_flags(platform_spec='OSX')
+    utr_args.extend(test_platform["extra_utr_flags"])
+    utr_args.extend(platform["extra_utr_flags"])
+    return  _cmd_base(project_folder, platform, utr_args, editor)
 
 def cmd_standalone_build(project_folder, platform, api, test_platform, editor, build_config, color_space):
-    return []
+    raise NotImplementedError('osx_metal: standalone_split set to true but build commands not specified')
