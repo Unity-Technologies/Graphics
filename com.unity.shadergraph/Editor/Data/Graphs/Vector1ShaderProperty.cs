8804849b3b52766d8acd26f1e206bba3dde256dc
sebastienlagarde <sebastien@unity3d.com>
2020-10-02 11:26:34 +0200

    Fix blackboard info name after merge of Antoie PR

--- com.unity.shadergraph/Editor/Data/Graphs/Vector1ShaderProperty.cs
+++ com.unity.shadergraph/Editor/Data/Graphs/Vector1ShaderProperty.cs
@@ -10,7 +10,7 @@ namespace UnityEditor.ShaderGraph.Internal
     [Serializable]
     [FormerName("UnityEditor.ShaderGraph.FloatShaderProperty")]
     [FormerName("UnityEditor.ShaderGraph.Vector1ShaderProperty")]
-    [BlackboardInputInfo(0)]
+    [BlackboardInputInfo(0, "Float")]
     public sealed class Vector1ShaderProperty : AbstractShaderProperty<float>
     {
         internal Vector1ShaderProperty()
