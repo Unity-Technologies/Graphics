8b8087df00e77b96b9c601ee4c4c0126266fd6dd
Marc Templin <marcte@unity3d.com>
2020-10-02 15:55:18 -0700

    Fix for visual issue where input/output areas were not hidden on initialization.

--- com.unity.shadergraph/Editor/Drawing/Views/MaterialNodeView.cs
+++ com.unity.shadergraph/Editor/Drawing/Views/MaterialNodeView.cs
@@ -568,6 +568,8 @@ namespace UnityEditor.ShaderGraph.Drawing
         {
             foreach (var slot in slots)
                 AddShaderPortForSlot(slot);
+            // Make sure the visuals are properly updated to reflect port list
+            RefreshPorts();
         }
 
         void OnEdgeDisconnected(Port obj)
