bc78d6e59965ecd2cc2dcf0b5ef9b01ce1a3f211
Sophia <sophia@unity3d.com>
2020-10-06 13:54:48 +0200

    edit mac and iphone commands

--- .yamato/ruamel/jobs/projects/commands/osx_metal.py
+++ .yamato/ruamel/jobs/projects/commands/osx_metal.py
@@ -31,7 +31,20 @@ def cmd_editmode(project_folder, platform, api, test_platform, editor, build_con
     )
     utr_args.extend(test_platform["extra_utr_flags"])
     utr_args.extend(platform["extra_utr_flags"])
-    return  _cmd_base(project_folder, platform, utr_args, editor)
+
+    base = _cmd_base(project_folder, platform, utr_args, editor)
+    
+    extra_cmds = extra_perf_cmd(project_folder)
+    unity_config = install_unity_config(project_folder)
+    extra_cmds = extra_cmds + unity_config
+    if project_folder.lower() == "BoatAttack".lower():
+        x=0
+        for y in extra_cmds:
+            base.insert(x, y)
+            x += 1
+        #base.extend(unity_config)
+    
+    return base
 
 def cmd_playmode(project_folder, platform, api, test_platform, editor, build_config, color_space):
     scripting_backend = build_config["scripting_backend"]
@@ -44,7 +57,19 @@ def cmd_playmode(project_folder, platform, api, test_platform, editor, build_con
     )
     utr_args.extend(test_platform["extra_utr_flags"])
     utr_args.extend(platform["extra_utr_flags"])
-    return  _cmd_base(project_folder, platform, utr_args, editor)
+
+    base = _cmd_base(project_folder, platform, utr_args, editor)
+    
+    extra_cmds = extra_perf_cmd(project_folder)
+    unity_config = install_unity_config(project_folder)
+    extra_cmds = extra_cmds + unity_config
+    if project_folder.lower() == "BoatAttack".lower():
+        x=0
+        for y in extra_cmds:
+            base.insert(x, y)
+            x += 1
+    
+    return base
 
 
 def cmd_standalone(project_folder, platform, api, test_platform, editor, build_config, color_space):
@@ -61,8 +86,57 @@ def cmd_standalone(project_folder, platform, api, test_platform, editor, build_c
     utr_args.extend(test_platform["extra_utr_flags"])
     utr_args.extend(platform["extra_utr_flags"])
     utr_args.append(f'--timeout={get_timeout(test_platform, "OSX_Metal")}')
-    return  _cmd_base(project_folder, platform, utr_args, editor)
+    base = _cmd_base(project_folder, platform, utr_args, editor)
+    
+    extra_cmds = extra_perf_cmd(project_folder)
+    unity_config = install_unity_config(project_folder)
+    extra_cmds = extra_cmds + unity_config
+    if project_folder.lower() == "BoatAttack".lower():
+        x=0
+        for y in extra_cmds:
+            base.insert(x, y)
+            x += 1
+    
+    return base
 
 
 def cmd_standalone_build(project_folder, platform, api, test_platform, editor, build_config, color_space):
     raise NotImplementedError('osx_metal: standalone_split set to true but build commands not specified')
+
+def extra_perf_cmd(project_folder):   
+    perf_list = [
+        f'git clone https://github.com/Unity-Technologies/BoatAttack.git -b feature/benchmark TestProjects/{project_folder}'
+        ]
+    return perf_list
+
+def install_unity_config(project_folder):
+    cmds = [
+        f'choco source add -n Unity -s https://artifactory.prd.it.unity3d.com/artifactory/api/nuget/unity-choco-local',
+        f'choco install unity-config',
+
+
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency "com.unity.render-pipelines.core@file:../../../com.unity.render-pipelines.core" --project-path .',
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency "com.unity.render-pipelines.universal@file:../../../com.unity.render-pipelines.universal" --project-path .',
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency "com.unity.shadergraph@file:../../../com.unity.shadergraph" --project-path .',
+
+
+		#f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project remove dependency com.unity.render-pipelines.universal',
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency com.unity.addressables@1.16.2-preview.200925 --project-path .',
+		f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency com.unity.test-framework@1.2.1-preview.1 --project-path .',
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency com.unity.test-framework.performance@2.3.1-preview --project-path .',
+		f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency com.unity.test-framework.utp-reporter@1.0.2-preview --project-path .',
+		f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency com.unity.test-framework.build@0.0.1-preview.12 --project-path .',
+        
+		f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency \"com.unity.test.metadata-manager@ssh://git@github.cds.internal.unity3d.com/unity/com.unity.test.metadata-manager.git\" --project-path .',        
+		f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency \"com.unity.testing.graphics-performance@ssh://git@github.cds.internal.unity3d.com/unity/com.unity.testing.graphics-performance.git\"  --project-path .',        
+		f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency \"unity.graphictests.performance.universal@ssh://git@github.cds.internal.unity3d.com/unity/unity.graphictests.performance.universal.git\" --project-path .',	
+		
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add testable com.unity.cli-project-setup  --project-path .',		
+		f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add testable com.unity.test.performance.runtimesettings  --project-path .',
+		f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add testable test.metadata-manager  --project-path .',
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add testable com.unity.testing.graphics-performance --project-path .',
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add testable com.unity.render-pipelines.core  --project-path .',
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add testable unity.graphictests.performance.universal  --project-path .',
+        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project set project-update false --project-path .'
+    ]
+    return cmds
