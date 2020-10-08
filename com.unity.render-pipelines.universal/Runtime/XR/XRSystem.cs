a70683286bc20d96f5bcc32a09e940901fbd94c1
thomas-zeng <thomas.zeng@unity3d.com>
2020-10-08 10:34:38 -0700

    Added next power of 2 logic and clamp logic to XR msaa setter.

--- com.unity.render-pipelines.universal/Runtime/XR/XRSystem.cs
+++ com.unity.render-pipelines.universal/Runtime/XR/XRSystem.cs
@@ -86,6 +86,12 @@ namespace UnityEngine.Rendering.Universal
 
         internal static void UpdateMSAALevel(int level)
         {
+            if (msaaLevel == level)
+                return;
+
+            level = Mathf.NextPowerOfTwo(level);
+            level = Mathf.Clamp(level, (int)MsaaQuality.Disabled, (int)MsaaQuality._8x);
+
             GetDisplaySubsystem();
 
 #if UNITY_2020_2_OR_NEWER
