57b5d03de3ed5ae1e061e994c68577decb6d85a7
John Parsaie <johnpa@unity3d.com>
2020-10-07 20:23:20 -0400

    Fix the temperature slider to fit 6500K to the white point on temperature gradient texture

--- com.unity.render-pipelines.high-definition/Editor/Lighting/LightUnit/LightUnitSlider.cs
+++ com.unity.render-pipelines.high-definition/Editor/Lighting/LightUnit/LightUnitSlider.cs
@@ -252,55 +252,6 @@ namespace UnityEditor.Rendering.HighDefinition
     }
 
     /// <summary>
-    /// Formats the provided descriptor into an exponential slider with contextual slider markers, tooltips, and icons.
-    /// </summary>
-    class ExponentialLightUnitSlider : LightUnitSlider
-    {
-        private Vector3 m_ExponentialConstraints;
-
-        /// <summary>
-        /// Exponential slider modeled to set a f(0.5) value.
-        /// ref: https://stackoverflow.com/a/17102320
-        /// </summary>
-        void PrepareExponentialConstraints(float lo, float mi, float hi)
-        {
-            float x = lo;
-            float y = mi;
-            float z = hi;
-
-            // https://www.desmos.com/calculator/yx2yf4huia
-            m_ExponentialConstraints.x = ((x * z) - (y * y)) / (x - (2 * y) + z);
-            m_ExponentialConstraints.y = ((y - x) * (y - x)) / (x - (2 * y) + z);
-            m_ExponentialConstraints.z = 2 * Mathf.Log((z - y) / (y - x));
-        }
-
-        float ValueToSlider(float x) => Mathf.Log((x - m_ExponentialConstraints.x) / m_ExponentialConstraints.y) / m_ExponentialConstraints.z;
-        float SliderToValue(float x) => m_ExponentialConstraints.x + m_ExponentialConstraints.y * Mathf.Exp(m_ExponentialConstraints.z * x);
-
-        public ExponentialLightUnitSlider(LightUnitSliderUIDescriptor descriptor) : base(descriptor)
-        {
-            var halfValue = 300; // TODO: Compute the median
-            PrepareExponentialConstraints(m_Descriptor.sliderRange.x, halfValue, m_Descriptor.sliderRange.y);
-        }
-
-        protected override float GetPositionOnSlider(float value)
-        {
-            return ValueToSlider(value);
-        }
-
-        protected override void DoSlider(Rect rect, SerializedProperty value, Vector2 sliderRange)
-        {
-            value.floatValue = ExponentialSlider(rect, value.floatValue);
-        }
-
-        float ExponentialSlider(Rect rect, float value)
-        {
-            var internalValue = GUI.HorizontalSlider(rect, ValueToSlider(value), 0f, 1f);
-            return SliderToValue(internalValue);
-        }
-    }
-
-    /// <summary>
     /// Formats the provided descriptor into a piece-wise linear slider with contextual slider markers, tooltips, and icons.
     /// </summary>
     class PiecewiseLightUnitSlider : LightUnitSlider
@@ -510,7 +461,7 @@ namespace UnityEditor.Rendering.HighDefinition
         {
             m_Light?.Update();
 
-            // Convert to the actual unit.
+            // Convert to the actual unit value.
             value.floatValue = LumenToUnit(preset.presetValue);
 
             m_Light?.Apply();
@@ -522,9 +473,42 @@ namespace UnityEditor.Rendering.HighDefinition
     /// </summary>
     class TemperatureSlider : LightUnitSlider
     {
+        private Vector3 m_ExponentialConstraints;
+
         private LightEditor.Settings m_Settings;
 
         private static Texture2D s_KelvinGradientTexture;
+
+        /// <summary>
+        /// Exponential slider modeled to set a f(0.5) value.
+        /// ref: https://stackoverflow.com/a/17102320
+        /// </summary>
+        void PrepareExponentialConstraints(float lo, float mi, float hi)
+        {
+            // float x = lo;
+            // float y = mi;
+            // float z = hi;
+            //
+            // // https://www.desmos.com/calculator/yx2yf4huia
+            // m_ExponentialConstraints.x = ((x * z) - (y * y)) / (x - (2 * y) + z);
+            // m_ExponentialConstraints.y = ((y - x) * (y - x)) / (x - (2 * y) + z);
+            // m_ExponentialConstraints.z = 2 * Mathf.Log((z - y) / (y - x));
+
+            // Warning: These are the coefficients for a system of equation fit for a continuous, monotonic curve that fits a f(0.44) value.
+            // f(0.44) is required instead of f(0.5) due to the location of the white in the temperature gradient texture.
+            // The equation is solved to get the coefficient for the following constraint for low, mid, hi:
+            // f(0)    = 1500
+            // f(0.44) = 6500
+            // f(1.0)  = 20000
+            // If for any reason the constraints are changed, then the function must be refit and the new coefficients found.
+            // Note that we can't re-use the original PowerSlider instead due to how it forces a text field, which we don't want in this case.
+            m_ExponentialConstraints.x = -3935.53965427f;
+            m_ExponentialConstraints.y =  5435.53965427f;
+            m_ExponentialConstraints.z =     1.48240556f;
+        }
+
+        protected float ValueToSlider(float x) => Mathf.Log((x - m_ExponentialConstraints.x) / m_ExponentialConstraints.y) / m_ExponentialConstraints.z;
+        protected float SliderToValue(float x) => m_ExponentialConstraints.x + m_ExponentialConstraints.y * Mathf.Exp(m_ExponentialConstraints.z * x);
 
         static Texture2D GetKelvinGradientTexture(LightEditor.Settings settings)
         {
@@ -543,7 +527,11 @@ namespace UnityEditor.Rendering.HighDefinition
             return s_KelvinGradientTexture;
         }
 
-        public TemperatureSlider(LightUnitSliderUIDescriptor descriptor) : base(descriptor) {}
+        public TemperatureSlider(LightUnitSliderUIDescriptor descriptor) : base(descriptor)
+        {
+            var halfValue = 6500;
+            PrepareExponentialConstraints(m_Descriptor.sliderRange.x, halfValue, m_Descriptor.sliderRange.y);
+        }
 
         public void Setup(LightEditor.Settings settings)
         {
@@ -569,7 +557,18 @@ namespace UnityEditor.Rendering.HighDefinition
         void SliderWithTextureNoTextField(Rect rect, SerializedProperty value, Vector2 range, LightEditor.Settings settings)
         {
             GUI.DrawTexture(rect, GetKelvinGradientTexture(settings));
-            value.floatValue = GUI.HorizontalSlider(rect, value.floatValue, range.x, range.y, SliderStyles.k_TemperatureBorder, SliderStyles.k_TemperatureThumb);
+
+            EditorGUI.BeginChangeCheck();
+
+            // Draw the exponential slider that fits 6500K to the white point on the gradient texture.
+            var internalValue = GUI.HorizontalSlider(rect, ValueToSlider(value.floatValue), 0f, 1f, SliderStyles.k_TemperatureBorder, SliderStyles.k_TemperatureThumb);
+
+            // Map the value back into kelvin.
+            value.floatValue = SliderToValue(internalValue);
+
+            // Round to nearest since so much precision is not necessary for kelvin while sliding.
+            if (EditorGUI.EndChangeCheck())
+                value.floatValue = Mathf.Round(value.floatValue);
         }
     }
 
