799b0ccd11f3a8d362b143243a27cb4dbd12b950
Liis Kivistik <liis.kivistik@unity3d.com>
2020-10-09 14:54:56 +0200

    fix packages agent

--- .yamato/ruamel/jobs/packages/project_publish_all.py
+++ .yamato/ruamel/jobs/packages/project_publish_all.py
@@ -14,6 +14,7 @@ class Project_PublishAllJob():
         # construct job
         job = YMLJob()
         job.set_name(f'Publish all packages [project context][recurrent]')
+        job.set_agent(agent)
         job.add_dependencies([f'{projectcontext_filepath()}#{projectcontext_job_id_publish(package["id"])}' for package in packages])
         job.add_commands([
                 f'git tag v$(cd com.unity.render-pipelines.core && node -e "console.log(require(\'./package.json\').version)")',
