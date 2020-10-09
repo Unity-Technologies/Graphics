481375fefea31e881508975dd0229ab9f97f484c
Liis Kivistik <liis.kivistik@unity3d.com>
2020-10-09 17:34:42 +0200

    [Yamato] Force update trunk revision (#2154)
    * Revert "[Yamato] Tmp let trunk use staging instead of latest_internal editor (#2131)"
    
    This reverts commit fa519312ac509a6490a3be9895bf4b266cacacbe.
    
    * force update trunk revision

--- .yamato/ruamel/jobs/shared/constants.py
+++ .yamato/ruamel/jobs/shared/constants.py
@@ -20,9 +20,8 @@ DEFAULT_TIMEOUT = 1200
 def get_editor_revision(editor, platform_os):
     if str(editor['track']).lower()=='custom-revision':
         return VAR_CUSTOM_REVISION
-    # TODO: Restore as soon as unity-downloader-cli is fixed on Windows (ETA ~10/10/2020)
-    # elif str(editor['track']).lower()=='trunk':
-    #     return editor["revisions"][f"{editor['track']}_latest_internal"][platform_os]["revision"]
+    elif str(editor['track']).lower()=='trunk':
+        return editor["revisions"][f"{editor['track']}_latest_internal"][platform_os]["revision"]
     else:
         return editor["revisions"][f"{editor['track']}_staging"][platform_os]["revision"]
 
