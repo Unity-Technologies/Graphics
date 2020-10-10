3e726bfb05834618d3ca461764aaf335f0685393
Sebastien Lagarde <sebastien@unity3d.com>
2020-10-10 15:49:47 +0200

    fix usage of primitive ID in HDRP

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/ShaderPass/ShaderPassFullScreenDebug.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/ShaderPass/ShaderPassFullScreenDebug.hlsl
@@ -31,21 +31,17 @@ PackedVaryingsToPS VertTesselation(VaryingsToDS input)
 #if !defined(_DEPTHOFFSET_ON)
 [earlydepthstencil] // quad overshading debug mode writes to UAV
 #endif
-void Frag(
-#ifdef PLATFORM_SUPPORTS_PRIMITIVE_ID_IN_PIXEL_SHADER
-    uint primitiveID : SV_PrimitiveID, // Must be declare before FRONT_FACE_SEMANTIC
-#endif
-    PackedVaryingsToPS packedInput)
+void Frag(PackedVaryingsToPS packedInput)
 {
     UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(packedInput);
     FragInputs input = UnpackVaryingsMeshToFragInputs(packedInput.vmesh);
 
     PositionInputs posInput = GetPositionInput(input.positionSS.xy, _ScreenSize.zw, input.positionSS.z, input.positionSS.w, input.positionRWS.xyz);
 
+#ifdef PLATFORM_SUPPORTS_PRIMITIVE_ID_IN_PIXEL_SHADER
     if (_DebugFullScreenMode == FULLSCREENDEBUGMODE_QUAD_OVERDRAW)
     {
-#ifdef PLATFORM_SUPPORTS_PRIMITIVE_ID_IN_PIXEL_SHADER
-        IncrementQuadOverdrawCounter(posInput.positionSS.xy, primitiveID);
-#endif
+        IncrementQuadOverdrawCounter(posInput.positionSS.xy, input.primitiveID);
     }
+#endif
 }
