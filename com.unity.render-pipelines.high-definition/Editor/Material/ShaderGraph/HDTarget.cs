bfabcd23b0997e61aeaead762007d17595f96437
sebastienlagarde <sebastien@unity3d.com>
2020-10-10 22:57:18 +0200

    Fix issue with SC_PrimitiveID not handlded correctly for overdraw (fail to compil with XR) (#2164)
    * fix usage of primitive ID in HDRP
    
    * Update HDStructFields.cs
    
    * Update SharedCode.template.hlsl
    
    * Update CHANGELOG.md

--- com.unity.render-pipelines.high-definition/Editor/Material/ShaderGraph/HDTarget.cs
+++ com.unity.render-pipelines.high-definition/Editor/Material/ShaderGraph/HDTarget.cs
@@ -407,6 +407,7 @@ namespace UnityEditor.Rendering.HighDefinition.ShaderGraph
             new FieldDependency(HDStructFields.FragInputs.texCoord2,                                 HDStructFields.VaryingsMeshToPS.texCoord2),
             new FieldDependency(HDStructFields.FragInputs.texCoord3,                                 HDStructFields.VaryingsMeshToPS.texCoord3),
             new FieldDependency(HDStructFields.FragInputs.color,                                     HDStructFields.VaryingsMeshToPS.color),
+            new FieldDependency(HDStructFields.FragInputs.primitiveID,                               HDStructFields.VaryingsMeshToPS.primitiveID),
             new FieldDependency(HDStructFields.FragInputs.IsFrontFace,                               HDStructFields.VaryingsMeshToPS.cullFace),
         };
 
