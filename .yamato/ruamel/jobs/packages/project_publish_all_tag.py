1717c39d84b132edf20195742a905561594f79a7
Liis Kivistik <liis.kivistik@unity3d.com>
2020-10-12 16:21:02 +0200

    take latest ruamel from master

--- .yamato/ruamel/jobs/packages/project_publish_all_tag.py
+++ .yamato/ruamel/jobs/packages/project_publish_all_tag.py
@@ -1,11 +1,11 @@
 from ruamel.yaml.scalarstring import DoubleQuotedScalarString as dss
-from ..shared.namer import packages_filepath, package_job_id_publish, package_job_id_publish_all
+from ..shared.namer import projectcontext_filepath, projectcontext_job_id_publish, projectcontext_job_id_publish_all_tag
 from ..shared.yml_job import YMLJob
 
-class Package_PublishAllJob():
+class Project_PublishAllTagJob():
     
     def __init__(self, packages, target_branch, agent):
-        self.job_id = package_job_id_publish_all()
+        self.job_id = projectcontext_job_id_publish_all_tag()
         self.yml = self.get_job_definition(packages, target_branch, agent).get_yml()
 
 
@@ -13,13 +13,12 @@ class Package_PublishAllJob():
 
         # construct job
         job = YMLJob()
-        job.set_name(f'Publish all packages')
+        job.set_name(f'Publish all packages [project context][manual][tag]')
         job.set_agent(agent)
-        job.add_dependencies([f'{packages_filepath()}#{package_job_id_publish(package["id"])}' for package in packages])
+        job.add_dependencies([f'{projectcontext_filepath()}#{projectcontext_job_id_publish(package["id"])}' for package in packages])
         job.add_commands([
                 f'git tag v$(cd com.unity.render-pipelines.core && node -e "console.log(require(\'./package.json\').version)")',
                 f'git push origin --tags'])
-        #job.add_trigger_recurrent(target_branch, 'daily')
         return job
 
 
