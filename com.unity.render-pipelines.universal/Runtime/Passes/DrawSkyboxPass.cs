bfb132b0b7637e9fb1fb527434b63d94e8eae217
thomas-zeng <thomas.zeng@unity3d.com>
2020-10-13 16:45:52 -0700

    Fixed camera matrix not restored after modification.

--- com.unity.render-pipelines.universal/Runtime/Passes/DrawSkyboxPass.cs
+++ com.unity.render-pipelines.universal/Runtime/Passes/DrawSkyboxPass.cs
@@ -43,6 +43,9 @@ namespace UnityEngine.Rendering.Universal
                     context.ExecuteCommandBuffer(cmd);
 
                     CommandBufferPool.Release(cmd);
+
+                    renderingData.cameraData.camera.ResetStereoProjectionMatrices();
+                    renderingData.cameraData.camera.ResetStereoViewMatrices();
                 }
                 else
                 {
@@ -50,6 +53,9 @@ namespace UnityEngine.Rendering.Universal
                     renderingData.cameraData.camera.worldToCameraMatrix = renderingData.cameraData.GetViewMatrix(0);
 
                     context.DrawSkybox(renderingData.cameraData.camera);
+
+                    renderingData.cameraData.camera.ResetProjectionMatrix();
+                    renderingData.cameraData.camera.ResetWorldToCameraMatrix();
                 }
             }
             else
