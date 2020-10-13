5373df4e1482d50fcc30f204b276a04205f6ee66
Liis Kivistik <liis.kivistik@unity3d.com>
2020-10-13 12:31:22 +0200

    fix flags in shared file

--- .yamato/ruamel/jobs/projects/commands/linux.py
+++ .yamato/ruamel/jobs/projects/commands/linux.py
@@ -37,5 +37,5 @@ def cmd_standalone(project_folder, platform, api, test_platform, editor, build_c
 
 
 def cmd_standalone_build(project_folder, platform, api, test_platform, editor, build_config, color_space):
-    utr_args = extract_flags(test_platform["extra_utr_flags"], platform["name"], api["name"])
+    utr_args = extract_flags(test_platform["extra_utr_flags_build"], platform["name"], api["name"])
     return _cmd_base(project_folder, platform, utr_args, editor)
