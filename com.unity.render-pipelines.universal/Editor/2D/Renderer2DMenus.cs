152081b6d64deaaaad665a624379031c85166f16
Yuanxing Cai <yuanxing@unity3d.com>
2020-10-13 11:51:08 +0800

    Revert "Changed the default value of light RT scale to 1.0 so that we don't compromise the visual quality of existing projects."
    This reverts commit 847dda2aa13503fad4a6869c4abc8551833c0920.

--- com.unity.render-pipelines.universal/Editor/2D/Renderer2DMenus.cs
+++ com.unity.render-pipelines.universal/Editor/2D/Renderer2DMenus.cs
@@ -26,7 +26,6 @@ namespace UnityEditor.Experimental.Rendering.Universal
             public override void Action(int instanceId, string pathName, string resourceFile)
             {
                 var instance = CreateInstance<Renderer2DData>();
-                instance.lightRenderTextureScale = 0.5f;
                 AssetDatabase.CreateAsset(instance, pathName);
                 Selection.activeObject = instance;
 
