d50995731b308ef419cb52dd1d190d6d2f042338
Elizabeth LeGros <elizabeth.legros@unity3d.com>
2020-10-16 14:57:53 -0500

    Deprecate view direction and add function call with conditional normalization to new version

--- com.unity.shadergraph/Editor/Data/Nodes/Input/Geometry/ViewDirectionNode.cs
+++ com.unity.shadergraph/Editor/Data/Nodes/Input/Geometry/ViewDirectionNode.cs
@@ -6,10 +6,11 @@ namespace UnityEditor.ShaderGraph
 {
     [FormerName("UnityEngine.MaterialGraph.ViewDirectionNode")]
     [Title("Input", "Geometry", "View Direction")]
-    class ViewDirectionNode : GeometryNode, IMayRequireViewDirection
+    class ViewDirectionNode : GeometryNode, IMayRequireViewDirection, IGeneratesFunction
     {
         private const int kOutputSlotId = 0;
         public const string kOutputSlotName = "Out";
+        public override int latestVersion => 1;
 
         public ViewDirectionNode()
         {
@@ -31,12 +32,46 @@ namespace UnityEditor.ShaderGraph
 
         public override string GetVariableNameForSlot(int slotId)
         {
-            return string.Format("IN.{0}", space.ToVariableName(InterpolatorType.ViewDirection));
+            switch(m_SGVersion)
+            {
+                case 0:
+                    return string.Format("IN.{0}", space.ToVariableName(InterpolatorType.ViewDirection));
+                case 1:
+                    return string.Format("{0}(IN.{1})",functionName, space.ToVariableName(InterpolatorType.ViewDirection));
+                default:
+                    return null;
+            }
         }
 
         public NeededCoordinateSpace RequiresViewDirection(ShaderStageCapability stageCapability)
         {
             return space.ToNeededCoordinateSpace();
+        }
+
+        private readonly string functionName = "NormalizeIfURP";
+
+        public void GenerateNodeFunction(FunctionRegistry registry, GenerationMode generationMode)
+        {
+            switch (sgVersion)
+            {
+                case 0:
+                    break;
+                case 1:
+                default:
+                    registry.ProvideFunction(functionName, s =>
+                    {
+                        s.AppendLine("$precision3 {0} ($precision3 In)", functionName);
+                        using (s.BlockScope())
+                        {
+                            s.AppendLine("#ifdef UNIVERSAL_PIPELINE_CORE_INCLUDED");
+                            s.AppendLine("\treturn normalize(In);");
+                            s.AppendLine("#else");
+                            s.AppendLine("\treturn In;");
+                            s.AppendLine("#endif");
+                        }
+                    });
+                    break;
+            }
         }
     }
 }
