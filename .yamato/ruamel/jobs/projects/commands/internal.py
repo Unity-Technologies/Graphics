890b14cc428ae61dd5c3a510b5c8d5518ae7b4c1
Sophia <16596228+sophiaaar@users.noreply.github.com>
2020-10-16 15:46:33 +0200

    [URP][Performance] Enable subset of perf tests in nightly jobs (#2157)
    Too many commits to list. see the PR for full changes
    
    Co-authored-by: Liis Kivistik <liis.kivistik@unity3d.com>
    Co-authored-by: Sean Stolberg <seans@unity3d.com>

--- .yamato/ruamel/jobs/projects/commands/internal.py
+++ .yamato/ruamel/jobs/projects/commands/internal.py
@@ -14,26 +14,31 @@ def _cmd_base(project_folder, platform, utr_flags, editor):
         f'cd {TEST_PROJECTS_DIR}/URP-Update-testing/{project_folder} && utr {" ".join(utr_flags)}'
     ]
 
-def cmd_editmode(project_folder, platform, api, test_platform, editor):
-    utr_args = utr_editmode_flags()
+def cmd_editmode(project_folder, platform, api, test_platform, editor, build_config, color_space):
+    scripting_backend = build_config["scripting_backend"]
+    api_level = build_config["api_level"]
+    utr_args = utr_editmode_flags(scripting_backend=f'{scripting_backend}', api_level=f'{api_level}', color_space=f'{color_space}')
     utr_args.extend(test_platform["extra_utr_flags"])
+    utr_args.extend(platform["extra_utr_flags"])
     if api["name"] != "":
         utr_args.append(f'--extra-editor-arg="{api["cmd"]}"')
 
     return  _cmd_base(project_folder, platform, utr_args, editor)
 
 
-def cmd_playmode(project_folder, platform, api, test_platform, editor):
+def cmd_playmode(project_folder, platform, api, test_platform, editor, build_config, color_space):
     utr_args = utr_playmode_flags()
     utr_args.extend(test_platform["extra_utr_flags"])
+    utr_args.extend(platform["extra_utr_flags"])
     if api["name"] != "":
         utr_args.append(f'--extra-editor-arg="{api["cmd"]}"')
 
     return  _cmd_base(project_folder, platform, utr_args, editor)
 
-def cmd_standalone(project_folder, platform, api, test_platform, editor):
+def cmd_standalone(project_folder, platform, api, test_platform, editor, build_config, color_space):
     utr_args = utr_standalone_split_flags("Windows64")
     utr_args.extend(test_platform["extra_utr_flags"])
+    utr_args.extend(platform["extra_utr_flags"])
     utr_args.append(f'--timeout={get_timeout(test_platform, "Win")}')
 
 
@@ -43,9 +48,10 @@ def cmd_standalone(project_folder, platform, api, test_platform, editor):
     return base
 
 
-def cmd_standalone_build(project_folder, platform, api, test_platform, editor):
-    utr_args = utr_standalone_build_flags("Windows64")
+def cmd_standalone_build(project_folder, platform, api, test_platform, editor, build_config, color_space):
+    utr_args = utr_standalone_build_flags("Windows64", graphics_api=api["name"])
     utr_args.extend(test_platform["extra_utr_flags_build"])
+    utr_args.extend(platform["extra_utr_flags_build"])
     utr_args.extend(['--extra-editor-arg="-executemethod"', f'--extra-editor-arg="CustomBuild.BuildWindows{api["name"]}Linear"'])
     utr_args.append(f'--timeout={get_timeout(test_platform, "Win", build=True)}')
 
