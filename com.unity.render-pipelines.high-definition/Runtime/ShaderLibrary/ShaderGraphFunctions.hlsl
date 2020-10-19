cc18c99e18f1d9d29a95b4a893f9736ead0026d4
Sebastien Lagarde <sebastien@unity3d.com>
2020-10-19 20:01:40 +0200

    Fix bug 1285640 - Gradient issue

--- com.unity.render-pipelines.high-definition/Runtime/ShaderLibrary/ShaderGraphFunctions.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/ShaderLibrary/ShaderGraphFunctions.hlsl
@@ -1,10 +1,8 @@
 #ifndef UNITY_GRAPHFUNCTIONS_HD_INCLUDED
 #define UNITY_GRAPHFUNCTIONS_HD_INCLUDED
 
-#define SHADERGRAPH_SAMPLE_SCENE_DEPTH(uv) shadergraph_HDSampleSceneDepth(uv)
-#define SHADERGRAPH_SAMPLE_SCENE_COLOR(uv) shadergraph_HDSampleSceneColor(uv)
-#define SHADERGRAPH_BAKED_GI(positionWS, normalWS, uvStaticLightmap, uvDynamicLightmap, applyScaling) shadergraph_HDBakedGI(positionWS, normalWS, uvStaticLightmap, uvDynamicLightmap, applyScaling)
-#define SHADERGRAPH_OBJECT_POSITION GetAbsolutePositionWS(UNITY_MATRIX_M._m03_m13_m23)
+// Due to order of includes (Gradient struct need to be define before the declaration of $splice(GraphProperties))
+// And HDRP require that Material.hlsl and BuiltInGI are after it, we have two files to defines shader graph functions, one header and one where we setup HDRP functions
 
 float shadergraph_HDSampleSceneDepth(float2 uv)
 {
@@ -28,9 +26,5 @@ float3 shadergraph_HDBakedGI(float3 positionWS, float3 normalWS, float2 uvStatic
     float3 positionRWS = GetCameraRelativePositionWS(positionWS);
     return SampleBakedGI(positionRWS, normalWS, uvStaticLightmap, uvDynamicLightmap);
 }
-
-// Always include Shader Graph version
-// Always include last to avoid double macros
-#include "Packages/com.unity.shadergraph/ShaderGraphLibrary/Functions.hlsl" 
 
 #endif // UNITY_GRAPHFUNCTIONS_HD_INCLUDED
