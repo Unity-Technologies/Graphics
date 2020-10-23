5f489fdbd7c5b69b80bdffe0fe5687c4f21c3bbe
Antoine Lelievre <antoinel@unity3d.com>
2020-10-23 12:19:04 +0200

    Expose HDRP shader tag IDs and per object config (#2292)
    * Expose shader pass names API
    
    * Updated changelog
    
    * exposed per object data in HDUtils
    
    * changed constants to accessors
    
    * Moved getters to static
    
    Co-authored-by: JulienIgnace-Unity <julien@unity3d.com>

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/RenderPass/DrawRenderersCustomPass.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/RenderPass/DrawRenderersCustomPass.cs
@@ -154,7 +154,7 @@ namespace UnityEngine.Rendering.HighDefinition
                 stencilState = new StencilState(false),
             };
 
-            PerObjectData renderConfig = ctx.hdCamera.frameSettings.IsEnabled(FrameSettingsField.Shadowmask) ? HDUtils.k_RendererConfigurationBakedLightingWithShadowMask : HDUtils.k_RendererConfigurationBakedLighting;
+            PerObjectData renderConfig = ctx.hdCamera.frameSettings.IsEnabled(FrameSettingsField.Shadowmask) ? HDUtils.GetBakedLightingWithShadowMaskRenderConfig() : HDUtils.GetBakedLightingRenderConfig();
 
             var result = new RendererListDesc(shaderPasses, ctx.cullingResults, ctx.hdCamera.camera)
             {
