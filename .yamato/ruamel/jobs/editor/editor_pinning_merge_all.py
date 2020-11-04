e3c9506773353aff73b39690fca259a26779e033
Liis Kivistik <liis.kivistik@unity3d.com>
2020-11-04 10:45:52 +0100

    [yamato] Let jobs use revisions straight from the metafile (#2511)

--- .yamato/ruamel/jobs/editor/editor_pinning_merge_all.py
+++ .yamato/ruamel/jobs/editor/editor_pinning_merge_all.py
@@ -22,26 +22,7 @@ class Editor_PinningMergeAllJob():
 
             dependencies.append(f'{editor_pinning_filepath()}#{editor_job_id_merge_revisions(editor["name"], abv)}')
         
-        commands = [
-            f'sudo pip3 install pipenv --index-url https://artifactory.prd.it.unity3d.com/artifactory/api/pypi/pypi/simple',# Remove when the image has this preinstalled.
-            f'python3 -m pipenv install --dev', 
-            f'curl -L https://artifactory.prd.it.unity3d.com/artifactory/api/gpg/key/public | sudo apt-key add -',
-            f'sudo sh -c "echo \'deb https://artifactory.prd.it.unity3d.com/artifactory/unity-apt-local bionic main\' > /etc/apt/sources.list.d/unity.list"',
-            f'sudo apt-get update',
-            pss(f'''
-            if [[ "$GIT_BRANCH" != "{target_branch_editor_ci }" ]]; then
-                echo "Should run on '{ target_branch_editor_ci }' but is running on '$GIT_BRANCH'"
-                exit 1
-            fi'''),# This should never run on anything other than stable. If you try it then it will fail
-            f'git config --global user.name "noreply@unity3d.com"', # TODO
-            f'git config --global user.email "noreply@unity3d.com"', # TODO
-            f'git checkout {target_branch}',
-            f'git pull',
-            f'pipenv run python3 .yamato/ruamel/build.py',
-            f'git add .yamato/*.yml',
-            f'git commit -m "[CI] Updated .ymls to new revision" --allow-empty',
-            f'git push'
-        ]
+        
 
         # construct job
         job = YMLJob()
@@ -52,8 +33,8 @@ class Editor_PinningMergeAllJob():
         else:
             job.set_name(f'Merge all [no ABV] [no CI]')
         
-        job.set_agent(agent)
+        #job.set_agent(agent)
         job.add_var_custom('CI', True)
         job.add_dependencies(dependencies)
-        job.add_commands(commands)
+        #job.add_commands(commands)
         return job
