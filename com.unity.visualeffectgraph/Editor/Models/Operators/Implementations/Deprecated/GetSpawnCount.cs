79717307c1109c308c27e1263b600836029b5bf0
Paul Demeulenaere <pauld@unity3d.com>
2020-11-05 11:11:43 +0100

    Sanitize GetSpawnCount using VFXAttributeParameter & VFXInlineOperator

--- com.unity.visualeffectgraph/Editor/Models/Operators/Implementations/Deprecated/GetSpawnCount.cs
+++ com.unity.visualeffectgraph/Editor/Models/Operators/Implementations/Deprecated/GetSpawnCount.cs
@@ -18,4 +18,25 @@ class GetSpawnCount : VFXOperator
     {
         return new VFXExpression[] { new VFXExpressionCastFloatToUint(new VFXAttributeExpression(VFXAttribute.SpawnCount, VFXAttributeLocation.Source))  };
     }
+
+    public override void Sanitize(int version)
+    {
+        var sourceSpawnCount = ScriptableObject.CreateInstance<VFXAttributeParameter>();
+        sourceSpawnCount.SetSettingValue("location", VFXAttributeLocation.Source);
+        sourceSpawnCount.SetSettingValue("attribute", VFXAttribute.SpawnCount.name);
+        sourceSpawnCount.position = position - new Vector2(300, 100);
+
+        var parent = GetParent();
+        if (parent)
+            parent.AddChild(sourceSpawnCount);
+
+        var inlineUInt = ScriptableObject.CreateInstance<VFXInlineOperator>();
+        inlineUInt.SetSettingValue("m_Type", (SerializableType)typeof(uint));
+        sourceSpawnCount.outputSlots[0].Link(inlineUInt.inputSlots[0]);
+
+        VFXSlot.CopyLinksAndValue(inlineUInt.outputSlots[0], outputSlots[0], true);
+        ReplaceModel(inlineUInt, this);
+
+        base.Sanitize(version);
+    }
 }
