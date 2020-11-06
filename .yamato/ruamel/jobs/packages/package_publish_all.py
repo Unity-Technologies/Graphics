c1bea4cfcf59db3bb61a26acad738f700527394f
Liis Kivistik <liis.kivistik@unity3d.com>
2020-11-06 11:30:53 +0100

    [Yamato] take clean copy of yml python from master (#2543)

---
+++ .yamato/ruamel/jobs/packages/package_publish_all.py
@@ -0,0 +1,23 @@
+from ruamel.yaml.scalarstring import DoubleQuotedScalarString as dss
+from ..shared.namer import packages_filepath, package_job_id_publish, package_job_id_publish_all
+from ..shared.yml_job import YMLJob
+
+class Package_PublishAllJob():
+    
+    def __init__(self, packages, target_branch, agent):
+        self.job_id = package_job_id_publish_all()
+        self.yml = self.get_job_definition(packages, target_branch, agent).get_yml()
+
+
+    def get_job_definition(self, packages, target_branch, agent):
+
+        # construct job
+        job = YMLJob()
+        job.set_name(f'Publish all packages [package context][manual]')
+        job.add_dependencies([f'{packages_filepath()}#{package_job_id_publish(package["id"])}' for package in packages])
+        return job
+
+
+    
+    
+    
--- .yamato/ruamel/jobs/packages/yml_project.py
+++ .yamato/ruamel/jobs/packages/yml_project.py
@@ -3,6 +3,7 @@ from .project_pack import Project_PackJob
 from .project_publish import Project_PublishJob
 from .project_test import Project_TestJob
 from .project_test_min_editor import Project_TestMinEditorJob
+from .project_publish_all import Project_PublishAllJob
 from .project_publish_all_tag import Project_PublishAllTagJob
 from .project_test_all import Project_AllPackageCiJob
 from .project_publish_dry import Project_PublishJob_DryRun
@@ -33,6 +34,9 @@ def create_projectcontext_ymls(metafile):
     for editor in metafile['editors']:
         job = Project_AllPackageCiJob(metafile["packages"], metafile["agent_publish"], metafile["platforms"], metafile["target_branch"], editor)
         yml[job.job_id] = job.yml
+
+    # job = Project_PublishAllJob(metafile["packages"], metafile["target_branch"], metafile["agent_publish_all"])
+    # yml[job.job_id] = job.yml
 
     job = Project_PublishAllTagJob(metafile["packages"], metafile["target_branch"], metafile["agent_publish_all"])
     yml[job.job_id] = job.yml
