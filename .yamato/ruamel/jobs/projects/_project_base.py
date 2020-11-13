3d6ce0bcbbf8667eb620e73f0b7546e12fdeff1e
Liis Kivistik <liis.kivistik@unity3d.com>
2020-11-13 15:06:44 +0100

    add variable for flags

--- .yamato/ruamel/jobs/projects/_project_base.py
+++ .yamato/ruamel/jobs/projects/_project_base.py
@@ -42,4 +42,6 @@ def _job(project, test_platform_name, editor, platform, api, cmd, build_config, 
         job.add_var_custom('BOAT_ATTACK_BRANCH', 'master')
         job.add_var_custom('BOAT_ATTACK_REVISION', 'head')
 
+    job.add_var_custom('UTR_FLAGS', '')
+
     return job
