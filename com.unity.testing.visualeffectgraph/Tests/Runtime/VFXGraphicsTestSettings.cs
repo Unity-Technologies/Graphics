9f73595258765f6217356a8fb550f7b83bc98533
Paul Demeulenaere <pauld@unity3d.com>
2020-11-17 09:27:08 +0100

    [8.x.x] [VFX] Fix graphic test when individually launched (backport) (#1142)
    * Isolate fix from https://github.cds.internal.unity3d.com/unity/vfx-graph/commit/6d165da2073410024e3fc3dac4ff6f550207ab84
    
    * *Update reference images
    
    * *Add Missing Image reference

--- com.unity.testing.visualeffectgraph/Tests/Runtime/VFXGraphicsTestSettings.cs
+++ com.unity.testing.visualeffectgraph/Tests/Runtime/VFXGraphicsTestSettings.cs
@@ -5,8 +5,8 @@ namespace UnityEngine.VFX.Test
     class VFXGraphicsTestSettings : GraphicsTestSettings
     {
         static public readonly int defaultCaptureFrameRate = 20;
-        static public readonly float defaultFrequency = 1.0f / (float)defaultCaptureFrameRate;
-        static public readonly float defaultSimulateTime = 6.0f;
+        static public readonly float defaultCapturePeriod = 1.0f / (float)defaultCaptureFrameRate;
+        static public readonly float defaultSimulateTime = 6.0f - defaultCapturePeriod;
 
         public int captureFrameRate = defaultCaptureFrameRate;
         public float simulateTime = defaultSimulateTime;
