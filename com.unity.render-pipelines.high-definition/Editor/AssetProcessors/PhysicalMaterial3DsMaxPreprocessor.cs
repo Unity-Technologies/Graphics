eaaa3a20fe6051793aa71bee71fae65d7c653548
Elvar Örn Unnþórsson <ellioman@ellioman.com>
2020-11-18 08:54:23 +0100

    Fixing issues with AssetImporters includes

--- com.unity.render-pipelines.high-definition/Editor/AssetProcessors/PhysicalMaterial3DsMaxPreprocessor.cs
+++ com.unity.render-pipelines.high-definition/Editor/AssetProcessors/PhysicalMaterial3DsMaxPreprocessor.cs
@@ -1,11 +1,7 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
-#if UNITY_2020_2_OR_NEWER
 using UnityEditor.AssetImporters;
-#else
-using UnityEditor.Experimental.AssetImporters;
-#endif
 
 namespace UnityEditor.Rendering.HighDefinition
 {
