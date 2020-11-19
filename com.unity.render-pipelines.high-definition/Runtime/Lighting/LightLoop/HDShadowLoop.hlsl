6b83df2741d4acf4149457a06e03385df3827e90
Evgenii <evgenii@unity3d.com>
2020-11-19 15:15:27 -0800

    Fix HDShadowLoop.hlsl (ShadowLoopMin)

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/HDShadowLoop.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/HDShadowLoop.hlsl
@@ -18,6 +18,10 @@ void ShadowLoopMin(HDShadowContext shadowContext, PositionInputs posInput, float
     shadow = float3(1, 1, 1);
 #endif
 
+    // TODO: may want to make 'tile' wave-uniform.
+    uint tile = ComputeTileIndex(posInput.positionSS);
+    uint zBin = ComputeZBinIndex(posInput.linearDepth);
+
     // With XR single-pass and camera-relative: offset position to do lighting computations from the combined center view (original camera matrix).
     // This is required because there is only one list of lights generated on the CPU. Shadows are also generated once and shared between the instanced views.
     ApplyCameraRelativeXR(posInput.positionWS);
@@ -54,116 +58,59 @@ void ShadowLoopMin(HDShadowContext shadowContext, PositionInputs posInput, float
         }
     }
 
+    uint i; // Declare once to avoid the D3D11 compiler warning.
+
     if (featureFlags & LIGHTFEATUREFLAGS_PUNCTUAL)
     {
-        uint lightCount, lightStart;
+        i = 0;
 
-#ifndef LIGHTLOOP_DISABLE_TILE_AND_CLUSTER
-        GetCountAndStart(posInput, LIGHTCATEGORY_PUNCTUAL, lightStart, lightCount);
-#else   // LIGHTLOOP_DISABLE_TILE_AND_CLUSTER
-        lightCount = _PunctualLightCount;
-        lightStart = 0;
-#endif
-
-        bool fastPath = false;
-        uint lightStartLane0;
-        fastPath = IsFastPath(lightStart, lightStartLane0);
-
-        if (fastPath)
+        LightData lightData;
+        while (TryLoadPunctualLightData(i, tile, zBin, lightData))
         {
-            lightStart = lightStartLane0;
-        }
-
-        // Scalarized loop. All lights that are in a tile/cluster touched by any pixel in the wave are loaded (scalar load), only the one relevant to current thread/pixel are processed.
-        // For clarity, the following code will follow the convention: variables starting with s_ are meant to be wave uniform (meant for scalar register),
-        // v_ are variables that might have different value for each thread in the wave (meant for vector registers).
-        // This will perform more loads than it is supposed to, however, the benefits should offset the downside, especially given that light data accessed should be largely coherent.
-        // Note that the above is valid only if wave intriniscs are supported.
-        uint v_lightListOffset = 0;
-        uint v_lightIdx = lightStart;
-
-        while (v_lightListOffset < lightCount)
-        {
-            v_lightIdx = FetchIndex(lightStart, v_lightListOffset);
-            uint s_lightIdx = ScalarizeElementIndex(v_lightIdx, fastPath);
-            if (s_lightIdx == -1)
-                break;
-
-            LightData s_lightData = FetchLight(s_lightIdx);
-
-            // If current scalar and vector light index match, we process the light. The v_lightListOffset for current thread is increased.
-            // Note that the following should really be ==, however, since helper lanes are not considered by WaveActiveMin, such helper lanes could
-            // end up with a unique v_lightIdx value that is smaller than s_lightIdx hence being stuck in a loop. All the active lanes will not have this problem.
-            if (s_lightIdx >= v_lightIdx)
+            if (IsMatchingLightLayer(lightData.lightLayers, renderLayer) &&
+                lightData.shadowIndex >= 0 &&
+                lightData.shadowDimmer > 0)
             {
-                v_lightListOffset++;
-                if (IsMatchingLightLayer(s_lightData.lightLayers, renderLayer) &&
-                    s_lightData.shadowIndex >= 0 &&
-                    s_lightData.shadowDimmer > 0)
+                float shadowP;
+                float3 L;
+                float4 distances; // {d, d^2, 1/d, d_proj}
+                GetPunctualLightVectors(posInput.positionWS, lightData, L, distances);
+                float distToLight = (lightData.lightType == GPULIGHTTYPE_PROJECTOR_BOX) ? distances.w : distances.x;
+                float lightRadSqr = lightData.size.x;
+                if (distances.x < lightData.range &&
+                    PunctualLightAttenuation(distances, lightData.rangeAttenuationScale, lightData.rangeAttenuationBias,
+                                                        lightData.angleScale,            lightData.angleOffset) > 0.0 &&
+                    L.y > 0.0)
                 {
-                    float shadowP;
-                    float3 L;
-                    float4 distances; // {d, d^2, 1/d, d_proj}
-                    GetPunctualLightVectors(posInput.positionWS, s_lightData, L, distances);
-                    float distToLight = (s_lightData.lightType == GPULIGHTTYPE_PROJECTOR_BOX) ? distances.w : distances.x;
-                    float lightRadSqr = s_lightData.size.x;
-                    if (distances.x < s_lightData.range &&
-                        PunctualLightAttenuation(distances, s_lightData.rangeAttenuationScale, s_lightData.rangeAttenuationBias,
-                                                            s_lightData.angleScale,            s_lightData.angleOffset) > 0.0 &&
-                        L.y > 0.0)
-                    {
-                        shadowP = GetPunctualShadowAttenuation(shadowContext, posInput.positionSS, posInput.positionWS, normalWS, s_lightData.shadowIndex, L, distances.x, s_lightData.lightType == GPULIGHTTYPE_POINT, s_lightData.lightType != GPULIGHTTYPE_PROJECTOR_BOX);
-                        shadowP = s_lightData.nonLightMappedOnly ? min(1.0f, shadowP) : shadowP;
-                        shadowP = lerp(1.0f, shadowP, s_lightData.shadowDimmer);
+                    shadowP = GetPunctualShadowAttenuation(shadowContext, posInput.positionSS, posInput.positionWS, normalWS, lightData.shadowIndex, L, distances.x, lightData.lightType == GPULIGHTTYPE_POINT, lightData.lightType != GPULIGHTTYPE_PROJECTOR_BOX);
+                    shadowP = lightData.nonLightMappedOnly ? min(1.0f, shadowP) : shadowP;
+                    shadowP = lerp(1.0f, shadowP, lightData.shadowDimmer);
 
 #ifdef SHADOW_LOOP_MULTIPLY
-                        shadow *= lerp(s_lightData.shadowTint, float3(1, 1, 1), shadowP);
+                    shadow *= lerp(lightData.shadowTint, float3(1, 1, 1), shadowP);
 #elif defined(SHADOW_LOOP_AVERAGE)
-                        shadow += lerp(s_lightData.shadowTint, float3(1, 1, 1), shadowP);
+                    shadow += lerp(lightData.shadowTint, float3(1, 1, 1), shadowP);
 #else
-                        shadow = min(shadow, shadowP.xxx);
+                    shadow = min(shadow, shadowP.xxx);
 #endif
-                        shadowCount += 1.0f;
-                        weight      += 1.0f - shadowP;
-                    }
+                    shadowCount += 1.0f;
+                    weight      += 1.0f - shadowP;
                 }
             }
+
+            i++;
         }
     }
 
     if (featureFlags & LIGHTFEATUREFLAGS_AREA)
     {
-        uint lightCount, lightStart;
+        i = 0;
 
-    #ifndef LIGHTLOOP_DISABLE_TILE_AND_CLUSTER
-        GetCountAndStart(posInput, LIGHTCATEGORY_AREA, lightStart, lightCount);
-    #else
-        lightCount = _AreaLightCount;
-        lightStart = _PunctualLightCount;
-    #endif
-
-        // COMPILER BEHAVIOR WARNING!
-        // If rectangle lights are before line lights, the compiler will duplicate light matrices in VGPR because they are used differently between the two types of lights.
-        // By keeping line lights first we avoid this behavior and save substantial register pressure.
-        // TODO: This is based on the current Lit.shader and can be different for any other way of implementing area lights, how to be generic and ensure performance ?
-
-        uint i;
-        if (lightCount > 0)
+        LightData lightData;
+        while (TryLoadAreaLightData(i, tile, zBin, lightData))
         {
-            i = 0;
-
-            uint      last      = lightCount - 1;
-            LightData lightData = FetchLight(lightStart, i);
-
-            while (i <= last && lightData.lightType == GPULIGHTTYPE_TUBE)
+            if (lightData.lightType == GPULIGHTTYPE_RECTANGLE)
             {
-                lightData = FetchLight(lightStart, min(++i, last));
-            }
-
-            while (i <= last) // GPULIGHTTYPE_RECTANGLE
-            {
-                lightData.lightType = GPULIGHTTYPE_RECTANGLE; // Enforce constant propagation
-
                 if (IsMatchingLightLayer(lightData.lightLayers, renderLayer))
                 {
                     float3 L;
@@ -208,9 +155,13 @@ void ShadowLoopMin(HDShadowContext shadowContext, PositionInputs posInput, float
                         weight      += 1.0f - shadowA;
                     }
                 }
-
-                lightData = FetchLight(lightStart, min(++i, last));
             }
+            else // GPULIGHTTYPE_TUBE
+            {
+                /* ??? */
+            }
+
+            i++;
         }
     }
 #ifdef SHADOW_LOOP_MULTIPLY
