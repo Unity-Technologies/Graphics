8cd49222cf4caed0b1f91790e6358f3a4fd1d8f3
sebastienlagarde <sebastien@unity3d.com>
2020-11-23 15:28:55 +0100

    Merge Hd/bugfix - Set of bugfix for 10.3 (#2681)
    * Update 1900_AlphaTestSG.renderTexture (#2608)
    
    * update GBuffer description.
    
    * Added a warning when trying to bake with static lighting being in an invalid state. (#2597)
    
    * Added a warning when trying to bake with static lighting being in an invalid state.
    
    * Update changelog.
    
    * [HDRP] Fix runtime debug UI showing wrong entry (#2595)
    
    * Redraw debug before re-registring the debugs
    
    * changelog
    
    * Fixed multi-editing with new light intensity slider (#2600)
    
    * Fixed multi-editing with new light intensity slider
    
    * Only update floatValue when changed
    
    * Fixed useless editor repaint when using lod bias (#2601)
    
    Co-authored-by: sebastienlagarde <sebastien@unity3d.com>
    
    * Fix issue with density volume gizmo editing (#2603)
    
    * Fix
    
    * changelog
    
    Co-authored-by: sebastienlagarde <sebastien@unity3d.com>
    
    * [HDRP][Compositor] Fix issues with image layers (#2606)
    
    * Empty image layers in the graphics compositor don't render black anymore
    
    * Improve camera management in compositor
    
    * Revert "Improve camera management in compositor"
    
    This reverts commit cc3971dcc2d0fce06823d50befd673ee37ac7bb8.
    
    * Update changelog
    
    * For image layers that are first, still clear to black if no texture is attached
    
    Co-authored-by: sebastienlagarde <sebastien@unity3d.com>
    
    * Fixed issue when decal projector material is none (#2615)
    
    * Fixed angle fading when rotating decal projector (#2618)
    
    Co-authored-by: sebastienlagarde <sebastien@unity3d.com>
    
    * Fix issue with gameview repaint in the graphics compositor (#2617)
    
    Co-authored-by: sebastienlagarde <sebastien@unity3d.com>
    
    * Remove cyanish color shifting during sharpening in YCoCg in TAA (#2627)
    
    * Do sharpening in RGB space.
    
    * Fix alpha case
    
    * Oops
    
    * Ref update aside OSX
    
    * OSX refs
    
    * More refs
    
    * Missing refs.
    
    * Did I get the wrong ref?
    
    * Fixed the sampling of the normal buffer in the the forward transparent pass. (#2643)
    
    * Fixed the sampling of the normal buffer in the the forward transparent pass.
    
    * Update changelog
    
    * Update CHANGELOG.md
    
    Co-authored-by: Sebastien Lagarde <sebastien@unity3d.com>
    
    * Fix custom pass reorder issue (#2609)
    
    * Fix custom pass reorder issue
    
    * Fix bad comment
    
    * Refactor
    
    * Fix bloom prefiltering tooltip (#2715)
    
    * update reference screenshot for test 1900_AlphaTest_SG
    
    * Fix error when loading new scene (#2713)
    
    * Fix error when loading new scene
    
    * Update CHANGELOG.md
    
    * AxF Fix missing alpha blend state properties and update stencil prop (#2705)
    
    * AxF: Fix missing alpha blend state properties and update stencil properties.
    
    * Add changelog entry.
    
    Co-authored-by: sebastienlagarde <sebastien@unity3d.com>
    
    * Fix custom pass normal and template (#2716)
    
    * Fix custom pass template not compiling
    
    * Fix custom pass normal buffer not bound
    
    * Updated changelog
    
    * Update CHANGELOG.md
    
    Co-authored-by: Sebastien Lagarde <sebastien@unity3d.com>
    
    * [HDRP][Compositor] Fix edge cases when cameras are missing or deleted  (#2700)
    
    * Fix warning spam related to cameras in compositor
    
    * Fix null reference in AOVs
    
    * Fix null reference when disabling camera in the compositor
    
    * Reset target texture when disabling the compositor
    
    * Update changelog + fix warning when output camera is deleted
    
    * Fix issue when adding image layer without any cameras in the scene
    
    * Fix error when disabling layer with no camera
    
    Co-authored-by: sebastienlagarde <sebastien@unity3d.com>
    
    * Update CHANGELOG.md
    
    Co-authored-by: JulienIgnace-Unity <julien@unity3d.com>
    Co-authored-by: FrancescoC-unity <43168857+FrancescoC-unity@users.noreply.github.com>
    Co-authored-by: Adrien de Tocqueville <adrien.tocqueville@unity3d.com>
    Co-authored-by: Pavlos Mavridis <pavlos.mavridis@unity3d.com>
    Co-authored-by: Antoine Lelievre <antoinel@unity3d.com>
    Co-authored-by: slunity <37302815+slunity@users.noreply.github.com>

--- com.unity.render-pipelines.high-definition/Editor/RenderPipeline/HDRenderPipelineUI.Skin.cs
+++ com.unity.render-pipelines.high-definition/Editor/RenderPipeline/HDRenderPipelineUI.Skin.cs
@@ -58,7 +58,7 @@ namespace UnityEditor.Rendering.HighDefinition
             public static readonly GUIContent maxRadiusQuality = EditorGUIUtility.TrTextContent("Max Radius");
             public static readonly GUIContent sampleCountQuality = EditorGUIUtility.TrTextContent("Sample Count");
             public static readonly GUIContent resolutionQuality = EditorGUIUtility.TrTextContent("Resolution");
-            public static readonly GUIContent highQualityPrefiltering = EditorGUIUtility.TrTextContent("High Quality Filtering");
+            public static readonly GUIContent highQualityPrefiltering = EditorGUIUtility.TrTextContent("High Quality Prefiltering");
             public static readonly GUIContent highQualityFiltering = EditorGUIUtility.TrTextContent("High Quality Filtering");
             public static readonly GUIContent dofPhysicallyBased = EditorGUIUtility.TrTextContent("Physically Based");
             public static readonly GUIContent maxSamplesQuality = EditorGUIUtility.TrTextContent("Max Samples");
