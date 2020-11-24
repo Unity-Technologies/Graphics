b8db6418c352d56ab80a4dffcbedde3fdaf6d524
Antoine Lelievre <antoinel@unity3d.com>
2020-11-24 18:51:34 +0100

    Expose material inspector API + Documentation [2021.1] (#2073)
    * Started to move all material UI classes to public
    
    * Moved all interesting UI block to public and added a common Unlit material UI class
    
    * Renamed decal UI and moved to public
    
    * Exposed more API
    
    * Fix documentation
    
    * Fixed documentation
    
    * Fixed exception with required material property loading
    
    * Fixed all the docs
    
    * Fix compilation
    
    * Code formating
    
    * Reverted unwanted changes
    
    * Fix last merge with master

--- com.unity.render-pipelines.high-definition/Editor/Material/Unlit/ShaderGraph/UnlitShaderGraphGUI.cs
+++ com.unity.render-pipelines.high-definition/Editor/Material/Unlit/ShaderGraph/UnlitShaderGraphGUI.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
 using UnityEngine.Rendering;
 
 // Include material common properties names
@@ -7,18 +8,16 @@ using static UnityEngine.Rendering.HighDefinition.HDMaterialProperties;
 namespace UnityEditor.Rendering.HighDefinition
 {
     /// <summary>
-    /// Common GUI for Lit ShaderGraphs.
+    /// The UI block that represents common properties for unlit Shader Graphs.
     /// </summary>
-    public class LightingShaderGraphGUI : HDShaderGUI
+    public class UnlitShaderGraphGUI : HDShaderGUI
     {
-        // For surface option shader graph we only want all unlit features but alpha clip and back then front rendering
-        const SurfaceOptionUIBlock.Features   surfaceOptionFeatures = SurfaceOptionUIBlock.Features.Lit
-            | SurfaceOptionUIBlock.Features.ShowDepthOffsetOnly;
+        const SurfaceOptionUIBlock.Features surfaceOptionFeatures = SurfaceOptionUIBlock.Features.Unlit;
 
         MaterialUIBlockList m_UIBlocks = new MaterialUIBlockList
         {
             new SurfaceOptionUIBlock(MaterialUIBlock.ExpandableBit.Base, features: surfaceOptionFeatures),
-            new ShaderGraphUIBlock(MaterialUIBlock.ExpandableBit.ShaderGraph),
+            new ShaderGraphUIBlock(MaterialUIBlock.ExpandableBit.ShaderGraph, ShaderGraphUIBlock.Features.Unlit),
             new AdvancedOptionsUIBlock(MaterialUIBlock.ExpandableBit.Advance, ~AdvancedOptionsUIBlock.Features.SpecularOcclusion)
         };
 
@@ -40,32 +39,19 @@ namespace UnityEditor.Rendering.HighDefinition
         }
 
         /// <summary>
-        /// Sets up the keywords and passes for a Lit Shader Graph material.
+        /// Sets up the keywords and passes for the Unlit Shader Graph material you pass in.
         /// </summary>
         /// <param name="material">The target material.</param>
-        public static void SetupLightingKeywordsAndPass(Material material)
+        public static void SetupUnlitKeywordsAndPass(Material material)
         {
             SynchronizeShaderGraphProperties(material);
-
-            BaseLitGUI.SetupBaseLitKeywords(material);
-            BaseLitGUI.SetupBaseLitMaterialPass(material);
-
-            bool receiveSSR = false;
-            if (material.GetSurfaceType() == SurfaceType.Transparent)
-                receiveSSR = material.HasProperty(kReceivesSSRTransparent) ? material.GetFloat(kReceivesSSRTransparent) != 0 : false;
-            else
-                receiveSSR = material.HasProperty(kReceivesSSR) ? material.GetFloat(kReceivesSSR) != 0 : false;
-            bool useSplitLighting = material.HasProperty(kUseSplitLighting) ? material.GetInt(kUseSplitLighting) != 0 : false;
-            BaseLitGUI.SetupStencil(material, receiveSSR, useSplitLighting);
-
-            if (material.HasProperty(kAddPrecomputedVelocity))
-                CoreUtils.SetKeyword(material, "_ADD_PRECOMPUTED_VELOCITY", material.GetInt(kAddPrecomputedVelocity) != 0);
+            UnlitGUI.SetupUnlitKeywordsAndPass(material);
         }
 
         /// <summary>
         /// Sets up the keywords and passes for the current selected material.
         /// </summary>
         /// <param name="material">The selected material.</param>
-        protected override void SetupMaterialKeywordsAndPass(Material material) => SetupLightingKeywordsAndPass(material);
+        protected override void SetupMaterialKeywordsAndPass(Material material) => SetupUnlitKeywordsAndPass(material);
     }
 }
