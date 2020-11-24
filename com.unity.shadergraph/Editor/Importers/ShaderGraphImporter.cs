a7d51e88923541c4ac844545d9c737c6f06bbedf
pastasfuture <pastasfuture@gmail.com>
2020-11-24 11:53:08 -0800

    Bumping shader graph asset version for new dissolve occluder property type change to HybridInstanced in HDRP package

--- com.unity.shadergraph/Editor/Importers/ShaderGraphImporter.cs
+++ com.unity.shadergraph/Editor/Importers/ShaderGraphImporter.cs
@@ -25,14 +25,15 @@ namespace UnityEditor.ShaderGraph
     // because the shader graph codegen is different for V2.
     // This ifdef can be removed once V2 is the only option.
     // custom-begin:
+    // + 1 Bump ShaderImporter version after _DissolveOnOcclusionOpacity parameter needed HLSLDeclaration.HybridPerInstance declaration
     // + 1 Bump ShaderImporter version after Return positionDS from ObjectSpacePosition in Decal Shader Graphs
     // + 1 Bump ShaderImporter version after stripping LIGHTMAP_ON variants from shadergraph codegen in 9.x.x/custom/cherry-pick.
     // + 1 Bump ShaderImporter version after _DissolveOnOcclusionOpacity parameter needed HLSLDeclaration.UnityPerMaterial declaration, changing codegen in 10.2.0.
     // [ScriptedImporter(111, Extension, -902)]
-    [ScriptedImporter(114, Extension, -902)]
+    [ScriptedImporter(115, Extension, -902)]
 #else
     // [ScriptedImporter(43, Extension, -902)]
-    [ScriptedImporter(46, Extension, -902)]
+    [ScriptedImporter(47, Extension, -902)]
     // custom-end
 #endif
 
