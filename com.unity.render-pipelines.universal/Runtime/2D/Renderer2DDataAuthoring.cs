f4cb9504db0a8e2a95c9a520aca771b8358a7360
Yuanxing Cai <yuanxing@unity3d.com>
2020-11-24 14:27:03 +0800

    New default blend styles.

--- com.unity.render-pipelines.universal/Runtime/2D/Renderer2DDataAuthoring.cs
+++ com.unity.render-pipelines.universal/Runtime/2D/Renderer2DDataAuthoring.cs
@@ -71,11 +71,19 @@ namespace UnityEngine.Experimental.Rendering.Universal
 
             m_LightBlendStyles = new Light2DBlendStyle[4];
 
-            for (int i = 0; i < m_LightBlendStyles.Length; ++i)
-            {
-                m_LightBlendStyles[i].name = "Blend Style " + i;
-                m_LightBlendStyles[i].blendMode = Light2DBlendStyle.BlendMode.Multiply;
-            }
+            m_LightBlendStyles[0].name = "Multiply";
+            m_LightBlendStyles[0].blendMode = Light2DBlendStyle.BlendMode.Multiply;
+
+            m_LightBlendStyles[1].name = "Additive";
+            m_LightBlendStyles[1].blendMode = Light2DBlendStyle.BlendMode.Additive;
+
+            m_LightBlendStyles[2].name = "Multiply with Mask";
+            m_LightBlendStyles[2].blendMode = Light2DBlendStyle.BlendMode.Multiply;
+            m_LightBlendStyles[2].maskTextureChannel = Light2DBlendStyle.TextureChannel.R;
+
+            m_LightBlendStyles[3].name = "Additive with Mask";
+            m_LightBlendStyles[3].blendMode = Light2DBlendStyle.BlendMode.Additive;
+            m_LightBlendStyles[3].maskTextureChannel = Light2DBlendStyle.TextureChannel.R;
         }
 
 #endif
