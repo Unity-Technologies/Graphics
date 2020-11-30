91422f8332870e29007f0a48283cebdfda24877d
anisunity <42026998+anisunity@users.noreply.github.com>
2020-11-30 22:34:42 +0100

    Added the possibility to disable distance based roughness for reflection probes. (#2641)
    * Added the possibility to disable distance based roughness for reflection probes.
    
    * Update ProbeSettings.cs
    
    * Update ProbeSettings.cs
    
    * Hide distance based roughness in planar + update doc
    
    * Formatting
    
    * Don't display distance based roughness with infinite projection
    
    Co-authored-by: sebastienlagarde <sebastien@unity3d.com>

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/LightLoop.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/LightLoop.cs
@@ -1858,8 +1858,9 @@ namespace UnityEngine.Rendering.HighDefinition
 
         internal bool GetEnvLightData(CommandBuffer cmd, HDCamera hdCamera, in ProcessedProbeData processedProbe, ref EnvLightData envLightData)
         {
-            // For the generic case, we consider that all probes have mip maps. The more specific case will override this attribute.
+            // By default, rough reflections are enabled for both types of probes.
             envLightData.roughReflections = 1.0f;
+            envLightData.distanceBasedRoughness = 0.0f;
 
             Camera camera = hdCamera.camera;
             HDProbe probe = processedProbe.hdProbe;
@@ -1962,6 +1963,9 @@ namespace UnityEngine.Rendering.HighDefinition
                     );
                     capturePosition = cameraPositionSettings.position;
                     envLightData.rangeCompressionFactorCompensation = Mathf.Max(probe.rangeCompressionFactor, 1e-6f);
+
+                    // Propagate the distance based information to the env light data (only if we are not an infinite projection)
+                    envLightData.distanceBasedRoughness = probe.settings.distanceBasedRoughness && !probe.isProjectionInfinite ? 1.0f : 0.0f;
 
                     break;
                 }
