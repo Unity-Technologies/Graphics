581be6c7868ca38fe799547025ba8059cd932f6d
Sophia <sophia@unity3d.com>
2020-12-02 13:35:48 +0100

    wrong job again

--- .yamato/ruamel/jobs/projects/commands/osx_metal.py
+++ .yamato/ruamel/jobs/projects/commands/osx_metal.py
@@ -76,6 +76,10 @@ def cmd_standalone_build(project_folder, platform, api, test_platform, editor, b
     base = [
         f'curl -s {UTR_INSTALL_URL} --output {TEST_PROJECTS_DIR}/{project_folder}/utr',
         f'chmod +x {TEST_PROJECTS_DIR}/{project_folder}/utr',
+        f'git clone https://github.cds.internal.unity3d.com/unity/gfx-sdet-tools.git TestProjects/{project_folder}',
+        f'cd ~~/{TEST_PROJECTS_DIR}/{project_folder}/gfx-sdet-tools/signing-scripts && ./sign.sh bokken bokken',
+        f'cd ~~/{TEST_PROJECTS_DIR}/{project_folder}/gfx-sdet-tools/signing-scripts && chmod +x import_certificate_into_new_keychain.sh',
+        f'cd ~~/{TEST_PROJECTS_DIR}/{project_folder}/gfx-sdet-tools/signing-scripts && ./import_certificate_into_new_keychain.sh bokken bokken',
         f'pip install unity-downloader-cli --index-url {UNITY_DOWNLOADER_CLI_URL} --upgrade',
         f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-downloader-cli { get_unity_downloader_cli_cmd(editor, platform["os"],cd=True) } {"".join([f"-c {c} " for c in platform["components"]])} --wait --published-only',
     ]
@@ -84,6 +88,8 @@ def cmd_standalone_build(project_folder, platform, api, test_platform, editor, b
     
     if project_folder.lower() == "BoatAttack".lower():
         base = extra_perf_cmd(project_folder) + install_unity_config(project_folder) + base
+
+    base.append(f'codesign -fs unity-player ~/players/{project_folder}/PlayerWithTests.app')
 
     return base
 
