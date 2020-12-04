f6f10de8f13e6c21a17eb4154f6829d8811925d8
Chris Tchou <ctchou@unity3d.com>
2020-12-04 09:59:09 -0800

    [ShaderGraph] [11.x] Fix texture associated state (samplerstate, TexelSize) for subgraphs, CFNs (#2651)
    * Cherry-pick of 10.x.x/sg/fix/textures (35071169762e245f719c572318892597b27524e3) merge into 10.x.x/release (6891010d415a8f90343a8f5dcac8cad934fe0f34)
    
    * Improved backwards compatibility, added graphics tests, addressing feedback
    
    * Fix for GLES2 compilation
    
    * Fix for OpenGL Core
    
    * Adding GLCore support for LOAD_TEXTURE3D and LOAD_TEXTURE3D_LOD
    
    * Fix for consoles
    
    * Disallow creating v0 of custom function node (which will upgrade incorrectly when saved/loaded)
    
    * Apply Formatting
    
    * Adding a comment to address feedback

--- com.unity.shadergraph/Editor/Data/Nodes/Input/Texture/TexelSizeNode.cs
+++ com.unity.shadergraph/Editor/Data/Nodes/Input/Texture/TexelSizeNode.cs
@@ -1,4 +1,4 @@
-using System.Linq;
+﻿using System.Linq;
 using UnityEngine;
 using UnityEditor.Graphing;
 using UnityEditor.ShaderGraph.Drawing.Controls;
@@ -35,8 +35,8 @@ namespace UnityEditor.ShaderGraph
         // Node generations
         public virtual void GenerateNodeCode(ShaderStringBuilder sb, GenerationMode generationMode)
         {
-            sb.AppendLine(string.Format("$precision {0} = {1}_TexelSize.z;", GetVariableNameForSlot(OutputSlotWId), GetSlotValue(TextureInputId, generationMode)));
-            sb.AppendLine(string.Format("$precision {0} = {1}_TexelSize.w;", GetVariableNameForSlot(OutputSlotHId), GetSlotValue(TextureInputId, generationMode)));
+            sb.AppendLine(string.Format("$precision {0} = {1}.texelSize.z;", GetVariableNameForSlot(OutputSlotWId), GetSlotValue(TextureInputId, generationMode)));
+            sb.AppendLine(string.Format("$precision {0} = {1}.texelSize.w;", GetVariableNameForSlot(OutputSlotHId), GetSlotValue(TextureInputId, generationMode)));
         }
 
         public bool RequiresMeshUV(UVChannel channel, ShaderStageCapability stageCapability)
