aa7e6a58fad2acdf1051c928b1d68bb4aed0a337
FrancescoC-Unity <francescoc@unity3d.com>
2020-12-08 13:47:43 +0100

    Fix asserts about z-axis not pointing forward in tests.

--- TestProjects/HDRP_Tests/Assets/GraphicTests/Scenes/2x_Lighting/2212_ReflectionProbes_Skies/BakeProbes.cs
+++ TestProjects/HDRP_Tests/Assets/GraphicTests/Scenes/2x_Lighting/2212_ReflectionProbes_Skies/BakeProbes.cs
@@ -3,23 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.Rendering.HighDefinition;
+using UnityEditor.Experimental.Rendering;
 
 public class BakeProbes : MonoBehaviour
 {
-    public ReflectionProbe[] refProbe;
-
     IEnumerator Start()
     {
         yield return new WaitForSeconds(0.5f);
-        foreach (var probe in refProbe)
-        {
-            Lightmapping.BakeReflectionProbe(probe, "Assets/" + probe.ToString() + ".hdr");
-            HDAdditionalReflectionData addRefData = probe.gameObject.GetComponent<HDAdditionalReflectionData>();
-
-            Texture texture = (Texture)AssetDatabase.LoadAssetAtPath("Assets/" + probe.ToString() + ".hdr", typeof(Texture));
-            addRefData.bakedTexture = texture;
-            yield return null;
-        }
+        var system = ScriptableBakedReflectionSystemSettings.system;
+        system.BakeAllReflectionProbes();
     }
 
 }
