3464cae6a805fe3c5082216388f7aea578fa1420
Esmeralda Salamone <esmeralda.salamone@unity3d.com>
2020-12-17 12:05:36 -0800

    Fixed Instance ID tests and added tests for Instance ID nodes.

--- TestProjects/ShaderGraph/Assets/CommonAssets/Scripts/ShaderGraphGraphicsTestSettings.cs
+++ TestProjects/ShaderGraph/Assets/CommonAssets/Scripts/ShaderGraphGraphicsTestSettings.cs
@@ -11,4 +11,8 @@ public class ShaderGraphGraphicsTestSettings : GraphicsTestSettings
         ImageComparisonSettings.AverageCorrectnessThreshold = 0.005f;
         ImageComparisonSettings.PerPixelCorrectnessThreshold = 0.005f;
     }
+
+    public virtual void OnTestComplete() { }
+
+    public virtual void OnTestBegin() { }
 }
