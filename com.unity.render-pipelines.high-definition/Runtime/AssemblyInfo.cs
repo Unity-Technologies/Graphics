d299c9f7b2660bb81bdbb64f98dd57a6b7d7ef1c
Joachim Ante <joe@unity3d.com>
2020-12-19 02:15:15 +0100

    Move water to environment package

--- com.unity.render-pipelines.high-definition/Runtime/AssemblyInfo.cs
+++ com.unity.render-pipelines.high-definition/Runtime/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("Unity.RenderPipelines.Water")]
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Editor")]
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Editor.Tests")]
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Runtime.Tests")]
