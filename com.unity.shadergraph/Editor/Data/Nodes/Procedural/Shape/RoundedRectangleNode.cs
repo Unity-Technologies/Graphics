ff8f2a8393ce8e588872495d673d20d2aa912a26
Kay Chang <kaychang@unity3d.com>
2020-12-23 13:42:25 -0800

    Fixed division by 0 on Nintendo Switch.

--- com.unity.shadergraph/Editor/Data/Nodes/Procedural/Shape/RoundedRectangleNode.cs
+++ com.unity.shadergraph/Editor/Data/Nodes/Procedural/Shape/RoundedRectangleNode.cs
@@ -30,7 +30,8 @@ namespace UnityEditor.ShaderGraph
     Radius = max(min(min(abs(Radius * 2), abs(Width)), abs(Height)), 1e-5);
     $precision2 uv = abs(UV * 2 - 1) - $precision2(Width, Height) + Radius;
     $precision d = length(max(0, uv)) / Radius;
-    Out = saturate((1 - d) / fwidth(d));
+    $precision fwd = max(fwidth(d), 1e-5);
+    Out = saturate((1 - d) / fwd);
 }";
         }
     }
