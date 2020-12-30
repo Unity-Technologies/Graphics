c5c432a157c02db5cdb615d66570d9ac8c882244
Sai Narayan <nightmask3@gmail.com>
2020-12-30 14:13:42 -0800

    Created branch and migrated code from old public repo

--- com.unity.shadergraph/Editor/Drawing/Controllers/SGController.cs
+++ com.unity.shadergraph/Editor/Drawing/Controllers/SGController.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Profiling;
@@ -34,6 +34,7 @@ namespace UnityEditor.ShaderGraph
 
     abstract class SGController
     {
+
     }
 
     abstract class SGController<T> : SGController
--- com.unity.shadergraph/Editor/Drawing/Interfaces/ISGControlledElement.cs
+++ com.unity.shadergraph/Editor/Drawing/Interfaces/ISGControlledElement.cs
@@ -1,4 +1,4 @@
-using UnityEditor.ShaderGraph;
+﻿using UnityEditor.ShaderGraph;
 
 namespace UnityEditor.ShaderGraph
 {
