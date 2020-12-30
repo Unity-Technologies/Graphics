532c8832edc92b35c475228d393ff0b4f67a71dd
Sai Narayan <nightmask3@gmail.com>
2020-12-30 14:13:42 -0800

    Created branch and migrated code from old public repo

---
+++ com.unity.shadergraph/Editor/Data/Interfaces/IGraphDataAction.cs
@@ -0,0 +1,8 @@
+﻿namespace UnityEditor.ShaderGraph
+{
+    interface IGraphDataAction
+    {
+        // Takes in GraphData, performs some modification on it, and then returns it
+        GraphData MutateGraphData(GraphData initialState);
+    }
+}
