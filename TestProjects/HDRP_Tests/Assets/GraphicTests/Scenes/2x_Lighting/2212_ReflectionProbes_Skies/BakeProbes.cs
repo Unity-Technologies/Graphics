e41c8a701ae429d7ac00b388edfd2ada0bf5ed2b
Evgenii <evgenii@unity3d.com>
2021-01-06 17:03:10 -0800

    Fix script breaking player build

--- TestProjects/HDRP_Tests/Assets/GraphicTests/Scenes/2x_Lighting/2212_ReflectionProbes_Skies/BakeProbes.cs
+++ TestProjects/HDRP_Tests/Assets/GraphicTests/Scenes/2x_Lighting/2212_ReflectionProbes_Skies/BakeProbes.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 using UnityEngine.Rendering.HighDefinition;
+#if UNITY_EDITOR
+using UnityEditor;
 using UnityEditor.Experimental.Rendering;
+#endif
 
 public class BakeProbes : MonoBehaviour
 {
     IEnumerator Start()
     {
         yield return new WaitForSeconds(0.5f);
+    #if UNITY_EDITOR
         var system = ScriptableBakedReflectionSystemSettings.system;
         system.BakeAllReflectionProbes();
+    #endif
     }
 
 }
