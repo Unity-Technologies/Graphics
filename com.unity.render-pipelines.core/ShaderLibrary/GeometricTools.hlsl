c9546653363b01332ad849dd5bca2d2bbf010919
Kleber Garcia <kleber.garcia@unity3d.com>
2021-01-11 12:40:59 -0500

    Renamed to a new function so we keep backwards compatibility. Restored the shadow shader pass path, accidentally changed it to bool4 and use near plane.

--- com.unity.render-pipelines.core/ShaderLibrary/GeometricTools.hlsl
+++ com.unity.render-pipelines.core/ShaderLibrary/GeometricTools.hlsl
@@ -237,7 +237,7 @@ bool CullTriangleFrustum(float3 p0, float3 p1, float3 p2, float epsilon, float4 
 //output packing: 
 // x,y,z - one component per triangle edge, true if outside, false otherwise
 // w - true if entire triangle is outside of at least 1 plane of the frustum, false otherwise 
-bool4 CullTriangleEdgesFrustum(float3 p0, float3 p1, float3 p2, float epsilon, float4 frustumPlanes[6], int numPlanes)
+bool4 CullFullTriangleAndEdgesFrustum(float3 p0, float3 p1, float3 p2, float epsilon, float4 frustumPlanes[6], int numPlanes)
 {
     bool4 edgesOutsideXYZ_triangleOutsideW = false;
 
@@ -247,16 +247,26 @@ bool4 CullTriangleEdgesFrustum(float3 p0, float3 p1, float3 p2, float epsilon, f
                                     DistanceFromPlane(p1, frustumPlanes[i]) < epsilon,
                                     DistanceFromPlane(p2, frustumPlanes[i]) < epsilon);
 
-	bool3 edgesOutside;
-        // If both points of the edge are behind any of the planes, we cull.
+        bool3 edgesOutside;
+            // If both points of the edge are behind any of the planes, we cull.
         edgesOutside.x = pointsOutside.y && pointsOutside.z;
         edgesOutside.y = pointsOutside.x && pointsOutside.z;
         edgesOutside.z = pointsOutside.x && pointsOutside.y;
-	
-	edgesOutsideXYZ_triangleOutsideW = edgesOutsideXYZ_triangleOutsideW || bool4(edgesOutside.xyz, all(pointsOutside));
+        
+        edgesOutsideXYZ_triangleOutsideW = edgesOutsideXYZ_triangleOutsideW || bool4(edgesOutside.xyz, all(pointsOutside));
     }
 
     return edgesOutsideXYZ_triangleOutsideW;
+}
+
+// Returns 'true' if the edge of the triangle is outside of the frustum.
+// The edges are defined s.t. they are on the opposite side of the point with the given index.
+// 'epsilon' is the (negative) distance to (outside of) the frustum below which we cull the triangle.
+//output packing: 
+// x,y,z - one component per triangle edge, true if outside, false otherwise
+bool3 CullTriangleEdgesFrustum(float3 p0, float3 p1, float3 p2, float epsilon, float4 frustumPlanes[6], int numPlanes)
+{
+    return CullFullTriangleAndEdgesFrustum(p0, p1, p2, epsilon, frustumPlanes, numPlanes).xyz;
 }
 
 bool CullTriangleBackFaceView(float3 p0, float3 p1, float3 p2, float epsilon, float3 V, float winding)
