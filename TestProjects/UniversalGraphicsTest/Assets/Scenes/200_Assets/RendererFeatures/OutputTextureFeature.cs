fd739fca597e4c60c7f8df0ebeaa6b43c06dcbd1
Elvar Örn Unnþórsson <ellioman@ellioman.com>
2021-01-12 09:45:36 +0100

    More scene updates

---
+++ TestProjects/UniversalGraphicsTest/Assets/Scenes/200_Assets/RendererFeatures/OutputTextureFeature.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class OutputTextureFeature : ScriptableRendererFeature
+{
+    public Shader shader;
+    private Material m_Material;
+    private CustomRenderPass m_ScriptablePass;
+
+    /// <inheritdoc/>
+    public override void Create()
+    {
+        m_ScriptablePass = new CustomRenderPass();
+        m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRendering + 2;
+    }
+
+    // Here you can inject one or multiple render passes in the renderer.
+    // This method is called when setting up the renderer once per-camera.
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        if (shader == null)
+        {
+            return;
+        }
+
+        if (m_Material == null)
+        {
+            m_Material = new Material(shader);
+        }
+
+        m_ScriptablePass.Setup(m_Material);
+        renderer.EnqueuePass(m_ScriptablePass);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        CoreUtils.Destroy(m_Material);
+    }
+
+
+
+    class CustomRenderPass : ScriptableRenderPass
+    {
+        private Material m_Material;
+        public void Setup(Material material)
+        {
+            m_Material = material;
+            ConfigureInput(ScriptableRenderPassInput.Normal);
+        }
+
+
+        // This method is called before executing the render pass.
+        // It can be used to configure render targets and their clear state. Also to create temporary render target textures.
+        // When empty this render pass will render to the active camera render target.
+        // You should never call CommandBuffer.SetRenderTarget. Instead call <c>ConfigureTarget</c> and <c>ConfigureClear</c>.
+        // The render pipeline will ensure target setup and clearing happens in a performant manner.
+        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
+        {
+
+        }
+
+        // Here you can implement the rendering logic.
+        // Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
+        // https://docs.unity3d.com/ScriptReference/Rendering.ScriptableRenderContext.html
+        // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            CommandBuffer cmd = CommandBufferPool.Get();
+            using (new ProfilingScope(cmd, new ProfilingSampler("OutputTextureFeature")))
+            {
+                cmd.SetRenderTarget(BuiltinRenderTextureType.CameraTarget,
+                    RenderBufferLoadAction.DontCare, RenderBufferStoreAction.Store, // color
+                    RenderBufferLoadAction.DontCare, RenderBufferStoreAction.DontCare); // depth
+                cmd.DrawMesh(RenderingUtils.fullscreenMesh, Matrix4x4.identity, m_Material, 0, 0);
+            }
+
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+
+        // Cleanup any allocated resources that were created during the execution of this render pass.
+        public override void OnCameraCleanup(CommandBuffer cmd)
+        {
+
+        }
+    }
+}
+
+
