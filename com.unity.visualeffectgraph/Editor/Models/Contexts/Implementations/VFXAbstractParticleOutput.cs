29a095e02a29056bf6d3157716f60b13be09e108
Gabriel de la Cruz <gabriel.delacruz@unity3d.com>
2021-01-13 15:25:47 +0100

    Motion Vector generation improvements (#194)
    * Added VFX_FEATURE_MOTION_VECTORS_VERTS mode to store motion vector info per vertex
    
    Optimized default motion vectors mode to store 4x3 matrix instead of 4x4.
    Added mode to store each previous vertex position for quads, triangles, lines and points.
    Refactor of motion vector defines.
    
    * Motion vectors support in lines and points
    
    * Resize buffer according to the element size
    
    * fix points and lines when not using the optimization
    
    * Fix motion vectors for lines
    
    * Changes suggested in review
    
    - Removed misplaced comment in VFXLineStripOutput
    - Access VFXLineOutput useTargetOffset through GetSettingValue in VFXOutputUpdate
    - Moved previous frame position calculation to the end of OutputUpdate compute
    - Added 4 bytes per particle for last frame index in elementToVFXBufferMotionVector buffer
    - Added noperspective when using VFX_FEATURE_MOTION_VECTORS_VERTS, through VFX_FEATURE_MOTION_VECTORS_INTERPOLATION define.
    - Fixed order of verts in lineSW particles.
    
    * Missing define rename in ParticleMeshes/PassForward.template
    
    * Improved condition for discarding due to not enough indirect buffers
    
    * Missing renaming of WRITE_MOTION_VECTOR_IN_FORWARD for particle lit meshes

--- com.unity.visualeffectgraph/Editor/Models/Contexts/Implementations/VFXAbstractParticleOutput.cs
+++ com.unity.visualeffectgraph/Editor/Models/Contexts/Implementations/VFXAbstractParticleOutput.cs
@@ -389,15 +389,32 @@ namespace UnityEditor.VFX
                         break;
                 }
 
+                if (hasMotionVector)
                 {
-                    if (hasMotionVector)
-                        if (isBlendModeOpaque)
-                            yield return "USE_MOTION_VECTORS_PASS";
-                        else
-                            yield return "WRITE_MOTION_VECTOR_IN_FORWARD";
-                    if (hasShadowCasting)
-                        yield return "USE_CAST_SHADOWS_PASS";
+                    yield return "VFX_FEATURE_MOTION_VECTORS";
+                    if (isBlendModeOpaque)
+                        yield return "USE_MOTION_VECTORS_PASS";
+                    else
+                        yield return "VFX_FEATURE_MOTION_VECTORS_FORWARD";
+                    switch (taskType)
+                    {
+                        case VFXTaskType.ParticleQuadOutput:
+                            yield return "VFX_FEATURE_MOTION_VECTORS_VERTS 4";
+                            break;
+                        case VFXTaskType.ParticleTriangleOutput:
+                            yield return "VFX_FEATURE_MOTION_VECTORS_VERTS 3";
+                            break;
+                        case VFXTaskType.ParticleLineOutput:
+                            yield return "VFX_FEATURE_MOTION_VECTORS_VERTS 2";
+                            break;
+                        case VFXTaskType.ParticlePointOutput:
+                            yield return "VFX_FEATURE_MOTION_VECTORS_VERTS 1";
+                            break;
+                    }
                 }
+
+                if (hasShadowCasting)
+                    yield return "USE_CAST_SHADOWS_PASS";
 
                 if (HasIndirectDraw())
                     yield return "VFX_HAS_INDIRECT_DRAW";
