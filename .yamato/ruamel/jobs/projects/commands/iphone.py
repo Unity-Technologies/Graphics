d79eff26126876f372f2f3e19e94280971324e62
Sophia <sophia@unity3d.com>
2021-01-13 17:14:44 +0100

    (hotfix) use old installer for ios for now

--- .yamato/ruamel/jobs/projects/commands/iphone.py
+++ .yamato/ruamel/jobs/projects/commands/iphone.py
@@ -65,13 +65,21 @@ def cmd_standalone(project, platform, api, test_platform, editor, build_config, 
         
 def cmd_standalone_build(project, platform, api, test_platform, editor, build_config, color_space):
 
-    base = [
-        f'brew tap --force-auto-update unity/unity git@github.cds.internal.unity3d.com:unity/homebrew-unity.git',
-        f'brew install unity/unity/unity-downloader-cli',
-        f'unity-downloader-cli { get_unity_downloader_cli_cmd(editor, platform["os"]) } {"".join([f"-c {c} " for c in platform["components"]])}  --wait --published-only',
-        f'curl -s {UTR_INSTALL_URL} --output utr',
-        f'chmod +x ./utr'
-     ]
+    if "BoatAttack" not in project["name"]:
+        base = [
+            f'brew tap --force-auto-update unity/unity git@github.cds.internal.unity3d.com:unity/homebrew-unity.git',
+            f'brew install unity/unity/unity-downloader-cli',
+            f'unity-downloader-cli { get_unity_downloader_cli_cmd(editor, platform["os"]) } {"".join([f"-c {c} " for c in platform["components"]])}  --wait --published-only',
+            f'curl -s {UTR_INSTALL_URL} --output utr',
+            f'chmod +x ./utr'
+        ]
+    else:
+        base = [
+            f'pip install unity-downloader-cli --index-url https://artifactory.prd.it.unity3d.com/artifactory/api/pypi/pypi/simple --upgrade',
+            f'unity-downloader-cli { get_unity_downloader_cli_cmd(editor, platform["os"]) } {"".join([f"-c {c} " for c in platform["components"]])}  --wait --published-only',
+            f'curl -s {UTR_INSTALL_URL} --output utr',
+            f'chmod +x ./utr'
+        ]
     
     utr_calls = get_repeated_utr_calls(test_platform, platform, api, build_config, color_space, project["folder"], utr_flags_key="utr_flags_build")
     for utr_args in utr_calls:
