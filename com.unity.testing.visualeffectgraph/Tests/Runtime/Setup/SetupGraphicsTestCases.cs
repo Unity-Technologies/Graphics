c451a43d613a837b04eb31a713807dab384d091c
Paul Demeulenaere <pauld@unity3d.com>
2021-01-15 15:14:22 +0100

    Use preserve directive for "SetupGraphicsTestCases"

--- com.unity.testing.visualeffectgraph/Tests/Runtime/Setup/SetupGraphicsTestCases.cs
+++ com.unity.testing.visualeffectgraph/Tests/Runtime/Setup/SetupGraphicsTestCases.cs
@@ -2,12 +2,14 @@ using System;
 using System.IO;
 using System.Linq;
 using UnityEditor;
+using UnityEngine.Scripting;
 using UnityEngine.TestTools;
 using UnityEngine.VFX;
 
 // Work around case #1033694, unable to use PrebuildSetup types directly from assemblies that don't have special names.
 // Once that's fixed, this class can be deleted and the SetupGraphicsTestCases class in Unity.TestFramework.Graphics.Editor
 // can be used directly instead.
+[assembly: Preserve]
 public class SetupGraphicsTestCases : IPrebuildSetup
 {
     public void Setup()
