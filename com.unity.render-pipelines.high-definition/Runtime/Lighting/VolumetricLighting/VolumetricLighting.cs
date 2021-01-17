426197e4613f7755d737448c7899c669476c9d27
Sebastien Lagarde <sebastien@unity3d.com>
2021-01-17 19:57:31 +0100

    Fix PrepareVolumetricLightingParameters compilation issue

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/VolumetricLighting/VolumetricLighting.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/VolumetricLighting/VolumetricLighting.cs
@@ -1111,7 +1111,7 @@ namespace UnityEngine.Rendering.HighDefinition
                 return;
             }
 
-            var parameters = PrepareVolumetricLightingParameters(hdCamera, frameIndex);
+            var parameters = PrepareVolumetricLightingParameters(hdCamera);
 
             using (new ProfilingScope(cmd, ProfilingSampler.Get(HDProfileId.VolumetricLighting)))
             {
