d91da7b06cf18911709fd981a11a932f7e2dbea7
Liis Kivistik <liis.kivistik@unity3d.com>
2021-01-19 14:32:46 +0200

    update ymls

--- .yamato/ruamel/jobs/projects/commands/android.py
+++ .yamato/ruamel/jobs/projects/commands/android.py
@@ -1,137 +1,113 @@
 from ...shared.constants import TEST_PROJECTS_DIR, PATH_UNITY_REVISION, PATH_TEST_RESULTS, PATH_PLAYERS, UTR_INSTALL_URL, UNITY_DOWNLOADER_CLI_URL, get_unity_downloader_cli_cmd, get_timeout
 from ruamel.yaml.scalarstring import PreservedScalarString as pss
-from ...shared.utr_utils import extract_flags
+from ...shared.utr_utils import extract_flags, get_repeated_utr_calls
 
 
-def _cmd_base(project_folder, components):
+def _cmd_base(project, components):
     return [    ]
 
 
-def cmd_editmode(project_folder, platform, api, test_platform, editor, build_config, color_space):    
-    utr_args = extract_flags(test_platform["utr_flags"], platform["name"], api["name"], build_config, color_space, project_folder)
-
+def cmd_editmode(project, platform, api, test_platform, editor, build_config, color_space):    
     base = [ 
         f'curl -s {UTR_INSTALL_URL}.bat --output utr.bat',
         f'pip install unity-downloader-cli --index-url {UNITY_DOWNLOADER_CLI_URL} --upgrade',
         f'unity-downloader-cli { get_unity_downloader_cli_cmd(editor, platform["os"]) } -p WindowsEditor {"".join([f"-c {c} " for c in platform["components"]])} --wait --published-only',
-        f'%ANDROID_SDK_ROOT%\platform-tools\\adb.exe connect %BOKKEN_DEVICE_IP%',
-        f'powershell %ANDROID_SDK_ROOT%\platform-tools\\adb.exe devices',
-        f'NetSh Advfirewall set allprofiles state off',
-        pss(f'''
-        set ANDROID_DEVICE_CONNECTION=%BOKKEN_DEVICE_IP%
-        utr {" ".join(utr_args)}'''),
-        f'start %ANDROID_SDK_ROOT%\platform-tools\\adb.exe kill-server'
-        ]
-    
-    extra_cmds = extra_perf_cmd(project_folder)
-    unity_config = install_unity_config(project_folder)
-    extra_cmds = extra_cmds + unity_config
-    if project_folder.lower() == "BoatAttack".lower():
-        base = extra_cmds + base
-    return base
+        f'NetSh Advfirewall set allprofiles state off']
 
-
-def cmd_playmode(project_folder, platform, api, test_platform, editor, build_config, color_space):
-
-    utr_args = extract_flags(test_platform["utr_flags"], platform["name"], api["name"], build_config, color_space, project_folder)
-
-    base = [ 
-        f'curl -s {UTR_INSTALL_URL}.bat --output utr.bat',
-        f'pip install unity-downloader-cli --index-url {UNITY_DOWNLOADER_CLI_URL} --upgrade',
-        f'unity-downloader-cli { get_unity_downloader_cli_cmd(editor, platform["os"]) } -p WindowsEditor {"".join([f"-c {c} " for c in platform["components"]])} --wait --published-only',
-        f'%ANDROID_SDK_ROOT%\platform-tools\\adb.exe connect %BOKKEN_DEVICE_IP%',
-        f'powershell %ANDROID_SDK_ROOT%\platform-tools\\adb.exe devices',
-        f'NetSh Advfirewall set allprofiles state off',
-        pss(f'''
-        set ANDROID_DEVICE_CONNECTION=%BOKKEN_DEVICE_IP%
-        utr {" ".join(utr_args)}'''),
-        f'start %ANDROID_SDK_ROOT%\platform-tools\\adb.exe kill-server'
-        ]
-    
-    extra_cmds = extra_perf_cmd(project_folder)
-    unity_config = install_unity_config(project_folder)
-    extra_cmds = extra_cmds + unity_config
-    if project_folder.lower() == "BoatAttack".lower():
-        base = extra_cmds + base
-    return base
-
-def cmd_standalone(project_folder, platform, api, test_platform, editor, build_config, color_space):   
-    utr_args = extract_flags(test_platform["utr_flags"], platform["name"], api["name"], build_config, color_space, project_folder)
-
-
-    base = [ 
-        f'curl -s {UTR_INSTALL_URL}.bat --output utr.bat',
-        f'%ANDROID_SDK_ROOT%\platform-tools\\adb.exe connect %BOKKEN_DEVICE_IP%',
-        f'powershell %ANDROID_SDK_ROOT%\platform-tools\\adb.exe devices',
-        f'NetSh Advfirewall set allprofiles state off',
-        pss(f'''
-        set ANDROID_DEVICE_CONNECTION=%BOKKEN_DEVICE_IP%
-        utr {" ".join(utr_args)}'''),
-        f'start %ANDROID_SDK_ROOT%\platform-tools\\adb.exe kill-server'
-        ]
-    
-    return base
-
-        
-def cmd_standalone_build(project_folder, platform, api, test_platform, editor, build_config, color_space):
-    utr_args = extract_flags(test_platform["utr_flags_build"], platform["name"], api["name"], build_config, color_space, project_folder)
-
-    base = [  
-        f'curl -s {UTR_INSTALL_URL}.bat --output utr.bat',
-        f'pip install unity-downloader-cli --index-url {UNITY_DOWNLOADER_CLI_URL} --upgrade',
-        f'unity-downloader-cli { get_unity_downloader_cli_cmd(editor, platform["os"]) } -p WindowsEditor {"".join([f"-c {c} " for c in platform["components"]])} --wait --published-only',
-        f'NetSh Advfirewall set allprofiles state off',
+    utr_calls = get_repeated_utr_calls(test_platform, platform, api, build_config, color_space, project["folder"])
+    for utr_args in utr_calls:
+        base.append(
         pss(f'''
          git rev-parse HEAD | git show -s --format=%%cI > revdate.tmp
          set /p GIT_REVISIONDATE=<revdate.tmp
          echo %GIT_REVISIONDATE%
          del revdate.tmp
-         utr {" ".join(utr_args)}''')
-        ]
+         utr {" ".join(utr_args)}'''))
     
-    extra_cmds = extra_perf_cmd(project_folder)
-    unity_config = install_unity_config(project_folder)
-    extra_cmds = extra_cmds + unity_config
-    if project_folder.lower() == "BoatAttack".lower():
-        base = extra_cmds + base
+    base = add_project_commands(project) + base
     return base
 
 
-def extra_perf_cmd(project_folder):   
-    perf_list = [
-        f'git clone https://github.com/Unity-Technologies/BoatAttack.git -b feature/benchmark TestProjects/{project_folder}'
-        ]
-    return perf_list
+def cmd_playmode(project, platform, api, test_platform, editor, build_config, color_space):
 
-def install_unity_config(project_folder):
-    cmds = [
-        f'choco source add -n Unity -s https://artifactory.prd.it.unity3d.com/artifactory/api/nuget/unity-choco-local',
-        f'choco install unity-config',
+    base = [ 
+        f'curl -s {UTR_INSTALL_URL}.bat --output utr.bat',
+        f'choco install unity-downloader-cli -y -s https://artifactory.prd.it.unity3d.com/artifactory/api/nuget/unity-choco-local',
+        f'unity-downloader-cli { get_unity_downloader_cli_cmd(editor, platform["os"]) } -p WindowsEditor {"".join([f"-c {c} " for c in platform["components"]])} --wait --published-only',
+        f'%ANDROID_SDK_ROOT%\platform-tools\\adb.exe connect %BOKKEN_DEVICE_IP%',
+        f'powershell %ANDROID_SDK_ROOT%\platform-tools\\adb.exe devices',
+        f'NetSh Advfirewall set allprofiles state off']
+    
+    utr_calls = get_repeated_utr_calls(test_platform, platform, api, build_config, color_space, project["folder"])
+    for utr_args in utr_calls:
+        base.append(
+        pss(f'''
+         git rev-parse HEAD | git show -s --format=%%cI > revdate.tmp
+         set /p GIT_REVISIONDATE=<revdate.tmp
+         echo %GIT_REVISIONDATE%
+         del revdate.tmp
+         utr {" ".join(utr_args)}'''))
+    base.append(f'start %ANDROID_SDK_ROOT%\platform-tools\\adb.exe kill-server')
+    base = add_project_commands(project) + base
+    return base
 
+def cmd_standalone(project, platform, api, test_platform, editor, build_config, color_space):   
+    base = [ 
+        f'curl -s {UTR_INSTALL_URL}.bat --output utr.bat',
+        f'%ANDROID_SDK_ROOT%\platform-tools\\adb.exe connect %BOKKEN_DEVICE_IP%',
+        f'powershell %ANDROID_SDK_ROOT%\platform-tools\\adb.exe devices',
+        f'NetSh Advfirewall set allprofiles state off']
+    
+    utr_calls = get_repeated_utr_calls(test_platform, platform, api, build_config, color_space, project["folder"])
+    for utr_args in utr_calls:
+        base.append(
+        pss(f'''
+        set ANDROID_DEVICE_CONNECTION=%BOKKEN_DEVICE_IP%
+         git rev-parse HEAD | git show -s --format=%%cI > revdate.tmp
+         set /p GIT_REVISIONDATE=<revdate.tmp
+         echo %GIT_REVISIONDATE%
+         del revdate.tmp
+        utr {" ".join(utr_args)}'''))
 
-        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency "com.unity.render-pipelines.core@file:../../../com.unity.render-pipelines.core" --project-path .',
-        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency "com.unity.render-pipelines.universal@file:../../../com.unity.render-pipelines.universal" --project-path .',
-        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency "com.unity.shadergraph@file:../../../com.unity.shadergraph" --project-path .',
+    base.append(f'start %ANDROID_SDK_ROOT%\platform-tools\\adb.exe kill-server')
+    return base
 
-
-		#f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project remove dependency com.unity.render-pipelines.universal',
-        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency com.unity.addressables@1.16.7 --project-path .',
-        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency com.unity.scriptablebuildpipeline@1.11.2 --project-path .',
-		f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency com.unity.test-framework@1.1.18 --project-path .',
-        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency com.unity.test-framework.performance@2.4.0 --project-path .',
-		f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency com.unity.test-framework.utp-reporter@1.0.2-preview --project-path .',
-		f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency com.unity.test-framework.build@0.0.1-preview.12 --project-path .',
         
-		f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency \"com.unity.test.metadata-manager@0.1.2-preview\" --project-path .',        
-		f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency \"com.unity.testing.graphics-performance@ssh://git@github.cds.internal.unity3d.com/unity/com.unity.testing.graphics-performance.git\"  --project-path .',        
-		f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add dependency \"unity.graphictests.performance.universal@ssh://git@github.cds.internal.unity3d.com/unity/unity.graphictests.performance.universal.git\" --project-path .',	
-		
-        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add testable com.unity.cli-project-setup  --project-path .',		
-		f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add testable com.unity.test.performance.runtimesettings  --project-path .',
-		f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add testable com.unity.test.metadata-manager  --project-path .',
-        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add testable com.unity.testing.graphics-performance --project-path .',
-        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add testable com.unity.render-pipelines.core  --project-path .',
-        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project add testable unity.graphictests.performance.universal  --project-path .',
-        f'cd {TEST_PROJECTS_DIR}/{project_folder} && unity-config project set project-update false --project-path .'
-    ]
+def cmd_standalone_build(project, platform, api, test_platform, editor, build_config, color_space):
+
+    base = [ 
+        f'curl -s {UTR_INSTALL_URL}.bat --output utr.bat',
+        f'pip install unity-downloader-cli --index-url {UNITY_DOWNLOADER_CLI_URL} --upgrade',
+        f'unity-downloader-cli { get_unity_downloader_cli_cmd(editor, platform["os"]) } -p WindowsEditor {"".join([f"-c {c} " for c in platform["components"]])} --wait --published-only',
+        f'NetSh Advfirewall set allprofiles state off' ]
+
+    utr_calls = get_repeated_utr_calls(test_platform, platform, api, build_config, color_space, project["folder"], utr_flags_key="utr_flags_build")
+    for utr_args in utr_calls:
+        base.append(
+        pss(f'''
+         git rev-parse HEAD | git show -s --format=%%cI > revdate.tmp
+         set /p GIT_REVISIONDATE=<revdate.tmp
+         echo %GIT_REVISIONDATE%
+         del revdate.tmp
+         utr {" ".join(utr_args)}'''))
+
+    base = add_project_commands(project) + base
+
+    return base
+    
+
+def add_project_commands(project):
+    cmds = []
+    if project.get("url"):
+        cmds.extend([
+            f'git clone {project["url"]} -b {project["branch"]} {TEST_PROJECTS_DIR}/{project["folder"]}',
+            f'cd {TEST_PROJECTS_DIR}/{project["folder"]} && git checkout {project["revision"]}',
+        ])
+    if project.get("unity_config_commands"):
+        cmds.extend([
+            f'choco source add -n Unity -s https://artifactory.prd.it.unity3d.com/artifactory/api/nuget/unity-choco-local',
+            f'choco install unity-config',
+        ])
+        for unity_config in project["unity_config_commands"]:
+            cmds.append(f'cd {TEST_PROJECTS_DIR}/{project["folder"]} && {unity_config}')
     return cmds
