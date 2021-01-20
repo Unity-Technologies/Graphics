94ca082acbdac80059de5217bfd14ab732a4b47e
martint-unity <martint@unity3d.com>
2021-01-20 08:46:51 +0100

    backport of PR 2977 undo issue additional light

--- com.unity.render-pipelines.universal/Editor/UniversalRenderPipelineAssetEditor.cs
+++ com.unity.render-pipelines.universal/Editor/UniversalRenderPipelineAssetEditor.cs
@@ -148,7 +148,6 @@ namespace UnityEditor.Rendering.Universal
 
         SerializedProperty m_ShaderVariantLogLevel;
 
-        LightRenderingMode selectedLightRenderingMode;
         SerializedProperty m_ColorGradingMode;
         SerializedProperty m_ColorGradingLutSize;
         SerializedProperty m_UseFastSRGBLinearConversion;
@@ -234,8 +233,6 @@ namespace UnityEditor.Rendering.Universal
             m_UseFastSRGBLinearConversion = serializedObject.FindProperty("m_UseFastSRGBLinearConversion");
 
             m_UseAdaptivePerformance = serializedObject.FindProperty("m_UseAdaptivePerformance");
-
-            selectedLightRenderingMode = (LightRenderingMode)m_AdditionalLightsRenderingModeProp.intValue;
 
             string Key = "Universal_Shadow_Setting_Unit:UI_State";
             m_State = new EditorPrefBoolFlags<EditorUtils.Unit>(Key);
@@ -324,8 +321,7 @@ namespace UnityEditor.Rendering.Universal
                 EditorGUILayout.Space();
 
                 // Additional light
-                selectedLightRenderingMode = (LightRenderingMode)EditorGUILayout.EnumPopup(Styles.addditionalLightsRenderingModeText, selectedLightRenderingMode);
-                m_AdditionalLightsRenderingModeProp.intValue = (int)selectedLightRenderingMode;
+                EditorGUILayout.PropertyField(m_AdditionalLightsRenderingModeProp, Styles.addditionalLightsRenderingModeText);
                 EditorGUI.indentLevel++;
 
                 disableGroup = m_AdditionalLightsRenderingModeProp.intValue == (int)LightRenderingMode.Disabled;
