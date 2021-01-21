95a3bba0885fa42a784618c84faa1b049522e650
Sophia <16596228+sophiaaar@users.noreply.github.com>
2021-01-21 17:31:49 +0100

    [2021.1][Yamato] Misc backports (#3175)
    * backport #3034
    
    * backport #3168
    
    * backport 650775a
    
    * remove win dx12 playmode editor from ci due to 1308059
    
    * Fix urp udate custom revision jobs (#3107)
    * backport #3129
    
    * fix nightlies
    
    * backport #2954
    
    Co-authored-by: Théo Penavaire <theo-pnv@users.noreply.github.com>
    Co-authored-by: Liis Kivistik <liis.kivistik@unity3d.com>

--- .yamato/ruamel/jobs/abv/abv_all_project_ci_nightly.py
+++ .yamato/ruamel/jobs/abv/abv_all_project_ci_nightly.py
@@ -4,18 +4,28 @@ from ..shared.yml_job import YMLJob
 
 class ABV_AllProjectCiNightlyJob():
     
-    def __init__(self, editor, projects, target_branch):
+    def __init__(self, editor, projects, nightly_config, target_branch):
         self.job_id = abv_job_id_all_project_ci_nightly(editor["name"])
-        self.yml = self.get_job_definition(editor, projects, target_branch).get_yml()
+        self.yml = self.get_job_definition(editor, projects, nightly_config.get("extra_dependencies",[]), target_branch).get_yml()
 
     
-    def get_job_definition(self, editor, projects, target_branch): 
-    
-        # define dependencies
-        dependencies = [{
+    def get_job_definition(self, editor, projects, extra_dependencies, target_branch): 
+        dependencies = [] 
+        for dep in extra_dependencies:
+            if dep.get("pr"):
+                dependencies.append({
+                    'path': f'{project_filepath_all(dep["project"])}#{project_job_id_pr(dep["project"], editor["name"])}',
+                    'rerun': editor["rerun_strategy"]})
+            else:
+                for tp in dep["test_platforms"]:
+                    dependencies.append({
+                        'path': f'{project_filepath_specific(dep["project"], dep["platform"], dep["api"])}#{project_job_id_test(dep["project"], dep["platform"], dep["api"], tp, editor["name"], dep["build_config"], dep["color_space"])}',
+                        'rerun': editor["rerun_strategy"]})
+
+        dependencies.append({
                 'path': f'{projectcontext_filepath()}#{projectcontext_job_id_test_all(editor["name"])}',
                 'rerun': editor["rerun_strategy"]
-            }]
+            })
 
         for project in projects:
             dependencies.append({
