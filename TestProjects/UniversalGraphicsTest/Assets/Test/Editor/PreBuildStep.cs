df1f9379df94419f0ee7485bd731df87508ae3ab
martint-unity <martint@unity3d.com>
2021-01-21 15:48:29 +0100

    changing to IPrebuild interface instead

--- TestProjects/UniversalGraphicsTest/Assets/Test/Editor/PreBuildStep.cs
+++ TestProjects/UniversalGraphicsTest/Assets/Test/Editor/PreBuildStep.cs
@@ -1,18 +1,16 @@
 using System;
 using System.Collections.Generic;
 using UnityEditor;
-using UnityEditor.Build;
-using UnityEditor.Build.Reporting;
 using UnityEngine;
+using UnityEngine.TestTools;
 
-public class PreBuildStep : IPreprocessBuildWithReport
+public class PreBuildStep : IPrebuildSetup
 {
     List<string> ScenesToNotBuild = new List<string> {"035_Shader_TerrainShaders", };
-    public int callbackOrder => 0;
-
-    public void OnPreprocessBuild(BuildReport report)
+    public void Setup()
     {
-        if (report.summary.platform == BuildTarget.StandaloneLinux64)
+        BuildTarget buildTarget = EditorUserBuildSettings.activeBuildTarget;
+        if (buildTarget == BuildTarget.StandaloneLinux64)
         {
             EditorBuildSettingsScene[] scenes = EditorBuildSettings.scenes;
             foreach (EditorBuildSettingsScene scene in scenes)
