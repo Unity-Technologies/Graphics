9066a0b3f58c784c9217ae6ad8660ead79bb9b57
Gabriel de la Cruz <gabriel.delacruz@unity3d.com>
2021-01-27 15:12:38 +0100

    Add _CameraViewMatrix shader variable (#208)
    Add _CameraViewMatrix shader variable in HDRP, instead of using legacy unity_CameraToWorld.
    Remove functions using unity_CameraToWorld from VFXCommonOutput.hlsl and move to SRP specific VFXCommon.hlsl files

--- com.unity.render-pipelines.high-definition/Runtime/ShaderLibrary/ShaderVariablesGlobal.cs
+++ com.unity.render-pipelines.high-definition/Runtime/ShaderLibrary/ShaderVariablesGlobal.cs
@@ -43,6 +43,7 @@ namespace UnityEngine.Rendering.HighDefinition
         // ================================
         // TODO: all affine matrices should be 3x4.
         public Matrix4x4 _ViewMatrix;
+        public Matrix4x4 _CameraViewMatrix;
         public Matrix4x4 _InvViewMatrix;
         public Matrix4x4 _ProjMatrix;
         public Matrix4x4 _InvProjMatrix;
