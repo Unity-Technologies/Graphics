d18e37130519a8a7ac1863a71a7b20757bc572cc
sebastienlagarde <sebastien@unity3d.com>
2021-01-27 22:38:40 +0100

    bump to 10.4 (#3270)

--- com.unity.render-pipelines.core/Runtime/Documentation.cs
+++ com.unity.render-pipelines.core/Runtime/Documentation.cs
@@ -15,7 +15,7 @@ namespace UnityEngine.Rendering
         /// <summary>
         /// Current version of the documentation.
         /// </summary>
-        public const string version = "10.3";
+        public const string version = "10.4";
     }
 
     //Need to live in Runtime as Attribute of documentation is on Runtime classes \o/
