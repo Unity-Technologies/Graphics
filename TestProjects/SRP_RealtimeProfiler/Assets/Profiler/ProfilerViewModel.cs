2223ee20f7ce52de122f26335c70075a74b29f23
Arttu Peltonen <arttu.peltonen@unity3d.com>
2021-01-27 15:46:45 +0200

    Rearrange document structure to be closer to current design.

--- TestProjects/SRP_RealtimeProfiler/Assets/Profiler/ProfilerViewModel.cs
+++ TestProjects/SRP_RealtimeProfiler/Assets/Profiler/ProfilerViewModel.cs
@@ -7,24 +7,10 @@ using UnityEngine.UIElements;
 
 public class ProfilerViewModel
 {
-    IMGUIContainer m_GraphContainer = null;
-    Texture m_GraphMockup = null;
-
     // Update "data bindings"
     public void UpdateUI(ProfilerDataModel model, VisualElement viewRoot)
     {
-        viewRoot.Q<Label>("CpuFrameTime").text = $"{model.CpuFrameTime:F2}";
-        viewRoot.Q<Label>("GpuFrameTime").text = $"{model.GpuFrameTime:F2}";
-        
-        if (m_GraphContainer == null)
-        {
-            m_GraphMockup = EditorGUIUtility.Load("Assets/Profiler/graph-mockup.png") as Texture2D;
-            m_GraphContainer = viewRoot.Q<IMGUIContainer>("GraphContainer");
-            m_GraphContainer.onGUIHandler = () =>
-            {
-                GUI.DrawTexture(new Rect(0, 0, 200, 100), m_GraphMockup);
-                //GUI.Box(new Rect(0, 0, 200, 100), "Test IMGUI content");
-            };
-        }
+        viewRoot.Q<Label>("CpuMainThreadFrameTime").text = $"{model.CpuFrameTime:F2}ms";
+        viewRoot.Q<Label>("GpuFrameTime").text = $"{model.GpuFrameTime:F2}ms";
     }
 }
