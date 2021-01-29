6f3cbf126d007685b8d0e93f40634e6cf5857e8e
hedvig <hedvig.axelsson@unity3d.com>
2021-01-29 10:17:20 +0100

    Remove formatting from HDTarget

--- com.unity.render-pipelines.high-definition/Editor/Material/ShaderGraph/HDTarget.cs
+++ com.unity.render-pipelines.high-definition/Editor/Material/ShaderGraph/HDTarget.cs
@@ -947,7 +947,7 @@ namespace UnityEditor.Rendering.HighDefinition.ShaderGraph
         public const string kPassForwardUnlit = "Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/ShaderPass/ShaderPassForwardUnlit.hlsl";
         public const string kPassConstant = "Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/ShaderPass/ShaderPassConstant.hlsl";
         public const string kPassFullScreenDebug = "Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/ShaderPass/ShaderPassFullScreenDebug.hlsl";
-        
+
         public static IncludeCollection MinimalCorePregraph = new IncludeCollection
         {
             { kShaderVariables, IncludeLocation.Pregraph },
