e15c2c802c89dded9f3ec2c74afad0a87071a2c2
Arttu Peltonen <arttu.peltonen@unity3d.com>
2021-02-05 17:06:41 +0200

    Fix UI in Player builds.
    - Removed UI Toolkit assets from project.
    - Added Theme & PanelSettings assets to Resources under the package, load using Resources.Load<>() to remove UnityEditor namespace use.

--- com.unity.render-pipelines.core/Runtime/Profiling/RealtimeProfilerRuntimeUpdater.cs
+++ com.unity.render-pipelines.core/Runtime/Profiling/RealtimeProfilerRuntimeUpdater.cs
@@ -1,5 +1,4 @@
 using System;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -15,24 +14,14 @@ public class RealtimeProfilerRuntimeUpdater : MonoBehaviour
         if (FindObjectOfType<RealtimeProfilerRuntimeUpdater>() != null)
             return;
 
-        // TODO fix
-        var panelSettings = AssetDatabase.LoadAssetAtPath<PanelSettings>("Packages/com.unity.render-pipelines.core/Resources/RuntimeProfilerPanelSettings.asset");
-        if (panelSettings == null)
-        {
-            // TODO: Create PanelSettings asset inside the package
-            Debug.LogError("Failed to initialize Runtime Profiler - UI Toolkit PanelSettings asset not found in project");
-            return;
-        }
-
         var go = new GameObject("[Realtime Profiler Updater]");
         DontDestroyOnLoad(go);
 
         var updater = go.AddComponent<RealtimeProfilerRuntimeUpdater>();
         updater.m_Model = go.AddComponent<RealtimeProfilerModel>();
         updater.m_Document = go.AddComponent<UIDocument>();
-        updater.m_Document.panelSettings = panelSettings;
-        // TODO: Must use Resources.Load here - AssetDatabase is Editor feature
-        updater.m_Document.visualTreeAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Packages/com.unity.render-pipelines.core/Resources/UXML/realtimeprofiler-player-container.uxml");
+        updater.m_Document.panelSettings = Resources.Load<PanelSettings>("RuntimeProfilerPanelSettings");
+        updater.m_Document.visualTreeAsset = Resources.Load<VisualTreeAsset>("UXML/realtimeprofiler-player-container");
     }
 
     void OnDisable()
--- com.unity.render-pipelines.core/Runtime/Profiling/RealtimeProfilerViewModel.cs
+++ com.unity.render-pipelines.core/Runtime/Profiling/RealtimeProfilerViewModel.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
