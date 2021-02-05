b4c0fffc2caeb21a9ee332e9ffcc8835694a8ddb
Liis Kivistik <liis.kivistik@unity3d.com>
2021-02-05 18:04:24 +0100

    [Yamato] [10.x.x] Move yml script to separate repo (#3349)
    * remove ruamel from repo
    
    * Fix update projects and editor pinning
    
    * change lostcrypt scene
    
    * remove upmNoDefaultPackages flag

--- .yamato/script/editor_scripts/store_green_revisions.py
+++ .yamato/script/editor_scripts/store_green_revisions.py
@@ -10,7 +10,7 @@ import requests
 import datetime
 import ruamel.yaml
 from collections import OrderedDict
-from update_revisions import load_config, DEFAULT_CONFIG_FILE, EXPECTATIONS_PATH
+from update_revisions import DEFAULT_CONFIG_FILE, DEFAULT_SHARED_FILE
 from util.subprocess_helpers import git_cmd, run_cmd
 
 yaml = ruamel.yaml.YAML()
@@ -35,20 +35,21 @@ def commit_and_push(commit_msg, working_dir, development_mode=False):
         git_cmd('pull', working_dir)
         git_cmd('push', working_dir)
 
-def get_last_nightly_id(api_key):
+def get_last_nightly_id(api_key, yamato_project_id, yamato_branch, yamato_nightly_job_definition):
     try:
         current_date = str(datetime.date.today())
-        url = f"http://yamato-api.cds.internal.unity3d.com/jobs?filter=project eq 902 and branch eq '10.x.x/release' and filename eq '.yamato%252F_abv.yml%2523all_project_ci_nightly_2020.2' and submitted gt '{current_date}'"
+        url = f"http://yamato-api.cds.internal.unity3d.com/jobs?filter=project eq {yamato_project_id} and branch eq '{yamato_branch}' and filename eq '{yamato_nightly_job_definition}' and submitted gt '{current_date}'"
+        print(f'Calling [{url}]')
         headers={"Authorization":f"ApiKey {api_key}"}
         response = requests.get(url=url, headers=headers)
         if response.status_code != 200:
             raise Exception()
-
+        
         jobs = response.json()["items"]
         if len(jobs) == 0:
             print(f"!! WARNING: No jobs found on {current_date}")
             return None
-
+        
         for job in jobs:
             if job["links"]["triggeredBy"] == "/users/0":
                 return job["id"]
@@ -73,10 +74,10 @@ def get_yamato_dependency_tree(job_id, api_key):
     except:
         print(f"!! ERROR: Failed to call Yamato API. Got {response.json()}")
         return None
-
+    
 def update_green_project_revisions(editor_versions_file, project_versions_file, track, green_revision_jobs, job_id, api_key, working_dir):
     """Updates green project revisions file for given track. If any updates present, adds to git and returns True. If not, returns False."""
-
+    
     # get the revisions used for the job, the last green project revisions, and Yamato dependency tree  
     updated_at = str(datetime.datetime.utcnow())
     revisions_key = f"{track}_latest_internal" if track=="trunk" else f"{track}_staging"
@@ -86,7 +87,7 @@ def update_green_project_revisions(editor_versions_file, project_versions_file, 
     dependency_tree = get_yamato_dependency_tree(job_id, api_key)
     if not dependency_tree:
         return False
-
+    
     # update revisions for each project
     is_updated = False 
     for job_name in green_revision_jobs:
@@ -98,7 +99,7 @@ def update_green_project_revisions(editor_versions_file, project_versions_file, 
         job = jobs[0]
         if job["status"] == 'success': 
             print(f'Updating "{job_name}" [job status: {job["status"]}]')
-
+            
             job_name = job_name.replace(' ', '_').lower()
             if not last_green_job_revisions.get(job_name):
                 last_green_job_revisions[job_name] = {}
@@ -112,10 +113,10 @@ def update_green_project_revisions(editor_versions_file, project_versions_file, 
         last_green_job_revisions = ordereddict_to_dict(last_green_job_revisions)
         with open(project_versions_file, 'w') as f:
             yaml.dump(last_green_job_revisions, f)
-
+        
         git_cmd(f'add {project_versions_file}', working_dir)
         return True
-
+    
     return False
 
 
@@ -123,47 +124,44 @@ def parse_args(flags):
     parser = argparse.ArgumentParser()
     parser.add_argument('--local', action='store_true',
                     help='For local development (doesn\'t switch branches, pull or push)')
-    parser.add_argument('--config', required=False, default=DEFAULT_CONFIG_FILE,
-                        help=f'Configuration YAML file to use. Default: {DEFAULT_CONFIG_FILE}')
-    #parser.add_argument("--revision", required=True)
     parser.add_argument("--track", required=True)
     parser.add_argument("--apikey", required=True, 
                         help='Needed for Yamato auth if jobid arg is specified.')
-    parser.add_argument("--working-dir", required=False,
-                        help='Working directory (optional). If omitted the root '
-                        'of the repo will be used.')
     parser.add_argument("--target-branch", required=True,
                         help='The Git branch to merge the changes in the file into.')
     args = parser.parse_args(flags)
-    if not os.path.isfile(args.config):
-        parser.error(f'Cannot find config file {args.config}')
     return args
 
 
 def main(argv):
     logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
     args = parse_args(argv)
-    config = load_config(args.config)
+    config = load_yml(DEFAULT_CONFIG_FILE)
+    shared = load_yml(DEFAULT_SHARED_FILE)
+    
     editor_versions_file = config['editor_versions_file'].replace('TRACK',str(args.track))
     green_revisions_file = config['green_revisions_file'].replace('TRACK',str(args.track))
+    yamato_branch = shared['target_branch']
+    yamato_project_id = config['yamato_project_id']
+    yamato_nightly_job_definition = config['nightly_job_definition']
 
     try:
-        working_dir = args.working_dir or os.path.abspath(git_cmd('rev-parse --show-toplevel', cwd='.').strip())
+        working_dir = os.path.abspath(git_cmd('rev-parse --show-toplevel', cwd='.').strip())
         print(f'Working directory: {working_dir}')
-
+        
         if args.local:
             logging.warning('\n\n!! DEVELOPMENT MODE: will not switch branch, pull or push !!\n')
         else:
             checkout_and_pull_branch(args.target_branch, working_dir, args.local)
-
-        nightly_job_id = get_last_nightly_id(args.apikey)
+        
+        nightly_job_id = get_last_nightly_id(args.apikey, yamato_project_id, yamato_branch, yamato_nightly_job_definition)
         print(f'Updating green project revisions according to job {nightly_job_id}.')
         if nightly_job_id:
             if update_green_project_revisions(editor_versions_file, green_revisions_file, str(args.track), config['green_revision_jobs'], nightly_job_id, args.apikey, working_dir):
                 commit_and_push(f'[CI] [{str(args.track)}] Updated green project revisions', working_dir, args.local)
             else:
                 print('No projects to update. Exiting successfully without any commit/push.')
-
+       
         return 0
     except subprocess.CalledProcessError as err:
         logging.error(f"Failed to run '{err.cmd}'\nStdout:\n{err.stdout}\nStderr:\n{err.stderr}")
@@ -171,4 +169,4 @@ def main(argv):
 
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:])) 
+    sys.exit(main(sys.argv[1:]))
