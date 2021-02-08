9e85f36cbba432158c38a58097e2692f06aee484
Jonasmortensen <Jonasmortensen3@gmail.com>
2021-02-08 15:54:22 +0100

    Merged URP package samples to single uber sample

---
+++ com.unity.render-pipelines.universal/Samples~/TeachingSamples/SharedAssets/Scripts/AutoLoadPipelineAsset.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+
+[ExecuteAlways]
+public class AutoLoadPipelineAsset : MonoBehaviour
+{
+    public UniversalRenderPipelineAsset pipelineAsset;
+
+    private void OnEnable()
+    {
+        UpdatePipeline();
+    }
+
+    void UpdatePipeline()
+    {
+        if (pipelineAsset)
+        {
+            GraphicsSettings.renderPipelineAsset = pipelineAsset;
+        }
+    }
+}
---
+++ com.unity.render-pipelines.universal/Samples~/TeachingSamples/SharedAssets/Scripts/Rotator.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public Space space;
+
+    public Vector3 axis;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(axis, speed * Time.deltaTime, space);
+    }
+}
