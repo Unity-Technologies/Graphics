18f932dce4c7673fdfbe063956272f16a02be690
John Parsaie <johnpa@unity3d.com>
2021-02-11 10:01:28 -0500

    Add back the support for debug

--- com.unity.render-pipelines.high-definition/Editor/Material/ShaderGraph/Templates/VFX/Config.template.hlsl
+++ com.unity.render-pipelines.high-definition/Editor/Material/ShaderGraph/Templates/VFX/Config.template.hlsl
@@ -54,5 +54,42 @@ $splice(VFXGeneratedBlockFunction)
 
 #define VaryingsMeshType VaryingsMeshToPS
 
-$PrimitiveType.Mesh:            $include("VFX/ConfigMesh.template.hlsl")
-$PrimitiveType.PlanarPrimitive: $include("VFX/ConfigPlanarPrimitive.template.hlsl")
+$OutputType.Mesh:            $include("VFX/ConfigMesh.template.hlsl")
+$OutputType.PlanarPrimitive: $include("VFX/ConfigPlanarPrimitive.template.hlsl")
+
+// Need to redefine GetVaryingsDataDebug since we omit FragInputs.hlsl and generate one procedurally.
+#include "Packages/com.unity.render-pipelines.high-definition/Runtime/Debug/MaterialDebug.cs.hlsl"
+
+void GetVaryingsDataDebug(uint paramId, FragInputs input, inout float3 result, inout bool needLinearToSRGB)
+{
+    switch (paramId)
+    {
+    case DEBUGVIEWVARYING_TEXCOORD0:
+        result = input.texCoord0.xyz;
+        break;
+    case DEBUGVIEWVARYING_TEXCOORD1:
+        result = input.texCoord1.xyz;
+        break;
+    case DEBUGVIEWVARYING_TEXCOORD2:
+        result = input.texCoord2.xyz;
+        break;
+    case DEBUGVIEWVARYING_TEXCOORD3:
+        result = input.texCoord3.xyz;
+        break;
+    case DEBUGVIEWVARYING_VERTEX_TANGENT_WS:
+        result = input.tangentToWorld[0].xyz * 0.5 + 0.5;
+        break;
+    case DEBUGVIEWVARYING_VERTEX_BITANGENT_WS:
+        result = input.tangentToWorld[1].xyz * 0.5 + 0.5;
+        break;
+    case DEBUGVIEWVARYING_VERTEX_NORMAL_WS:
+        result = IsNormalized(input.tangentToWorld[2].xyz) ?  input.tangentToWorld[2].xyz * 0.5 + 0.5 : float3(1.0, 0.0, 0.0);
+        break;
+    case DEBUGVIEWVARYING_VERTEX_COLOR:
+        result = input.color.rgb; needLinearToSRGB = true;
+        break;
+    case DEBUGVIEWVARYING_VERTEX_COLOR_ALPHA:
+        result = input.color.aaa;
+        break;
+    }
+}
