76c4ac560ad2433c40dbdf796454b9ee54a1aeff
Adrien de Tocqueville <adrien.tocqueville@unity3d.com>
2021-02-24 12:13:00 +0100

    Updates graphic test

--- com.unity.render-pipelines.high-definition/Runtime/Material/Decal/DecalUtilities.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Material/Decal/DecalUtilities.hlsl
@@ -270,8 +270,8 @@ DecalSurfaceData GetDecalSurfaceData(PositionInputs posInput, FragInputs input, 
 #ifdef _DOUBLESIDED_ON
     // 'doubleSidedConstants' is float3(-1, -1, -1) in flip mode and float3(1, 1, -1) in mirror mode.
     // It's float3(1, 1, 1) in the none mode.
-    float3 flipSign = input.isFrontFace ? float3(1.0, 1.0, 1.0) : _DoubleSidedConstants;
-    decalSurfaceData.normalWS *= flipSign;
+    float3 flipSign = input.isFrontFace ? float3(1.0, 1.0, 1.0) : _DoubleSidedConstants.xyz;
+    decalSurfaceData.normalWS.xyz *= flipSign;
 #endif // _DOUBLESIDED_ON
 
     return decalSurfaceData;
