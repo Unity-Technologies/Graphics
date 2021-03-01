b47d3a39b8a484a90e24287628309b949511177c
Paul Demeulenaere <pauld@unity3d.com>
2021-03-01 17:33:45 +0100

    Add custom namespace for test script

--- com.unity.testing.visualeffectgraph/Runtime/DirectLinkRLE.cs
+++ com.unity.testing.visualeffectgraph/Runtime/DirectLinkRLE.cs
@@ -3,70 +3,74 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VFX;
 
-public class DirectLinkRLE : MonoBehaviour
+
+namespace Unity.Testing.VisualEffectGraph
 {
-    public Texture2D m_SourceTexture;
-
-    struct SpawnEvent
+    public class DirectLinkRLE : MonoBehaviour
     {
-        public Vector3 color;
-        public float count;
-    }
-    SpawnEvent[] m_SpawnEvents;
+        public Texture2D m_SourceTexture;
 
-    void Start()
-    {
-        var pixels = m_SourceTexture.GetPixels();
-        var spawnEvents = new Stack<SpawnEvent>();
-        foreach (var pixel in pixels)
+        struct SpawnEvent
         {
-            var currentColor = new Vector3(pixel.r, pixel.g, pixel.b);
-            if (spawnEvents.Count == 0 || currentColor != spawnEvents.Peek().color)
+            public Vector3 color;
+            public float count;
+        }
+        SpawnEvent[] m_SpawnEvents;
+
+        void Start()
+        {
+            var pixels = m_SourceTexture.GetPixels();
+            var spawnEvents = new Stack<SpawnEvent>();
+            foreach (var pixel in pixels)
             {
-                spawnEvents.Push(new SpawnEvent()
+                var currentColor = new Vector3(pixel.r, pixel.g, pixel.b);
+                if (spawnEvents.Count == 0 || currentColor != spawnEvents.Peek().color)
                 {
-                    color = currentColor,
-                    count = 0
-                });
+                    spawnEvents.Push(new SpawnEvent()
+                    {
+                        color = currentColor,
+                        count = 0
+                    });
+                }
+
+                var entry = spawnEvents.Pop();
+                entry.count += 1.0f;
+                spawnEvents.Push(entry);
             }
 
-            var entry = spawnEvents.Pop();
-            entry.count += 1.0f;
-            spawnEvents.Push(entry);
+            m_SpawnEvents = spawnEvents.ToArray();
         }
 
-        m_SpawnEvents = spawnEvents.ToArray();
-    }
+        static readonly float s_WaitTime = 0.5f;
+        float m_WaitingTime = s_WaitTime;
+        uint m_ReadCursor;
 
-    static readonly float s_WaitTime = 0.5f;
-    float m_WaitingTime = s_WaitTime;
-    uint m_ReadCursor;
+        static readonly int s_SpawnCountID = Shader.PropertyToID("spawnCount");
+        static readonly int s_ColorID = Shader.PropertyToID("color");
+        static readonly int s_FireID = Shader.PropertyToID("fire");
 
-    static readonly int s_SpawnCountID = Shader.PropertyToID("spawnCount");
-    static readonly int s_ColorID = Shader.PropertyToID("color");
-    static readonly int s_FireID = Shader.PropertyToID("fire");
-
-    void Update()
-    {
-        m_WaitingTime -= Time.deltaTime;
-        if (m_WaitingTime < 0)
+        void Update()
         {
-            m_WaitingTime = s_WaitTime;
-
-            var vfx = gameObject.GetComponent<VisualEffect>();
-            var vfxEventAttribute = vfx.CreateVFXEventAttribute();
-            int spawnEventsToSend = m_SpawnEvents.Length / 20;
-            for (int i = 0; i < spawnEventsToSend; i++)
+            m_WaitingTime -= Time.deltaTime;
+            if (m_WaitingTime < 0)
             {
-                var spawnEvent = m_SpawnEvents[m_ReadCursor];
+                m_WaitingTime = s_WaitTime;
 
-                vfxEventAttribute.SetFloat(s_SpawnCountID, spawnEvent.count);
-                vfxEventAttribute.SetVector3(s_ColorID, spawnEvent.color);
-                vfx.SendEvent(s_FireID, vfxEventAttribute);
+                var vfx = gameObject.GetComponent<VisualEffect>();
+                var vfxEventAttribute = vfx.CreateVFXEventAttribute();
+                int spawnEventsToSend = m_SpawnEvents.Length / 20;
+                for (int i = 0; i < spawnEventsToSend; i++)
+                {
+                    var spawnEvent = m_SpawnEvents[m_ReadCursor];
 
-                m_ReadCursor++;
-                if (m_ReadCursor >= m_SpawnEvents.Length)
-                    m_ReadCursor = 0u;
+                    vfxEventAttribute.SetFloat(s_SpawnCountID, spawnEvent.count);
+                    vfxEventAttribute.SetVector3(s_ColorID, spawnEvent.color);
+                    vfx.SendEvent(s_FireID, vfxEventAttribute);
+
+                    m_ReadCursor++;
+                    if (m_ReadCursor >= m_SpawnEvents.Length)
+                        m_ReadCursor = 0u;
+                }
             }
         }
     }
