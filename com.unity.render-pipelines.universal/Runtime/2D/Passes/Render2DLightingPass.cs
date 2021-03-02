340a65402afa207b1f6eea2b9bbe5300f4f16be5
Paul Tham <paul@unity3d.com>
2021-03-02 14:08:10 +0800

    Add keyword to disable 2d lighting in custom lit graph while in sceneview

--- com.unity.render-pipelines.universal/Runtime/2D/Passes/Render2DLightingPass.cs
+++ com.unity.render-pipelines.universal/Runtime/2D/Passes/Render2DLightingPass.cs
@@ -285,6 +285,7 @@ namespace UnityEngine.Experimental.Rendering.Universal
                 for (var i = 0; i < batchCount; i += batchesDrawn)
                     batchesDrawn = DrawLayerBatches(layerBatches, batchCount, i, cmd, context, ref renderingData, ref filterSettings, ref normalsDrawSettings, ref combinedDrawSettings, ref desc);
 
+                this.DisableAllKeywords(cmd);
                 this.ReleaseRenderTextures(cmd);
                 context.ExecuteCommandBuffer(cmd);
                 CommandBufferPool.Release(cmd);
@@ -310,6 +311,7 @@ namespace UnityEngine.Experimental.Rendering.Universal
                     }
                 }
 
+                this.DisableAllKeywords(cmd);
                 context.ExecuteCommandBuffer(cmd);
                 CommandBufferPool.Release(cmd);
 
