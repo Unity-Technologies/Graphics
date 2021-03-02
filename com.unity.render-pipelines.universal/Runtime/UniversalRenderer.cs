d30837a914a1b2f8edbf0a6ecc738f1ecda04203
Erik Hakala <erik.hakala@unity3d.com>
2021-03-02 18:32:52 +0200

    Integrate core blitter. Add core blit shaders.

--- com.unity.render-pipelines.universal/Runtime/UniversalRenderer.cs
+++ com.unity.render-pipelines.universal/Runtime/UniversalRenderer.cs
@@ -101,6 +101,9 @@ namespace UnityEngine.Rendering.Universal
 #if ENABLE_VR && ENABLE_XR_MODULE
             UniversalRenderPipeline.m_XRSystem.InitializeXRSystemData(data.xrSystemData);
 #endif
+            // TODO: should merge shaders with HDRP into core, XR dependency for now.
+            // TODO: replace/merge URP blit into core blitter.
+            Blitter.Initialize(data.shaders.coreBlitPS, data.shaders.coreBlitColorAndDepthPS);
 
             m_BlitMaterial = CoreUtils.CreateEngineMaterial(data.shaders.blitPS);
             m_CopyDepthMaterial = CoreUtils.CreateEngineMaterial(data.shaders.copyDepthPS);
@@ -244,6 +247,8 @@ namespace UnityEngine.Rendering.Universal
             CoreUtils.Destroy(m_TileDepthInfoMaterial);
             CoreUtils.Destroy(m_TileDeferredMaterial);
             CoreUtils.Destroy(m_StencilDeferredMaterial);
+
+            Blitter.Cleanup();
         }
 
         /// <inheritdoc />
