662d500a1d21aaee12fdaa00cc6283261450606b
sebastienlagarde <sebastien@unity3d.com>
2021-03-04 00:03:40 +0100

    Merge Hd/bugfix #3599 (#3751)

--- com.unity.render-pipelines.high-definition/Runtime/Material/Decal/DecalData.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Material/Decal/DecalData.hlsl
@@ -24,7 +24,7 @@ void GetSurfaceData(FragInputs input, float3 V, PositionInputs posInput, float a
 #endif
 
     float albedoMapBlend = fadeFactor;
-    float maskMapBlend = fadeFactor;
+    float maskMapBlend = _DecalMaskMapBlueScale * fadeFactor;
 
     ZERO_INITIALIZE(DecalSurfaceData, surfaceData);
 
@@ -57,16 +57,12 @@ void GetSurfaceData(FragInputs input, float3 V, PositionInputs posInput, float a
 #ifdef _MATERIAL_AFFECTS_MASKMAP
     #ifdef _MASKMAP
     surfaceData.mask = SAMPLE_TEXTURE2D(_MaskMap, sampler_MaskMap, texCoords);
-    surfaceData.mask.z *= _DecalMaskMapBlueScale;
-    maskMapBlend *= surfaceData.mask.z; // store before overwriting with smoothness
     #ifdef DECALS_4RT
     surfaceData.mask.x = lerp(_MetallicRemapMin, _MetallicRemapMax, surfaceData.mask.x);
     surfaceData.mask.y = lerp(_AORemapMin, _AORemapMax, surfaceData.mask.y);
     #endif
     surfaceData.mask.z = lerp(_SmoothnessRemapMin, _SmoothnessRemapMax, surfaceData.mask.w);
     #else
-    surfaceData.mask.z = _DecalMaskMapBlueScale;
-    maskMapBlend *= surfaceData.mask.z; // store before overwriting with smoothness
     #ifdef DECALS_4RT
     surfaceData.mask.x = _Metallic;
     surfaceData.mask.y = _AO;
