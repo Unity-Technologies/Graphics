77f0227a4db9c9be3e9abcf51f2c3e2b97d3ba6a
jfryer_unity <julienf@unity3D.com>
2021-03-03 11:32:43 +0100

    Add VFXMaterialSerializedSettings

--- com.unity.visualeffectgraph/Editor/Models/Contexts/IVFXSubRenderer.cs
+++ com.unity.visualeffectgraph/Editor/Models/Contexts/IVFXSubRenderer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace UnityEditor.VFX
@@ -12,5 +13,52 @@ namespace UnityEditor.VFX
 
         // Allow to setup material generated during import
         void SetupMaterial(Material material);
+    }
+
+    [Serializable]
+    class VFXMaterialSerializedSettings
+    {
+        [Serializable]
+        private struct FloatProperty
+        {
+            public string name;
+            public float value;
+        }
+
+        [SerializeField]
+        private List<FloatProperty> properties = new List<FloatProperty>();
+
+        public static VFXMaterialSerializedSettings CreateFromMaterial(Material material)
+        {
+            VFXMaterialSerializedSettings settings = new VFXMaterialSerializedSettings();
+            settings.SyncFromMaterial(material);
+            return settings;
+        }
+
+        public void SyncFromMaterial(Material material)
+        {
+            properties.Clear();
+
+            var matProperties = ShaderUtil.GetMaterialProperties(new UnityEngine.Object[] { material });
+
+            foreach (var p in matProperties)
+            {
+                // TODO we probably want to also filter out potential properties coming from compilation? (No compilation output data should be serialized)
+                if (p.type != MaterialProperty.PropType.Float)
+                    continue;
+
+                properties.Add(new FloatProperty()
+                {
+                    name = p.name,
+                    value = p.floatValue
+                });
+            }
+        }
+
+        public void ApplyToMaterial(Material material)
+        {
+            foreach (var p in properties)
+                material.SetFloat(p.name, p.value);
+        }
     }
 }
