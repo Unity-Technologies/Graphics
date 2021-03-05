5ec86e67ba1b01487627ff42eb459a2c59eee7da
Kay Chang <kaychang@unity3d.com>
2021-03-05 13:27:35 -0800

    Made floating point fixes permanent to all platforms.

--- com.unity.render-pipelines.core/ShaderLibrary/Color.hlsl
+++ com.unity.render-pipelines.core/ShaderLibrary/Color.hlsl
@@ -254,11 +254,7 @@ real YCoCgCheckBoardEdgeFilter(real centerLum, real2 a0, real2 a1, real2 a2, rea
 }
 
 // Converts linear RGB to LMS
-#if defined(SHADER_API_SWITCH) // Must use full float precision on Switch to avoid artefact when using ACES tonemapping
-float3 LinearToLMS(float3 x)
-#else
-real3 LinearToLMS(real3 x)
-#endif
+float3 LinearToLMS(float3 x) // Full float precision to avoid precision artefact when using ACES tonemapping
 {
     const real3x3 LIN_2_LMS_MAT = {
         3.90405e-1, 5.49941e-1, 8.92632e-3,
@@ -269,11 +265,7 @@ real3 LinearToLMS(real3 x)
     return mul(LIN_2_LMS_MAT, x);
 }
 
-#if defined(SHADER_API_SWITCH) // Must use full float precision on Switch to avoid artefact when using ACES tonemapping
-float3 LMSToLinear(float3 x)
-#else
-real3 LMSToLinear(real3 x)
-#endif
+float3 LMSToLinear(float3 x) // Full float precision to avoid precision artefact when using ACES tonemapping
 {
     const real3x3 LMS_2_LIN_MAT = {
         2.85847e+0, -1.62879e+0, -2.48910e-2,
@@ -403,11 +395,7 @@ real LinearToLogC_Precise(real x)
     return o;
 }
 
-#if defined(SHADER_API_SWITCH) // Must use full float precision on Switch to avoid artefact when using ACES tonemapping
-float3 LinearToLogC(float3 x)
-#else
-real3 LinearToLogC(real3 x)
-#endif
+float3 LinearToLogC(float3 x) // Full float precision to avoid precision artefact when using ACES tonemapping
 {
 #if USE_PRECISE_LOGC
     return real3(
@@ -420,11 +408,7 @@ real3 LinearToLogC(real3 x)
 #endif
 }
 
-#if defined(SHADER_API_SWITCH) // Must use full float precision on Switch to avoid artefact when using ACES tonemapping
-float3 LogCToLinear(float3 x)
-#else
 real LogCToLinear_Precise(real x)
-#endif
 {
     real o;
     if (x > LogC.e * LogC.cut + LogC.f)
@@ -434,7 +418,7 @@ real LogCToLinear_Precise(real x)
     return o;
 }
 
-real3 LogCToLinear(real3 x)
+float3 LogCToLinear(float3 x) // Full float precision to avoid precision artefact when using ACES tonemapping
 {
 #if USE_PRECISE_LOGC
     return real3(
