6cf0c4b0819f8a7b527e43998a3e3b2529a16336
martint-unity <martint@unity3d.com>
2021-03-09 16:59:23 +0100

    Updated styling and added new converter item

--- com.unity.render-pipelines.universal/Editor/Converter.cs
+++ com.unity.render-pipelines.universal/Editor/Converter.cs
@@ -1,11 +1,31 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 // Might need to change this name before making it public
 public abstract class CoreConverter
 {
+    /// <summary>
+    /// A structure holding the information for each Item that needs to be Converted.
+    /// </summary>
+    public struct ConverterItemInfo
+    {
+        public string Name;
+        public string Path;
+        public string InitialInfo;
+        public string HelpLink;
+    }
+
     public abstract string Name { get; }
     public abstract string Info { get; }
 
+    /// <summary>
+    /// The method that will be run when converting the assets.
+    /// </summary>
     public abstract void Convert();
+
+    /// <summary>
+    /// The method will run when the Converter UI starts to populate some info regarding assets that needs converted.
+    /// </summary>
+    public abstract List<ConverterItemInfo> Initialize();
 }
--- com.unity.render-pipelines.universal/Editor/URPConvertersEditor.cs
+++ com.unity.render-pipelines.universal/Editor/URPConvertersEditor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.UIElements;
@@ -7,8 +8,11 @@ public class URPConvertersEditor : EditorWindow
 {
     public VisualTreeAsset converterEditorAsset;
     public VisualTreeAsset converterListAsset;
-    ListView listView;
+    public VisualTreeAsset converterItem;
+
+    ScrollView scrollView;
     TypeCache.TypeCollection converters;
+    List<CoreConverter.ConverterItemInfo> ConverterItemInfos;
 
     [MenuItem("URPConverter/URPConverter")]
     public static void ShowWindow()
@@ -19,18 +23,66 @@ public class URPConvertersEditor : EditorWindow
 
     public void CreateGUI()
     {
+        // Getting all the converters
         converters = TypeCache.GetTypesDerivedFrom<CoreConverter>();
         converterEditorAsset.CloneTree(rootVisualElement);
-        listView = rootVisualElement.Q<ListView>("convertersListView");
-        listView.itemsSource = converters;
-        listView.makeItem = converterListAsset.CloneTree;
-        listView.bindItem = BindItem;
+
+        // Getting the scrollview where the converters should be added
+        scrollView = rootVisualElement.Q<ScrollView>("convertersScrollView");
+        for (int i = 0; i < converters.Count; ++i)
+        {
+            // Iterate over the converters
+            CoreConverter conv = (CoreConverter)Activator.CreateInstance(converters[i]);
+
+            // Making an item using the converterListAsset as a template.
+            // Then adding the information needed for each converter
+            VisualElement item = new VisualElement();
+            converterListAsset.CloneTree(item);
+            item.Q<Label>("converterName").text = conv.Name;
+            item.Q<VisualElement>("converterTopVisualElement").tooltip = conv.Info;
+
+            // Get the ListView for the converter items
+            ListView listView = item.Q<ListView>("converterItems");
+
+
+            // Need to get the assets that this converter is converting.
+            // Need to return Name, Path, Initial info, Help link.
+            ConverterItemInfos = conv.Initialize();
+
+            listView.itemsSource = ConverterItemInfos;
+            listView.makeItem = converterItem.CloneTree;
+            listView.bindItem = BindItem;
+
+            // // Iterate over all the items for the converter and populate the UI
+            // for (int k = 0; k < ConverterItemInfos.Count; ++k)
+            // {
+            //     //VisualElement convItem = new VisualElement();
+            //     //converterItem.CloneTree(convItem);
+            //
+            //     convItem.Q<Label>("converterItemName").text = ConverterItemInfos[i].Name;
+            //     convItem.Q<Label>("converterItemPath").text = ConverterItemInfos[i].Path;
+            //     listView.hierarchy.Add(convItem);
+            // }
+
+            scrollView.Add(item);
+        }
+
+        // converters = TypeCache.GetTypesDerivedFrom<CoreConverter>();
+        // converterEditorAsset.CloneTree(rootVisualElement);
+        // listView = rootVisualElement.Q<ListView>("convertersListView");
+        // listView.itemsSource = converters;
+        // listView.makeItem = converterListAsset.CloneTree;
+        // listView.bindItem = BindItem;
+
+        //listView.itemsSource = converters;
+        //listView.makeItem = converterListAsset.CloneTree;
+        //listView.bindItem = BindItem;
     }
 
     void BindItem(VisualElement item, int index)
     {
-        CoreConverter conv = (CoreConverter)Activator.CreateInstance(converters[index]);
-        item.Q<Label>("converterName").text = conv.Name;
-        item.Q<Label>("converterInfo").text = conv.Info;
+        CoreConverter.ConverterItemInfo convItem = ConverterItemInfos[index];
+        item.Q<Label>("converterItemName").text = convItem.Name;
+        item.Q<Label>("converterItemPath").text = convItem.Path;
     }
 }
