f9e34fbcd42c8c42cb64016c1c3194dab0bc49fd
Antoine Lelievre <antoinel@unity3d.com>
2021-03-15 15:35:27 +0100

    Fixed tube light mode selection (case 1317776)

--- com.unity.render-pipelines.high-definition/Editor/Lighting/HDLightUI.cs
+++ com.unity.render-pipelines.high-definition/Editor/Lighting/HDLightUI.cs
@@ -208,8 +208,14 @@ namespace UnityEditor.Rendering.HighDefinition
             }
             EditorGUI.showMixedValue = false;
 
-            //Draw the mode
-            serialized.settings.DrawLightmapping();
+            // Draw the mode, for Tube and Disc lights, there is only one choice, so we can disable the enum.
+            if (serialized.areaLightShape == AreaLightShape.Tube || serialized.areaLightShape == AreaLightShape.Disc)
+            {
+                using (new EditorGUI.DisabledScope(true))
+                    serialized.settings.DrawLightmapping();
+            }
+            else
+                serialized.settings.DrawLightmapping();
 
             if (updatedLightType == HDLightType.Area)
             {
