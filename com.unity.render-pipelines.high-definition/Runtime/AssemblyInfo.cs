86226d034c62d0c418c5cfcc31c5326b5a83eb31
slunity <stephanel@unity3d.com>
2021-03-17 21:28:55 -0400

    HDRP and SG API access for our plugin material.

--- com.unity.render-pipelines.high-definition/Runtime/AssemblyInfo.cs
+++ com.unity.render-pipelines.high-definition/Runtime/AssemblyInfo.cs
@@ -4,3 +4,5 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Editor.Tests")]
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Runtime.Tests")]
 [assembly: InternalsVisibleTo("HDRP_TestRunner")]
+[assembly: InternalsVisibleTo("Unity.Industrial.Materials.AVRD.Runtime")]
+[assembly: InternalsVisibleTo("Unity.Industrial.Materials.AVRD.Editor")]
