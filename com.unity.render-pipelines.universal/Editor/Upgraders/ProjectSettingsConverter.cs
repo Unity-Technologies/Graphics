ae7efceb1b99b652755341fd999eaa14cdaff507
Andrem <andrem@unity3d.com>
2021-03-25 18:26:39 +0100

    Added lots of pesudo code

--- com.unity.render-pipelines.universal/Editor/Upgraders/ProjectSettingsConverter.cs
+++ com.unity.render-pipelines.universal/Editor/Upgraders/ProjectSettingsConverter.cs
@@ -1,8 +1,14 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEditor;
 using UnityEditor.Rendering;
 using UnityEditor.Rendering.Universal;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
 
 public class ProjectSettingsConverter : RenderPipelineConverter
 {
@@ -12,15 +18,29 @@ public class ProjectSettingsConverter : RenderPipelineConverter
         "This converter will look at creating Universal Render Pipeline assets and respective renderers and set their " +
         "settings based on equivalent settings from builtin renderer.";
     public override Type conversion => typeof(BuiltInToURPConversion);
+
+    private List<ProjectSettingItem> settingsItems = new List<ProjectSettingItem>();
+    private List<CameraSettings> cameraSettings = new List<CameraSettings>();
+
+    private bool needsForward = false;
+    private bool needsDeferred = false;
+
     public override void OnInitialize(InitializeConverterContext context)
     {
+
+        //checking if deferred or forward
+        // check graphics tiers
+        GatherGraphicsTiers();
+
+        // find all cameras and thier info
+        GatherCameras(context);
+
         var id = 0;
         foreach (var levelName in QualitySettings.names)
         {
             var setting = QualitySettings.GetRenderPipelineAssetAt(id);
-            var item = new ConverterItemInfo();
-            item.id = id;
-            item.name = levelName;
+            var item = new ConverterItemDescriptor();
+            item.name = $"Quality Level {id}:{levelName}";
 
             var text = "";
             if (setting != null)
@@ -39,7 +59,6 @@ public class ProjectSettingsConverter : RenderPipelineConverter
                 text = "Will Generate Pipeline Asset";
             }
 
-
             item.path = text;
 
             context.AddAssetToConvert(item);
@@ -52,8 +71,151 @@ public class ProjectSettingsConverter : RenderPipelineConverter
         foreach (var item in context.items)
         {
             // which one am I?
-            Debug.Log(item);
-            UniversalRenderPipelineAsset.
+            Debug.Log($"Starting upgrade of Quality Level {item.index}:{item.descriptor.name}");
+
+            //creating pipeline asset
+
+            //create renderers
+            if (needsForward)
+            {
+                Debug.Log($"Generating a forward renderer");
+            }
+
+            if (needsDeferred)
+            {
+                Debug.Log("Generating a deferred renderer");
+            }
+
+            //looping through all settings
         }
+    }
+
+    public override void OnClicked(int index)
+    {
+        if (index > cameraSettings.Count - 1)
+        {
+            // in quality levels
+            SettingsService.OpenProjectSettings("Project/Quality");
+        }
+        else
+        {
+            var obj = AssetDatabase.LoadAssetAtPath<Object>(cameraSettings[index].assetPath);
+
+            if(obj)
+                EditorGUIUtility.PingObject(obj);
+        }
+    }
+
+    internal void GatherGraphicsTiers()
+    {
+
+    }
+
+    internal void GatherCameras(InitializeConverterContext context)
+    {
+        var cameras = Object.FindObjectsOfType<Camera>();
+        // final version will take some time so...
+        Thread.Sleep(100);
+
+        foreach (var camera in cameras)
+        {
+            CameraSettings camSettings = null;
+
+            switch (camera.renderingPath)
+            {
+                case RenderingPath.UsePlayerSettings:
+                    Debug.Log($"Camera {camera.name} not needed, is {camera.renderingPath}");
+                    break;
+                case RenderingPath.VertexLit:
+                    Debug.Log($"Camera {camera.name} Render path {camera.renderingPath} is not supported.");
+                    break;
+                case RenderingPath.Forward:
+                    camSettings = new CameraSettings();
+                    CaptureCamera(camera, ref camSettings);
+                    break;
+                case RenderingPath.DeferredLighting:
+                    camSettings = new CameraSettings();
+                    CaptureCamera(camera, ref camSettings);
+                    break;
+                case RenderingPath.DeferredShading:
+                    camSettings = new CameraSettings();
+                    CaptureCamera(camera, ref camSettings);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            if (camSettings == null)
+                return;
+
+            cameraSettings.Add(camSettings);
+
+            ConverterItemDescriptor info = new ConverterItemDescriptor()
+            {
+                name = $"Camera:{camera.name} > {camSettings.renderingPath}",
+                path = camSettings.assetPath,
+                initialInfo = "",
+            };
+            context.AddAssetToConvert(info);
+        }
+    }
+
+    internal void CaptureCamera(Camera camera, ref CameraSettings camSettings)
+    {
+        camSettings.goName = camera.name;
+        camSettings.objectID = camera.GetInstanceID();
+        //render path
+        camSettings.renderingPath = camera.renderingPath;
+
+        if (PrefabUtility.IsPartOfAnyPrefab(camera))
+        {
+            camSettings.assetPath = PrefabUtility.GetPrefabAssetPathOfNearestInstanceRoot(camera);
+
+            if (PrefabUtility.IsPartOfVariantPrefab(camera))
+            {
+                camSettings.parent = Parent.PrefabVariant;
+                var overrides = PrefabUtility.GetObjectOverrides(camera.gameObject);
+                foreach (var objectOverride in overrides)
+                {
+                    // figure out if the rendering mode has been overridden
+                }
+            }
+            else
+            {
+                camSettings.parent = Parent.Prefab;
+            }
+        }
+        else
+        {
+            camSettings.parent = Parent.Scene;
+            camSettings.assetPath = camera.gameObject.scene.path;
+        }
+        //save path to object if !use graphics settings && not same as graphics tier
+        Debug.Log($"Camera {camSettings.objectID}:{camSettings.goName} " +
+                  $"is using {camSettings.renderingPath}, " +
+                  $"it lives in a {camSettings.parent} at {camSettings.assetPath}.");
+    }
+
+    internal class ProjectSettingItem
+    {
+        public int index;
+        public string levelName;
+        public UniversalRenderPipelineAsset URPAsset;
+    }
+
+    internal class CameraSettings
+    {
+        public string goName;
+        public int objectID;
+        public Parent parent;
+        public string assetPath;
+        public RenderingPath renderingPath;
+    }
+
+    internal enum Parent
+    {
+        Prefab,
+        PrefabVariant,
+        Scene,
     }
 }
