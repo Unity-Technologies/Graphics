6ef98521318b50b624a94b8f1808a99bbe6aedbc
Joshua Davis <joshua.davis@unity3d.com>
2021-03-26 09:43:33 -0700

    Removed the legacy cginc files as they're no longer needed

--- com.unity.shadergraph/Editor/Generation/Targets/BuiltIn/ShaderLibrary/Shim/Shims.hlsl
+++ com.unity.shadergraph/Editor/Generation/Targets/BuiltIn/ShaderLibrary/Shim/Shims.hlsl
@@ -14,12 +14,12 @@
 #include "InputsShim.hlsl"
 #include "SurfaceShaderProxy.hlsl"
 
-#include "Packages/com.unity.shadergraph/Editor/Generation/Targets/BuiltIn/ShaderLibrary/Legacy/UnityShaderVariables.cginc"
-#include "Packages/com.unity.shadergraph/Editor/Generation/Targets/BuiltIn/ShaderLibrary/Legacy/UnityShaderUtilities.cginc"
-#include "Packages/com.unity.shadergraph/Editor/Generation/Targets/BuiltIn/ShaderLibrary/Legacy/UnityCG.cginc"
-#include "Packages/com.unity.shadergraph/Editor/Generation/Targets/BuiltIn/ShaderLibrary/Legacy/Lighting.cginc"
-#include "Packages/com.unity.shadergraph/Editor/Generation/Targets/BuiltIn/ShaderLibrary/Legacy/UnityPBSLighting.cginc"
-#include "Packages/com.unity.shadergraph/Editor/Generation/Targets/BuiltIn/ShaderLibrary/Legacy/AutoLight.cginc"
+#include "UnityShaderVariables.cginc"
+#include "UnityShaderUtilities.cginc"
+#include "UnityCG.cginc"
+#include "Lighting.cginc"
+#include "UnityPBSLighting.cginc"
+#include "AutoLight.cginc"
 
 
 #ifdef POINT
