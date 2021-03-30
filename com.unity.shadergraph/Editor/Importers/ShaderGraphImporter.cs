db949b55138a5432c6a5d73dcb559036813f95a3
pastasfuture <pastasfuture@gmail.com>
2021-03-29 19:08:34 -0700

    bugfix to DotsDeformation.hlsl::FetchComputeVertexData() to tangentOS.w in tangentOS output so mirrored normals produce correct normal maps

--- com.unity.shadergraph/Editor/Importers/ShaderGraphImporter.cs
+++ com.unity.shadergraph/Editor/Importers/ShaderGraphImporter.cs
@@ -29,11 +29,12 @@ namespace UnityEditor.ShaderGraph
     // + 1 Bump ShaderImporter version after Return positionDS from ObjectSpacePosition in Decal Shader Graphs
     // + 1 Bump ShaderImporter version after stripping LIGHTMAP_ON variants from shadergraph codegen in 9.x.x/custom/cherry-pick.
     // + 1 Bump ShaderImporter version after _DissolveOnOcclusionOpacity parameter needed HLSLDeclaration.UnityPerMaterial declaration, changing codegen in 10.2.0.
+    // + 1 Bump ShaderImporter version after bugfix to DotsDeformation.hlsl::FetchComputeVertexData() to tangentOS.w in tangentOS output so mirrored normals produce correct normal maps.
     // [ScriptedImporter(111, Extension, -902)]
-    [ScriptedImporter(115, Extension, -902)]
+    [ScriptedImporter(116, Extension, -902)]
 #else
     // [ScriptedImporter(43, Extension, -902)]
-    [ScriptedImporter(47, Extension, -902)]
+    [ScriptedImporter(48, Extension, -902)]
     // custom-end
 #endif
 
