2df8d2403240bab47a6f1e601ef551e8217fb498
Paul Demeulenaere <pauld@unity3d.com>
2021-03-31 15:38:04 +0200

    Remove struct DummyTest
    (cherry picked from commit 3ac5fdeac295e7c19d32dc74e62c11cd8ac8de54)

--- com.unity.visualeffectgraph/Editor/Models/Operators/Implementations/SampleBuffer.cs
+++ com.unity.visualeffectgraph/Editor/Models/Operators/Implementations/SampleBuffer.cs
@@ -5,16 +5,30 @@ using UnityEngine.VFX;
 
 namespace UnityEditor.VFX.Operator
 {
-    [VFXType]
-    struct DummyTest
-    {
-        public Vector3 position;
-        public Vector3 color;
-    }
-
     [VFXInfo(category = "Sampling")]
-    class SampleBuffer : VFXOperator
+    class SampleBuffer : VFXOperatorDynamicType
     {
+        public override IEnumerable<int> staticSlotIndex
+        {
+            get
+            {
+                yield return 0;
+                yield return 1;
+            }
+        }
+
+        public override IEnumerable<Type> validTypes
+        {
+            get
+            {
+                //TODOPAUL : filter only ones declared with a flag
+                return VFXLibrary.GetSlotsType();
+            }
+        }
+
+
+        protected override Type defaultValueType => null;
+
         override public string name { get { return "Sample Graphics Buffer"; } }
 
         public class InputProperties
@@ -25,25 +39,26 @@ namespace UnityEditor.VFX.Operator
             public uint index;
         }
 
-
         protected override IEnumerable<VFXPropertyWithValue> outputProperties
         {
             get
             {
-                yield return new VFXPropertyWithValue(new VFXProperty(typeof(DummyTest), "s"));
+                if (m_Type != null)
+                    yield return new VFXPropertyWithValue(new VFXProperty(m_Type, "s"));
             }
         }
-
         protected override sealed VFXExpression[] BuildExpression(VFXExpression[] inputExpression)
         {
+            if (GetNbOutputSlots() == 0)
+                return new VFXExpression[] {};
+
             var outputSlot = GetOutputSlot(0);
             var slots = outputSlot.GetVFXValueTypeSlots();
 
-            var type = typeof(DummyTest);
             var expressions = new List<VFXExpression>();
             foreach (var slot in slots)
             {
-                var current = new VFXExpressionSampleBuffer(type, slot.valueType, slot.name, inputExpression[0], inputExpression[1]);
+                var current = new VFXExpressionSampleBuffer(m_Type, slot.valueType, slot.name, inputExpression[0], inputExpression[1]);
                 expressions.Add(current);
             }
             return expressions.ToArray();
