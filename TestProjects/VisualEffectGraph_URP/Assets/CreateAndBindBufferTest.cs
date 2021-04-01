00547c6f316c672f55f18d6c8f732d72d4c6f9f3
Paul Demeulenaere <pauld@unity3d.com>
2021-04-01 16:45:12 +0200

    Add test data with structure in a structure
    (cherry picked from commit 7669a7a6d00d068730c79a745b59bdfd8a967f9a)

--- TestProjects/VisualEffectGraph_URP/Assets/CreateAndBindBufferTest.cs
+++ TestProjects/VisualEffectGraph_URP/Assets/CreateAndBindBufferTest.cs
@@ -27,20 +27,21 @@ public class CreateAndBindBufferTest : MonoBehaviour
     }
 
     [UnityEditor.VFX.VFXType(UnityEditor.VFX.VFXTypeAttribute.Flags.GraphicsBuffer), StructLayout(LayoutKind.Sequential)]
-    struct CustomDataBis
+    public struct Rectangle
     {
-        [UnityEditor.VFX.VFXType(UnityEditor.VFX.VFXTypeAttribute.Flags.GraphicsBuffer), StructLayout(LayoutKind.Sequential)]
-        public struct Rectangle
-        {
-            public Vector2 size;
-            public Vector3 color;
-        }
-        public Rectangle rectangle;
-        public Vector3 position;
+        public Vector2 size;
         public Vector3 color;
     }
 
+    [UnityEditor.VFX.VFXType(UnityEditor.VFX.VFXTypeAttribute.Flags.GraphicsBuffer), StructLayout(LayoutKind.Sequential)]
+    struct CustomDataBis
+    {
+        public Rectangle rectangle;
+        public Vector3 position;
+    }
+
     private GraphicsBuffer m_buffer;
+    private GraphicsBuffer m_buffer2;
     void Update()
     {
         if (m_buffer == null)
@@ -55,8 +56,23 @@ public class CreateAndBindBufferTest : MonoBehaviour
             m_buffer.SetData(data);
         }
 
+        if (m_buffer2 == null)
+        {
+            m_buffer2 = new GraphicsBuffer(GraphicsBuffer.Target.Structured, 3, Marshal.SizeOf(typeof(CustomDataBis)));
+            var data = new List<CustomDataBis>()
+            {
+                new CustomDataBis() { position = new Vector3(0, 0, 1), rectangle = new Rectangle() { color = new Vector3(1, 0, 0), size = new Vector2(1, 1) }},
+                new CustomDataBis() { position = new Vector3(1, 0, 1), rectangle = new Rectangle() { color = new Vector3(0, 1, 0), size = new Vector2(1, 2) }},
+                new CustomDataBis() { position = new Vector3(2, 0, 1), rectangle = new Rectangle() { color = new Vector3(0, 0, 1), size = new Vector2(1, 3) } },
+            };
+            m_buffer2.SetData(data);
+        }
+
         var vfx = GetComponent<VisualEffect>();
         if (vfx.GetGraphicsBuffer("buffer") == null)
             vfx.SetGraphicsBuffer("buffer", m_buffer);
+
+        if (vfx.GetGraphicsBuffer("buffer2") == null)
+            vfx.SetGraphicsBuffer("buffer2", m_buffer2);
     }
 }
