1336ae2cb69b79fd711a7c2fff65b0637d1a1947
Seongdae <seongdae@unity3d.com>
2021-04-09 09:48:34 +0900

    Add ScreenSpaceShadowsEditor

---
+++ com.unity.render-pipelines.universal/Editor/RendererFeatures/ScreenSpaceShadowsEditor.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace UnityEditor.Rendering.Universal
+{
+    [CustomEditor(typeof(ScreenSpaceShadows))]
+    internal class ScreenSpaceShadowsEditor : Editor
+    {
+        #region Serialized Properties
+        #endregion
+
+        public override void OnInspectorGUI()
+        {
+            
+        }
+    }
+}
