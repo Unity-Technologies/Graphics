2af3aff62924be8e188f53576f27977e0006615c
lukasc <lukasc@unity3d.com>
2021-04-12 18:03:03 +0300

    Cleaning up normal reconstruction

--- com.unity.render-pipelines.universal/Runtime/NormalReconstruction.cs
+++ com.unity.render-pipelines.universal/Runtime/NormalReconstruction.cs
@@ -5,20 +5,8 @@ namespace UnityEngine.Rendering.Universal
     /// </summary>
     public static class NormalReconstruction
     {
-        private static readonly int s_ProjectionParams2ID = Shader.PropertyToID("_ProjectionParams2");
-        private static readonly int s_CameraViewProjectionsID = Shader.PropertyToID("_CameraViewProjections");
-        private static readonly int s_CameraViewTopLeftCornerID = Shader.PropertyToID("_CameraViewTopLeftCorner");
-        private static readonly int s_CameraViewXExtentID = Shader.PropertyToID("_CameraViewXExtent");
-        private static readonly int s_CameraViewYExtentID = Shader.PropertyToID("_CameraViewYExtent");
-        private static readonly int s_CameraViewZExtentID = Shader.PropertyToID("_CameraViewZExtent");
-
-        private const string k_OrthographicCameraKeyword = "_ORTHOGRAPHIC";
-
-        private static Matrix4x4[] s_CameraViewProjections = new Matrix4x4[2];
-        private static Vector4[] s_CameraTopLeftCorner = new Vector4[2];
-        private static Vector4[] s_CameraXExtent = new Vector4[2];
-        private static Vector4[] s_CameraYExtent = new Vector4[2];
-        private static Vector4[] s_CameraZExtent = new Vector4[2];
+        private static readonly int s_NormalReconstructionMatrixID = Shader.PropertyToID("_NormalReconstructionMatrix");
+        private static Matrix4x4[] s_NormalReconstructionMatrix = new Matrix4x4[2];
 
         /// <summary>
         /// Setup properties needed for normal reconstruction from depth using shader functions in NormalReconstruction.hlsl
@@ -36,7 +24,7 @@ namespace UnityEngine.Rendering.Universal
             {
                 Matrix4x4 view = cameraData.GetViewMatrix(eyeIndex);
                 Matrix4x4 proj = cameraData.GetProjectionMatrix(eyeIndex);
-                s_CameraViewProjections[eyeIndex] = proj * view;
+                s_NormalReconstructionMatrix[eyeIndex] = proj * view;
 
                 // camera view space without translation, used by SSAO.hlsl ReconstructViewPos() to calculate view vector.
                 Matrix4x4 cview = view;
@@ -44,29 +32,11 @@ namespace UnityEngine.Rendering.Universal
                 Matrix4x4 cviewProj = proj * cview;
                 Matrix4x4 cviewProjInv = cviewProj.inverse;
 
-                Vector4 topLeftCorner = cviewProjInv.MultiplyPoint(new Vector4(-1, 1, -1, 1));
-                Vector4 topRightCorner = cviewProjInv.MultiplyPoint(new Vector4(1, 1, -1, 1));
-                Vector4 bottomLeftCorner = cviewProjInv.MultiplyPoint(new Vector4(-1, -1, -1, 1));
-                Vector4 farCentre = cviewProjInv.MultiplyPoint(new Vector4(0, 0, 1, 1));
-                s_CameraTopLeftCorner[eyeIndex] = topLeftCorner;
-                s_CameraXExtent[eyeIndex] = topRightCorner - topLeftCorner;
-                s_CameraYExtent[eyeIndex] = bottomLeftCorner - topLeftCorner;
-                s_CameraZExtent[eyeIndex] = farCentre;
-
-                cmd.SetGlobalMatrix("_NormalReconstructionMatrix", cviewProjInv);
+                s_NormalReconstructionMatrix[eyeIndex] = cviewProjInv;
+                cmd.SetGlobalMatrix(s_NormalReconstructionMatrixID, cviewProjInv);
             }
 
-            cmd.SetGlobalVector(s_ProjectionParams2ID, new Vector4(1.0f / cameraData.camera.nearClipPlane, 0.0f, 0.0f, 0.0f));
-            cmd.SetGlobalMatrixArray(s_CameraViewProjectionsID, s_CameraViewProjections);
-            cmd.SetGlobalVectorArray(s_CameraViewTopLeftCornerID, s_CameraTopLeftCorner);
-            cmd.SetGlobalVectorArray(s_CameraViewXExtentID, s_CameraXExtent);
-            cmd.SetGlobalVectorArray(s_CameraViewYExtentID, s_CameraYExtent);
-            cmd.SetGlobalVectorArray(s_CameraViewZExtentID, s_CameraZExtent);
-
-            PostProcessUtils.SetSourceSize(cmd, cameraData.cameraTargetDescriptor);
-
-            // Update keywords
-            CoreUtils.SetKeyword(cmd, k_OrthographicCameraKeyword, cameraData.camera.orthographic);
+            cmd.SetGlobalMatrixArray(s_NormalReconstructionMatrixID, s_NormalReconstructionMatrix);
         }
     }
 }
--- com.unity.render-pipelines.universal/ShaderLibrary/NormalReconstruction.hlsl
+++ com.unity.render-pipelines.universal/ShaderLibrary/NormalReconstruction.hlsl
@@ -2,7 +2,6 @@
 #define UNIVERSAL_NORMAL_RECONSTRUCTION
 
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareDepthTexture.hlsl"
-#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/UnityInput.hlsl"
 
 #if defined(USING_STEREO_MATRICES)
 #define unity_eyeIndex unity_StereoEyeIndex
@@ -10,198 +9,30 @@
 #define unity_eyeIndex 0
 #endif
 
-float4 _ProjectionParams2;
-float4 _CameraViewTopLeftCorner[2]; // TODO: check if we can use half type
-float4 _CameraViewXExtent[2];
-float4 _CameraViewYExtent[2];
-float4 _CameraViewZExtent[2];
+float4x4 _NormalReconstructionMatrix[2];
 
-float4x4 _NormalReconstructionMatrix;
-
-float RawToLinearDepth(float rawDepth)
-{
-#if defined(_ORTHOGRAPHIC)
-#if UNITY_REVERSED_Z
-    return ((_ProjectionParams.z - _ProjectionParams.y) * (1.0 - rawDepth) + _ProjectionParams.y);
-#else
-    return ((_ProjectionParams.z - _ProjectionParams.y) * (rawDepth)+_ProjectionParams.y);
-#endif
-#else
-    return LinearEyeDepth(rawDepth, _ZBufferParams); // 1.0 / (zBufferParam.z * depth + zBufferParam.w);
-#endif
-    //return _NormalReconstruction_DepthScale.y + rawDepth * _NormalReconstruction_DepthScale.x
-}
-
-float SampleAndGetLinearDepth(float2 uv)
-{
-    float rawDepth = SampleSceneDepth(uv.xy).r;
-    return RawToLinearDepth(rawDepth);
-}
-
-// This returns a vector in world unit (not a position), from camera to the given point described by uv screen coordinate and depth (in absolute world unit).
-float3 ReconstructViewPos(float2 uv, float depth)
-{
-    // Screen is y-inverted.
-    uv.y = 1.0 - uv.y;
-
-    // view pos in world space
-#if defined(_ORTHOGRAPHIC)
-    float zScale = depth * _ProjectionParams.w; // divide by far plane
-    float3 viewPos = _CameraViewTopLeftCorner[unity_eyeIndex].xyz
-        + _CameraViewXExtent[unity_eyeIndex].xyz * uv.x
-        + _CameraViewYExtent[unity_eyeIndex].xyz * uv.y
-        + _CameraViewZExtent[unity_eyeIndex].xyz * zScale;
-#else
-    float zScale = depth * _ProjectionParams2.x; // divide by near plane
-    float3 viewPos = _CameraViewTopLeftCorner[unity_eyeIndex].xyz
-        + _CameraViewXExtent[unity_eyeIndex].xyz * uv.x
-        + _CameraViewYExtent[unity_eyeIndex].xyz * uv.y;
-    viewPos *= zScale;
-#endif
-
-    return viewPos;
-}
-
-// Try reconstructing normal accurately from depth buffer.
-// Low:    DDX/DDY on the current pixel
-// https://atyuwen.github.io/posts/normal-reconstruction/
-// https://wickedengine.net/2019/09/22/improved-normal-reconstruction-from-depth/
-float3 ReconstructNormalTap1_OLD(float3 vpos)
-{
-    return normalize(cross(ddy(vpos), ddx(vpos)));
-}
-
-// Try reconstructing normal accurately from depth buffer.
-// Medium: 3 taps on each direction | x | * | y |
-// https://atyuwen.github.io/posts/normal-reconstruction/
-// https://wickedengine.net/2019/09/22/improved-normal-reconstruction-from-depth/
-float3 ReconstructNormalTap5_OLD(float2 uv, float depth, float3 vpos)
-{
-    float2 delta = _ScreenSize.zw * 2.0;
-
-    // Sample the neighbour fragments
-    float2 lUV = float2(-delta.x, 0.0);
-    float2 rUV = float2(delta.x, 0.0);
-    float2 uUV = float2(0.0, delta.y);
-    float2 dUV = float2(0.0, -delta.y);
-
-    float3 l1 = float3(uv + lUV, 0.0); l1.z = SampleAndGetLinearDepth(l1.xy); // Left1
-    float3 r1 = float3(uv + rUV, 0.0); r1.z = SampleAndGetLinearDepth(r1.xy); // Right1
-    float3 u1 = float3(uv + uUV, 0.0); u1.z = SampleAndGetLinearDepth(u1.xy); // Up1
-    float3 d1 = float3(uv + dUV, 0.0); d1.z = SampleAndGetLinearDepth(d1.xy); // Down1
-
-    // Determine the closest horizontal and vertical pixels...
-    // horizontal: left = 0.0 right = 1.0
-    // vertical  : down = 0.0    up = 1.0
-    uint closest_horizontal = l1.z > r1.z ? 0 : 1;
-    uint closest_vertical = d1.z > u1.z ? 0 : 1;
-
-    // Calculate the triangle, in a counter-clockwize order, to
-    // use based on the closest horizontal and vertical depths.
-    // h == 0.0 && v == 0.0: p1 = left,  p2 = down
-    // h == 1.0 && v == 0.0: p1 = down,  p2 = right
-    // h == 1.0 && v == 1.0: p1 = right, p2 = up
-    // h == 0.0 && v == 1.0: p1 = up,    p2 = left
-    // Calculate the view space positions for the three points...
-    float3 P1;
-    float3 P2;
-    if (closest_vertical == 0)
-    {
-        P1 = closest_horizontal == 0 ? l1 : d1;
-        P2 = closest_horizontal == 0 ? d1 : r1;
-    }
-    else
-    {
-        P1 = closest_horizontal == 0 ? u1 : r1;
-        P2 = closest_horizontal == 0 ? l1 : u1;
-    }
-
-    P1 = ReconstructViewPos(P1.xy, P1.z);
-    P2 = ReconstructViewPos(P2.xy, P2.z);
-
-    // Use the cross product to calculate the normal...
-    return normalize(cross(P2 - vpos, P1 - vpos));
-}
-
-// Try reconstructing normal accurately from depth buffer.
-// High:   5 taps on each direction: | z | x | * | y | w |
-// https://atyuwen.github.io/posts/normal-reconstruction/
-// https://wickedengine.net/2019/09/22/improved-normal-reconstruction-from-depth/
-float3 ReconstructNormalTap9_OLD(float2 uv, float depth, float3 vpos)
-{
-    float2 delta = _ScreenSize.zw * 2.0;
-
-    // Sample the neighbour fragments
-    float2 lUV = float2(-delta.x, 0.0);
-    float2 rUV = float2(delta.x, 0.0);
-    float2 uUV = float2(0.0, delta.y);
-    float2 dUV = float2(0.0, -delta.y);
-
-    float3 l1 = float3(uv + lUV, 0.0); l1.z = SampleAndGetLinearDepth(l1.xy); // Left1
-    float3 r1 = float3(uv + rUV, 0.0); r1.z = SampleAndGetLinearDepth(r1.xy); // Right1
-    float3 u1 = float3(uv + uUV, 0.0); u1.z = SampleAndGetLinearDepth(u1.xy); // Up1
-    float3 d1 = float3(uv + dUV, 0.0); d1.z = SampleAndGetLinearDepth(d1.xy); // Down1
-
-    // Determine the closest horizontal and vertical pixels...
-    // horizontal: left = 0.0 right = 1.0
-    // vertical  : down = 0.0    up = 1.
-    float3 l2 = float3(uv + lUV * 2.0, 0.0); l2.z = SampleAndGetLinearDepth(l2.xy); // Left2
-    float3 r2 = float3(uv + rUV * 2.0, 0.0); r2.z = SampleAndGetLinearDepth(r2.xy); // Right2
-    float3 u2 = float3(uv + uUV * 2.0, 0.0); u2.z = SampleAndGetLinearDepth(u2.xy); // Up2
-    float3 d2 = float3(uv + dUV * 2.0, 0.0); d2.z = SampleAndGetLinearDepth(d2.xy); // Down2
-
-    const uint closest_horizontal = abs((2.0 * l1.z - l2.z) - depth) < abs((2.0 * r1.z - r2.z) - depth) ? 0 : 1;
-    const uint closest_vertical = abs((2.0 * d1.z - d2.z) - depth) < abs((2.0 * u1.z - u2.z) - depth) ? 0 : 1;
-
-    // Calculate the triangle, in a counter-clockwize order, to
-    // use based on the closest horizontal and vertical depths.
-    // h == 0.0 && v == 0.0: p1 = left,  p2 = down
-    // h == 1.0 && v == 0.0: p1 = down,  p2 = right
-    // h == 1.0 && v == 1.0: p1 = right, p2 = up
-    // h == 0.0 && v == 1.0: p1 = up,    p2 = left
-    // Calculate the view space positions for the three points...
-    float3 P1;
-    float3 P2;
-    if (closest_vertical == 0)
-    {
-        P1 = closest_horizontal == 0 ? l1 : d1;
-        P2 = closest_horizontal == 0 ? d1 : r1;
-    }
-    else
-    {
-        P1 = closest_horizontal == 0 ? u1 : r1;
-        P2 = closest_horizontal == 0 ? l1 : u1;
-    }
-
-    P1 = ReconstructViewPos(P1.xy, P1.z);
-    P2 = ReconstructViewPos(P2.xy, P2.z);
-
-    // Use the cross product to calculate the normal...
-    return normalize(cross(P2 - vpos, P1 - vpos));
-}
-
-float getRawDepth(float2 uv) { return SampleSceneDepth(uv.xy).r; }
+float GetRawDepth(float2 uv) { return SampleSceneDepth(uv.xy).r; }
 
 // inspired by keijiro's depth inverse projection
 // https://github.com/keijiro/DepthInverseProjection
 // constructs view space ray at the far clip plane from the screen uv
 // then multiplies that ray by the linear 01 depth
-float3 viewSpacePosAtScreenUV(float2 uv)
+half3 ViewSpacePosAtScreenUV(float2 uv)
 {
-    float3 viewSpaceRay = mul(_NormalReconstructionMatrix, float4(uv * 2.0 - 1.0, 1.0, 1.0) * _ProjectionParams.z);
-    float rawDepth = getRawDepth(uv);
-    return viewSpaceRay * Linear01Depth(rawDepth, _ZBufferParams);
+    half3 viewSpaceRay = (half3)(mul(_NormalReconstructionMatrix[unity_eyeIndex], float4(uv * 2.0 - 1.0, 1.0, 1.0) * _ProjectionParams.z));
+    float rawDepth = GetRawDepth(uv);
+    return viewSpaceRay * half(Linear01Depth(rawDepth, _ZBufferParams));
 }
 
-float3 viewSpacePosAtPixelPosition(float2 positionSS)
+half3 ViewSpacePosAtPixelPosition(float2 positionSS)
 {
     float2 uv = positionSS * _ScreenSize.zw;
-    return viewSpacePosAtScreenUV(uv);
+    return ViewSpacePosAtScreenUV(uv);
 }
 
-half3 ReconstructNormalTap1(float2 positionSS)
+half3 ReconstructNormalDerivative(float2 positionSS)
 {
-    half3 viewSpacePos = viewSpacePosAtPixelPosition(positionSS);
+    half3 viewSpacePos = ViewSpacePosAtPixelPosition(positionSS);
     return normalize(cross(ddy(viewSpacePos), ddx(viewSpacePos)));
 }
 
@@ -210,11 +41,11 @@ half3 ReconstructNormalTap1(float2 positionSS)
 half3 ReconstructNormalTap3(float2 positionSS)
 {
     // get current pixel's view space position
-    half3 viewSpacePos_c = viewSpacePosAtPixelPosition(positionSS + float2(0.0, 0.0));
+    half3 viewSpacePos_c = ViewSpacePosAtPixelPosition(positionSS + float2(0.0, 0.0));
 
     // get view space position at 1 pixel offsets in each major direction
-    half3 viewSpacePos_r = viewSpacePosAtPixelPosition(positionSS + float2(1.0, 0.0));
-    half3 viewSpacePos_u = viewSpacePosAtPixelPosition(positionSS + float2(0.0, 1.0));
+    half3 viewSpacePos_r = ViewSpacePosAtPixelPosition(positionSS + float2(1.0, 0.0));
+    half3 viewSpacePos_u = ViewSpacePosAtPixelPosition(positionSS + float2(0.0, 1.0));
 
     // get the difference between the current and each offset position
     half3 hDeriv = viewSpacePos_r - viewSpacePos_c;
@@ -231,10 +62,10 @@ half3 ReconstructNormalTap3(float2 positionSS)
 half3 ReconstructNormalTap4(float2 positionSS)
 {
     // get view space position at 1 pixel offsets in each major direction
-    half3 viewSpacePos_l = viewSpacePosAtPixelPosition(positionSS + float2(-1.0, 0.0));
-    half3 viewSpacePos_r = viewSpacePosAtPixelPosition(positionSS + float2(1.0, 0.0));
-    half3 viewSpacePos_d = viewSpacePosAtPixelPosition(positionSS + float2(0.0, -1.0));
-    half3 viewSpacePos_u = viewSpacePosAtPixelPosition(positionSS + float2(0.0, 1.0));
+    half3 viewSpacePos_l = ViewSpacePosAtPixelPosition(positionSS + float2(-1.0, 0.0));
+    half3 viewSpacePos_r = ViewSpacePosAtPixelPosition(positionSS + float2(1.0, 0.0));
+    half3 viewSpacePos_d = ViewSpacePosAtPixelPosition(positionSS + float2(0.0, -1.0));
+    half3 viewSpacePos_u = ViewSpacePosAtPixelPosition(positionSS + float2(0.0, 1.0));
 
     // get the difference between the current and each offset position
     half3 hDeriv = viewSpacePos_r - viewSpacePos_l;
@@ -251,13 +82,13 @@ half3 ReconstructNormalTap4(float2 positionSS)
 half3 ReconstructNormalTap5(float2 positionSS)
 {
     // get current pixel's view space position
-    half3 viewSpacePos_c = viewSpacePosAtPixelPosition(positionSS + float2(0.0, 0.0));
+    half3 viewSpacePos_c = ViewSpacePosAtPixelPosition(positionSS + float2(0.0, 0.0));
 
     // get view space position at 1 pixel offsets in each major direction
-    half3 viewSpacePos_l = viewSpacePosAtPixelPosition(positionSS + float2(-1.0, 0.0));
-    half3 viewSpacePos_r = viewSpacePosAtPixelPosition(positionSS + float2(1.0, 0.0));
-    half3 viewSpacePos_d = viewSpacePosAtPixelPosition(positionSS + float2(0.0, -1.0));
-    half3 viewSpacePos_u = viewSpacePosAtPixelPosition(positionSS + float2(0.0, 1.0));
+    half3 viewSpacePos_l = ViewSpacePosAtPixelPosition(positionSS + float2(-1.0, 0.0));
+    half3 viewSpacePos_r = ViewSpacePosAtPixelPosition(positionSS + float2(1.0, 0.0));
+    half3 viewSpacePos_d = ViewSpacePosAtPixelPosition(positionSS + float2(0.0, -1.0));
+    half3 viewSpacePos_u = ViewSpacePosAtPixelPosition(positionSS + float2(0.0, 1.0));
 
     // get the difference between the current and each offset position
     half3 l = viewSpacePos_c - viewSpacePos_l;
@@ -283,16 +114,16 @@ half3 ReconstructNormalTap9(float2 positionSS)
     float2 uv = positionSS * _ScreenSize.zw;
 
     // current pixel's depth
-    float c = getRawDepth(uv);
+    float c = GetRawDepth(uv);
 
     // get current pixel's view space position
-    half3 viewSpacePos_c = viewSpacePosAtScreenUV(uv);
+    half3 viewSpacePos_c = ViewSpacePosAtScreenUV(uv);
 
     // get view space position at 1 pixel offsets in each major direction
-    half3 viewSpacePos_l = viewSpacePosAtScreenUV(uv + float2(-1.0, 0.0) * _ScreenSize.zw);
-    half3 viewSpacePos_r = viewSpacePosAtScreenUV(uv + float2(1.0, 0.0) * _ScreenSize.zw);
-    half3 viewSpacePos_d = viewSpacePosAtScreenUV(uv + float2(0.0, -1.0) * _ScreenSize.zw);
-    half3 viewSpacePos_u = viewSpacePosAtScreenUV(uv + float2(0.0, 1.0) * _ScreenSize.zw);
+    half3 viewSpacePos_l = ViewSpacePosAtScreenUV(uv + float2(-1.0, 0.0) * _ScreenSize.zw);
+    half3 viewSpacePos_r = ViewSpacePosAtScreenUV(uv + float2(1.0, 0.0) * _ScreenSize.zw);
+    half3 viewSpacePos_d = ViewSpacePosAtScreenUV(uv + float2(0.0, -1.0) * _ScreenSize.zw);
+    half3 viewSpacePos_u = ViewSpacePosAtScreenUV(uv + float2(0.0, 1.0) * _ScreenSize.zw);
 
     // get the difference between the current and each offset position
     half3 l = viewSpacePos_c - viewSpacePos_l;
@@ -302,18 +133,18 @@ half3 ReconstructNormalTap9(float2 positionSS)
 
     // get depth values at 1 & 2 pixels offsets from current along the horizontal axis
     half4 H = half4(
-        getRawDepth(uv + float2(-1.0, 0.0) * _ScreenSize.zw.xy),
-        getRawDepth(uv + float2(1.0, 0.0) * _ScreenSize.zw.xy),
-        getRawDepth(uv + float2(-2.0, 0.0) * _ScreenSize.zw.xy),
-        getRawDepth(uv + float2(2.0, 0.0) * _ScreenSize.zw.xy)
+        GetRawDepth(uv + float2(-1.0, 0.0) * _ScreenSize.zw.xy),
+        GetRawDepth(uv + float2(1.0, 0.0) * _ScreenSize.zw.xy),
+        GetRawDepth(uv + float2(-2.0, 0.0) * _ScreenSize.zw.xy),
+        GetRawDepth(uv + float2(2.0, 0.0) * _ScreenSize.zw.xy)
         );
 
     // get depth values at 1 & 2 pixels offsets from current along the vertical axis
     half4 V = half4(
-        getRawDepth(uv + float2(0.0, -1.0) * _ScreenSize.zw.xy),
-        getRawDepth(uv + float2(0.0, 1.0) * _ScreenSize.zw.xy),
-        getRawDepth(uv + float2(0.0, -2.0) * _ScreenSize.zw.xy),
-        getRawDepth(uv + float2(0.0, 2.0) * _ScreenSize.zw.xy)
+        GetRawDepth(uv + float2(0.0, -1.0) * _ScreenSize.zw.xy),
+        GetRawDepth(uv + float2(0.0, 1.0) * _ScreenSize.zw.xy),
+        GetRawDepth(uv + float2(0.0, -2.0) * _ScreenSize.zw.xy),
+        GetRawDepth(uv + float2(0.0, 2.0) * _ScreenSize.zw.xy)
         );
 
     // current pixel's depth difference from slope of offset depth samples
