06ef4a891968d27d0607187f42298636b1c0ee4e
Jessica <jessica.thomson@unity3d.com>
2021-04-13 15:51:29 -0700

    Added wrap_utr.py to swallow the utr exitcode for intentional re-runs

---
+++ .yamato/tooling/wrap_utr.py
@@ -0,0 +1,9 @@
+import subprocess
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Wraps a UTR call to swallow the output for expected failing runs")
+    parser.add_argument("--args")
+
+    args = parser.parse_args()
+    subprocess.run(["utr"] + args.args.split())
