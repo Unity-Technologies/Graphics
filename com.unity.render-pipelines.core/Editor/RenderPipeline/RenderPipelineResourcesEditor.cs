62c761f03e75b8f6366e0be471021afaf85c2b89
RSlysz <remi@unity3d.com>
2021-04-14 14:34:35 +0200

    fix inspector fallback and clean code

--- com.unity.render-pipelines.core/Editor/RenderPipeline/RenderPipelineResourcesEditor.cs
+++ com.unity.render-pipelines.core/Editor/RenderPipeline/RenderPipelineResourcesEditor.cs
@@ -1,22 +1,30 @@
+using UnityEditor;
+
 namespace UnityEngine.Rendering
 {
-    [UnityEditor.CustomEditor(typeof(RenderPipelineResources), isFallback = true)]
-    class RenderPipelineResourcesEditor : UnityEditor.Editor
+    [CustomEditor(typeof(RenderPipelineResources), editorForChildClasses: true)]
+    class RenderPipelineResourcesEditor : Editor
     {
         public override void OnInspectorGUI()
         {
             DrawDefaultInspector();
 
             // Add a "Reload All" button in inspector when we are in developer's mode
-            if (UnityEditor.EditorPrefs.GetBool("DeveloperMode")
+            if (EditorPrefs.GetBool("DeveloperMode")
                 && GUILayout.Button("Reload All"))
             {
                 foreach (RenderPipelineResources t in targets)
                 {
+                    if (string.IsNullOrEmpty(t.packagePath_Internal))
+                    {
+                        Debug.LogError("packagePath is not set in this Resource. We will not be able to reload it. Skiping.");
+                        continue;
+                    }
+
                     foreach (var field in t.GetType().GetFields())
                         field.SetValue(t, null);
 
-                    ResourceReloader.ReloadAllNullIn(target, t.packagePath_Internal);
+                    ResourceReloader.ReloadAllNullIn(t, t.packagePath_Internal);
                 }
             }
         }
