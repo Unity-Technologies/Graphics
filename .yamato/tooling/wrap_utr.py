d34713f40aaa0d55c6d5725c33fb83139acaa236
Jessica <jessica.thomson@unity3d.com>
2021-04-14 16:11:51 -0700

    Added a platform switch in Python for utr file extensions

--- .yamato/tooling/wrap_utr.py
+++ .yamato/tooling/wrap_utr.py
@@ -1,9 +1,15 @@
 import subprocess
 import argparse
+import platform
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Wraps a UTR call to swallow the output for expected failing runs")
     parser.add_argument("--args")
 
     args = parser.parse_args()
-    subprocess.call(["utr"] + args.args.split())
+    cur_sys = platform.system()
+    if cur_sys == 'Windows':
+        subprocess.call(["utr.bat"] + args.args.split())
+    else:
+        subprocess.call(["utr"] + args.args.split())
+
