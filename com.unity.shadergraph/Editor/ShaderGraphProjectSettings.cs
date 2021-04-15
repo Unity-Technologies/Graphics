3e650d323d65e2952c7d671af5e36c82cab547d5
Esmeralda Salamone <esmeralda.salamone@unity3d.com>
2021-04-15 01:40:00 -0700

    just updating the URL to threshold documentation to match the expected URL.

--- com.unity.shadergraph/Editor/ShaderGraphProjectSettings.cs
+++ com.unity.shadergraph/Editor/ShaderGraphProjectSettings.cs
@@ -21,8 +21,7 @@ namespace UnityEditor.ShaderGraph
         private static int kMaxChannelThreshold = 32;
         private static int kMinChannelThreshold = 8;
         private static string kCustomInterpolatorHelpBox = "Unity uses these options to help ShaderGraph users maintain known compatibilities with target platform(s) when using Custom Interpolators.";
-        // TODO: Direct url to Custom Interpolator page.
-        private static string kCustomInterpolatorDocumentationURL = "https://docs.unity3d.com/Packages/com.unity.shadergraph@latest/index.html?preview=1&subfolder=/manual/";
+        private static string kCustomInterpolatorDocumentationURL = "https://docs.unity3d.com/Packages/com.unity.shadergraph@latest/manual/custom-interpolators.html#thresholds";
 
         private class Styles
         {
