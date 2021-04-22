919ec7cef94cd1b0bb7b0bf035438fc3b1742371
Simon Bouvier-Zappa <simonbz@unity3d.com>
2021-04-22 14:03:11 -0400

    Moved AnimationClipUpgrader_UnitTests and UpgradeUtility_UnitTests outside of com.unity.render-pipelines.universal. Removed dependency on Moq in com.unity.render-pipelines.universal.

--- com.unity.render-pipelines.core/Editor/AssemblyInfo.cs
+++ com.unity.render-pipelines.core/Editor/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using System.Runtime.CompilerServices;
 
 // remove as needed when/if MaterialUpgrader.RenameType can be made public
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.Universal.Editor")]
-[assembly: InternalsVisibleTo("Unity.RenderPipelines.Universal.Editor.Tests")]
+[assembly: InternalsVisibleTo("Unity.Testing.AnimationUpgrade.Editor")]
