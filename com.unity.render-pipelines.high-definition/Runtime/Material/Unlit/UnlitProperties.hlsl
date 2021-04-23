0708326c1aa053c707deb9cdc8c7b71d689b70ae
Jussi Knuuttila <jussi.knuuttila@unity3d.com>
2021-04-23 17:04:48 +0300

    Change HDRP DOTS instancing definitions to use the new macros.

--- com.unity.render-pipelines.high-definition/Runtime/Material/Unlit/UnlitProperties.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Material/Unlit/UnlitProperties.hlsl
@@ -51,8 +51,8 @@ UNITY_DOTS_INSTANCING_START(MaterialPropertyMetadata)
     UNITY_DOTS_INSTANCED_PROP(float , _AlphaCutoff);
 UNITY_DOTS_INSTANCING_END(MaterialPropertyMetadata)
 
-#define _UnlitColor     UNITY_ACCESS_DOTS_INSTANCED_PROP_FROM_MACRO(float4, Metadata__UnlitColor)
-#define _EmissiveColor  UNITY_ACCESS_DOTS_INSTANCED_PROP_FROM_MACRO(float3, Metadata__EmissiveColor)
-#define _AlphaCutoff    UNITY_ACCESS_DOTS_INSTANCED_PROP_FROM_MACRO(float , Metadata__AlphaCutoff)
+#define _UnlitColor     UNITY_ACCESS_DOTS_INSTANCED_PROP(float4, _UnlitColor)
+#define _EmissiveColor  UNITY_ACCESS_DOTS_INSTANCED_PROP(float3, _EmissiveColor)
+#define _AlphaCutoff    UNITY_ACCESS_DOTS_INSTANCED_PROP(float , _AlphaCutoff)
 
 #endif
