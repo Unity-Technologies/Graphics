5e66cb136477a7a4b676b6aeb8aced5694b76fd7
Josh Davis <76977132+joshua-davis@users.noreply.github.com>
2021-04-28 09:14:47 -0700

    Added an updater for shader graph with the builtin target that will u… (#4318)
    * Added an updater for shader graph with the builtin target that will udpate all materials using that shader graph when it's saved. This is a heavy-handed solution that should get updated when import dependencies are finished.

--- com.unity.shadergraph/Editor/Generation/Targets/BuiltIn/Editor/ShaderGraph/Targets/BuiltInTarget.cs
+++ com.unity.shadergraph/Editor/Generation/Targets/BuiltIn/Editor/ShaderGraph/Targets/BuiltInTarget.cs
@@ -266,6 +266,8 @@ namespace UnityEditor.Rendering.BuiltIn.ShaderGraph
             m_ActiveSubTarget.value.ProcessPreviewMaterial(material);
         }
 
+        public override object saveContext => m_ActiveSubTarget.value?.saveContext;
+
         public override void GetPropertiesGUI(ref TargetPropertyGUIContext context, Action onChange, Action<String> registerUndo)
         {
             if (m_ActiveSubTarget.value == null)
