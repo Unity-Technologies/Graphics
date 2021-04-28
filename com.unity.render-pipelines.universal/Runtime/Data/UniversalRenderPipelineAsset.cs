1caf37beafa74c63d5bb0c3306b6963015f54403
manuele-bonanno <69153427+manuele-bonanno@users.noreply.github.com>
2021-04-28 09:17:00 +0200

    [Universal] MSAA store actions optimizations for forward renderer Drawobjects pass (#4161)
    * initial store and load action SRP support
    
    * removed depth prepass related temporary changes
    
    * removing commented out code
    
    * removing unused variables
    
    * Added Store Actions settings
    
    * moved the code that checks for injected passes
    
    * cleanup and addressing reviewer's feedback
    
    * moved the "Store Actions" setting in the Advanced options section
    
    * further depth store action optimization for when MSAA is disabled
    
    * Added changelog entry
    
    * Added documentation
    
    * handling the opaque pass depth store actions separately for a more optimal configurations
    
    * if "Store Actions"=Store, make sure to preserve and StoreAndResolve actions and not override them with Store
    
    * addressed reviewers feedback
    
    * added new API docs as requested by reviewers
    
    * Added storeActionsOptimization property setter after reviewer's feedback
    
    * the opaqueColorStoreAction should be StoreAndResolve only if copy color is needed
    
    * renamed parameter of CoreUtils.SetRenderTarget after reviewer's feedback
    
    * updated the tooltip after reviewer's feedback
    
    * removed using System.ComponentModel.Design.Serialization; line added by Rider..
    
    * removing files accidentally added by a previous merge
    
    * Edited the __Store Actions__ description.
    
    * Edited the StoreActionsOptimization API descriptions.
    
    * editor test fix
    
    Co-authored-by: Oleksandr Kokoshyn <oleksandr.kokoshyn@unity3d.com>
    Co-authored-by: Felipe Lira <felipedrl@gmail.com>

--- com.unity.render-pipelines.universal/Runtime/Data/UniversalRenderPipelineAsset.cs
+++ com.unity.render-pipelines.universal/Runtime/Data/UniversalRenderPipelineAsset.cs
@@ -87,6 +87,19 @@ namespace UnityEngine.Rendering.Universal
         HighDynamicRange
     }
 
+    /// <summary>
+    /// Defines if Unity discards or stores the render targets of the DrawObjects Passes. Selecting the Store option significantly increases the memory bandwidth on mobile and tile-based GPUs.
+    /// </summary>
+    public enum StoreActionsOptimization
+    {
+        /// <summary>Unity uses the Discard option by default, and falls back to the Store option if it detects any injected Passes.</summary>
+        Auto,
+        /// <summary>Unity discards the render targets of render Passes that are not reused later (lower memory bandwidth).</summary>
+        Discard,
+        /// <summary>Unity stores all render targets of each Pass (higher memory bandwidth).</summary>
+        Store
+    }
+
     [ExcludeFromPreset]
     public partial class UniversalRenderPipelineAsset : RenderPipelineAsset, ISerializationCallbackReceiver
     {
@@ -127,6 +140,7 @@ namespace UnityEngine.Rendering.Universal
         [SerializeField] bool m_RequireOpaqueTexture = false;
         [SerializeField] Downsampling m_OpaqueDownsampling = Downsampling._2xBilinear;
         [SerializeField] bool m_SupportsTerrainHoles = true;
+        [SerializeField] StoreActionsOptimization m_StoreActionsOptimization = StoreActionsOptimization.Auto;
 
         // Quality settings
         [SerializeField] bool m_SupportsHDR = true;
@@ -532,6 +546,16 @@ namespace UnityEngine.Rendering.Universal
         public bool supportsTerrainHoles
         {
             get { return m_SupportsTerrainHoles; }
+        }
+
+        /// <summary>
+        /// Returns the active store action optimization value.
+        /// </summary>
+        /// <returns>Returns the active store action optimization value.</returns>
+        public StoreActionsOptimization storeActionsOptimization
+        {
+            get { return m_StoreActionsOptimization; }
+            set { m_StoreActionsOptimization = value; }
         }
 
         public bool supportsHDR
