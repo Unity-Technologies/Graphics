afd7e203193e95d11efe586222fb32b5395fecd9
Kleber Garcia <kleber.garcia@unity3d.com>
2021-04-29 10:33:18 -0400

    Adding better comment to explain offseting of bicubic texture offset fix

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.PostProcess.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.PostProcess.cs
@@ -3166,7 +3166,10 @@ namespace UnityEngine.Rendering.HighDefinition
             m_BloomBicubicParams = new Vector4(passData.bloomMipInfo[0].x, passData.bloomMipInfo[0].y, 1.0f / passData.bloomMipInfo[0].x, 1.0f / passData.bloomMipInfo[0].y);
             var mip0Scale = new Vector2(passData.bloomMipInfo[0].z, passData.bloomMipInfo[0].w);
 
-            //apply DRS on bloom bicubic params
+            // We undo the scale here, because bloom uses these parameters for its bicubic filtering offset.
+            // The bicubic filtering function is SampleTexture2DBicubic, and it requires the underlying texture's
+            // unscaled pixel sizes to compute the offsets of the samples.
+            // For more info please see the implementation of SampleTexture2DBicubic
             m_BloomBicubicParams.x /= RTHandles.rtHandleProperties.rtHandleScale.x;
             m_BloomBicubicParams.y /= RTHandles.rtHandleProperties.rtHandleScale.y;
             m_BloomBicubicParams.z *= RTHandles.rtHandleProperties.rtHandleScale.x;
