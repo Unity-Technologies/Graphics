4e8b411fc497ae25b099c11b44640eefc049c2b7
julienf-unity <julienf@unity3D.com>
2021-04-30 14:35:07 +0200

    [VFX] Staging to master (#4383)
    VFX fixes from staging + big doc update
    
    Fixed
    
    Fixed null reference exception when exposing Camera type in VFX graph Case 1315582
    Fixed VFX with output mesh being always reimported Case 1309753
    Modified state in the VFX tab has now a correct state
    Motion Vector map sampling for flipbooks were not using correct mips
    Remove unexpected expression in spawn context evaluation Case 1318412

--- com.unity.visualeffectgraph/Editor/ShaderGraph/VFXShaderGraphParticleOutput.cs
+++ com.unity.visualeffectgraph/Editor/ShaderGraph/VFXShaderGraphParticleOutput.cs
@@ -391,8 +391,8 @@ namespace UnityEditor.VFX
         {
             base.CheckGraphBeforeImport();
             // If the graph is reimported it can be because one of its depedency such as the shadergraphs, has been changed.
-
-            ResyncSlots(true);
+            if (!VFXGraph.explicitCompile)
+                ResyncSlots(true);
 
             // Ensure that the output context name is in sync with the shader graph shader enum name.
             if (GetOrRefreshShaderGraphObject() != null &&
