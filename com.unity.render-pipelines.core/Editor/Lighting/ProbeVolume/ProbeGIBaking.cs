0c4adf8f9ba6a6909b51cca1c2b5d37ee8558ecf
FrancescoC-Unity <francescoc@unity3d.com>
2021-05-03 16:27:02 +0200

    Fix warning

--- com.unity.render-pipelines.core/Editor/Lighting/ProbeVolume/ProbeGIBaking.cs
+++ com.unity.render-pipelines.core/Editor/Lighting/ProbeVolume/ProbeGIBaking.cs
@@ -116,7 +116,7 @@ namespace UnityEngine.Experimental.Rendering
                 if (reference.transform.localScale != compare.transform.localScale)
                     return null;
 
-                if (reference.profile != compare.profile)
+                if (!reference.profile.IsEquivalent(compare.profile))
                     return null;
             }
 
