d34d4972b18cb03fe4cf36e33b7ce85d39b7a6a9
Jessica Thomson <jessica.thomson@unity3d.com>
2021-05-04 16:05:06 -0700

    Refactored commitback script to re-run tests itself

--- .yamato/tooling/shadergraph_commitback.py
+++ .yamato/tooling/shadergraph_commitback.py
@@ -1,16 +1,15 @@
 import argparse
 import os
+import subprocess
 from os import path
 from shutil import copyfile
-
+import sys
 from git import Repo, exc
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Takes the output from shadergraph's image tests and commits them "
-                                                 "back to the source repo")
-    parser.add_argument("--root")
 
-    args = parser.parse_args()
+    root = sys.argv[1]
+    utr = sys.argv[2:]
 
     repo = Repo(".\\")
 
@@ -30,12 +29,13 @@ if __name__ == "__main__":
         pass
 
     editor = ""
-    update_tests_file_path = path.join(args.root, "Assets/Resources/UpdateTests.txt")
+    update_tests_file_path = path.join(root, "Assets/Resources/UpdateTests.txt")
     if not path.exists(update_tests_file_path):
         print("No update file found, skipping recommit")
         print(os.getcwd())
         for file in os.listdir("C:\\build\\output\\Unity-Technologies\\Graphics\\TestProjects\\ShaderGraph"):
             print(file)
+        # Skip the additional run if there's nothing to re-commit
         exit(0)
     with open(update_tests_file_path) as f:
         while True:
@@ -46,21 +46,28 @@ if __name__ == "__main__":
             _, _, colorspace, editor, platform, vr, testname, testasset = asset_path.split("/")
 
             if should_update_image == "True":
-                actual_img_path = path.join(os.getcwd(), args.root, "Assets", "ActualImages",
+                actual_img_path = path.join(os.getcwd(), root, "Assets", "ActualImages",
                                             colorspace, editor, platform, vr, test_name + ".png")
-                reference_img_path = path.join(os.getcwd(), args.root, "Assets", "ReferenceImages",
+                reference_img_path = path.join(os.getcwd(), root, "Assets", "ReferenceImages",
                                                colorspace, editor, platform, vr, test_name)
                 copyfile(actual_img_path, reference_img_path + ".png")
                 repo.git.add(reference_img_path + ".png")
-                asset_meta_dir_path = path.join(args.root, asset_path.rsplit("/", 1)[0])
+                asset_meta_dir_path = path.join(root, asset_path.rsplit("/", 1)[0])
                 repo.git.add(asset_meta_dir_path + ".meta")
                 if path.exists(reference_img_path + ".png.meta"):
                     repo.index.add([reference_img_path + ".png.meta"])  # Doesn't seem to always exist, so we check
 
-            repo.git.add(path.join(args.root, asset_path))
-            repo.git.add(path.join(args.root, asset_path + ".meta"))
+            repo.git.add(path.join(root, asset_path))
+            repo.git.add(path.join(root, asset_path + ".meta"))
             print(asset_path + " Added")
-            full_asset_path = path.join(os.getcwd(), args.root, asset_path)
+            full_asset_path = path.join(os.getcwd(), root, asset_path)
 
     repo.git.commit("-m", "Generated reference images for " + editor)
     repo.remote(name="origin").push(["--set-upstream", new_branch_name])
+
+    utr_file_name = ""
+    if platform.system() == 'Windows':
+        utr_file_name = "utr.bat"
+    else:
+        utr_file_name = "utr"
+    exit(subprocess.call(utr_file_name + utr))
