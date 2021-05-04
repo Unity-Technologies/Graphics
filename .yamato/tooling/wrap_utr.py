4c667fda78e11b3c5708cd729e7acefd4addd31c
Jessica Thomson <jessica.thomson@unity3d.com>
2021-05-04 14:59:24 -0700

    Updated WrapUTR to not need argparse to avoid double quotes from being consumed

--- .yamato/tooling/wrap_utr.py
+++ .yamato/tooling/wrap_utr.py
@@ -1,15 +1,11 @@
 import subprocess
-import argparse
 import platform
+import sys
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("Wraps a UTR call to swallow the output for expected failing runs")
-    parser.add_argument("--args")
-
-    args = parser.parse_args()
     cur_sys = platform.system()
     if cur_sys == 'Windows':
-        subprocess.call(["utr.bat"] + args.args.split())
+        subprocess.call(["utr.bat"] + sys.argv[1:])
     else:
-        subprocess.call(["utr"] + args.args.split())
+        subprocess.call(["utr"] + sys.argv[1:])
 
