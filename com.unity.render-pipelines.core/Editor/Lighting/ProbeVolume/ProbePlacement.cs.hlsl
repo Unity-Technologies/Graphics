6d1146dc441bfef75612dc7d7499d50c1f230f9a
Antoine Lelievre <antoinel@unity3d.com>
2021-05-05 17:54:39 +0200

    Started to voxelize the probe volume data

---
+++ com.unity.render-pipelines.core/Editor/Lighting/ProbeVolume/ProbePlacement.cs.hlsl
@@ -0,0 +1,37 @@
+//
+// This file was automatically generated. Please don't edit by hand. Execute Editor command [ Edit > Rendering > Generate Shader Includes ] instead
+//
+
+#ifndef PROBEPLACEMENT_CS_HLSL
+#define PROBEPLACEMENT_CS_HLSL
+// Generated from UnityEngine.Experimental.Rendering.ProbePlacement+GPUProbeVolumeOBB
+// PackingRules = Exact
+struct GPUProbeVolumeOBB
+{
+    float3 corner;
+    float3 X;
+    float3 Y;
+    float3 Z;
+};
+
+//
+// Accessors for UnityEngine.Experimental.Rendering.ProbePlacement+GPUProbeVolumeOBB
+//
+float3 GetCorner(GPUProbeVolumeOBB value)
+{
+    return value.corner;
+}
+float3 GetX(GPUProbeVolumeOBB value)
+{
+    return value.X;
+}
+float3 GetY(GPUProbeVolumeOBB value)
+{
+    return value.Y;
+}
+float3 GetZ(GPUProbeVolumeOBB value)
+{
+    return value.Z;
+}
+
+#endif
