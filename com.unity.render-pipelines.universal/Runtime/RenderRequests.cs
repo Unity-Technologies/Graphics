3bc9a111e9a4caafb62985869fb15b233698426b
Jussi Knuuttila <jussi.knuuttila@unity3d.com>
2021-05-06 16:03:42 +0300

    Split selection masks into a separate renderer class since they had a different code path.

--- com.unity.render-pipelines.universal/Runtime/RenderRequests.cs
+++ com.unity.render-pipelines.universal/Runtime/RenderRequests.cs
@@ -8,7 +8,13 @@ namespace UnityEngine.Rendering.Universal
     {
         protected override ScriptableRenderer Create()
         {
-            return new RenderRequestRenderer(this);
+            switch (request.mode)
+            {
+                case Camera.RenderRequestMode.SelectionMask:
+                    return new RenderRequestSelectionMaskRenderer(this);
+                default:
+                    return new RenderRequestRenderer(this);
+            }
         }
 
         public Camera.RenderRequest request { get; set; }
@@ -18,51 +24,17 @@ namespace UnityEngine.Rendering.Universal
     {
         DrawObjectsPass m_RenderOpaqueForwardPass;
         DrawObjectsPass m_RenderTransparentForwardPass;
-        DrawObjectsPass m_RenderOpaqueForwardPassZDisabled;
-        DrawObjectsPass m_RenderTransparentForwardPassZDisabled;
 
         LayerMask m_OpaqueLayerMask = -1;
         LayerMask m_TransparentLayerMask = -1;
         StencilState m_DefaultStencilState = StencilState.defaultValue;
-        Camera.RenderRequestMode m_Mode;
 
         public RenderRequestRenderer(RenderRequestRendererData data) : base(data)
         {
-            m_Mode = data.request.mode;
-
             var shaderTags = new[] {new ShaderTagId("DataExtraction")};
 
             m_RenderOpaqueForwardPass = new DrawObjectsPass("Render Opaques", shaderTags, true, RenderPassEvent.BeforeRenderingOpaques, RenderQueueRange.opaque, m_OpaqueLayerMask, m_DefaultStencilState, 0 );
             m_RenderTransparentForwardPass = new DrawObjectsPass("Render Transparents", shaderTags, false, RenderPassEvent.BeforeRenderingTransparents, RenderQueueRange.transparent, m_TransparentLayerMask, m_DefaultStencilState, 0);
-
-            // Selection mask requires separate passes for occluded objects
-            if (IsSelectionMaskRequest)
-            {
-                m_RenderOpaqueForwardPassZDisabled = new DrawObjectsPass("Render Opaques (Z disabled)", shaderTags, true, RenderPassEvent.BeforeRenderingOpaques, RenderQueueRange.opaque, m_OpaqueLayerMask, m_DefaultStencilState, 0 );
-                m_RenderTransparentForwardPassZDisabled = new DrawObjectsPass("Render Transparents (Z disabled)", shaderTags, false, RenderPassEvent.BeforeRenderingTransparents, RenderQueueRange.transparent, m_TransparentLayerMask, m_DefaultStencilState, 0);
-
-                var state = m_RenderOpaqueForwardPassZDisabled.RenderStateBlock;
-                state.rasterState = new RasterState(CullMode.Back, 0, -0.02f);
-                // There is no Z buffer for selection mask, use blending to make sure "unoccluded" (G = 1) values
-                // always remain on top.
-                state.blendState = new BlendState
-                {
-                    blendState0 = new RenderTargetBlendState
-                    {
-                        colorBlendOperation = BlendOp.Max,
-                        sourceColorBlendMode = BlendMode.One,
-                        destinationColorBlendMode = BlendMode.One,
-                        alphaBlendOperation = BlendOp.Max,
-                        sourceAlphaBlendMode = BlendMode.One,
-                        destinationAlphaBlendMode = BlendMode.One,
-                        writeMask = ColorWriteMask.Green | ColorWriteMask.Blue | ColorWriteMask.Alpha,
-                    }
-                };
-                state.depthState = new DepthState {compareFunction = CompareFunction.Always, writeEnabled = false};
-                state.mask = RenderStateMask.Everything;
-                m_RenderOpaqueForwardPassZDisabled.RenderStateBlock = state;
-                m_RenderTransparentForwardPassZDisabled.RenderStateBlock = state;
-            }
 
             List<Tuple<string, int>> values = new List<Tuple<string, int>>
             {
@@ -72,33 +44,13 @@ namespace UnityEngine.Rendering.Universal
 
             m_RenderOpaqueForwardPass.SetAdditionalValues(values);
             m_RenderTransparentForwardPass.SetAdditionalValues(values);
-
-            if (IsSelectionMaskRequest)
-            {
-                List<Tuple<string, int>> valuesZDisabled = new List<Tuple<string, int>>
-                {
-                    new Tuple<string, int>("UNITY_DataExtraction_Mode", (int)data.request.mode),
-                    new Tuple<string, int>("UNITY_DataExtraction_Space", (int)data.request.outputSpace),
-                };
-
-                m_RenderOpaqueForwardPassZDisabled.SetAdditionalValues(valuesZDisabled);
-                m_RenderTransparentForwardPassZDisabled.SetAdditionalValues(valuesZDisabled);
-            }
         }
 
         /// <inheritdoc />
         public override void Setup(ScriptableRenderContext context, ref RenderingData renderingData)
         {
-            if (IsSelectionMaskRequest)
-            {
-                EnqueuePass(m_RenderOpaqueForwardPassZDisabled);
-                EnqueuePass(m_RenderTransparentForwardPassZDisabled);
-            }
-            else
-            {
-                EnqueuePass(m_RenderOpaqueForwardPass);
-                EnqueuePass(m_RenderTransparentForwardPass);
-            }
+            EnqueuePass(m_RenderOpaqueForwardPass);
+            EnqueuePass(m_RenderTransparentForwardPass);
         }
 
         public override void FinishRendering(CommandBuffer cmd)
@@ -107,7 +59,73 @@ namespace UnityEngine.Rendering.Universal
             cmd.SetGlobalInt("UNITY_DataExtraction_Mode", 0);
             cmd.SetGlobalInt("UNITY_DataExtraction_Space", 0);
         }
+    }
 
-        public bool IsSelectionMaskRequest => m_Mode == Camera.RenderRequestMode.SelectionMask;
+    internal class RenderRequestSelectionMaskRenderer : ScriptableRenderer
+    {
+        DrawObjectsPass m_RenderOpaqueForwardPassZDisabled;
+        DrawObjectsPass m_RenderTransparentForwardPassZDisabled;
+
+        LayerMask m_OpaqueLayerMask = -1;
+        LayerMask m_TransparentLayerMask = -1;
+        StencilState m_DefaultStencilState = StencilState.defaultValue;
+
+        private const float kSelectionZFudgeFactor = -0.02f;
+
+        public RenderRequestSelectionMaskRenderer(RenderRequestRendererData data) : base(data)
+        {
+            Debug.Assert(data.request.mode == Camera.RenderRequestMode.SelectionMask, "RenderRequestSelectionMaskRenderer should only be used with RenderRequestMode.SelectionMask");
+
+            var shaderTags = new[] {new ShaderTagId("DataExtraction")};
+
+            // There is no Z buffer for selection mask, use blending to make sure "unoccluded" (G = 1) values
+            // always remain on top when both occluded and unoccluded objects overlap.
+
+            m_RenderOpaqueForwardPassZDisabled = new DrawObjectsPass("Render Opaques (Z disabled)", shaderTags, true, RenderPassEvent.BeforeRenderingOpaques, RenderQueueRange.opaque, m_OpaqueLayerMask, m_DefaultStencilState, 0 );
+            m_RenderTransparentForwardPassZDisabled = new DrawObjectsPass("Render Transparents (Z disabled)", shaderTags, false, RenderPassEvent.BeforeRenderingTransparents, RenderQueueRange.transparent, m_TransparentLayerMask, m_DefaultStencilState, 0);
+
+            var state = m_RenderOpaqueForwardPassZDisabled.RenderStateBlock;
+            state.rasterState = new RasterState(CullMode.Back, 0, kSelectionZFudgeFactor);
+            state.blendState = new BlendState
+            {
+                blendState0 = new RenderTargetBlendState
+                {
+                    colorBlendOperation = BlendOp.Max,
+                    sourceColorBlendMode = BlendMode.One,
+                    destinationColorBlendMode = BlendMode.One,
+                    alphaBlendOperation = BlendOp.Max,
+                    sourceAlphaBlendMode = BlendMode.One,
+                    destinationAlphaBlendMode = BlendMode.One,
+                    writeMask = ColorWriteMask.Green | ColorWriteMask.Blue | ColorWriteMask.Alpha,
+                }
+            };
+            state.depthState = new DepthState {compareFunction = CompareFunction.Always, writeEnabled = false};
+            state.mask = RenderStateMask.Everything;
+            m_RenderOpaqueForwardPassZDisabled.RenderStateBlock = state;
+            m_RenderTransparentForwardPassZDisabled.RenderStateBlock = state;
+
+            List<Tuple<string, int>> values = new List<Tuple<string, int>>
+            {
+                new Tuple<string, int>("UNITY_DataExtraction_Mode", (int)data.request.mode),
+                new Tuple<string, int>("UNITY_DataExtraction_Space", (int)data.request.outputSpace),
+            };
+
+            m_RenderOpaqueForwardPassZDisabled.SetAdditionalValues(values);
+            m_RenderTransparentForwardPassZDisabled.SetAdditionalValues(values);
+        }
+
+        /// <inheritdoc />
+        public override void Setup(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            EnqueuePass(m_RenderOpaqueForwardPassZDisabled);
+            EnqueuePass(m_RenderTransparentForwardPassZDisabled);
+        }
+
+        public override void FinishRendering(CommandBuffer cmd)
+        {
+            base.FinishRendering(cmd);
+            cmd.SetGlobalInt("UNITY_DataExtraction_Mode", 0);
+            cmd.SetGlobalInt("UNITY_DataExtraction_Space", 0);
+        }
     }
 }
