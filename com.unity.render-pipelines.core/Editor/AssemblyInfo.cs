c12129437be685e5764c53f50ec86f1ead708bec
Simon Bouvier-Zappa <simonbz@unity3d.com>
2021-05-06 15:55:06 -0400

    Removed obsolete AssemblyInfo.cs.

--- com.unity.render-pipelines.core/Editor/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Runtime.CompilerServices;
-
-// remove as needed when/if MaterialUpgrader.RenameType can be made public
-[assembly: InternalsVisibleTo("Unity.RenderPipelines.Universal.Editor")]
-[assembly: InternalsVisibleTo("Unity.Testing.SRP.Universal-Upgrade.Editor")]
