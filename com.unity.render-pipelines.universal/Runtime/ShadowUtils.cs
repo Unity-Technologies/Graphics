53da414954f76d00037e6204322a26f467a05950
Graphine Charles <charles@graphinesoftware.com>
2021-05-07 10:58:34 +0200

    Update to follow recent changes to the depthformat branch.

--- com.unity.render-pipelines.universal/Runtime/ShadowUtils.cs
+++ com.unity.render-pipelines.universal/Runtime/ShadowUtils.cs
@@ -262,7 +262,7 @@ namespace UnityEngine.Rendering.Universal
         {
 
             var format = Experimental.Rendering.GraphicsFormatUtility.GetDepthStencilFormat(bits, 0);
-            RenderTextureDescriptor rtd = new RenderTextureDescriptor(width, height, format, Experimental.Rendering.GraphicsFormat.None);
+            RenderTextureDescriptor rtd = new RenderTextureDescriptor(width, height, Experimental.Rendering.GraphicsFormat.None, format);
             rtd.shadowSamplingMode = (RenderingUtils.SupportsRenderTextureFormat(RenderTextureFormat.Shadowmap)
                                         && (SystemInfo.graphicsDeviceType != GraphicsDeviceType.OpenGLES2)) ?
                                      ShadowSamplingMode.CompareDepths : ShadowSamplingMode.None;
