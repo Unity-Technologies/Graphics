6ea27b32c1c177e54fc2e8d5aec8d9a38f1510ac
Tianliang Ning <paulan@unity3d.com>
2021-05-07 16:46:57 -0700

    Baked GI sceneview debug modes now work for instanced terrain in Universal.

--- com.unity.render-pipelines.universal/Shaders/Terrain/TerrainLitMetaPass.hlsl
+++ com.unity.render-pipelines.universal/Shaders/Terrain/TerrainLitMetaPass.hlsl
@@ -6,20 +6,26 @@
 struct Attributes
 {
     float4 positionOS   : POSITION;
+    float3 normalOS     : NORMAL;
     float2 uv0          : TEXCOORD0;
     float2 uv1          : TEXCOORD1;
     float2 uv2          : TEXCOORD2;
+    UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
 struct Varyings
 {
     float4 positionCS   : SV_POSITION;
     float2 uv           : TEXCOORD0;
+    //UNITY_VERTEX_OUTPUT_STEREO
 };
 
 Varyings TerrainVertexMeta(Attributes input)
 {
     Varyings output;
+    UNITY_SETUP_INSTANCE_ID(input);
+    //UNITY_TRANSFER_INSTANCE_ID(input, output);
+    TerrainInstancing(input.positionOS, input.normalOS, input.uv0);
     output.positionCS = MetaVertexPosition(input.positionOS, input.uv1, input.uv2,
         unity_LightmapST, unity_DynamicLightmapST);
     output.uv = TRANSFORM_TEX(input.uv0, _MainTex);
