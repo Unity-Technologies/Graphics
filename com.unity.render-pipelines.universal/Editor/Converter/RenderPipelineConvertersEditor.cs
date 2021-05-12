554a15c9a183fade37030f2a043031a70d383d69
Sven Santema <svens@hq.unity3d.com>
2021-05-12 11:40:29 +0200

    Create search index async

--- com.unity.render-pipelines.universal/Editor/Converter/RenderPipelineConvertersEditor.cs
+++ com.unity.render-pipelines.universal/Editor/Converter/RenderPipelineConvertersEditor.cs
@@ -2,12 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
-using UnityEditor;
-using UnityEditor.Rendering;
 using UnityEditor.Search;
 using UnityEditor.UIElements;
 using UnityEngine.UIElements;
-
+using UnityEngine.Assertions;
 
 namespace UnityEditor.Rendering.Universal
 {
@@ -55,7 +53,8 @@ namespace UnityEditor.Rendering.Universal
         public int success;
         internal int index;
 
-        public bool shouldInit => !isInitialized && isEnabled && isActive;
+        public bool isActiveAndEnabled => isEnabled && isActive;
+        public bool requiresInitialization => !isInitialized && isActiveAndEnabled;
     }
 
     [Serializable]
@@ -279,17 +278,17 @@ namespace UnityEditor.Rendering.Universal
             button.RegisterCallback<ClickEvent>(Convert);
 
             var initButton = rootVisualElement.Q<Button>("initializeButton");
-            initButton.RegisterCallback<ClickEvent>(Init);
+            initButton.RegisterCallback<ClickEvent>(InitializeAllActiveConverters);
         }
 
-        void GetAndSetData(int i)
+        void GetAndSetData(int i, Action OnAllConvertersCompleted = null)
         {
             // This need to be in Init method
             // Need to get the assets that this converter is converting.
             // Need to return Name, Path, Initial info, Help link.
             // New empty list of ConverterItemInfos
             List<ConverterItemDescriptor> converterItemInfos = new List<ConverterItemDescriptor>();
-            var initCtx = new InitializeConverterContext {items = converterItemInfos};
+            var initCtx = new InitializeConverterContext { items = converterItemInfos };
 
             var conv = m_CoreConvertersList[i];
 
@@ -298,109 +297,184 @@ namespace UnityEditor.Rendering.Universal
             // This should also go to the init method
             // This will fill out the converter item infos list
             int id = i;
-            conv.OnInitialize(initCtx,
-                () =>
+            conv.OnInitialize(initCtx, OnConverterCompleteDataCollection);
+
+            void OnConverterCompleteDataCollection()
+            {
+                // Set the item infos list to to the right index
+                m_ItemsToConvert[id] = converterItemInfos;
+                m_ConverterStates[id].items = new List<ConverterItemState>(converterItemInfos.Count);
+
+                // Default all the entries to true
+                for (var j = 0; j < converterItemInfos.Count; j++)
                 {
-                    // Set the item infos list to to the right index
-                    m_ItemsToConvert[id] = converterItemInfos;
-                    m_ConverterStates[id].items = new List<ConverterItemState>(converterItemInfos.Count);
-
-                    // Default all the entries to true
-                    for (var j = 0; j < converterItemInfos.Count; j++)
+                    string message = string.Empty;
+                    Status status;
+                    bool active = true;
+                    // If this data hasn't been filled in from the init phase then we can assume that there are no issues / warnings
+                    if (string.IsNullOrEmpty(converterItemInfos[j].warningMessage))
                     {
-                        string message = string.Empty;
-                        Status status;
-                        bool active = true;
-                        // If this data hasn't been filled in from the init phase then we can assume that there are no issues / warnings
-                        if (string.IsNullOrEmpty(converterItemInfos[j].warningMessage))
-                        {
-                            status = Status.Pending;
-                        }
-                        else
-                        {
-                            status = Status.Warning;
-                            message = converterItemInfos[j].warningMessage;
-                            active = false;
-                            m_ConverterStates[id].warnings++;
-                        }
-
-                        m_ConverterStates[id].items.Add(new ConverterItemState
-                        {
-                            isActive = active,
-                            message = message,
-                            status = status,
-                            hasConverted = false,
-                        });
+                        status = Status.Pending;
+                    }
+                    else
+                    {
+                        status = Status.Warning;
+                        message = converterItemInfos[j].warningMessage;
+                        active = false;
+                        m_ConverterStates[id].warnings++;
                     }
 
-                    m_ConverterStates[id].isLoading = false;
-                    m_ConverterStates[id].isInitialized = true;
+                    m_ConverterStates[id].items.Add(new ConverterItemState
+                    {
+                        isActive = active,
+                        message = message,
+                        status = status,
+                        hasConverted = false,
+                    });
+                }
 
-                    // Making sure that the pending amount is set to the amount of items needs converting
-                    m_ConverterStates[id].pending = m_ConverterStates[id].items.Count;
+                m_ConverterStates[id].isLoading = false;
+                m_ConverterStates[id].isInitialized = true;
 
-                    EditorUtility.SetDirty(this);
-                    m_SerializedObject.ApplyModifiedProperties();
-                });
-        }
+                // Making sure that the pending amount is set to the amount of items needs converting
+                m_ConverterStates[id].pending = m_ConverterStates[id].items.Count;
 
-        void OnSearchIndexCreated(string name, string path, IEnumerable<SearchItem> items, Action finished)
-        {
-            var siObj = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(path);
-            Debug.Log($"Search index {name} is ready to be used", siObj);
+                EditorUtility.SetDirty(this);
+                m_SerializedObject.ApplyModifiedProperties();
 
-            // TODO: Run your query on the temporary index
-            var searchQuery = $"p: a={name} t:Material";
-            Debug.Log($"Listing materials using <b>{searchQuery}</b>");
-            UnityEditor.Search.SearchService.Request(searchQuery, (context, materials) =>
-            {
-                // Process materials...
-                foreach (var m in materials)
-                    Debug.Log(m.GetDescription(context));
-            }, _ =>
-                {
-                    AllQueriesFinished();
-                });
-
-            void AllQueriesFinished()
-            {
-                Type type = typeof(EditorApplication);
-                EditorApplication.CallbackFunction callback = () =>
-                {
-                    Debug.Log($"Project upgrading finished");
-                    finished();
-                };
-                MethodInfo delayedMethod = type.GetMethod("CallDelayed", BindingFlags.NonPublic | BindingFlags.Static);
-                object[] parameters = new object[] {callback, 3d};
-
-                delayedMethod?.Invoke(null, parameters);
-            }
-        }
-
-        void Init(ClickEvent evt)
-        {
-            // Need to check if the indexing should be created or not
-            bool createIndex = ShouldCreateIndex();
-
-            if (createIndex)
-            {
-                CreateSearchIndex(m_URPConverterIndex, OnSearchIndexCreated);
+                CheckAllConvertersCompleted();
             }
 
-            for (int i = 0; i < m_ConverterStates.Count; ++i)
+            void CheckAllConvertersCompleted()
             {
-                if (m_ConverterStates[i].shouldInit)
+                int convertersToIntialize = 0;
+                int convertersInitialized = 0;
+
+                for (var j = 0; j < m_ConverterStates.Count; j++)
                 {
-                    GetAndSetData(i);
+                    var converter = m_ConverterStates[j];
+
+                    // Skip inactive converters
+                    if (!converter.isActiveAndEnabled)
+                        continue;
+
+                    if (converter.isInitialized)
+                        convertersInitialized++;
+                    else
+                        convertersToIntialize++;
+                }
+
+                var sum = convertersToIntialize + convertersInitialized;
+
+                Assert.IsFalse(sum == 0);
+
+                // Show our progress so far
+                EditorUtility.ClearProgressBar();
+                EditorUtility.DisplayProgressBar($"Initializing converters", $"Initializing converters ({convertersInitialized}/{sum})...", (float)convertersInitialized / sum);
+
+                // If all converters are inialized call the complete callback
+                if (convertersToIntialize == 0)
+                {
+                    OnAllConvertersCompleted?.Invoke();
                 }
             }
         }
 
-        bool ShouldCreateIndex()
+        void InitializeAllActiveConverters(ClickEvent evt)
+        {
+            // If we use search index, go async
+            if (true || ShouldCreateSearchIndex())
+            {
+                CreateSearchIndex(m_URPConverterIndex);
+            }
+            // Otherwise do everything directly
+            else
+            {
+                ConverterCollectData();
+            }
+
+            void CreateSearchIndex(string name)
+            {
+                // Create <guid>.index in the project
+                var title = $"Building {name} search index";
+                EditorUtility.DisplayProgressBar(title, "Creating search index...", -1f);
+
+                // Private implementation of a file naming function which puts the file at the selected path.
+                Type assetdatabase = typeof(AssetDatabase);
+                var indexPath = (string)assetdatabase.GetMethod("GetUniquePathNameAtSelectedPath", BindingFlags.NonPublic | BindingFlags.Static).Invoke(assetdatabase, new object[] { name });
+
+                // Write search index manifest
+                System.IO.File.WriteAllText(indexPath,
+                @"{
+                ""roots"": [""Assets""],
+                ""includes"": [],
+                ""excludes"": [],
+                ""options"": {
+                    ""types"": true,
+                    ""properties"": true,
+                    ""extended"": true,
+                    ""dependencies"": true
+                    },
+                ""baseScore"": 9999
+                }");
+
+                // Import the search index
+                AssetDatabase.ImportAsset(indexPath, ImportAssetOptions.ForceSynchronousImport | ImportAssetOptions.DontDownloadFromCacheServer);
+
+                EditorApplication.delayCall += () =>
+                {
+                    // Create dummy request to ensure indexing ahs finished
+                    var context = Search.SearchService.CreateContext("asset", $"p: a=\"{name}\"");
+                    Search.SearchService.Request(context, (_, items) =>
+                    {
+                        OnSearchIndexCreated(name, indexPath, items, () =>
+                        {
+                            context?.Dispose();
+                            context = null;
+                            Debug.Log($"Deleted search index at path {indexPath}");
+                            // Client code has finished with the created index. We can delete it.
+                            AssetDatabase.DeleteAsset(indexPath);
+                            EditorUtility.ClearProgressBar();
+                        });
+                    });
+                };
+            }
+
+            void OnSearchIndexCreated(string name, string path, IEnumerable<SearchItem> items, Action onComplete)
+            {
+                var siObj = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(path);
+                Debug.Log($"Search index {name} is ready to be used", siObj);
+                EditorUtility.ClearProgressBar();
+
+                ConverterCollectData(onComplete);
+            }
+
+            void ConverterCollectData(Action OnConverterDataCollectionComplete = null)
+            {
+                EditorUtility.DisplayProgressBar($"Initializing converters", $"Initializing converters...", -1f);
+
+                int convertersTocConvert = 0;
+                for (int i = 0; i < m_ConverterStates.Count; ++i)
+                {
+                    if (m_ConverterStates[i].requiresInitialization)
+                    {
+                        convertersTocConvert++;
+                        GetAndSetData(i, OnConverterDataCollectionComplete);
+                    }
+                }
+
+                // If we did not kick off any converter intialization
+                // We can complete everything immediately
+                if(convertersTocConvert == 0)
+                    OnConverterDataCollectionComplete();
+            }
+        }
+
+        bool ShouldCreateSearchIndex()
         {
             for (int i = 0; i < m_ConverterStates.Count; ++i)
             {
-                if (m_ConverterStates[i].shouldInit)
+                if (m_ConverterStates[i].requiresInitialization)
                 {
                     var converter = m_CoreConvertersList[i];
                     if (converter.NeedsIndexing)
@@ -503,68 +577,6 @@ namespace UnityEditor.Rendering.Universal
                 m_CoreConvertersList[coreConverterIndex].OnRun(ref ctx);
                 UpdateInfo(coreConverterIndex, ctx);
             }
-        }
-
-        /// <summary>
-        /// Handler called when the temporary search index is created and ready to be used.
-        /// </summary>
-        /// <param name="name">Name of the search index</param>
-        /// <param name="path">Asset path of the temporary index</param>
-        /// <param name="items">Search results of the temporary index.</param>
-        /// <param name="finished">Callback to be invoked when you are done using the temporary index.</param>
-        public delegate void SearchIndexCreatedHandler(string name, string path, IEnumerable<SearchItem> items, Action finished);
-
-        /// <summary>
-        /// Create a search index. When the search index is ready to be used, callback the user code.
-        /// IMPORTANT: The user code must call finished() when the search index can be disposed.
-        /// </summary>
-        /// <param name="name"></param>
-        /// <param name="onIndexReady"></param>
-        public static void CreateSearchIndex(string name, SearchIndexCreatedHandler onIndexReady)
-        {
-            // Create <guid>.index in the project
-            var title = $"Building {name} search index";
-            EditorUtility.DisplayProgressBar(title, "Creating search index...", -1f);
-
-            // Private implementation of a file naming function which puts the file at the selected path.
-            Type assetdatabase = typeof(AssetDatabase);
-            var indexPath = (string)assetdatabase.GetMethod("GetUniquePathNameAtSelectedPath", BindingFlags.NonPublic | BindingFlags.Static).Invoke(assetdatabase, new object[] {name});
-
-            // Write search index manifest
-            System.IO.File.WriteAllText(indexPath,
-                @"{
-                ""roots"": [""Assets""],
-                ""includes"": [],
-                ""excludes"": [],
-                ""options"": {
-                    ""types"": true,
-                    ""properties"": true,
-                    ""extended"": true,
-                    ""dependencies"": true
-                    },
-                ""baseScore"": 9999
-                }");
-
-            // Import the search index
-            AssetDatabase.ImportAsset(indexPath, ImportAssetOptions.ForceSynchronousImport | ImportAssetOptions.DontDownloadFromCacheServer);
-            EditorApplication.delayCall += () =>
-            {
-                // Wait for the index to be finished
-                var context = Search.SearchService.CreateContext("asset", $"p: a=\"{name}\"");
-                Search.SearchService.Request(context, (_, items) =>
-                {
-                    // Raise onIndexReady callback
-                    onIndexReady?.Invoke(name, indexPath, items, () =>
-                    {
-                        context?.Dispose();
-                        context = null;
-
-                        // Client code has finished with the created index. We can delete it.
-                        AssetDatabase.DeleteAsset(indexPath);
-                        EditorUtility.ClearProgressBar();
-                    });
-                });
-            };
         }
     }
 }
