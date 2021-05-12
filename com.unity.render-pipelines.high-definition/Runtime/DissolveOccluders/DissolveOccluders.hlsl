101270129031740ec8aff6b7593524fbb44e1475
Szymon Swistun <szymon@bonfirestudios.com>
2021-05-11 22:16:12 -0700

    New influence map based dissolve occlusion 2.0 shader code

--- com.unity.render-pipelines.high-definition/Runtime/DissolveOccluders/DissolveOccluders.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/DissolveOccluders/DissolveOccluders.hlsl
@@ -4,6 +4,10 @@
 // custom-begin:
 #include "Packages/com.unity.render-pipelines.high-definition/Runtime/DissolveOccluders/DissolveOccludersData.cs.hlsl"
 
+#if defined(HIGH_DEFINITION_EXTENSIONS_ENABLED)
+#include "Packages/com.unity.render-pipelines.high-definition-extensions/Runtime/DissolveOccluders/DissolveOccludersCustom.hlsl"
+#endif
+
 StructuredBuffer<DissolveOccludersCylinder> _DissolveOccludersCylinders;
 int _DissolveOccludersCylindersCount;
 float2 _DissolveOccludersFadeHeightScaleBias;
@@ -28,11 +32,56 @@ float ComputeAlphaFromDissolveOccluders(const in PositionInputs posInput, const 
     return max(alphaMin, _DissolveOnOcclusionOpacity);
 }
 
-void ClipFromDissolveOccluders(const in PositionInputs posInput, const in float4 screenSize)
+void ClipFromDissolveOccludersBase(const in PositionInputs posInput, const in float4 screenSize)
 {
     float dither = LoadBlueNoiseRGB((uint2)posInput.positionSS.xy).z;
     float alpha = ComputeAlphaFromDissolveOccluders(posInput, screenSize);
     clip(alpha - max(1e-5f, dither));
+}
+
+void ClipFromDissolveLOS(const in PositionInputs posInput, const in float4 screenSize)
+{
+    float los = 0;
+    float3 worldPos = GetAbsolutePositionWS(posInput.positionWS);
+    
+    // multi-tap version
+    float3 clipCameraPos = _WorldSpaceCameraPos;
+    float3 clipWorldPos = worldPos;
+    float elevationMin = _InfluenceMapObserverPosition.y + 0.5;
+    float elevationDist = max(0, worldPos.y - elevationMin);
+    if(elevationDist > 0)
+    {
+        const int NUM_STEPS = 4;
+        float elevationStep = elevationDist / NUM_STEPS;
+        for(int i=0; i < NUM_STEPS; ++i)
+        {        
+            clipWorldPos.y -= elevationStep;
+            clipCameraPos.y -= elevationStep;
+            float3 worldView = normalize(clipWorldPos - clipCameraPos);
+            float3 raycastWorldPos = InfluenceFloorRaycast(worldView, clipCameraPos);
+            float losSample = SampleInfluenceLOS(raycastWorldPos);
+            los = max(losSample, los);
+        }        
+    }
+
+    // ideal single tap version
+    //float3 worldView = normalize(worldPos - _WorldSpaceCameraPos);
+    //float3 raycastWorldPos = InfluenceFloorRaycast(worldView, _WorldSpaceCameraPos);
+    //los = SampleInfluenceLOS(raycastWorldPos);
+
+    float fadeAmount = 1.0 - saturate(((_InfluenceMapObserverPosition.y + 1.5) - worldPos.y ) * 4);
+    float finalDissolve = (1.0 - saturate(los * fadeAmount));
+    ClipFromLOS(posInput, _ScreenSize, finalDissolve);    
+}
+
+void ClipFromDissolveOccluders(const in PositionInputs posInput, const in float4 screenSize)
+{
+#if defined(CLIP_FROM_DISSOLVE_OCCLUDERS_CUSTOM)
+    if (!TryClipFromDissolveOccludersCustom(posInput, screenSize))
+#endif
+    {
+        ClipFromDissolveOccludersBase(posInput, screenSize);
+    } 
 }
 
 #define _EVALUATE_DISSOLVE_ON_OCCLUSION (defined(_ENABLE_DISSOLVE_ON_OCCLUSION) && (SHADERPASS == SHADERPASS_GBUFFER || SHADERPASS == SHADERPASS_DEPTH_ONLY || SHADERPASS == SHADERPASS_MOTION_VECTORS || SHADERPASS == SHADERPASS_FORWARD || SHADERPASS == SHADERPASS_FORWARD_UNLIT || SHADERPASS == SHADERPASS_DISTORTION))
