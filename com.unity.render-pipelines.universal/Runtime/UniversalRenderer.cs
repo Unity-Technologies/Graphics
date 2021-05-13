cea23bdb52054e0e7d525b50c2a4dd2b8534f48b
Kay Chang <kaychang@unity3d.com>
2021-05-13 15:20:07 -0700

    Fixed bug where CopyDepthPass was always enqueued when deferred rendering mode was selected. It should only be enqueued when the UniversalRenderPipelineAsset requires depthTexture output.

--- com.unity.render-pipelines.universal/Runtime/UniversalRenderer.cs
+++ com.unity.render-pipelines.universal/Runtime/UniversalRenderer.cs
@@ -406,7 +406,7 @@ namespace UnityEngine.Rendering.Universal
             // TODO: We could cache and generate the LUT before rendering the stack
             bool generateColorGradingLUT = cameraData.postProcessEnabled && m_PostProcessPasses.isCreated;
             bool isSceneViewCamera = cameraData.isSceneViewCamera;
-            bool requiresDepthTexture = cameraData.requiresDepthTexture || renderPassInputs.requiresDepthTexture || (this.actualRenderingMode == RenderingMode.Deferred && !useRenderPassEnabled) || m_DepthPrimingMode == DepthPrimingMode.Forced;
+            bool requiresDepthTexture = cameraData.requiresDepthTexture || renderPassInputs.requiresDepthTexture || m_DepthPrimingMode == DepthPrimingMode.Forced;
 
 #if UNITY_EDITOR
             bool isGizmosEnabled = UnityEditor.Handles.ShouldRenderGizmos();
@@ -452,7 +452,7 @@ namespace UnityEngine.Rendering.Universal
             // around a bug where during gbuffer pass (MRT pass), the camera depth attachment is correctly bound, but during
             // deferred pass ("camera color" + "camera depth"), the implicit depth surface of "camera color" is used instead of "camera depth",
             // because BuiltinRenderTextureType.CameraTarget for depth means there is no explicit depth attachment...
-            bool createDepthTexture = requiresDepthTexture && !requiresDepthPrepass;
+            bool createDepthTexture = requiresDepthTexture && !requiresDepthPrepass || (this.actualRenderingMode == RenderingMode.Deferred && !useRenderPassEnabled);
             createDepthTexture |= (cameraData.renderType == CameraRenderType.Base && !cameraData.resolveFinalTarget);
             // Deferred renderer always need to access depth buffer.
             createDepthTexture |= (this.actualRenderingMode == RenderingMode.Deferred && !useRenderPassEnabled);
