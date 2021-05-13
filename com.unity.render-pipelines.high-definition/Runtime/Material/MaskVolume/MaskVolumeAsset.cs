2cf9d71da19d19315f5cfeeef33ee06434b16146
Andrew Saraev <67020478+AndrewSaraevUnity@users.noreply.github.com>
2021-05-13 10:11:00 +0100

    Probe and Mask Volumes binary serialization and minor fixes (#26)
    * Fixed the case when the painted Mask Volume asset is not saved.
    Updated the default brush settings based on the user feedback.
    
    * Prefer binary serialization of Probe Volume and Mask Volume assets.
    
    * Added "Reserialize All" action to context menus of ProbeVolumeAsset and MaskVolumeAsset to be able to convert all assets in a project to binary format.

--- com.unity.render-pipelines.high-definition/Runtime/Material/MaskVolume/MaskVolumeAsset.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Material/MaskVolume/MaskVolumeAsset.cs
@@ -2,8 +2,13 @@ using System;
 using UnityEngine.Serialization;
 using UnityEngine.SceneManagement;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace UnityEngine.Rendering.HighDefinition
 {
+    [PreferBinarySerialization]
     internal class MaskVolumeAsset : ScriptableObject
     {
         [Serializable]
@@ -262,6 +267,24 @@ namespace UnityEngine.Rendering.HighDefinition
             this.dilationIterations = dilationIterations;
         }
         */
+
+        [ContextMenu("Reserialize All")]
+        void ReserializeAll()
+        {
+            const string k_ProgressBarTitle = "Reserializing all Mask Volume assets";
+            EditorUtility.DisplayProgressBar(k_ProgressBarTitle, "Searching assets", 1f / 6f);
+            var assetGuids = AssetDatabase.FindAssets("t:" + nameof(MaskVolumeAsset));
+            EditorUtility.DisplayProgressBar(k_ProgressBarTitle, "Loading assets", 3f / 6f);
+            for (int i = 0; i < assetGuids.Length; i++)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(assetGuids[i]);
+                var asset = AssetDatabase.LoadAssetAtPath<MaskVolumeAsset>(path);
+                EditorUtility.SetDirty(asset);
+            }
+            EditorUtility.DisplayProgressBar(k_ProgressBarTitle, "Saving assets", 5f / 6f);
+            AssetDatabase.SaveAssets();
+            EditorUtility.ClearProgressBar();
+        }
 #endif
     }
 }
