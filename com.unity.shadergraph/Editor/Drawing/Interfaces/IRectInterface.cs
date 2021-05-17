555cb7425d31ee0f60d00681f99fc27d4a87c7ee
Joshua Davis <joshua.davis@unity3d.com>
2021-05-17 11:22:29 -0700

    Removed unneeded usings and fixed a compilation error

--- com.unity.shadergraph/Editor/Drawing/Interfaces/IRectInterface.cs
+++ com.unity.shadergraph/Editor/Drawing/Interfaces/IRectInterface.cs
@@ -1,14 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEditor.Graphing;
-using UnityEditor.ShaderGraph.Drawing.Colors;
-using UnityEditor.ShaderGraph.Internal;
-using UnityEditor.ShaderGraph.Drawing;
-using UnityEditor.ShaderGraph.Serialization;
-using UnityEngine.Assertions;
-using UnityEngine.Pool;
 
 namespace UnityEditor.ShaderGraph
 {
