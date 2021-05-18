f5d8982ecf47ef5e2390a884406c3559523f1f83
jfryer_unity <julienf@unity3D.com>
2021-05-18 11:46:59 +0200

    Add API doc

--- com.unity.visualeffectgraph/Runtime/Types/VFXTypes.cs
+++ com.unity.visualeffectgraph/Runtime/Types/VFXTypes.cs
@@ -7,15 +7,24 @@ using UnityEngine.VFX;
 
 namespace UnityEngine.VFX
 {
+    /// <summary>
+    /// Attribute to define a VFXType that will be usable in VFX Graph view.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Struct, AllowMultiple = false)]
     public class VFXTypeAttribute : Attribute
     {
+        /// <summary>Usage of VFXType.</summary>
         [Flags]
         public enum Usage
         {
+            /// <summary>Usable as a port type.</summary>
             Default,
+            /// <summary>Usable as a GraphicsBuffer data type.</summary>
             GraphicsBuffer
         }
+
+        /// <summary>Constructor of VFXType.</summary>
+        /// <param name="usages">Usage of the VFXType.</param>
         public VFXTypeAttribute(Usage usages = Usage.Default)
         {
             this.usages = usages;
