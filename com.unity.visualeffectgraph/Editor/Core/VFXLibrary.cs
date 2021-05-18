66752b92e0905d3c5e3905ee60a208b1e1c21247
Jennifer Roig-Deslandes <jenniferd@unity3d.com>
2021-05-18 12:38:40 +0200

    [VFX] make sure to use the now public callback activeRenderPipelineTypeChanged

--- com.unity.visualeffectgraph/Editor/Core/VFXLibrary.cs
+++ com.unity.visualeffectgraph/Editor/Core/VFXLibrary.cs
@@ -508,15 +508,7 @@ namespace UnityEditor.VFX
         [InitializeOnLoadMethod]
         private static void RegisterSRPChangeCallback()
         {
-            EventInfo onRPChanged = typeof(RenderPipelineManager).GetEvent("activeRenderPipelineTypeChanged", BindingFlags.NonPublic | BindingFlags.Static);
-            if (onRPChanged != null)
-            {
-                MethodInfo addHandler = onRPChanged.GetAddMethod(nonPublic: true);
-                addHandler.Invoke(null, new Action[] { SRPChanged });
-            }
-
-            // Once activeRenderPipelineTypeChanged is public don't use reflection anymore
-            //RenderPipelineManager.activeRenderPipelineTypeChanged += OnSRPChanged;
+            RenderPipelineManager.activeRenderPipelineTypeChanged += OnSRPChanged;
         }
 
         public delegate void OnSRPChangedEvent();
