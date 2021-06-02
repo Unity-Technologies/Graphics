012f8c978f922ed134b69f686e66fd16b20a1ec9
Jonas Sideravicius <jonass@unity3d.com>
2021-06-02 11:14:23 +0300

    Making Gbuffer attachments memoryless

--- com.unity.render-pipelines.universal/Runtime/NativeRenderPass.cs
+++ com.unity.render-pipelines.universal/Runtime/NativeRenderPass.cs
@@ -453,8 +453,10 @@ namespace UnityEngine.Rendering.Universal
                 }
 
                 // Assume input attachment has to be transient as ScriptableRenderPass currently has only setters for StoreAction
+                // We also change the target of the descriptor for it to be initialized engine-side as a transient resource.
                 m_ActiveColorAttachmentDescriptors[pass.m_InputAttachmentIndices[i]].loadAction = RenderBufferLoadAction.DontCare;
                 m_ActiveColorAttachmentDescriptors[pass.m_InputAttachmentIndices[i]].storeAction = RenderBufferStoreAction.DontCare;
+                m_ActiveColorAttachmentDescriptors[pass.m_InputAttachmentIndices[i]].loadStoreTarget = BuiltinRenderTextureType.None;
             }
         }
 
