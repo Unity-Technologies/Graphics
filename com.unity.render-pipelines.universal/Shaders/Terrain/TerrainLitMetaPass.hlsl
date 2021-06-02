e1dec759f985500bb4d78454d77c50dcfb38975d
Tianliang Ning <paulan@unity3d.com>
2021-06-02 13:14:11 -0700

    Use uv0 for uv1 and uv2 when rendering terrain meta passes is URP

--- com.unity.render-pipelines.universal/Shaders/Terrain/TerrainLitMetaPass.hlsl
+++ com.unity.render-pipelines.universal/Shaders/Terrain/TerrainLitMetaPass.hlsl
@@ -8,6 +8,8 @@ Varyings TerrainVertexMeta(Attributes input)
     Varyings output;
     UNITY_SETUP_INSTANCE_ID(input);
     TerrainInstancing(input.positionOS, input.normalOS, input.uv0);
+    // For some reason, uv1 and uv2 are not populated for instanced terrain. Use uv0.
+    input.uv1 = input.uv2 = input.uv0;
     output = UniversalVertexMeta(input);
     return output;
 }
