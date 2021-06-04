43b03244e2e732dc482f8ea6ea4e27da9fdce2cf
thomas-zeng <thomas.zeng@unity3d.com>
2021-06-04 13:20:28 -0700

    * Fixed an issue where system generated semantics are placed before non system generated semantics.

--- com.unity.shadergraph/Editor/Generation/Processors/GenerationUtils.cs
+++ com.unity.shadergraph/Editor/Generation/Processors/GenerationUtils.cs
@@ -154,6 +154,10 @@ namespace UnityEditor.ShaderGraph
                     //
                     if (subscript.HasPreprocessor() && (subscript.preprocessor.Contains("SHADER_STAGE_FRAGMENT")))
                         postUnpackedSubscripts.Add(subscript);
+                    // special case, "UNITY_STEREO_INSTANCING_ENABLED" fields must be packed at the end of the struct because they are system generated semantics
+                    //
+                    else if(subscript.HasPreprocessor() && (subscript.preprocessor.Contains("UNITY_STEREO_INSTANCING_ENABLED")))
+                        postUnpackedSubscripts.Add(subscript);
                     else if (subscript.HasSemantic() || subscript.vectorCount == 0)
                         packedSubscripts.Add(subscript);
                     else
