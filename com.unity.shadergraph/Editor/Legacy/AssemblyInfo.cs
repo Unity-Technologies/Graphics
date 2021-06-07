56c837b26a751febea817346d33dc244fa6e7c62
Esmeralda Salamone <esmeralda.salamone@unity3d.com>
2021-06-07 14:07:00 -0700

    move

--- com.unity.shadergraph/Editor/Legacy/AssemblyInfo.cs
+++ com.unity.shadergraph/Editor/Legacy/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Unity.ShaderGraph.Editor.Tests")]
+[assembly: InternalsVisibleTo("Unity.ShaderGraph.Legacy.Tests")]
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.Universal.Editor")]
 [assembly: InternalsVisibleTo("Unity.ShaderGraph.GraphicsTests")]
-[assembly: InternalsVisibleTo("Unity.ShaderGraph.Editor.GraphicsTests")]
+[assembly: InternalsVisibleTo("Unity.ShaderGraph.Legacy.GraphicsTests")]
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Editor")]
 [assembly: InternalsVisibleTo("Unity.VisualEffectGraph.Editor")]
 [assembly: InternalsVisibleTo("Unity.Industrial.Materials.AVRD.Editor")]
