753bdb8205107056599cee806b4fddb5a455edd4
Jessica Thomson <jessica.thomson@unity3d.com>
2021-06-08 13:16:58 -0700

    Fixed inverted logic in wrap utr

--- .yamato/tooling/wrap_utr_check_update_file.py
+++ .yamato/tooling/wrap_utr_check_update_file.py
@@ -8,16 +8,14 @@ if __name__ == "__main__":
     cur_sys = platform.system()
     output = 0
 
-    print("Working directory: " + os.getcwd())
-    print("Resources dir contents: " + os.listdir(path.join(sys.argv[1], "Assets/Resources/")))
-
     if cur_sys == 'Windows':
         output = subprocess.call(["utr.bat"] + sys.argv[2:])
     else:
         output = subprocess.call(["utr"] + sys.argv[2:])
 
     update_tests_file_path = path.join(sys.argv[1], "Assets/Resources/UpdateTests.txt")
-    if not path.exists(update_tests_file_path):
+    print(update_tests_file_path)
+    if path.exists(update_tests_file_path):
         print("Error: Test assets need to be updated")
         exit(1)
     exit(output)
