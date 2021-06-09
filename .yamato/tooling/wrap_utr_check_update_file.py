23edede0bc16a76673332b1a58e29550f7b13cea
Jessica Thomson <jessica.thomson@unity3d.com>
2021-06-08 21:38:17 -0700

    Switching to invoking utr with Path.home()

--- .yamato/tooling/wrap_utr_check_update_file.py
+++ .yamato/tooling/wrap_utr_check_update_file.py
@@ -2,6 +2,7 @@ import subprocess
 import platform
 import sys
 from os import path, listdir
+from pathlib import Path
 
 if __name__ == "__main__":
     cur_sys = platform.system()
@@ -9,9 +10,16 @@ if __name__ == "__main__":
 
     if cur_sys == 'Windows':
         output = subprocess.call(["utr.bat"] + sys.argv[2:])
-    else:
-        print(listdir())
+    elif cur_sys == 'Linux':
         output = subprocess.call(["utr"] + sys.argv[2:])
+    else:
+        print(listdir(".\\"))
+        # Path.home() is the same as ~
+        utr_path = path.join(str(Path.home()), "Graphics/utr")
+        print(utr_path)
+        output = subprocess.call([utr_path] + sys.argv[2:])
+
+
 
     update_tests_file_path = path.join(sys.argv[1], "Assets/Resources/UpdateTests.txt")
     print(update_tests_file_path)
