99d041144fae4b2cee065c6e93278000f12f7883
Théo Penavaire <theo.penavaire@unity3d.com>
2021-06-09 11:07:25 +0200

    Legit didn't start on a **Documentation** file

--- com.unity.render-pipelines.high-definition/Runtime/PackageInfo.cs
+++ com.unity.render-pipelines.high-definition/Runtime/PackageInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition-Tests.Runtime")]
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition-Tests.Editor")]
 [assembly: InternalsVisibleTo("Unity.GraphicTests.Performance.HDRP.Runtime")]
-[assembly: InternalsVisibleTo("Unity.GraphicTests.Performance.HDRP.Editor")]
+[assembly: InternalsVisibleTo("Unity.GraphicTests.Performance.HDRP.Editors")]
 [assembly: InternalsVisibleTo("TestRuntime")]
