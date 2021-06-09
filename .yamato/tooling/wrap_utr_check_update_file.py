bd871d91ad2ad338f3a57a8280e0516c37dfaa3a
Jessica Thomson <jessica.thomson@unity3d.com>
2021-06-08 21:10:41 -0700

    Added a debug listdir()

--- .yamato/tooling/wrap_utr_check_update_file.py
+++ .yamato/tooling/wrap_utr_check_update_file.py
@@ -1,8 +1,7 @@
-import os
 import subprocess
 import platform
 import sys
-from os import path
+from os import path, listdir
 
 if __name__ == "__main__":
     cur_sys = platform.system()
@@ -11,6 +10,7 @@ if __name__ == "__main__":
     if cur_sys == 'Windows':
         output = subprocess.call(["utr.bat"] + sys.argv[2:])
     else:
+        print(listdir())
         output = subprocess.call(["utr"] + sys.argv[2:])
 
     update_tests_file_path = path.join(sys.argv[1], "Assets/Resources/UpdateTests.txt")
