0c7fa2ed2e6b5fda0a37a1948440ff110f2ce5cb
martint-unity <martint@unity3d.com>
2021-06-10 15:51:48 +0200

    Updated the info icon size

--- com.unity.render-pipelines.universal/Editor/Converter/BuiltInToURPConverterContainer.cs
+++ com.unity.render-pipelines.universal/Editor/Converter/BuiltInToURPConverterContainer.cs
@@ -3,6 +3,6 @@ namespace UnityEditor.Rendering.Universal.Converters
     internal sealed class BuiltInToURPConverterContainer : RenderPipelineConverterContainer
     {
         public override string name => "Built-in to URP";
-        public override string info => "The Render Pipeline Conversion Wizard is meant to help Convert projects from built-in to Universal Render Pipeline.";
+        public override string info => "The Render Pipeline Convertor is meant to help convert projects from built-in to URP or upgrade URP assets.";
     }
 }
