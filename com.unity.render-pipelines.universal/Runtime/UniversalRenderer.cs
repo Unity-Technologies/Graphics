6cbc293c69045f61492982255da5af6da5dd63b5
Robin De Moor <robin.demoor@unity3d.com>
2021-06-14 11:27:08 +0200

    swapbuffer fix: chaneglog + formatting

--- com.unity.render-pipelines.universal/Runtime/UniversalRenderer.cs
+++ com.unity.render-pipelines.universal/Runtime/UniversalRenderer.cs
@@ -1087,7 +1087,6 @@ namespace UnityEngine.Rendering.Universal
 
             m_ActiveCameraColorAttachment = m_ColorBufferSystem.GetBackBuffer();
             cmd.SetGlobalTexture("_CameraColorTexture", m_ActiveCameraColorAttachment.id);
-
             //Set _AfterPostProcessTexture, users might still rely on this although it is now always the cameratarget due to swapbuffer
             cmd.SetGlobalTexture("_AfterPostProcessTexture", m_ActiveCameraColorAttachment.id);
         }
