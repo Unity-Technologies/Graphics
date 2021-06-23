34ea702fd3bf835b659b606ae7bb6e3311afbda2
Anis Benyoub <anis@unity3d.com>
2021-06-23 16:11:09 +0200

    Adding the cs hlsl

---
+++ com.unity.render-pipelines.high-definition/Runtime/VBuffer/HDRenderPipeline.VisibilityBuffer.cs.hlsl
@@ -0,0 +1,18 @@
+//
+// This file was automatically generated. Please don't edit by hand. Execute Editor command [ Edit > Rendering > Generate Shader Includes ] instead
+//
+
+#ifndef HDRENDERPIPELINE_VISIBILITYBUFFER_CS_HLSL
+#define HDRENDERPIPELINE_VISIBILITYBUFFER_CS_HLSL
+//
+// UnityEngine.Rendering.HighDefinition.HDRenderPipeline+LightVariants:  static fields
+//
+#define LIGHTVARIANTS_SKY_ENV (622592)
+#define LIGHTVARIANTS_SKY_DIR_PUNCTUAL (610304)
+#define LIGHTVARIANTS_SKY_DIR_AREA (614400)
+#define LIGHTVARIANTS_SKY_DIR_ENV (638976)
+#define LIGHTVARIANTS_SKY_DIR_PUNCTUAL_ENV (643072)
+#define LIGHTVARIANTS_SKY_DIR_PUNCTUAL_AREA_ENV (651264)
+
+
+#endif
