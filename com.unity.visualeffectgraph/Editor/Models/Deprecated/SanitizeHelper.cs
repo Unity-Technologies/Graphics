b32f31a554c353dc1791997556b718665b362024
Paul Demeulenaere <pauld@unity3d.com>
2021-06-29 19:34:11 +0200

    Fix arc cone access

--- com.unity.visualeffectgraph/Editor/Models/Deprecated/SanitizeHelper.cs
+++ com.unity.visualeffectgraph/Editor/Models/Deprecated/SanitizeHelper.cs
@@ -281,7 +281,7 @@ namespace UnityEditor.VFX
 
             var refSlot = from.refSlot;
             var from_cone = refSlot; //The ArcCone wasn't a composition
-            var from_arc = refSlot[1];
+            var from_arc = refSlot[4];
             VFXSlot.CopySpace(to, refSlot, true);
 
             var hasDirectLink = from.HasLink(false);
