4aaa3f251c527b63c3512c437de64062fbf29dca
Sai Narayan Natarajan <Nightmask3@gmail.com>
2021-06-30 16:30:02 -0700

    [2021.2][SG] Cleanup Blackboard Categories (#4870)
    * integrated Herbs fixes
    
    * Added auto-expand of collapsed categories on hover and data for graph input ordering
    
    * Setup flat list of graph inputs for ordering information
    
    * Fixed category bugs and tweaked UX based on tech-artist feedback
    
    * Fixed issue with drag indicators being visible after collapse of category
    
    * fix ordering in shader property block to match blackboard
    
    * bugfix of import order issue and cleanup
    
    * Fixed property moving and inserting behaviors
    
    * Fixed drag indicator bug with not showing up when exiting and re-entering the blackboard
    
    * Fixed insertion before last item behavior
    
    * Fixed property insertion data bugs
    
    * actually fixed the property moving and insertion for good (hopefully)
    
    * made drag indicator slightly larger
    
    * ran formatter, and addressed review comments
    
    * Fixed test failures
    
    * ran formatter
    
    * bumped correct SG importer numbers
    
    Co-authored-by: Elizabeth LeGros <elizabeth.legros@unity3d.com>

--- com.unity.shadergraph/Editor/Data/Graphs/GraphData.cs
+++ com.unity.shadergraph/Editor/Data/Graphs/GraphData.cs
@@ -1609,8 +1609,11 @@ namespace UnityEditor.ShaderGraph
                 if (categoryData.categoryGuid == associatedCategoryGuid && categoryData.IsItemInCategory(itemToMove))
                 {
                     // Validate new index to move the item to
-                    if (newIndex < 0 || newIndex >= categoryData.childCount)
+                    if (newIndex < -1 || newIndex >= categoryData.childCount)
+                    {
+                        AssertHelpers.Fail("Provided invalid index input to MoveItemInCategory.");
                         return;
+                    }
 
                     categoryData.MoveItemInCategory(itemToMove, newIndex);
                     break;
