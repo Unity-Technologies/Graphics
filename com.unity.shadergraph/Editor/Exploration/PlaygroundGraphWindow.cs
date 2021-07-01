17e45ea985a9628f649d75eefb9e0100fe323dbc
Joseph Savell <joseph.savell@unity3d.com>
2021-07-01 16:42:48 -0400

    Add Node Inspector overlay and factor out common event handler

--- com.unity.shadergraph/Editor/Exploration/PlaygroundGraphWindow.cs
+++ com.unity.shadergraph/Editor/Exploration/PlaygroundGraphWindow.cs
@@ -10,12 +10,13 @@ namespace GtfPlayground
     {
         protected override bool CanHandleAssetType(IGraphAssetModel asset) => asset is PlaygroundGraphAssetModel;
 
+        public ModelInspectorView nodeInspector { get; private set; }
+
         [InitializeOnLoadMethod]
         static void RegisterTool()
         {
             ShortcutHelper.RegisterDefaultShortcuts<PlaygroundGraphWindow>(PlaygroundStencil.Name);
         }
-
 
         [MenuItem("Window/GTF Playground", false)]
         public static void ShowRecipeGraphWindow()
@@ -26,12 +27,17 @@ namespace GtfPlayground
         protected override void OnEnable()
         {
             EditorToolName = "GTF Playground";
+            WithSidePanel = false;
+
             base.OnEnable();
 
             // Needed to ensure that graph view takes up full window when overlay canvas is present
             rootVisualElement.style.position = new StyleEnum<Position>(Position.Absolute);
             rootVisualElement.style.width = new StyleLength(new Length(100, LengthUnit.Percent));
             rootVisualElement.style.height = new StyleLength(new Length(100, LengthUnit.Percent));
+
+            // TODO: See if we can eventually use m_SidePanel instead
+            nodeInspector = CreateModelInspectorView();
         }
 
         protected override GraphView CreateGraphView()
