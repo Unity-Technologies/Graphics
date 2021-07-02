6ba147863b43a1fbcb70c7ed4b308a23e4d248d9
FrancescoC-Unity <francescoc@unity3d.com>
2021-07-02 18:11:05 +0200

    Fix wrong constant

--- com.unity.render-pipelines.high-definition/Runtime/PostProcessing/Shaders/TemporalAntialiasing.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/PostProcessing/Shaders/TemporalAntialiasing.hlsl
@@ -777,7 +777,7 @@ CTYPE SharpenColor(NeighbourhoodSamples samples, CTYPE color, float sharpenStren
 
     linearC = ConvertToWorkingSpace(linearC);
 #else
-    linearC = linearC + (linearC - linearAvg) * sharpenStrength * 4;
+    linearC = linearC + (linearC - linearAvg) * sharpenStrength * 3;
     linearC = clamp(linearC, 0, CLAMP_MAX);
 #endif
     CTYPE outputSharpened = linearC * PerceptualWeight(linearC);
