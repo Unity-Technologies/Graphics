9734159906646399290d164ebc8f54cb80f8b160
Robin De Moor <robin.demoor@unity3d.com>
2021-07-05 10:51:50 +0200

    Change BindMS check depth

--- com.unity.render-pipelines.universal/Runtime/UniversalRenderer.cs
+++ com.unity.render-pipelines.universal/Runtime/UniversalRenderer.cs
@@ -987,6 +987,9 @@ namespace UnityEngine.Rendering.Universal
                     // XRTODO: Enabled this line for non-XR pass? URP copy depth pass is already capable of handling MSAA.
                     depthDescriptor.bindMS = depthDescriptor.msaaSamples > 1 && !SystemInfo.supportsMultisampleAutoResolve && (SystemInfo.supportsMultisampledTextures != 0);
 #endif
+
+                    depthDescriptor.bindMS |= depthDescriptor.msaaSamples > 1 && (SystemInfo.supportsMultisampledTextures != 0);
+
                     depthDescriptor.colorFormat = RenderTextureFormat.Depth;
                     depthDescriptor.depthBufferBits = k_DepthStencilBufferBits;
                     cmd.GetTemporaryRT(m_ActiveCameraDepthAttachment.id, depthDescriptor, FilterMode.Point);
