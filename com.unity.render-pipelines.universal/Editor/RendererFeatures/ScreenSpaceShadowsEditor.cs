30b00a91f194110c610c9fb877dfb3806b23444a
Seongdae <Seongdae@unity3d.com>
2021-07-07 16:40:20 +0900

    Add tooltip for screen space shadows

--- com.unity.render-pipelines.universal/Editor/RendererFeatures/ScreenSpaceShadowsEditor.cs
+++ com.unity.render-pipelines.universal/Editor/RendererFeatures/ScreenSpaceShadowsEditor.cs
@@ -7,11 +7,30 @@ namespace UnityEditor.Rendering.Universal
     internal class ScreenSpaceShadowsEditor : Editor
     {
         #region Serialized Properties
+        private SerializedProperty m_SettingsProp;
         #endregion
+
+        private bool m_IsInitialized = false;
+
+        private struct Styles
+        {
+            public static GUIContent Description = EditorGUIUtility.TrTextContent("Description", "This feature resolves the cascaded shadows in screen space, so there is no options now. It might have additional settings later.");
+        }
+
+        private void Init()
+        {
+            m_SettingsProp = serializedObject.FindProperty("m_Settings");
+            m_IsInitialized = true;
+        }
 
         public override void OnInspectorGUI()
         {
+            if (!m_IsInitialized)
+            {
+                Init();
+            }
 
+            EditorGUILayout.PropertyField(m_SettingsProp, Styles.Description);
         }
     }
 }
