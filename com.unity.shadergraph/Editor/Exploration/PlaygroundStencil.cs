8c55a54e15607b3c3c6e8815ae9fc1dfe79d3767
Joseph Savell <joseph.savell@unity3d.com>
2021-07-08 15:02:39 -0400

    Add randomized node and CustomizableNode presets

--- com.unity.shadergraph/Editor/Exploration/PlaygroundStencil.cs
+++ com.unity.shadergraph/Editor/Exploration/PlaygroundStencil.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEditor.GraphToolsFoundation.Overdrive;
 using UnityEditor.GraphToolsFoundation.Overdrive.BasicModel;
 using UnityEngine.GraphToolsFoundation.Overdrive;
@@ -19,6 +20,69 @@ namespace GtfPlayground
             return typeHandle == PlaygroundTypes.DayOfWeek
                 ? typeof(DayOfWeekConstant)
                 : TypeToConstantMapper.GetConstantNodeType(typeHandle);
+        }
+
+        public override ISearcherDatabaseProvider GetSearcherDatabaseProvider()
+        {
+            return new PlaygroundSearcherDatabaseProvider(this,
+                new List<PlaygroundSearcherDatabaseProvider.Preset>
+                {
+                    new("Vectors")
+                    {
+                        inputs =
+                        {
+                            {"Vec2", TypeHandle.Vector2},
+                            {"Vec3", TypeHandle.Vector3},
+                            {"Vec4", TypeHandle.Vector4},
+                        },
+                        outputs =
+                        {
+                            {"Vec2", TypeHandle.Vector2},
+                            {"Vec3", TypeHandle.Vector3},
+                            {"Vec4", TypeHandle.Vector4},
+                        }
+                    },
+
+                    new("And")
+                    {
+                        inputs =
+                        {
+                            {"A", TypeHandle.Bool},
+                            {"B", TypeHandle.Bool},
+                        },
+                        outputs =
+                        {
+                            {"A AND B", TypeHandle.Bool},
+                        }
+                    },
+
+                    new("Construct Vec3")
+                    {
+                        inputs =
+                        {
+                            {"X", TypeHandle.Float},
+                            {"Y", TypeHandle.Float},
+                            {"Z", TypeHandle.Float},
+                        },
+                        outputs =
+                        {
+                            {"Vec3", TypeHandle.Vector3},
+                        }
+                    },
+
+                    new("Sample Texture2D")
+                    {
+                        inputs =
+                        {
+                            {"UV", TypeHandle.Vector2},
+                            {"Texture", PlaygroundTypes.Texture2D},
+                        },
+                        outputs =
+                        {
+                            {"RGBA", TypeHandle.Vector4},
+                        }
+                    },
+                });
         }
     }
 }
