9e1eafa74ddfce14e5d201ac1a1c84910489a2ca
Kleber Garcia <kleber.garcia@unity3d.com>
2021-07-09 22:18:13 -0400

    enabling 16 bit precision for almost every pass

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/LightLoop.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/LightLoop.hlsl
@@ -243,7 +243,7 @@ void LightLoop( float3 V, PositionInputs posInput, PreLightData preLightData, BS
     // This struct is define in the material. the Lightloop must not access it
     // PostEvaluateBSDF call at the end will convert Lighting to diffuse and specular lighting
     PackedAggregateLighting aggregateLighting;
-    InitPackedAggregateLighting(aggregateLighting, GetCurrentExposureMultiplier(), GetInverseCurrentExposureMultiplier());
+    InitPackedAggregateLighting(aggregateLighting, GetCurrentExposureMultiplier(), featureFlags, GetInverseCurrentExposureMultiplier());
 
     if (featureFlags & LIGHTFEATUREFLAGS_PUNCTUAL)
     {
--- com.unity.render-pipelines.high-definition/Runtime/Material/AggregateLighting.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Material/AggregateLighting.hlsl
@@ -42,44 +42,45 @@ void AccumulateIndirectLighting(IndirectLighting src, inout AggregateLighting ds
 //Structure representing 2 packed lighting colors
 struct PackedLightingColor2
 {
-#if AGGREGATE_LIGHTING_ENABLE_16BIT_PACKING_SOFTWARE
+    //16 bit data
     uint3 m_data;
-#else
+
+    //flat data
     float3 m_first;
     float3 m_second;
-#endif
 };
 
-void PackColor2(in float3 first, in float3 second, inout PackedLightingColor2 dst)
+void PackColor2(bool enable16bitPacking, in float3 first, in float3 second, inout PackedLightingColor2 dst)
 {
-#if AGGREGATE_LIGHTING_ENABLE_16BIT_PACKING_SOFTWARE
+    if (enable16bitPacking)
+    {
         dst.m_data = uint3(
             f32tof16(first.x) | (f32tof16(second.x) << 16),
             f32tof16(first.y) | (f32tof16(second.y) << 16),
             f32tof16(first.z) | (f32tof16(second.z) << 16)
         );
-#else
+    }
+    else
+    {
         dst.m_first = first;
         dst.m_second = second;
-#endif
+    }
 }
 
-float3 UnpackFirstColor(in PackedLightingColor2 src)
+float3 UnpackFirstColor(int enable16bitPacking, in PackedLightingColor2 src)
 {
-#if AGGREGATE_LIGHTING_ENABLE_16BIT_PACKING_SOFTWARE
-    return float3(f16tof32(src.m_data.x), f16tof32(src.m_data.y), f16tof32(src.m_data.z));
-#else
-    return src.m_first;
-#endif
+    if (enable16bitPacking)
+        return float3(f16tof32(src.m_data.x), f16tof32(src.m_data.y), f16tof32(src.m_data.z));
+    else
+        return src.m_first;
 }
 
-float3 UnpackSecondColor(in PackedLightingColor2 src)
+float3 UnpackSecondColor(int enable16bitPacking, in PackedLightingColor2 src)
 {
-#if AGGREGATE_LIGHTING_ENABLE_16BIT_PACKING_SOFTWARE
-    return float3(f16tof32(src.m_data.x >> 16), f16tof32(src.m_data.y >> 16), f16tof32(src.m_data.z >> 16));
-#else
-    return src.m_second;
-#endif
+    if (enable16bitPacking)
+        return float3(f16tof32(src.m_data.x >> 16), f16tof32(src.m_data.y >> 16), f16tof32(src.m_data.z >> 16));
+    else
+        return src.m_second;
 }
 
 struct PackedDirectLighting
@@ -87,19 +88,19 @@ struct PackedDirectLighting
     PackedLightingColor2 m_data;
 };
 
-void PackDirect(float3 diffuse, float3 specular, inout PackedDirectLighting dst)
+void PackDirect(int enable16bitPacking, float3 diffuse, float3 specular, inout PackedDirectLighting dst)
 {
-    PackColor2(diffuse, specular, dst.m_data);
+    PackColor2(enable16bitPacking, diffuse, specular, dst.m_data);
 }
 
-float3 UnpackDiffuse(in PackedDirectLighting direct)
+float3 UnpackDiffuse(int enable16bitPacking, in PackedDirectLighting direct)
 {
-    return UnpackFirstColor(direct.m_data);
+    return UnpackFirstColor(enable16bitPacking, direct.m_data);
 }
 
-float3 UnpackSpecular(in PackedDirectLighting direct)
+float3 UnpackSpecular(int enable16bitPacking, in PackedDirectLighting direct)
 {
-    return UnpackSecondColor(direct.m_data);
+    return UnpackSecondColor(enable16bitPacking, direct.m_data);
 }
 
 struct PackedIndirectLighting
@@ -107,76 +108,91 @@ struct PackedIndirectLighting
     PackedLightingColor2 m_data;
 };
 
-void PackIndirect(float3 specularReflected, float3 specularTransmitted, inout PackedIndirectLighting dst)
+void PackIndirect(int enable16bitPacking, float3 specularReflected, float3 specularTransmitted, inout PackedIndirectLighting dst)
 {
-    PackColor2(specularReflected, specularTransmitted, dst.m_data);
+    PackColor2(enable16bitPacking, specularReflected, specularTransmitted, dst.m_data);
 }
 
-float3 UnpackSpecularReflected(in PackedIndirectLighting indirect)
+float3 UnpackSpecularReflected(int enable16bitPacking, in PackedIndirectLighting indirect)
 {
-    return UnpackFirstColor(indirect.m_data);
+    return UnpackFirstColor(enable16bitPacking, indirect.m_data);
 }
 
-float3 UnpackSpecularTransmitted(in PackedIndirectLighting indirect)
+float3 UnpackSpecularTransmitted(int enable16bitPacking, in PackedIndirectLighting indirect)
 {
-    return UnpackSecondColor(indirect.m_data);
+    return UnpackSecondColor(enable16bitPacking, indirect.m_data);
 }
 
 struct PackedAggregateLighting
 {
+    int m_enable16bitPacking;
     float2 m_multipliers;
     PackedDirectLighting direct;
     PackedIndirectLighting indirect;
 };
 
-void InitPackedAggregateLighting(inout PackedAggregateLighting self, float exposureMultiplier, float exposureMultiplierInv)
+void InitPackedAggregateLighting(inout PackedAggregateLighting self, float exposureMultiplier, int enable16bitPacking, float exposureMultiplierInv)
 {
     ZERO_INITIALIZE(PackedAggregateLighting, self);
 #if AGGREGATE_LIGHTING_ENABLE_16BIT_PACKING_SOFTWARE
-    self.m_multipliers = float2(exposureMultiplier, exposureMultiplierInv);
+    self.m_enable16bitPacking = enable16bitPacking;
 #else
-    self.m_multipliers = float2(1.0, 1.0);
+    self.m_enable16bitPacking = 0;
 #endif
+
+    if (self.m_enable16bitPacking)
+        self.m_multipliers = float2(exposureMultiplier, exposureMultiplierInv);
+    else
+        self.m_multipliers = float2(1.0, 1.0);
 }
 
 void PackedAccumulatePackedDirect(in PackedDirectLighting src, inout PackedAggregateLighting dst)
 {
+    int enabled = dst.m_enable16bitPacking;
     PackDirect(
-        UnpackDiffuse(dst.direct) + UnpackDiffuse(src),
-        UnpackSpecular(dst.direct) + UnpackSpecular(src),
+        enabled,
+        UnpackDiffuse(enabled, dst.direct) + UnpackDiffuse(enabled, src),
+        UnpackSpecular(enabled, dst.direct) + UnpackSpecular(enabled, src),
         dst.direct);
 }
 
 void PackedAccumulateDirect(in DirectLighting src, inout PackedAggregateLighting dst)
 {
+    int enabled = dst.m_enable16bitPacking;
     PackDirect(
-        UnpackDiffuse(dst.direct) + src.diffuse * dst.m_multipliers.x,
-        UnpackSpecular(dst.direct) + src.specular * dst.m_multipliers.x,
+        enabled, 
+        UnpackDiffuse(enabled, dst.direct) + src.diffuse * dst.m_multipliers.x,
+        UnpackSpecular(enabled, dst.direct) + src.specular * dst.m_multipliers.x,
         dst.direct);
 }
 
 void PackedAccumulatePackedIndirect(in PackedIndirectLighting src, inout PackedAggregateLighting dst)
 {
+    int enabled = dst.m_enable16bitPacking;
     PackIndirect(
-        UnpackSpecularReflected(dst.indirect) + UnpackSpecularReflected(src),
-        UnpackSpecularTransmitted(dst.indirect) + UnpackSpecularTransmitted(src),
+        enabled,
+        UnpackSpecularReflected(enabled, dst.indirect) + UnpackSpecularReflected(enabled, src),
+        UnpackSpecularTransmitted(enabled, dst.indirect) + UnpackSpecularTransmitted(enabled, src),
         dst.indirect);
 }
 
 void PackedAccumulateIndirect(in IndirectLighting src, inout PackedAggregateLighting dst)
 {
+    int enabled = dst.m_enable16bitPacking;
     PackIndirect(
-        UnpackSpecularReflected(dst.indirect) + src.specularReflected * dst.m_multipliers.x,
-        UnpackSpecularTransmitted(dst.indirect) + src.specularTransmitted * dst.m_multipliers.x,
+        enabled,
+        UnpackSpecularReflected(enabled, dst.indirect) + src.specularReflected * dst.m_multipliers.x,
+        UnpackSpecularTransmitted(enabled, dst.indirect) + src.specularTransmitted * dst.m_multipliers.x,
         dst.indirect);
 }
 
 void UnpackAggregateLighting(in PackedAggregateLighting src, out AggregateLighting output)
 {
-    output.direct.diffuse = UnpackDiffuse(src.direct) * src.m_multipliers.y;
-    output.direct.specular = UnpackSpecular(src.direct) * src.m_multipliers.y;
-    output.indirect.specularReflected = UnpackSpecularReflected(src.indirect) * src.m_multipliers.y;
-    output.indirect.specularTransmitted = UnpackSpecularTransmitted(src.indirect) * src.m_multipliers.y;
+    int enabled = src.m_enable16bitPacking;
+    output.direct.diffuse = UnpackDiffuse(enabled, src.direct) * src.m_multipliers.y;
+    output.direct.specular = UnpackSpecular(enabled, src.direct) * src.m_multipliers.y;
+    output.indirect.specularReflected = UnpackSpecularReflected(enabled, src.indirect) * src.m_multipliers.y;
+    output.indirect.specularTransmitted = UnpackSpecularTransmitted(enabled, src.indirect) * src.m_multipliers.y;
 }
 
 
