36a530d0b98d1ec25fc6e7ba328906b61e7ecd57
John Parsaie <johnpa@unity3d.com>
2021-07-15 17:23:58 -0400

    Add the local scattering term.

--- com.unity.render-pipelines.high-definition/Runtime/Material/Hair/HairScattering.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Material/Hair/HairScattering.hlsl
@@ -16,6 +16,10 @@ struct HairScatteringData
     float3 averageScatteringFront;
     float3 averageScatteringBack;
 
+    // 0: Front, 1: Back
+    float3 averageVariance[2];
+    float3 averageShift[2];
+
     float3 globalScattering;
     float3 localScattering;
 };
@@ -50,13 +54,14 @@ float EvaluateStrandCount(float3 L, float3 P)
         }
     }
 
-    return strandCountApprox;
+    return strandCountApprox * 0.1;
 #else
     // TODO
     return 1;
 #endif
 }
 
+// TODO: Maybe collapse all of this into one scattering data gather.
 HairScatteringData SampleAverageScattering(float3 diffuseColor, float perceptualRoughness, float sinThetaI)
 {
     HairScatteringData scatteringData;
@@ -79,6 +84,54 @@ HairScatteringData SampleAverageScattering(float3 diffuseColor, float perceptual
     return scatteringData;
 }
 
+void ComputeAverageScatteringVariance(BSDFData bsdfData, inout HairScatteringData scatteringData)
+{
+    const float3 beta = float3(
+        bsdfData.roughnessR,
+        bsdfData.roughnessTT,
+        bsdfData.roughnessTRT
+    );
+
+    // Here we should be deriving the average variance with the per-component (R, TT, TRT)
+    // BSDF average in the hemisphere, and not the BSDF average per-absorption channel.
+
+    // Front scattering (Disney Eq. 15)
+    {
+        const float3 af = scatteringData.averageScatteringFront;
+        scatteringData.averageVariance[0] = dot(beta, af) / (af.r + af.g + af.b);
+    }
+
+    // Back scattering (Disney Eq. 16)
+    {
+        const float3 ab = scatteringData.averageScatteringBack;
+        scatteringData.averageVariance[0] = dot(beta, ab) / (ab.r + ab.g + ab.b);
+    }
+}
+
+void ComputeAverageScatteringShift(BSDFData bsdfData, inout HairScatteringData scatteringData)
+{
+    const float3 shift = float3(
+        bsdfData.cuticleAngleR,
+        bsdfData.cuticleAngleTT,
+        bsdfData.cuticleAngleTRT
+    );
+
+    // Here we should be deriving the average shift with the per-component (R, TT, TRT)
+    // BSDF average in the hemisphere, and not the BSDF average per-absorption channel.
+
+    // Front scattering (Disney Eq. 13)
+    {
+        const float3 af = scatteringData.averageScatteringFront;
+        scatteringData.averageShift[0] = dot(shift, af) / (af.r + af.g + af.b);
+    }
+
+    // Back scattering (Disney Eq. 14)
+    {
+        const float3 ab = scatteringData.averageScatteringBack;
+        scatteringData.averageShift[1] = dot(shift, ab) / (ab.r + ab.g + ab.b);
+    }
+}
+
 HairScatteringData EvaluateMultipleScattering(BSDFData bsdfData, float3 V, float3 L, float3 P)
 {
     float3 T = bsdfData.hairStrandDirectionWS;
@@ -87,20 +140,24 @@ HairScatteringData EvaluateMultipleScattering(BSDFData bsdfData, float3 V, float
     // of the angle between the incident direction and the normal plane.
     float sinThetaI = dot(T, L);
 
-    // TEMP: Extra angle derivation.
-    float thetaH, cosThetaD;
+    // TEMP: Extra angle derivation, this is redundant work as we already do this for the same light on the BSDF evaluation.
+    float thetaH, thetaD;
     {
         float sinThetaR = dot(T, V);
 
         float thetaI = FastASin(sinThetaI);
         float thetaR = FastASin(sinThetaR);
-        thetaH = (thetaI + thetaR) * 0.5;
 
-        cosThetaD = cos((thetaR - thetaI) * 0.5);
+        thetaH = (thetaI + thetaR) * 0.5;
+        thetaD = (thetaR - thetaI) * 0.5;
     }
 
     // Sample the average front and back scattering.
+    // TODO: Gather everything at once.
     HairScatteringData scatteringData = SampleAverageScattering(bsdfData.diffuseColor, bsdfData.perceptualRoughness, sinThetaI);
+
+    ComputeAverageScatteringVariance(bsdfData, scatteringData);
+    ComputeAverageScatteringShift   (bsdfData, scatteringData);
 
     // Fetch the number of hair fibers between the shading point x and the light.
     scatteringData.strandCount = EvaluateStrandCount(L, P);
@@ -114,26 +171,42 @@ HairScatteringData EvaluateMultipleScattering(BSDFData bsdfData, float3 V, float
     {
         // Following the observation of Zinke et. al., density factor (ratio of occlusion of the shading point by neighboring strands)
         // can be approximated with this constant to match most path traced references.
-        const float densityFactorFront = 0.7;
+        const float df = 0.7;
 
-        // By assuming that all hair strands between the shading point and the light are oriented the same, we can avoid evaluating the average
-        // scattering at every intersection, and instead approximate the transmittance like so. This is suitable for long, straighter hair, but
-        // have a greater error for curlier hair styles.
-        float3 forwardTransmittance = densityFactorFront * pow(scatteringData.averageScatteringFront, scatteringData.strandCount);
+        // Pre-define some shorthand for the symbols. .
+        const float  n  = scatteringData.strandCount;
+        const float3 Bf = scatteringData.averageVariance[0];
 
-        // We make a similar approximation for computation of the accumulated variance, by assuming strands all have the same roughness.
-        // Since we only evaluate front scattering for global scattering, where TT is the dominant lobe, we accumulate that roughness.
-        float sigmaF = bsdfData.roughnessTT * scatteringData.strandCount;
+        // Approximate the transmittance by assuming that all hair strands between the shading point and the light are
+        // oriented the same. This is suitable for long, straighter hair ( Eq. 6 Disney ).
+        float3 Tf = df * pow(scatteringData.averageScatteringFront, n);
 
-        float forwardSpread = ScatteringSpreadGaussian(thetaH, sigmaF) * rcp(PI * cosThetaD);
+        // Approximate the accumulated variance, by assuming strands all have the same average roughness. ( Eq. 7 Disney )
+        float3 sigmaF = Sq(Bf) * n;
 
-        scatteringData.globalScattering = forwardTransmittance * forwardSpread;
+        // Computes the forward scattering spread ( Eq. 7 ).
+        float3 Sf = float3( ScatteringSpreadGaussian(thetaH, sigmaF.r),
+                            ScatteringSpreadGaussian(thetaH, sigmaF.g),
+                            ScatteringSpreadGaussian(thetaH, sigmaF.b)) * rcp(PI * cos(thetaD));
+
+        // Resolve the final global scattering term ( Eq. 4 ).
+        scatteringData.globalScattering = Tf * Sf;
     }
 
     // Local scattering.
     {
         // Similarly to front scattering, this same density coefficient is suggested for matching most path traced references.
-        const float densityFactorBack = 0.7;
+        const float db = 0.7;
+
+        // Pre-define some shorthand for the symbols.
+        const float3 af  = scatteringData.averageScatteringFront;
+        const float3 ab  = scatteringData.averageScatteringBack;
+        const float3 sf  = scatteringData.averageShift[0];
+        const float3 sb  = scatteringData.averageShift[1];
+        const float3 Bf  = scatteringData.averageVariance[0];
+        const float3 Bb  = scatteringData.averageVariance[1];
+        const float3 Bf2 = Sq(Bf);
+        const float3 Bb2 = Sq(Bb);
 
         // Compute the average backscattering attenuation, the attenuation in the neighborhood of x.
         // Here we only model the first and third backscattering event, as the following are negligible.
@@ -146,23 +219,41 @@ HairScatteringData EvaluateMultipleScattering(BSDFData bsdfData, float3 V, float
         //                 (F) <--- ... ---> (B)
         // L -------------->|
 
-        float3 af1 = scatteringData.averageScatteringFront;
+        float3 af1 = af;
         float3 af2 = Sq(af1);
-        float3 afI = 1 - af2;
 
-        float3 ab1 = scatteringData.averageScatteringBack;
+        float3 afI1 = 1 - af2;
+        float3 afI2 = Sq(afI1);
+        float3 afI3 = afI2 * afI1;
+
+        float3 ab1 = ab;
         float3 ab2 = Sq(ab1);
-        float3 ab3 = ab2 * ab1
+        float3 ab3 = ab2 * ab1;
 
-        // Solve eq. 11, 13, & 14, analytic solutions to the potential infinite permutations of backward scattering
-        // in a volume of fibers (for one and three backward scatters).
-        float3 A1 = (ab1 * af2) / afI;
-        float3 A3 = (ab3 * af2) / pow(afI, 3);
-        float3 AB = A1 + A3;
+        // Analytic solutions to the potential infinite permutations of backward scattering
+        // in a volume of fibers for one and three backward scatters ( Eq. 11, 13, & 14 ).
+        float3 A1 = (ab1 * af2) / afI1;
+        float3 A3 = (ab3 * af2) / afI2;
+        float3 Ab = A1 + A3;
 
-        // TODO: Average backscattering spread
+        // Computes the average longitudinal shift ( Eq. 16 ).
+        float3 shiftB = 1 - (2 * ab2 / afI2);
+        float3 shiftF = ((2 * afI2) + (4 * af2 * ab2)) / afI3;
+        float3 deltaB = (sb * shiftB) + (sf * shiftF);
 
-        scatteringData.localScattering = AB;
+        // Compute the average back scattering standard deviation ( Eq. 17 ).
+        float3 sigmaB = (1 + db * af2);
+        sigmaB *= (ab * sqrt((2 * Bf2) + Bb2)) + (ab3 * sqrt((2 * Bf2) + Bb2));
+        sigmaB /= ab + (ab3 * ((2 * Bf) + (3 * Bb)));
+        sigmaB  = Sq(sigmaB);
+
+        // Computes the average back scattering spread ( Eq. 15 ).
+        float3 Sb = float3( ScatteringSpreadGaussian(thetaH - deltaB, sigmaB.r),
+                            ScatteringSpreadGaussian(thetaH - deltaB, sigmaB.g),
+                            ScatteringSpreadGaussian(thetaH - deltaB, sigmaB.b)) * rcp(PI * cos(thetaD));
+
+        // Resolve the overall local scattering term ( Eq. 9 & 10 ).
+        scatteringData.localScattering = db * (2 * Ab * Sb) / cos(thetaD);
     }
 
     return scatteringData;
