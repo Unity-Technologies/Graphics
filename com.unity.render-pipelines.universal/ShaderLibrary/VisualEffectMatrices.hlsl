4f3ebc0d5250d42c7a2f088e983b30883928941b
Paul Demeulenaere <pauld@unity3d.com>
2021-07-22 17:21:14 +0200

    *WIP* Investigation about failure to reach the object space

--- com.unity.render-pipelines.universal/ShaderLibrary/VisualEffectMatrices.hlsl
+++ com.unity.render-pipelines.universal/ShaderLibrary/VisualEffectMatrices.hlsl
@@ -1,6 +1,13 @@
+//TODOPAUL : Move it to vfx
+
+
 #ifndef UNITY_VISUAL_EFFECT_MATRICES_INCLUDED
 #define UNITY_VISUAL_EFFECT_MATRICES_INCLUDED
 #ifdef  HAVE_VFX_MODIFICATION
+
+#ifdef UNITY_SPACE_TRANSFORMS_INCLUDED
+#error VisualEffectMatrices must be included *before* space transform
+#endif
 
 // Abstraction of Unity matrices for VFX element/particles.
 #undef  UNITY_MATRIX_M
