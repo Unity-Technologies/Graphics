bf1b3e0794c40efb586a815b7c8bd87206e467d7
Adrien de Tocqueville <adrien.tocqueville@unity3d.com>
2021-07-28 13:07:29 +0200

    Prevent material from having infinite intensity (#5132)
    * Prevent material from having infinite intensity
    
    * Fix switching from ldr to hdr emissive
    
    * Fix input field precision
    
    * Round the max value
    
    * Make two variables
    
    Co-authored-by: sebastienlagarde <sebastien@unity3d.com>

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/LightUtils.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/LightUtils.cs
@@ -8,6 +8,9 @@ namespace UnityEngine.Rendering.HighDefinition
     /// </summary>
     class LightUtils
     {
+        static float s_LuminanceToEvFactor =  Mathf.Log(100f / ColorUtils.s_LightMeterCalibrationConstant);
+        static float s_EvToLuminanceFactor = -Mathf.Log(100f / ColorUtils.s_LightMeterCalibrationConstant);
+
         // Physical light unit helper
         // All light unit are in lumen (Luminous power)
         // Punctual light (point, spot) are convert to candela (cd = lumens / steradian)
@@ -164,7 +167,7 @@ namespace UnityEngine.Rendering.HighDefinition
         public static float ConvertEvToLuminance(float ev)
         {
             float k = ColorUtils.s_LightMeterCalibrationConstant;
-            return (k / 100.0f) * Mathf.Pow(2, ev);
+            return Mathf.Pow(2, ev + s_EvToLuminanceFactor);
         }
 
         /// <summary>
@@ -194,7 +197,7 @@ namespace UnityEngine.Rendering.HighDefinition
         public static float ConvertLuminanceToEv(float luminance)
         {
             float k = ColorUtils.s_LightMeterCalibrationConstant;
-            return (float)Math.Log((luminance * 100f) / k, 2);
+            return Mathf.Log(luminance, 2) + s_LuminanceToEvFactor;
         }
 
         /// <summary>
