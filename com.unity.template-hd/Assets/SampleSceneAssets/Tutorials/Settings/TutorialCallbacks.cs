587ec30ceb45e4aec23393a6de433fd005e446f1
pierre-unity <39901544+pierre-unity@users.noreply.github.com>
2021-08-03 13:38:52 +0100

    updated manifest to final versions of packages and regenerated tutorialcallback scrpit

--- com.unity.template-hd/Assets/SampleSceneAssets/Tutorials/Settings/TutorialCallbacks.cs
+++ com.unity.template-hd/Assets/SampleSceneAssets/Tutorials/Settings/TutorialCallbacks.cs
@@ -1,4 +1,5 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEditor;
 using Unity.Tutorials.Core.Editor;
 
 /// <summary>
@@ -7,41 +8,49 @@ using Unity.Tutorials.Core.Editor;
 [CreateAssetMenu(fileName = DefaultFileName, menuName = "Tutorials/" + DefaultFileName + " Instance")]
 public class TutorialCallbacks : ScriptableObject
 {
+    /// <summary>
+    /// The default file name used to create asset of this class type.
+    /// </summary>
     public const string DefaultFileName = "TutorialCallbacks";
 
-    public static ScriptableObject CreateInstance()
+    /// <summary>
+    /// Creates a TutorialCallbacks asset and shows it in the Project window.
+    /// </summary>
+    /// <param name="assetPath">
+    /// A relative path to the project's root. If not provided, the Project window's currently active folder path is used.
+    /// </param>
+    /// <returns>The created asset</returns>
+    public static ScriptableObject CreateAndShowAsset(string assetPath = null)
     {
-        return ScriptableObjectUtils.CreateAsset<TutorialCallbacks>(DefaultFileName);
+        assetPath = assetPath ?? $"{TutorialEditorUtils.GetActiveFolderPath()}/{DefaultFileName}.asset";
+        var asset = CreateInstance<TutorialCallbacks>();
+        AssetDatabase.CreateAsset(asset, AssetDatabase.GenerateUniqueAssetPath(assetPath));
+        EditorUtility.FocusProjectWindow(); // needed in order to make the selection of newly created asset to really work
+        Selection.activeObject = asset;
+        return asset;
     }
 
-    // Example callback for basic UnityEvent
+    /// <summary>
+    /// Example callback for basic UnityEvent
+    /// </summary>
     public void ExampleMethod()
     {
         Debug.Log("ExampleMethod");
     }
 
-    // Example callbacks for AtrbitraryCriterion's BoolCallback
-    public bool IsEnabled()
+    /// <summary>
+    /// Example callbacks for ArbitraryCriterion's BoolCallback
+    /// </summary>
+    /// <returns></returns>
+    public bool DoesFooExist()
     {
-        if(!objectToCheck)
-        {
-            FindObjectToCheck();
-            return false;
-        }
-        return objectToCheck.activeInHierarchy;
+        return GameObject.Find("Foo") != null;
     }
 
-    GameObject objectToCheck;
-    public void FindObjectToCheck()
-    {
-        objectToCheck = GameObject.FindGameObjectWithTag("Finish");
-        if (!objectToCheck)
-        {
-            //Debug.LogError("In order to be completed, this tutorial expects exactly one 'objectToCheck' brick tagged as 'Finished'");
-            return;
-        }
-    }
-
+    /// <summary>
+    /// Implement the logic to automatically complete the criterion here, if wanted/needed.
+    /// </summary>
+    /// <returns>True if the auto-completion logic succeeded.</returns>
     public bool AutoComplete()
     {
         var foo = GameObject.Find("Foo");
