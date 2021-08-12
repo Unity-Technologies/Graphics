5e226f1e1d576105eaae5c1fa12808cfc4c701a3
Joseph Savell <joseph.savell@unity3d.com>
2021-08-12 18:22:58 -0400

    Remove test menu item

--- com.unity.shadergraph/Editor/GraphUI/GraphElements/ModelUI/RedirectNode.cs
+++ com.unity.shadergraph/Editor/GraphUI/GraphElements/ModelUI/RedirectNode.cs
@@ -1,10 +1,5 @@
-﻿using System.Linq;
-using System.Text;
-using UnityEditor.GraphToolsFoundation.Overdrive;
-using UnityEditor.ShaderGraph.GraphUI.DataModel;
+﻿using UnityEditor.GraphToolsFoundation.Overdrive;
 using UnityEditor.ShaderGraph.GraphUI.Utilities;
-using UnityEngine;
-using UnityEngine.UIElements;
 
 namespace UnityEditor.ShaderGraph.GraphUI.GraphElements
 {
@@ -15,31 +10,6 @@ namespace UnityEditor.ShaderGraph.GraphUI.GraphElements
             PartList.AppendPart(InOutPortContainerPart.Create(portContainerPartName, Model, this, ussClassName));
             AddToClassList("sg-redirect-node");
             this.AddStylesheet("RedirectNode.uss");
-        }
-
-        protected override void BuildContextualMenu(ContextualMenuPopulateEvent evt)
-        {
-            base.BuildContextualMenu(evt);
-
-            // TODO: Remove
-            evt.menu.InsertAction(0, "Log Source and Destinations", _ =>
-            {
-                var src = ((RedirectNodeModel) Model).ResolveSource();
-                var dsts = ((RedirectNodeModel) Model).ResolveDestinations().ToList();
-
-                var msg = new StringBuilder($"Node {Model.Guid}\n");
-                msg.AppendLine(src is null ? "No source" : $"Source: {GetPortDisplayString(src)}");
-                msg.AppendLine(!dsts.Any()
-                    ? "No destinations"
-                    : $"Destinations: \n\t{string.Join(", \n\t", dsts.Select(GetPortDisplayString))}");
-
-                Debug.Log(msg.ToString());
-            });
-        }
-
-        static string GetPortDisplayString(IPortModel port)
-        {
-            return $"{port.UniqueName} ({(port.NodeModel is IHasTitle hasTitle ? hasTitle.DisplayTitle : port.NodeModel.Guid)})";
         }
     }
 }
