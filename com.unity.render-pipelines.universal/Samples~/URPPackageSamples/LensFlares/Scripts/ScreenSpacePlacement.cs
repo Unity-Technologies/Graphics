d5980fdd5e05114c9b683f2e66e1b9232fb4a688
Jonasmortensen <Jonasmortensen3@gmail.com>
2021-08-16 09:33:44 +0200

    Clean up URP lens flare sample

--- com.unity.render-pipelines.universal/Samples~/URPPackageSamples/LensFlares/Scripts/ScreenSpacePlacement.cs
+++ com.unity.render-pipelines.universal/Samples~/URPPackageSamples/LensFlares/Scripts/ScreenSpacePlacement.cs
@@ -5,37 +5,23 @@ using UnityEngine;
 [ExecuteAlways]
 public class ScreenSpacePlacement : MonoBehaviour
 {
-    public Camera cam;
-
-    public Transform flareObject;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //cam.ScreenToWorldPoint()
-    }
+    [SerializeField]
+    private Camera m_Cam;
+    [SerializeField]
+    private Transform m_FlareObject;
 
     void OnGUI()
     {
-        Vector3 point = new Vector3();
-        Event   currentEvent = Event.current;
+        Event currentEvent = Event.current;
         Vector2 mousePos = new Vector2();
 
-        // Get the mouse position from Event.
-        // Note that the y position from Event is inverted.
         mousePos.x = currentEvent.mousePosition.x;
-        mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
+        mousePos.y = m_Cam.pixelHeight - currentEvent.mousePosition.y;
 
-        point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
-
-        if (flareObject != null && mousePos.x > 0 && mousePos.y > 0 && mousePos.x < cam.pixelWidth && mousePos.y < cam.pixelHeight)
+        if (m_FlareObject != null && mousePos.x > 0 && mousePos.y > 0 && mousePos.x < m_Cam.pixelWidth && mousePos.y < m_Cam.pixelHeight)
         {
-            flareObject.position = point;
+            Vector3 point = m_Cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, m_Cam.nearClipPlane));
+            m_FlareObject.position = point;
         }
     }
 }
