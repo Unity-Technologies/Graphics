d61593ad1a2d569d7178d4926dbd89d4a26d2468
Jessica Thomson <jessica.thomson@unity3d.com>
2021-08-18 14:57:23 -0700

    Removed [1:] indexing on argv from wrap_utr.py to prevent arguments from getting swallowed

--- .yamato/tooling/wrap_utr.py
+++ .yamato/tooling/wrap_utr.py
@@ -9,14 +9,14 @@ from os import listdir
 if __name__ == "__main__":
     cur_sys = platform.system()
     if cur_sys == 'Windows':
-        subprocess.call(["utr.bat"] + sys.argv[1:])
+        subprocess.call(["utr.bat"] + sys.argv)
     elif cur_sys == 'Linux':
-        subprocess.call(["utr"] + sys.argv[1:])
+        subprocess.call(["utr"] + sys.argv)
     else:
         cwd = path.abspath('.')
         # expanduser("~") is the same as ~
         utr_path = path.join(expanduser("~"), "Graphics/utr")
         if path.exists(utr_path):
-            output = subprocess.call([utr_path] + sys.argv[1:])
+            output = subprocess.call([utr_path] + sys.argv)
         else:
-            output = subprocess.call([path.join(getcwd(), "utr")] + sys.argv[1:])
+            output = subprocess.call([path.join(getcwd(), "utr")] + sys.argv)
