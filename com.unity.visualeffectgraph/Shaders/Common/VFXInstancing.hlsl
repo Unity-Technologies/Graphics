c6a15fc94340a8ffda033914dc0b5ca617710f0e
Ludovic Theobald <ludovic.theobald@unity3d.com>
2021-08-26 17:03:14 +0200

    Binary search to find instanceIndex in VS

--- com.unity.visualeffectgraph/Shaders/Common/VFXInstancing.hlsl
+++ com.unity.visualeffectgraph/Shaders/Common/VFXInstancing.hlsl
@@ -7,6 +7,31 @@ uint GetThreadId(uint3 groupId, uint3 groupThreadId, uint nbThreadPerGroup, uint
     return groupThreadId.x + groupId.x * nbThreadPerGroup + groupId.y * dispatchWidth * nbThreadPerGroup;
 }
 
+//Binary search in Inclusive Prefix sum
+//Returns the index among the found group
+uint BinarySearchPrefixSum(uint id, StructuredBuffer<uint> prefixSum, uint bufferSize, inout uint foundId)
+{
+    uint left = 0;
+    uint right = max(bufferSize, 1) - 1;
+    while (left <= right)
+    {
+        foundId = (int)((left + right) / 2);
+        if (id < prefixSum[foundId])
+        {
+            if (foundId == 0 || id >= prefixSum[max(foundId - 1, 0)])
+                break;
+            right = foundId - 1;
+        }
+        else
+        {
+            left = foundId + 1;
+        }
+    }
+
+    //return prefixSum[foundId] - (foundId > 0 ? prefixSum[foundId-1] : 0); //group size
+    return id - (foundId > 0 ? prefixSum[foundId-1] : 0);
+}
+
 uint GetInstanceIndexFromVertexId(uint vertexId)
 {
     return 0; //Not implemented
