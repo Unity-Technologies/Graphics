2f6ea2426cc3c42ba95405ea143fa195dc8668c0
Jan Jakobsen <jan.jakobsen@unity3d.com>
2021-08-27 14:26:50 +0200

    debug statement removed

--- TestProjects/ShaderGraph/Assets/IndividualTestSrc/Runtime/ShaderGraphIndividualTests.cs
+++ TestProjects/ShaderGraph/Assets/IndividualTestSrc/Runtime/ShaderGraphIndividualTests.cs
@@ -72,7 +72,7 @@ public class ShaderGraphIndividualTests
                 UpdatedTestAssetMessage updatedMessage = new UpdatedTestAssetMessage();
                 updatedMessage.testData = data;
                 updatedMessage.expectsResultImage = false;
-                Debug.Log($"Sending player connection for data: {updatedMessage.Serialize}");
+                //Debug.Log($"Sending player connection for data: {updatedMessage.Serialize}");
                 PlayerConnection.instance.Send(UpdatedTestAssetMessage.MessageId, updatedMessage.Serialize());
 #endif
             }
