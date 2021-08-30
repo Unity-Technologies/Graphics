Unity-Technologies/Graphics

commit 42cfbe43cd59955061d5547f77a49725dda169d4

[VFX] Fix Sanitize behavior (#4971)
* Add regression test on basic sanitize

See https://unity.slack.com/archives/G1BTWN88Z/p1623825854027500

* Move SanitizeForImport

See this conversation : https://unity.slack.com/archives/C47JPNNTZ/p1624277477261600?thread_ts=1623913383.239100&cid=C47JPNNTZ
However, some concerns :
- No idea about side effects of this solution
- It will lead to a double compilation of sanitized files (if there is a sanitize needed, we will have a new reimport)
- OnPostprocessAllAssets can't be filtered efficiently on the VisualEffectAsset

* *Minor : Missing InvariantCultureIgnoreCase flag in EndsWith

* *Update changelog.md

* *Fix HDRP warning

Hoping it will prevent this issue :
```
Unhandled log message: '[Exception] UnityException: DestroyImmediate is not allowed to be called from a ScriptableObject constructor (or instance field initializer), call it in OnEnable instead. Called from ScriptableObject 'HDWizard'.
See "Script Serialization" page in the Unity Manual for further details.'. Use UnityEngine.TestTools.LogAssert.Expect
```
See https://yamato-artifactviewer.prd.cds.internal.unity3d.com/9f8a60fd-d1f9-48b5-9513-e4e535cf5d7c%2Flogs%2FTestProjects%2FVisualEffectGraph_HDRP%2Ftest-results/TestReportV1.html

* Revert "*Fix HDRP warning"

This reverts commit 99ab5fb3d52971eee3e745fe410dbcef7539032a.

* More conservative way of workaround Preprocess issue

Use a static cache of already santized object
This solution remains a pretty dirty trick :-/
*Before*
- Preprocess : Compile (possibly with side effect modifying the graph)
- PostProcess : Sanitize, if there is a modification, it should relaunch the import & recompile

*After*
- Preprocess : Compile only if the sanitize has been done
- PosProcess : Sanitize, if it's the first sanitize, relaunch the reimport

Locally, it fixes unexpecting missing import of point cache asset (see 09_AttributeMaps).

* Remove DestroyImmediate

com.unity.visualeffectgraph/Editor/Models/VFXGraph.cs
@@ -21,19 +21,59 @@ namespace UnityEditor.VFX
     [InitializeOnLoad]
     class VFXGraphPreprocessor : AssetPostprocessor
     {
-        void OnPreprocessAsset()
+        static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
         {
-            bool isVFX = VisualEffectAssetModicationProcessor.HasVFXExtension(assetPath);
-            if (isVFX)
+            List<string> assetToReimport = null;
+
+            foreach (var assetPath in importedAssets)
             {
-                VisualEffectResource resource = VisualEffectResource.GetResourceAtPath(assetPath);
-                if (resource == null)
-                    return;
-                VFXGraph graph = resource.graph as VFXGraph;
-                if (graph != null)
-                    graph.SanitizeForImport();
-                else
-                    Debug.LogError("VisualEffectGraphResource without graph");
+                bool isVFX = VisualEffectAssetModicationProcessor.HasVFXExtension(assetPath);
+                if (isVFX)
+                {
+                    VisualEffectResource resource = VisualEffectResource.GetResourceAtPath(assetPath);
+                    if (resource == null)
+                        continue;
+                    VFXGraph graph = resource.GetOrCreateGraph(); //resource.graph should be already != null at this stage but GetOrCreateGraph is also assigning the visualEffectResource. It's required for UpdateSubAssets
+                    if (graph != null)
+                    {
+                        bool wasGraphSanitized = graph.sanitized;
+
+                        try
+                        {
+                            graph.SanitizeForImport();
+                            if (!wasGraphSanitized && graph.sanitized)
+                            {
+                                if (assetToReimport == null)
+                                    assetToReimport = new List<string>();
+                                assetToReimport.Add(assetPath);
+                            }
+                        }
+                        catch (Exception exception)
+                        {
+                            Debug.LogErrorFormat("Exception during sanitization of {0} : {1}", assetPath, exception);
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogErrorFormat("VisualEffectGraphResource without graph : {0}", assetPath);
+                    }
+                }
+            }
+
+            //Relaunch previously skipped OnCompileResource
+            if (assetToReimport != null)
+            {
+                foreach (var assetPath in assetToReimport)
+                {
+                    try
+                    {
+                        AssetDatabase.ImportAsset(assetPath, ImportAssetOptions.ForceUpdate);
+                    }
+                    catch (Exception exception)
+                    {
+                        Debug.LogErrorFormat("Exception during reimport of {0} : {1}", assetPath, exception);
+                    }
+                }
             }
         }
 
@@ -57,7 +97,17 @@ namespace UnityEditor.VFX
             {
                 VFXGraph graph = resource.graph as VFXGraph;
                 if (graph != null)
-                    resource.GetOrCreateGraph().CompileForImport();
+                {
+                    if (!graph.sanitized)
+                    {
+                        //Early return, the reimport will be forced with the next OnPostprocessAllAssets after Sanitize
+                        resource.ClearRuntimeData();
+                    }
+                    else
+                    {
+                        resource.GetOrCreateGraph().CompileForImport();
+                    }
+                }
                 else
                     Debug.LogError("OnCompileResource error - VisualEffectResource without graph");
             }
@@ -83,6 +133,12 @@ namespace UnityEditor.VFX
                 //    Debug.LogError("OnSetupMaterial error - VisualEffectResource and VFXModel graph do not match");
                 //    return;
                 //}
+
+                if (!resource.GetOrCreateGraph().sanitized)
+                {
+                    Debug.LogError("OnSetupMaterial error - Graph hasn't been sanitized");
+                    return;
+                }
 
                 // Actual call
                 if (model is IVFXSubRenderer)
@@ -182,9 +238,9 @@ namespace UnityEditor.VFX
         {
             if (string.IsNullOrEmpty(filePath))
                 return false;
-            return filePath.EndsWith(VisualEffectResource.Extension)
-                || filePath.EndsWith(VisualEffectSubgraphBlock.Extension)
-                || filePath.EndsWith(VisualEffectSubgraphOperator.Extension);
+            return filePath.EndsWith(VisualEffectResource.Extension, StringComparison.InvariantCultureIgnoreCase)
+                || filePath.EndsWith(VisualEffectSubgraphBlock.Extension, StringComparison.InvariantCultureIgnoreCase)
+                || filePath.EndsWith(VisualEffectSubgraphOperator.Extension, StringComparison.InvariantCultureIgnoreCase);
         }
 
         static string[] OnWillSaveAssets(string[] paths)
@@ -467,7 +523,7 @@ namespace UnityEditor.VFX
             m_GraphSanitized = true;
             m_GraphVersion = CurrentVersion;
 
-            UpdateSubAssets(); //Should not be necessary : force remove no more referenced object from asset
+            UpdateSubAssets(); //Force remove no more referenced object from the asset & *important* register as persistent new dependencies
         }
 
         private void SanitizeCameraBuffers(HashSet<ScriptableObject> objs)
@@ -947,6 +1003,9 @@ namespace UnityEditor.VFX
                 return m_CompiledData;
             }
         }
+
+        public bool sanitized { get { return m_GraphSanitized; } }
+
         public int version { get { return m_GraphVersion; } }
 
         [SerializeField]

TestProjects/VisualEffectGraph_HDRP/Assets/AllTests/Editor/Tests/VFXDataTests.cs
@@ -93,31 +93,16 @@ namespace UnityEditor.VFX.Test
             Assert.IsInstanceOf<VFXDataParticle>(output.GetData());
         }
 
-        string tempFilePath = "Assets/Temp_vfxTest_Ddata.vfx";
-
-        VFXGraph MakeTemporaryGraph()
-        {
-            if (System.IO.File.Exists(tempFilePath))
-            {
-                AssetDatabase.DeleteAsset(tempFilePath);
-            }
-            var asset = VisualEffectAssetEditorUtility.CreateNewAsset(tempFilePath);
-            VisualEffectResource resource = asset.GetResource(); // force resource creation
-            VFXGraph graph = ScriptableObject.CreateInstance<VFXGraph>();
-            graph.visualEffectResource = resource;
-            return graph;
-        }
-
         [OneTimeTearDown]
         public void CleanUp()
         {
-            AssetDatabase.DeleteAsset(tempFilePath);
+            VFXTestCommon.DeleteAllTemporaryGraph();
         }
 
         [Test]
         public void CheckName_Sharing_Between_Output_Event()
         {
-            var graph = MakeTemporaryGraph();
+            var graph = VFXTestCommon.MakeTemporaryGraph();
 
             var gameObj = new GameObject("CheckData_Sharing_Between_Output_Event");
             var vfxComponent = gameObj.AddComponent<VisualEffect>();

TestProjects/VisualEffectGraph_HDRP/Assets/AllTests/Editor/Tests/VFXSerializationTests.cs
@@ -28,6 +28,38 @@ namespace UnityEditor.VFX.Test
             return VisualEffectAssetEditorUtility.CreateNewAsset(path);
         }
 
+        [Test]
+        public void Sanitize_GetSpawnCount()
+        {
+            string kSourceAsset = "Assets/AllTests/Editor/Tests/VFXSerializationTests_GetSpawnCount.vfx_";
+            var graph = VFXTestCommon.CopyTemporaryGraph(kSourceAsset);
+
+            Assert.AreEqual(5, graph.children.OfType<VFXAttributeParameter>().Count());
+            Assert.AreEqual(5, graph.children.OfType<VFXInlineOperator>().Where(o => o.type == typeof(uint)).Count());
+            Assert.AreEqual(1, graph.children.OfType<VFXInlineOperator>().Where(o => o.type == typeof(float)).Count());
+            Assert.AreEqual(1, graph.children.OfType<Operator.Add>().Count());
+            Assert.AreEqual(1, graph.children.OfType<VFXInlineOperator>().Where(o => o.type == typeof(Color)).Count());
+
+            foreach (var attribute in graph.children.OfType<VFXAttributeParameter>())
+                Assert.IsTrue(attribute.outputSlots[0].HasLink());
+
+            foreach (var inlineUInt in graph.children.OfType<VFXInlineOperator>().Where(o => o.type == typeof(uint)))
+                Assert.IsTrue(inlineUInt.inputSlots[0].HasLink());
+
+            foreach (var inlineFloat in graph.children.OfType<VFXInlineOperator>().Where(o => o.type == typeof(float)))
+                Assert.IsTrue(inlineFloat.inputSlots[0].HasLink());
+
+            foreach (var inlineColor in graph.children.OfType<VFXInlineOperator>().Where(o => o.type == typeof(Color)))
+                Assert.IsTrue(inlineColor.inputSlots[0].HasLink());
+
+            foreach (var add in graph.children.OfType<Operator.Add>())
+            {
+                Assert.AreEqual(2, add.operandCount);
+                Assert.IsTrue(add.inputSlots[0].HasLink());
+                Assert.IsTrue(add.inputSlots[1].HasLink());
+            }
+        }
+
         [OneTimeSetUpAttribute]
         public void OneTimeSetUpAttribute()
         {

TestProjects/VisualEffectGraph_HDRP/Assets/AllTests/Editor/Tests/VFXSerializationTests_GetSpawnCount.vfx_.meta
@@ -1,0 +1,7 @@
+fileFormatVersion: 2
+guid: cbad9e9da51a90e49b5311ce7e1a450d
+DefaultImporter:
+  externalObjects: {}
+  userData: 
+  assetBundleName: 
+  assetBundleVariant: 

TestProjects/VisualEffectGraph_HDRP/ProjectSettings/HDRPProjectSettings.asset
@@ -13,9 +13,16 @@ MonoBehaviour:
   m_Name: 
   m_EditorClassIdentifier: 
   m_ProjectSettingFolderPath: HDRPDefaultResources
+  m_WizardPopupAtStart: 0
   m_LastMaterialVersion: 12
+  m_HDShaderGraphLastSeenVersion: 0
+  m_PluginMaterialVersions:
+    m_Keys: []
+    m_Values: 
+  m_PluginSubTargetVersions:
+    m_Keys: []
+    m_Values: 
   m_Version: 2
-  m_ObsoleteWizardPopupAtStart: 0
   m_ObsoleteWizardPopupAlreadyShownOnce: 0
   m_ObsoleteWizardActiveTab: 0
   m_ObsoleteWizardNeedRestartAfterChangingToDX12: 0

commit f62f1b672d635a221abb96636ecd38ca52c49df6

Actual fix from https://fogbugz.unity3d.com/f/cases/1361601/
The regression has been probably introduced by https://github.com/Unity-Technologies/Graphics/pull/4971

What happens, 1st VFX import:
- Not Sanitized
- Skip first compilation
- PostProcess => Sanitize & CheckGraphBeforeImport
- Reimport

Later, the ShaderGraph changes
- Because the VFX is sanitized
- Doesn't skip the first compilation => Fail
- Post Process => CheckGraphBeforeImport is applied
- ... Later VFX import will be ok until the SG isn't modified.

com.unity.visualeffectgraph/Editor/Models/VFXGraph.cs
@@ -950,6 +950,11 @@ namespace UnityEditor.VFX
                         }
                     }
                 }
+
+                // Check Graph Before Import can be needed to synchronize modified shaderGraph
+                foreach (var child in children)
+                    child.CheckGraphBeforeImport();
+
                 // Graph must have been sanitized at this point by the VFXGraphPreprocessor.OnPreprocess
                 BuildSubgraphDependencies();
                 PrepareSubgraphs();