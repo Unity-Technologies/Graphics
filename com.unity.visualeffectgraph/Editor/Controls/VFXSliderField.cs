2aab0924135e0a871341d87beb3d912a9a04968e
Julien Amsellem <julien.amsellem@unity3d.com>
2021-09-02 16:54:29 +0200

    Do not recompile the VFX when property min/max value change

--- com.unity.visualeffectgraph/Editor/Controls/VFXSliderField.cs
+++ com.unity.visualeffectgraph/Editor/Controls/VFXSliderField.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEditor.UIElements;
@@ -123,16 +124,17 @@ namespace UnityEditor.VFX.UI
             }
             set
             {
-                m_Range = value;
                 m_IgnoreNotification = true;
-                if (m_Slider.lowValue != m_Range.x || m_Slider.highValue != m_Range.y)
+                if ((m_Slider.lowValue != value.x || m_Slider.highValue != value.y) && value.x < value.y)
                 {
+                    m_Range = value;
                     m_Slider.lowValue = m_Range.x;
                     m_Slider.highValue = m_Range.y;
 
-                    m_Slider.value = m_Range.x;
-                    m_Slider.value = m_Range.y;
-                    m_Slider.value = ValueToFloat(this.value);
+                    if (m_Slider.value < m_Slider.lowValue || m_Slider.value > m_Slider.highValue)
+                    {
+                        m_Slider.value = m_Slider.lowValue;
+                    }
                 }
                 m_IgnoreNotification = false;
             }
