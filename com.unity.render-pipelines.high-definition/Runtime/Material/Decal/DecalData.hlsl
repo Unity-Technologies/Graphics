4f2d7d748a2f06153fae3ac645add991f4e97431
Jarkko Lempiäinen <jarkko.lempiainen@unity3d.com>
2021-09-06 11:05:15 +0300

    - initial fix for the emissive decal object silhouette issue

--- com.unity.render-pipelines.high-definition/Runtime/Material/Decal/DecalData.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Material/Decal/DecalData.hlsl
@@ -31,7 +31,17 @@ void GetSurfaceData(FragInputs input, float3 V, PositionInputs posInput, float a
 #ifdef _MATERIAL_AFFECTS_EMISSION
     surfaceData.emissive = _EmissiveColor.rgb * fadeFactor;
     #ifdef _EMISSIVEMAP
-    surfaceData.emissive *= SAMPLE_TEXTURE2D(_EmissiveColorMap, sampler_EmissiveColorMap, texCoords).rgb;
+    float2 emissiveColorMapSize;
+    float emissiveColorMapLODs;
+    _EmissiveColorMap.GetDimensions(0, emissiveColorMapSize.x, emissiveColorMapSize.y, emissiveColorMapLODs);
+    float2 uvdx = ddx(texCoords * emissiveColorMapSize), uvdy = ddy(texCoords * emissiveColorMapSize);
+//    float lod = 0.5f * log2(dot(uvdx, uvdx) + dot(uvdy, uvdy)) - 1.0f;
+    float lod = 0.5f * log2(max(dot(uvdx, uvdx), dot(uvdy, uvdy))) - 1.0f;
+    float lddx = ddx(posInput.linearDepth), lddy  = ddy(posInput.linearDepth);
+    float ldd = dot(lddx, lddx) + dot(lddy, lddy);
+    float maxlod = emissiveColorMapLODs * (1.0f - saturate(8.0f * ldd));
+    surfaceData.emissive *= SAMPLE_TEXTURE2D_LOD(_EmissiveColorMap, sampler_EmissiveColorMap, texCoords, min(lod, maxlod)).rgb;
+//    surfaceData.emissive *= SAMPLE_TEXTURE2D(_EmissiveColorMap, sampler_EmissiveColorMap, texCoords).rgb;
     #endif
 
     // Inverse pre-expose using _EmissiveExposureWeight weight
