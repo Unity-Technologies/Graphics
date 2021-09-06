ca07992f43ebf8719e2f1319bebac13e62e95946
Alex Vázquez <alex.vazquez@unity3d.com>
2021-09-06 11:59:34 +0200

    Docs

--- com.unity.render-pipelines.core/Editor/Lighting/LightUI.Drawers.cs
+++ com.unity.render-pipelines.core/Editor/Lighting/LightUI.Drawers.cs
@@ -9,6 +9,11 @@ namespace UnityEditor.Rendering
     /// </summary>
     public partial class LightUI
     {
+        /// <summary>
+        /// Draws the color temperature for a serialized light
+        /// </summary>
+        /// <param name="serialized">The serizalized light</param>
+        /// <param name="owner">The editor</param>
         public static void DrawColor(ISerializedLight serialized, Editor owner)
         {
             if (GraphicsSettings.lightsUseLinearIntensity && GraphicsSettings.lightsUseColorTemperature)
