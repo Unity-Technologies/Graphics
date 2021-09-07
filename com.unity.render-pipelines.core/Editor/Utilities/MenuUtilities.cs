85899d3d71b1c945580afb54d7bd73fb8fabc5e2
RSlysz <remi@unity3d.com>
2021-09-07 10:57:12 +0200

    Remove Unused MenuItemForRenderPipeline

--- com.unity.render-pipelines.core/Editor/Utilities/MenuUtilities.cs
+++ com.unity.render-pipelines.core/Editor/Utilities/MenuUtilities.cs
@@ -14,8 +14,6 @@ namespace UnityEditor.Rendering.Utilities
         {
             InitMethodsWithReflection();
 
-            ConstructMenuItemForRenderPipeline();
-
             RenderPipelineManager.activeRenderPipelineTypeChanged += Update;
 
             // PR on Hold due to this:
@@ -29,9 +27,6 @@ namespace UnityEditor.Rendering.Utilities
         {
             UpdateRenderGraph();
             UpdateLookDev();
-
-            // should always be updated last
-            UpdateMenuItemForRenderPipeline();
         }
 
         #region Reflection
@@ -79,83 +74,6 @@ namespace UnityEditor.Rendering.Utilities
 
         #endregion
 
-        #region MenuItemForRenderPipeline Management
-
-        class MenuItemData
-        {
-            public MenuItemForRenderPipeline attribute;
-            public Action action;
-            public Func<bool> validate;
-        }
-
-        static private Dictionary<Type, List<MenuItemData>> s_MenuMap = new Dictionary<Type, List<MenuItemData>>();
-
-        static void RegisterMenu(MenuItemData element)
-            => s_AddMenuItem(element.attribute.path,
-                string.Empty,
-                false,
-                element.attribute.priority,
-                element.action,
-                element.validate);
-
-        static void UnregisterMenu(MenuItemData element)
-            => s_RemoveMenuItem(element.attribute.path);
-
-        static void ConstructMenuItemForRenderPipeline()
-        {
-            // Construct the MenuMap
-            // Possible scenario to take into acount:
-            //   - validate method can come before action method
-            //   - all packages add with same attribute (renderPipelineAssetTypes = { HDRPAsset, URPAsset })
-            //   - each package add its own attribute (one with renderPipelineAssetTypes = { HDRPAsset }, other with renderPipelineAssetTypes = { URPAsset })
-            var filteredMethods = TypeCache.GetMethodsWithAttribute<MenuItemForRenderPipeline>();
-            foreach (var methodInfo in filteredMethods)
-            {
-                var attribute = methodInfo.GetCustomAttribute<MenuItemForRenderPipeline>();
-                foreach (Type rpAssetType in attribute.renderPipelineAssetTypes)
-                {
-                    if (!s_MenuMap.TryGetValue(rpAssetType, out var menuItemsForRP))
-                    {
-                        menuItemsForRP = new List<MenuItemData>();
-                        s_MenuMap[rpAssetType] = menuItemsForRP;
-                    }
-
-                    var query = menuItemsForRP.Where(mi => mi.attribute.path == attribute.path);
-                    bool alreadyRegistered = query.Count() > 0;
-                    var menuItemData = alreadyRegistered ? query.First() : new MenuItemData();
-
-                    menuItemData.attribute = attribute;
-                    if (attribute.validate)
-                        menuItemData.validate = (Func<bool>)Delegate.CreateDelegate(typeof(Func<bool>), methodInfo);
-                    else
-                        menuItemData.action = (Action)Delegate.CreateDelegate(typeof(Action), methodInfo);
-
-                    if (!alreadyRegistered)
-                        menuItemsForRP.Add(menuItemData);
-                }
-            }
-        }
-
-        static void UpdateMenuItemForRenderPipeline()
-        {
-            // remove
-            foreach (var menuItemData in s_MenuMap.Values.SelectMany(mid => mid))
-                UnregisterMenu(menuItemData); //do nothing if not registered before
-
-            // add if required
-
-            // Note: GraphicsSettings.currentRenderPipeline == null possible and should mean built-in
-            // though we cannot use null as key so it was early replaced by typeof(RenderPipelineAsset)
-            Type currentSRPType = GraphicsSettings.currentRenderPipeline?.GetType() ?? typeof(RenderPipelineAsset);
-            if (!s_MenuMap.TryGetValue(currentSRPType, out var menuItemDatasForRP))
-                return;
-
-            foreach (var menuItemData in menuItemDatasForRP)
-                RegisterMenu(menuItemData);
-        }
-
-        #endregion
-
         #region Specific Menus
 
         static void UpdateLookDev()
@@ -193,42 +111,5 @@ namespace UnityEditor.Rendering.Utilities
         }
 
         #endregion
-    }
-
-    /// <summary>MenuItem filtered by used render pipeline</summary>
-    [AttributeUsage(AttributeTargets.Method)]
-    public class MenuItemForRenderPipeline : Attribute
-    {
-        /// <summary>Supported render pipeline types</summary>
-        public readonly Type[] renderPipelineAssetTypes;
-        /// <summary>Path in menu tree</summary>
-        public readonly string path;
-        /// <summary>Position in tree</summary>
-        public readonly int priority;
-        /// <summary>Is this the validate method?</summary>
-        public readonly bool validate;
-
-        /// <summary>MenuItem filtered by used render pipeline</summary>
-        /// <param name="path">Path in menu tree</param>
-        /// <param name="validate">Is this the validate method?</param>
-        /// <param name="priority">Position in tree</param>
-        /// <param name="renderPipelineAssetTypes">Supported render pipeline types</param>
-        public MenuItemForRenderPipeline(string path, bool validate = false, int priority = 0, params Type[] renderPipelineAssetTypes)
-        {
-            if (renderPipelineAssetTypes == null || renderPipelineAssetTypes.Length == 0)
-                throw new ArgumentNullException("Argument cannot be null. Use MenuItem instead if you don't need to filter per RenderPipeline.", nameof(renderPipelineAssetTypes));
-
-            // Note: rpAssetType == null possible and should mean built-in
-            // but this is not possible as a key in s_MenuMap so we use the forbiden RenderPipelineAsset type for that
-            foreach (Type type in renderPipelineAssetTypes)
-                if (type != null && !typeof(RenderPipelineAsset).IsAssignableFrom(type))
-                    throw new ArgumentException("Argument must be a RenderPipelineAsset child type", nameof(renderPipelineAssetTypes));
-
-            this.renderPipelineAssetTypes = renderPipelineAssetTypes
-                .Select(rpat => rpat != null ? rpat : typeof(RenderPipelineAsset)).ToArray();
-            this.path = path;
-            this.priority = priority;
-            this.validate = validate;
-        }
     }
 }
