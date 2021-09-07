9f193bce1a950f3d510c390e95ca64521ba14f00
Alex Vázquez <alex.vazquez@unity3d.com>
2021-09-07 16:31:35 +0200

    Docs

--- com.unity.render-pipelines.core/Runtime/Common/IAdditionalData.cs
+++ com.unity.render-pipelines.core/Runtime/Common/IAdditionalData.cs
@@ -1,6 +1,9 @@
 
 namespace UnityEngine.Rendering
 {
+    /// <summary>
+    /// Interface to identify additional data components
+    /// </summary>
     public interface IAdditionalData
     {
     }
