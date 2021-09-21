288f49726fd8ae63d9a645738c7ce51f1f3f59c7
Lukas <lukasc@unity3d.com>
2021-09-21 13:30:14 +0300

    Adding fake passes to custom renderer

--- com.unity.testing.urp/Scripts/Runtime/CustomRenderPipeline/CustomRenderer.cs
+++ com.unity.testing.urp/Scripts/Runtime/CustomRenderPipeline/CustomRenderer.cs
@@ -7,11 +7,16 @@ namespace UnityEngine.Rendering.Universal
         private DrawObjectsPass m_RenderOpaqueForwardPass;
 
         ForwardLights m_ForwardLights;
+        FakeMainLightShadowCasterPass m_FakeMainLightShadowCasterPass;
+        FakeAdditionalLightsShadowCasterPass m_FakeAdditionalLightsShadowCasterPass;
 
         public CustomRenderer(CustomRenderGraphData data) : base(data)
         {
             m_RenderOpaqueForwardPass = new DrawObjectsPass("Render Opaques", true, RenderPassEvent.BeforeRenderingOpaques + 1, RenderQueueRange.opaque, -1, StencilState.defaultValue, 0);
             m_ForwardLights = new ForwardLights();
+            m_FakeMainLightShadowCasterPass = new FakeMainLightShadowCasterPass(RenderPassEvent.BeforeRenderingShadows);
+            m_FakeAdditionalLightsShadowCasterPass = new FakeAdditionalLightsShadowCasterPass(RenderPassEvent.BeforeRenderingShadows);
+            stripShadowsOffVariants = true;
         }
 
         public override void Setup(ScriptableRenderContext context, ref RenderingData renderingData)
@@ -21,6 +26,8 @@ namespace UnityEngine.Rendering.Universal
             foreach (var feature in rendererFeatures)
                 feature.AddRenderPasses(this, ref renderingData);
             EnqueuePass(m_RenderOpaqueForwardPass);
+            EnqueuePass(m_FakeMainLightShadowCasterPass);
+            EnqueuePass(m_FakeAdditionalLightsShadowCasterPass);
         }
 
         public override void SetupLights(ScriptableRenderContext context, ref RenderingData renderingData)
