cd004670314ac1c5ea2976bd56a92582a9e400de
Jesse Barker <jesseb@unity3d.com>
2021-09-21 11:05:52 -0700

    Fix for 1366058 - Make sure to visit all search results once and select best match based on a score of the relevance of the match.

--- com.unity.shadergraph/Editor/Drawing/SearchWindowAdapter.cs
+++ com.unity.shadergraph/Editor/Drawing/SearchWindowAdapter.cs
@@ -18,50 +18,117 @@ namespace UnityEditor.ShaderGraph
             m_DefaultItemTemplate = Resources.Load<VisualTreeAsset>("SearcherItem");
         }
 
-        public override SearcherItem OnSearchResultsFilter(IEnumerable<SearcherItem> searchResults, string searchQuery)
+        private SearcherItem GetFirstChildItem(SearcherItem item)
         {
-            // Sort results by length so that shorter length results are prioritized
-            // prevents entries with short names getting stuck at end of list after entries with longer names when both contain the same word
-            searchResults = searchResults.OrderBy(x => x.Name.Length).ToList();
-
-            var scrollToItem = searchResults.First();
-
-            if (scrollToItem.Children.Count != 0)
+            if (item.Children.Count != 0)
             {
                 SearcherItem childIterator = null;
                 // Discard searcher item for selection if it is a category, get next best child item from it instead
                 // There is no utility in selecting category headers/titles, only the leaf entries
-                childIterator = scrollToItem.Children[0];
+                childIterator = item.Children[0];
                 while (childIterator != null && childIterator.Children.Count != 0)
                 {
                     childIterator = childIterator.Children[0];
                 }
 
-                scrollToItem = childIterator;
+                item = childIterator;
             }
 
-            if (searchQuery.Length != 0 && scrollToItem.Parent != null)
-            {
-                var queryTerms = searchQuery.Split(' ');
+            return item;
+        }
 
-                SearcherItem parentItem = scrollToItem.Parent;
-                foreach (var word in queryTerms)
+        private int ComputeScoreForMatch(string queryTerm, SearcherItem matchItem)
+        {
+            // Scoring Criteria:
+            // - Exact name match is most preferred.
+            // - Partial name match is next.
+            // - Exact synonym match is next.
+            // - Partial synonym match is next.
+            // - No match is last.
+            int score = 0;
+
+            bool isSynonymMatch = false;
+            bool isSynonymPartial = false;
+            bool isNameMatch = false;
+            bool isNamePartial = false;
+
+            // Split the entry name so that we can remove suffix that looks like "Clamp: In(4)"
+            var nameSansSuffix = matchItem.Name.Split(':');
+            if (nameSansSuffix[0].Equals(queryTerm, StringComparison.OrdinalIgnoreCase))
+            {
+                isNameMatch = true;
+                score += 1000000;
+            }
+            else if (nameSansSuffix[0].Contains(queryTerm, StringComparison.OrdinalIgnoreCase))
+            {
+                isNamePartial = true;
+                score += 100000;
+                score -= (nameSansSuffix[0].Length - queryTerm.Length);
+            }
+
+            if (matchItem.Synonyms == null)
+                return score;
+
+            // Check for synonym matches
+            foreach (var syn in matchItem.Synonyms)
+            {
+                if (syn.Equals(queryTerm, StringComparison.OrdinalIgnoreCase))
                 {
-                    // Do a check for an exact match within this last tier of search results
-                    foreach (var exactMatchItem in parentItem.Children)
+                    isSynonymMatch = true;
+                    score += 10000;
+                }
+                else if (syn.Contains(queryTerm, StringComparison.OrdinalIgnoreCase))
+                {
+                    isSynonymPartial = true;
+                    score += 1000;
+                    score -= (syn.Length - queryTerm.Length);
+                }
+            }
+
+            return score;
+        }
+
+        public override SearcherItem OnSearchResultsFilter(IEnumerable<SearcherItem> searchResults, string searchQuery)
+        {
+            if (searchQuery.Length == 0)
+                return searchResults.FirstOrDefault();
+
+            // Sort results by length so that shorter length results are prioritized
+            // prevents entries with short names getting stuck at end of list after entries with longer names when both contain the same word
+            searchResults = searchResults.OrderBy(x => x.Name.Length).ToList();
+
+            var bestMatch = GetFirstChildItem(searchResults.FirstOrDefault());
+            int bestScore = 0;
+            List<int> visitedItems = new List<int>();
+            foreach (var result in searchResults)
+            {
+                var currentItem = GetFirstChildItem(result);
+
+                if (currentItem.Parent != null)
+                {
+                    var queryTerms = searchQuery.Split(' ');
+
+                    SearcherItem parentItem = currentItem.Parent;
+                    foreach (var word in queryTerms)
                     {
-                        // Split the entry name so that we can remove suffix that looks like "Clamp: In(4)"
-                        var nameSansSuffix = exactMatchItem.Name.Split(':');
-                        if (nameSansSuffix[0].Equals(word, StringComparison.OrdinalIgnoreCase))
+                        foreach (var matchItem in parentItem.Children)
                         {
-                            scrollToItem = exactMatchItem;
-                            break;
+                            if (visitedItems.Contains(matchItem.Id))
+                                continue;
+
+                            int currentScore = ComputeScoreForMatch(word, matchItem);
+                            if (currentScore > bestScore)
+                            {
+                                bestScore = currentScore;
+                                bestMatch = matchItem;
+                            }
+                            visitedItems.Add(matchItem.Id);
                         }
                     }
                 }
             }
 
-            return scrollToItem;
+            return bestMatch;
         }
     }
 
