9e9583d64d4b69e3e5aa361ce88cb9377b8f7a7c
Julien Amsellem <julien.amsellem@unity3d.com>
2021-09-21 10:43:59 +0200

    Added error feedback on texture slots if the value has wrong dimensions

--- com.unity.visualeffectgraph/Editor/Models/Slots/Implementations/VFXSlotTexture3D.cs
+++ com.unity.visualeffectgraph/Editor/Models/Slots/Implementations/VFXSlotTexture3D.cs
@@ -1,6 +1,4 @@
-using System;
 using UnityEngine;
-using UnityEngine.VFX;
 using UnityEngine.Rendering;
 
 
@@ -9,6 +7,14 @@ namespace UnityEditor.VFX
     [VFXInfo(type = typeof(Texture3D))]
     class VFXSlotTexture3D : VFXSlotObject
     {
+        protected override void GenerateErrors(VFXInvalidateErrorReporter manager)
+        {
+            if (value is Texture texture && texture.dimension != TextureDimension.Tex3D)
+                manager.RegisterError("Slot_Value_Incorrect_Texture3D", VFXErrorType.Error, "This slot expects a Texture3D");
+
+            base.GenerateErrors(manager);
+        }
+
         public override VFXValue DefaultExpression(VFXValue.Mode mode)
         {
             return new VFXTexture3DValue(0, mode);
