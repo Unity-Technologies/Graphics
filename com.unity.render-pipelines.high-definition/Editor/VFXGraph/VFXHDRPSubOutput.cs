8f7cc14a943c921d750fd0a97b8982c047b661b5
Paul Demeulenaere <pauld@unity3d.com>
2021-09-23 15:39:51 +0200

    Apply rename in VFXHDRPSubOutput

--- com.unity.render-pipelines.high-definition/Editor/VFXGraph/VFXHDRPSubOutput.cs
+++ com.unity.render-pipelines.high-definition/Editor/VFXGraph/VFXHDRPSubOutput.cs
@@ -147,18 +147,18 @@ namespace UnityEditor.VFX.HDRP
         private int GetRenderQueueOffset()
         {
             var renderQueueType = GetRenderQueueType();
-            var materialOffset = GetMaterialOffset();
+            var materialSortingPriority = GetMaterialSortingPriority();
             return owner is VFXDecalHDRPOutput ?
-                HDRenderQueue.Clamps(k_RenderQueue_AllOpaque, ChangeType(renderQueueType, 0, owner.hasAlphaClipping) + materialOffset) :
-                ChangeType(renderQueueType, materialOffset, owner.hasAlphaClipping);
+                HDRenderQueue.Clamps(k_RenderQueue_AllOpaque, ChangeType(renderQueueType, 0, owner.hasAlphaClipping) + materialSortingPriority) :
+                ChangeType(renderQueueType, materialSortingPriority, owner.hasAlphaClipping);
         }
 
-        private int GetMaterialOffset()
+        private int GetMaterialSortingPriority()
         {
             if (supportsSortingPriority)
             {
-                int rawMaterialOffset = owner.GetMaterialOffset();
-                return ClampsTransparentRangePriority(rawMaterialOffset);
+                int rawMaterialSortingPriority = owner.GetMaterialSortingPriority();
+                return ClampsTransparentRangePriority(rawMaterialSortingPriority);
             }
             return 0;
         }
