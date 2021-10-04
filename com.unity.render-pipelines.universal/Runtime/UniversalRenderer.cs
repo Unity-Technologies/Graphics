3639f88d534e32c89b4d1d1f23d73b6c966c654f
nigeljw-unity <72749964+nigeljw-unity@users.noreply.github.com>
2021-10-04 19:43:24 +0200

    Minimal enclosing sphere backport to 2021.2 (#5722)
    Co-authored-by: Erik Hakala <erik.hakala@unity3d.com>
    Co-authored-by: Oleksandr Kokoshyn <oleksandr.kokoshyn@unity3d.com>

--- com.unity.render-pipelines.universal/Runtime/UniversalRenderer.cs
+++ com.unity.render-pipelines.universal/Runtime/UniversalRenderer.cs
@@ -896,6 +896,10 @@ namespace UnityEngine.Rendering.Universal
                 cullingParameters.maximumVisibleLights = UniversalRenderPipeline.maxVisibleAdditionalLights + 1;
             }
             cullingParameters.shadowDistance = cameraData.maxShadowDistance;
+
+            cullingParameters.conservativeEnclosingSphere = UniversalRenderPipeline.asset.conservativeEnclosingSphere;
+
+            cullingParameters.numIterationsEnclosingSphere = UniversalRenderPipeline.asset.numIterationsEnclosingSphere;
         }
 
         /// <inheritdoc />
