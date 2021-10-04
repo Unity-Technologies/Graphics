7ba683ebebb880dcab098fd14d8e62559ceb921e
Jonas Sideravicius <jonass@unity3d.com>
2021-10-04 12:16:37 +0300

    reverting ProjectionParams.x changes to test some issues

--- com.unity.render-pipelines.universal/Runtime/ScriptableRenderer.cs
+++ com.unity.render-pipelines.universal/Runtime/ScriptableRenderer.cs
@@ -205,7 +205,11 @@ namespace UnityEngine.Rendering.Universal
             // Projection flip sign logic is very deep in GfxDevice::SetInvertProjectionMatrix
             // This setup is tailored especially for overlay camera game view
             // For other scenarios this will be overwritten correctly by SetupCameraProperties
-            float projectionFlipSign = cameraData.IsCameraProjectionMatrixFlipped() ? -1.0f : 1.0f;
+
+            // float projectionFlipSign = cameraData.IsCameraProjectionMatrixFlipped() ? -1.0f : 1.0f;
+            bool isOffscreen = cameraData.targetTexture != null;
+            bool invertProjectionMatrix = isOffscreen && SystemInfo.graphicsUVStartsAtTop;
+            float projectionFlipSign = invertProjectionMatrix ? -1.0f : 1.0f;
             Vector4 projectionParams = new Vector4(projectionFlipSign, near, far, 1.0f * invFar);
             cmd.SetGlobalVector(ShaderPropertyId.projectionParams, projectionParams);
 
