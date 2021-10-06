084242d3ef6f5e1fdc3dca9dbdae4649fb28dba3
Antoine Lelievre <antoinel@unity3d.com>
2021-10-06 11:21:05 +0200

    Cleanup + doc

--- com.unity.render-pipelines.core/Runtime/RenderGraph/RenderGraph.cs
+++ com.unity.render-pipelines.core/Runtime/RenderGraph/RenderGraph.cs
@@ -56,6 +56,28 @@ namespace UnityEngine.Experimental.Rendering.RenderGraphModule
         public CommandBuffer commandBuffer;
     }
 
+    /// <summary>
+    /// This struct is used to define the scope where the Render Graph is recorded before the execution.
+    /// When this struct goes out of scope or is disposed, the Render Graph will be automatically executed.
+    /// </summary>
+    /// <seealso cref="RenderGraph.RecordAndExecute(in RenderGraphParameters)"/>
+    public struct RenderGraphExecution : IDisposable
+    {
+        RenderGraph renderGraph;
+
+        /// <summary>
+        /// Internal constructor for RenderGraphExecution
+        /// </summary>
+        /// <param name="renderGraph">renderGraph</param>
+        internal RenderGraphExecution(RenderGraph renderGraph)
+            => this.renderGraph = renderGraph;
+
+        /// <summary>
+        /// This function triggers the Render Graph to be executed.
+        /// </summary>
+        public void Dispose() => renderGraph.Execute();
+    }
+
     class RenderGraphDebugParams
     {
         DebugUI.Widget[] m_DebugItems;
@@ -596,26 +618,21 @@ namespace UnityEngine.Experimental.Rendering.RenderGraphModule
             return AddRenderPass(passName, out passData, GetDefaultProfilingSampler(passName));
         }
 
-        public struct RenderGraphExecution : IDisposable
-        {
-            RenderGraph renderGraph;
-
-            internal RenderGraphExecution(RenderGraph renderGraph)
-            {
-                this.renderGraph = renderGraph;
-            }
-
-            public void Dispose()
-            {
-                renderGraph.Execute();
-            }
-        }
-
         /// <summary>
-        /// Begin using the render graph.
+        /// Starts the recording of the the render graph and then automatically execute when the return value goes out of scope.
         /// This must be called before adding any pass to the render graph.
         /// </summary>
         /// <param name="parameters">Parameters necessary for the render graph execution.</param>
+        /// <example>
+        /// This shows how to increment an integer.
+        /// <code>
+        /// using (renderGraph.RecordAndExecute(parameters))
+        /// {
+        ///     // Add your render graph passes here.
+        /// }
+        /// </code>
+        /// </example>
+        /// <seealso cref="RenderGraphExecution"/>
         public RenderGraphExecution RecordAndExecute(in RenderGraphParameters parameters)
         {
             m_CurrentFrameIndex = parameters.currentFrameIndex;
@@ -670,7 +687,7 @@ namespace UnityEngine.Experimental.Rendering.RenderGraphModule
             try
             {
                 if (m_RenderGraphContext.cmd == null)
-                    throw new InvalidOperationException("RenderGraph.Begin was not called before executing the render graph.");
+                    throw new InvalidOperationException("RenderGraph.RecordAndExecute was not called before executing the render graph.");
 
 
                 if (!m_DebugParameters.immediateMode)
