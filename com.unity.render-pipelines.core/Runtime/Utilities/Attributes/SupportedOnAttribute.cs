9134141877f057a2faf3f76b1bb05e38d728effe
Frédéric Vauchelles <fredericv@unity3d.com>
2021-10-11 09:51:23 +0200

    Updated attribute SupportedOn

--- com.unity.render-pipelines.core/Runtime/Utilities/Attributes/SupportedOnAttribute.cs
+++ com.unity.render-pipelines.core/Runtime/Utilities/Attributes/SupportedOnAttribute.cs
@@ -1,30 +1,29 @@
+#define SUPPORT_DYNAMIC
+
 using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
-using System.Collections.Generic;
 
-// TODO: Evaluate performance improvement at domain reload, vs all type attribute parsing.
-
-// Note: Static method is about 10x faster at querying for generic API than dynamic method
 // Note: This is safe for AOT platforms:
 //  - If you use the object based API, it will fallback on a in memory dictionary
 //  - If you use the generic API, the AOT compiler will pick up the associated classes and include them into the build.
 namespace UnityEngine.Rendering
 {
-    #region Public API
     /// <summary>
     /// Use this attribute to specify that a type is compatible with another.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    [AttributeUsage(AttributeTargets.Class)]
     public class SupportedOnAttribute : Attribute
     {
         /// <summary>
         /// Constructor of the attribute.
         /// </summary>
-        /// <param name="subject"></param>
-        /// <param name="target"></param>
+        /// <param name="subject">Which type will support <paramref name="target"/>.</param>
+        /// <param name="target">Defines what to support.</param>
         public SupportedOnAttribute(Type subject, Type target)
         {
+            // Call directly registration here. This avoids storing properties in the memory footprint
             IsSupportedOn.RegisterStaticRelation(subject, target);
             IsSupportedOn.RegisterDynamicRelation(subject, target);
         }
@@ -33,55 +32,98 @@ namespace UnityEngine.Rendering
     /// <summary>
     /// Utilities to query the IsSupported relation.
     /// </summary>
-    public static partial class IsSupportedOn
+    public static class IsSupportedOn
     {
+#if SUPPORT_DYNAMIC
+        static DynamicTypeRelation s_Relations = new DynamicTypeRelation();
+#else
+        const string k_Unsupported = "Dynamic querying is unsupported, please add SUPPORT_DYNAMIC keyword";
+#endif
+
         /// <summary>
-        /// Is <typeparamref name="TSubject"/> supported by  <typeparamref name="TTarget"/>?
+        /// Is <typeparamref name="TSubject"/> supported by  <typeparamref name="TTarget"/>?.
+        ///
+        /// This is 10x faster than querying dynamically.
+        /// See <see cref="IsSupportedOn{TSubject,TTarget}.Value"/>.
         /// </summary>
-        /// <typeparam name="TSubject"></typeparam>
-        /// <typeparam name="TTarget"></typeparam>
-        /// <returns></returns>
-        public static bool IsRelated<TSubject, TTarget>()
+        /// <typeparam name="TSubject">Which type will support <typeparamref name="TTarget"/>.</typeparam>
+        /// <typeparam name="TTarget">Defines what to support.</typeparam>
+        /// <returns>Is <typeparamref name="TSubject"/> supported by  <typeparamref name="TTarget"/>?.</returns>
+        public static bool IsRelated<TSubject, TTarget>() => IsSupportedOn<TSubject, TTarget>.Value;
+
+        /// <summary>
+        /// Does <typeparamref name="TSubject"/> explicitly support another type?
+        /// </summary>
+        /// <typeparam name="TSubject">Does it explicitly support another type?</typeparam>
+        /// <returns>Does <typeparamref name="TSubject"/> explicitly support another type?</returns>
+        public static bool HasRelations<TSubject>() => HasIsSupportedOn<TSubject>.Value;
+
+        /// <summary>
+        /// Set the value in the static types
+        /// </summary>
+        /// <param name="subject">Which type will support <paramref name="target"/>.</param>
+        /// <param name="target">Defines what to support.</param>
+        internal static void RegisterStaticRelation(Type subject, Type target)
         {
-            return StaticIsRelated<TSubject, TTarget>();
+            var hasIsSupportedOn = typeof(HasIsSupportedOn<>).MakeGenericType(subject);
+            hasIsSupportedOn.GetProperty("Value", BindingFlags.Static | BindingFlags.Public).SetValue(null, true);
+
+            var isSupportedOn = typeof(IsSupportedOn<,>).MakeGenericType(subject, target);
+            isSupportedOn.GetProperty("InternalValue", BindingFlags.Static | BindingFlags.NonPublic).SetValue(null, true);
+        }
+
+        /// <summary>
+        /// Set the value in a dictionary.
+        ///
+        /// Note for AOT platforms: it may be required to use the dynamic backend because using generic structs
+        ///   require JIT.
+        /// </summary>
+        /// <param name="subject">Which type will support <paramref name="target"/>.</param>
+        /// <param name="target">Defines what to support.</param>
+        [Conditional("SUPPORT_DYNAMIC")]
+        internal static void RegisterDynamicRelation(Type subject, Type target)
+        {
+#if SUPPORT_DYNAMIC
+            s_Relations.RegisterRelation(subject, target);
+#endif
         }
 
         /// <summary>
         /// Is <paramref name="subject"/> supported by  <typeparamref name="target"/>?
         /// </summary>
-        /// <param name="subject"></param>
-        /// <param name="target"></param>
+        /// <param name="subject">Which type will support <paramref name="target"/>.</param>
+        /// <param name="target">Defines what to support.</param>
         /// <returns></returns>
         public static bool IsRelated(Type subject, Type target)
         {
-            return DynamicIsRelated(subject, target);
-        }
-
-        /// <summary>
-        /// Does <typeparamref name="TSubject"/> explicitly support another type?
-        /// </summary>
-        /// <typeparam name="TSubject"></typeparam>
-        /// <returns></returns>
-        public static bool HasRelations<TSubject>()
-        {
-            return StaticHasRelations<TSubject>();
+#if SUPPORT_DYNAMIC
+            return s_Relations.IsRelated(subject, target);
+#else
+            Debug.Log(k_Unsupported);
+            return default;
+#endif
         }
 
         /// <summary>
         /// Does <paramref name="subject"/> explicitly support another type?
         /// </summary>
-        /// <param name="subject"></param>
+        /// <param name="subject">Does it explicitly support another type?</param>
         /// <returns></returns>
         public static bool HasRelations(Type subject)
         {
-            return DynamicHasRelations(subject);
+#if SUPPORT_DYNAMIC
+            return s_Relations.HasRelations(subject);
+#else
+            Debug.Log(k_Unsupported);
+            return default;
+#endif
         }
     }
 
     /// <summary>
     /// Use <see cref="Value"/> to know if <typeparamref name="TSubject"/> explicitly support at least one type.
     /// </summary>
-    /// <typeparam name="TSubject"></typeparam>
+    /// <typeparam name="TSubject">The type to query.</typeparam>
     public struct HasIsSupportedOn<TSubject>
     {
         /// <summary>
@@ -93,8 +135,8 @@ namespace UnityEngine.Rendering
     /// <summary>
     /// Use <see cref="Value"/> to know if <typeparamref name="TSubject"/> explicitly supports <typeparamref name="TTarget"/>.
     /// </summary>
-    /// <typeparam name="TSubject"></typeparam>
-    /// <typeparam name="TTarget"></typeparam>
+    /// <typeparam name="TSubject">The subject to query.</typeparam>
+    /// <typeparam name="TTarget">The type that defines what to support.</typeparam>
     public struct IsSupportedOn<TSubject, TTarget>
     {
         static bool InternalValue
@@ -106,87 +148,6 @@ namespace UnityEngine.Rendering
         public static bool Value
             => HasIsSupportedOn<TSubject>.Value && InternalValue || !HasIsSupportedOn<TSubject>.Value;
     }
-    #endregion
-
-    #region Static Query support
-    public static partial class IsSupportedOn
-    {
-        /// <summary>
-        /// Set the value in the static types
-        /// </summary>
-        /// <param name="subject"></param>
-        /// <param name="target"></param>
-        internal static void RegisterStaticRelation(Type subject, Type target)
-        {
-            var hasIsSupportedOn = typeof(HasIsSupportedOn<>).MakeGenericType(subject);
-            hasIsSupportedOn.GetProperty("Value", BindingFlags.Static | BindingFlags.Public).SetValue(null, true);
-
-            var isSupportedOn = typeof(IsSupportedOn<,>).MakeGenericType(subject, target);
-            isSupportedOn.GetProperty("InternalValue", BindingFlags.Static | BindingFlags.NonPublic).SetValue(null, true);
-        }
-
-        internal static bool StaticIsRelated<TSubject, TTarget>() => IsSupportedOn<TSubject, TTarget>.Value;
-
-        internal static bool StaticIsRelated(Type subject, Type target)
-        {
-            if (subject == null || target == null)
-                return false;
-
-            return (bool)typeof(IsSupportedOn<,>).MakeGenericType(subject,
-                    target)
-                .GetProperty("Value",
-                    BindingFlags.Static | BindingFlags.Public)
-                .GetValue(null);
-        }
-
-        internal static bool StaticHasRelations<TSubject>() => HasIsSupportedOn<TSubject>.Value;
-
-        internal static bool StaticHasRelations(Type subject)
-        {
-            if (subject == null)
-                return false;
-
-            return (bool)typeof(HasIsSupportedOn<>).MakeGenericType(subject)
-                .GetProperty("Value",
-                    BindingFlags.Static | BindingFlags.Public)
-                .GetValue(null);
-        }
-    }
-    #endregion
-
-    #region Dynamic Query Support
-    public static partial class IsSupportedOn
-    {
-        static DynamicTypeRelation s_Relations = new DynamicTypeRelation();
-
-        /// <summary>
-        /// Set the value in a dictionary.
-        ///
-        /// Note for AOT platforms: it may be required to use the dynamic backend because using generic structs
-        ///   require JIT.
-        /// </summary>
-        /// <param name="subject"></param>
-        /// <param name="target"></param>
-        internal static void RegisterDynamicRelation(Type subject, Type target)
-        {
-            s_Relations.RegisterRelation(subject, target);
-        }
-
-        internal static bool DynamicIsRelated<TSubject, TTarget>() => DynamicIsRelated(typeof(TSubject), typeof(TTarget));
-
-        internal static bool DynamicIsRelated(Type subject, Type target)
-        {
-            return s_Relations.IsRelated(subject, target);
-        }
-
-        internal static bool DynamicHasRelations<TSubject>() => DynamicHasRelations(typeof(TSubject));
-
-        internal static bool DynamicHasRelations(Type subject)
-        {
-            return s_Relations.HasRelations(subject);
-        }
-    }
-    #endregion
 
     #region Registration Executor
     /// <summary>
@@ -205,15 +166,15 @@ namespace UnityEngine.Rendering
             if (s_AssemblyConstructorRan)
                 return;
 
-            // Make sure to run the attribute constructor once
-            // The registration happens in the attribute constructor: you don't need to store anything in the attribute.
-            s_AssemblyConstructorRan = true;
-            s_AssembliesWithAttribute = AppDomain.CurrentDomain.GetAssemblies()
-                .Count(assembly => assembly.GetCustomAttributes(typeof(SupportedOnAttribute), true).Length > 0);
+            // Note: Querying type with attribute with TypeCache is 4x faster that querying for assembly attribute
+            foreach (var type in UnityEditor.TypeCache.GetTypesWithAttribute<SupportedOnAttribute>())
+            {
+                // Trigger attribute constructor here
+                var _ = type.GetCustomAttributes(typeof(SupportedOnAttribute)).FirstOrDefault() as SupportedOnAttribute;
+            }
         }
 
         static bool s_AssemblyConstructorRan = false;
-        static int s_AssembliesWithAttribute = 0;
     }
     #endregion
 }
