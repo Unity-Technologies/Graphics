96f850f82ebfb7e8d67759635d87ba2c44e0d691
Sandy Carter <sandy.carter@unity3d.com>
2021-10-12 10:33:12 -0400

    GBufferPass: Fix GBuffer2 warning

--- com.unity.render-pipelines.universal/Runtime/Passes/GBufferPass.cs
+++ com.unity.render-pipelines.universal/Runtime/Passes/GBufferPass.cs
@@ -136,7 +136,7 @@ namespace UnityEngine.Rendering.Universal.Internal
                 // If any sub-system needs camera normal texture, make it available.
                 // Input attachments will only be used when this is not needed so safe to skip in that case
                 if (!m_DeferredLights.UseRenderPass)
-                    gbufferCommands.SetGlobalTexture(s_CameraNormalsTextureID, m_DeferredLights.GbufferAttachmentIdentifiers[m_DeferredLights.GBufferNormalSmoothnessIndex]);
+                    gbufferCommands.SetGlobalTexture(s_CameraNormalsTextureID, m_DeferredLights.GbufferAttachments[m_DeferredLights.GBufferNormalSmoothnessIndex]);
             }
 
             context.ExecuteCommandBuffer(gbufferCommands);
