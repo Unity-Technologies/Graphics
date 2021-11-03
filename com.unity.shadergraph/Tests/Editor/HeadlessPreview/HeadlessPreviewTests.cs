75b05f60352452de7fb73edcc8ecf1aababbbd1f
Sai Narayan <nightmask3@gmail.com>
2021-11-03 15:43:36 -0700

    Added headless preview manager class, test fixture and test assembly

---
+++ com.unity.shadergraph/Tests/Editor/HeadlessPreview/HeadlessPreviewTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.ContextLayeredDataStorage;
+using UnityEditor.ShaderGraph.Registry;
+
+namespace UnityEditor.ShaderGraph.HeadlessPreview.UnitTests
+{
+    [TestFixture]
+    class HeadlessPreviewTestFixture
+    {
+        HeadlessPreviewManager m_PreviewManager = new HeadlessPreviewManager();
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void BasicPreviewOutputTest()
+        {
+            // Create
+        }
+    }
+}
