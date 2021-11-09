aed3ff672e9754cbdea58c554a08a3819ba7087a
Torbjorn Laedre <271210+tlaedre@users.noreply.github.com>
2021-11-09 13:59:59 +0100

    Project repo extraction

--- com.unity.render-pipelines.high-definition/Editor/Material/Eye/ShaderGraph/EyeSurfaceOptionPropertyBlock.cs
+++ com.unity.render-pipelines.high-definition/Editor/Material/Eye/ShaderGraph/EyeSurfaceOptionPropertyBlock.cs
@@ -20,6 +20,8 @@ namespace UnityEditor.Rendering.HighDefinition.ShaderGraph
         {
             public static GUIContent materialType = new GUIContent("Material Type", "Allow to select the type of lighting model used with this Eye Material.");
             public static GUIContent irisNormalType = new GUIContent("Iris Normal", "Override the iris normal");
+            public static GUIContent irisHeight = new GUIContent("Iris Height", "Height/distance of iris surface");
+            public static GUIContent irisRadius = new GUIContent("Iris Radius", "Iris Radius in object space");
         }
 
         EyeData eyeData;
@@ -36,6 +38,11 @@ namespace UnityEditor.Rendering.HighDefinition.ShaderGraph
             // Eye specific properties:
             AddProperty(subsurfaceEnableText, () => eyeData.subsurfaceScattering, (newValue) => eyeData.subsurfaceScattering = newValue);
             AddProperty(Styles.irisNormalType, () => eyeData.irisNormal, (newValue) => eyeData.irisNormal = newValue);
+            if (eyeData.materialType == EyeData.MaterialType.EyeCinematicWithCaustic)
+            {
+                AddProperty(Styles.irisHeight, () => eyeData.irisHeight, (newValue) => eyeData.irisHeight = newValue);
+                AddProperty(Styles.irisRadius, () => eyeData.irisRadius, (newValue) => eyeData.irisRadius = newValue);
+            }
         }
     }
 }
