41b6dfa9301ae438fcbc26e6c5e31203b681d3b6
Simon Brown <simon.brown@unity3d.com>
2021-11-24 15:15:38 +0000

    Add some edit modes for the capsule shape

--- com.unity.render-pipelines.high-definition/Editor/Lighting/CapsuleShadows/CapsuleOccluderEditor.cs
+++ com.unity.render-pipelines.high-definition/Editor/Lighting/CapsuleShadows/CapsuleOccluderEditor.cs
@@ -9,6 +9,54 @@ namespace UnityEngine.Rendering.HighDefinition
 {
     [CustomEditor(typeof(CapsuleOccluder))]
     public class CapsuleOccluderEditor : Editor {
+        static EditMode.SceneViewEditMode[] s_EditModes = new EditMode.SceneViewEditMode[]{
+            (EditMode.SceneViewEditMode)100,
+            (EditMode.SceneViewEditMode)101,
+        };
+        static GUIContent[] s_EditModesContent;
+
+        SerializedProperty m_Center, m_Radius, m_Height, m_Range;
+
+        private void OnEnable()
+        {
+            m_Center = serializedObject.FindProperty("center");
+            m_Radius = serializedObject.FindProperty("radius");
+            m_Height = serializedObject.FindProperty("height");
+            m_Range = serializedObject.FindProperty("range");
+
+            if (s_EditModesContent == null)
+            {
+                s_EditModesContent = new GUIContent[]
+                {
+                    EditorGUIUtility.TrIconContent("MoveTool", "Translate."),
+                    EditorGUIUtility.TrIconContent("ScaleTool", "Scale."),
+                };
+            }
+        }
+
+        internal static Func<Bounds> GetBoundsGetter(Editor o)
+        {
+            return () =>
+            {
+                var bounds = new Bounds();
+                var rp = ((Component)o.target).transform;
+                var b = rp.position;
+                bounds.Encapsulate(b);
+                return bounds;
+            };
+        }
+
+        public override void OnInspectorGUI()
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            EditMode.DoInspectorToolbar(s_EditModes, s_EditModesContent, GetBoundsGetter(this), this);
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+
+            DrawDefaultInspector();
+        }
+
         internal static void DrawWireCapsule(float radius, float height)
         {
             var offset = Mathf.Max(0.0f, 0.5f * height - radius);
@@ -31,7 +79,6 @@ namespace UnityEngine.Rendering.HighDefinition
             var t = target as CapsuleOccluder;
 
             Handles.matrix = t.capsuleToWorld;
-
             Handles.color = Color.white;
             Handles.zTest = CompareFunction.LessEqual;
             DrawWireCapsule(t.radius, t.height);
@@ -39,7 +86,59 @@ namespace UnityEngine.Rendering.HighDefinition
             Handles.zTest = CompareFunction.Greater;
             DrawWireCapsule(t.radius, t.height);
 
+            Handles.matrix = t.transform.localToWorldMatrix;
             Handles.zTest = CompareFunction.Always;
+            if (EditMode.editMode == s_EditModes[0])
+            {
+                EditorGUI.BeginChangeCheck();
+                Vector3 center = Handles.PositionHandle(t.center, Quaternion.identity);
+                if (EditorGUI.EndChangeCheck())
+                    m_Center.vector3Value = center;
+            }
+            if (EditMode.editMode == s_EditModes[1])
+            {
+                float handleScale = 0.025f;
+                Vector3 position = t.center;
+                Handles.color = Color.yellow;
+
+                EditorGUI.BeginChangeCheck();
+                position = t.center + new Vector3(t.radius, 0.0f, 0.0f);
+                position = Handles.Slider(position, Vector3.right, handleScale * HandleUtility.GetHandleSize(position), Handles.DotHandleCap, 0.5f);
+                if (EditorGUI.EndChangeCheck())
+                    m_Radius.floatValue = position.x - t.center.x;
+
+                EditorGUI.BeginChangeCheck();
+                position = t.center + new Vector3(-t.radius, 0.0f, 0.0f);
+                position = Handles.Slider(position, -Vector3.right, handleScale * HandleUtility.GetHandleSize(position), Handles.DotHandleCap, 0.5f);
+                if (EditorGUI.EndChangeCheck())
+                    m_Radius.floatValue = -(position.x - t.center.x);
+
+                EditorGUI.BeginChangeCheck();
+                position = t.center + new Vector3(0.0f, t.radius, 0.0f);
+                position = Handles.Slider(position, Vector3.up, handleScale * HandleUtility.GetHandleSize(position), Handles.DotHandleCap, 0.5f);
+                if (EditorGUI.EndChangeCheck())
+                    m_Radius.floatValue = position.y - t.center.y;
+
+                EditorGUI.BeginChangeCheck();
+                position = t.center + new Vector3(0.0f, -t.radius, 0.0f);
+                position = Handles.Slider(position, -Vector3.up, handleScale * HandleUtility.GetHandleSize(position), Handles.DotHandleCap, 0.5f);
+                if (EditorGUI.EndChangeCheck())
+                    m_Radius.floatValue = -(position.y - t.center.y);
+
+                EditorGUI.BeginChangeCheck();
+                position = t.center + new Vector3(0.0f, 0.0f, 0.5f * t.height);
+                position = Handles.Slider(position, Vector3.forward, handleScale * HandleUtility.GetHandleSize(position), Handles.DotHandleCap, 0.5f);
+                if (EditorGUI.EndChangeCheck())
+                    m_Height.floatValue = 2.0f * (position.z - t.center.z);
+
+                EditorGUI.BeginChangeCheck();
+                position = t.center + new Vector3(0.0f, 0.0f, -0.5f * t.height);
+                position = Handles.Slider(position, -Vector3.forward, handleScale * HandleUtility.GetHandleSize(position), Handles.DotHandleCap, 0.5f);
+                if (EditorGUI.EndChangeCheck())
+                    m_Height.floatValue = -2.0f * (position.z - t.center.z);
+            }
+
+            serializedObject.ApplyModifiedProperties();
         }
     }
 }
