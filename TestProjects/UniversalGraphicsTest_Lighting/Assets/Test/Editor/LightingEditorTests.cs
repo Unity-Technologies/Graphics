b9721f20be07d851b71dd2bf9165bbb2df7f2b3f
Erik Hakala <erik.hakala@unity3d.com>
2021-11-25 15:28:51 +0200

    Non-allocating string compare.

--- TestProjects/UniversalGraphicsTest_Lighting/Assets/Test/Editor/LightingEditorTests.cs
+++ TestProjects/UniversalGraphicsTest_Lighting/Assets/Test/Editor/LightingEditorTests.cs
@@ -23,6 +23,26 @@ class LightingEditorTests
     [Test]
     public void EnsureSingleQualityOption()
     {
-        Assert.IsTrue(QualitySettings.names?.Length == 1, $"{kProjectName} project MUST have ONLY single quality setting to ensure test consistency!!!");
+        // TODO: Ideally we only want 1 quality setting, but we need to have a second quality setting for Light Layer test.
+        Assert.IsTrue(QualitySettings.names?.Length == 2, $"{kProjectName} project MUST have ONLY single quality setting to ensure test consistency!!!");
+
+        //TODO: Test that the "Ultra" quality setting is the first == default for consistent results
+        {
+            // A non-allocating string compare
+            bool isSame = true;
+            {
+                var r = "Ultra";
+                var s = QualitySettings.names[0];
+                var len = s.Length < r.Length ? s.Length : r.Length;
+                for (int i = 0; i < len; i++)
+                    if (s[i] != r[i])
+                    {
+                        isSame = false;
+                        break;
+                    }
+            }
+            Assert.IsTrue(isSame, $"{kProjectName} project MUST have 'Ultra' quality setting as first (default) to ensure test consistency!!!");
+        }
+
     }
 }
