3987746f777a04a613fa1486161189afc6404270
tim <tim@unity3d.com>
2021-11-30 11:24:41 +0100

    Port Lens Distortion

---
+++ com.unity.render-pipelines.core/Runtime/PostProcessing/Components/URPUpgradable/LensDistortion.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace UnityEngine.Rendering.Universal.Obsolete
+{
+    [Serializable]
+    [Obsolete]
+    public sealed class LensDistortion : Rendering.LensDistortion, IDeprecatedVolumeComponent
+    { }
+}
