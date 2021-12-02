1d1da21b03a3b0910aa4e6558959747773b1468f
Sandy Carter <sandy.carter@unity3d.com>
2021-12-02 10:13:07 -0500

    URP: Rename rendering mode to avoid confusion between requested and actual

--- com.unity.render-pipelines.universal/Runtime/RendererFeatures/DecalRendererFeature.cs
+++ com.unity.render-pipelines.universal/Runtime/RendererFeatures/DecalRendererFeature.cs
@@ -262,7 +262,7 @@ namespace UnityEngine.Rendering.Universal
                 return DecalTechnique.Invalid;
             }
 
-            bool isDeferred = universalRenderer.renderingMode == RenderingMode.Deferred;
+            bool isDeferred = universalRenderer.renderingModeRequested == RenderingMode.Deferred;
             return GetTechnique(isDeferred);
         }
 
@@ -393,7 +393,7 @@ namespace UnityEngine.Rendering.Universal
                     m_DecalDrawForwardEmissiveSystem = new DecalDrawFowardEmissiveSystem(m_DecalEntityManager);
                     m_ForwardEmissivePass = new DecalForwardEmissivePass(m_DecalDrawForwardEmissiveSystem);
 
-                    if (universalRenderer.actualRenderingMode == RenderingMode.Deferred)
+                    if (universalRenderer.renderingModeActual == RenderingMode.Deferred)
                     {
                         m_DBufferRenderPass.deferredLights = universalRenderer.deferredLights;
                         m_DBufferRenderPass.deferredLights.DisableFramebufferFetchInput();
@@ -487,7 +487,7 @@ namespace UnityEngine.Rendering.Universal
                 m_DBufferRenderPass.Setup(renderingData.cameraData);
 
                 var universalRenderer = renderer as UniversalRenderer;
-                if (universalRenderer.actualRenderingMode == RenderingMode.Deferred)
+                if (universalRenderer.renderingModeActual == RenderingMode.Deferred)
                     m_CopyDepthPass.Setup(
                         renderer.cameraDepthTargetHandle,
                         universalRenderer.m_DepthTexture
