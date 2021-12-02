c35a0dcea156893ee5d6a83d531ff170c6e939a9
Jussi Knuuttila <jussi.knuuttila@unity3d.com>
2021-12-02 16:26:15 +0200

    First WIP version of SubmitRenderRequests test for HDRP

---
+++ TestProjects/HDRP_Tests/Assets/GraphicTests/Scenes/9x_Other/9905_SubmitRenderRequests/RenderRequestTestCamera.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+
+public class RenderRequestTestCamera : MonoBehaviour
+{
+    public Camera.RenderRequestMode debugMode = Camera.RenderRequestMode.None;
+
+    private Camera m_Camera;
+    private RenderRequestTestTarget[] m_Targets;
+    private Dictionary<RenderRequestTestTarget, bool> m_Failed;
+
+    private List<Camera.RenderRequest> m_RenderRequests;
+    private const int kRenderTargetDimension = 1024;
+    private const GraphicsFormat kVectorFormat = GraphicsFormat.R32G32B32A32_SFloat;
+    private const GraphicsFormat kIdFormat = GraphicsFormat.R8G8B8A8_UNorm;
+    private const float kVectorTolerance = 0.1f;
+
+    private int2 RenderTargetDimension => new int2(m_Camera.pixelWidth, m_Camera.pixelHeight);
+
+    private RenderTexture MakeRenderTarget(GraphicsFormat fmt) =>
+        new RenderTexture(RenderTargetDimension.x, RenderTargetDimension.y,
+            fmt,
+            GraphicsFormat.D32_SFloat);
+
+    private int ReadbackDataIndex(float3 viewportSpace)
+    {
+        float2 uv = viewportSpace.xy;
+        uv.y = 1 - uv.y;
+        float2 px = math.saturate(uv) * RenderTargetDimension;
+        int2 xy = (int2)px;
+        //Debug.Log($"{viewportSpace} {uv} {px} {xy}");
+        return xy.y * RenderTargetDimension.x + xy.x;
+    }
+
+    private static float3 UnpackNormal(float3 packedNormal)
+    {
+        return packedNormal.xyz * 2.0f - 1.0f;
+    }
+
+    private void Start()
+    {
+        m_Camera = GetComponent<Camera>();
+        m_Targets = FindObjectsOfType<RenderRequestTestTarget>();
+        m_RenderRequests = new List<Camera.RenderRequest>();
+        m_Failed = new Dictionary<RenderRequestTestTarget, bool>();
+
+        Camera.RenderRequest requestPosition = new Camera.RenderRequest(
+            Camera.RenderRequestMode.WorldPosition,
+            MakeRenderTarget(kVectorFormat));
+        m_RenderRequests.Add(requestPosition);
+
+        Camera.RenderRequest requestNormal = new Camera.RenderRequest(
+            Camera.RenderRequestMode.Normal,
+            MakeRenderTarget(kVectorFormat));
+        m_RenderRequests.Add(requestNormal);
+
+        Camera.RenderRequest requestObjectId = new Camera.RenderRequest(
+            Camera.RenderRequestMode.ObjectId,
+            MakeRenderTarget(kIdFormat));
+        m_RenderRequests.Add(requestObjectId);
+
+        m_Camera.SubmitRenderRequests(m_RenderRequests);
+
+        var readbackPosition = AsyncGPUReadback.Request(requestPosition.result);
+        var readbackNormal = AsyncGPUReadback.Request(requestNormal.result);
+        var readbackObjectId = AsyncGPUReadback.Request(requestObjectId.result);
+
+        AsyncGPUReadback.WaitAllRequests();
+
+        ValidatePosition(readbackPosition);
+        ValidateNormal(readbackNormal);
+        ValidateObjectId(readbackObjectId);
+
+        UpdateTestText();
+    }
+
+    private void UpdateTestText()
+    {
+        foreach (var t in m_Targets)
+        {
+            t.SetTestText(m_Camera, m_Failed.ContainsKey(t) ? "FAILED" : "OK");
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (debugMode == Camera.RenderRequestMode.None)
+            return;
+
+        foreach (var r in m_RenderRequests)
+        {
+            if (r.mode == debugMode)
+            {
+                GUI.DrawTexture(m_Camera.pixelRect, r.result, ScaleMode.ScaleToFit, false);
+                return;
+            }
+        }
+    }
+
+    private void Validate(RenderRequestTestTarget t, float3 actual, float3 expected, Camera.RenderRequestMode mode)
+    {
+        float diff = math.length(actual - expected);
+        if (diff > kVectorTolerance)
+        {
+            m_Failed[t] = true;
+            Debug.LogError($"Mode {mode} failed for {t}: {actual} vs {expected}, diff {diff}");
+        }
+    }
+
+    private void Validate(RenderRequestTestTarget t, int actual, int expected, Camera.RenderRequestMode mode)
+    {
+        if (actual != expected)
+        {
+            m_Failed[t] = true;
+            Debug.LogError($"Mode {mode} failed for {t}: {actual} vs {expected}");
+        }
+    }
+
+    private void ValidatePosition(AsyncGPUReadbackRequest readbackPosition)
+    {
+        var positions = readbackPosition.GetData<float4>();
+
+        foreach (var t in m_Targets)
+        {
+            float3 viewportSpacePos = t.ViewportSpacePosition(m_Camera);
+            var actualPosition = positions[ReadbackDataIndex(viewportSpacePos)].xyz;
+            var expectedPosition = t.ExpectedWorldPosition(m_Camera);
+            Validate(t, actualPosition, expectedPosition, Camera.RenderRequestMode.WorldPosition);
+        }
+    }
+
+    private void ValidateNormal(AsyncGPUReadbackRequest readbackNormal)
+    {
+        var normals = readbackNormal.GetData<float4>();
+
+        foreach (var t in m_Targets)
+        {
+            float3 viewportSpacePos = t.ViewportSpacePosition(m_Camera);
+            var actualNormal = UnpackNormal(normals[ReadbackDataIndex(viewportSpacePos)].xyz);
+            var expectedNormal = t.ExpectedWorldNormal(m_Camera);
+            Validate(t, actualNormal, expectedNormal, Camera.RenderRequestMode.Normal);
+        }
+    }
+
+    private void ValidateObjectId(AsyncGPUReadbackRequest readbackObjectId)
+    {
+        var objectIds = readbackObjectId.GetData<int>();
+
+        foreach (var t in m_Targets)
+        {
+            float3 viewportSpacePos = t.ViewportSpacePosition(m_Camera);
+            var actualId = objectIds[ReadbackDataIndex(viewportSpacePos)];
+            var expectedId = t.ExpectedObjectId();
+            Validate(t, actualId, expectedId, Camera.RenderRequestMode.ObjectId);
+        }
+    }
+
+    private void OnDisable()
+    {
+        Dispose();
+    }
+
+    private void Dispose()
+    {
+        foreach (var rr in m_RenderRequests)
+            rr.result.Release();
+    }
+}
---
+++ TestProjects/HDRP_Tests/Assets/GraphicTests/Scenes/9x_Other/9905_SubmitRenderRequests/RenderRequestTestTarget.cs
@@ -0,0 +1,48 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class RenderRequestTestTarget : MonoBehaviour
+{
+    private string m_Text = null;
+    private Rect m_TextRect;
+
+    private float Radius => transform.localScale.x / 2.0f;
+    private const int kTextOffsetX = -20;
+    private const int kTextOffsetY = -10;
+    private const int kTextDim = 100;
+
+    public float3 ExpectedWorldPosition(Camera viewedFrom)
+    {
+        return (float3)transform.position + ExpectedWorldNormal(viewedFrom) * Radius;
+    }
+
+    public float3 ExpectedWorldNormal(Camera viewedFrom)
+    {
+        return (viewedFrom.transform.position - transform.position).normalized;
+    }
+
+    public int ExpectedObjectId()
+    {
+        return GetComponent<MeshRenderer>().GetInstanceID();
+    }
+
+    public float3 ViewportSpacePosition(Camera camera)
+    {
+        return camera.WorldToViewportPoint(transform.position);
+    }
+
+    public void SetTestText(Camera camera, string text)
+    {
+        float2 xy = ((float3)camera.WorldToScreenPoint(transform.position)).xy;
+
+        m_Text = text;
+        m_TextRect = new Rect(xy.x + kTextOffsetX, camera.pixelHeight - xy.y + kTextOffsetY, kTextDim, kTextDim);
+    }
+
+    void OnGUI()
+    {
+        if (m_Text != null)
+            GUI.Label(m_TextRect, m_Text);
+    }
+}
