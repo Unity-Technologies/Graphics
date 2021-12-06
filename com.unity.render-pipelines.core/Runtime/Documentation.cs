06e9cb9af764b77544329d7e6f84a4ba5675f378
sebastienlagarde <sebastien@unity3d.com>
2021-12-06 09:13:22 +0100

    bump version to 10.9.0

--- com.unity.render-pipelines.core/Runtime/Documentation.cs
+++ com.unity.render-pipelines.core/Runtime/Documentation.cs
@@ -15,7 +15,7 @@ namespace UnityEngine.Rendering
         /// <summary>
         /// Current version of the documentation.
         /// </summary>
-        public const string version = "10.8";
+        public const string version = "10.9";
     }
 
     //Need to live in Runtime as Attribute of documentation is on Runtime classes \o/
