d27cf10d6ff4e4d4b8c95ba3b63ec4d30331d0d4
Erik Hakala <erik.hakala@unity3d.com>
2021-12-07 15:23:43 +0200

    Fix overlay depth.

--- com.unity.render-pipelines.universal/Runtime/UniversalRenderPipeline.cs
+++ com.unity.render-pipelines.universal/Runtime/UniversalRenderPipeline.cs
@@ -919,7 +919,7 @@ namespace UnityEngine.Rendering.Universal
             if (isOverlayCamera)
             {
                 cameraData.requiresOpaqueTexture = false;
-                cameraData.postProcessingRequiresDepthTexture = false;
+                //cameraData.postProcessingRequiresDepthTexture = false;
             }
 
             Matrix4x4 projectionMatrix = camera.projectionMatrix;
