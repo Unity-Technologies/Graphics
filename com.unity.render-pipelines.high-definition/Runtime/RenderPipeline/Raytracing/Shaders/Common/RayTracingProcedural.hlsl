63999d790c6ca9c7a76f59144db2b2973b7ae8a6
Anis Benyoub <anis@unity3d.com>
2021-12-08 19:50:28 +0100

    Adding support for the quad and a bit of hackyness here and there

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/Raytracing/Shaders/Common/RayTracingProcedural.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/Raytracing/Shaders/Common/RayTracingProcedural.hlsl
@@ -18,17 +18,44 @@ AABB FetchPrimitiveAABB(uint primitiveIndex)
 	return aabbBuffer[primitiveIndex];
 }
 
-[shader("intersection")]
-void IntersectionShader()
+bool RayPlaneIntersection(float3 rayOrigin, float3 rayDir, float3 planeOrigin, float3 planeNormal, out float t)
 {
-    UNITY_XR_ASSIGN_VIEW_INDEX(DispatchRaysIndex().z);
-    
+    t = dot((planeOrigin - rayOrigin), planeNormal) / dot(rayDir, planeNormal);
+    return t > 0.0;
+}
+
+bool RayQuadIntersection(float3 rayOrigin, float3 rayDir, float3 planeOrigin, float3 planeNormal, float4x4 localToPrimitive, out float t, out float2 uv)
+{
+    if(RayPlaneIntersection(rayOrigin, rayDir, planeOrigin, planeNormal, t))
+    {
+        // Compute the intersection point
+        float3 hitPoint = rayOrigin + rayDir * t;
+
+        // Evaluate the projected UVs
+        uv = mul(localToPrimitive, float4(hitPoint, 1.0f)).xy + 0.5f;
+        return !(any(uv < 0.0f) || any(uv > 1.0f));
+    }
+    return false;
+}
+
+void IntersectPrimitive(VFXAttributes attributes, float3 size3)
+{
     // Grab the current particle's AABB
     AABB particleAABB = FetchPrimitiveAABB(PrimitiveIndex());
 
-    // Compute the center and extent
-    float3 centerOS = (particleAABB.minPosOS + particleAABB.maxPosOS) * 0.5f;
-    float3 extents = particleAABB.maxPosOS - particleAABB.minPosOS;
+    // These parameters are only required by the quad primitive for now
+#if defined(RAY_TRACING_QUAD_PRIMTIIVE)
+    // Object <-> primtive matrices
+    const float4x4 primtiveToObject = GetElementToVFXMatrix(attributes.axisX, attributes.axisY, attributes.axisZ,
+        float3(attributes.angleX,attributes.angleY,attributes.angleZ),
+        float3(attributes.pivotX,attributes.pivotY,attributes.pivotZ),
+        size3, attributes.position);
+
+    const float4x4 objectToPrimitive = GetVFXToElementMatrix(attributes.axisX, attributes.axisY, attributes.axisZ,
+        float3(attributes.angleX, attributes.angleY, attributes.angleZ),
+        float3(attributes.pivotX, attributes.pivotY, attributes.pivotZ),
+        size3, attributes.position);
+#endif
 
 #if defined(RAY_TRACING_BOX_PRIMITIVE)
     // Intersect the box and fill the intersection data
@@ -42,16 +69,35 @@ void IntersectionShader()
     }
 #endif
 
+#if defined(RAY_TRACING_QUAD_PRIMTIIVE)
+    float t;
+    float2 uv;
+    if (RayQuadIntersection(ObjectRayOrigin(), ObjectRayDirection(), primtiveToObject._m03_m13_m23, -primtiveToObject._m02_m12_m22, objectToPrimitive, t, uv))
+    {
+        AttributeData attributeData;
+        attributeData.barycentrics = uv;
+
+        float alpha = SampleTexture(VFX_SAMPLER(mainTexture), uv).a;
+        if(alpha > 0.5f)
+            ReportHit(t, 0, attributeData);
+    }
+#endif
+
 #if defined(RAY_TRACING_SPHERE_PRIMITIVE)
-    float3 sphereCenter = (particleAABB.maxPosOS + particleAABB.minPosOS) * 0.5f;
+    float3 sphereCenter = (particleAABB.maxPosOS + particleAABB.minPosOS) * 0.5;
     float sphereRadius = (particleAABB.maxPosOS.x - particleAABB.minPosOS.x) * 0.5;
 
     // Intersect the box and fill the intersection data
     float2 intersection = 0;
-    if (IntersectRaySphere(ObjectRayOrigin() - sphereCenter, ObjectRayDirection(), sphereRadius, intersection))
+    float3 rayStart = ObjectRayOrigin() - sphereCenter;
+    float3 rayDir = ObjectRayDirection();
+    if (IntersectRaySphere(rayStart, rayDir, sphereRadius, intersection))
     {
+        // Pack the normal into the attribute data
         AttributeData attributeData;
-        attributeData.barycentrics = float2(0.5, 0.5);
+        attributeData.barycentrics = PackNormalOctQuadEncode(normalize(rayStart + intersection.x * rayDir));
+
+        // Report the hit
         ReportHit(intersection.x, 0, attributeData);
     }
 #endif
