54f2a904f188dffc6e90e965ba8078a4d64ce2f2
Ludovic Theobald <ludovic.theobald@unity3d.com>
2021-12-09 14:43:11 +0100

    Updated working tests

--- TestProjects/VisualEffectGraph_HDRP/Assets/AllTests/Editor/Tests/VFXBoundsHelperTest.cs
+++ TestProjects/VisualEffectGraph_HDRP/Assets/AllTests/Editor/Tests/VFXBoundsHelperTest.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.VFX;
 using UnityEngine.TestTools;
 using System.Linq;
 using System.Collections;
-using UnityEditor.VFX.Block;
 using UnityEditor.VFX.UI;
 
 namespace UnityEditor.VFX.Test
@@ -14,21 +12,27 @@ namespace UnityEditor.VFX.Test
     {
 
 #pragma warning disable 0414
-        private static VFXCoordinateSpace[] available_Space = { VFXCoordinateSpace.Local, VFXCoordinateSpace.World };
+        private static VFXCoordinateSpace[] availableSpaces = { VFXCoordinateSpace.Local, VFXCoordinateSpace.World };
 #pragma warning restore 0414
 
-        Vector3 expectedCenter =  new Vector3(0,1,0);
-        Vector3 expectedExtent =  new Vector3(4.14f,4.14f,4.14f);
+        private readonly Vector3 m_Translation = new Vector3(1.0f, 2.0f, 3.0f);
+        private readonly Quaternion m_Rotation = Quaternion.Euler(20.0f, 30.0f, 40.0f);
+        private readonly Vector3 m_Scale = new Vector3(3.0f, 2.0f, 1.0f);
+        private readonly Vector3 m_ParticleSize = 0.1f * Vector3.one;
+
         [UnityTest]
-        public IEnumerator TestBoundsHelperResults([ValueSource(nameof(available_Space))] object systemSpace, [ValueSource(nameof(available_Space))] object boundSpace)
+        public IEnumerator TestBoundsHelperResults([ValueSource(nameof(availableSpaces))] object systemSpace)
         {
             string kSourceAsset = "Assets/AllTests/Editor/Tests/VFXBoundsHelperTest.vfx";
             var graph = VFXTestCommon.CopyTemporaryGraph(kSourceAsset);
-
-            graph.children.OfType<VFXBasicInitialize>().First().space = (VFXCoordinateSpace)systemSpace;
+            VFXCoordinateSpace space = (VFXCoordinateSpace)systemSpace;
+            graph.children.OfType<VFXBasicInitialize>().First().space = space;
 
             var gameObj = new GameObject("GameObjectToCheck");
-            gameObj.transform.position = new Vector3(0,1,0);
+            gameObj.transform.position = m_Translation;
+            gameObj.transform.rotation = m_Rotation;
+            gameObj.transform.localScale = m_Scale;
+
             var vfxComponent = gameObj.AddComponent<VisualEffect>();
             vfxComponent.visualEffectAsset = graph.visualEffectResource.asset;
 
@@ -41,14 +45,32 @@ namespace UnityEditor.VFX.Test
             VFXBoundsRecorder boundsRecorder = new VFXBoundsRecorder(vfxComponent, view);
 
             boundsRecorder.ToggleRecording();
-            vfxComponent.Simulate(1.0f/60.0f, 10u);
-            boundsRecorder.UpdateBounds();
+            vfxComponent.Simulate(1.0f/60.0f);
+            const int maxFrameTimeout = 100;
+            for (int i = 0; i < maxFrameTimeout; i++)
+            {
+                boundsRecorder.UpdateBounds();
+                if (boundsRecorder.bounds.Count > 0)
+                    break;
+                yield return null; //skip a frame.
+            }
             boundsRecorder.ToggleRecording();
             Bounds bounds = boundsRecorder.bounds.FirstOrDefault().Value;
             boundsRecorder.CleanUp();
 
-            Assert.AreEqual(expectedCenter.y, bounds.center.y, .01);
-            Debug.Log("Bounds : " + bounds  );
+            Vector3 expectedCenter = Vector3.zero;
+            Vector3 expectedExtent = new Vector3(2.0f,2.0f,2.0f);
+            expectedExtent += 0.5f * m_ParticleSize * (space == VFXCoordinateSpace.Local
+                ? Mathf.Sqrt(3.0f)
+                : Mathf.Sqrt(1.0f / Mathf.Pow(m_Scale.x,2) + 1.0f / Mathf.Pow(m_Scale.y,2) + 1.0f /  Mathf.Pow(m_Scale.z,2)));
+
+            Assert.AreEqual(expectedCenter.x, bounds.center.x, .001);
+            Assert.AreEqual(expectedCenter.y, bounds.center.y, .001);
+            Assert.AreEqual(expectedCenter.z, bounds.center.z, .001);
+            Assert.AreEqual(expectedExtent.x, bounds.extents.x, .005);
+            Assert.AreEqual(expectedExtent.y, bounds.extents.y, .005);
+            Assert.AreEqual(expectedExtent.z, bounds.extents.z, .005);
+
             view.attachedComponent = null;
             window.Close();
 
