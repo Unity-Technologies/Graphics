99fc51fc28aab4351b1fe207fd0dc67b78943c32
etienne cella <etienne.p.cella@gmail.com>
2021-12-13 11:48:55 -0500

    update srp-renderer fix, reset urp foundation gfx tests
    # Conflicts:
    #	com.unity.render-pipelines.universal/Runtime/UniversalRenderer.cs

--- com.unity.render-pipelines.universal/Runtime/UniversalRenderer.cs
+++ com.unity.render-pipelines.universal/Runtime/UniversalRenderer.cs
@@ -1191,13 +1191,7 @@ namespace UnityEngine.Rendering.Universal
         {
             m_ColorBufferSystem.Swap();
 
-            // TODO So here we have 2 calls to the render-target-system.
-            // One reallocates, the other does not.
-            //Check if we are using the depth that is attached to color buffer
-            if (m_ActiveCameraDepthAttachment.nameID != BuiltinRenderTextureType.CameraTarget)
-                ConfigureCameraTarget(m_ColorBufferSystem.GetBackBuffer(cmd), m_ColorBufferSystem.GetBufferA());
-            else
-                ConfigureCameraColorTarget(m_ColorBufferSystem.GetBackBuffer(cmd));
+            ConfigureCameraColorTarget(m_ColorBufferSystem.GetBackBuffer(cmd));
 
             m_ActiveCameraColorAttachment = m_ColorBufferSystem.GetBackBuffer(cmd);
             cmd.SetGlobalTexture("_CameraColorTexture", m_ActiveCameraColorAttachment.nameID);
