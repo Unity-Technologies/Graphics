41b9815f5d9313ef2694c0511ca7c2ad55b44afc
alex-vazquez <76204843+alex-vazquez@users.noreply.github.com>
2021-12-13 11:38:41 +0100

    [RPW] Fix crash when upgrading URP Global Settings when no render has been done (#6582)

--- com.unity.render-pipelines.universal/Runtime/Data/UniversalRenderPipelineAsset.cs
+++ com.unity.render-pipelines.universal/Runtime/Data/UniversalRenderPipelineAsset.cs
@@ -1205,7 +1205,7 @@ namespace UnityEngine.Rendering.Universal
 
             if (asset.k_AssetPreviousVersion < 10)
             {
-                UniversalRenderPipelineGlobalSettings.instance.shaderVariantLogLevel = (Rendering.ShaderVariantLogLevel) asset.m_ShaderVariantLogLevel;
+                UniversalRenderPipelineGlobalSettings.Ensure().shaderVariantLogLevel = (Rendering.ShaderVariantLogLevel) asset.m_ShaderVariantLogLevel;
                 asset.k_AssetPreviousVersion = 10;
             }
 
