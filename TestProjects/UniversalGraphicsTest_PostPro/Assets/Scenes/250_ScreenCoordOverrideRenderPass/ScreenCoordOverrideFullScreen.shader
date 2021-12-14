3ac92ae940a1fa49d6f0619856597c7577e23eff
etienne cella <etienne.p.cella@gmail.com>
2021-12-14 15:02:40 -0500

    refactor test render feature

--- TestProjects/UniversalGraphicsTest_PostPro/Assets/Scenes/250_ScreenCoordOverrideRenderPass/ScreenCoordOverrideFullScreen.shader
+++ TestProjects/UniversalGraphicsTest_PostPro/Assets/Scenes/250_ScreenCoordOverrideRenderPass/ScreenCoordOverrideFullScreen.shader
@@ -1,35 +1,34 @@
 Shader "Hidden/Test/ScreenCoordOverrideFullScreen"
 {
-    Properties
-    {
-        _MainTex ("Texture", 2D) = "white" {}
-    }
     SubShader
     {
-        ZTest Always
-        ZWrite Off
-        Cull Off
+        Tags { "RenderType" = "Opaque" "RenderPipeline" = "UniversalPipeline"}
+        LOD 100
 
         Pass
         {
+            Name "ScreenCoordOverrideFullScreen"
+            ZTest Always
+            ZWrite Off
+            Cull Off
+
             HLSLPROGRAM
             #pragma vertex FullscreenVert
             #pragma fragment Fragment
+            #pragma multi_compile _ SCREEN_COORD_OVERRIDE
 
             #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/ScreenCoordOverride.hlsl"
             #include "Packages/com.unity.render-pipelines.universal/Shaders/Utils/Fullscreen.hlsl"
 
-            #pragma multi_compile _ SCREEN_COORD_OVERRIDE
+            TEXTURE2D_X(_SourceTex);
+            SAMPLER(sampler_SourceTex);
 
-            TEXTURE2D(_MainTex);
-            SAMPLER(sampler_MainTex);
-
-            float4 Fragment (Varyings input) : SV_Target
+            half4 Fragment (Varyings input) : SV_Target
             {
                 UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
-                float4 baseColor = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, input.uv);
+                half4 baseColor = SAMPLE_TEXTURE2D_X(_SourceTex, sampler_SourceTex, input.uv);
                 float2 transformedUV = SCREEN_COORD_APPLY_SCALEBIAS(input.uv);
-                return float4(lerp(baseColor.rgb, float3(transformedUV, 0), 0.5), 1);
+                return half4(lerp(baseColor.rgb, float3(transformedUV, 0), 0.5), 1);
             }
 
             ENDHLSL
--- TestProjects/UniversalGraphicsTest_PostPro/Assets/Scenes/250_ScreenCoordOverrideRenderPass/ScreenCoordOverrideScriptableRenderFeature.cs
+++ TestProjects/UniversalGraphicsTest_PostPro/Assets/Scenes/250_ScreenCoordOverrideRenderPass/ScreenCoordOverrideScriptableRenderFeature.cs
@@ -1,53 +1,28 @@
+using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
 public class ScreenCoordOverrideScriptableRenderFeature : ScriptableRendererFeature
 {
-    class ScreenCoordOverrideFullScreenPass : ScriptableRenderPass
-    {
-        Material m_Material;
-
-        public ScreenCoordOverrideFullScreenPass(Material material)
-        {
-            m_Material = material;
-        }
-
-        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
-        {
-            var cmd = CommandBufferPool.Get("Test Screen Coord Override");
-            Blit(cmd, ref renderingData, m_Material);
-            context.ExecuteCommandBuffer(cmd);
-            CommandBufferPool.Release(cmd);
-        }
-    }
-
-    const string k_ShaderPath = "Hidden/Test/ScreenCoordOverrideFullScreen";
-
-    ScreenCoordOverrideFullScreenPass m_ScriptablePass;
-    Material m_Material;
+    ScreenCoordOverrideRenderPass m_ScriptablePass;
 
     public override void Create()
     {
-        if (m_Material == null)
-        {
-            m_Material = CoreUtils.CreateEngineMaterial(k_ShaderPath);
-        }
-
-        m_ScriptablePass = new ScreenCoordOverrideFullScreenPass(m_Material);
-        m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRenderingPostProcessing;
+        m_ScriptablePass = new ScreenCoordOverrideRenderPass(RenderPassEvent.AfterRenderingPostProcessing);
     }
 
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
         if (renderingData.cameraData.cameraType == CameraType.Game)
         {
+            Assert.IsNotNull(m_ScriptablePass);
             renderer.EnqueuePass(m_ScriptablePass);
         }
     }
 
     protected override void Dispose(bool disposing)
     {
-        CoreUtils.Destroy(m_Material);
+        Assert.IsNotNull(m_ScriptablePass);
+        m_ScriptablePass.Cleanup();
     }
 }
