71c1a06260d7556e9e004d64c8ba6c3eb6e81218
Peter Bay Bastian <peter.bastian@gmail.com>
2021-12-20 10:58:08 +0100

    Fix loop unroll bug

--- com.unity.render-pipelines.universal/ShaderLibrary/RealtimeLights.hlsl
+++ com.unity.render-pipelines.universal/ShaderLibrary/RealtimeLights.hlsl
@@ -43,7 +43,7 @@ struct Light
 #if USE_CLUSTERED_LIGHTING
     #define LIGHT_LOOP_BEGIN(lightCount) \
     ClusteredLightLoop cll = ClusteredLightLoopInit(inputData.normalizedScreenSpaceUV, inputData.positionWS); \
-    while (ClusteredLightLoopNextWord(cll)) { while (ClusteredLightLoopNextLight(cll)) { \
+    [loop] while (ClusteredLightLoopNextWord(cll)) { [loop] while (ClusteredLightLoopNextLight(cll)) { \
         uint lightIndex = ClusteredLightLoopGetLightIndex(cll);
     #define LIGHT_LOOP_END } }
 #elif !_USE_WEBGL1_LIGHTS
