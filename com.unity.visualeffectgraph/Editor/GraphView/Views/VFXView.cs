5f50167b5437d8110ad079275d4d0ebb4fdff216
Julien Amsellem <julien.amsellem@unity3d.com>
2022-01-03 17:49:48 +0100

    Avoid error when using select-all shortcut to delete vfx content (#305)
    * Avoid error when using select-all shortcut to delete vfx content
    
    This action was including the VFXSystemBorder in the list of elements to delete whereas it seems not expected because it does not happen when doing a rectangle selection with the mouse.
    
    * Updated changelog

--- com.unity.visualeffectgraph/Editor/GraphView/Views/VFXView.cs
+++ com.unity.visualeffectgraph/Editor/GraphView/Views/VFXView.cs
@@ -2104,6 +2104,12 @@ namespace UnityEditor.VFX.UI
             (groupNode as VFXGroupNode).GroupNodeTitleChanged(title);
         }
 
+        private void AddRangeToSelection(List<ISelectable> selectables)
+        {
+            selectables.ForEach(base.AddToSelection);
+            UpdateGlobalSelection();
+        }
+
         public override void AddToSelection(ISelectable selectable)
         {
             base.AddToSelection(selectable);
@@ -2435,10 +2441,7 @@ namespace UnityEditor.VFX.UI
             {
                 ClearSelection();
 
-                foreach (var element in graphElements.ToList())
-                {
-                    AddToSelection(element);
-                }
+                AddRangeToSelection(graphElements.Where(x => x is not VFXSystemBorder).OfType<ISelectable>().ToList());
                 e.StopPropagation();
             }
         }
