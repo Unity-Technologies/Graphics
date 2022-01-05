6eaf019457634c077ffc83651147783cf22d8d25
Kleber Garcia <kleber.garcia@unity3d.com>
2022-01-05 13:35:00 -0500

    Removing usage of GUID in function lookup for pipeline material info

--- com.unity.render-pipelines.core/Runtime/BatchRenderer/RenderBRG.cs
+++ com.unity.render-pipelines.core/Runtime/BatchRenderer/RenderBRG.cs
@@ -1266,11 +1266,11 @@ namespace UnityEngine.Rendering
 
     public class RenderBRG : MonoBehaviour
     {
-        private static Dictionary<Guid, RenderBRGGetMaterialRenderInfoCallback> s_SrpMatInfoCallbacks = new();
+        private static Dictionary<Type, RenderBRGGetMaterialRenderInfoCallback> s_SrpMatInfoCallbacks = new();
 
         public static void RegisterSRPRenderInfoCallback(RenderPipelineAsset pipelineAsset, RenderBRGGetMaterialRenderInfoCallback callbackValue)
         {
-            s_SrpMatInfoCallbacks[pipelineAsset.GetType().GUID] = callbackValue;
+            s_SrpMatInfoCallbacks[pipelineAsset.GetType()] = callbackValue;
         }
 
         internal static RenderBRGGetMaterialRenderInfoCallback GetActiveMaterialRenderInfoCallback(out RenderPipelineAsset activePipeline)
@@ -1279,7 +1279,7 @@ namespace UnityEngine.Rendering
             if (activePipeline == null)
                 return null;
 
-            if (s_SrpMatInfoCallbacks.TryGetValue(activePipeline.GetType().GUID, out var outCallback))
+            if (s_SrpMatInfoCallbacks.TryGetValue(activePipeline.GetType(), out var outCallback))
                 return outCallback;
 
             return null;
