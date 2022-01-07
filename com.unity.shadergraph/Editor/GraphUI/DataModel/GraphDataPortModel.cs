60297b8d4ee0ec0c9520c9a1f8ca4fbc21383c33
Esmeralda Salamone <esmeralda.salamone@unity3d.com>
2022-01-07 13:49:35 -0800

    merging in some 'headless' work to resolve shaderFoundry changes, GTF changes since then still need resolution.

---
+++ com.unity.shadergraph/Editor/GraphUI/DataModel/GraphDataPortModel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor.GraphToolsFoundation.Overdrive.BasicModel;
+
+namespace UnityEditor.ShaderGraph.GraphUI.DataModel
+{
+    public class GraphDataPortModel : PortModel
+    {
+        public string graphDataName => UniqueName;
+        public GraphDataNodeModel graphDataNodeModel => (GraphDataNodeModel) NodeModel;
+    }
+}
