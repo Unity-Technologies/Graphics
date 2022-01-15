dbdd4035f4da220781d047eb9c52f6b527d57f01
Remi Chapelain <remi.chapelain@unity3d.com>
2022-01-15 18:36:45 +0100

    default vignette param

--- com.unity.render-pipelines.high-definition/Runtime/PostProcessing/Components/AutoLensFlare.cs
+++ com.unity.render-pipelines.high-definition/Runtime/PostProcessing/Components/AutoLensFlare.cs
@@ -13,18 +13,18 @@ namespace UnityEngine.Rendering.HighDefinition
 
         public ClampedFloatParameter blurSize = new ClampedFloatParameter(4f, 0f, 16f);
         public ClampedIntParameter blurSampleCount = new ClampedIntParameter(4, 2, 8);
-        public ClampedFloatParameter vignetteInfluence = new ClampedFloatParameter(0.75f, 0f, 1f);
-		
-		public ClampedFloatParameter chromaticAbberationIntensity = new ClampedFloatParameter(0.025f, 0f, 1f);
+        public ClampedFloatParameter vignetteInfluence = new ClampedFloatParameter(1f, 0f, 1f);
+
+        public ClampedFloatParameter chromaticAbberationIntensity = new ClampedFloatParameter(0.025f, 0f, 1f);
         public ClampedIntParameter chromaticAbberationSampleCount = new ClampedIntParameter(16, 2, 64);
-		
-		public ClampedFloatParameter firstFlareIntensity = new ClampedFloatParameter(0.1f, 0f, 1f);
+
+        public ClampedFloatParameter firstFlareIntensity = new ClampedFloatParameter(0.1f, 0f, 1f);
         public ClampedFloatParameter secondaryFlareIntensity = new ClampedFloatParameter(0.1f, 0f, 1f);
         public ClampedFloatParameter polarFlareIntensity = new ClampedFloatParameter(0.1f, 0f, 1f);
-		
+
         public ClampedFloatParameter blurContribution = new ClampedFloatParameter(0.5f, 0f, 1f);
         public ClampedFloatParameter chromaContribution = new ClampedFloatParameter(1f, 0f, 1f);
-		
+
         /// <summary>
         /// Mandatory function, cannot have an Override without it
         /// </summary>
