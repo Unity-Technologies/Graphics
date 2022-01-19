7fb0ef1f5d1d6b61a542799c5f56c333c128a15e
Andrew Theisen <andrew.theisen@unity3d.com>
2022-01-19 15:29:52 +0100

    -boids optimization -boids with tunable settings -boids cleanup

--- TestProjects/Hackweek_BRG/Assets/Scripts/Boids.cs
+++ TestProjects/Hackweek_BRG/Assets/Scripts/Boids.cs
@@ -1,7 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.Burst;
 using Unity.Jobs;
 using Unity.Collections;
 
@@ -9,12 +7,6 @@ public struct BoidIndividual {
     public Vector3 position;
     public Vector3 acceleration;
     public Vector3 velocity;
-    public float maxForce;
-    public float maxSpeed;
-    public float cohesion;
-    public float repel;
-    public float align;
-    public float goal;
 };
 
 public class Boids : MonoBehaviour
@@ -27,11 +19,15 @@ public class Boids : MonoBehaviour
     public float cohesion = 1.0f;
     public float repel = 1.0f;
     public float align = 1.0f;
-    public float goal = 1.0f;
+    public float matchGoal = 1.0f;
 
     private NativeArray<BoidIndividual> m_boidsBufferA;
     private NativeArray<BoidIndividual> m_boidsBufferB;
     private List<GameObject> m_boidsGO;
+
+    private bool m_firstRun = true;
+    private JobHandle m_handle;
+    private float m_accumulatedTime = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -40,30 +36,16 @@ public class Boids : MonoBehaviour
         m_boidsBufferA = new NativeArray<BoidIndividual>(boidCount, Allocator.Persistent);
         m_boidsBufferB = new NativeArray<BoidIndividual>(boidCount, Allocator.Persistent);
         int rowCount = 0;
-        for (int i = 0; i < boidCount; i++) {
+        for (int i = 0; i < boidCount; i++)
+        {
             Vector3 pos = new Vector3(i % 10, rowCount, i % 10);
-            m_boidsBufferA[i] = new BoidIndividual() {
+            m_boidsBufferA[i] = new BoidIndividual()
+            {
                   position = pos
                 , acceleration = Vector3.zero
                 , velocity = Vector3.zero
-                , maxForce = maxForce
-                , maxSpeed = maxSpeed
-                , cohesion = cohesion
-                , repel = repel
-                , align = align
-                , goal = goal
             };
-            m_boidsBufferB[i] = new BoidIndividual() {
-                  position = pos
-                , acceleration = Vector3.zero
-                , velocity = Vector3.zero
-                , maxForce = maxForce
-                , maxSpeed = maxSpeed
-                , cohesion = cohesion
-                , repel = repel
-                , align = align
-                , goal = goal
-            };
+            m_boidsBufferB[i] = m_boidsBufferA[i];
             var go = Instantiate(boidsPrefab, pos, Quaternion.identity);
             go.transform.position = m_boidsBufferA[i].position;
             m_boidsGO.Add(go);
@@ -74,28 +56,50 @@ public class Boids : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        m_accumulatedTime += Time.deltaTime;
+        if (m_handle.IsCompleted || m_firstRun)
+        {
+            if ( ! m_firstRun)
+            {
+                m_handle.Complete();
 
-        Vector3 mouseGoal = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
-        mouseGoal = Camera.main.ScreenToWorldPoint(mouseGoal);
-        //mouseGoal = mouseGoal + (Camera.main.transform.forward * 10.0f);
+                for (int i = 0; i < boidCount; i++)
+                {
+                    m_boidsGO[i].transform.position = m_boidsBufferB[i].position;
+                }
 
-        BoidMoveJob move_job = new BoidMoveJob();
-        move_job.Input = m_boidsBufferA;
-        move_job.Output = m_boidsBufferB;
-        move_job.deltaTime = Time.deltaTime;
-        move_job.boidCount = boidCount;
-        move_job.goal = mouseGoal;
+                NativeArray<BoidIndividual> temp = m_boidsBufferA;
+                m_boidsBufferA = m_boidsBufferB;
+                m_boidsBufferB = temp;
 
-        JobHandle handle = move_job.Schedule(boidCount, 1);
-        handle.Complete();
+            }
 
-        for (int i = 0; i < boidCount; i++) {
-            m_boidsGO[i].transform.position = m_boidsBufferB[i].position;
+            Vector3 mouseGoal = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+            mouseGoal = Camera.main.ScreenToWorldPoint(mouseGoal);
+
+            if (m_firstRun)
+            {
+                m_accumulatedTime = Time.deltaTime;
+            }
+
+            BoidMoveJob moveJob = new BoidMoveJob();
+            moveJob.Input = m_boidsBufferA;
+            moveJob.Output = m_boidsBufferB;
+            moveJob.deltaTime = m_accumulatedTime;
+            moveJob.boidCount = boidCount;
+            moveJob.goalPosition = mouseGoal;
+            moveJob.maxForce = maxForce;
+            moveJob.maxSpeed = maxSpeed;
+            moveJob.cohesionAmount = cohesion;
+            moveJob.repelAmount = repel;
+            moveJob.alignAmount = align;
+            moveJob.seekGoalAmount = matchGoal;
+            m_accumulatedTime = 0.0f;
+
+            m_handle = moveJob.Schedule(boidCount, 16);
+            m_firstRun = false;
         }
 
-        NativeArray<BoidIndividual> temp = m_boidsBufferA;
-        m_boidsBufferA = m_boidsBufferB;
-        m_boidsBufferB = temp;
     }
 }
 
@@ -103,7 +107,14 @@ public struct BoidMoveJob : IJobParallelFor
 {
     public float deltaTime;
     public int boidCount;
-    public Vector3 goal;
+    public float maxForce;
+    public float maxSpeed;
+    public float cohesionAmount;
+    public float repelAmount;
+    public float alignAmount;
+    public float seekGoalAmount;
+
+    public Vector3 goalPosition;
 
     [ReadOnly]
     public NativeArray<BoidIndividual> Input;
@@ -111,114 +122,98 @@ public struct BoidMoveJob : IJobParallelFor
     [WriteOnly]
     public NativeArray<BoidIndividual> Output;
 
-    public Vector3 cohesion(int index, BoidIndividual individual) 
+    public Vector3 limitForce(Vector3 force, BoidIndividual individual)
     {
-        Vector3 attract = Vector3.zero;
-
-        for (int i = 0; i < boidCount; i++) {
-            if (i == index) {continue;}
-
-            attract += Input[i].position;
-        }
-        attract /= (boidCount - 1);
-        attract = attract - individual.position;
-        //attract /= 5.0f;
-
-        attract = Vector3.Normalize(attract);
-        attract *= individual.maxSpeed;
-        attract -= individual.velocity;
-        if (attract.magnitude > individual.maxForce) {
-            attract = Vector3.Normalize(attract) * individual.maxForce;
-        }
-
-        return attract;
-    }
-
-    public Vector3 repel_boid(int index, BoidIndividual individual) 
-    {
-        Vector3 repel = Vector3.zero;
-
-        for (int i = 0; i < boidCount; i++) {
-            if (i == index) {continue;}
-
-            Vector3 other_pos = Input[i].position;
-
-            Vector3 r = other_pos - individual.position;
-            if (r.magnitude < 0.5f) {
-                //repel -= (r *30);
-                repel -= r;
-            }
-        }
-        repel /= (boidCount - 1);
-
-        repel = Vector3.Normalize(repel);
-        repel *= individual.maxSpeed;
-        repel -= individual.velocity;
-        if (repel.magnitude > individual.maxForce) {
-            repel = Vector3.Normalize(repel) * individual.maxForce;
-        }
-
-        return repel;
-    }
-
-    public Vector3 align_boid(int index, BoidIndividual individual) 
-    {
-        Vector3 match_vel = Vector3.zero;
-        for (int i = 0; i < boidCount; i++)
+        force = Vector3.Normalize(force);
+        force *= maxSpeed;
+        force -= individual.velocity;
+        if (force.magnitude > maxForce)
         {
-            if (i == index) {continue;}
-
-            match_vel += Input[i].velocity;
+            force = Vector3.Normalize(force) * maxForce;
         }
-        match_vel /= (boidCount - 1);
+        return force;
+    }
 
-        match_vel = Vector3.Normalize(match_vel);
-        match_vel *= individual.maxSpeed;
-        match_vel -= individual.velocity;
-        if (match_vel.magnitude > individual.maxForce) {
-            match_vel = Vector3.Normalize(match_vel) * individual.maxForce;
+    public Vector3 cohere(Vector3 cohesionForce, BoidIndividual individual)
+    {
+        cohesionForce /= (boidCount - 1);
+        cohesionForce = cohesionForce - individual.position;
+
+        return limitForce(cohesionForce, individual);
+    }
+
+    public Vector3 repel(Vector3 repelForce, BoidIndividual individual)
+    {
+        repelForce /= (boidCount - 1);
+
+        return limitForce(repelForce, individual);
+    }
+
+    public Vector3 align(Vector3 alignForce, BoidIndividual individual)
+    {
+        alignForce /= (boidCount - 1);
+
+        return limitForce(alignForce, individual);
+    }
+
+    public Vector3 seekGoal(BoidIndividual individual)
+    {
+        Vector3 seekGoalForce = goalPosition - individual.position;
+        return limitForce(seekGoalForce, individual);
+    }
+
+    public Vector3 limitVelocity(Vector3 velocity)
+    {
+        if (velocity.magnitude > maxSpeed)
+        {
+            velocity = Vector3.Normalize(velocity) * maxSpeed;
         }
-        //match_vel /= 10.0f;
-        return match_vel;
+
+        return velocity;
     }
 
     public void Execute(int index)
     {
-        Debug.Log(index);
         BoidIndividual individual = Input[index];
 
-        Vector3 attract = Vector3.zero;
-        Vector3 repel = Vector3.zero;
-        Vector3 align = Vector3.zero;
-        Vector3 match_goal = Vector3.zero;
+        Vector3 cohesionForce = Vector3.zero;
+        Vector3 repelForce = Vector3.zero;
+        Vector3 alignForce = Vector3.zero;
+        Vector3 seekGoalForce = Vector3.zero;
 
-        attract = cohesion(index, individual);
-        repel = repel_boid(index, individual);
-        align = align_boid(index, individual);
+        for (int i = 0; i < boidCount; i++)
+        {
+            if (i == index) {continue;}
 
-        //match_goal = (goal - individual.position) / 2.0f;
-        match_goal = Vector3.Normalize(goal - individual.position);
-        match_goal *= individual.maxSpeed;
-        match_goal -= individual.velocity;
-        if (match_goal.magnitude > individual.maxForce) {
-            match_goal = Vector3.Normalize(match_goal) * individual.maxForce;
+            cohesionForce += Input[i].position;
+
+            Vector3 otherPos = Input[i].position;
+            Vector3 r = otherPos - individual.position;
+            if (r.magnitude < 0.5f)
+            {
+                repelForce -= r;
+            }
+
+            alignForce += Input[i].velocity;
         }
 
-        attract *= individual.cohesion;
-        repel *= individual.repel;
-        align *= individual.align;
-        match_goal *= individual.goal;
+        cohesionForce = cohere(cohesionForce, individual);
+        repelForce = repel(repelForce, individual);
+        alignForce = align(alignForce, individual);
+        seekGoalForce = seekGoal(individual);
 
-        individual.acceleration += attract + repel + align + match_goal;
+        cohesionForce *= cohesionAmount;
+        repelForce *= repelAmount;
+        alignForce *= alignAmount;
+        seekGoalForce *= seekGoalAmount;
+
+        individual.acceleration += cohesionForce + repelForce + alignForce + seekGoalForce;
 
         individual.velocity += individual.acceleration * deltaTime;
-
-        if (individual.velocity.magnitude > individual.maxSpeed)
-        {
-            individual.velocity = Vector3.Normalize(individual.velocity) * individual.maxSpeed;
-        }
+        individual.velocity = limitVelocity(individual.velocity);
 
         individual.position += (individual.velocity * deltaTime);
+
         Output[index] = individual;
     }
 }
