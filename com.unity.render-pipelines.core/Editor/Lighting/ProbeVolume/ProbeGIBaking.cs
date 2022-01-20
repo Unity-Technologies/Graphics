b6829a852bad5410eca1b3d530ef8ec3b52a5a22
Torbjorn Laedre <271210+tlaedre@users.noreply.github.com>
2022-01-20 13:50:05 +0100

    Explicitly make binary blob files editable.

--- com.unity.render-pipelines.core/Editor/Lighting/ProbeVolume/ProbeGIBaking.cs
+++ com.unity.render-pipelines.core/Editor/Lighting/ProbeVolume/ProbeGIBaking.cs
@@ -787,7 +787,14 @@ namespace UnityEngine.Experimental.Rendering
             EditorUtility.SetDirty(asset);
             AssetDatabase.SaveAssets();
 
+            // Get binary blob output filenames
             data.GetBlobFileNames(out var cellDataFilename, out var cellOptionalDataFilename, out var cellSharedDataFilename, out var cellSupportDataFilename);
+
+            // Explicitly make sure the binary output files are writable since we write them using the C# file API (i.e. check out Perforce files if applicable)
+            var outputPaths = new List<string>(new [] {cellDataFilename, cellSharedDataFilename, cellSupportDataFilename});
+            if(asset.bands == ProbeVolumeSHBands.SphericalHarmonicsL2) outputPaths.Add(cellOptionalDataFilename);
+            if(!AssetDatabase.MakeEditable(outputPaths.ToArray()))
+                Debug.LogWarning($"Failed to make one or more probe volume output file(s) writable. This could result in baked data not being properly written to disk. {string.Join(",", outputPaths)}");
 
             unsafe
             {
