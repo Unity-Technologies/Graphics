47f2694ac3068f9c3e0ac73d647301b855b024c8
martint-unity <martint@unity3d.com>
2022-01-21 13:46:58 +0100

    added scriptableobject for platforms and APIs

---
+++ TestProjects/UniversalGraphicsTest_Foundation/Assets/AllPlatformsAndAPI.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Platforms And API", menuName = "Tests/Add Platform And API")]
+public class AllPlatformsAndAPI : ScriptableObject
+{
+    public PlatformsAndAPIForTesting[] allPlatformsAndAPIForTesting;
+}
---
+++ TestProjects/UniversalGraphicsTest_Foundation/Assets/PlatformsAndAPIForTesting.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+
+[System.Serializable]
+public class PlatformsAndAPIForTesting
+{
+    public RuntimePlatform Platform;
+    public GraphicsDeviceType[] APIS;
+}
