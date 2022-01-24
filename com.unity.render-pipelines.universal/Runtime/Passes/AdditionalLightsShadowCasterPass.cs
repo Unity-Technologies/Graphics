22dad7ae0a55cd668a1f1564670b330c00ef6ebb
Apoorva Joshi <apoorvaj@apoorvaj.io>
2022-01-24 15:30:41 +0100

    Pass isOrthographic parameter to ShadowDrawingSettings constructor in URP

--- com.unity.render-pipelines.universal/Runtime/Passes/AdditionalLightsShadowCasterPass.cs
+++ com.unity.render-pipelines.universal/Runtime/Passes/AdditionalLightsShadowCasterPass.cs
@@ -913,7 +913,7 @@ namespace UnityEngine.Rendering.Universal.Internal
 
                     ShadowSliceData shadowSliceData = m_AdditionalLightsShadowSlices[globalShadowSliceIndex];
 
-                    var settings = new ShadowDrawingSettings(cullResults, originalLightIndex);
+                    var settings = new ShadowDrawingSettings(cullResults, originalLightIndex, false);
                     settings.useRenderingLayerMaskTest = UniversalRenderPipeline.asset.supportsLightLayers;
                     settings.splitData = shadowSliceData.splitData;
                     Vector4 shadowBias = ShadowUtils.GetShadowBias(ref shadowLight, visibleLightIndex,
