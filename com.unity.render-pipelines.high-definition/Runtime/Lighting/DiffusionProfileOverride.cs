dd96c9bffc9009c1bb806289594035aa59b21821
Adrien de Tocqueville <adrien.tocqueville@unity3d.com>
2022-01-24 10:12:04 +0100

    Open Diffusion Profile Volume Override API

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/DiffusionProfileOverride.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/DiffusionProfileOverride.cs
@@ -2,18 +2,32 @@ using System;
 
 namespace UnityEngine.Rendering.HighDefinition
 {
+    /// <summary>
+    /// A volume component that holds Diffusion Profile Overrides.
+    /// </summary>
     [Serializable, VolumeComponentMenuForRenderPipeline("Material/Diffusion Profile Override", typeof(HDRenderPipeline))]
     [HDRPHelpURLAttribute("Override-Diffusion-Profile")]
-    sealed class DiffusionProfileOverride : VolumeComponent
+    public sealed class DiffusionProfileOverride : VolumeComponent
     {
+        /// <summary>
+        /// List of diffusion profiles used inside the volume.
+        /// </summary>
         [Tooltip("List of diffusion profiles used inside the volume.")]
         [SerializeField]
-        internal DiffusionProfileSettingsParameter diffusionProfiles = new DiffusionProfileSettingsParameter(default(DiffusionProfileSettings[]));
+        public DiffusionProfileSettingsParameter diffusionProfiles = new DiffusionProfileSettingsParameter(default(DiffusionProfileSettings[]));
     }
 
+    /// <summary>
+    /// A <see cref="VolumeParameter"/> that holds a <see cref="DiffusionProfileSettings"/> value.
+    /// </summary>
     [Serializable]
-    sealed class DiffusionProfileSettingsParameter : VolumeParameter<DiffusionProfileSettings[]>
+    public sealed class DiffusionProfileSettingsParameter : VolumeParameter<DiffusionProfileSettings[]>
     {
+        /// <summary>
+        /// Creates a new <see cref="DiffusionProfileSettingsParameter"/> instance.
+        /// </summary>
+        /// <param name="value">The initial value to store in the parameter.</param>
+        /// <param name="overrideState">The initial override state for the parameter.</param>
         public DiffusionProfileSettingsParameter(DiffusionProfileSettings[] value, bool overrideState = true)
             : base(value, overrideState) { }
     }
