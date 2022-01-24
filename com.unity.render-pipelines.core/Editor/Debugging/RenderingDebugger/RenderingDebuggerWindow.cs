821370c93f46eda077ba8833a4b2a907e252d45e
Alex Vázquez <alex.vazquez@unity3d.com>
2022-01-24 17:43:00 +0100

    UITK tabs implementations and fixes

--- com.unity.render-pipelines.core/Editor/Debugging/RenderingDebugger/RenderingDebuggerWindow.cs
+++ com.unity.render-pipelines.core/Editor/Debugging/RenderingDebugger/RenderingDebuggerWindow.cs
@@ -1,7 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
+using UnityEditor.Rendering.UI;
 using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -11,11 +10,7 @@ namespace UnityEditor.Rendering
 {
     public class RenderingDebuggerWindow : EditorWindow
     {
-        private IMGUIContainer tabContainer;
-        private List<SerializedObject> serializedObjects = new ();
-
-        private int selectedPanel; // TODO needs to be serialized to support undo & domain reload persistence
-        private string selectedPanelName;
+        private TabbedMenuController controller;
 
         [MenuItem("Window/Analysis/Rendering Debugger (UITK)", priority = 10006)]
         public static void ShowDefaultWindow()
@@ -32,6 +27,9 @@ namespace UnityEditor.Rendering
 
             windowTemplate.CloneTree(this.rootVisualElement);
 
+            var tabsVisualElement = this.rootVisualElement.Q<VisualElement>("tabs");
+            var tabContentVisualElement = this.rootVisualElement.Q<VisualElement>("tabContent");
+
             UIDocument runtimeUIDocument = null;
             // TODO: Move this to another place on runtime checking the command for opening rendering debugger
             var runtimeRenderingDebugger = FindObjectOfType<RenderingDebuggerRuntime>();
@@ -40,8 +38,9 @@ namespace UnityEditor.Rendering
                 var runtimeRenderingDebuggerGO = new GameObject("RenderingDebugger");
                 runtimeRenderingDebugger = runtimeRenderingDebuggerGO.AddComponent<RenderingDebuggerRuntime>();
                 runtimeUIDocument = runtimeRenderingDebuggerGO.AddComponent<UIDocument>();
-                runtimeUIDocument.visualTreeAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Packages/com.unity.render-pipelines.core/Runtime/Debugging/RenderingDebugger/runtime-container.uxml");
+                runtimeUIDocument.visualTreeAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Packages/com.unity.render-pipelines.core/Runtime/Debugging/RenderingDebugger/RenderingDebuggerRuntimeContainer.uxml");
                 runtimeUIDocument.panelSettings = AssetDatabase.LoadAssetAtPath<PanelSettings>("Packages/com.unity.render-pipelines.core/Runtime/Debugging/RenderingDebugger/RenderingDebuggerPanelSettings.asset");
+                runtimeUIDocument.panelSettings.themeStyleSheet = AssetDatabase.LoadAssetAtPath<ThemeStyleSheet>("Assets/UI Toolkit/UnityThemes/UnityDefaultRuntimeTheme.tss");
             }
             else
             {
@@ -49,95 +48,61 @@ namespace UnityEditor.Rendering
             }
             // END TODO
 
-            tabContainer = new IMGUIContainer(() => TabContainerOnGUI());
-            this.rootVisualElement.Add(tabContainer);
-
-            serializedObjects.Clear();
-
+            bool firstTabAdded = false;
             foreach (var panel in TypeCache.GetTypesWithAttribute<RenderingDebuggerPanelAttribute>())
             {
                 var panelDescriptionAttribute = panel.GetCustomAttribute<RenderingDebuggerPanelAttribute>();
                 var panelVisualTreeAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(panelDescriptionAttribute.uiDocumentPath);
-                if (panelVisualTreeAsset != null)
+                if (panelVisualTreeAsset == null)
+                    continue;
+
+                // Create the tab
+                var panelHeader = new Label()
                 {
-                    // Create the visual element and clone the panel document
-                    VisualElement panelVisualElement = new VisualElement();
-                    panelVisualElement.AddToClassList("PanelElement");
-                    panelVisualElement.name = panelDescriptionAttribute.panelName;
-                    panelVisualTreeAsset.CloneTree(panelVisualElement);
+                    name = $"{panelDescriptionAttribute.panelName}{TabbedMenuController.k_TabNameSuffix}",
+                    text = panelDescriptionAttribute.panelName
+                };
+                panelHeader.AddToClassList(TabbedMenuController.k_TabClassName);
 
-                    // TODO: Scriptable object must be saved somewhere into a rendering debugger manager, obtain it from there
-                    var scriptableObject = ScriptableObject.CreateInstance(panel);
+                // Create the content of the tab
+                VisualElement panelVisualElement = new VisualElement() {name = $"{panelDescriptionAttribute.panelName}{TabbedMenuController.k_ContentNameSuffix}"};
+                panelVisualTreeAsset.CloneTree(panelVisualElement);
 
-                    SerializedObject so = new SerializedObject(scriptableObject);
+                var scriptableObject = ScriptableObject.CreateInstance(panel);
 
-                    // Bind it to the visual element of the panel
-                    panelVisualElement.Bind(so);
+                SerializedObject so = new SerializedObject(scriptableObject);
 
-                    // Initialize first selected
-                    if (!serializedObjects.Any())
-                    {
-                        selectedPanel = 0;
-                        selectedPanelName = panelVisualElement.name;
-                    }
-                    else
-                    {
-                        panelVisualElement.AddToClassList("Hidden");
-                    }
+                // Bind it to the visual element of the panel
+                panelVisualElement.Bind(so);
 
-                    serializedObjects.Add(so);
-
-                    // EDITOR UI
-                    this.rootVisualElement.Add(panelVisualElement);
-
-                    // RUNTIME UI
-                    // Create the visual element and clone the panel document
-                    var panelHeaderRuntime = new Label {text = panelDescriptionAttribute.panelName};
-                    VisualElement panelVisualElementRuntime = new VisualElement();
-                    panelVisualTreeAsset.CloneTree(panelVisualElementRuntime);
-                    panelVisualElementRuntime.Bind(so);
-                    runtimeUIDocument.rootVisualElement.Add(panelHeaderRuntime);
-                    runtimeUIDocument.rootVisualElement.Add(panelVisualElementRuntime);
+                if (firstTabAdded == false)
+                {
+                    panelHeader.AddToClassList(TabbedMenuController.k_CurrentlySelectedTabClassName);
+                    firstTabAdded = true;
                 }
+                else
+                {
+                    panelVisualElement.AddToClassList(TabbedMenuController.k_UnselectedContentClassName);
+                }
+
+                tabsVisualElement.Add(panelHeader);
+                tabContentVisualElement.Add(panelVisualElement);
+
+
+                // RUNTIME UI TODO MOVE
+                // Create the visual element and clone the panel document
+                var panelHeaderRuntime = new Label {text = panelDescriptionAttribute.panelName};
+                VisualElement panelVisualElementRuntime = new VisualElement();
+                panelVisualTreeAsset.CloneTree(panelVisualElementRuntime);
+                panelVisualElementRuntime.Bind(so);
+                runtimeUIDocument.rootVisualElement.Add(panelHeaderRuntime);
+                runtimeUIDocument.rootVisualElement.Add(panelVisualElementRuntime);
             }
+
+            controller = new(rootVisualElement);
+            controller.RegisterTabCallbacks();
 
             this.minSize = new Vector2(400, 250);
-        }
-
-        public void SetSelectedPanel(int panelIndex, string panelName)
-        {
-            // hide previously selected panel
-            this.rootVisualElement.Q<VisualElement>(selectedPanelName, "PanelElement").AddToClassList("Hidden");
-
-            selectedPanel = panelIndex;
-            selectedPanelName = panelName;
-
-            // display newly selected panel
-            this.rootVisualElement.Q<VisualElement>(panelName, "PanelElement").RemoveFromClassList("Hidden");
-        }
-
-        public void TabContainerOnGUI()
-        {
-            var numPanels = serializedObjects.Count;
-            for (int i = 0; i < numPanels; i++)
-            {
-                var so = serializedObjects[i];
-
-                var panelAttribute = so.targetObject.GetType().GetCustomAttribute<RenderingDebuggerPanelAttribute>();
-
-                var elementRect = GUILayoutUtility.GetRect(EditorGUIUtility.TrTextContent(panelAttribute.panelName), DebugWindow.s_Styles.sectionElement, GUILayout.ExpandWidth(true));
-
-                if (selectedPanel == i && Event.current.type == EventType.Repaint)
-                    DebugWindow.s_Styles.selected.Draw(elementRect, false, false, false, false);
-
-                EditorGUI.BeginChangeCheck();
-                GUI.Toggle(elementRect, selectedPanel == i, panelAttribute.panelName, DebugWindow.s_Styles.sectionElement);
-                if (EditorGUI.EndChangeCheck())
-                {
-                    //Undo.RegisterCompleteObjectUndo(m_Settings, $"Debug Panel '{panel.displayName}' Selection");
-                    SetSelectedPanel(i, panelAttribute.panelName);
-                }
-            }
         }
     }
 }
---
+++ com.unity.render-pipelines.core/Editor/UITK/TabbedMenuController.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine.UIElements;
+
+namespace UnityEditor.Rendering.UI
+{
+    class TabbedMenuController
+    {
+        /* Define member variables*/
+        public const string k_TabClassName = "tab";
+        public const string k_CurrentlySelectedTabClassName = "currentlySelectedTab";
+        public const string k_UnselectedContentClassName = "unselectedContent";
+        // Tab and tab content have the same prefix but different suffix
+        // Define the suffix of the tab name
+        public const string k_TabNameSuffix = "Tab";
+        // Define the suffix of the tab content name
+        public const string k_ContentNameSuffix = "Content";
+
+        private readonly VisualElement m_Root;
+
+        public TabbedMenuController(VisualElement root)
+        {
+            this.m_Root = root;
+        }
+
+        public void RegisterTabCallbacks()
+        {
+            UQueryBuilder<Label> tabs = GetAllTabs();
+            tabs.ForEach((Label tab) => {
+                tab.RegisterCallback<ClickEvent>(TabOnClick);
+            });
+        }
+
+        /* Method for the tab on-click event:
+
+           - If it is not selected, find other tabs that are selected, unselect them
+           - Then select the tab that was clicked on
+        */
+        private void TabOnClick(ClickEvent evt)
+        {
+            Label clickedTab = evt.currentTarget as Label;
+            if (!TabIsCurrentlySelected(clickedTab))
+            {
+                GetAllTabs().Where(
+                    (tab) => tab != clickedTab && TabIsCurrentlySelected(tab)
+                ).ForEach(UnselectTab);
+                SelectTab(clickedTab);
+            }
+        }
+        //Method that returns a Boolean indicating whether a tab is currently selected
+        private static bool TabIsCurrentlySelected(Label tab)
+        {
+            return tab.ClassListContains(k_CurrentlySelectedTabClassName);
+        }
+
+        private UQueryBuilder<Label> GetAllTabs()
+        {
+            return m_Root.Query<Label>(className: k_TabClassName);
+        }
+
+        /* Method for the selected tab:
+           -  Takes a tab as a parameter and adds the currentlySelectedTab class
+           -  Then finds the tab content and removes the unselectedContent class */
+        private void SelectTab(Label tab)
+        {
+            tab.AddToClassList(k_CurrentlySelectedTabClassName);
+            VisualElement content = FindContent(tab);
+            content.RemoveFromClassList(k_UnselectedContentClassName);
+        }
+
+        /* Method for the unselected tab:
+           -  Takes a tab as a parameter and removes the currentlySelectedTab class
+           -  Then finds the tab content and adds the unselectedContent class */
+        private void UnselectTab(Label tab)
+        {
+            tab.RemoveFromClassList(k_CurrentlySelectedTabClassName);
+            VisualElement content = FindContent(tab);
+            content.AddToClassList(k_UnselectedContentClassName);
+        }
+
+        // Method to generate the associated tab content name by for the given tab name
+        private static string GenerateContentName(Label tab) =>
+            tab.name.Replace(k_TabNameSuffix, k_ContentNameSuffix);
+
+        // Method that takes a tab as a parameter and returns the associated content element
+        private VisualElement FindContent(Label tab)
+        {
+            return m_Root.Q(GenerateContentName(tab));
+        }
+    }
+}
