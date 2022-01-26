dc6cd57283bf7da6b5e9bd183bcfa1fe975ef536
Sai Narayan <nightmask3@gmail.com>
2022-01-26 12:30:55 -0800

    added preview manager class

---
+++ com.unity.shadergraph/Editor/GraphUI/EditorCommon/Preview/PreviewManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Editor.GraphUI.Utilities;
+using UnityEditor.ShaderGraph.GraphDelta;
+using UnityEngine;
+using UnityEditor.ShaderGraph.GraphUI.DataModel;
+
+namespace UnityEditor.ShaderGraph.GraphUI.EditorCommon.Preview
+{
+	public class PreviewManager
+	{
+
+	}
+}
