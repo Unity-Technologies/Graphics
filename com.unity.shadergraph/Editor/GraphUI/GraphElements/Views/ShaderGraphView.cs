5007a40091032f2e0d6c3f0efd288ea409e8a9ed
Brett Groshong <brett.groshong@unity3d.com>
2022-01-27 11:37:08 -0800

    Delete meta file for removed Window dir.

--- com.unity.shadergraph/Editor/GraphUI/GraphElements/Views/ShaderGraphView.cs
+++ com.unity.shadergraph/Editor/GraphUI/GraphElements/Views/ShaderGraphView.cs
@@ -9,30 +9,31 @@ namespace UnityEditor.ShaderGraph.GraphUI.GraphElements.Views
 {
     public class ShaderGraphView : GraphView
     {
-        public ShaderGraphView(GraphViewEditorWindow window, BaseGraphTool graphTool, string graphViewName)
+        public ShaderGraphView(
+            GraphViewEditorWindow window,
+            BaseGraphTool graphTool,
+            string graphViewName)
             : base(window, graphTool, graphViewName)
         {
-
         }
 
         protected override void BuildContextualMenu(ContextualMenuPopulateEvent evt)
         {
             base.BuildContextualMenu(evt);
-
             for (var i = 0; i < evt.menu.MenuItems().Count; ++i)
             {
                 var menuItem = evt.menu.MenuItems()[i];
                 if (menuItem is DropdownMenuAction { name: "Disable Nodes" })
                     evt.menu.RemoveItemAt(i);
             }
-
             evt.menu.AppendSeparator();
         }
 
-        protected override void CollectCopyableGraphElements(IEnumerable<IGraphElementModel> elements, HashSet<IGraphElementModel> elementsToCopySet)
+        protected override void CollectCopyableGraphElements(
+            IEnumerable<IGraphElementModel> elements,
+            HashSet<IGraphElementModel> elementsToCopySet)
         {
             var elementsList = elements.ToList();
-
             base.CollectCopyableGraphElements(elementsList, elementsToCopySet);
 
             // Pasting a redirect should also paste an edge to its source node.
