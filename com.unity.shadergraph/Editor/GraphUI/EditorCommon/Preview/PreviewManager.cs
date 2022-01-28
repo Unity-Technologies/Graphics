ea5fba0e2d2563ef64048da52e4065859be97a32
Sai Narayan <nightmask3@gmail.com>
2022-01-27 17:44:35 -0800

    fixing formatting for indents

--- com.unity.shadergraph/Editor/GraphUI/EditorCommon/Preview/PreviewManager.cs
+++ com.unity.shadergraph/Editor/GraphUI/EditorCommon/Preview/PreviewManager.cs
@@ -7,45 +7,27 @@ using UnityEditor.ShaderGraph.GraphUI.DataModel;
 namespace UnityEditor.ShaderGraph.GraphUI.EditorCommon.Preview
 {
 
-using PreviewRenderMode = HeadlessPreviewManager.PreviewRenderMode;
+    using PreviewRenderMode = HeadlessPreviewManager.PreviewRenderMode;
 
-	public class PreviewManager
-	{
-		private HeadlessPreviewManager m_PreviewHandlerInstance;
+    public class PreviewManager
+    {
+        private HeadlessPreviewManager m_PreviewHandlerInstance;
 
-		public PreviewManager()
+        public PreviewManager()
         {
-			m_PreviewHandlerInstance = new HeadlessPreviewManager();
+            m_PreviewHandlerInstance = new HeadlessPreviewManager();
         }
 
-		public void OnPreviewExpansionChanged(string nodeName, bool newExpansionState)
-        {
+        public void OnPreviewExpansionChanged(string nodeName, bool newExpansionState) { }
 
-        }
+        public void OnPreviewModeChanged(string nodeName, PreviewRenderMode newPreviewMode) { }
 
-		public void OnPreviewModeChanged(string nodeName, PreviewRenderMode newPreviewMode)
-        {
+        public void OnNodeFlowChanged(string nodeName) { }
 
-        }
+        public void OnNodeAdded(string nodeName) { }
 
-		public void OnNodeFlowChanged(string nodeName)
-        {
+        public void OnGlobalPropertyChanged(string propertyName, object newValue) { }
 
-        }
-
-		public void OnNodeAdded(string nodeName)
-        {
-
-        }
-
-        public void OnGlobalPropertyChanged(string propertyName, object newValue)
-        {
-
-        }
-
-        public void OnLocalPropertyChanged(string nodeName, string propertyName, object newValue)
-        {
-
-        }
-	}
+        public void OnLocalPropertyChanged(string nodeName, string propertyName, object newValue) { }
+    }
 }
