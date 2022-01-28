8fb002ae3d589767eb3e525043e9c515519f86cf
Brett Groshong <brett.groshong@unity3d.com>
2022-01-28 11:01:05 -0800

    Reformat extension functions for view factory to be called correctly.

--- com.unity.shadergraph/Editor/GraphUI/Factory/ShaderGraphViewFactoryExtensions.cs
+++ com.unity.shadergraph/Editor/GraphUI/Factory/ShaderGraphViewFactoryExtensions.cs
@@ -2,13 +2,15 @@ using UnityEditor.GraphToolsFoundation.Overdrive;
 using UnityEditor.GraphToolsFoundation.Overdrive.BasicModel;
 using UnityEditor.ShaderGraph.GraphUI.DataModel;
 using UnityEditor.ShaderGraph.GraphUI.GraphElements;
+using UnityEditor.ShaderGraph.GraphUI.GraphElements.Views;
 
 namespace UnityEditor.ShaderGraph.GraphUI.Factory
 {
     [GraphElementsExtensionMethodsCache(typeof(GraphView))]
     public static class ShaderGraphViewFactoryExtensions
     {
-        public static IModelUI CreateGraphDataNode(this ElementBuilder elementBuilder, CommandDispatcher store,
+        public static IModelUI CreateNode(
+            this ElementBuilder elementBuilder,
             GraphDataNodeModel model)
         {
             var ui = new GraphDataNode();
@@ -16,7 +18,8 @@ namespace UnityEditor.ShaderGraph.GraphUI.Factory
             return ui;
         }
 
-        public static IModelUI CreateRedirectNode(this ElementBuilder elementBuilder, CommandDispatcher store,
+        public static IModelUI CreateRedirectNode(
+            this ElementBuilder elementBuilder,
             RedirectNodeModel model)
         {
             var ui = new RedirectNode();
@@ -24,7 +27,9 @@ namespace UnityEditor.ShaderGraph.GraphUI.Factory
             return ui;
         }
 
-        public static IModelUI CreateEdge(this ElementBuilder elementBuilder, CommandDispatcher store,
+        public static IModelUI CreateEdge(
+            this ElementBuilder elementBuilder,
+            CommandDispatcher store,
             EdgeModel model)
         {
             var ui = new RedirectableEdge();
