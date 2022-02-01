615646fe39b0e017cf4564df8432796813bfa856
Antoine Lelievre <antoinel@unity3d.com>
2022-02-01 14:53:30 +0100

    Update HDRP fullscreen tests

--- TestProjects/HDRP_Tests/Assets/GraphicTests/Scenes/8x_ShaderGraph/8212_Fullscreen/CustomPP2_8210.cs
+++ TestProjects/HDRP_Tests/Assets/GraphicTests/Scenes/8x_ShaderGraph/8212_Fullscreen/CustomPP2_8210.cs
@@ -3,8 +3,8 @@ using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
 using System;
 
-[Serializable, VolumeComponentMenu("Post-processing/Custom/8210_CustomPP1")]
-public sealed class CustomPP1_8210 : CustomPostProcessVolumeComponent, IPostProcessComponent
+[Serializable, VolumeComponentMenu("Post-processing/Custom/8212_CustomPP2")]
+public sealed class CustomPP2_8210 : CustomPostProcessVolumeComponent, IPostProcessComponent
 {
     [Tooltip("Controls the intensity of the effect.")]
     public ClampedFloatParameter intensity = new ClampedFloatParameter(0f, 0f, 1f);
@@ -14,16 +14,16 @@ public sealed class CustomPP1_8210 : CustomPostProcessVolumeComponent, IPostProc
     public bool IsActive() => m_Material != null && intensity.value > 0f;
 
     // Do not forget to add this post process in the Custom Post Process Orders list (Project Settings > Graphics > HDRP Settings).
-    public override CustomPostProcessInjectionPoint injectionPoint => CustomPostProcessInjectionPoint.BeforePostProcess;
+    public override CustomPostProcessInjectionPoint injectionPoint => CustomPostProcessInjectionPoint.AfterPostProcess;
 
-    const string kShaderName = "Hidden/Shader/8210_CustomPP1";
+    const string kShaderName = "Hidden/Shader/8212_CustomPP2";
 
     public override void Setup()
     {
         if (Shader.Find(kShaderName) != null)
             m_Material = new Material(Shader.Find(kShaderName));
         else
-            Debug.LogError($"Unable to find shader '{kShaderName}'. Post Process Volume 8210_CustomPP1 is unable to load.");
+            Debug.LogError($"Unable to find shader '{kShaderName}'. Post Process Volume 8212_CustomPP2 is unable to load.");
     }
 
     public override void Render(CommandBuffer cmd, HDCamera camera, RTHandle source, RTHandle destination)
