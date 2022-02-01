7e15d6f722a2b802e72e4e116505c10094e75542
FrancescoC-Unity <francescoc@unity3d.com>
2022-02-01 13:13:48 +0100

    Name back.

--- com.unity.render-pipelines.core/Editor/Lighting/ProbeVolume/ProbeGIBaking.VirtualOffset.cs
+++ com.unity.render-pipelines.core/Editor/Lighting/ProbeVolume/ProbeGIBaking.VirtualOffset.cs
@@ -309,37 +309,63 @@ namespace UnityEngine.Experimental.Rendering
             const float k0 = 0, k1 = 1, k2 = (float)0.70710678118654752440084436210485, k3 = (float)0.57735026918962576450914878050196;
             static readonly Vector3[] kRayDirections =
             {
-                new(-k3, +k3, -k3),
-                new( k0, +k2, -k2),
-                new(+k3, +k3, -k3),
-                new(-k2, +k2,  k0),
-                new( k0, +k1,  k0),
-                new(+k2, +k2,  k0),
-                new(-k3, +k3, +k3),
-                new( k0, +k2, +k2),
-                new(+k3, +k3, +k3),
+                new(-k3, +k3, -k3), // -1  1 -1
+                new( k0, +k2, -k2), //  0  1 -1
+                new(+k3, +k3, -k3), //  1  1 -1
+                new(-k2, +k2,  k0), // -1  1  0
+                new( k0, +k1,  k0), //  0  1  0
+                new(+k2, +k2,  k0), //  1  1  0
+                new(-k3, +k3, +k3), // -1  1  1
+                new( k0, +k2, +k2), //  0  1  1
+                new(+k3, +k3, +k3), //  1  1  1
 
-                new(-k2,  k0, -k2),
-                new( k0,  k0, -k1),
-                new(+k2,  k0, -k2),
-                new(-k1,  k0,  k0),
+                new(-k2,  k0, -k2), // -1  0 -1
+                new( k0,  k0, -k1), //  0  0 -1
+                new(+k2,  k0, -k2), //  1  0 -1
+                new(-k1,  k0,  k0), // -1  0  0
                 // k0, k0, k0 - skip center position (which would be a zero-length ray)
-                new(+k1,  k0,  k0),
-                new(-k2,  k0, +k2),
-                new( k0,  k0, +k1),
-                new(+k2,  k0, +k2),
+                new(+k1,  k0,  k0), //  1  0  0
+                new(-k2,  k0, +k2), // -1  0  1
+                new( k0,  k0, +k1), //  0  0  1
+                new(+k2,  k0, +k2), //  1  0  1
 
-                new(-k3, -k3, -k3),
-                new( k0, -k2, -k2),
-                new(+k3, -k3, -k3),
-                new(-k2, -k2,  k0),
-                new( k0, -k1,  k0),
-                new(+k2, -k2,  k0),
-                new(-k3, -k3, +k3),
-                new( k0, -k2, +k2),
-                new(+k3, -k3, +k3),
+                new(-k3, -k3, -k3), // -1 -1 -1
+                new( k0, -k2, -k2), //  0 -1 -1
+                new(+k3, -k3, -k3), //  1 -1 -1
+                new(-k2, -k2,  k0), // -1 -1  0
+                new( k0, -k1,  k0), //  0 -1  0
+                new(+k2, -k2,  k0), //  1 -1  0
+                new(-k3, -k3, +k3), // -1 -1  1
+                new( k0, -k2, +k2), //  0 -1  1
+                new(+k3, -k3, +k3), //  1 -1  1
             };
         }
+
+#if USE_BURST
+        [BurstCompile]
+#endif
+        struct RestoreValidityJob
+        {
+            [ReadOnly] public VirtualOffsetSettings voSettings;
+
+            [NativeDisableContainerSafetyRestriction]
+            [ReadOnly] public NativeArray<bool> positionHasColliders;
+
+            [ReadOnly] public int startIdx;
+            [ReadOnly] public int endIdx;
+
+            [ReadOnly] public NativeArray<RaycastCommand> raycastCommands;
+            [ReadOnly] public NativeArray<RaycastHit> raycastHits;
+
+            [NativeDisableContainerSafetyRestriction]
+            public NativeArray<bool> needRestoring;
+
+            public void Execute()
+            {
+
+            }
+        }
+
 
         // A job that pushes probe positions out of geometry based on raycast results.
 #if USE_BURST
