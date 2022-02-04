4792a6a89655d20f8f60e6033333d9417714a17f
Sandy Carter <sandy.carter@unity3d.com>
2022-02-04 16:57:40 -0500

    URP PP: Use Blitter for PaniniProjection

--- com.unity.render-pipelines.universal/Runtime/Passes/PostProcessPass.cs
+++ com.unity.render-pipelines.universal/Runtime/Passes/PostProcessPass.cs
@@ -1032,8 +1032,7 @@ namespace UnityEngine.Rendering.Universal
                 1f - Mathf.Abs(paniniD) > float.Epsilon
                 ? ShaderKeywordStrings.PaniniGeneric : ShaderKeywordStrings.PaniniUnitDistance
             );
-
-            RenderingUtils.Blit(cmd, source, destination, material, 0, m_UseDrawProcedural, RenderBufferLoadAction.DontCare);
+            Blitter.BlitCameraTexture(cmd, source, destination, RenderBufferLoadAction.DontCare, RenderBufferStoreAction.Store, material, 0);
         }
 
         Vector2 CalcViewExtents(Camera camera)
