b9508aa3aeca5ce963f7926d6bf3e04fcddcdcd8
Tracy Chen <67800322+tracychen1234@users.noreply.github.com>
2022-02-07 10:39:19 -0800

    Add Absolute option for input A to the Power node

--- com.unity.shadergraph/Editor/Data/Nodes/Math/Basic/PowerNode.cs
+++ com.unity.shadergraph/Editor/Data/Nodes/Math/Basic/PowerNode.cs
@@ -1,4 +1,7 @@
 using System.Reflection;
+using UnityEditor.Graphing;
+using UnityEditor.ShaderGraph.Drawing.Controls;
+using UnityEngine;
 
 namespace UnityEditor.ShaderGraph
 {
@@ -9,10 +12,37 @@ namespace UnityEditor.ShaderGraph
         {
             name = "Power";
         }
+        [SerializeField]
+        bool m_AbsA = true;
+
+        [ToggleControl("Absolute A")]
+        public ToggleData absA
+        {
+            get { return new ToggleData(m_AbsA); }
+            set
+            {
+                if (m_AbsA == value.isOn)
+                    return;
+                m_AbsA = value.isOn;
+                Dirty(ModificationScope.Graph);
+
+            }
+        }
 
         protected override MethodInfo GetFunctionToConvert()
         {
-            return GetType().GetMethod("Unity_Power", BindingFlags.Static | BindingFlags.NonPublic);
+            MethodInfo powerFunction;
+            if (m_AbsA)
+            {
+                powerFunction = GetType().GetMethod("Unity_Power_Absolute", BindingFlags.Static | BindingFlags.NonPublic);
+            }
+            else
+            {
+                powerFunction = GetType().GetMethod("Unity_Power", BindingFlags.Static | BindingFlags.NonPublic);
+
+            }
+            return powerFunction;
+            //return GetType().GetMethod("Unity_Power_Absolute", BindingFlags.Static | BindingFlags.NonPublic);
         }
 
         static string Unity_Power(
@@ -24,6 +54,19 @@ namespace UnityEditor.ShaderGraph
 @"
 {
     Out = pow(A, B);
+}
+";
+        }
+
+        static string Unity_Power_Absolute(
+    [Slot(0, Binding.None, 0, 0, 0, 0)] DynamicDimensionVector A,
+    [Slot(1, Binding.None, 2, 2, 2, 2)] DynamicDimensionVector B,
+    [Slot(2, Binding.None)] out DynamicDimensionVector Out)
+        {
+            return
+@"
+{
+    Out = pow(abs(A), B);
 }
 ";
         }
