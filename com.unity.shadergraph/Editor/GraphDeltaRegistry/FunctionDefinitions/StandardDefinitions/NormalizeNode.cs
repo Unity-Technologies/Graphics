be67f964b0fabc1dc8f8aac3701a4b1a68d38878
Esmeralda Salamone <esmeralda.salamone@unity3d.com>
2022-02-10 15:06:27 -0800

    A few simple node definitions wrapping built-in functions.

---
+++ com.unity.shadergraph/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/NormalizeNode.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using static UnityEditor.ShaderGraph.Registry.Types.GraphType;
+
+namespace com.unity.shadergraph.defs
+{
+
+    internal class NormalizeNode : IStandardNode
+    {
+        public static FunctionDescriptor FunctionDescriptor => new(
+            1,              // Version
+            "Normalize",    // Name
+            "Out = normalize(In);",
+            new ParameterDescriptor("In", TYPE.Vector, Usage.In),
+            new ParameterDescriptor("Out", TYPE.Vector, Usage.Out)
+        );
+
+        public static Dictionary<string, string> UIStrings => new()
+        {
+            { "Name.Synonyms", "Normalize, Unitize" },
+            { "Tooltip", "Normalization Function" },
+            { "Parameters.In.Tooltip", "Input" },
+            { "Parameters.Out.Tooltip", "In / Length(In)" }
+        };
+    }
+}
---
+++ com.unity.shadergraph/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/PowerNode.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using static UnityEditor.ShaderGraph.Registry.Types.GraphType;
+
+namespace com.unity.shadergraph.defs
+{
+
+    internal class PowerNode : IStandardNode
+    {
+        public static FunctionDescriptor FunctionDescriptor => new(
+            1,          // Version
+            "Power",    // Name
+            "Out = pow(Base, Exp);",
+            new ParameterDescriptor("Base", TYPE.Any, Usage.In),
+            new ParameterDescriptor("Exp", TYPE.Any, Usage.In),
+            new ParameterDescriptor("Out", TYPE.Any, Usage.Out)
+        );
+
+        public static Dictionary<string, string> UIStrings => new()
+        {
+            { "Name.Synonyms", "Power, Exponentiation" },
+            { "Tooltip", "Power function" },
+            { "Parameters.Base.Tooltip", "Base" },
+            { "Parameters.Exp.Tooltip", "Exponent" },
+            { "Parameters.Out.Tooltip", "Base ^ Exp" }
+        };
+    }
+}
