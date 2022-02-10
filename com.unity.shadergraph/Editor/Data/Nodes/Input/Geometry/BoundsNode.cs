42089bafa4bfe5a88b679d238b86eb646a656485
alelievr <antoinel@unity3d.com>
2022-02-10 19:42:23 +0100

    Added space in bounds node

--- com.unity.shadergraph/Editor/Data/Nodes/Input/Geometry/BoundsNode.cs
+++ com.unity.shadergraph/Editor/Data/Nodes/Input/Geometry/BoundsNode.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
+using System.Linq;
 using System;
 using UnityEngine;
+using UnityEditor;
 using UnityEditor.Graphing;
 using UnityEditor.ShaderGraph;
 using UnityEditor.ShaderGraph.Drawing.Controls;
@@ -10,7 +12,7 @@ namespace UnityEditor.ShaderGraph
 {
     // TODO: disable this node for bultin
     [Title("Input", "Geometry", "Bounds")]
-    class BoundsNode : AbstractMaterialNode, IGeneratesBodyCode
+    class BoundsNode : AbstractMaterialNode, IGeneratesBodyCode, IMayRequireTangent, IMayRequireBitangent, IMayRequireNormal, IMayRequireTransform
     {
         public BoundsNode()
         {
@@ -18,10 +20,21 @@ namespace UnityEditor.ShaderGraph
             UpdateNodeAfterDeserialization();
         }
 
-        // public override string documentationURL => Documentation.GetPageLink("SGNode-Bounds");
+        [SerializeField]
+        CoordinateSpace m_Space;
 
-        public CoordinateSpace space;
-        // TODO: space option: world, object, view, etc.
+        [EnumControl]
+        public CoordinateSpace space
+        {
+            get { return m_Space; }
+            set
+            {
+                if (Equals(m_Space, value))
+                    return;
+                m_Space = value;
+                Dirty(ModificationScope.Graph);
+            }
+        }
 
         const int kBoundsMinOutputSlotId = 0;
         const int kBoundsMaxOutputSlotId = 1;
@@ -31,6 +44,8 @@ namespace UnityEditor.ShaderGraph
         const string kBoundsSizeOutputSlotName = "Size";
 
         public override bool hasPreview { get { return false; } }
+
+        internal SpaceTransform spaceTransform => new SpaceTransform(CoordinateSpace.AbsoluteWorld, space, ConversionType.Position, false, sgVersion);
 
         public sealed override void UpdateNodeAfterDeserialization()
         {
@@ -46,12 +61,43 @@ namespace UnityEditor.ShaderGraph
 
         public void GenerateNodeCode(ShaderStringBuilder sb, GenerationMode generationMode)
         {
-            if (IsSlotConnected(kBoundsMinOutputSlotId))
-                sb.AppendLine($"$precision3 {GetSlotValue(kBoundsMinOutputSlotId, generationMode)} = unity_RendererBounds_Min;");
-            if (IsSlotConnected(kBoundsMaxOutputSlotId))
-                sb.AppendLine($"$precision3 {GetSlotValue(kBoundsMaxOutputSlotId, generationMode)} = unity_RendererBounds_Max;");
-            if (IsSlotConnected(kBoundsSizeOutputSlotId))
-                sb.AppendLine($"$precision3 {GetSlotValue(kBoundsSizeOutputSlotId, generationMode)} = unity_RendererBounds_Max - unity_RendererBounds_Min;");
+            if (generationMode == GenerationMode.ForReals)
+            {
+                if (IsSlotConnected(kBoundsMinOutputSlotId))
+                {
+                    string slot = GetSlotValue(kBoundsMinOutputSlotId, generationMode);
+                    sb.AddLine($"$precision3 {slot};");
+                    SpaceTransformUtil.GenerateTransformCodeStatement(spaceTransform, "unity_RendererBounds_Min.xyz", slot, sb);
+                }
+                if (IsSlotConnected(kBoundsMaxOutputSlotId))
+                {
+                    string slot = GetSlotValue(kBoundsMaxOutputSlotId, generationMode);
+                    sb.AddLine($"$precision3 {slot};");
+                    SpaceTransformUtil.GenerateTransformCodeStatement(spaceTransform, "unity_RendererBounds_Max.xyz", slot, sb);
+                }
+                if (IsSlotConnected(kBoundsSizeOutputSlotId))
+                {
+                    string slot = GetSlotValue(kBoundsSizeOutputSlotId, generationMode);
+                    sb.AddLine($"$precision3 {slot} = (unity_RendererBounds_Max - unity_RendererBounds_Min).xyz;");
+                }
+            }
+            else
+            {
+                if (IsSlotConnected(kBoundsMinOutputSlotId))
+                    sb.AddLine($"$precision3 {GetSlotValue(kBoundsMinOutputSlotId, generationMode)} = 0;");
+                if (IsSlotConnected(kBoundsMaxOutputSlotId))
+                    sb.AddLine($"$precision3 {GetSlotValue(kBoundsMaxOutputSlotId, generationMode)} = 0;");
+                if (IsSlotConnected(kBoundsSizeOutputSlotId))
+                    sb.AddLine($"$precision3 {GetSlotValue(kBoundsSizeOutputSlotId, generationMode)} = 0;");
+            }
         }
+        public NeededCoordinateSpace RequiresTangent(ShaderStageCapability stageCapability)
+            => spaceTransform.RequiresTangent;
+        public NeededCoordinateSpace RequiresBitangent(ShaderStageCapability stageCapability)
+            => spaceTransform.RequiresBitangent;
+        public NeededCoordinateSpace RequiresNormal(ShaderStageCapability stageCapability)
+            => spaceTransform.RequiresNormal;
+        public NeededTransform[] RequiresTransform(ShaderStageCapability stageCapability)
+            => spaceTransform.RequiresTransform.ToArray();
     }
 }
