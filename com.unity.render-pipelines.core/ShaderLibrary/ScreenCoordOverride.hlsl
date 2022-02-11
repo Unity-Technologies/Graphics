1fb1402707de63b638d319f1d5bf4e57fbcd96bb
etienne cella <etienne.p.cella@gmail.com>
2022-02-11 14:22:15 -0500

    add comment

--- com.unity.render-pipelines.core/ShaderLibrary/ScreenCoordOverride.hlsl
+++ com.unity.render-pipelines.core/ShaderLibrary/ScreenCoordOverride.hlsl
@@ -11,6 +11,7 @@ float2 ScreenCoordRemoveScaleBias(float2 xy, float4 screenCoordScaleBias)
     return (xy - screenCoordScaleBias.zw) / screenCoordScaleBias.xy;
 }
 
+// Note that SCREEN_SIZE_OVERRIDE will be redefined in HDRP to use _PostProcessScreenSize.
 #if defined(SCREEN_COORD_OVERRIDE)
     #define SCREEN_COORD_APPLY_SCALEBIAS(xy)          ScreenCoordApplyScaleBias(xy, _ScreenCoordScaleBias)
     #define SCREEN_COORD_REMOVE_SCALEBIAS(xy)         ScreenCoordRemoveScaleBias(xy, _ScreenCoordScaleBias)
