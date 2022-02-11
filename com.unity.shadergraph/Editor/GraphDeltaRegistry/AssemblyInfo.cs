1703fdfb4f5e7ce98a4168e6014ee845d9d4c594
Esmeralda Salamone <esmeralda.salamone@unity3d.com>
2022-02-11 12:37:34 -0800

    Revert "move standard definitions to a separate assembly."
    This reverts commit 7644f3b25b12140f7f4e5f7c74ef654d1e73968d.

--- com.unity.shadergraph/Editor/GraphDeltaRegistry/AssemblyInfo.cs
+++ com.unity.shadergraph/Editor/GraphDeltaRegistry/AssemblyInfo.cs
@@ -1,4 +1,3 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Unity.ShaderGraph.HeadlessPreview.Tests")]
-[assembly: InternalsVisibleTo("Unity.ShaderGraph.StandardDefinitions")]
