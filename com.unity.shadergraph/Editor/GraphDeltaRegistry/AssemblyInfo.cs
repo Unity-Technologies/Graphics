7644f3b25b12140f7f4e5f7c74ef654d1e73968d
Esmeralda Salamone <esmeralda.salamone@unity3d.com>
2022-02-11 10:50:35 -0800

    move standard definitions to a separate assembly.

--- com.unity.shadergraph/Editor/GraphDeltaRegistry/AssemblyInfo.cs
+++ com.unity.shadergraph/Editor/GraphDeltaRegistry/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Unity.ShaderGraph.HeadlessPreview.Tests")]
+[assembly: InternalsVisibleTo("Unity.ShaderGraph.StandardDefinitions")]
