f57ce72c6e9f6ced7363857afa854b83142e71c1
Fabien Houlmann <fabien@unity3d.com>
2022-02-17 13:15:38 -0500

    Restore deleted obsolete fields and tag them as errors instead

--- com.unity.render-pipelines.universal/Runtime/UniversalRenderPipelineCore.cs
+++ com.unity.render-pipelines.universal/Runtime/UniversalRenderPipelineCore.cs
@@ -219,6 +219,9 @@ namespace UnityEngine.Rendering.Universal
         internal XRPass xr;
         internal XRPassUniversal xrUniversal => xr as XRPassUniversal;
 
+        [Obsolete("Please use xr.enabled instead.", true)]
+        public bool isStereoEnabled;
+
         public float maxShadowDistance;
         public bool postProcessEnabled;
 
@@ -523,6 +526,20 @@ namespace UnityEngine.Rendering.Universal
                 throw new ArgumentNullException("camera");
 
             return camera.cameraType == CameraType.Game || camera.cameraType == CameraType.VR;
+        }
+
+        /// <summary>
+        /// Checks if a camera is rendering in stereo mode.
+        /// </summary>
+        /// <param name="camera">Camera to check state from.</param>
+        /// <returns>Returns true if the given camera is rendering in stereo mode, false otherwise.</returns>
+        [Obsolete("Please use CameraData.xr.enabled instead.", true)]
+        public static bool IsStereoEnabled(Camera camera)
+        {
+            if (camera == null)
+                throw new ArgumentNullException("camera");
+
+            return IsGameCamera(camera) && (camera.stereoTargetEye == StereoTargetEyeMask.Both);
         }
 
         /// <summary>
