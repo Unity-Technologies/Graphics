e2e7e15055a809ee25cc3b0a39400faae248cb0e
Julien Amsellem <julien.amsellem@unity3d.com>
2022-02-17 21:16:53 +0100

    Removed unused "using"

--- com.unity.visualeffectgraph/Editor/GraphView/Views/VFXView.cs
+++ com.unity.visualeffectgraph/Editor/GraphView/Views/VFXView.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.ObjectModel;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
 using System.Reflection;
