a28b8b1e75fbb13f6ff7088b281ca4e68b27a446
joseph.savell <joseph.savell@unity3d.com>
2022-02-18 15:46:48 -0800

    Integrate float and int parts with graph data

--- com.unity.shadergraph/Editor/GraphUI/GraphElements/ModelUI/NodeFieldParts/IntPart.cs
+++ com.unity.shadergraph/Editor/GraphUI/GraphElements/ModelUI/NodeFieldParts/IntPart.cs
@@ -1,4 +1,6 @@
 using UnityEditor.GraphToolsFoundation.Overdrive;
+using UnityEditor.ShaderGraph.Registry;
+using UnityEditor.ShaderGraph.Registry.Types;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -9,9 +11,13 @@ namespace UnityEditor.ShaderGraph.GraphUI
         const string k_IntPartTemplate = "NodeFieldParts/IntPart";
         const string k_IntFieldName = "sg-int-field";
 
-        public IntPart(string name, IGraphElementModel model, IModelUI ownerElement, string parentClassName)
-            : base(name, model, ownerElement, parentClassName) { }
+        public IntPart(string name, IGraphElementModel model, IModelUI ownerElement, string parentClassName, string portName)
+            : base(name, model, ownerElement, parentClassName)
+        {
+            m_PortName = portName;
+        }
 
+        string m_PortName;
         VisualElement m_Root;
         IntegerField m_IntegerField;
         public override VisualElement Root => m_Root;
@@ -24,8 +30,8 @@ namespace UnityEditor.ShaderGraph.GraphUI
             m_IntegerField = m_Root.Q<IntegerField>(k_IntFieldName);
             m_IntegerField.RegisterValueChangedCallback(change =>
             {
-                // TODO: write
-                Debug.Log($"Int is now {change.newValue}");
+                if (m_Model is not GraphDataNodeModel graphDataNodeModel) return;
+                m_OwnerElement.View.Dispatch(new SetGraphTypeValueCommand(graphDataNodeModel, m_PortName, (GraphType.Length)1, (GraphType.Height)1, new float[] { change.newValue }));
             });
 
             parent.Add(m_Root);
@@ -33,7 +39,12 @@ namespace UnityEditor.ShaderGraph.GraphUI
 
         protected override void UpdatePartFromModel()
         {
-            // TODO: read
+            if (m_Model is not GraphDataNodeModel model) return;
+            if (!model.TryGetNodeReader(out var nodeReader)) return;
+            if (!nodeReader.TryGetPort(m_PortName, out var portReader)) return;
+
+            if (!portReader.GetField("c0", out float value)) value = 0;
+            m_IntegerField.value = (int) value;
         }
     }
 }
