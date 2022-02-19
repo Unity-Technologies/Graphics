60ca45325578f6e6d5ed9773c84dc81ebb91c68f
Tracy Chen <67800322+tracychen1234@users.noreply.github.com>
2022-02-18 18:31:44 -0800

    group 4

---
+++ com.unity.shadergraph/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/TruncateNode.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using static UnityEditor.ShaderGraph.Registry.Types.GraphType;
+
+namespace com.unity.shadergraph.defs
+{
+
+    internal class TruncateNode : IStandardNode
+    {
+        public static FunctionDescriptor FunctionDescriptor => new(
+            1,
+            "Truncate",
+            "Out = trunc(In);",
+            new ParameterDescriptor("In", TYPE.Any, Usage.In),
+            new ParameterDescriptor("Out", TYPE.Any, Usage.Out)
+        );
+
+        public static Dictionary<string, string> UIStrings => new()
+        {
+            { "Tooltip", "returns the integer, or whole number, component of the input" },
+            { "Parameters.In.Tooltip", "input value" },
+            { "Parameters.Out.Tooltip", "the whole number of the input without the decimal portion" },
+            { "Category", "Math, Round" }
+        };
+    }
+}
