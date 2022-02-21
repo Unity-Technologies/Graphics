fab7ad0bc9d2214c6f14815478fe1357de283ee7
Rémi Chapelain <remi.chapelain@unity3d.com>
2022-02-21 22:11:24 +0100

    Minor reword

--- com.unity.render-pipelines.high-definition/Runtime/PostProcessing/Components/AutoLensFlare.cs
+++ com.unity.render-pipelines.high-definition/Runtime/PostProcessing/Components/AutoLensFlare.cs
@@ -7,29 +7,29 @@ namespace UnityEngine.Rendering.HighDefinition
     {
         [Header("General")]
         /// <summary>
-        /// Controls the global intensity of the effect.
+        /// Controls the global intensity of the Lens Flare effect.
         /// </summary>
-        [Tooltip("Controls the global intensity of the lens flare effect.")]
+        [Tooltip("Controls the global intensity of the Lens Flare effect.")]
         public ClampedFloatParameter intensityMultiplier = new ClampedFloatParameter(1f, 0f, 8f);
         /// <summary>
-        /// Controls the intensity of the opposite lens flare.
+        /// Controls the intensity of the opposite Lens Flare. Those flares are sampled using scaled and inverted screen coordinates.
         /// </summary>
-        [Tooltip("Controls the intensity of the opposite lens flare. Those flares are sampled using scaled and inverted screen coordinates.")]
+        [Tooltip("Controls the intensity of the opposite Lens Flare. Those flares are sampled using scaled and inverted screen coordinates.")]
         public ClampedFloatParameter firstFlareIntensity = new ClampedFloatParameter(0.75f, 0f, 1f);
         /// <summary>
-        /// Controls the intensity of the opposite lens flare.
+        /// Controls the intensity of the opposite Lens Flare.
         /// </summary>
-        [Tooltip("Controls the intensity of the opposite lens flare. Those flares are sampled using scaled screen coordinates.")]
+        [Tooltip("Controls the intensity of the opposite Lens Flare. Those flares are sampled using scaled screen coordinates.")]
         public ClampedFloatParameter secondaryFlareIntensity = new ClampedFloatParameter(0.5f, 0f, 1f);
         /// <summary>
-        /// Controls the intensity of the opposite lens flare.
+        /// Controls the intensity of the opposite Lens Flare.
         /// </summary>
-        [Tooltip("Controls the intensity of the warped lens flare. Those flares are sampled using polar UV coordinates.")]
+        [Tooltip("Controls the intensity of the warped Lens Flare. Those flares are sampled using polar UV coordinates.")]
         public ClampedFloatParameter warpedFlareIntensity = new ClampedFloatParameter(0.25f, 0f, 1f);
         /// <summary>
-        /// Controls the intensity of the opposite lens flare.
+        /// Controls the intensity of the vignette effect to occlude the Lens Flare effect at the center of the screen
         /// </summary>
-        [Tooltip("Controls the intensity of the vignette effect to occlude the lens flare effect at the center of the screen.")]
+        [Tooltip("Controls the intensity of the vignette effect to occlude the Lens Flare effect at the center of the screen.")]
         public ClampedFloatParameter vignetteIntensity = new ClampedFloatParameter(1f, 0f, 1f);
 
 
@@ -38,9 +38,9 @@ namespace UnityEngine.Rendering.HighDefinition
 
         [Header("Chromatic Abberation")]
         /// <summary>
-        /// Specifies a Texture which HDRP uses to shift the hue of chromatic aberrations.
+        /// Specifies a Texture which HDRP uses to shift the hue of chromatic aberrations. If null, HDRP creates a default texture.
         /// </summary>
-        [Tooltip("Specifies a Texture which HDRP uses to shift the hue of chromatic aberrations.")]
+        [Tooltip("Specifies a Texture which HDRP uses to shift the hue of chromatic aberrations. If null, HDRP creates a default texture.")]
         public Texture2DParameter spectralLut = new Texture2DParameter(null);
         /// <summary>
         /// Controls the strength of the Chromatic Aberration effect. The higher the value, the more light is dispersed on the sides of the screen
@@ -48,9 +48,9 @@ namespace UnityEngine.Rendering.HighDefinition
         [Tooltip("Controls the strength of the Chromatic Aberration effect. The higher the value, the more light is dispersed on the sides of the screen.")]
         public ClampedFloatParameter chromaticAbberationIntensity = new ClampedFloatParameter(0.5f, 0f, 1f);
         /// <summary>
-        /// Controls the maximum number of samples HDRP uses to render the effect. A lower sample number results in better performance.
+        /// Controls the number of samples HDRP uses to render the effect. A lower sample number results in better performance.
         /// </summary>
-        [Tooltip("Controls the maximum number of samples HDRP uses to render the effect. A lower sample number results in better performance.")]
+        [Tooltip("Controls the number of samples HDRP uses to render the effect. A lower sample number results in better performance.")]
         public ClampedIntParameter chromaticAbberationSampleCount = new ClampedIntParameter(6, 2, 24);
 
 
