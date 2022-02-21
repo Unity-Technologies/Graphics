c5de42dfe34deaf8379b86a16451ba92069bd945
Paul Demeulenaere <pauld@unity3d.com>
2022-02-21 08:29:46 +0100

    Fix build after automatic merge
    `using UnityEditor.Graphing;` was listed twice

--- com.unity.render-pipelines.high-definition/Editor/Material/ShaderGraph/HDSubTarget.cs
+++ com.unity.render-pipelines.high-definition/Editor/Material/ShaderGraph/HDSubTarget.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 using UnityEngine.Rendering.HighDefinition;
 using UnityEditor.ShaderGraph;
 using UnityEditor.ShaderGraph.Internal;
-using UnityEditor.Graphing;
 using UnityEditor.ShaderGraph.Legacy;
 using UnityEditor.Rendering.HighDefinition.ShaderGraph.Legacy;
 using UnityEditor.VFX;
