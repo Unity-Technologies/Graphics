c6838db2cc2321390b6cb2409dd18450b7175a9e
Brett Groshong <brett.groshong@unity3d.com>
2022-02-22 15:14:54 -0800

    Bring back DefaultParameters as a variadic parameter list.

--- com.unity.shadergraph/Editor/GraphDeltaRegistry/FunctionDefinitions/ParameterDescriptor.cs
+++ com.unity.shadergraph/Editor/GraphDeltaRegistry/FunctionDefinitions/ParameterDescriptor.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using static UnityEditor.ShaderGraph.Registry.Types.GraphType;
 
 namespace com.unity.shadergraph.defs
@@ -18,15 +19,18 @@ namespace com.unity.shadergraph.defs
         public string Name { get; }  // Must be a valid reference name
         public TypeDescriptor TypeDescriptor { get; }
         public Usage Usage { get; }
+        public IReadOnlyCollection<float> DefaultValue { get; }
 
         public ParameterDescriptor(
             string name,
             TypeDescriptor typeDescriptor,
-            Usage usage)
+            Usage usage,
+            params float[] defaultValue)
         {
             Name = name;
             TypeDescriptor = typeDescriptor;
             Usage = usage;
+            DefaultValue = defaultValue.ToList();
         }
     }
 }
