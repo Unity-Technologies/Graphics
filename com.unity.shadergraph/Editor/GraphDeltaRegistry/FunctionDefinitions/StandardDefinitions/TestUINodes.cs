3c6e7652573a0dcdfd90a7712b8f51c950588f68
joseph.savell <joseph.savell@unity3d.com>
2022-02-22 16:26:37 -0800

    Update test nodes for inclusion in staging branch

--- com.unity.shadergraph/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/TestUINodes.cs
+++ com.unity.shadergraph/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/TestUINodes.cs
@@ -1,47 +1,45 @@
-// TODO: For testing static UI parts - remove before merging
-
 using UnityEditor.ShaderGraph.Registry.Types;
 
 namespace com.unity.shadergraph.defs
 {
-    internal class TempMat3Node : IStandardNode
+    internal class TestUIMat3Node : IStandardNode
     {
         public static FunctionDescriptor FunctionDescriptor => new(
             1, // Version
-            "TempMat3", // Name
+            "TestUIMat3", // Name
             "Out = float4(In[0][0], In[1][1], In[2][2], 1.0);",
             new ParameterDescriptor("In", TYPE.Mat3, GraphType.Usage.Static),
             new ParameterDescriptor("Out", TYPE.Vec4, GraphType.Usage.Out)
         );
     }
 
-    internal class TempMat4Node : IStandardNode
+    internal class TestUIMat4Node : IStandardNode
     {
         public static FunctionDescriptor FunctionDescriptor => new(
             1, // Version
-            "TempMat4", // Name
+            "TestUIMat4", // Name
             "Out = float4(In[0][0], In[1][1], In[2][2], In[3][3]);",
             new ParameterDescriptor("In", TYPE.Mat4, GraphType.Usage.Static),
             new ParameterDescriptor("Out", TYPE.Vec4, GraphType.Usage.Out)
         );
     }
 
-    internal class TempIntNode : IStandardNode
+    internal class TestUIIntNode : IStandardNode
     {
         public static FunctionDescriptor FunctionDescriptor => new(
             1, // Version
-            "TempInt", // Name
+            "TestUIInt", // Name
             "Out = float4(In, In, In, In);",
             new ParameterDescriptor("In", TYPE.Int, GraphType.Usage.Static),
             new ParameterDescriptor("Out", TYPE.Vec4, GraphType.Usage.Out)
         );
     }
 
-    internal class TempFloatNode : IStandardNode
+    internal class TestUIFloatNode : IStandardNode
     {
         public static FunctionDescriptor FunctionDescriptor => new(
             1, // Version
-            "TempFloat", // Name
+            "TestUIFloat", // Name
             "Out = float4(In, In, In, In);",
             new ParameterDescriptor("In", TYPE.Float, GraphType.Usage.Static),
             new ParameterDescriptor("Out", TYPE.Vec4, GraphType.Usage.Out)
