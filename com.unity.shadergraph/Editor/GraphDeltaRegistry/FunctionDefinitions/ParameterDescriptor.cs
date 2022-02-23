a201fcaa0cf0d2ca8b0c65ab95a2a499c6319ec0
Brett Groshong <96441566+brett-groshong@users.noreply.github.com>
2022-02-23 12:00:29 -0800

    Default Values in ParameterDescriptor (#7214)
    * Fill in default values in PDToField.
    
    * Bring back DefaultParameters as a variadic parameter list.
    
    * Change default parameters from a variadic param to a list.
    
    * Add unit test and doc for default values.

--- com.unity.shadergraph/Editor/GraphDeltaRegistry/FunctionDefinitions/ParameterDescriptor.cs
+++ com.unity.shadergraph/Editor/GraphDeltaRegistry/FunctionDefinitions/ParameterDescriptor.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using static UnityEditor.ShaderGraph.Registry.Types.GraphType;
 
 namespace com.unity.shadergraph.defs
@@ -9,24 +10,39 @@ namespace com.unity.shadergraph.defs
     ///   input (Usage.In) - the function receives this parameter
     ///   output (Usage.Out) - the function assigns this parameter
     ///   static (Usage.Static) - the parameter must be already defined
-    ///
     /// In registration (See: FunctionDescriptorNodeBuilder) ParameterDescriptors
     /// may create port/fields on a node.
+    ///
+    /// Basic Example
+    /// new ParameterDescriptor("A", TYPE.Any, Usage.In)
+    ///
+    /// Example with Default Value
+    /// new ParameterDescriptor("A", TYPE.Any, Usage.In, new float[] {1f, 0f, 0f, 0f})
     /// </summary>
     internal readonly struct ParameterDescriptor
     {
         public string Name { get; }  // Must be a valid reference name
         public TypeDescriptor TypeDescriptor { get; }
         public Usage Usage { get; }
+        public IReadOnlyCollection<float> DefaultValue { get; }
 
         public ParameterDescriptor(
             string name,
             TypeDescriptor typeDescriptor,
-            Usage usage)
+            Usage usage,
+            float[] defaultValue = null)
         {
             Name = name;
             TypeDescriptor = typeDescriptor;
             Usage = usage;
+            if (defaultValue == null)
+            {
+                DefaultValue = new List<float>();
+            }
+            else
+            {
+                DefaultValue = defaultValue.ToList();
+            }
         }
     }
 }
