e16f612e293adb65517c8b2feb2c6dfe4e1bfcee
Esmeralda Salamone <esmelusina@gmail.com>
2022-02-25 12:33:31 -0800

    Sg2/bugfix npe andnode (#7267)
    * fixed support for bool and int types on port icons and input fields.
    * updated test nodes to exercise these changes.
    
    Co-authored-by: Brett Groshong <brett.groshong@unity3d.com>
    Co-authored-by: Esmeralda Salamone <esmeralda.salamone@unity3d.com>

--- com.unity.shadergraph/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/TestUINodes.cs
+++ com.unity.shadergraph/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/TestUINodes.cs
@@ -29,8 +29,10 @@ namespace com.unity.shadergraph.defs
         public static FunctionDescriptor FunctionDescriptor => new(
             1, // Version
             "TestUIInt", // Name
-            "Out = In;",
-            new ParameterDescriptor("In", TYPE.Int, GraphType.Usage.Static),
+            "Local = In + Static; Out = Local;",
+            new ParameterDescriptor("In", TYPE.Int, GraphType.Usage.In),
+            new ParameterDescriptor("Static", TYPE.Int, GraphType.Usage.Static),
+            new ParameterDescriptor("Local", TYPE.Int, GraphType.Usage.Local),
             new ParameterDescriptor("Out", TYPE.Int, GraphType.Usage.Out)
         );
     }
@@ -40,8 +42,10 @@ namespace com.unity.shadergraph.defs
         public static FunctionDescriptor FunctionDescriptor => new(
             1, // Version
             "TestUIFloat", // Name
-            "Out = In;",
-            new ParameterDescriptor("In", TYPE.Float, GraphType.Usage.Static),
+            "Local = In + Static; Out = Local;",
+            new ParameterDescriptor("In", TYPE.Float, GraphType.Usage.In),
+            new ParameterDescriptor("Static", TYPE.Float, GraphType.Usage.Static),
+            new ParameterDescriptor("Local", TYPE.Float, GraphType.Usage.Local),
             new ParameterDescriptor("Out", TYPE.Float, GraphType.Usage.Out)
         );
     }
@@ -51,20 +55,48 @@ namespace com.unity.shadergraph.defs
         public static FunctionDescriptor FunctionDescriptor => new(
             1, // Version
             "TestUIBool", // Name
-            "Out = In;",
-            new ParameterDescriptor("In", TYPE.Bool, GraphType.Usage.Static),
+            "Local = In || Static; Out = Local;",
+            new ParameterDescriptor("In", TYPE.Bool, GraphType.Usage.In),
+            new ParameterDescriptor("Static", TYPE.Bool, GraphType.Usage.Static),
+            new ParameterDescriptor("Local", TYPE.Bool, GraphType.Usage.Local),
             new ParameterDescriptor("Out", TYPE.Bool, GraphType.Usage.Out)
         );
     }
 
-    internal class TestLocalVec4Node : IStandardNode
+    internal class TestUIVec2Node : IStandardNode
     {
         public static FunctionDescriptor FunctionDescriptor => new(
             1, // Version
-            "TestLocalVec4", // Name
+            "TestUIVec2", // Name
+            "Local = In + Static; Out = Local;",
+            new ParameterDescriptor("In", TYPE.Vec2, GraphType.Usage.In),
+            new ParameterDescriptor("Static", TYPE.Vec2, GraphType.Usage.Static),
+            new ParameterDescriptor("Local", TYPE.Vec2, GraphType.Usage.Local),
+            new ParameterDescriptor("Out", TYPE.Vec2, GraphType.Usage.Out)
+        );
+    }
+
+    internal class TestUIVec3Node : IStandardNode
+    {
+        public static FunctionDescriptor FunctionDescriptor => new(
+            1, // Version
+            "TestUIVec3", // Name
+            "Local = In + Static; Out = Local;",
+            new ParameterDescriptor("In", TYPE.Vec3, GraphType.Usage.In),
+            new ParameterDescriptor("Static", TYPE.Vec3, GraphType.Usage.Static),
+            new ParameterDescriptor("Local", TYPE.Vec3, GraphType.Usage.Local),
+            new ParameterDescriptor("Out", TYPE.Vec3, GraphType.Usage.Out)
+        );
+    }
+
+    internal class TestUIVec4Node : IStandardNode
+    {
+        public static FunctionDescriptor FunctionDescriptor => new(
+            1, // Version
+            "TestUIVec4", // Name
             "Local = In + Static; Out = Local;",
             new ParameterDescriptor("In", TYPE.Vec4, GraphType.Usage.In),
-            new ParameterDescriptor("Static", TYPE.Float, GraphType.Usage.Static),
+            new ParameterDescriptor("Static", TYPE.Vec4, GraphType.Usage.Static),
             new ParameterDescriptor("Local", TYPE.Vec4, GraphType.Usage.Local),
             new ParameterDescriptor("Out", TYPE.Vec4, GraphType.Usage.Out)
         );
