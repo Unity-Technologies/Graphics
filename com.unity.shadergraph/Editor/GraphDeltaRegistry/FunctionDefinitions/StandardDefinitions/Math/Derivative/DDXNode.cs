8be89589b42b675125616a988b4ac1f33945668a
bencloward <88061215+bencloward@users.noreply.github.com>
2022-03-02 09:51:07 -0600

    A Set of Bonus Nodes!
    I found these nodes in our lists for future sprints and decided to implement them now.

---
+++ com.unity.shadergraph/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/Math/Derivative/DDXNode.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using static UnityEditor.ShaderGraph.Registry.Types.GraphType;
+
+namespace com.unity.shadergraph.defs
+{
+
+    internal class DDXNode : IStandardNode
+    {
+        public static FunctionDescriptor FunctionDescriptor => new(
+            1,          // Version
+            "DDX",    // Name
+            "Out = ddx(In);",
+            new ParameterDescriptor("In", TYPE.Vector, Usage.In),
+            new ParameterDescriptor("Out", TYPE.Vector, Usage.Out)
+        );
+
+        public static Dictionary<string, string> UIStrings => new()
+        {
+            { "Name.Synonyms", "derivative, slope" },
+            { "Tooltip", "returns the partial derivative of the input with respect to the screen-space x-coordinate" },
+            { "Parameters.In.Tooltip", "input value" },
+            { "Parameters.Out.Tooltip", "the difference between the value of the current pixel and the horizontal neighboring pixel" },
+            { "Category", "Math, Derivative" }
+        };
+    }
+}
