f44f3118b27c34250ec9897a675ad890dd090a7f
Scott--Anderson <scott.anderson@unity3d.com>
2022-03-18 13:58:14 -0700

    rename leak multiplier to leak mitigation for consistency

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/ProbeVolume/DynamicGI/ProbeDynamicGI.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/ProbeVolume/DynamicGI/ProbeDynamicGI.cs
@@ -35,7 +35,7 @@ namespace UnityEngine.Rendering.HighDefinition
         [Tooltip("Advanced control for the SG sharpness used when propagating light")]
         public ClampedFloatParameter propagationSharpness = new ClampedFloatParameter(2.0f, 0.0f, 16.0f);
         [Tooltip("Advanced control for darkening down the indirect light coming from invalid probes")]
-        public ClampedFloatParameter leakMultiplier = new ClampedFloatParameter(1.0f, 0.0f, 1.0f);
+        public ClampedFloatParameter leakMitigation = new ClampedFloatParameter(1.0f, 0.0f, 1.0f);
         [Tooltip("Advanced control to bias the distance from the normal of the hit surface to perform direct lighting evaluation on")]
         public ClampedFloatParameter bias = new ClampedFloatParameter(0.05f, 0.0f, 0.33f);
         [Tooltip("Advanced control for how probes at volume boundaries propagate light from neighboring Probe Volumes.\n\nDisabled: No light is propagated across neighbors.\n\nSample Neighbors Direction Only: Samples all probe volumes once per probe and evaluates light from the resulting probe data for each axis.\n\nSample Neighbors Position and Direction: Samples all probe volumes for all propagation axes at each axes neighboring probe position.\nPotentially more accurate than Sample Neighbors Direction Only, but significantly more expensive.")]
@@ -49,7 +49,7 @@ namespace UnityEngine.Rendering.HighDefinition
         [Tooltip("Advanced control to clear all dynamic GI buffers in the event lighting blows up when tuning")]
         public BoolParameter clear = new BoolParameter(false);
 
-        
+
         [Serializable]
         public sealed class ProbeVolumeDynamicGIBasisParameter : VolumeParameter<ProbeVolumeDynamicGI.ProbeVolumeDynamicGIBasis>
         {
