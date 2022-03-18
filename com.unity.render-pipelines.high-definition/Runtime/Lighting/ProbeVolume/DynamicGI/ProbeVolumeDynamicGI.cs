a5debab00fc7aaff34c34c12354ee34cc8778240
Scott--Anderson <scott.anderson@unity3d.com>
2022-03-17 23:54:29 -0700

    Fix another crash in il2cpp build

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/ProbeVolume/DynamicGI/ProbeVolumeDynamicGI.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/ProbeVolume/DynamicGI/ProbeVolumeDynamicGI.cs
@@ -1081,7 +1081,7 @@ namespace UnityEngine.Rendering.HighDefinition
         {
             fixed (ProbeVolumeSimulationRequest* requestPtr = &_probeVolumeSimulationRequests[0])
             {
-                CoreUnsafeUtils.QuickSort<ProbeVolumeSimulationRequest>(_probeVolumeSimulationRequestCount, requestPtr);
+                CoreUnsafeUtils.QuickSort<ProbeVolumeSimulationRequest, ProbeVolumeSimulationRequest, ProbeVolumeSimulationRequest.ProbeVolumeSimulationRequestKeyGetter>(_probeVolumeSimulationRequestCount, requestPtr);
             }
 
             int maxSimulationsPerFrame;
@@ -1104,6 +1104,12 @@ namespace UnityEngine.Rendering.HighDefinition
             {
                 return other.simulationFrameDelta - simulationFrameDelta;
             }
+            
+            public struct ProbeVolumeSimulationRequestKeyGetter : CoreUnsafeUtils.IKeyGetter<ProbeVolumeSimulationRequest, ProbeVolumeSimulationRequest>
+            {
+                public ProbeVolumeSimulationRequest Get(ref ProbeVolumeSimulationRequest v) { return v; }
+            }
+            
         }
 
         struct NeighborAxisLookup : IComparable<NeighborAxisLookup>
