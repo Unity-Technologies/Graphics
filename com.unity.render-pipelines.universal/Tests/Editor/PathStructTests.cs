1c49c3c76a5fea557804e6629e12fd810dddac7d
Chris Chu <chris.chu@unity3d.com>
2022-03-26 11:12:05 +0800

    Updated tests for structs

--- com.unity.render-pipelines.universal/Tests/Editor/PathStructTests.cs
+++ com.unity.render-pipelines.universal/Tests/Editor/PathStructTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,16 @@ namespace UnityEngine.Rendering.Universal.Tests
     {
         const int k_TestListLen = 4;
 
+        struct DisposableStruct : IDisposable
+        {
+            static public int disposeCount;
+
+            public void Dispose()
+            {
+                disposeCount++;
+            }
+        }
+
         [Test]
         public void PathArray_CreateAndDestroy()
         {
@@ -18,6 +29,13 @@ namespace UnityEngine.Rendering.Universal.Tests
 
             pathArray0.Dispose();
             pathArray1.Dispose();
+
+            int startDisposes = DisposableStruct.disposeCount;
+            PathArray<DisposableStruct> pathArray2 = new PathArray<DisposableStruct>(10);
+            pathArray2.Dispose();
+            int endDisposes = DisposableStruct.disposeCount;
+
+            Assert.AreEqual(10, endDisposes - startDisposes);
         }
 
         [Test]
@@ -26,7 +44,7 @@ namespace UnityEngine.Rendering.Universal.Tests
             int[] expectedValues = new int[] { 0, 1, 2, 3 };
 
             PathArray<int> pathArray = new PathArray<int>(4);
-            for(int i=0;i < k_TestListLen; i++)
+            for (int i = 0; i < k_TestListLen; i++)
             {
                 pathArray[i] = expectedValues[i];
             }
@@ -39,7 +57,6 @@ namespace UnityEngine.Rendering.Universal.Tests
             pathArray.Dispose();
         }
 
-
         [Test]
         public void PathList_CreateAndDestroy()
         {
@@ -47,13 +64,98 @@ namespace UnityEngine.Rendering.Universal.Tests
             PathList<int> pathList1 = new PathList<int>();
             PathList<PathList<int>> pathList2 = new PathList<PathList<int>>();
 
+            PathList<int> pathList3 = new PathList<int>(100);
+            Assert.IsTrue(pathList3.Capacity == 100);
+
             pathList0.Dispose();
             pathList1.Dispose();
             pathList2.Dispose();
+            pathList3.Dispose();
         }
 
         [Test]
-        public void PathList_CheckAdd()
+        public void PathList_Capacity()
+        {
+            PathList<int> pathList0 = new PathList<int>();
+            int[] expectedValues = new int[] { 0, 1, 2, 3 };
+
+            for (int i = 0; i < k_TestListLen - 1; i++)
+                pathList0.Add(i);
+
+            pathList0.Capacity = 100;
+
+            pathList0.Add(3);
+
+            Assert.IsTrue(pathList0.Capacity == 100);
+
+            for (int i = 0; i < k_TestListLen; i++)
+                Assert.IsTrue(pathList0[i] == expectedValues[i]);
+
+            pathList0.Dispose();
+        }
+
+        [Test]
+        public void PathList_TryAdjustCapacityRequirements()
+        {
+            PathList<int> pathList = new PathList<int>();
+
+            Assert.IsTrue(pathList.Capacity == 0);
+            pathList.TryAdjustCapacityRequirements();
+
+            Assert.IsTrue(pathList.Capacity == 1);
+            pathList.Add(0);
+
+            pathList.TryAdjustCapacityRequirements();
+            Assert.IsTrue(pathList.Capacity == 2);
+
+            pathList.Dispose();
+        }
+
+
+        [Test]
+        public void PathList_Count()
+        {
+            PathList<int> pathList0 = new PathList<int>();
+            int[] expectedValues = new int[] { 0, 1, 2, 3 };
+
+            for (int i = 0; i < k_TestListLen - 1; i++)
+                pathList0.Add(i);
+
+            pathList0.Capacity = 100;
+
+            pathList0.Add(3);
+
+            Assert.IsTrue(pathList0.Count == 4);
+
+            pathList0.Dispose();
+        }
+
+
+        [Test]
+        public void PathList_Clear()
+        {
+            int startDisposes = DisposableStruct.disposeCount;
+
+            PathList<DisposableStruct> pathList0 = new PathList<DisposableStruct>();
+
+            for (int i = 0; i < k_TestListLen - 1; i++)
+                pathList0.Add(new DisposableStruct());
+
+            pathList0.Capacity = 100;
+
+            pathList0.Add(new DisposableStruct());
+
+            pathList0.Clear();
+
+            int totalDisposes = DisposableStruct.disposeCount - startDisposes;
+
+            Assert.IsTrue(pathList0.Count == 0 && pathList0.Capacity == 100 && totalDisposes == k_TestListLen);
+
+            pathList0.Dispose();
+        }
+
+        [Test]
+        public void PathList_Add()
         {
             PathList<int> pathList = new PathList<int>();
             Assert.IsTrue(pathList.Capacity == 0);
@@ -74,7 +176,7 @@ namespace UnityEngine.Rendering.Universal.Tests
         }
 
         [Test]
-        public void PathList_CheckInsert()
+        public void PathList_Insert()
         {
             PathList<int> pathList = new PathList<int>();
 
@@ -95,7 +197,7 @@ namespace UnityEngine.Rendering.Universal.Tests
         }
 
         [Test]
-        public void PathList_CheckReverseSubset()
+        public void PathList_ReverseSubset()
         {
             const int k_TestListLen = 6;
 
@@ -107,18 +209,13 @@ namespace UnityEngine.Rendering.Universal.Tests
 
             pathList.Reverse(1, 4);
             for (int i = 0; i < k_TestListLen; i++)
-            {
-                int curValue = pathList[i];
-                Assert.IsTrue(curValue == expectedValues[i]);
-            }
+                Assert.IsTrue(pathList[i] == expectedValues[i]);
 
             pathList.Dispose();
         }
 
-
-
         [Test]
-        public void PathList_CheckReverse()
+        public void PathList_Reverse()
         {
             PathList<int> pathList = new PathList<int>();
             int[] expectedValues = new int[] { 3, 2, 1, 0 };
@@ -133,9 +230,134 @@ namespace UnityEngine.Rendering.Universal.Tests
                 Assert.IsTrue(curValue == expectedValues[i]);
             }
 
-                
-
             pathList.Dispose();
+        }
+
+        [Test]
+        public void PathList_CopyTo_PathList()
+        {
+            const int k_TestListLen = 6;
+            int[] expectedValues = new int[] { 0, 1, 1, 2, 3, 5 };
+
+            PathList<int> pathListSource = new PathList<int>(k_TestListLen);
+            PathList<int> pathListDest = new PathList<int>(k_TestListLen);
+
+            for (int i = 0; i < k_TestListLen; i++)
+            {
+                pathListSource[i] = i;
+                pathListDest[i] = i;
+            }
+
+            int[] array = new int[k_TestListLen];
+            pathListSource.CopyTo(1, pathListDest, 2, 3);
+            for (int i = 0; i < k_TestListLen; i++)
+            {
+                Assert.IsTrue(pathListDest[i] == expectedValues[i]);
+            }
+
+            pathListSource.Dispose();
+            pathListDest.Dispose();
+        }
+
+        [Test]
+        public void PathList_CopyTo_PathArray()
+        {
+            // Basic Copy
+            PathList<int>   pathListSource = new PathList<int>(k_TestListLen);
+            PathArray<int> pathArrayDest = new PathArray<int>(2 * k_TestListLen);
+
+            int[] expectedValues = new int[] { 10, 0, 1, 2, 3, 10, 10, 10 };
+
+            for (int i = 0; i < k_TestListLen; i++)
+                pathListSource[i] = i;
+
+            for (int i = 0; i < 2 * k_TestListLen; i++)
+                pathArrayDest[i] = 10;
+
+            pathListSource.CopyTo(pathArrayDest, 1);
+
+            for (int i = 0; i < 2 * k_TestListLen; i++)
+                Assert.IsTrue(pathArrayDest[i] == expectedValues[i]);
+
+            pathListSource.Dispose();
+            pathArrayDest.Dispose();
+        }
+
+        [Test]
+        public void PathList_CopyTo_PathList_Append()
+        {
+            int[] expectedValues = new int[] { 0, 1, 2, 3, 0, 1, 2, 3 };
+
+            PathList<int> pathListSource = new PathList<int>(k_TestListLen);
+            PathList<int> pathListDest = new PathList<int>(k_TestListLen);
+
+            for (int i = 0; i < k_TestListLen; i++)
+            {
+                pathListSource[i] = i;
+                pathListDest[i] = i;
+            }
+
+            pathListSource.CopyTo(0, pathListDest, k_TestListLen, k_TestListLen);
+
+            int[] array = new int[2 * k_TestListLen];
+            for (int i = 0; i < 2*k_TestListLen; i++)
+                array[i] = pathListDest[i];
+
+            for (int i = 0; i < k_TestListLen; i++)
+            {
+                Assert.IsTrue(pathListDest[i] == expectedValues[i]);
+                Assert.IsTrue(pathListDest[i+k_TestListLen] == expectedValues[i]);
+            }
+
+            pathListSource.Dispose();
+            pathListDest.Dispose();
+        }
+
+        [Test]
+        public void PathList_RemoveAt()
+        {
+            PathList<int> pathList0 = new PathList<int>();
+            PathList<int> pathList1 = new PathList<int>();
+            PathList<int> pathList2 = new PathList<int>();
+
+            int[] expectedValues0 = new int[] { 1, 2, 3};
+            int[] expectedValues1 = new int[] { 0, 1, 2 };
+            int[] expectedValues2 = new int[] { 0, 2, 3 };
+
+            for (int i = 0; i < k_TestListLen; i++)
+            {
+                pathList0.Add(i);
+                pathList1.Add(i);
+                pathList2.Add(i);
+            }
+
+            pathList0.RemoveAt(0);
+            pathList1.RemoveAt(3);
+            pathList2.RemoveAt(1);
+
+            Assert.IsTrue(pathList0.Count == k_TestListLen - 1);
+            Assert.IsTrue(pathList1.Count == k_TestListLen - 1);
+            Assert.IsTrue(pathList2.Count == k_TestListLen - 1);
+
+            for (int i=0;i < k_TestListLen-1;i++)
+            {
+                Assert.IsTrue(pathList0[i] == expectedValues0[i]);
+                Assert.IsTrue(pathList1[i] == expectedValues1[i]);
+                Assert.IsTrue(pathList2[i] == expectedValues2[i]);
+            }
+        }
+
+        [Test]
+        public void PathList_SharedData()
+        {
+            PathList<int> pathList0 = new PathList<int>(10);
+            PathList<int> pathList1 = pathList0;
+
+            pathList1.Capacity = 100;
+
+            int foo = pathList0.Capacity;
+
+            Assert.IsTrue(pathList0.Capacity == 100);
         }
 
     }
