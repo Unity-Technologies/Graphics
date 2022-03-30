9df88c000c6311a70195c36c851d79c4493ae819
Brett Groshong <brett.groshong@unity3d.com>
2022-03-30 08:55:21 -0700

    Add NodeUIDescriptors. (batch 4).

--- com.unity.shadergraph/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/Math/Trigonometry/CosineNode.cs
+++ com.unity.shadergraph/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/Math/Trigonometry/CosineNode.cs
@@ -3,23 +3,44 @@ using Usage = UnityEditor.ShaderGraph.GraphDelta.GraphType.Usage;
 
 namespace UnityEditor.ShaderGraph.Defs
 {
-
     internal class CosineNode : IStandardNode
     {
+        public static string Name = "Cosine";
+        public static int Version = 1;
+
         public static FunctionDescriptor FunctionDescriptor => new(
-            1,
-            "Cosine",
+            Version,
+            Name,
             "Out = cos(In);",
             new ParameterDescriptor("In", TYPE.Vector, Usage.In),
             new ParameterDescriptor("Out", TYPE.Vector, Usage.Out)
         );
 
+        public static NodeUIDescriptor NodeUIDescriptor => new(
+            Version,
+            Name,
+            tooltip: "returns the cosine of the input",
+            categories: new string[2] { "Math", "Trigonometry" },
+            synonyms: new string[0],
+            parameters: new ParameterUIDescriptor[2] {
+                new ParameterUIDescriptor(
+                    name: "In",
+                    tooltip: "input value"
+                ),
+                new ParameterUIDescriptor(
+                    name: "Out",
+                    tooltip: "the cosine of the input"
+                )
+            }
+        );
+
+
         public static Dictionary<string, string> UIStrings => new()
         {
             { "Tooltip", "returns the cosine of the input" },
+            { "Category", "Math, Trigonometry" },
             { "Parameters.In.Tooltip", "input value" },
-            { "Parameters.Out.Tooltip", "the cosine of the input" },
-            { "Category", "Math, Trigonometry" }
+            { "Parameters.Out.Tooltip", "the cosine of the input" }
         };
     }
 }
--- com.unity.shadergraph/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/Math/Vector/RejectionNode.cs
+++ com.unity.shadergraph/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/Math/Vector/RejectionNode.cs
@@ -3,16 +3,40 @@ using Usage = UnityEditor.ShaderGraph.GraphDelta.GraphType.Usage;
 
 namespace UnityEditor.ShaderGraph.Defs
 {
-
     internal class RejectionNode : IStandardNode
     {
+        public static string Name = "Rejection";
+        public static int Version = 1;
+
         public static FunctionDescriptor FunctionDescriptor => new(
-            1,     // Version
-            "Rejection", // Name
+            Version,
+            Name,
             "Out = A - (B * dot(A, B) / dot(B, B));",
             new ParameterDescriptor("A", TYPE.Vector, Usage.In),
             new ParameterDescriptor("B", TYPE.Vector, Usage.In),
             new ParameterDescriptor("Out", TYPE.Vector, Usage.Out)
+        );
+
+        public static NodeUIDescriptor NodeUIDescriptor => new(
+            Version,
+            Name,
+            tooltip: "returns the projection of input A onto the plane orthogonal, or perpendicular, to input B",
+            categories: new string[2] { "Math", "Vector" },
+            synonyms: new string[0],
+            parameters: new ParameterUIDescriptor[3] {
+                new ParameterUIDescriptor(
+                    name: "A",
+                    tooltip: "Input A"
+                ),
+                new ParameterUIDescriptor(
+                    name: "B",
+                    tooltip: "Input B"
+                ),
+                new ParameterUIDescriptor(
+                    name: "Out",
+                    tooltip: "the projection of A onto the plane orthogonal, or perpendicular, to B"
+                )
+            }
         );
 
         public static Dictionary<string, string> UIStrings => new()
