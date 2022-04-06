c5608f2e3c4e894c0c2f2232ff166b6a63c60432
Brett Groshong <brett.groshong@unity3d.com>
2022-04-06 10:18:58 -0700

    Remove UIHints and UIStrings from IStandardNode def.

--- com.unity.shadergraph/Editor/GraphDeltaRegistry/FunctionDefinitions/IStandardNode.cs
+++ com.unity.shadergraph/Editor/GraphDeltaRegistry/FunctionDefinitions/IStandardNode.cs
@@ -1,16 +1,11 @@
-using System.Collections.Generic;
 
 namespace UnityEditor.ShaderGraph.Defs
 {
-
     internal interface IStandardNode
     {
         static string Name { get; }
         static int Version { get; }
         static FunctionDescriptor FunctionDescriptor { get; }
         static NodeUIDescriptor NodeUIDescriptor { get; }
-        static Dictionary<string, string> UIStrings { get; }
-        static Dictionary<string, float> UIHints { get; }
     }
-
 }
