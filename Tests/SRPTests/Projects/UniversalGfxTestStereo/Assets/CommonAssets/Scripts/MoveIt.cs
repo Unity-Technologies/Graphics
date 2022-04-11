ccfc21800d43338d8c9a179d25562a55bcc4d526
Théo Penavaire <theo.penavaire@unity3d.com>
2022-04-11 17:00:04 +0200

    Restructure repo

---
+++ Tests/SRPTests/Projects/UniversalGfxTestStereo/Assets/CommonAssets/Scripts/MoveIt.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveIt : MonoBehaviour
+{
+    public float translateX = 0.0f;
+    public float translateY = 0.0f;
+    public float translateZ = 0.0f;
+
+    public float rotateX = 0.0f;
+    public float rotateY = 0.0f;
+    public float rotateZ = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotateX, rotateY, rotateZ);
+        transform.Translate(translateX, translateY, translateZ);
+    }
+}
