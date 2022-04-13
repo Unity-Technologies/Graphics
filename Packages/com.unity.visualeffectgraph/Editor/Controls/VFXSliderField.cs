3965a3a78ee9b935c38d1a21be1ff42c8d99e5cf
Julien Amsellem <julien.amsellem@unity3d.com>
2022-04-13 10:55:25 +0000

    Added an attribute to support logarithmic sliders
    POI: https://jira.unity3d.com/browse/RND-159
    
    In the VFX Graph nodes UI is defined with attributes. A new coming feature needs a **logarithmic** slider to cover a specific range of values (power of 2). So this PR adds a generic and reusable way to define a logarithmic slider.
    
    The purpose of a logarithmic slider is that the distance between each power of the base (10, 2 or even e) is constant.
    In the capture below the slider is setup with a logarithmic scale on base 2 with a minimum to 1 and maximum to 2048.
    We can see that the distance between 2^0 (1), 2^1 (2), 2^2 (4), 2^3 (8) ... 2^11 (2048) is constant.
    ![logarithmic-scale-base2](https://media.github.cds.internal.unity3d.com/user/4003/files/9c3075cd-6979-45f3-92ef-57325a9fd87e)

--- Packages/com.unity.visualeffectgraph/Editor/Controls/VFXSliderField.cs
+++ Packages/com.unity.visualeffectgraph/Editor/Controls/VFXSliderField.cs
@@ -1,127 +1,164 @@
 using System;
+
 using UnityEngine;
 using UnityEngine.UIElements;
-using UnityEditor.UIElements;
-using System.Collections.Generic;
-using System.Linq;
-
-using Action = System.Action;
 
 namespace UnityEditor.VFX.UI
 {
-    abstract class VFXBaseSliderField<T> : VisualElement, INotifyValueChanged<T>
+    public interface ISliderScale
     {
-        protected Slider m_Slider;
-        protected INotifyValueChanged<T> m_Field;
+        float ToLinear(float scaledValue);
+        float ToScaled(float linearValue);
+    }
 
+    class LinearSliderScale : ISliderScale
+    {
+        public float ToLinear(float scaledValue) => scaledValue;
+
+        public float ToScaled(float linearValue) => linearValue;
+    }
+
+    class LogarithmicSliderScale : ISliderScale
+    {
+        private readonly float m_Min;
+        private readonly float m_Max;
+        private readonly float m_MinValue;
+        private readonly float m_MinLogValue;
+        private readonly float m_MaxMinLogValue;
+        private readonly float m_Factor;
+        private readonly float m_InvLogBaseValue;
+        private readonly float m_Base;
+        private readonly bool m_SnapToPower;
+
+        public LogarithmicSliderScale(Vector2 range, float logBase = 10, bool snapToPower = false)
+        {
+            if (range.x <= 0)
+            {
+                throw new ArgumentException("Logarithmic scale does not support a minimum range less or equal to zero", nameof(range));
+            }
+
+            // Check that maximum value can be snapped to
+            if (snapToPower)
+            {
+                var expoMax = Math.Log10(range.y) / Math.Log10(logBase);
+                var expoMin = Math.Log10(range.x) / Math.Log10(logBase);
+
+                if (expoMax != Math.Round(expoMax) || expoMin != Math.Round(expoMin))
+                {
+                    throw new ArgumentException($"Logarithmic scale minimum and maximum values must be a power of the base when snapping is enabled ([{range.x}..{range.y}]");
+                }
+            }
+
+            m_Min = range.x;
+            m_Max = range.y;
+            m_Base = logBase;
+            m_InvLogBaseValue = 1f / (float)Math.Log10(logBase);
+            m_MinValue = range.x;
+            m_MinLogValue = (float)Math.Log10(m_MinValue) * m_InvLogBaseValue;
+            m_MaxMinLogValue = (float)Math.Log10(range.y/range.x) * m_InvLogBaseValue;
+            m_Factor = (range.y - range.x) / m_MaxMinLogValue;
+            m_SnapToPower = snapToPower;
+        }
+
+        public float ToLinear(float scaledValue)
+        {
+            scaledValue = Mathf.Clamp(scaledValue, m_Min, m_Max);
+            return m_MinValue + (float)Math.Log10(scaledValue/m_MinValue) * m_InvLogBaseValue * m_Factor;
+        }
+
+        public float ToScaled(float linearValue)
+        {
+            linearValue = Mathf.Clamp(linearValue, m_Min, m_Max);
+            float power = m_MinLogValue + (linearValue - m_MinValue) / m_Factor;
+            if (m_SnapToPower)
+                power = Mathf.Round(power);
+            return (float)Math.Pow(m_Base, power);
+        }
+    }
+
+    abstract class VFXBaseSliderField<T> : VisualElement, INotifyValueChanged<T>, IVFXDraggedElement
+    {
+        protected readonly Slider m_Slider;
+        protected readonly TextValueField<T> m_Field;
+
+        private static readonly LinearSliderScale s_LinearSliderScale = new LinearSliderScale();
+
+        private Action<IVFXDraggedElement> m_OnValueDragFinished;
+        private Action<IVFXDraggedElement> m_OnValueDragStarted;
+        private bool m_IgnoreNotification;
+        private ISliderScale m_Scale;
+        private T m_Value;
+        private Vector2 m_Range;
 
         class StartFinishSliderManipulator : Manipulator
         {
+            VisualElement m_DragContainer;
             VFXBaseSliderField<T> m_Slider;
-            bool m_InDrag;
+
+            public StartFinishSliderManipulator(VFXBaseSliderField<T> slider)
+            {
+                m_Slider = slider;
+            }
+
             protected override void RegisterCallbacksOnTarget()
             {
-                m_Slider = target.GetFirstOfType<VFXBaseSliderField<T>>();
-
-                target.RegisterCallback<MouseDownEvent>(OnMouseDown, TrickleDown.TrickleDown);
-                target.RegisterCallback<MouseUpEvent>(OnMouseUp, TrickleDown.TrickleDown);
+                target.RegisterCallback<MouseDownEvent>(OnMouseDown, TrickleDown.NoTrickleDown);
+                m_DragContainer = target.Q("unity-drag-container"); // Weakness: if UIToolkit change the internal structure of a slider this code could break
+                m_DragContainer.RegisterCallback<MouseUpEvent>(OnMouseUp, TrickleDown.TrickleDown);
             }
 
             protected override void UnregisterCallbacksFromTarget()
             {
                 target.UnregisterCallback<MouseDownEvent>(OnMouseDown, TrickleDown.TrickleDown);
-                target.UnregisterCallback<MouseUpEvent>(OnMouseUp, TrickleDown.TrickleDown);
+                m_DragContainer.UnregisterCallback<MouseUpEvent>(OnMouseUp, TrickleDown.TrickleDown);
             }
 
             void OnMouseDown(MouseDownEvent e)
             {
-                target.RegisterCallback<MouseMoveEvent>(OnMouseMove, TrickleDown.TrickleDown);
-            }
-
-            void OnMouseMove(MouseMoveEvent e)
-            {
-                if (!m_InDrag)
-                {
-                    m_InDrag = true;
-                    m_Slider.ValueDragStarted();
-                    target.UnregisterCallback<MouseMoveEvent>(OnMouseMove, TrickleDown.TrickleDown); //we care only about the first drag event
-                }
+                m_Slider.ValueDragStarted();
             }
 
             void OnMouseUp(MouseUpEvent e)
             {
-                if (m_InDrag)
-                    m_Slider.ValueDragFinished();
-                else
-                    target.UnregisterCallback<MouseMoveEvent>(OnMouseMove, TrickleDown.TrickleDown);
-                m_InDrag = false;
+                m_Slider.ValueDragFinished();
             }
         }
 
-        protected void ValueDragFinished()
+        protected VFXBaseSliderField(TextValueField<T> field, ISliderScale customScale = null)
         {
-            if (onValueDragFinished != null)
-                onValueDragFinished();
-        }
-
-        protected void ValueDragStarted()
-        {
-            if (onValueDragStarted != null)
-                onValueDragStarted();
-        }
-
-        public Action onValueDragFinished;
-        public Action onValueDragStarted;
-
-        public VFXBaseSliderField()
-        {
+            m_Slider = new Slider(0, 1, SliderDirection.Horizontal);
+            m_Slider.AddToClassList("textfield");
             AddToClassList("sliderField");
+            Add(m_Slider);
+
+            m_Field = field;
+            Add(m_Field);
+            RegisterCallBack();
+
+            scale = customScale ?? s_LinearSliderScale;
         }
 
-        protected void RegisterCallBack()
+        public ISliderScale scale
         {
-            (m_Field as VisualElement).RegisterCallback<BlurEvent>(OnFocusLost);
-
-            m_Slider.Children().First().AddManipulator(new StartFinishSliderManipulator());
+            get => m_Scale;
+            set => m_Scale = value ?? s_LinearSliderScale;
         }
 
-        void OnFocusLost(BlurEvent e)
+        public void SetValueWithoutNotify(T newValue)
         {
-            //forward the focus lost event
-            using (BlurEvent newE = BlurEvent.GetPooled(this, e.relatedTarget, e.direction, panel.focusController))
-            {
-                SendEvent(newE);
-            }
-
-            e.StopPropagation();
+            SetValueWithoutNotify(ValueToFloat(newValue), newValue);
         }
-
-        public T m_Value;
 
         public T value
         {
-            get
-            {
-                return m_Value;
-            }
+            get => m_Value;
 
-            set
-            {
-                SetValueAndNotify(value);
-            }
+            set => SetValueAndNotify(ValueToFloat(value), value);
         }
-
-        protected abstract float ValueToFloat(T value);
-
-
-        private Vector2 m_Range;
 
         public Vector2 range
         {
-            get
-            {
-                return m_Range;
-            }
+            get => m_Range;
             set
             {
                 m_Range = value;
@@ -140,181 +177,106 @@ namespace UnityEditor.VFX.UI
             }
         }
 
-        protected bool m_IgnoreNotification;
+        public void SetOnValueDragStarted(Action<IVFXDraggedElement> callback) => m_OnValueDragStarted = callback;
+        public void SetOnValueDragFinished(Action<IVFXDraggedElement> callback) => m_OnValueDragFinished = callback;
 
-        public abstract bool hasFocus { get; }
-        public void OnValueChanged(EventCallback<ChangeEvent<T>> callback)
+        private bool hasFocus => m_Field.HasFocus() || panel?.focusController.focusedElement == m_Field;
+
+        private void ValueDragFinished()
         {
-            RegisterCallback(callback);
+            m_OnValueDragFinished?.Invoke(this);
         }
 
-        public void RemoveOnValueChanged(EventCallback<ChangeEvent<T>> callback)
+        private void ValueDragStarted()
         {
-            UnregisterCallback(callback);
+            m_OnValueDragStarted?.Invoke(this);
         }
 
-        public void SetValueAndNotify(T newValue)
+        private void RegisterCallBack()
         {
-            if (!EqualityComparer<T>.Default.Equals(value, newValue))
+            m_Field.RegisterValueChangedCallback(ValueChanged);
+            m_Field.RegisterCallback<BlurEvent>(OnFocusLost);
+            m_Slider.RegisterValueChangedCallback(OnSliderValueChanged);
+            m_Slider.AddManipulator(new StartFinishSliderManipulator(this));
+        }
+
+        private void OnFocusLost(BlurEvent e)
+        {
+            //forward the focus lost event
+            using (BlurEvent newE = BlurEvent.GetPooled(this, e.relatedTarget, e.direction, panel.focusController))
             {
-                using (ChangeEvent<T> evt = ChangeEvent<T>.GetPooled(value, newValue))
+                SendEvent(newE);
+            }
+
+            e.StopPropagation();
+        }
+
+        private void OnSliderValueChanged(ChangeEvent<float> evt)
+        {
+            var scaledValue = m_Scale.ToScaled(evt.newValue);
+            SetValueAndNotify(Mathf.Clamp(evt.newValue, range.x, range.y), FloatToValue(scaledValue));
+        }
+
+        private void SetValueAndNotify(float sliderValue, T typedNewValue)
+        {
+            if (!value.Equals(typedNewValue))
+            {
+                using (var evt = ChangeEvent<T>.GetPooled(value, typedNewValue))
                 {
                     evt.target = this;
-                    SetValueWithoutNotify(newValue);
+                    SetValueWithoutNotify(sliderValue, typedNewValue);
                     SendEvent(evt);
                 }
             }
         }
 
-        public void SetValueWithoutNotify(T newValue)
+        private void SetValueWithoutNotify(float sliderValue, T newTypedValue)
         {
             m_IgnoreNotification = true;
-            m_Value = newValue;
-            tooltip = newValue.ToString();
+            m_Value = newTypedValue;
+            tooltip = newTypedValue.ToString();
             if (!hasFocus)
-                m_Field.value = newValue;
-            m_Slider.value = ValueToFloat(value);
+                m_Field.value = newTypedValue;
+            m_Slider.value = sliderValue;
             m_IgnoreNotification = false;
         }
 
-        protected void ValueChanged(ChangeEvent<T> e)
+        private void ValueChanged(ChangeEvent<T> e)
         {
             e.StopPropagation();
             if (!m_IgnoreNotification)
-                SetValueAndNotify(e.newValue);
+                SetValueAndNotify(ValueToFloat(e.newValue), e.newValue);
+        }
+
+        private float ValueToFloat(T v)
+        {
+            var scaledValue = (float)Convert.ChangeType(v, typeof(float));
+            return scale.ToLinear(scaledValue);
+        }
+
+        private T FloatToValue(float v)
+        {
+            return (T)Convert.ChangeType(v, typeof(T));
         }
     }
+
     class VFXFloatSliderField : VFXBaseSliderField<float>
     {
-        public VFXFloatSliderField()
-        {
-            m_Slider = new Slider(0, 1, SliderDirection.Horizontal);
-            m_Slider.AddToClassList("textfield");
-            m_Slider.RegisterValueChangedCallback(evt => ValueChanged(evt.newValue));
+        public VFXFloatSliderField() : base(CreateField()) { }
 
-            m_FloatField = new FloatField();
-            m_FloatField.RegisterValueChangedCallback(ValueChanged);
-            m_FloatField.name = "Field";
-            m_Field = m_FloatField;
-
-            m_IndeterminateLabel = new Label()
-            {
-                name = "indeterminate",
-                text = VFXControlConstants.indeterminateText
-            };
-            m_IndeterminateLabel.SetEnabled(false);
-
-            Add(m_Slider);
-            Add(m_FloatField);
-            RegisterCallBack();
-        }
-
-        VisualElement m_IndeterminateLabel;
-        FloatField m_FloatField;
-
-        public override bool hasFocus
-        {
-            get { return ((FloatField)m_Field).HasFocus() || (panel != null && panel.focusController.focusedElement == m_Field as VisualElement); }
-        }
-
-        protected override float ValueToFloat(float value)
-        {
-            return value;
-        }
-
-        void ValueChanged(float newValue)
-        {
-            if (!m_IgnoreNotification)
-                SetValueAndNotify(newValue);
-        }
-
-        public bool indeterminate
-        {
-            get { return m_FloatField.parent == null; }
-
-            set
-            {
-                if (indeterminate != value)
-                {
-                    if (value)
-                    {
-                        m_FloatField.RemoveFromHierarchy();
-                        Add(m_IndeterminateLabel);
-                    }
-                    else
-                    {
-                        m_IndeterminateLabel.RemoveFromHierarchy();
-                        Add(m_FloatField);
-                    }
-                    m_Slider.SetEnabled(!value);
-                }
-            }
-        }
+        private static FloatField CreateField() => new FloatField { name = "Field" };
     }
+
     class VFXIntSliderField : VFXBaseSliderField<int>
     {
-        public VFXIntSliderField()
-        {
-            m_Slider = new Slider(0, 1, SliderDirection.Horizontal);
-            m_Slider.AddToClassList("textfield");
-            m_Slider.RegisterValueChangedCallback(evt => ValueChanged(evt.newValue));
-
-            var integerField = new IntegerField();
-            integerField.RegisterValueChangedCallback(ValueChanged);
-            integerField.name = "Field";
-            m_Field = integerField;
-
-            Add(m_Slider);
-            Add(integerField);
-            RegisterCallBack();
-        }
-
-        public override bool hasFocus
-        {
-            get { return ((IntegerField)m_Field).HasFocus() || (panel != null && panel.focusController.focusedElement == m_Field as VisualElement); }
-        }
-
-        protected override float ValueToFloat(int value)
-        {
-            return (float)value;
-        }
-
-        void ValueChanged(float newValue)
-        {
-            SetValueAndNotify((int)newValue);
-        }
+        public VFXIntSliderField() : base(CreateField()) { }
+        private static IntegerField CreateField() => new IntegerField { name = "Field" };
     }
+
     class VFXLongSliderField : VFXBaseSliderField<long>
     {
-        public VFXLongSliderField()
-        {
-            m_Slider = new Slider(0, 1, SliderDirection.Horizontal);
-            m_Slider.AddToClassList("textfield");
-            m_Slider.RegisterValueChangedCallback(evt => ValueChanged(evt.newValue));
+        public VFXLongSliderField() : base(CreateField()) { }
 
-            var integerField = new LongField();
-            integerField.RegisterValueChangedCallback(ValueChanged);
-            integerField.name = "Field";
-            m_Field = integerField;
-
-            Add(m_Slider);
-            Add(integerField);
-            RegisterCallBack();
-        }
-
-        public override bool hasFocus
-        {
-            get { return ((LongField)m_Field).HasFocus() || (panel != null && panel.focusController.focusedElement == m_Field as VisualElement); }
-        }
-
-        protected override float ValueToFloat(long value)
-        {
-            return (float)value;
-        }
-
-        void ValueChanged(float newValue)
-        {
-            SetValueAndNotify((long)newValue);
-        }
+        private static LongField CreateField() => new LongField { name = "Field" };
     }
 }
