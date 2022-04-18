da7eac65688607a7dff7a997f59a33978cf9b795
Chris Chu <chris.chu@unity3d.com>
2022-04-18 15:15:57 +0800

    Updated  assemblyinfo and asmdefs

--- com.unity.render-pipelines.universal/Runtime/2D/AssemblyInfo.cs
+++ com.unity.render-pipelines.universal/Runtime/2D/AssemblyInfo.cs
@@ -1,3 +1,8 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.Universal.Editor")]
+[assembly: InternalsVisibleTo("Universal2DGraphicsTests")]
+[assembly: InternalsVisibleTo("Unity.RenderPipelines.Universal.Editor")]
+[assembly: InternalsVisibleTo("Unity.RenderPipelines.Universal.Editor.Tests")]
+[assembly: InternalsVisibleTo("Unity.RenderPipelines.Universal.Runtime.Tests")]
+[assembly: InternalsVisibleTo("Unity.GraphicTests.Performance.Universal.Runtime")]
