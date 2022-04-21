c257002e155c105fafde6cd8e9db4813633f83a7
weiweig <weiwei.gu@unity3d.com>
2022-04-22 00:48:37 +0800

    change get rawdata API

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.Distributed.Video.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.Distributed.Video.cs
@@ -234,7 +234,8 @@ namespace UnityEngine.Rendering.HighDefinition
                                        new ProfilingSampler($"Load Data {i} to Video Packet")))
                             {
                                 Datagram datagram = null;
-                                datagram = SocketServer.Instance.ReceiveReadyFrame(i);
+                                if(GetColorBuffer != null)
+                                    datagram = GetColorBuffer(i);
                                 if (datagram == null)
                                     continue;
 
@@ -243,8 +244,6 @@ namespace UnityEngine.Rendering.HighDefinition
                                 VideoDecoders[i].SetPacketData(datagram.data);
                                 VideoDecoders[i].FrameID = CurrentFrameID;
                                 VideoDecoders[i].SignalCodecThread();
-                                SocketServer.Instance.AddReceiveRingBuffer(i, Datagram.DatagramType.VideoFrame,
-                                    in datagram);
                             }
 
                             // Decoding process starts right after signalling the thread, no need for another pass here
