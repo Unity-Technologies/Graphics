68ebd5b3d2b39d36f10915c95e38f20090aa1229
joseph.savell <joseph.savell@unity3d.com>
2022-05-06 11:03:33 -0700

    Revert changes to ShaderGraphView.cs

--- com.unity.sg2/Editor/GraphUI/GraphElements/Views/ShaderGraphView.cs
+++ com.unity.sg2/Editor/GraphUI/GraphElements/Views/ShaderGraphView.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor.GraphToolsFoundation.Overdrive;
-using UnityEngine;
-using UnityEngine.GraphToolsFoundation.CommandStateObserver;
 using UnityEngine.UIElements;
 
 namespace UnityEditor.ShaderGraph.GraphUI
@@ -14,7 +12,9 @@ namespace UnityEditor.ShaderGraph.GraphUI
             BaseGraphTool graphTool,
             string graphViewName,
             GraphViewDisplayMode displayMode = GraphViewDisplayMode.Interactive)
-            : base(window, graphTool, graphViewName, displayMode) { }
+            : base(window, graphTool, graphViewName, displayMode)
+        {
+        }
 
         protected override void BuildContextualMenu(ContextualMenuPopulateEvent evt)
         {
@@ -22,31 +22,25 @@ namespace UnityEditor.ShaderGraph.GraphUI
             for (var i = 0; i < evt.menu.MenuItems().Count; ++i)
             {
                 var menuItem = evt.menu.MenuItems()[i];
-                if (menuItem is DropdownMenuAction {name: "Disable Nodes"})
+                if (menuItem is DropdownMenuAction { name: "Disable Nodes" })
                     evt.menu.RemoveItemAt(i);
             }
-
             evt.menu.AppendSeparator();
         }
 
-        public override void Dispatch(ICommand command, Diagnostics diagnosticsFlags = Diagnostics.None)
-        {
-            base.Dispatch(command, diagnosticsFlags | Diagnostics.LogAllCommands);
-        }
+    //    protected override void CollectCopyableGraphElements(
+    //        IEnumerable<IGraphElementModel> elements,
+    //        HashSet<IGraphElementModel> elementsToCopySet)
+    //    {
+    //        var elementsList = elements.ToList();
+    //        base.CollectCopyableGraphElements(elementsList, elementsToCopySet);
 
-        //    protected override void CollectCopyableGraphElements(
-        //        IEnumerable<IGraphElementModel> elements,
-        //        HashSet<IGraphElementModel> elementsToCopySet)
-        //    {
-        //        var elementsList = elements.ToList();
-        //        base.CollectCopyableGraphElements(elementsList, elementsToCopySet);
-
-        //        // Pasting a redirect should also paste an edge to its source node.
-        //        foreach (var redirect in elementsList.OfType<RedirectNodeModel>())
-        //        {
-        //            var incomingEdge = redirect.GetIncomingEdges().FirstOrDefault();
-        //            if (incomingEdge != null) elementsToCopySet.Add(incomingEdge);
-        //        }
-        //    }
+    //        // Pasting a redirect should also paste an edge to its source node.
+    //        foreach (var redirect in elementsList.OfType<RedirectNodeModel>())
+    //        {
+    //            var incomingEdge = redirect.GetIncomingEdges().FirstOrDefault();
+    //            if (incomingEdge != null) elementsToCopySet.Add(incomingEdge);
+    //        }
+    //    }
     }
 }
