73f90f4d88fc1651ee2a6637a356e8d53083319a
Ben Cloward <88061215+bencloward@users.noreply.github.com>
2022-05-13 18:30:08 -0500

    Oops, I missed a file (#7429)
    * Texture Nodes!
    
    * Updating my migration groups shader
    
    * Improving Formatting of shader code in all my nodes
    
    * Some additional code formatting changes
    
    * A few updates based on Tracy's feedback
    
    * New Referable Nodes
    
    plus a couple of new features added to cubemap sample nodes
    
    * removing these files from the PR
    
    * Added referables to previously existing nodes that needed them
    
    * Adding the transformation matrix node
    
    * Added View Vector node
    
    also updated migration groups shader graph
    
    * Fixed a couple of issues that Tracy pointed out
    
    * Added Some missing displayName tags
    
    Added Some missing displayName tags
    renamed the RadiansToDegrees file because it didn't end with "Node"
    
    * Fixed a typo that Tracy found
    
    * removed defaultValue tags from referables
    
    * oops, I forgot to push this file

--- com.unity.sg2/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/UV/PolarCoordinatesNode.cs
+++ com.unity.sg2/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/UV/PolarCoordinatesNode.cs
@@ -17,7 +17,7 @@ namespace UnityEditor.ShaderGraph.Defs
     angle = atan2(delta.x, delta.y) * 1.0/6.28 * LengthScale;
     Out.x = radius;
     Out.y = angle;",
-            new ParameterDescriptor("UV", TYPE.Vec2, Usage.In, defaultValue: REF.UV0),
+            new ParameterDescriptor("UV", TYPE.Vec2, Usage.In, REF.UV0),
             new ParameterDescriptor("Center", TYPE.Vec2, Usage.In, new float[] { 0.5f, 0.5f }),
             new ParameterDescriptor("RadialScale", TYPE.Float, Usage.In, new float[] { 1f }),
             new ParameterDescriptor("LengthScale", TYPE.Float, Usage.In, new float[] { 1f }),
@@ -46,10 +46,12 @@ namespace UnityEditor.ShaderGraph.Defs
                 ),
                 new ParameterUIDescriptor(
                     name: "RadialScale",
+                    displayName: "Radial Scale",
                     tooltip: "scale of distance value"
                 ),
                 new ParameterUIDescriptor(
                     name: "LengthScale",
+                    displayName: "Length Scale",
                     tooltip: "scale of angle value"
                 ),
                 new ParameterUIDescriptor(
