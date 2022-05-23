e394b31e143dc2e2a314f48d9f884990427eb361
Adrien de Tocqueville <adrien.tocqueville@unity3d.com>
2022-05-23 11:26:58 +0000

    Apply fog on sky reflections
    https://fogbugz.unity3d.com/f/cases/1357622/
    
    Apply fog when sampling sky texture for reflections. Doesn't apply volumetric fog.
    Currently, fog is applied in screenspace after rendering, so the sky env texture doesn't contain fog.
    SSR correctly reflects fog since it samples the rendered texture, but sampling the cubemap doesn't
    So we should apply fog between pixel position and skybox when sampling the sky to have a consistent look between reflections.
    But applying volumetric fog requires sampling the vbuffer which is kind of expensive
    
    Following screenshot have a black plane with smoothness = 1
    SSR is enabled, which is why some part of the reflection looks correct
    
    |   | Master | Branch |
    | ------------- | ------------- | ------------- |
    | Fog only  | ![image](https://user-images.githubusercontent.com/61282776/150821819-b87f613e-85f9-4ddc-afef-31df7b7715c6.png )  | ![image](https://user-images.githubusercontent.com/61282776/150822029-f4b319f2-301c-4f85-84f5-60f485d46f99.png ) |
    | Fog + Volumetric Fog  | ![image](https://user-images.githubusercontent.com/61282776/150822047-9c186ada-a9ec-476d-862d-e5e5b6c3153a.png )  | ![image](https://user-images.githubusercontent.com/61282776/150822055-63df5807-ea31-49e7-ac0c-cb0d0d3a8175.png ) |

--- Packages/com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/LightLoop.hlsl
+++ Packages/com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/LightLoop.hlsl
@@ -196,6 +196,9 @@ void LightLoop( float3 V, PositionInputs posInput, PreLightData preLightData, BS
     context.shadowValue      = 1;
     context.sampleReflection = 0;
     context.splineVisibility = -1;
+#ifdef APPLY_FOG_ON_SKY_REFLECTIONS
+    context.positionWS       = posInput.positionWS;
+#endif
 
     // With XR single-pass and camera-relative: offset position to do lighting computations from the combined center view (original camera matrix).
     // This is required because there is only one list of lights generated on the CPU. Shadows are also generated once and shared between the instanced views.
