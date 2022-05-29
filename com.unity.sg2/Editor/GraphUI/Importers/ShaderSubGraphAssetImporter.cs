67c65a2b20dfad4bb9d85c7e52a452123b819134
Esmeralda Salamone <esmeralda.salamone@unity3d.com>
2022-05-28 18:00:38 -0700

    remove some usings

--- com.unity.sg2/Editor/GraphUI/Importers/ShaderSubGraphAssetImporter.cs
+++ com.unity.sg2/Editor/GraphUI/Importers/ShaderSubGraphAssetImporter.cs
@@ -1,9 +1,4 @@
-using System.IO;
-using System.Text;
 using UnityEditor.AssetImporters;
-using UnityEditor.ShaderGraph.Defs;
-using UnityEditor.ShaderGraph.Generation;
-using UnityEditor.ShaderGraph.GraphDelta;
 using UnityEditor.ShaderGraph.GraphUI;
 using UnityEngine;
 
