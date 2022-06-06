0ff445ccc80f9789207c38c520ee88aa00e1a83c
joseph.savell <joseph.savell@unity3d.com>
2022-06-06 15:24:35 -0700

    Add tests for new commands

--- com.unity.sg2/Tests/GraphUI/SubGraphTests.cs
+++ com.unity.sg2/Tests/GraphUI/SubGraphTests.cs
@@ -1,16 +1,18 @@
 using System.Collections;
-using System.Linq;
+using System.Reflection;
 using NUnit.Framework;
 using UnityEditor.GraphToolsFoundation.Overdrive;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.UIElements;
 using Assert = UnityEngine.Assertions.Assert;
 
 namespace UnityEditor.ShaderGraph.GraphUI.UnitTests
 {
     public class SubGraphTests : BaseGraphWindowTest
     {
-        protected override string testAssetPath =>  $"Assets\\{ShaderGraphStencil.DefaultSubGraphAssetName}.{ShaderGraphStencil.SubGraphExtension}";
+        protected override string testAssetPath => $"Assets\\{ShaderGraphStencil.DefaultSubGraphAssetName}.{ShaderGraphStencil.SubGraphExtension}";
+        ModelInspectorView m_InspectorView;
 
         [SetUp]
         public override void SetUp()
@@ -26,6 +28,21 @@ namespace UnityEditor.ShaderGraph.GraphUI.UnitTests
             m_Window.GraphTool.Update();
 
             m_Window.Focus();
+
+            FindInspectorView();
+        }
+
+        private void FindInspectorView()
+        {
+            const string viewFieldName = "m_InspectorView";
+
+            var found = m_Window.TryGetOverlay(k_InspectorOverlayId, out var inspectorOverlay);
+            Assert.IsTrue(found, "Inspector overlay was not found");
+
+            m_InspectorView = (ModelInspectorView)inspectorOverlay.GetType()
+                .GetField(viewFieldName, BindingFlags.NonPublic | BindingFlags.Instance)?
+                .GetValue(inspectorOverlay);
+            Assert.IsNotNull(m_InspectorView, "Inspector view was not found");
         }
 
         [Test]
@@ -46,11 +63,28 @@ namespace UnityEditor.ShaderGraph.GraphUI.UnitTests
         {
             yield return AddNodeFromSearcherAndValidate("Add");
             yield return SaveAndReopenGraph();
+
             // Wait till the graph model is loaded back up
             while (m_Window.GraphView.GraphModel == null)
                 yield return null;
 
             Assert.IsTrue(FindNodeOnGraphByName("Add"));
+        }
+
+        [UnityTest]
+        public IEnumerator TestSubgraphOutputHasEditor()
+        {
+            const string outputNodeName = "DefaultContextDescriptor";
+            const string outputInspectorListName = "sg-subgraph-output-list";
+
+            var output = GetNodeModelFromGraphByName(outputNodeName);
+            Assert.IsNotNull(output);
+
+            m_GraphView.Dispatch(new SelectElementsCommand(SelectElementsCommand.SelectionMode.Replace, output));
+            yield return null;
+
+            var list = m_InspectorView.Q<ListView>(outputInspectorListName);
+            Assert.IsNotNull(list, "Subgraph output node should display a list in the inspector");
         }
     }
 }
