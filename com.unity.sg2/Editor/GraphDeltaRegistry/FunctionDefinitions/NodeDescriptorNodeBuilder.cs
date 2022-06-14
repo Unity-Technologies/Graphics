7562b5957b736d8cd713bc46cf1f9e560a3e9efc
Brett Groshong <brett.groshong@unity3d.com>
2022-06-14 11:51:45 -0700

    Add shader entries for dependent functions.

--- com.unity.sg2/Editor/GraphDeltaRegistry/FunctionDefinitions/NodeDescriptorNodeBuilder.cs
+++ com.unity.sg2/Editor/GraphDeltaRegistry/FunctionDefinitions/NodeDescriptorNodeBuilder.cs
@@ -4,6 +4,7 @@ using System.Text.RegularExpressions;
 using UnityEditor.ShaderFoundry;
 using UnityEditor.ShaderGraph.GraphDelta;
 using UnityEngine;
+using static UnityEditor.ShaderGraph.GraphDelta.GraphType;
 
 namespace UnityEditor.ShaderGraph.Defs
 {
@@ -28,9 +29,9 @@ namespace UnityEditor.ShaderGraph.Defs
         {
             m_nodeDescriptor = nodeDescriptor; // copy
 
-            // If there are no functions in nodeDescriptor:
-            // - leave m_defaultFunction == null
-            // - leave m_nameToFunction == null
+            // If there are no functions in nodeDescriptor
+            //   leave m_defaultFunction == null
+            //   leave m_nameToFunction == null
             if (m_nodeDescriptor.Functions.Count < 1)
             {
                 var msg = $"BuildNode called for NodeDescriptor with no defined functions: {m_nodeDescriptor.Name}";
@@ -129,9 +130,81 @@ namespace UnityEditor.ShaderGraph.Defs
             }
         }
 
+        private static ShaderType GetShaderTypeForParametricTypeDescriptor(ShaderContainer container, ParametricTypeDescriptor resolvedType)
+        {
+            var height = resolvedType.Height;
+            var length = resolvedType.Length;
+            var primitive = resolvedType.Primitive;
+            var precision = resolvedType.Precision;
+
+            int l = Mathf.Clamp((int)length, 1, 4);
+            int h = Mathf.Clamp((int)height, 1, 4);
+
+            string name = "float";
+
+            switch (primitive)
+            {
+                case Primitive.Bool: name = "bool"; break;
+                case Primitive.Int: name = "int"; break;
+                case Primitive.Float:
+                    switch (precision)
+                    {
+                        case Precision.Fixed: name = "float"; break;
+                        case Precision.Half: name = "half"; break;
+                    }
+                    break;
+            }
+
+            var shaderType = ShaderType.Scalar(container, name);
+            if (h != 1 && l != 1)
+            {
+                shaderType = ShaderType.Matrix(container, shaderType, l, h);
+            }
+            else if (h != 1 || l != 1)
+            {
+                shaderType = ShaderType.Vector(container, shaderType, Mathf.Max(l, h));
+            }
+            return shaderType;
+        }
+
+        private static ShaderType ShaderTypeForParameter(
+            ShaderContainer container,
+            ParameterDescriptor parameter,
+            ParametricTypeDescriptor fallbackType)
+        {
+            switch (parameter.TypeDescriptor)
+            {
+                case ParametricTypeDescriptor paramType:
+                    ParametricTypeDescriptor resolvedType = new(
+                        paramType.Precision == GraphType.Precision.Any ? fallbackType.Precision : paramType.Precision,
+                        paramType.Primitive == GraphType.Primitive.Any ? fallbackType.Primitive : paramType.Primitive,
+                        paramType.Length == GraphType.Length.Any ? fallbackType.Length : paramType.Length,
+                        paramType.Height == GraphType.Height.Any ? fallbackType.Height : paramType.Height
+                    );
+                    return GetShaderTypeForParametricTypeDescriptor(container, resolvedType);
+                case SamplerStateTypeDescriptor:
+                    return container._UnitySamplerState;
+                case TextureTypeDescriptor textureType:
+                    return textureType.TextureType switch
+                    {
+                        BaseTextureType.TextureType.Texture3D => container._UnityTexture3D,
+                        BaseTextureType.TextureType.Texture2DArray => container._UnityTexture2DArray,
+                        BaseTextureType.TextureType.CubeMap => container._UnityTextureCube,
+                        _ => container._UnityTexture2D,
+                    };
+                case GradientTypeDescriptor:
+                    var gradientBuilder = new ShaderType.StructBuilder(container, "Gradient");
+                    gradientBuilder.DeclaredExternally();
+                    return gradientBuilder.Build();
+                default:
+                    throw new Exception($"Parameter ({parameter.Name}) does not have an equivalent shader type.");
+            }
+        }
+
         private ShaderFunction BuildShaderFunction(
             ShaderContainer container,
-            FunctionDescriptor functionDescriptor)
+            FunctionDescriptor functionDescriptor,
+            ParametricTypeDescriptor fallbackType)
         {
             // Get a shader function builder
             var shaderFunctionBuilder = new ShaderFunction.Builder(container, functionDescriptor.Name);
@@ -139,17 +212,14 @@ namespace UnityEditor.ShaderGraph.Defs
             // Set up the vars in the shader function.
             foreach (var param in functionDescriptor.Parameters)
             {
-                //var port = node.GetPort(param.Name);
-                //var field = port.GetTypeField();
-                //var shaderType = registry.GetShaderType(field, container);
-
-                if (param.Usage == GraphType.Usage.In ||
-                    param.Usage == GraphType.Usage.Static ||
-                    param.Usage == GraphType.Usage.Local)
+                var shaderType = ShaderTypeForParameter(container, param, fallbackType);
+                if (param.Usage == Usage.In ||
+                    param.Usage == Usage.Static ||
+                    param.Usage == Usage.Local)
                 {
                     shaderFunctionBuilder.AddInput(shaderType, param.Name);
                 }
-                else if (param.Usage == GraphType.Usage.Out)
+                else if (param.Usage == Usage.Out)
                 {
                     shaderFunctionBuilder.AddOutput(shaderType, param.Name);
                 }
@@ -190,7 +260,7 @@ namespace UnityEditor.ShaderGraph.Defs
 
             FunctionDescriptor selectedFunction = (FunctionDescriptor)m_defaultFunction;
 
-            // check node metadata for a selected function name
+            // find the selected function
             if (node.HasMetadata(SELECTED_FUNCTION_FIELD_NAME))
             {
                 string functionName = node.GetMetadata<string>(SELECTED_FUNCTION_FIELD_NAME);
@@ -200,10 +270,14 @@ namespace UnityEditor.ShaderGraph.Defs
                 }
             }
 
+            // determine the dynamic fallback type
+            var fallbackType = NodeBuilderUtils.FallbackTypeResolver(node);
+
+            // make shader functions for each internal function descriptor
             Dictionary<string, ShaderFunction> nameToShaderFunction = new();
             foreach (FunctionDescriptor fd in m_nodeDescriptor.Functions)
             {
-                ShaderFunction shaderFunction = BuildShaderFunction(container, fd);
+                ShaderFunction shaderFunction = BuildShaderFunction(container, fd, fallbackType);
                 nameToShaderFunction[fd.Name] = shaderFunction;
             }
 
