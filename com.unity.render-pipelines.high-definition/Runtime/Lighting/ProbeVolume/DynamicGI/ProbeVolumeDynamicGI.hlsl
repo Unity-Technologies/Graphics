3f2774edb94b7772503074b318769137e50ff12a
Andrew Saraev <andrew.saraev@unity3d.com>
2022-06-15 19:45:06 +0100

    Added YCoCg encoding for DGI radiance for testing.

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/ProbeVolume/DynamicGI/ProbeVolumeDynamicGI.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/ProbeVolume/DynamicGI/ProbeVolumeDynamicGI.hlsl
@@ -104,6 +104,8 @@ void UnpackIndicesAndValidityOnly(uint packedVal, out uint hitIndex, out float v
     hitIndex = (packedVal >> 8) & PROBE_VOLUME_NEIGHBOR_MAX_HIT_AXIS;
 }
 
+// LogLUV encoding
+
 const static float3x3 LOG_LUV_ENCODE_MAT = float3x3(
   0.2209, 0.3390, 0.4184,
   0.1138, 0.6780, 0.7319,
@@ -135,27 +137,100 @@ float3 LogLuvDecode(in float4 vLogLuv) {
     return max(vRGB, 0);
 }
 
+// YCoCg encoding
+
+float3 RgbToYcocg(const in float3 rgbColor) {
+    float g = rgbColor.g * 0.5;
+    float rb = rgbColor.r + rgbColor.b;
+
+    float3 ycocg;
+    ycocg.x = rb * 0.25 + g;
+    ycocg.y = 0.5 * (rgbColor.r - rgbColor.b);
+    ycocg.z = rb * -0.25 + g;
+
+    return ycocg;
+}
+
+float3 YcocgToRgb(const in float3 ycocgColor) {
+    float3 rgbColor;
+    float xz = ycocgColor.x - ycocgColor.z;
+
+    rgbColor.r = xz + ycocgColor.y;
+    rgbColor.g = ycocgColor.x + ycocgColor.z;
+    rgbColor.b = xz - ycocgColor.y;
+
+    return rgbColor;
+}
+
+// Radiance encoding
+
+#define RADIANCE_ENCODING 0
+
+uint ZeroRadiance()
+{
+#if RADIANCE_ENCODING == 0
+    return 0;
+#else
+    uint packedOutput = 0;
+    packedOutput |= (uint)round(0.5 * 255) << 8;
+    packedOutput |= (uint)round(0.5 * 255) << 16;
+    return packedOutput;
+#endif
+}
+
 uint PackRadiance(float3 color)
 {
+    uint packedOutput = 0;
+    
+#if RADIANCE_ENCODING == 0 // LogLUV
     float4 logLuv = LogLuvEncode(color);
 
-    uint packedOutput = 0;
     packedOutput |= min(255, (uint)round(logLuv.x * 255)) << 0;
     packedOutput |= min(255, (uint)round(logLuv.y * 255)) << 8;
     packedOutput |= min(255, (uint)round(logLuv.z * 255)) << 16;
     packedOutput |= min(255, (uint)round(logLuv.w * 255)) << 24;
+
+#else // YCoCg
+    float scale = max(1, max(max(color.r, color.g), color.b));
+    color /= scale;
+
+    float3 ycocg = RgbToYcocg(color);
+    ycocg.x *= scale;
+    ycocg.yz += 0.5;
+
+    packedOutput |= min(65535, (uint)round(ycocg.x * 255)) << 0;
+    packedOutput |= min(255, (uint)round(ycocg.y * 255)) << 16;
+    packedOutput |= min(255, (uint)round(ycocg.z * 255)) << 24;
+#endif
 
     return packedOutput;
 }
 
 float3 UnpackRadiance(uint packedVal)
 {
+#if RADIANCE_ENCODING == 0 // LogLUV
     float4 outVal;
     outVal.x = ((packedVal >> 0) & 255) / 255.0f;
     outVal.y = ((packedVal >> 8) & 255) / 255.0f;
     outVal.z = ((packedVal >> 16) & 255) / 255.0f;
     outVal.w = ((packedVal >> 24) & 255) / 255.0f;
-    return LogLuvDecode(outVal);
+    float3 color = LogLuvDecode(outVal);
+
+#else // YCoCg
+    float3 ycocg;
+    ycocg.x = ((packedVal >> 0) & 65535) / 255.0f;
+    ycocg.y = ((packedVal >> 16) & 255) / 255.0f;
+    ycocg.z = ((packedVal >> 24) & 255) / 255.0f;
+
+    float scale = max(1, ycocg.x);
+    ycocg.x /= scale;
+    ycocg.yz -= 0.5;
+
+    float3 color = YcocgToRgb(ycocg);
+    color *= scale;
+#endif
+
+    return color;
 }
 
 #endif // endof PROBE_VOLUME_DYNAMIC_GI
