e6664b3fb6ec59025c885493d941cc118d90f67d
Ben Cloward <88061215+bencloward@users.noreply.github.com>
2022-06-22 13:37:12 -0500

    Two-Function nodes (#7465)
    * Texture Nodes!
    
    * Updating my migration groups shader
    
    * Improving Formatting of shader code in all my nodes
    
    * Some additional code formatting changes
    
    * A few updates based on Tracy's feedback
    
    * New Referable Nodes
    
    plus a couple of new features added to cubemap sample nodes
    
    * removing these files from the PR
    
    * Added referables to previously existing nodes that needed them
    
    * Adding the transformation matrix node
    
    * Added View Vector node
    
    also updated migration groups shader graph
    
    * Fixed a couple of issues that Tracy pointed out
    
    * Added Some missing displayName tags
    
    Added Some missing displayName tags
    renamed the RadiansToDegrees file because it didn't end with "Node"
    
    * Fixed a typo that Tracy found
    
    * removed defaultValue tags from referables
    
    * oops, I forgot to push this file
    
    * Created Triplanar Node
    
    * Created the Calculate Level Of Detail Texture 2D Node
    
    * Updated based on Tracy's feedback
    
    * Updates based on Tracy's feedback
    
    * A couple of small fixes to shader nodes
    
    Renamed the Arccosine node's file
    Changed the data type for the power node's ports
    
    * Created a first pass at all of the nodes in the "two function" category
    
    * Fixing conflicts
    
    * Added parameter display names where they were missing and fixed a function name
    
    * Updated the commented out code to match what will be needed with Brett's additions.
    
    * latest updates
    
    * Fixed all include paths
    
    * Added main function names
    
    * Removed SG2Test
    
    * Fixing issues with the SampleTexture2D node
    
    * Updating nodes for correct output code
    
    * removing unrequired nodes from the PR
    
    * Updates to work-tracking shaders

---
+++ com.unity.sg2/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/UV/ParallaxMappingNode.cs
@@ -0,0 +1,131 @@
+using System.Collections.Generic;
+using Usage = UnityEditor.ShaderGraph.GraphDelta.GraphType.Usage;
+
+namespace UnityEditor.ShaderGraph.Defs
+{
+    internal class ParallaxMappingNode : IStandardNode
+    {
+        public static string Name = "ParallaxMapping";
+        public static int Version = 1;
+        public static NodeDescriptor NodeDescriptor => new(
+            Version,
+            Name,
+            "ParallaxMappingRed",
+            functions: new FunctionDescriptor[] {
+                new(
+                    "ParallaxMappingRed",
+                    "    ParallaxUVs = Heightmap.GetTransformedUV(UVs) + ParallaxMappingChannel(TEXTURE2D_ARGS(Heightmap.tex, HeightmapSampler.samplerstate), ViewDirectionTS, Amplitude * 0.01, Heightmap.GetTransformedUV(UVs), channel);",
+                    new ParameterDescriptor[]
+                    {
+                        new ParameterDescriptor("Heightmap", TYPE.Texture2D, Usage.In),
+                        new ParameterDescriptor("HeightmapSampler", TYPE.SamplerState, Usage.In),
+                        new ParameterDescriptor("Amplitude", TYPE.Float, Usage.In, new float[] {1}),
+                        new ParameterDescriptor("UVs", TYPE.Vec2, Usage.In, REF.UV0),
+                        new ParameterDescriptor("ParallaxUVs", TYPE.Vec2, Usage.Out),
+                        new ParameterDescriptor("channel", TYPE.Int, Usage.Local, new float[] {0}),
+                        new ParameterDescriptor("ViewDirectionTS", TYPE.Vec3, Usage.Local, REF.TangentSpace_ViewDirection)
+                    },
+                    new string[]
+                    {
+                        "\"Packages/com.unity.render-pipelines.core/ShaderLibrary/ParallaxMapping.hlsl\""
+                    }
+                ),
+                new(
+                    "ParallaxMappingGreen",
+                    "    ParallaxUVs = Heightmap.GetTransformedUV(UVs) + ParallaxMappingChannel(TEXTURE2D_ARGS(Heightmap.tex, HeightmapSampler.samplerstate), ViewDirectionTS, Amplitude * 0.01, Heightmap.GetTransformedUV(UVs), channel);",
+                    new ParameterDescriptor[]
+                    {
+                        new ParameterDescriptor("Heightmap", TYPE.Texture2D, Usage.In),
+                        new ParameterDescriptor("HeightmapSampler", TYPE.SamplerState, Usage.In),
+                        new ParameterDescriptor("Amplitude", TYPE.Float, Usage.In, new float[] {1}),
+                        new ParameterDescriptor("UVs", TYPE.Vec2, Usage.In, REF.UV0),
+                        new ParameterDescriptor("ParallaxUVs", TYPE.Vec2, Usage.Out),
+                        new ParameterDescriptor("channel", TYPE.Int, Usage.Local, new float[] {1f}),
+                        new ParameterDescriptor("ViewDirectionTS", TYPE.Vec3, Usage.Local, REF.TangentSpace_ViewDirection)
+                    },
+                    new string[]
+                    {
+                        "\"Packages/com.unity.render-pipelines.core/ShaderLibrary/ParallaxMapping.hlsl\""
+                    }
+                ),
+                new(
+                    "ParallaxMappingBlue",
+                    "    ParallaxUVs = Heightmap.GetTransformedUV(UVs) + ParallaxMappingChannel(TEXTURE2D_ARGS(Heightmap.tex, HeightmapSampler.samplerstate), ViewDirectionTS, Amplitude * 0.01, Heightmap.GetTransformedUV(UVs), channel);",
+                    new ParameterDescriptor[]
+                    {
+                        new ParameterDescriptor("Heightmap", TYPE.Texture2D, Usage.In),
+                        new ParameterDescriptor("HeightmapSampler", TYPE.SamplerState, Usage.In),
+                        new ParameterDescriptor("Amplitude", TYPE.Float, Usage.In, new float[] {1}),
+                        new ParameterDescriptor("UVs", TYPE.Vec2, Usage.In, REF.UV0),
+                        new ParameterDescriptor("ParallaxUVs", TYPE.Vec2, Usage.Out),
+                        new ParameterDescriptor("channel", TYPE.Int, Usage.Local, new float[] {2f}),
+                        new ParameterDescriptor("ViewDirectionTS", TYPE.Vec3, Usage.Local, REF.TangentSpace_ViewDirection)
+                    },
+                    new string[]
+                    {
+                        "\"Packages/com.unity.render-pipelines.core/ShaderLibrary/ParallaxMapping.hlsl\""
+                    }
+                ),
+                new(
+                    "ParallaxMappingAlpha",
+                    "    ParallaxUVs = Heightmap.GetTransformedUV(UVs) + ParallaxMappingChannel(TEXTURE2D_ARGS(Heightmap.tex, HeightmapSampler.samplerstate), ViewDirectionTS, Amplitude * 0.01, Heightmap.GetTransformedUV(UVs), channel);",
+                    new ParameterDescriptor[]
+                    {
+                        new ParameterDescriptor("Heightmap", TYPE.Texture2D, Usage.In),
+                        new ParameterDescriptor("HeightmapSampler", TYPE.SamplerState, Usage.In),
+                        new ParameterDescriptor("Amplitude", TYPE.Float, Usage.In, new float[] {1}),
+                        new ParameterDescriptor("UVs", TYPE.Vec2, Usage.In, REF.UV0),
+                        new ParameterDescriptor("ParallaxUVs", TYPE.Vec2, Usage.Out),
+                        new ParameterDescriptor("channel", TYPE.Int, Usage.Local, new float[] {3f}),
+                        new ParameterDescriptor("ViewDirectionTS", TYPE.Vec3, Usage.Local, REF.TangentSpace_ViewDirection)
+                    },
+                    new string[]
+                    {
+                        "\"Packages/com.unity.render-pipelines.core/ShaderLibrary/ParallaxMapping.hlsl\""
+                    }
+                )
+            }
+        );
+        public static NodeUIDescriptor NodeUIDescriptor => new(
+            Version,
+            Name,
+            displayName: "Parallax Mapping",
+            tooltip: "creates a parallax effect that displaces a Material's UVs to create the illusion of depth",
+            categories: new string[1] { "UV" },
+            synonyms: new string[1] { "offset mapping" },
+            hasPreview: false,
+            selectableFunctions: new()
+            {
+                { "ParallaxMappingRed", "Red" },
+                { "ParallaxMappingGreen", "Green" },
+                { "ParallaxMappingBlue", "Blue" },
+                { "ParallaxMappingAlpha", "Alpha" }
+            },
+            parameters: new ParameterUIDescriptor[5] {
+                new ParameterUIDescriptor(
+                    name: "Heightmap",
+                    tooltip: "the texture that specifies the depth of the displacement"
+                ),
+                new ParameterUIDescriptor(
+                    name: "HeightmapSampler",
+                    displayName:"Heightmap Sampler",
+                    tooltip: "the sampler to sample Heightmap with"
+                ),
+                new ParameterUIDescriptor(
+                    name: "Amplitude",
+                    tooltip: "a multiplier to apply to the height of the Heightmap (in centimeters)"
+                ),
+                new ParameterUIDescriptor(
+                    name: "UVs",
+                    tooltip: "the UVs that the sampler uses to sample the texture",
+                    options: REF.OptionList.UVs
+                ),
+                new ParameterUIDescriptor(
+                    name: "ParallaxUVs",
+                    displayName:"Parallax UVs",
+                    tooltip: "the UVs after adding the parallax offset"
+                )
+            }
+        );
+    }
+}
