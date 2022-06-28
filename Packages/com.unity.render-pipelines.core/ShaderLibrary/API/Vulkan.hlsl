82c5374a919ab770f5cb0ce43fc7d199bf15b9a1
Juho Oravainen <juhoo@unity3d.com>
2022-06-28 08:50:31 +0000

    Fix explicit precision texture declaration macros to emit Texture2D<half4> style syntax
    We have helper macros for declaring samplers in shaders in a cross-platform way. However, these macros have been using outdated method of magically named Texture2D_float/Texture2D_half that were historically used for propagating the sampler precision to the shader translation process. However, with that approach FXC will treat the samplers as float and therefore we cannot generate 100% accurate shader code being generated for mobile targets.
    
    This PR changes the macros to use Texture2D< half4 > style declarations which gives FXC all the needed information about the data types which therefore is able to add proper precision info on the sample operation and the following calculations on the data. This way the produced GLSL/Metal/Vulkan code will use correct precision for all of the processing.

--- Packages/com.unity.render-pipelines.core/ShaderLibrary/API/Vulkan.hlsl
+++ Packages/com.unity.render-pipelines.core/ShaderLibrary/API/Vulkan.hlsl
@@ -44,17 +44,17 @@
 #define TEXTURECUBE_ARRAY(textureName)        TextureCubeArray textureName
 #define TEXTURE3D(textureName)                Texture3D textureName
 
-#define TEXTURE2D_FLOAT(textureName)          Texture2D_float textureName
-#define TEXTURE2D_ARRAY_FLOAT(textureName)    Texture2DArray_float textureName
-#define TEXTURECUBE_FLOAT(textureName)        TextureCube_float textureName
-#define TEXTURECUBE_ARRAY_FLOAT(textureName)  TextureCubeArray_float textureName
-#define TEXTURE3D_FLOAT(textureName)          Texture3D_float textureName
+#define TEXTURE2D_FLOAT(textureName)          Texture2D<float4> textureName
+#define TEXTURE2D_ARRAY_FLOAT(textureName)    Texture2DArray<float4> textureName
+#define TEXTURECUBE_FLOAT(textureName)        TextureCube<float4> textureName
+#define TEXTURECUBE_ARRAY_FLOAT(textureName)  TextureCubeArray<float4> textureName
+#define TEXTURE3D_FLOAT(textureName)          Texture3D<float4> textureName
 
-#define TEXTURE2D_HALF(textureName)           Texture2D_half textureName
-#define TEXTURE2D_ARRAY_HALF(textureName)     Texture2DArray_half textureName
-#define TEXTURECUBE_HALF(textureName)         TextureCube_half textureName
-#define TEXTURECUBE_ARRAY_HALF(textureName)   TextureCubeArray_half textureName
-#define TEXTURE3D_HALF(textureName)           Texture3D_half textureName
+#define TEXTURE2D_HALF(textureName)           Texture2D<half4> textureName
+#define TEXTURE2D_ARRAY_HALF(textureName)     Texture2DArray<half4> textureName
+#define TEXTURECUBE_HALF(textureName)         TextureCube<half4> textureName
+#define TEXTURECUBE_ARRAY_HALF(textureName)   TextureCubeArray<half4> textureName
+#define TEXTURE3D_HALF(textureName)           Texture3D<half4> textureName
 
 #define TEXTURE2D_SHADOW(textureName)         TEXTURE2D(textureName)
 #define TEXTURE2D_ARRAY_SHADOW(textureName)   TEXTURE2D_ARRAY(textureName)
