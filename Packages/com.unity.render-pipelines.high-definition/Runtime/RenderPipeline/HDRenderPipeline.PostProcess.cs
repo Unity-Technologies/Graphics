81c734acfadc9b1eebf889b7db9fe4829cbb6c37
Sebastien Lagarde <sebastien@unity3d.com>
2022-07-06 15:32:42 +0000

    [Backport 2022.1] Various HDRP bugfixes (Version 3)
    Backport of https://github.cds.internal.unity3d.com/unity/unity/pull/9738
    
    [Fix Texture Field to accept only 2D Textures](https://github.cds.internal.unity3d.com/unity/unity/pull/8549)
    
    [Fixed the documentation for recursive rendering not being clear enough for the smoothness' behavior ](https://github.cds.internal.unity3d.com/unity/unity/pull/8643)
    
    [[HDRP][Path Tracing] Making sure there is no unwanted camera jittering](https://github.cds.internal.unity3d.com/unity/unity/pull/9419)
    
    [Update HDR in comparison docs #9086](https://github.cds.internal.unity3d.com/unity/unity/pull/9086)
    
    [[HDRP][Path Tracing] Facilitated support for Ray Tracing Light Cluster in Path Tracing](https://github.cds.internal.unity3d.com/unity/unity/pull/9064)
    
    [Update thick transmission docs](https://github.cds.internal.unity3d.com/unity/unity/pull/8568)
    
    [Remove check for TAA when checking if pyramid history is required.](https://github.cds.internal.unity3d.com/unity/unity/pull/8746)
    
    [[HDRP] Fix cloud layer overflow due to alpha blending](https://github.cds.internal.unity3d.com/unity/unity/pull/8565)
    
    [Force sync compilation on SMAA shaders](https://github.cds.internal.unity3d.com/unity/unity/pull/8555)
    
    [Fix Light Loop Performance Issue with HDRP Rendering Debugger](https://github.cds.internal.unity3d.com/unity/unity/pull/8496)
    
    [Fix Decal Scaling](https://github.cds.internal.unity3d.com/unity/unity/pull/8464)
    
    [Allow for null parameter for Texture2D and Texture3D parameters](https://github.cds.internal.unity3d.com/unity/unity/pull/8463)
    
    [Fixed the SSR not properly working on deferred with tiles with multiple variants (case 1399311).](https://github.cds.internal.unity3d.com/unity/unity/pull/8533)

--- Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.PostProcess.cs
+++ Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.PostProcess.cs
@@ -132,7 +132,6 @@ namespace UnityEngine.Rendering.HighDefinition
         ShadowsMidtonesHighlights m_ShadowsMidtonesHighlights;
         ColorCurves m_Curves;
         FilmGrain m_FilmGrain;
-        PathTracing m_PathTracing;
 
         // Prefetched frame settings (updated on every frame)
         bool m_StopNaNFS;
@@ -346,7 +345,6 @@ namespace UnityEngine.Rendering.HighDefinition
             m_ShadowsMidtonesHighlights = stack.GetComponent<ShadowsMidtonesHighlights>();
             m_Curves = stack.GetComponent<ColorCurves>();
             m_FilmGrain = stack.GetComponent<FilmGrain>();
-            m_PathTracing = stack.GetComponent<PathTracing>();
 
             // Prefetch frame settings - these aren't free to pull so we want to do it only once
             // per frame
@@ -367,7 +365,7 @@ namespace UnityEngine.Rendering.HighDefinition
             m_AntialiasingFS = frameSettings.IsEnabled(FrameSettingsField.Antialiasing) || camera.IsTAAUEnabled();
 
             // Override full screen anti-aliasing when doing path tracing (which is naturally anti-aliased already)
-            m_AntialiasingFS &= !m_PathTracing.enable.value;
+            m_AntialiasingFS &= !camera.IsPathTracingEnabled();
 
             // Sanity check, cant run dlss unless the pass is not null
             m_DLSSPassEnabled = m_DLSSPass != null && camera.IsDLSSEnabled();
@@ -558,7 +556,7 @@ namespace UnityEngine.Rendering.HighDefinition
 
                 source = DepthOfFieldPass(renderGraph, hdCamera, depthBuffer, motionVectors, depthBufferMipChain, source, depthMinMaxAvgMSAA, prepassOutput.stencilBuffer);
 
-                if (m_DepthOfField.IsActive() && m_SubFrameManager.isRecording && m_SubFrameManager.subFrameCount > 1 && !m_PathTracing.enable.value)
+                if (m_DepthOfField.IsActive() && m_SubFrameManager.isRecording && m_SubFrameManager.subFrameCount > 1 && !hdCamera.IsPathTracingEnabled())
                 {
                     RenderAccumulation(m_RenderGraph, hdCamera, source, source, false);
                 }
@@ -2915,7 +2913,7 @@ namespace UnityEngine.Rendering.HighDefinition
 
             // If Path tracing is enabled, then DoF is computed in the path tracer by sampling the lens aperure (when using the physical camera mode)
             bool isDoFPathTraced = (hdCamera.frameSettings.IsEnabled(FrameSettingsField.RayTracing) &&
-                hdCamera.volumeStack.GetComponent<PathTracing>().enable.value &&
+                hdCamera.IsPathTracingEnabled() &&
                 hdCamera.camera.cameraType != CameraType.Preview &&
                 m_DepthOfField.focusMode == DepthOfFieldMode.UsePhysicalCamera);
 
