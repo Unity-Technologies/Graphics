82c22e81b06255fd763cf5a0ceb4ef688c03c6f7
Joe S <joseph.savell@unity3d.com>
2022-07-25 11:32:02 -0700

    [SG2] Assign display names for Blackboard variables (#7560)
    * Pipe correct values through to display name field
    
    * Remove rename command handler (no longer needed)
    
    * Move Rename later in file for consistency

--- com.unity.sg2/Editor/GraphUI/EditorCommon/CommandStateObserver/ShaderGraphCommandOverrides.cs
+++ com.unity.sg2/Editor/GraphUI/EditorCommon/CommandStateObserver/ShaderGraphCommandOverrides.cs
@@ -232,27 +232,6 @@ namespace UnityEditor.ShaderGraph.GraphUI
             return deletedModels;
         }
 
-        public static void HandleGraphElementRenamed(
-            GraphViewStateComponent graphViewState,
-            PreviewManager previewManager,
-            RenameElementCommand renameElementCommand
-        )
-        {
-            // TODO: Handle Properties being renamed when those come online
-            //if (renameElementCommand.Model is IVariableDeclarationModel variableDeclarationModel)
-            //{
-            //    // React to property being renamed by finding all linked property nodes and marking them as requiring recompile and also needing constant value update
-            //    var graphNodes = graphViewState.GraphModel.NodeModels;
-            //    foreach (var graphNode in graphNodes)
-            //    {
-            //        if (graphNode is IVariableNodeModel variableNodeModel && Equals(variableNodeModel.VariableDeclarationModel, variableDeclarationModel))
-            //        {
-            //            previewManager.NotifyNodeFlowChanged(variableNodeModel);
-            //        }
-            //    }
-            //}
-        }
-
         public static void HandleUpdateConstantValue(
             UndoStateComponent undoState,
             GraphModelStateComponent graphModelState,
