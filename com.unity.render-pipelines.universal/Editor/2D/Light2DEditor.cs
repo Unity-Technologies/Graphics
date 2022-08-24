91aae96be3f07d9906a980e3b025c3b1dee584f6
Kenny Tan <kenny.tan@unity3d.com>
2022-08-24 15:43:26 +0800

    2d/shapecache (#2520)
    * [2d] Add Cache Mesh Data.
    
    * [2d] Ensure all Checks are tested.
    
    * [2d] Added Tests.
    
    * [2d] Added Comments.
    
    * [2d] Review Fixes.
    
    * [2d] Review Fixes.
    
    * [2d] Cleanup.
    
    * [2d] Indent Fix.
    
    * [2d] Review Fixes.
    
    * [2d] Review Fixes.
    
    * [2d] Add Changelog.
    
    * [2d] Fix Point Light Mesh.
    # Conflicts:
    #	com.unity.render-pipelines.universal/CHANGELOG.md

--- com.unity.render-pipelines.universal/Editor/2D/Light2DEditor.cs
+++ com.unity.render-pipelines.universal/Editor/2D/Light2DEditor.cs
@@ -42,7 +42,7 @@ namespace UnityEditor.Experimental.Rendering.Universal
                 for (var i = 0; i < shapeEditor.pointCount; ++i)
                     pointsProperty.GetArrayElementAtIndex(i).vector3Value = shapeEditor.GetPoint(i).position;
 
-                ((Light2D)(serializedObject.targetObject)).UpdateMesh();
+                ((Light2D)(serializedObject.targetObject)).UpdateMesh(true);
 
                 // This is untracked right now...
                 serializedObject.ApplyModifiedProperties();
@@ -642,7 +642,7 @@ namespace UnityEditor.Experimental.Rendering.Universal
             if (serializedObject.ApplyModifiedProperties())
             {
                 if(meshChanged)
-                    lightObject.UpdateMesh();
+                    lightObject.UpdateMesh(true);
             }
         }
 
