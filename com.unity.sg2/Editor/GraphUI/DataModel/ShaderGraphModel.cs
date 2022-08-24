4cc5ba4e6a56ba00ebee5032b9e72ba7e1a2f30d
Sai Narayan <nightmask3@gmail.com>
2022-08-23 18:17:38 -0700

    caching main context node

--- com.unity.sg2/Editor/GraphUI/DataModel/ShaderGraphModel.cs
+++ com.unity.sg2/Editor/GraphUI/DataModel/ShaderGraphModel.cs
@@ -129,6 +129,8 @@ namespace UnityEditor.ShaderGraph.GraphUI
 
         internal string DefaultContextName => Registry.ResolveKey<ShaderGraphContext>().Name;
 
+        internal GraphDataContextNodeModel m_DefaultContextNode = null;
+
         [NonSerialized]
         public GraphModelStateComponent graphModelStateComponent;
 
@@ -179,6 +181,7 @@ namespace UnityEditor.ShaderGraph.GraphUI
             }
             GraphHandler.ReconcretizeAll();
             mainPreviewData = new(Guid.ToString());
+            m_DefaultContextNode = GetMainContextNode();
         }
 
         internal void InitializeContextFromTarget(Target target)
@@ -190,6 +193,17 @@ namespace UnityEditor.ShaderGraph.GraphUI
             {
                 contextNode.DefineNode();
             }
+        }
+
+        GraphDataContextNodeModel GetMainContextNode()
+        {
+            foreach (var node in NodeModels)
+            {
+                if (node is GraphDataContextNodeModel graphDataContextNodeModel && graphDataContextNodeModel.IsMainContextNode())
+                    return graphDataContextNodeModel;
+            }
+
+            return null;
         }
 
         public override bool CanBeSubgraph() => isSubGraph;
@@ -685,22 +699,11 @@ namespace UnityEditor.ShaderGraph.GraphUI
             return false;
         }
 
-        GraphDataContextNodeModel GetMainContextNode()
-        {
-            foreach (var node in NodeModels)
-            {
-                if (node is GraphDataContextNodeModel graphDataContextNodeModel && graphDataContextNodeModel.IsMainContextNode())
-                    return graphDataContextNodeModel;
-            }
-
-            return null;
-        }
-
         public bool DoesNodeRequireTime(string graphDataName)
         {
             // Special casing for main context node now as we don't use a GTF guid as its CLDS ID
             if (graphDataName == DefaultContextName)
-                return IsConnectedToTimeNode(GetMainContextNode());
+                return IsConnectedToTimeNode(m_DefaultContextNode);
 
             return TryGetModelFromGuid(new SerializableGUID(graphDataName), out var elementModel)
                 && elementModel is GraphDataNodeModel graphDataNodeModel && IsConnectedToTimeNode(graphDataNodeModel);
