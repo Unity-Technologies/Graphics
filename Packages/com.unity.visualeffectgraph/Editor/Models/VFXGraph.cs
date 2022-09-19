fd415baf6167fcbdffab16526d0096de0b08eae5
Paul Demeulenaere <pauld@unity3d.com>
2022-09-19 22:10:40 +0000

    [VFX] Improve import using Start/StopAssetEditing (to 2021.3)
    Minor improvement of import time, it actually only have impact on 21.3 but it's relevant to deploy the same change in every version between 21.3 & trunk to avoid code divergence.

--- Packages/com.unity.visualeffectgraph/Editor/Models/VFXGraph.cs
+++ Packages/com.unity.visualeffectgraph/Editor/Models/VFXGraph.cs
@@ -60,9 +60,11 @@ namespace UnityEditor.VFX
                 }
             }
 
+           
             //Relaunch previously skipped OnCompileResource
             if (assetToReimport != null)
             {
+                AssetDatabase.StartAssetEditing();
                 foreach (var assetPath in assetToReimport)
                 {
                     try
@@ -74,6 +76,7 @@ namespace UnityEditor.VFX
                         Debug.LogErrorFormat("Exception during reimport of {0} : {1}", assetPath, exception);
                     }
                 }
+                AssetDatabase.StopAssetEditing();
             }
         }
 
