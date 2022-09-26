e9dfdf734a4adf62da6ff8e3742aa7c67ee11976
bencloward <ben.cloward@unity3d.com>
2022-09-26 11:41:56 -0500

    Added versioning to the color node

--- com.unity.sg2/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/Input/Basic/ColorNode.cs
+++ com.unity.sg2/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/Input/Basic/ColorNode.cs
@@ -2,11 +2,81 @@ using Usage = UnityEditor.ShaderGraph.GraphDelta.GraphType.Usage;
 
 namespace UnityEditor.ShaderGraph.Defs
 {
-
-    internal class ColorNode : IStandardNode
+    internal class ColorNodeV1 : IStandardNode
     {
         public static string Name => "Color";
         public static int Version => 1;
+        public static NodeDescriptor NodeDescriptor => new(
+            Version,
+            Name,
+            functions: new FunctionDescriptor[] {
+                new(
+                    "Default",
+                    //regular color picker assumes SRGB space
+@"    LinearColor.rgb = SRGBToLinear(Color.rgb); LinearColor.a = Color.a;
+    Out = IsGammaSpace() ? Color : LinearColor;",
+                    new ParameterDescriptor[]
+                    {
+                        new ParameterDescriptor("Color", TYPE.Vec4, Usage.Static),
+                        new ParameterDescriptor("LinearColor", TYPE.Vec4, Usage.Local),
+                        new ParameterDescriptor("Out", TYPE.Vec4, Usage.Out)
+                    }
+                ),
+                new(
+                    "HDR",
+                    //older version used the same code for both pickers but it was wrong because the HDR picker assumes linear
+@"    LinearColor.rgb = SRGBToLinear(Color.rgb); LinearColor.a = Color.a;
+    Out = IsGammaSpace() ? Color : LinearColor;",
+                    new ParameterDescriptor[]
+                    {
+                        new ParameterDescriptor("ColorHDR", TYPE.Vec4, Usage.Static),
+                        new ParameterDescriptor("SRGBColor", TYPE.Vec4, Usage.Local),
+                        new ParameterDescriptor("Out", TYPE.Vec4, Usage.Out)
+                    }
+                )
+            }
+        );
+
+        public static NodeUIDescriptor NodeUIDescriptor => new(
+            Version,
+            Name,
+            displayName: "Color",
+            tooltip: "Creates an RGBA value.",
+            category: "Input/Basic",
+            synonyms: new string[1] { "rgba" },
+            selectableFunctions: new()
+            {
+                { "Default", "Default" },
+                { "HDR", "HDR" }
+            },
+            functionSelectorLabel: "Mode",
+            parameters: new ParameterUIDescriptor[3] {
+                new ParameterUIDescriptor(
+                    name: "Color",
+                    displayName: " ",
+                    tooltip: "Default color picker",
+                    useColor: true
+                ),
+                new ParameterUIDescriptor(
+                    name: "ColorHDR",
+                    displayName: " ",
+                    tooltip: "HDR color picker",
+                    useColor: true,
+                    isHdr: true
+                ),
+                new ParameterUIDescriptor(
+                    name: "Out",
+                    displayName: " ",
+                    tooltip: "a constant RGBA color value"
+                )
+            }
+        );
+    }
+
+    internal class ColorNodeV2 : IStandardNode
+    {
+        public static string Name => "Color";
+        public static int Version => 2;
         public static NodeDescriptor NodeDescriptor => new(
             Version,
             Name,
