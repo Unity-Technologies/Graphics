166d88a4a50bd277b5a7f655b0ffa2c3b70c64e0
Evergreen <evergreen@unity3d.com>
2022-09-30 15:09:52 +0000

    [2022.2 backport] URP: Use cmd.Blit when in wireframe editor
    Backport of #12137
    
    Fix blitting in wireframe mode

--- Packages/com.unity.render-pipelines.universal/Runtime/RenderingUtils.cs
+++ Packages/com.unity.render-pipelines.universal/Runtime/RenderingUtils.cs
@@ -220,7 +220,16 @@ namespace UnityEngine.Rendering.Universal
             if (isRenderToBackBufferTarget)
                 cmd.SetViewport(cameraData.pixelRect);
 
-            if (source.rt == null)
+            if (cameraData.isSceneViewCamera)
+            {
+                cmd.SetGlobalTexture("_BlitTexture", source);
+                // This set render target is necessary so we change the LOAD state to DontCare.
+                cmd.SetRenderTarget(BuiltinRenderTextureType.CameraTarget,
+                    loadAction, storeAction, // color
+                    RenderBufferLoadAction.DontCare, RenderBufferStoreAction.DontCare); // depth
+                cmd.Blit(source.nameID, destination.nameID);
+            }
+            else if (source.rt == null)
                 Blitter.BlitTexture(cmd, source.nameID, scaleBias, material, passIndex);  // Obsolete usage of RTHandle aliasing a RenderTargetIdentifier
             else
                 Blitter.BlitTexture(cmd, source, scaleBias, material, passIndex);
