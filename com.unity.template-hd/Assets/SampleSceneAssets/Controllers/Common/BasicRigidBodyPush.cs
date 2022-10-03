f50198351cbc92bdf720472484ed3cd9dfa5313f
pierre-unity <39901544+pierre-unity@users.noreply.github.com>
2022-10-03 10:51:48 +0100

    DX12 raytracing & IET updates for the HDRP template (#7531)
    * converted to DX12, added 2 new RT quality levels
    
    * rebaked reflection probes and updated hdrp asset to work with new reflection probe atlas system
    
    * updated RT settings, recaptured a few tutorial images
    
    * Added 3rd person controller for testing
      - Renamed Scripts folder to Controllers
      - Deleted script SimpleCameraController because it was not used
      - Added 3rd person controller
    
    * updated IET pages, cleaned up hierarchy, updated a few RT settings
    
    * merged master, minor cleanups
    
    * updated IET
    
    * pushed auto updated materials
    
    * updated materials
    
    * pushed updated quality levels
    
    * added "addtionnal properties" page
    
    * added prefab editing scene
    
    * fixed collision on chair (pivot was incorrect)
    
    * fixed typos
    
    * Added First person controller. Need to remove old one if this is better.
    
    * Small tweaks to Controllers
      - Enabled Dithering
      - Tweaked the camera to feel more personal for 3rd
      - Created a prefab for 1st
    
    * Removed old controller
    
    * Update low/mid/high volume settings for RTAO and SSR
    
    * implemented rendering feedback from Remi
    
    - slightly moved spotlights in 2 lamp prefabs for optimal path tracing
    - disabled receive motion occluder for ssgi
    - probe near exit of room 1 increased in size
    - removed clear coat from sandblast metal
    - set directional shadow fallback for room 1
    
    * Modified 3d Controller textures and changed its camera positioning
      - Changed the way textures were resized. Used bicubic smoother instead of sharper
      - Manually painted the body normal map to fix some issues
    
    * Fixing last commit, didnt push material changes
    
    * changed mask maps to be TIFs, fixed aliasing around visor, tuned iridescence, set max light cluster to 18 for raytracing to avoid light popping
    
    * set texture to 8bit per channel rather than 16
    
    * disabled receiver motion rejection for RT AO as well
    
    * Renamed RTX presets, RT shadows are enabled for both, updated 1 tutorial pagae and 1 image
    
    * Update CHANGELOG.md
    
    * bump package version to 15.1
    
    Co-authored-by: Motiejus Viltrakis <motiejus.viltrakis@hq.unity3d.com>
    Co-authored-by: sebastienlagarde <sebastien@unity3d.com>

---
+++ com.unity.template-hd/Assets/SampleSceneAssets/Controllers/Common/BasicRigidBodyPush.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class BasicRigidBodyPush : MonoBehaviour
+{
+	public LayerMask pushLayers;
+	public bool canPush;
+	[Range(0.5f, 5f)] public float strength = 1.1f;
+
+	private void OnControllerColliderHit(ControllerColliderHit hit)
+	{
+		if (canPush) PushRigidBodies(hit);
+	}
+
+	private void PushRigidBodies(ControllerColliderHit hit)
+	{
+		// https://docs.unity3d.com/ScriptReference/CharacterController.OnControllerColliderHit.html
+
+		// make sure we hit a non kinematic rigidbody
+		Rigidbody body = hit.collider.attachedRigidbody;
+		if (body == null || body.isKinematic) return;
+
+		// make sure we only push desired layer(s)
+		var bodyLayerMask = 1 << body.gameObject.layer;
+		if ((bodyLayerMask & pushLayers.value) == 0) return;
+
+		// We dont want to push objects below us
+		if (hit.moveDirection.y < -0.3f) return;
+
+		// Calculate push direction from move direction, horizontal motion only
+		Vector3 pushDir = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z);
+
+		// Apply the push and take strength into account
+		body.AddForce(pushDir * strength, ForceMode.Impulse);
+	}
+}
