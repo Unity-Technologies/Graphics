bf1c4238c4ee9a4b6d404f3dfd2f057530bd2b33
Anis Benyoub <anis@unity3d.com>
2022-10-07 15:36:55 +0000

    Fixed hard eges on volumetric clouds
    This is a backport of this PR: https://github.cds.internal.unity3d.com/unity/unity/pull/11365

--- Packages/com.unity.render-pipelines.high-definition/Runtime/Lighting/VolumetricClouds/VolumetricCloudsUtilities.hlsl
+++ Packages/com.unity.render-pipelines.high-definition/Runtime/Lighting/VolumetricClouds/VolumetricCloudsUtilities.hlsl
@@ -27,8 +27,6 @@
 #define MAX_SKYBOX_VOLUMETRIC_CLOUDS_DISTANCE 200000.0f
 // Maximal size of a light step
 #define LIGHT_STEP_MAXIMAL_SIZE 1000.0f
-// Threshold at which the clouds are considered inexistant (Found experimentally)
-#define TRANSMITTANCE_DISCARD_THRESHOLD 0.992
 
 // Just define a flag when the other is not defined as it is easier for the logic
 #if !defined(LOCAL_VOLUMETRIC_CLOUDS)
