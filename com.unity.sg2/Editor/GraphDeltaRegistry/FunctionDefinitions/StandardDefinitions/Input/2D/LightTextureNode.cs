f9fee2bb2070a92ff769b274c0a612aaab039cec
Tracy Chen <s.chen@unity3d.com>
2022-10-20 01:30:54 -0700

    added nodes

---
+++ com.unity.sg2/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/Input/2D/LightTextureNode.cs
@@ -0,0 +1,74 @@
+using Usage = UnityEditor.ShaderGraph.GraphDelta.GraphType.Usage;
+
+namespace UnityEditor.ShaderGraph.Defs
+{
+    internal class LightTextureNode : IStandardNode
+    {
+        public static string Name => "LightTexture";
+        public static int Version => 1;
+
+        public static NodeDescriptor NodeDescriptor => new(
+            Version,
+            Name,
+            functions: new FunctionDescriptor[] {
+                new (
+                    "LightTex0",
+                    "Out = UnityBuildTexture2DStructNoScale(_ShapeLightTexture0);",
+                    new ParameterDescriptor[]
+                    {//TODO: need a light texture type, or a light tetexture REF
+                        new ParameterDescriptor("Out", TYPE.Texture2D, Usage.Out)
+                    }
+                ),
+                new (
+                    "LightTex1",
+                    "Out = UnityBuildTexture2DStructNoScale(_ShapeLightTexture1);",
+                    new ParameterDescriptor[]
+                    {
+                        new ParameterDescriptor("Out", TYPE.Texture2D, Usage.Out)
+                    }
+                ),
+                new (
+                    "LightTex2",
+                    "Out = UnityBuildTexture2DStructNoScale(_ShapeLightTexture2);",
+                    new ParameterDescriptor[]
+                    {
+                        new ParameterDescriptor("Out", TYPE.Texture2D, Usage.Out)
+                    }
+                ),
+                new (
+                    "LightTex3",
+                    "Out = UnityBuildTexture2DStructNoScale(_ShapeLightTexture3);",
+                    new ParameterDescriptor[]
+                    {
+                        new ParameterDescriptor("Out", TYPE.Texture2D, Usage.Out)
+                    }
+                ),
+            }
+        );
+
+        public static NodeUIDescriptor NodeUIDescriptor => new(
+            Version,
+            Name,
+            displayName: "2D Light Texture",
+            tooltip: "A 2D Light texture asset.",
+            category: "Utility/Input/2D",
+            synonyms: new string[0],
+            hasPreview: false,
+            selectableFunctions: new()
+            {
+                { "LightTex0", "LightTex0" },
+                { "LightTex1", "LightTex1" },
+                { "LightTex2", "LightTex2" },
+                { "LightTex3", "LightTex3" }
+        },
+            functionSelectorLabel: "Blend Style",
+            parameters: new ParameterUIDescriptor[] {
+                new ParameterUIDescriptor(
+                    name: "Out",
+                    displayName: "Out",
+                    tooltip: "selected 2D light texture"
+                )
+            }
+        );
+    }
+}
---
+++ com.unity.sg2/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/Input/URP/URPSampleBufferNode.cs
@@ -0,0 +1,72 @@
+using Usage = UnityEditor.ShaderGraph.GraphDelta.GraphType.Usage;
+
+namespace UnityEditor.ShaderGraph.Defs
+{
+    internal class URPSampleBufferNode : IStandardNode
+    {
+        public static string Name => "URPSampleBuffer";
+        public static int Version => 1;
+
+        public static NodeDescriptor NodeDescriptor => new(
+            Version,
+            Name,
+            functions: new FunctionDescriptor[] {
+                new (
+                    "NormalWorldSpace",
+                    "Out = SHADERGRAPH_SAMPLE_SCENE_NORMAL(UV);",
+                    new ParameterDescriptor[]
+                    {
+                        new ParameterDescriptor("UV", TYPE.Vec4, Usage.In, REF.ScreenPosition_Default),
+                        new ParameterDescriptor("Out", TYPE.Vec3, Usage.Out)
+                    }
+                ),
+                new (
+                    "MotionVectors",
+@"TEXTURE2D(_MotionVectorTexture);
+uint2 pixelCoords = uint2(UV.xy * _ScreenSize.xy);
+Out = LOAD_TEXTURE2D_X_LOD(_MotionVectorTexture, pixelCoords, 0).xy;",
+                    new ParameterDescriptor[]
+                    {
+                        new ParameterDescriptor("UV", TYPE.Vec4, Usage.In, REF.ScreenPosition_Default),
+                        new ParameterDescriptor("Out", TYPE.Vec2, Usage.Out)                    }
+                ),
+                new (
+                    "BlitSource",
+@"uint2 pixelCoords = uint2(UV.xy * _ScreenSize.xy);
+Out = LOAD_TEXTURE2D_X_LOD(_BlitTexture, pixelCoords, 0);",
+                    new ParameterDescriptor[]
+                    {
+                        new ParameterDescriptor("UV", TYPE.Vec4, Usage.In, REF.ScreenPosition_Default),
+                        new ParameterDescriptor("Out", TYPE.Vec4, Usage.Out)                    }
+                ),
+            }
+        );
+
+        public static NodeUIDescriptor NodeUIDescriptor => new(
+            Version,
+            Name,
+            displayName: "URP Sample Buffer",
+            tooltip: "Return data from the selected buffer.",
+            category: "Utility/Input/URP",
+            synonyms: new string[0],
+            selectableFunctions: new()
+            {
+                { "NormalWorldSpace", "Normal World Space" },
+                { "MotionVectors", "Motion Vectors" },
+                { "BlitSource", "Blit Source" }
+            },
+            functionSelectorLabel: "Source Buffer",
+            parameters: new ParameterUIDescriptor[] {
+                new ParameterUIDescriptor(
+                    name: "UV",
+                    tooltip: "the screen coordinates to use for the sample",
+                    options: REF.OptionList.ScreenPositions
+                ),
+                new ParameterUIDescriptor(
+                    name: "Out",
+                    tooltip: "the sample of the selected buffer"
+                )
+            }
+        );
+    }
+}
