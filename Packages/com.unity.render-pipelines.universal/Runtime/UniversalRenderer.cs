a84b714f702b117ebb9ae64960fa98ef72e1d7c6
Suresh Patil <spatil@rythmos.com>
2022-10-30 16:40:36 +0000

    2022.2: Fix crash on adreno 630 devices when using shader with mixed stencil
    Avoid Depth32Stencil8 on Android.
    This fixes a crash on some Adreno 6xx devices, artifacts on some Adreno 5xx devices and general performance regressions (Adreno 5xx has lower depth test throughput using floating point depth, and D32_S8 has also a significant cost in bandwidth)
    
    * Backport case: https://jira.unity3d.com/browse/UUM-6774
    * Parent case: https://jira.unity3d.com/browse/UUM-4367

--- Packages/com.unity.render-pipelines.universal/Runtime/UniversalRenderer.cs
+++ Packages/com.unity.render-pipelines.universal/Runtime/UniversalRenderer.cs
@@ -38,7 +38,7 @@ namespace UnityEngine.Rendering.Universal
     /// </summary>
     public sealed partial class UniversalRenderer : ScriptableRenderer
     {
-        #if UNITY_SWITCH
+        #if UNITY_SWITCH || UNITY_ANDROID
         const GraphicsFormat k_DepthStencilFormat = GraphicsFormat.D24_UNorm_S8_UInt;
         const int k_DepthBufferBits = 24;
         #else
