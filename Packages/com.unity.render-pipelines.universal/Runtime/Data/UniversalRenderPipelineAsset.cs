b9456890814b0acfae8859c8509e77c198b8ca82
Elvar Örn Unnþórsson <elvar.orn@unity3d.com>
2022-11-09 17:53:08 +0000

    [URP][2021.3] Fixing recursive setters in URP Asset (UUM-18025)
    Backport of #8851
    Fixes case UUM-18025.

--- Packages/com.unity.render-pipelines.universal/Runtime/Data/UniversalRenderPipelineAsset.cs
+++ Packages/com.unity.render-pipelines.universal/Runtime/Data/UniversalRenderPipelineAsset.cs
@@ -759,7 +759,7 @@ namespace UnityEngine.Rendering.Universal
         public int additionalLightsShadowResolutionTierLow
         {
             get { return (int)m_AdditionalLightsShadowResolutionTierLow; }
-            internal set { additionalLightsShadowResolutionTierLow = value; }
+            internal set { m_AdditionalLightsShadowResolutionTierLow = value; }
         }
 
         /// <summary>
@@ -777,7 +777,7 @@ namespace UnityEngine.Rendering.Universal
         public int additionalLightsShadowResolutionTierHigh
         {
             get { return (int)m_AdditionalLightsShadowResolutionTierHigh; }
-            internal set { additionalLightsShadowResolutionTierHigh = value; }
+            internal set { m_AdditionalLightsShadowResolutionTierHigh = value; }
         }
 
         internal int GetAdditionalLightsShadowResolution(int additionalLightsShadowResolutionTier)
@@ -867,7 +867,7 @@ namespace UnityEngine.Rendering.Universal
         public float cascadeBorder
         {
             get { return m_CascadeBorder; }
-            set { cascadeBorder = value; }
+            set { m_CascadeBorder = value; }
         }
 
         /// <summary>
