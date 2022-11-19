951e5f0595c5b8839e73351b156574936dd37f78
Ben Cloward <88061215+bencloward@users.noreply.github.com>
2022-11-19 09:38:05 -0600

    Noise3D and Noise Hash Nodes (#7736)
    * Noise3D Node and Noise Hash Node
    
    * Renamed the Noise3D file
    
    * Added Noise2D and textures, documentation for Noise 3D, improved matrix rotation
    
    * Added documentation for Noise 2D and updated texture metadata
    
    * Re-balanced the output of all noise types and added the sRGB checkbox. Also added the Scale control.
    
    * Added Scale to Noise Hash. Added Noise Hash input defaults.  Added Noise Hash preview documentation

---
+++ com.unity.sg2/Documentation~/previews/Noise2D.md
@@ -0,0 +1,24 @@
+## Description
+Creates 2d noise based on the selected options.
+
+## Inputs
+**Position** - The 2d seed for the noise pattern.
+**Scale** - Controls the size of the noise.
+**Octaves** - The number of times to repeat the noise algorithm. More octaves creates more detail and more expensive noise.
+**Lacunarity** - The scale adjustment between each octave.
+**Gain** - The contrast adjustment between each octave.
+**Offset** - Controls the brightess or height of the multifractal ridges
+**Gradient Texture** - The texture to use for each octave instead of using math.
+**Noise Hash Texture** - The texture to use as a hash instead of using math for Value Noise.
+**Random Octave Rotation** - When true, each octave is rotated to create better variation.
+**sRGB** - When checked, the output is in sRGB space instead of linear. This should be checked when using the noise for Base Color.
+
+## Output
+**Out** - A smooth, non-tiling noise pattern using the selected options
+
+## Controls
+**Noise Type** - This dropdown offers three different selections:
+
+1. Base Pattern - the base noise type: Value Noise, Gradient Noise, or Worley Noise. These are in order of least expensive to most.
+2. Calculation Method - select whether to use Math or Texture for the base pattern.
+3. Fractal Type - select Fractal Brownian Motion, Turbulance, or Ridged Multifractal. These are in order of least expensive to most.
---
+++ com.unity.sg2/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/Procedural/Noise/NoiseHashNode.cs
@@ -0,0 +1,120 @@
+using Usage = UnityEditor.ShaderGraph.GraphDelta.GraphType.Usage;
+
+namespace UnityEditor.ShaderGraph.Defs
+{
+    internal class NoiseHashNode : IStandardNode
+    {
+        public static string Name => "NoiseHash";
+        public static int Version => 1;
+
+        public static NodeDescriptor NodeDescriptor => new(
+            Version,
+            Name,
+            functions: new FunctionDescriptor[] {
+                new (
+                    "Hash21",
+                    @"    Hash_Tchou_2_1_float(UV*Scale, Out);",
+                    new ParameterDescriptor[]
+                    {
+                        new ParameterDescriptor("UV", TYPE.Vec2, Usage.In, REF.UV0),
+                        new ParameterDescriptor("Scale", TYPE.Vec2, Usage.In, new float[] {10.0f, 10.0f}),
+                        new ParameterDescriptor("Out", TYPE.Float, Usage.Out),
+                    },
+                    new string[]
+                    {
+                        "\"Packages/com.unity.render-pipelines.core/ShaderLibrary/Hashes.hlsl\""
+                    }
+                ),
+                new (
+                    "Hash23",
+                    @"    Hash_Tchou_2_3_float(UV*Scale, Out);",
+                    new ParameterDescriptor[]
+                    {
+                        new ParameterDescriptor("UV", TYPE.Vec2, Usage.In, REF.UV0),
+                        new ParameterDescriptor("Scale", TYPE.Vec2, Usage.In, new float[] {10.0f, 10.0f}),
+                        new ParameterDescriptor("Out", TYPE.Vec3, Usage.Out),
+                    },
+                    new string[]
+                    {
+                        "\"Packages/com.unity.render-pipelines.core/ShaderLibrary/Hashes.hlsl\""
+                    }
+                ),
+                new (
+                    "Hash22",
+                    @"    Hash_Tchou_2_2_float(UV*Scale, Out);",
+                    new ParameterDescriptor[]
+                    {
+                        new ParameterDescriptor("UV", TYPE.Vec2, Usage.In, REF.UV0),
+                        new ParameterDescriptor("Scale", TYPE.Vec2, Usage.In, new float[] {10.0f, 10.0f}),
+                        new ParameterDescriptor("Out", TYPE.Vec2, Usage.Out),
+                    },
+                    new string[]
+                    {
+                        "\"Packages/com.unity.render-pipelines.core/ShaderLibrary/Hashes.hlsl\""
+                    }
+                ),
+                new (
+                    "Hash31",
+                    @"    Hash_Tchou_3_1_float(Position*Scale, Out);",
+                    new ParameterDescriptor[]
+                    {
+                        new ParameterDescriptor("Position", TYPE.Vec3, Usage.In, REF.WorldSpace_Position),
+                        new ParameterDescriptor("Scale", TYPE.Vec3, Usage.In, new float[] {10.0f, 10.0f, 10.0f}),
+                        new ParameterDescriptor("Out", TYPE.Float, Usage.Out),
+                    },
+                    new string[]
+                    {
+                        "\"Packages/com.unity.render-pipelines.core/ShaderLibrary/Hashes.hlsl\""
+                    }
+                ),
+                new(
+                    "Hash33",
+                    @"    Hash_Tchou_3_3_float(Position*Scale, Out);",
+                    new ParameterDescriptor[]
+                    {
+                        new ParameterDescriptor("Position", TYPE.Vec3, Usage.In, REF.WorldSpace_Position),
+                        new ParameterDescriptor("Scale", TYPE.Vec3, Usage.In, new float[] {10.0f, 10.0f, 10.0f}),
+                        new ParameterDescriptor("Out", TYPE.Vec3, Usage.Out)
+                    }
+                )
+            }
+        );
+        public static NodeUIDescriptor NodeUIDescriptor => new(
+            Version,
+            Name,
+            tooltip: "generates a random value based on the input",
+            category: "Procedural/Noise",
+            displayName: "Noise Hash",
+            synonyms: new string[0] {  },
+            selectableFunctions: new()
+            {
+                { "Hash21", "Vec2 to Float" },
+                { "Hash23", "Vec2 to Vec3" },
+                { "Hash22", "Vec2 to Vec2" },
+                { "Hash31", "Vec3 to Float" },
+                { "Hash33", "Vec3 to Vec3" }
+            },
+            functionSelectorLabel: "Hash Type",
+            parameters: new ParameterUIDescriptor[4] {
+                new ParameterUIDescriptor(
+                    name: "UV",
+                    tooltip: "the hash seed value",
+                    options: REF.OptionList.UVs
+                ),
+                new ParameterUIDescriptor(
+                    name: "Position",
+                    tooltip: "the hash seed value",
+                    options: REF.OptionList.Positions
+                ),
+                new ParameterUIDescriptor(
+                    name: "Scale",
+                    tooltip: "controls the size of the hash"
+                ),
+                new ParameterUIDescriptor(
+                    name: "Out",
+                    tooltip: "the resulting hash"
+                )
+            }
+        );
+    }
+}
