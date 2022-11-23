352fde683ee86af0834a7b613ae5e6e75d8f53ad
Julien Amsellem <julien.amsellem@unity3d.com>
2022-11-23 10:20:42 +0000

    [VFX] Fixed two issues with blackboard categories
    # First bug
    https://jira.unity3d.com/browse/UUM-17435
    
    How to reproduce:
    Open HUB;
    Create 3D(HDRP) project;
    Create visual effect graph in the "Project" window (Create->Visual Effects-> Visual Effect Graph);
    Drag it to the "Scene" window;
    Select newly created graph in the "Project" window and go to "Inspector" window;
    Click "Open" button;
    In the opened graph window turn on "Blackboard";
    Click "+" in it and select "Category";
    Select newly created category and duplicate it;
    Select first category and rename it to "AAA".
    Expected result: Selected and renamed category should have AAA title and second category should stay in the screen with the default title.
    Actual result: Renamed category stays in the blackboard screen, but the second category disappears.
    
    Reproducible with: 12.1.7 (2021.3.12f1), 13.1.8 (2022.1.21f1),  14.0.3 (2022.2.0b12), 15.0.1 (2023.1.0a15).
    Could not test with: 10.10.0 (2020.3.41f1), because "Duplicate" functionality not available on this stream.
    
    [Download video](https://jira.unity3d.com/secure/attachment/601529/Duplicate%20function.mov)
    
    # Second bug
    https://jira.unity3d.com/browse/UUM-18000
    
    How to reproduce:
    
    Open HUB;
    Create 3D(HDRP) project;
    Create visual effect graph in the "Project" window (Create->Visual Effects-> Visual Effect Graph);
    Drag it to the "Scene" window;
    Select newly created graph in the "Project" window and go to "Inspector" window;
    Click "Open" button;
    In the opened graph window turn on "Blackboard";
    Click "+" in it and select "Category";
    Select added category and make a right click on it;
    Select "Rename" function;
    Delete default title, enter one space and click "Enter";
    Select the same category and make a right click on it again;
    Select "Duplicate".
    Expected result: User should get validation message during category saving with the space instead of the title.
    Actual result: Console displays error  "ArgumentNullException: Value cannot be null." 
    
    Reproducible with: 12.1.7 (2021.3.12f1), 13.1.8 (2022.1.21f1),  14.0.3 (2022.2.0b12), 15.0.1 (2023.1.0a15).
    Could not test with: 10.10.0 (2020.3.41f1), because "Duplicate" functionality not available on this stream.

---
+++ Tests/SRPTests/Projects/VisualEffectGraph_HDRP/Assets/AllTests/Editor/Tests/VFXBlackboardTests.cs
@@ -0,0 +1,96 @@
+#if !UNITY_EDITOR_OSX || MAC_FORCE_TESTS
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using NUnit.Framework;
+
+using UnityEditor.VFX.UI;
+using UnityEngine.TestTools;
+
+namespace UnityEditor.VFX.Test
+{
+    [TestFixture]
+    public class VFXBlackboardTests
+    {
+
+        [SetUp]
+        public void Init()
+        {
+            VFXViewWindow.GetAllWindows().ToList().ForEach(x => x.Close());
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            VFXTestCommon.DeleteAllTemporaryGraph();
+        }
+
+        [UnityTest]
+        public IEnumerator Add_Category_With_Only_Spaces_In_Name()
+        {
+            // Prepare
+            var categoryName = "new category";
+            var graph = VFXTestCommon.MakeTemporaryGraph();
+            var window = VFXViewWindow.GetWindow(graph, true);
+            window.LoadResource(graph.visualEffectResource);
+            VFXBlackboard blackboard = window.graphView.blackboard;
+            blackboard.AddCategory(categoryName);
+            yield return null;
+            window.graphView.OnSave();
+            yield return null;
+            var cat = GetCategory(blackboard, categoryName);
+
+            // Act
+            blackboard.SetCategoryName(cat, " ");
+
+            // Assert
+            Assert.AreEqual(categoryName, cat.title);
+        }
+
+        [UnityTest]
+        public IEnumerator Duplicate_Category_And_Rename()
+        {
+            // Prepare
+            var categoryName = "new category";
+            var graph = VFXTestCommon.MakeTemporaryGraph();
+            var window = VFXViewWindow.GetWindow(graph, true);
+            window.LoadResource(graph.visualEffectResource);
+            VFXBlackboard blackboard = window.graphView.blackboard;
+            blackboard.AddCategory(categoryName);
+            yield return null;
+            window.graphView.OnSave();
+            yield return null;
+            var cat = GetCategory(blackboard, categoryName);
+            cat.Select(blackboard, false);
+
+            // Act
+            // Duplicate category
+            DuplicateSelectedCategory(window.graphView);
+            blackboard.SetCategoryName(cat, "new name");
+            window.graphView.OnSave();
+            yield return null;
+
+            // Assert
+            Assert.NotNull(GetCategory(blackboard, categoryName + " 1"));
+        }
+
+        private VFXBlackboardCategory GetCategory(VFXBlackboard blackboard, string name)
+        {
+            var fieldInfo = typeof(VFXBlackboard).GetField("m_Categories", BindingFlags.Instance | BindingFlags.NonPublic);
+            var categories = (Dictionary<string, VFXBlackboardCategory>)fieldInfo.GetValue(blackboard);
+
+            return categories.TryGetValue(name, out var cat)
+                ? cat
+                : null;
+        }
+
+        private void DuplicateSelectedCategory(VFXView view)
+        {
+            var methodInfo = typeof(VFXView).GetMethod("DuplicateBlackBoardCategorySelection", BindingFlags.Instance | BindingFlags.NonPublic);
+            methodInfo.Invoke(view, null);
+        }
+    }
+}
+#endif
