646e2abdc1982df34cc6a71c19b7446cc11b44c4
Brett Groshong <brett.groshong@unity3d.com>
2022-12-07 06:48:37 -0800

    Initialize SGUIData as null.

--- com.unity.sg2/Editor/GraphUI/DataModel/SGGraphModel.cs
+++ com.unity.sg2/Editor/GraphUI/DataModel/SGGraphModel.cs
@@ -32,7 +32,7 @@ namespace UnityEditor.ShaderGraph.GraphUI
         internal string DefaultContextName => Registry.ResolveKey<ShaderGraphContext>().Name;
 
         [NonSerialized]
-        Dictionary<RegistryKey, SGNodeViewModel> m_NodeUIData = new();
+        Dictionary<RegistryKey, SGNodeViewModel> m_NodeUIData;
 
         [NonSerialized]
         SGContextNodeModel m_DefaultContextNode;
