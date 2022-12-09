768cef51e3be08d3a21347de1aa6ae7bc4f48584
Nicolaj Zøllner <nicolaj.zollner@unity3d.com>
2022-12-09 13:29:11 +0000

    [Backport 2022.2] Fixed Shadowmask not working with rendering layers in deferred
    This PR fixes the bug [UUM-19788](https://jira.unity3d.com/browse/UUM-19788) for lights not working when using deferred renderer with shadow mask.

--- Packages/com.unity.render-pipelines.universal/Runtime/DeferredLights.cs
+++ Packages/com.unity.render-pipelines.universal/Runtime/DeferredLights.cs
@@ -134,8 +134,9 @@ namespace UnityEngine.Rendering.Universal.Internal
         internal int GBufferNormalSmoothnessIndex { get { return 2; } }
         internal int GBufferLightingIndex { get { return 3; } }
         internal int GbufferDepthIndex { get { return UseRenderPass ? GBufferLightingIndex + 1 : -1; } }
-        internal int GBufferShadowMask { get { return UseShadowMask ? GBufferLightingIndex + (UseRenderPass ? 1 : 0) + 1 : -1; } }
-        internal int GBufferRenderingLayers { get { return UseRenderingLayers ? GBufferLightingIndex + (UseRenderPass ? 1 : 0) + (UseShadowMask ? 1 : 0) + 1 : -1; } }
+        internal int GBufferRenderingLayers { get { return UseRenderingLayers ? GBufferLightingIndex + (UseRenderPass ? 1 : 0) + 1 : -1; } }
+        // Shadow Mask can change at runtime. Because of this it needs to come after the non-changing buffers.
+        internal int GBufferShadowMask { get { return UseShadowMask ? GBufferLightingIndex + (UseRenderPass ? 1 : 0) + (UseRenderingLayers ? 1 : 0) + 1 : -1; } }
         // Color buffer count (not including dephStencil).
         internal int GBufferSliceCount { get { return 4 + (UseRenderPass ? 1 : 0) + (UseShadowMask ? 1 : 0) + (UseRenderingLayers ? 1 : 0); } }
 
