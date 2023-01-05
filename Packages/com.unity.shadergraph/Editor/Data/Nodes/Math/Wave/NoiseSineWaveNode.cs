5a0a8dde5453d3002ac3c0957f24bdb1bdaa501b
Jussi Knuuttila <jussi.knuuttila@unity3d.com>
2023-01-05 20:12:02 +0000

    Backport Automatically release leaked BatchRendererGroups and don't repeat the error messages
    When Unity does a domain reload (e.g. when the user changes script code), it's possible for some objects to leak if scripts are not written correctly to handle the situation. If this happens to a BatchRendererGroup object, Unity will keep repeatedly issuing a loud error message that the user can only remove by restarting the editor, which is very disruptive.
    
    This PR turns the error message to a warning, and will automatically release the leaked object when detected, so the warning is issued only once and does not severely impact editor usability.

--- Packages/com.unity.shadergraph/Editor/Data/Nodes/Math/Wave/NoiseSineWaveNode.cs
+++ Packages/com.unity.shadergraph/Editor/Data/Nodes/Math/Wave/NoiseSineWaveNode.cs
@@ -6,8 +6,6 @@ namespace UnityEditor.ShaderGraph
     [Title("Math", "Wave", "Noise Sine Wave")]
     class NoiseSineWaveNode : CodeFunctionNode
     {
-        public override int latestVersion => 1;
-
         public NoiseSineWaveNode()
         {
             name = "Noise Sine Wave";
@@ -15,19 +13,10 @@ namespace UnityEditor.ShaderGraph
 
         protected override MethodInfo GetFunctionToConvert()
         {
-            // Always make the latest version the default case, this gets around otherwise not returning in all codepaths.
-            switch (sgVersion)
-            {
-                case 0:
-                    return GetType().GetMethod("NoiseSineWave_V0", BindingFlags.Static | BindingFlags.NonPublic);
-
-                case 1:
-                default:
-                    return GetType().GetMethod("NoiseSineWave_V1", BindingFlags.Static | BindingFlags.NonPublic);
-            }
+            return GetType().GetMethod("NoiseSineWave", BindingFlags.Static | BindingFlags.NonPublic);
         }
 
-        static string NoiseSineWave_V0(
+        static string NoiseSineWave(
             [Slot(0, Binding.None)] DynamicDimensionVector In,
             [Slot(1, Binding.None, -0.5f, 0.5f, 1, 1)] Vector2 MinMax,
             [Slot(2, Binding.None)] out DynamicDimensionVector Out)
@@ -39,23 +28,6 @@ namespace UnityEditor.ShaderGraph
     $precision sinInOffset = sin(In + 1.0);
     $precision randomno =  frac(sin((sinIn - sinInOffset) * (12.9898 + 78.233))*43758.5453);
     $precision noise = lerp(MinMax.x, MinMax.y, randomno);
-    Out = sinIn + noise;
-}
-";
-        }
-
-        static string NoiseSineWave_V1(
-            [Slot(0, Binding.None)] DynamicDimensionVector In,
-            [Slot(1, Binding.None, -0.5f, 0.5f, 1, 1)] Vector2 MinMax,
-            [Slot(2, Binding.None)] out DynamicDimensionVector Out)
-        {
-            return
-@"
-{
-    $precision{slot2dimension} sinIn = sin(In);
-    $precision{slot2dimension} sinInOffset = sin(In + 1.0);
-    $precision{slot2dimension} randomno =  frac(sin((sinIn - sinInOffset) * (12.9898 + 78.233))*43758.5453);
-    $precision{slot2dimension} noise = lerp(MinMax.x, MinMax.y, randomno);
     Out = sinIn + noise;
 }
 ";
