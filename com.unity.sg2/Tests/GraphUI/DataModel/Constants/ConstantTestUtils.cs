29cfa944fa0e5a6ef125f65615e73e0eebb41620
joseph.savell <joseph.savell@unity3d.com>
2023-01-09 12:30:50 -0800

    Remove an unnecessary graph model from constant tests

--- com.unity.sg2/Tests/GraphUI/DataModel/Constants/ConstantTestUtils.cs
+++ com.unity.sg2/Tests/GraphUI/DataModel/Constants/ConstantTestUtils.cs
@@ -5,10 +5,11 @@ namespace UnityEditor.ShaderGraph.GraphUI.UnitTests
 {
     static class ConstantTestUtils
     {
+        // NOTE: Default value `value` is only applicable if supported by the type. See NodeBuilderUtils.ParameterDescriptorToField.
         public static (NodeHandler, PortHandler) MakeTestField(SGGraphModel model, ITypeDescriptor type, object value = null, string name = "TestParam")
         {
-            var node = model.CreateGraphDataNode(new RegistryKey {Name = "Add", Version = 1});
-            node.TryGetNodeHandler(out var nodeHandler);
+            // Arbitrary node to hold our test field
+            var nodeHandler = model.GraphHandler.AddNode(new RegistryKey {Name = "Add", Version = 1}, $"Test_{name}");
 
             var param = new ParameterDescriptor(name, type, GraphType.Usage.In, value);
             var portHandler = NodeBuilderUtils.ParameterDescriptorToField(param, TYPE.Any, nodeHandler, model.GraphHandler.registry);
