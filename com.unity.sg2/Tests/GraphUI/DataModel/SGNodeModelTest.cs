e78a223975eeb6cdd284c1f03f957ac28790d97a
joseph.savell <joseph.savell@unity3d.com>
2023-01-20 15:40:40 -0800

    Fix singleton Registry references, update SGGraphModelMock

--- com.unity.sg2/Tests/GraphUI/DataModel/SGNodeModelTest.cs
+++ com.unity.sg2/Tests/GraphUI/DataModel/SGNodeModelTest.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 using Unity.GraphToolsFoundation.Editor;
 using UnityEditor.ShaderGraph.Defs;
 using UnityEditor.ShaderGraph.GraphDelta;
 using UnityEngine;
+using UnityEngine.TestTools;
 
 namespace UnityEditor.ShaderGraph.GraphUI.UnitTests.DataModel
 {
@@ -15,6 +17,19 @@ namespace UnityEditor.ShaderGraph.GraphUI.UnitTests.DataModel
         static readonly RegistryKey k_EmptyNodeKey = new() {Name = "TestEmpty", Version = 1};
         static readonly FunctionDescriptor k_EmptyNodeDescriptor = new(k_EmptyNodeKey.Name, "", Array.Empty<ParameterDescriptor>());
 
+        // Multi function node: two empty functions, "Main" and "Alternate".
+        static readonly RegistryKey k_MultiFunctionKey = new() {Name = "TestMultiFunction", Version = 1};
+        const string k_MultiFunctionMainFunc = "Main";
+        const string k_MultiFunctionAltFunc = "Alternate";
+        static readonly NodeDescriptor k_MultiFunctionDescriptor = new(k_MultiFunctionKey.Version,
+            k_MultiFunctionKey.Name,
+            k_MultiFunctionMainFunc,
+            new FunctionDescriptor(k_MultiFunctionMainFunc, "", Array.Empty<ParameterDescriptor>()),
+            new FunctionDescriptor(k_MultiFunctionAltFunc, "", Array.Empty<ParameterDescriptor>())
+        );
+
+        // TODO: Options node: one port with two referable options, OptionA and OptionB.
+
         #endregion
 
         SGGraphModel graphModel { get; set; }
@@ -23,7 +38,18 @@ namespace UnityEditor.ShaderGraph.GraphUI.UnitTests.DataModel
         public void SetUp()
         {
             var registry = new ShaderGraphRegistry();
+
+            registry.Register<GraphType>();
+            registry.Register<GraphTypeAssignment>();
+            registry.Register<GradientType>();
+            registry.Register<GradientTypeAssignment>();
+            registry.Register<BaseTextureType>();
+            registry.Register<BaseTextureTypeAssignment>();
+            registry.Register<SamplerStateType>();
+            registry.Register<SamplerStateAssignment>();
+
             registry.Register(k_EmptyNodeDescriptor, 1);
+            registry.Register(k_MultiFunctionDescriptor);
 
             graphModel = SGGraphModelMock.CreateWithGraphHandler(registry);
         }
@@ -33,6 +59,24 @@ namespace UnityEditor.ShaderGraph.GraphUI.UnitTests.DataModel
             var nodeHandler = graphModel.GraphHandler.AddNode(key ?? k_EmptyNodeKey, "Test");
             var node = graphModel.CreateNode<SGNodeModel>("Test", initializationCallback: nm => nm.graphDataName = "Test");
             return (nodeHandler, node);
+        }
+
+        SGNodeModel MakeSearcherPreviewNode(RegistryKey? key = null)
+        {
+            return graphModel.CreateNode<SGNodeModel>("Test", spawnFlags: SpawnFlags.Orphan, initializationCallback: nm => nm.SetSearcherPreviewRegistryKey(key ?? k_EmptyNodeKey));
+        }
+
+        SGNodeModel MakeNodeWithoutBackingData()
+        {
+            // OnDefineNode will log an error because the created node doesn't have a valid backing
+            var ignoreFailing = LogAssert.ignoreFailingMessages;
+            LogAssert.ignoreFailingMessages = true;
+
+            var node = graphModel.CreateNode<SGNodeModel>("Test");
+
+            LogAssert.ignoreFailingMessages = ignoreFailing;
+
+            return node;
         }
 
         [Test]
