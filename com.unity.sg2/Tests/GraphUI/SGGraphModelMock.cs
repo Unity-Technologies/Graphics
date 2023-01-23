c95e19b84b14b377effcd2b2657d2dc8aea7e083
joseph.savell <joseph.savell@unity3d.com>
2023-01-23 15:36:11 -0800

    Add node model test to demo SGGraphModelMock

--- com.unity.sg2/Tests/GraphUI/SGGraphModelMock.cs
+++ com.unity.sg2/Tests/GraphUI/SGGraphModelMock.cs
@@ -11,12 +11,27 @@ namespace UnityEditor.ShaderGraph.GraphUI.UnitTests
     /// </summary>
     class SGGraphModelMock : SGGraphModel
     {
-        ShaderGraphRegistry m_Registry;
-        internal override ShaderGraphRegistry RegistryInstance => m_Registry;
+        internal override ShaderGraphRegistry RegistryInstance { get; }
 
+        // Note: Provide a GraphHandler using .Init() before use. See ShaderGraphAssetUtils.CreateNewAssetGraph
+        // for how this happens in the tool, or use the CreateWithGraphHandler factory method.
         internal SGGraphModelMock(ShaderGraphRegistry registry)
         {
-            m_Registry = registry;
+            RegistryInstance = registry;
+        }
+
+        /// <summary>
+        /// Creates a new SGGraphModelMock and initializes it with an empty GraphHandler using the given registry.
+        /// The graph will have a null target and not be a subgraph.
+        /// </summary>
+        /// <param name="registry">ShaderGraphRegistry containing definitions to use.</param>
+        /// <returns>A new SGGraphModelMock using the given registry and initialized with an empty GraphHandler.</returns>
+        public static SGGraphModelMock CreateWithGraphHandler(ShaderGraphRegistry registry)
+        {
+            var model = new SGGraphModelMock(registry);
+            var graphHandler = new GraphHandler(registry.Registry);
+            model.Init(graphHandler, false, null);
+            return model;
         }
     }
 }
