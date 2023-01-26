900fdf8d7f81dc54c3b828e330b67b66a875324f
Brett Groshong <brett.groshong@unity3d.com>
2023-01-26 08:50:26 -0800

    Fix ignored tests.

--- com.unity.sg2/Tests/GraphUI/TestInteractionHelpers.cs
+++ com.unity.sg2/Tests/GraphUI/TestInteractionHelpers.cs
@@ -12,95 +12,26 @@ namespace UnityEditor.ShaderGraph.GraphUI.UnitTests
     /// </summary>
     class TestInteractionHelpers
     {
-        TestEditorWindow m_Window;
-
-        TestEventHelpers m_TestEventHelper;
+        private readonly TestEditorWindow _window;
+        private readonly TestEventHelpers _testEventHelper;
 
         public TestInteractionHelpers(TestEditorWindow targetWindow, TestEventHelpers testEventHelper)
         {
-            m_Window = targetWindow;
-            m_TestEventHelper = testEventHelper;
+            _window = targetWindow;
+            _testEventHelper = testEventHelper;
         }
 
         public IEnumerator SelectAndCopyNodes(List<AbstractNodeModel> nodeModels)
         {
             // Select both the nodes
-            m_Window.GraphView.Dispatch(new SelectElementsCommand(SelectElementsCommand.SelectionMode.Replace, nodeModels));
+            _window.GraphView.Dispatch(new SelectElementsCommand(SelectElementsCommand.SelectionMode.Replace, nodeModels));
             yield return null;
 
-            m_TestEventHelper.SimulateKeyPress("C", modifiers: EventModifiers.Control);
+            _testEventHelper.SimulateKeyPress("C", modifiers: EventModifiers.Control);
             yield return null;
 
-            m_TestEventHelper.SimulateKeyPress("V", modifiers: EventModifiers.Control);
+            _testEventHelper.SimulateKeyPress("V", modifiers: EventModifiers.Control);
             yield return null;
-        }
-
-        public IEnumerator CreateNodesAndConnect(string fromNodeName = "Add", string toNodeName = "Preview", string fromPortName = "Out", string toPortName = "In")
-        {
-            yield return AddNodeFromSearcherAndValidate(fromNodeName);
-            yield return AddNodeFromSearcherAndValidate(toNodeName);
-
-            var fromNode = m_Window.GetNodeModelFromGraphByName(fromNodeName);
-            var outPort = SGGraphModel.FindOutputPortByName(fromNode, fromPortName);
-            Assert.IsNotNull(outPort, "Could not find output port: " + fromPortName + "on node: " + fromNodeName);
-
-            var toNode = m_Window.GetNodeModelFromGraphByName(toNodeName);
-            var inPort =  SGGraphModel.FindInputPortByName(toNode, toPortName);
-            Assert.IsNotNull(inPort, "Could not find input port: " + toPortName + "on node: " + toNodeName);
-
-            m_Window.GraphView.Dispatch(new CreateWireCommand(inPort, outPort));
-        }
-
-        public void ConnectNodes(string fromNodeName, string toNodeName, string fromPortName = "Out", string toPortName = "In")
-        {
-            var fromNode = m_Window.GetNodeModelFromGraphByName(fromNodeName);
-            var fromPortModel = SGGraphModel.FindOutputPortByName(fromNode, fromPortName);
-
-            var toNode = m_Window.GetNodeModelFromGraphByName(toNodeName);
-            var toPortModel =  SGGraphModel.FindInputPortByName(toNode, toPortName);
-
-            m_Window.GraphView.Dispatch(new CreateWireCommand(toPortModel, fromPortModel));
-        }
-
-        ItemLibraryWindow SummonSearcher()
-        {
-            m_Window.GraphView.ShowItemLibrary(new Vector2());
-
-            // TODO: (Sai) This throws an exception on some occasions in DisplaySmartSearch, ask Vlad for help figuring out why?
-            //TestEventHelpers.SendKeyDownEvent(m_MainWindow, KeyCode.Space);
-            //TestEventHelpers.SendKeyUpEvent(m_MainWindow, KeyCode.Space);
-
-            var searcherWindow = (ItemLibraryWindow)EditorWindow.GetWindow(typeof(ItemLibraryWindow));
-            return searcherWindow;
-        }
-
-        public IEnumerator AddNodeFromSearcherAndValidate(string nodeName)
-        {
-            var searcherWindow = SummonSearcher();
-            var searcherWindowTestHelper = new TestEventHelpers(searcherWindow);
-
-            yield return null;
-
-            searcherWindow.Focus();
-            yield return null;
-            yield return null;
-
-            foreach (char c in nodeName)
-            {
-                searcherWindowTestHelper.SimulateKeyPress(c.ToString());
-                yield return null;
-            }
-
-            // Sending two key-down events followed by a key-up for the Return as we normally do causes an exception
-            // it seems like the searcher is waiting for that first Return event and closes immediately after,
-            // any further key events sent cause a MissingReferenceException as the searcher window is now invalid
-            searcherWindowTestHelper.SimulateKeyPress(KeyCode.Return, false, false);
-            yield return null;
-            yield return null;
-            yield return null;
-            yield return null;
-
-            Assert.IsNotNull(m_Window.GetNodeModelFromGraphByName(nodeName));
         }
     }
 }
