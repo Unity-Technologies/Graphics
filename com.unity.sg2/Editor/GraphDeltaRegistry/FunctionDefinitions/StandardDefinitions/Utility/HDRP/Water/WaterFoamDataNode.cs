a36fced25bf6cc5bbafdb49c13b3a85f6c688206
bencloward <ben.cloward@unity3d.com>
2023-02-16 09:45:09 -0600

    Fixed a small typo in the WaterFoamData node

--- com.unity.sg2/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/Utility/HDRP/Water/WaterFoamDataNode.cs
+++ com.unity.sg2/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/Utility/HDRP/Water/WaterFoamDataNode.cs
@@ -5,7 +5,7 @@ namespace UnityEditor.ShaderGraph.Defs
     {
         internal class WaterFoamDataNode : IStandardNode
         {
-            public static string Name => WaterFoamData";
+            public static string Name => "WaterFoamData";
             public static int Version => 1;
 
             public static FunctionDescriptor FunctionDescriptor => new(
