77708df332ceec2e643f3615ffdada81b883e77c
Ben Cloward <88061215+bencloward@users.noreply.github.com>
2023-02-21 17:14:51 -0600

    Renaming Nodes based on feedback from Anita and Martin	 (#7821)
    * Renaming Nodes based on feedback from Anita and Martin
    
    This set of nodes have been renamed to remove verbs from the title, remove abbreviations, and name the names more clear.
    
    * Fixed a small typo in the WaterFoamData node
    
    * Renamed one more node, renamed documentation files and documentation preview files to match the renamed nodes
    
    * Updated synonyms so that the newly renamed nodes show up in the search for the old node names
    
    * Added synonym for the Water Normal Blend node

---
+++ com.unity.sg2/Editor/GraphDeltaRegistry/FunctionDefinitions/StandardDefinitions/Utility/HDRP/Water/WaterRefractionDataNode.cs
@@ -0,0 +1,66 @@
+using System;
+using Usage = UnityEditor.ShaderGraph.GraphDelta.GraphType.Usage;
+
+namespace UnityEditor.ShaderGraph.Defs
+{
+    internal class WaterRefractionDataNode : IStandardNode
+    {
+        public static string Name => "WaterRefractionData";
+        public static int Version => 1;
+
+        public static FunctionDescriptor FunctionDescriptor => new(
+            Name,
+@"ComputeWaterRefractionParams(positionWS, NormalWS, LowFrequencyNormalWS, screenPos.xy, viewWS, true, _MaxRefractionDistance, _TransparencyColor.xyz, _OutScatteringCoefficient, RefractedPositionWS, DistortedWaterNDC, refractedDistance, AbsorptionTint);",
+            new ParameterDescriptor[]
+            {
+                new ParameterDescriptor("refractedDistance", TYPE.Float, Usage.Local),
+                new ParameterDescriptor("positionWS", TYPE.Vec3, Usage.Local, REF.WorldSpace_Position),
+                new ParameterDescriptor("viewWS", TYPE.Vec3, Usage.Local, REF.WorldSpace_ViewDirection),
+                new ParameterDescriptor("screenPos", TYPE.Vec2, Usage.Local, REF.ScreenPosition),
+                new ParameterDescriptor("NormalWS", TYPE.Vec3, Usage.In),
+                new ParameterDescriptor("LowFrequencyNormalWS", TYPE.Vec3, Usage.In),
+                new ParameterDescriptor("RefractedPositionWS", TYPE.Vec3, Usage.Out),
+                new ParameterDescriptor("DistortedWaterNDC", TYPE.Vec2, Usage.Out),
+                new ParameterDescriptor("AbsorptionTint", TYPE.Vec3, Usage.Out)
+            }
+        );
+
+        public static NodeUIDescriptor NodeUIDescriptor => new(
+            Version,
+            Name,
+            displayName: "Water Refraction Data",
+            tooltip: "",
+            category: "Utility/HDRP/Water",
+            description: "pkg://Documentation~/previews/WaterRefractionData.md",
+            synonyms: new string[1] { "Evaluate Water Refraction Data" },
+            hasPreview: false,
+            parameters: new ParameterUIDescriptor[] {
+                new ParameterUIDescriptor(
+                    name: "NormalWS",
+                    displayName: "Normal WS",
+                    tooltip: ""
+                ),
+                new ParameterUIDescriptor(
+                    name: "LowFrequencyNormalWS",
+                    displayName: "Low Frequency Normal WS",
+                    tooltip: ""
+                ),
+                new ParameterUIDescriptor(
+                    name: "RefractedPositionWS",
+                    displayName: "Refracted Position WS",
+                    tooltip: ""
+                ),
+                new ParameterUIDescriptor(
+                    name: "DistortedWaterNDC",
+                    displayName: "Distorted Water NDC",
+                    tooltip: ""
+                ),
+                new ParameterUIDescriptor(
+                    name: "AbsorptionTint",
+                    displayName: "Absorption Tint",
+                    tooltip: ""
+                )
+            }
+        );
+    }
+}
