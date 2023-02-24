edbf880ab98c893f4dbd26b687ace7ef52d0996c
Elvar Örn Unnþórsson <elvar.orn@unity3d.com>
2023-02-24 22:11:46 +0000

    [URP][2022.2] Fixing Shader warnings and updating one reference image
    Backport of some shader warning fixes done in #23434.
    Also updates one reference image for FXAA in URP's PostPro project.

--- Packages/com.unity.render-pipelines.universal/Shaders/Particles/ParticlesSimpleLitForwardPass.hlsl
+++ Packages/com.unity.render-pipelines.universal/Shaders/Particles/ParticlesSimpleLitForwardPass.hlsl
@@ -115,7 +115,7 @@ half4 ParticlesLitFragment(VaryingsParticle input) : SV_Target
     half3 diffuse = AlphaModulate(albedo.rgb, albedo.a);
     half alpha = albedo.a;
 #if defined(_EMISSION)
-    half3 emission = BlendTexture(TEXTURE2D_ARGS(_EmissionMap, sampler_EmissionMap), particleParams.uv, particleParams.blendUv) * _EmissionColor.rgb;
+    half3 emission = BlendTexture(TEXTURE2D_ARGS(_EmissionMap, sampler_EmissionMap), particleParams.uv, particleParams.blendUv).rgb * _EmissionColor.rgb;
 #else
     half3 emission = half3(0, 0, 0);
 #endif
