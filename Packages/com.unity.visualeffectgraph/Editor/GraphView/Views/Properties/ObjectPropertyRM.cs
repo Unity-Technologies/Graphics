6a8a04380f7ebbe6535a3f322e05eb9fd2fbb7f4
Julien Amsellem <julien.amsellem@unity3d.com>
2023-03-18 05:12:13 +0000

    [VFX] Removed custom object field so that search window preference
    Jira: https://jira.unity3d.com/browse/UUM-26418
    
    How to reproduce:
    1. Open the user’s attached “ObjectSelectorTest.zip” project
    2. Open “VFX.vfx” Visual Effects Graph
    3. Press the select button of “Main Texture”
    4. Observe the Search window
    
    **Actual**
    ![](https://jira.unity3d.com/secure/attachment/679065/Actual.png)
    **Expected**
    ![](https://jira.unity3d.com/secure/attachment/679066/Expected.png)
    Expected result: Search Engine is classic
    Actual result: Search Engine is advanced
    
    Reproducible with: 13.1.1 (2022.1.0a16), 14.0.6 (2022.2.5f1), 15.0.4 (2023.1.0b2)
    Not reproducible with: 10.10.1 (2020.3.44f1), 12.1.10 (2021.3.18f1), 13.1.0 (2022.1.0a15)
    
    Reproduced on: macOS 12.4 (Intel)
    
    Note: Search Engine is set in Preferences > Search > Search Engines > Object Selector

--- Packages/com.unity.visualeffectgraph/Editor/GraphView/Views/Properties/ObjectPropertyRM.cs
+++ Packages/com.unity.visualeffectgraph/Editor/GraphView/Views/Properties/ObjectPropertyRM.cs
@@ -1,90 +1,37 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using UnityEditor.ShaderGraph.Internal;
+using UnityEditor.UIElements;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.UIElements;
-
 using UnityObject = UnityEngine.Object;
 
 namespace UnityEditor.VFX.UI
 {
     class ObjectPropertyRM : PropertyRM<UnityObject>
     {
-        static readonly Dictionary<Type, TextureDimension> s_TypeToDimensionMap = new()
-        {
-            { typeof(Texture2D), TextureDimension.Tex2D },
-            { typeof(Texture3D), TextureDimension.Tex3D },
-            { typeof(Cubemap), TextureDimension.Cube },
-        };
-
-        readonly TextField m_TextField;
-        readonly Image m_ValueIcon;
-        readonly TextureDimension m_textureDimension;
+        readonly ObjectField m_ObjectField;
 
         public ObjectPropertyRM(IPropertyRMProvider controller, float labelWidth) : base(controller, labelWidth)
         {
             styleSheets.Add(VFXView.LoadStyleSheet("ObjectPropertyRM"));
 
-            m_TextField = new TextField { name = "PickLabel", isReadOnly = true };
-            var button = new Button { name = "PickButton" };
-            var icon = new VisualElement { name = "PickIcon" };
-            m_ValueIcon = new Image { name = "ValueIcon" };
 
-            button.clicked += OnPickObject;
-            button.Add(icon);
-            m_TextField.Add(m_ValueIcon);
-            m_TextField.Add(button);
-            Add(m_TextField);
-
-            m_TextField.RegisterCallback<ClickEvent>(OnClickToShow);
-            RegisterCallback<DragUpdatedEvent>(OnDragUpdate);
-            RegisterCallback<DragEnterEvent>(OnDragEnter);
-            RegisterCallback<DragPerformEvent>(OnDragPerformed);
-
-            // This allow to detect when a resource is deleted or restored
-            RegisterCallback<AttachToPanelEvent>((evt) =>
-            {
-                EditorApplication.projectChanged += OnProjectOrHierarchyChanged;
-                EditorApplication.hierarchyChanged += OnProjectOrHierarchyChanged;
-            });
-            RegisterCallback<DetachFromPanelEvent>((evt) =>
-            {
-                EditorApplication.projectChanged -= OnProjectOrHierarchyChanged;
-                EditorApplication.hierarchyChanged -= OnProjectOrHierarchyChanged;
-            });
-
-
-            if (!s_TypeToDimensionMap.TryGetValue(m_Provider.portType, out m_textureDimension))
-            {
-                m_textureDimension = TextureDimension.Unknown;
-            }
+            m_ObjectField = new ObjectField { objectType = m_Provider.portType };
+            m_ObjectField.RegisterCallback<ChangeEvent<UnityObject>>(OnValueChanged);
+            Add(m_ObjectField);
         }
 
         public override float GetPreferredControlWidth() => 120;
 
-        public override void UpdateGUI(bool force) { }
+        public override void UpdateGUI(bool force)
+        {
+            m_ObjectField.value = m_Value;
+        }
 
-        public override void SetValue(object obj) // object setvalue should accept null
+        public override void SetValue(object obj)
         {
             try
             {
                 m_Value = (UnityObject)obj;
-                if (m_Provider.portType == typeof(ShaderGraphVfxAsset))
-                {
-                    //m_ValueIcon.image = AssetPreview.GetMiniTypeThumbnail(typeof(Shader));
-                    m_ValueIcon.image = Resources.Load<Texture2D>("Icons/sg_graph_icon");
-                }
-                else
-                {
-                    m_ValueIcon.image = obj != null
-                        ? AssetPreview.GetMiniTypeThumbnail(m_Value)
-                        : AssetPreview.GetMiniTypeThumbnail(m_Provider.portType);
-                }
-
-                CheckForMissingReference();
             }
             catch (Exception)
             {
@@ -96,80 +43,14 @@ namespace UnityEditor.VFX.UI
 
         public override bool showsEverything => true;
 
-        protected override void UpdateEnabled() => SetEnabled(propertyEnabled);
+        protected override void UpdateEnabled() => m_ObjectField.SetEnabled(propertyEnabled);
 
         protected override void UpdateIndeterminate() => visible = !indeterminate;
 
-        void OnPickObject() => CustomObjectPicker.Pick(m_Provider.portType, m_textureDimension, SelectHandler);
-
-        void SelectHandler(UnityObject obj, bool isCanceled)
+        private void OnValueChanged(ChangeEvent<UnityObject> evt)
         {
-            if (!isCanceled)
-            {
-                SetValue(obj);
-                NotifyValueChanged();
-            }
-        }
-
-        void OnClickToShow(ClickEvent evt)
-        {
-            EditorGUI.PingObjectOrShowPreviewOnClick(m_Value, Rect.zero);
-        }
-
-        bool CanDrag()
-        {
-            if (DragAndDrop.objectReferences.Length == 1)
-            {
-                var type = DragAndDrop.objectReferences[0].GetType();
-                if (m_Provider.portType.IsAssignableFrom(type))
-                {
-                    return true;
-                }
-
-                if (m_textureDimension != TextureDimension.Unknown && DragAndDrop.objectReferences[0] is Texture texture)
-                {
-                    return texture.dimension == m_textureDimension;
-                }
-            }
-
-            return false;
-        }
-
-        void OnDragEnter(DragEnterEvent evt)
-        {
-            DragAndDrop.visualMode = CanDrag() ? DragAndDropVisualMode.Link : DragAndDropVisualMode.Rejected;
-            evt.StopPropagation();
-        }
-
-        void OnDragUpdate(DragUpdatedEvent evt)
-        {
-            DragAndDrop.visualMode = CanDrag() ? DragAndDropVisualMode.Link : DragAndDropVisualMode.Rejected;
-            evt.StopPropagation();
-        }
-
-        void OnDragPerformed(DragPerformEvent evt)
-        {
-            var dragObject = DragAndDrop.objectReferences.First();
-            SelectHandler(dragObject, false);
-        }
-
-        void OnProjectOrHierarchyChanged()
-        {
-            CheckForMissingReference();
-        }
-
-        void CheckForMissingReference()
-        {
-            if (m_Value == null)
-            {
-                m_TextField.value = !object.ReferenceEquals(m_Value, null) && m_Value.GetInstanceID() != 0
-                    ? $"Missing ({m_Provider.portType.Name})"
-                    : $"None ({m_Provider.portType.Name})";
-            }
-            else
-            {
-                m_TextField.value = m_Value.name;
-            }
+            SetValue(evt.newValue);
+            NotifyValueChanged();
         }
     }
 }
