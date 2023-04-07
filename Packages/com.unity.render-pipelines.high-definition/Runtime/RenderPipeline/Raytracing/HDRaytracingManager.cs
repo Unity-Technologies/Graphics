d12b4c152b9f50765b5eb284e4829fdb124f84ab
Justin Schwartz <justin.schwartz@unity3d.com>
2023-04-07 22:11:52 +0000

    2023.1/graphics/hd/find by type none
    Usage of FindObjectsByType was implemented to address performance concerns, but was still sorting the returned Objects which was the cause of the initial issue. Removing the need to sort the returned Objects addresses the performance issue and does not affect the final result.

--- Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/Raytracing/HDRaytracingManager.cs
+++ Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/Raytracing/HDRaytracingManager.cs
@@ -564,7 +564,7 @@ namespace UnityEngine.Rendering.HighDefinition
             m_RayTracingLights.hdLightEntityArray.AddRange(m_RayTracingLights.hdRectLightArray);
 
             // Process the lights
-            HDAdditionalReflectionData[] reflectionProbeArray = UnityEngine.GameObject.FindObjectsByType<HDAdditionalReflectionData>(FindObjectsSortMode.InstanceID);
+            HDAdditionalReflectionData[] reflectionProbeArray = UnityEngine.GameObject.FindObjectsByType<HDAdditionalReflectionData>(FindObjectsSortMode.None);
             for (int reflIdx = 0; reflIdx < reflectionProbeArray.Length; ++reflIdx)
             {
                 HDAdditionalReflectionData reflectionProbe = reflectionProbeArray[reflIdx];
