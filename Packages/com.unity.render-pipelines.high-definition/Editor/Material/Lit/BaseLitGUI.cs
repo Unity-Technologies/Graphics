52d43ceb50737ca67284914cf5356327c9b7d0b4
Adrien de Tocqueville <adrien.tocqueville@unity3d.com>
2023-04-18 05:06:08 +0000

    [2021.3] [HDRP] Fix forward only material validation
    Backport https://github.cds.internal.unity3d.com/unity/unity/pull/19836

--- Packages/com.unity.render-pipelines.high-definition/Editor/Material/Lit/BaseLitGUI.cs
+++ Packages/com.unity.render-pipelines.high-definition/Editor/Material/Lit/BaseLitGUI.cs
@@ -145,7 +145,9 @@ namespace UnityEditor.Rendering.HighDefinition
 
         static public void SetupStencil(Material material, bool receivesSSR, bool useSplitLighting)
         {
-            bool forwardOnly = !material.HasPass("GBuffer");
+            // To determine if the shader is forward only, we can't rely on the presence of GBuffer pass because that depends on the active subshader, which
+            // depends on the active render pipeline, giving an inconsistent result. The properties of a shader are always the same so it's ok to check them
+            bool forwardOnly = material.shader.FindPropertyIndex(kZTestGBuffer) == -1;
 
             ComputeStencilProperties(forwardOnly, receivesSSR, useSplitLighting, out int stencilRef, out int stencilWriteMask,
                 out int stencilRefDepth, out int stencilWriteMaskDepth, out int stencilRefGBuffer, out int stencilWriteMaskGBuffer,
