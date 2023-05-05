92914c7238482bbdb3b0f2dee24c8e5fec660d39
Robin Bradley <robinb@unity3d.com>
2023-05-05 19:28:00 +0000

    [content automatically redacted] touching Platforms folder

--- Packages/com.unity.render-pipelines.core/ShaderLibrary/FoveatedRendering.hlsl
+++ Packages/com.unity.render-pipelines.core/ShaderLibrary/FoveatedRendering.hlsl
@@ -8,14 +8,6 @@
 # if defined(SHADER_API_METAL)
 #   include "Packages/com.unity.render-pipelines.core/ShaderLibrary/API/FoveatedRendering_Metal.hlsl"
 # endif
-
-// Adapt old remap functions to their new name
-float2 RemapFoveatedRenderingResolve(float2 uv) { return RemapFoveatedRenderingLinearToNonUniform(uv); }
-float2 RemapFoveatedRenderingPrevFrameResolve(float2 uv) {return RemapFoveatedRenderingPrevFrameLinearToNonUniform(uv); }
-float2 RemapFoveatedRenderingDistort(float2 uv) { return RemapFoveatedRenderingNonUniformToLinear(uv); }
-float2 RemapFoveatedRenderingPrevFrameDistort(float2 uv) { return RemapFoveatedRenderingPrevFrameNonUniformToLinear(uv); }
-int2 RemapFoveatedRenderingDistortCS(int2 positionCS, bool yflip) { return RemapFoveatedRenderingNonUniformToLinearCS(positionCS, yflip); }
-
 #endif
 
 #endif // UNITY_FOVEATED_RENDERING_INCLUDED
