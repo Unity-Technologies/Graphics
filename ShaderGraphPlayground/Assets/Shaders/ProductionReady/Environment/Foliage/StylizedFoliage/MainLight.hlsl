d3ee3fe3edfd1573199c8d16087c51ec0355f8ce
Tracy Chen <s.chen@unity3d.com>
2023-05-23 14:54:29 -0700

    Adds ice, stylized foliage, and sg urp lit shaders to ShaderGraphPlayGround

---
+++ ShaderGraphPlayground/Assets/Shaders/ProductionReady/Environment/Foliage/StylizedFoliage/MainLight.hlsl
@@ -0,0 +1,22 @@
+
+void MainLight_half(float3 WorldPos, out half3 Direction, out half3 Color, out half DistanceAtten, out half ShadowAtten)
+{
+#if SHADERGRAPH_PREVIEW
+    Direction = half3(0.5, 0.5, 0);
+    Color = 1;
+    DistanceAtten = 1;
+    ShadowAtten = 1;
+#else
+#if SHADOWS_SCREEN
+    half4 clipPos = TransformWorldToHClip(WorldPos);
+    half4 shadowCoord = ComputeScreenPos(clipPos);
+#else
+    half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+#endif
+    Light mainLight = GetMainLight(shadowCoord);
+    Direction = mainLight.direction;
+    Color = mainLight.color;
+    DistanceAtten = mainLight.distanceAttenuation;
+    ShadowAtten = mainLight.shadowAttenuation;
+#endif
+}
