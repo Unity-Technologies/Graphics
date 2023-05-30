3353b7982a9a04e67ab3adef5b32c5e92b2b3647
Sai Prasad Gogulakonda <sgogulakonda@rythmos.com>
2023-05-30 11:25:42 +0000

    2022.2: [URP] Fix for the UI being drawn twice in some scenarios on URP project
    The issue is UI being drawn twice in some scenarios on URP project. 
    This PR solves the above issue.
    
    Bug: UUM-32848
    Backport: UUM-33284

--- Packages/com.unity.render-pipelines.universal/Runtime/UniversalRenderPipeline.cs
+++ Packages/com.unity.render-pipelines.universal/Runtime/UniversalRenderPipeline.cs
@@ -602,14 +602,13 @@ namespace UnityEngine.Rendering.Universal
 
                 SetupPerCameraShaderConstants(cmd);
 
-#if UNITY_EDITOR
-                // Emit scene view UI
-                if (isSceneViewCamera)
-                    ScriptableRenderContext.EmitWorldGeometryForSceneView(camera);
-                else
-#endif
-                if (cameraData.camera.targetTexture != null && cameraData.cameraType != CameraType.Preview)
+                // Emit scene/game view UI. The main game camera UI is always rendered, so this needs to be handled only for different camera types
+                if (camera.cameraType == CameraType.Reflection || camera.cameraType == CameraType.Preview)
                     ScriptableRenderContext.EmitGeometryForCamera(camera);
+#if UNITY_EDITOR
+                 else if (isSceneViewCamera)
+                    ScriptableRenderContext.EmitWorldGeometryForSceneView(camera);
+#endif
 
                 // Update camera motion tracking (prev matrices) from cameraData.
                 // Called and updated only once, as the same camera can be rendered multiple times.
