2fff59727735e3fcf02c1b98e8821a43ccb1dd81
Mircea Catana <mircea.catana@unity3d.com>
2023-06-21 09:49:42 +0000

    [2022.3][metal] Backport Previous Frame Remap Functions for Foveated Rendering
    Backport missing functions

--- Packages/com.unity.render-pipelines.core/ShaderLibrary/API/FoveatedRendering_Metal.hlsl
+++ Packages/com.unity.render-pipelines.core/ShaderLibrary/API/FoveatedRendering_Metal.hlsl
@@ -11,7 +11,7 @@
     float3 _UV_HlslccVRRResolve0 = float3(0.0, 0.0, 0.0);
     float3 _UV_HlslccVRRResolve1 = float3(0.0, 0.0, 0.0);
 
-    float2 RemapFoveatedRenderingResolve(float2 uv, bool yFlip = false)
+    float2 RemapFoveatedRenderingLinearToNonUniform(float2 uv, bool yFlip = false)
     {
         if (yFlip)
             uv.y = 1.0 - uv.y;
@@ -37,18 +37,30 @@
         return uv;
     }
 
+    float2 RemapFoveatedRenderingPrevFrameLinearToNonUniform(float2 uv, bool yFlip = false)
+    {
+        // TODO : implement me to support eye tracking that can change the remap each frame
+        return RemapFoveatedRenderingLinearToNonUniform(uv, yFlip);
+    }
+
     float2 RemapFoveatedRenderingDensity(float2 uv, bool yFlip = false)
     {
         // TODO: Implement density look up
         return uv;
     }
 
-    float2 RemapFoveatedRenderingDistort(float2 uv, bool yFlip = false)
+    float2 RemapFoveatedRenderingPrevFrameDensity(float2 uv, bool yFlip = false)
+    {
+        // TODO : implement me to support eye tracking that can change the remap each frame
+        return RemapFoveatedRenderingDensity(uv, yFlip);
+    }
+
+    float2 RemapFoveatedRenderingNonUniformToLinear(float2 uv, bool yFlip = false)
     {
         if (yFlip)
             uv.y = 1.0 - uv.y;
 
-        // NOTE: Check comment for similar code in RemapFoveatedRenderingResolve
+        // NOTE: Check comment for similar code in RemapFoveatedRenderingLinearToNonUniform
         if (unity_StereoEyeIndex == 1)
         {
             uv += _UV_HlslccVRRDistort0.yz;
@@ -66,14 +78,20 @@
         return uv;
     }
 
-    float2 RemapFoveatedRenderingDistortCS(float2 uv, bool yFlip = false)
+    float2 RemapFoveatedRenderingPrevFrameNonUniformToLinear(float2 uv, bool yFlip = false)
+    {
+        // TODO : implement me to support eye tracking that can change the remap each frame
+        return RemapFoveatedRenderingNonUniformToLinear(uv, yFlip);
+    }
+
+    float2 RemapFoveatedRenderingNonUniformToLinearCS(float2 uv, bool yFlip = false)
     {
         uv /= _ScreenSize.xy;
 
         if (yFlip)
             uv.y = 1.0 - uv.y;
 
-        // NOTE: Check comment for similar code in RemapFoveatedRenderingResolve
+        // NOTE: Check comment for similar code in RemapFoveatedRenderingLinearToNonUniform
         if (unity_StereoEyeIndex == 1)
         {
             uv += _UV_HlslccVRRDistort0.yz;
@@ -91,6 +109,14 @@
         return uv * _ScreenSize.xy;
     }
 
+    // Adapt old remap functions to their new name
+    float2 RemapFoveatedRenderingResolve(float2 uv) { return RemapFoveatedRenderingLinearToNonUniform(uv); }
+    float2 RemapFoveatedRenderingPrevFrameResolve(float2 uv) {return RemapFoveatedRenderingPrevFrameLinearToNonUniform(uv); }
+    float2 RemapFoveatedRenderingDistort(float2 uv) { return RemapFoveatedRenderingNonUniformToLinear(uv); }
+    float2 RemapFoveatedRenderingPrevFrameDistort(float2 uv) { return RemapFoveatedRenderingPrevFrameNonUniformToLinear(uv); }
+    int2 RemapFoveatedRenderingDistortCS(int2 positionCS, bool yflip) { return RemapFoveatedRenderingNonUniformToLinearCS(positionCS, yflip); }
+
 #endif // SHADER_API_METAL && _FOVEATED_RENDERING_NON_UNIFORM_RASTER && !UNITY_COMPILER_DXC
 
 #endif // UNITY_FOVEATED_RENDERING_METAL_INCLUDED
+
