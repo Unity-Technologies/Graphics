bf21beb06549af3fefe0d861756c8cbfbd7c4b07
Arttu Peltonen <arttu.peltonen@unity3d.com>
2023-06-29 18:36:22 +0000

    [RPW] Port VolumeEditor to UITK and implement UX improvements
    This PR reimplements the VolumeEditor with UITK and implements a couple of UX improvements.
    
    JIRAs: 
    - [XPIPELINE-681](https://jira.unity3d.com/browse/XPIPELINE-681)
    - [XPIPELINE-72](https://jira.unity3d.com/browse/XPIPELINE-72)
    
    Before:
    ![image](https://media.github.cds.internal.unity3d.com/user/3380/files/49d4a454-a52b-43ef-aecc-2a7724192ec4)
    
    After:
    ![image](https://media.github.cds.internal.unity3d.com/user/3380/files/795b25a9-f53f-45a7-b5e7-90807fff97b3)
    
    What's changed
    - Reimplemented VolumeEditor with UITK (couple of IMGUIContainers still used)
    - Implemented new "Asset-style Header" ([XPIPELINE-681](https://jira.unity3d.com/browse/XPIPELINE-681))
      - As all new UI must be UITK, that meant the entire editor needed to be ported to UITK 
      - "New" button is only visible if "None" is selected
      - Added new info message, visible if "None" is selected
      - New profile context menu for the assigned volume profile
      - "Clone" button moved under the new profile context menu
      - "Enable All/Disable All/Remove All" actions moved from Volume context menu to the new profile context menu
    - Changed "Clone" action to use the name of the GameObject instead of the cloned profile ([XPIPELINE-72](https://jira.unity3d.com/browse/XPIPELINE-72))
    - Bugfixes
      - Fixed VolumeProfile "Copy Settings" context menu action sorting the components in alphabetical order
      - Fixed some "Fix Me" boxes related to colliders not working correctly with Undo/Redo

---
+++ Packages/com.unity.render-pipelines.core/Editor/Utilities/LocalizationHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace UnityEditor.Rendering.Utilities
+{
+    // Temporary helper class duplicated from GraphicsSettingsUtils.cs while better UXML localization support is not available.
+    internal class LocalizationHelper
+    {
+        static void Localize(VisualElement visualElement, Func<VisualElement, string> get, Action<VisualElement, string> set)
+        {
+            var extractedText = get.Invoke(visualElement);
+            if (string.IsNullOrWhiteSpace(extractedText))
+                return;
+
+            var localizedString = L10n.Tr(extractedText);
+            set.Invoke(visualElement, localizedString);
+        }
+
+        internal static void LocalizeTooltip(VisualElement visualElement)
+        {
+            Localize(visualElement, e => e.tooltip, (e, s) => e.tooltip = s);
+        }
+
+        internal static void LocalizeText(Label label)
+        {
+            Localize(label, e => ((Label)e).text, (e, s) => ((Label)e).text = s);
+        }
+
+        internal static void LocalizePropertyField(PropertyField propertyField)
+        {
+            Localize(propertyField, e => ((PropertyField)e).label, (e, s) => ((PropertyField)e).label = s);
+        }
+
+        internal static void LocalizeVisualTree(VisualElement root)
+        {
+            root.Query<VisualElement>().ForEach(LocalizeTooltip);
+            root.Query<PropertyField>().ForEach(LocalizePropertyField);
+            root.Query<Label>().ForEach(label =>
+            {
+                if (label.ClassListContains("unity-object-field-display__label"))
+                    return;
+                LocalizeText(label);
+            });
+        }
+    }
+}
