1bd18854594153562a7622c99655900c8d4253cf
Peter Kuhn <peter@unity3d.com>
2023-07-31 22:27:42 +0000

    2022.3/visionos/fixes
    Fixes for 2022.3 visionOS:
    
     * VR mode (fully immersive) now renders on device
     * Fix crashes on simulator in windowed and VR modes

--- Packages/com.unity.render-pipelines.core/Runtime/Textures/TextureXR.cs
+++ Packages/com.unity.render-pipelines.core/Runtime/Textures/TextureXR.cs
@@ -42,6 +42,7 @@ namespace UnityEngine.Rendering
                     case GraphicsDeviceType.PlayStation5:
                     case GraphicsDeviceType.PlayStation5NGGC:
                     case GraphicsDeviceType.Vulkan:
+                    case GraphicsDeviceType.Metal:
                         return true;
 
                     default:
