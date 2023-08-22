727f50df7359dac824a58efd21911e1968587a53
Elvar Örn Unnþórsson <elvar.orn@unity3d.com>
2023-08-22 08:24:51 +0000

    [URP][2023.3] Exposing some properties and adding 8K Shadow map option
    The main purpose of the PR is to:
    - Add 8K Shadow Texture option for Main and Additional Lights.
    - Address user feedback regarding API being internal but should be made public. These are:
      - mainLightShadowmapResolution
      - additionalLightsShadowmapResolution
      - cascade2Split
      - cascade3Split
      - cascade4Split
    
    On top of that the PR:
    - cleans up the code around properties in `UniversalRenderPipelineAsset.cs`.
    - adds a callback interface for any script that needs to do runtime changes in our test projects
    - adds a Shadow Setting test script to change the new exposed API
    - adds a test scene using 8K shadow textures

--- Tests/SRPTests/Packages/com.unity.testing.urp/Scripts/Runtime/SetQualityLevelOnAwake.cs
+++ Tests/SRPTests/Packages/com.unity.testing.urp/Scripts/Runtime/SetQualityLevelOnAwake.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
 
+public abstract class SetQualityCallbackObject : MonoBehaviour
+{
+    public abstract void BeforeChangingQualityLevel(int prevQualityLevelIndex, int newQualityLevelIndex);
+    public abstract void BeforeRevertingQualityLevel(int prevQualityLevelIndex, int newQualityLevelIndex);
+}
+
 public class SetQualityLevelOnAwake : MonoBehaviour
 {
     public int qualityLevelIndex;
+    public SetQualityCallbackObject[] callbacks;
 
     private int prevQualityLevelIndex;
     private string[] qualityLevelNames;
 
     void Awake()
     {
+        InvokeBeforeBeforeChangingCallbacks();
+
         qualityLevelNames = QualitySettings.names;
         prevQualityLevelIndex = QualitySettings.GetQualityLevel();
 
@@ -28,10 +37,10 @@ public class SetQualityLevelOnAwake : MonoBehaviour
 
     void OnDisable()
     {
+        InvokeBeforeRevertingCallbacks();
+
         if (qualityLevelIndex >= qualityLevelNames.Length)
-        {
             return;
-        }
 
         /*int curIndex = QualitySettings.GetQualityLevel();
         int nextIndex = prevQualityLevelIndex;
@@ -39,5 +48,23 @@ public class SetQualityLevelOnAwake : MonoBehaviour
         string next = qualityLevelNames[nextIndex];
         Debug.Log("SetQualityLevelOnAwake.OnDisable():\nSwitching from " + cur + "(" + curIndex + ") to " + next + "(" + nextIndex + ")");*/
         QualitySettings.SetQualityLevel(prevQualityLevelIndex, true);
+    }
+
+    private void InvokeBeforeBeforeChangingCallbacks()
+    {
+        if (callbacks == null)
+            return;
+
+        for (int i = 0; i < callbacks.Length; i++)
+            callbacks[i]?.BeforeChangingQualityLevel(prevQualityLevelIndex, qualityLevelIndex);
+    }
+
+    private void InvokeBeforeRevertingCallbacks()
+    {
+        if (callbacks == null)
+            return;
+
+        for (int i = 0; i < callbacks.Length; i++)
+            callbacks[i]?.BeforeRevertingQualityLevel(prevQualityLevelIndex, qualityLevelIndex);
     }
 }
