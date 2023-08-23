53e3062900e80c6402b5d4df20fa795825df8a39
Elvar Örn Unnþórsson <elvar.orn@unity3d.com>
2023-08-23 01:15:36 +0000

    [URP][2023.3] Resetting Shader IDs in the DepthNormals pass after rendering (UUM-36069)
    Fixes UUM-36069

--- Packages/com.unity.render-pipelines.universal/Runtime/UniversalRendererRenderGraph.cs
+++ Packages/com.unity.render-pipelines.universal/Runtime/UniversalRendererRenderGraph.cs
@@ -678,6 +678,7 @@ namespace UnityEngine.Rendering.Universal
                 m_DeferredPass.OnCameraCleanup(renderingData.commandBuffer);
 
             m_CopyDepthPass.OnCameraCleanup(renderingData.commandBuffer);
+            m_DepthNormalPrepass.OnCameraCleanup(renderingData.commandBuffer);
         }
 
         private static bool m_CreateColorTexture;
