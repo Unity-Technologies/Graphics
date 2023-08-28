34aa6b7ce2f990fdabbf4219cc9379ed9d374e56
Paul Demeulenaere <pauld@unity3d.com>
2023-08-28 16:22:12 +0000

    [VFX] Fix Create Asset From None VFX View (to 2023.2)
    Backport of https://github.cds.internal.unity3d.com/unity/unity/pull/32052

--- Tests/SRPTests/Projects/VisualEffectGraph_HDRP/Assets/AllTests/Editor/Tests/VFXTemplateWindowTest.cs
+++ Tests/SRPTests/Projects/VisualEffectGraph_HDRP/Assets/AllTests/Editor/Tests/VFXTemplateWindowTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -247,6 +248,173 @@ namespace UnityEditor.VFX.Test
             Assert.AreEqual(0, controller.contexts.Count());
         }
 
+        private static void CreateAndOpenVFX(int i, out string path)
+        {
+            var graph = VFXTestCommon.CreateGraph_And_System();
+            var inlineOperator = ScriptableObject.CreateInstance<VFXInlineOperator>();
+            inlineOperator.SetSettingValue("m_Type", (SerializableType)typeof(int));
+            inlineOperator.inputSlots[0].value = i;
+            graph.AddChild(inlineOperator);
+            path = AssetDatabase.GetAssetPath(graph);
+            AssetDatabase.ImportAsset(path);
+
+            var asset = AssetDatabase.LoadAssetAtPath<VisualEffectAsset>(path);
+            Assert.IsTrue(VisualEffectAssetEditor.OnOpenVFX(asset.GetInstanceID(), 0));
+            var window = VFXViewWindow.GetWindow(asset, true);
+            window.LoadAsset(asset, null);
+            window.Show();
+        }
+
+        private static VFXViewWindow GetWindowFromPath(string path)
+        {
+            var vfx = AssetDatabase.LoadAssetAtPath<VisualEffectAsset>(path);
+            return VFXViewWindow.GetWindow(vfx);
+        }
+
+        [UnityTest, Description("Relative to UUM-41334, cover basic asset replacement.")]
+        public IEnumerator VFXView_Open_Three_View_Replace_Two_Content()
+        {
+            CreateAndOpenVFX(666, out var pathA);
+            CreateAndOpenVFX(777, out var pathB);
+            CreateAndOpenVFX(888, out var pathC);
+            yield return null;
+
+            var content = File.ReadAllBytes(pathA);
+            File.WriteAllBytes(pathB, content);
+            File.WriteAllBytes(pathC, content);
+            AssetDatabase.ImportAsset(pathB);
+            AssetDatabase.ImportAsset(pathC);
+            yield return null;
+
+            var resources = VFXViewWindow.GetAllWindows()
+                .Select(o => o.displayedResource.ToString())
+                .ToArray();
+
+            Assert.AreEqual(3, resources.Length);
+            Assert.AreEqual(resources.Length, resources.Distinct().Count());
+
+            var pathCResource = GetWindowFromPath(pathC).displayedResource;
+            var getWindow = VFXViewWindow.GetWindow(pathCResource);
+            Assert.IsNotNull(getWindow);
+
+            //Close now all windows to track potential failure
+            VFXViewWindow.GetAllWindows().ToList().ForEach(x => x.Close());
+
+            yield return null;
+        }
+
+        [UnityTest, Description("Relative to UUM-41334, cover create new from an empty window.")]
+        public IEnumerator VFXView_Create_From_No_Asset_Reuse_Same_View()
+        {
+            CreateAndOpenVFX(159, out var path);
+
+            Assert.AreEqual(1, VFXViewWindow.GetAllWindows().Count());
+            AssetDatabase.DeleteAsset(path);
+
+            //Insure the displayedResource has been correctly updated between "null" and null
+            //not really needed but ease debug and more realistic regarding user real usage
+            int maxFrame = 16;
+            while (!object.ReferenceEquals(VFXViewWindow.GetAllWindows().First().displayedResource, null) && --maxFrame > 0)
+            {
+                VFXViewWindow.GetAllWindows().First().Focus();
+                Assert.AreEqual(1, VFXViewWindow.GetAllWindows().Count());
+                yield return null;
+            }
+            Assert.Greater(maxFrame, 0);
+
+            var templateWindow = EditorWindow.GetWindow<VFXTemplateWindow>(true, "Create_From_No_Asset_Reuse_Same_View", false);
+            Assert.NotNull(templateWindow);
+            var treeView = GetTreeView(templateWindow);
+            treeView.selectedIndex = 2;
+
+            var oldWindow = VFXViewWindow.GetAllWindows().First();
+            var newPath = $"{VFXTestCommon.tempBasePath}vfx_from_template_{System.Guid.NewGuid()}.vfx";
+            var mockSaveFileDialogHelper = new MockSaveFileDialogHelper(newPath);
+            SetSaveFileDialogHelper(templateWindow, mockSaveFileDialogHelper);
+            CallMethod(templateWindow, "Setup", oldWindow.graphView, /*VFXTemplateWindow.CreateMode.CreateNew*/0, (Action<string>)null);
+            CallMethod(templateWindow, "OnCreate");
+            Assert.AreEqual(1u, mockSaveFileDialogHelper.callCount);
+            yield return null;
+
+            Assert.IsTrue(File.Exists(newPath));
+            Assert.AreEqual(1, VFXViewWindow.GetAllWindows().Count());
+
+            var newWindow = VFXViewWindow.GetAllWindows().First();
+            Assert.AreEqual(oldWindow, newWindow);
+            Assert.IsTrue(newWindow.displayedResource != null);
+            var openedAsset = AssetDatabase.GetAssetPath(newWindow.displayedResource.asset);
+            Assert.AreEqual(newPath, openedAsset);
+            yield return null;
+        }
+
+        [UnityTest, Description("Repro UUM-41334")]
+        public IEnumerator VFXView_Open_Two_View_Delete_Replace_Other_Content()
+        {
+            Assert.AreEqual(0, VFXViewWindow.GetAllWindows().Count());
+
+            CreateAndOpenVFX(123, out var pathA);
+            CreateAndOpenVFX(456, out var pathB);
+
+            Assert.AreNotEqual(pathA, pathB);
+            var windowA = GetWindowFromPath(pathA);
+            var windowB = GetWindowFromPath(pathB);
+            Assert.AreNotEqual(windowA, windowB);
+            Assert.IsTrue(windowA.displayedResource != null);
+            Assert.IsTrue(windowB.displayedResource != null);
+
+            //Delete ObjectA
+            AssetDatabase.DeleteAsset(pathA);
+            yield return null;
+            windowA = GetWindowFromPath(pathA);
+            windowB = GetWindowFromPath(pathB);
+            Assert.IsFalse(windowA.displayedResource != null);
+            Assert.IsTrue(windowB.displayedResource != null);
+
+            //Insure the displayedResource has been correctly updated between "null" and null (not really needed but ease later debug)
+            int maxFrame = 16;
+            while (!object.ReferenceEquals(windowA.displayedResource, null) && --maxFrame > 0)
+            {
+                windowA.Focus();
+                yield return null;
+            }
+            Assert.Greater(maxFrame, 0);
+
+            //windowsA simulate creation of new asset with same path than windowB
+            var templateWindow = EditorWindow.GetWindow<VFXTemplateWindow>(true, "Open_Two_View_Delete_Replace_Other_Content", false);
+            Assert.NotNull(templateWindow);
+            var treeView = GetTreeView(templateWindow);
+            treeView.selectedIndex = 2;
+
+            var mockSaveFileDialogHelper = new MockSaveFileDialogHelper(pathB);
+            SetSaveFileDialogHelper(templateWindow, mockSaveFileDialogHelper);
+            CallMethod(templateWindow, "Setup", windowA.graphView, /*VFXTemplateWindow.CreateMode.CreateNew*/0, (Action<string>)null);
+            Assert.IsTrue(windowB.displayedResource != null);
+            CallMethod(templateWindow, "OnCreate");
+            Assert.AreNotEqual(0, mockSaveFileDialogHelper.callCount);
+            yield return null;
+
+            windowA = GetWindowFromPath(pathA);
+            windowB = GetWindowFromPath(pathB);
+
+            //We are supposed to switch back to the other single windows
+            Assert.IsTrue(File.Exists(pathB));
+            Assert.IsTrue(windowB.displayedResource != null);
+            Assert.IsFalse(windowA.displayedResource != null);
+
+            //Focus is expected to be have been requested
+            maxFrame = 16;
+            while (!windowB.hasFocus && --maxFrame > 0)
+            {
+                yield return null;
+            }
+            Assert.Greater(maxFrame, 0);
+
+            //Check if expected path still opened
+            var displayedPath = AssetDatabase.GetAssetPath(windowB.displayedResource.asset);
+            Assert.AreEqual(pathB, displayedPath);
+
+            yield return null;
+        }
 
         private IEnumerator CheckNewVFXIsCreated()
         {
@@ -306,11 +474,11 @@ namespace UnityEditor.VFX.Test
             return treeView;
         }
 
-        private void CallMethod(VFXTemplateWindow window, string methodName)
+        private void CallMethod(VFXTemplateWindow window, string methodName, params object[] paramMethod)
         {
             var method = window.GetType().GetMethod(methodName, BindingFlags.Instance | BindingFlags.NonPublic);
             Assert.NotNull(method);
-            method.Invoke(window, null);
+            method.Invoke(window, paramMethod);
         }
 
         private void Cleanup()
