0ea93242830c6db98a06b475d1093e6c5f5709ea
Jannah Mekhaemar <jannah.mekhaemar@unity3d.com>
2023-09-08 12:34:20 +0000

    [2022.3] [URP] Ensure HDR output can be turned off by script in the editor
    HDR Output is automatically turned on (by the URP) in the editor if available, so the game view can be the closest to the final result once built and run on an HDR display. However it would turn HDR on even if the users explicitly disabled it during playmode.
    This PR ensures the URP takes into account calls to `HDROutputSettings.main.RequestHDRModeChange(false)` in the editor, by enabling HDR output only once when it detects it as available for the first time.

--- Packages/com.unity.render-pipelines.universal/Runtime/UniversalRenderPipeline.cs
+++ Packages/com.unity.render-pipelines.universal/Runtime/UniversalRenderPipeline.cs
@@ -1747,13 +1747,17 @@ namespace UnityEngine.Rendering.Universal
             return !Application.isMobilePlatform && hdrOutputSupported && hdrOutputActive;
         }
 
+        // We only want to enable HDR Output for the game view once
+        // since the game itself might want to control this
+        internal bool enableHDROnce = true;
+
         /// <summary>
         /// Configures the render pipeline to render to HDR output or disables HDR output.
         /// </summary>
 #if UNITY_2021_1_OR_NEWER
-        static void SetHDRState(List<Camera> cameras)
+        void SetHDRState(List<Camera> cameras)
 #else
-        static void SetHDRState(Camera[] cameras)
+        void SetHDRState(Camera[] cameras)
 #endif
         {
             bool hdrOutputActive = HDROutputSettings.main.available && HDROutputSettings.main.active;
@@ -1782,10 +1786,11 @@ namespace UnityEngine.Rendering.Universal
                     requestedHDRModeChange = hdrOutputActive;
                     HDROutputSettings.main.RequestHDRModeChange(false);
                 }
-                else
+                else if (enableHDROnce)
                 {
                     requestedHDRModeChange = !hdrOutputActive;
                     HDROutputSettings.main.RequestHDRModeChange(true);
+                    enableHDROnce = false;
                 }
             }
 
