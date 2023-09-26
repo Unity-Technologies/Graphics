b8a19d6aea01a2ef2035990bfee78fb18d20e1ac
Esmeralda Salamone <esmeralda.salamone@unity3d.com>
2023-09-26 23:47:24 +0000

    [ShaderGraph] Fixed NAND node and other issues
    Bugfixes for the following:
    https://jira.unity3d.com/browse/UUM-46774
    https://jira.unity3d.com/browse/UUM-47325
    https://jira.unity3d.com/browse/UUM-47824

--- Packages/com.unity.shadergraph/Editor/Drawing/Views/GraphEditorView.cs
+++ Packages/com.unity.shadergraph/Editor/Drawing/Views/GraphEditorView.cs
@@ -412,9 +412,9 @@ namespace UnityEditor.ShaderGraph.Drawing
             m_SearchWindowProvider.connectedPort = null;
             m_SearchWindowProvider.target = c.target ?? m_HoveredContextView;
             var displayPosition = (c.screenMousePosition - m_EditorWindow.position.position);
-
+            displayPosition = graphView.cachedMousePosition;
             SearcherWindow.Show(m_EditorWindow, (m_SearchWindowProvider as SearcherProvider).LoadSearchWindow(),
-                item => (m_SearchWindowProvider as SearcherProvider).OnSearcherSelectEntry(item, c.screenMousePosition - m_EditorWindow.position.position),
+                item => (m_SearchWindowProvider as SearcherProvider).OnSearcherSelectEntry(item, displayPosition),
                 displayPosition, null, new SearcherWindow.Alignment(SearcherWindow.Alignment.Vertical.Center, SearcherWindow.Alignment.Horizontal.Left));
         }
 
