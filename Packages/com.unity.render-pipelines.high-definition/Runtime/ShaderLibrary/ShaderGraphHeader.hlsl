2a420e7830ed1ba29f618e68f5c30f8bc3fa8be6
Reach Platform Support <svc-reach-platform-support@unity3d.com>
2023-09-28 22:01:27 +0000

    [Port] [2023.2] Restore shadergraph header for HDRP
    Fixes missing shadergraph include on HDRP see https://jira.unity3d.com/browse/UUM-51851

---
+++ Packages/com.unity.render-pipelines.high-definition/Runtime/ShaderLibrary/ShaderGraphHeader.hlsl
@@ -0,0 +1,8 @@
+#ifndef UNITY_HEADER_HD_INCLUDED
+#define UNITY_HEADER_HD_INCLUDED
+
+// Always include Shader Graph version
+// Always include last to avoid double macros
+#include "Packages/com.unity.shadergraph/ShaderGraphLibrary/Functions.hlsl"
+
+#endif // UNITY_HEADER_HD_INCLUDED
