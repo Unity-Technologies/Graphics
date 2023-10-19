d29f74a57acabbb4aa0c547dd4dfa7f774584bff
Adrien de Tocqueville <adrien.tocqueville@unity3d.com>
2023-10-19 06:48:52 +0000

    Fix error when enabling debug modes with volumetric clouds
    Reported here https://unity.slack.com/archives/C6Y79CZM0/p1696952455221199
    Fixed error in the console when enbling a debug mode and volumetric clouds are on
    
    This acutally fixes a more serious issue where volumetric clouds shadow would be computed on all directional lights
    introduced by https://github.cds.internal.unity3d.com/unity/unity/pull/19281

--- Packages/com.unity.render-pipelines.high-definition/Runtime/Lighting/LightEvaluation.hlsl
+++ Packages/com.unity.render-pipelines.high-definition/Runtime/Lighting/LightEvaluation.hlsl
@@ -234,12 +234,6 @@ float4 EvaluateLight_Directional(LightLoopContext lightLoopContext, PositionInpu
     }
 #endif
 
-#ifndef LIGHT_EVALUATION_NO_CLOUDS_SHADOWS
-    // Apply the volumetric cloud shadow if relevant
-    if (_VolumetricCloudsShadowOriginToggle.w == 1.0)
-        color.a *= EvaluateVolumetricCloudsShadows(lightLoopContext, light, posInput.positionWS);
-#endif
-
     return color;
 }
 
