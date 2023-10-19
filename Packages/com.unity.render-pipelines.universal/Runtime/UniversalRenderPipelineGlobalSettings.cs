0fb8db1e3f4746d38c11c84d3548fda66ae12517
Jonas Sideravičius <jonass@unity3d.com>
2023-10-19 19:16:10 +0000

    [URP][2022.3] Fixing rendering layers not updated when opening a project. (UUM-44741)
    Fixes UUM-44741.
    The issue was that the property for getting rendering layers didn't update until the user clicked on a renderer in a scene. The fix is to make sure the property checks if it has been initialized when retrieving it.

--- Packages/com.unity.render-pipelines.universal/Runtime/UniversalRenderPipelineGlobalSettings.cs
+++ Packages/com.unity.render-pipelines.universal/Runtime/UniversalRenderPipelineGlobalSettings.cs
@@ -216,8 +216,16 @@ namespace UnityEngine.Rendering.Universal
         [SerializeField]
         uint m_ValidRenderingLayers;
         /// <summary>Valid rendering layers that can be used by graphics. </summary>
-        public uint validRenderingLayers => m_ValidRenderingLayers;
+        public uint validRenderingLayers
+        {
+            get
+            {
+                if (m_PrefixedRenderingLayerNames == null)
+                    UpdateRenderingLayerNames();
 
+                return m_ValidRenderingLayers;
+            }
+        }
         /// <summary>Regenerate Rendering Layer names and their prefixed versions.</summary>
         internal void UpdateRenderingLayerNames()
         {
