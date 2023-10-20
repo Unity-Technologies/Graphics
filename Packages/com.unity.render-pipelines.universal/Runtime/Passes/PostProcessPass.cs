fdd48321b3985d662580e748a7edb2fa00de00c0
Erik Hakala <erik.hakala@unity3d.com>
2023-10-20 11:25:56 +0000

    2022.3/graphics/urp/backport/fov editmode motionblur disable
    Disable motion blur in EditMode to keep editing clear.

--- Packages/com.unity.render-pipelines.universal/Runtime/Passes/PostProcessPass.cs
+++ Packages/com.unity.render-pipelines.universal/Runtime/Passes/PostProcessPass.cs
@@ -382,6 +382,10 @@ namespace UnityEngine.Rendering.Universal
             bool useMotionBlur = m_MotionBlur.IsActive() && !isSceneViewCamera;
             bool usePaniniProjection = m_PaniniProjection.IsActive() && !isSceneViewCamera;
 
+            // Disable MotionBlur in EditMode, so that editing remains clear and readable.
+            // NOTE: HDRP does the same via CoreUtils::AreAnimatedMaterialsEnabled().
+            useMotionBlur = useMotionBlur && Application.isPlaying;
+
             // Note that enabling jitters uses the same CameraData::IsTemporalAAEnabled(). So if we add any other kind of overrides (like
             // disable useTemporalAA if another feature is disabled) then we need to put it in CameraData::IsTemporalAAEnabled() as opposed
             // to tweaking the value here.
