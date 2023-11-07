b1181be781ad200f76f186ceafbcbf942e75c0bd
Reach Platform Support <svc-reach-platform-support@unity3d.com>
2023-11-07 12:08:25 +0000

    [Port] [2022.3] [URP] Fix Color and Depth textures being misoriented when Display Rotation is enabled
    We have Vulkan's PreTransform feature implemented on Android under `Apply display rotation during rendering` setting. It would apply image transformation before presentation to change displayed orientation from engine's natural. With URP we can expose color or/and depth textures. If we would expose only depth texture and would not use any Post-Processing, backbuffer would be used to store color buffer and depth would be in separate texture. Because we can not be certain what orientation backbuffer will take, game starting in horizontal orientation would do rendering with misoriented backbuffer and depth texture. This would lead to half of screen being corrupted. On Adreno - half of the screen is black, Mali - corrupted skybox without opaque objects.
    
    Status | Color Target | Depth Target
    --- | --- | ---
    Vertical Launch | Backbuffer <br><img src="https://media.github.cds.internal.unity3d.com/user/6961/files/1a22ba20-ef98-482a-9087-b1e471a9cfa9" height=250> | RenderTexture <br> <img src="https://media.github.cds.internal.unity3d.com/user/6961/files/5062b93a-dc48-4bb9-a6da-693e6d06d4ba" height=250>
    Horizontal Launch | Backbuffer <br> <img src="https://media.github.cds.internal.unity3d.com/user/6961/files/d56cbaff-633d-43d3-8941-a1c311ce4cdc" height=250> | RenderTexture <br><img src="https://media.github.cds.internal.unity3d.com/user/6961/files/09f2bfdd-5986-4b75-9d0b-bf2c32158db4" width=250>
    Vertical Launch + Flip to Horizontal <br> OR <br> Horizontal Launch + Flip to Vertical + Flip to Horizontal  | RenderTexture (**IT CHANGED**) <br> <img src="https://media.github.cds.internal.unity3d.com/user/6961/files/264d58d7-ed44-4ddc-aed7-2b8b49944e22" width=250> | RenderTexture <br><img src="https://media.github.cds.internal.unity3d.com/user/6961/files/3f3d1272-4dd7-45d7-a770-e511cba46e0c" width=250>
    
    This PR fixes misoriented backbuffer by forcing to use texture for color when pre transform is enabled.

--- Packages/com.unity.render-pipelines.universal/Runtime/UniversalRenderer.cs
+++ Packages/com.unity.render-pipelines.universal/Runtime/UniversalRenderer.cs
@@ -143,6 +143,7 @@ namespace UnityEngine.Rendering.Universal
         StencilState m_DefaultStencilState;
         LightCookieManager m_LightCookieManager;
         IntermediateTextureMode m_IntermediateTextureMode;
+        bool m_VulkanEnablePreTransform;
 
         // Materials used in URP Scriptable Render Passes
         Material m_BlitMaterial = null;
@@ -345,6 +346,8 @@ namespace UnityEngine.Rendering.Universal
             LensFlareCommonSRP.mergeNeeded = 0;
             LensFlareCommonSRP.maxLensFlareWithOcclusionTemporalSample = 1;
             LensFlareCommonSRP.Initialize();
+
+            m_VulkanEnablePreTransform = GraphicsSettings.HasShaderDefine(BuiltinShaderDefine.UNITY_PRETRANSFORM_TO_DISPLAY_ORIENTATION);
         }
 
         /// <inheritdoc />
@@ -723,7 +726,8 @@ namespace UnityEngine.Rendering.Universal
 #if UNITY_ANDROID || UNITY_WEBGL
             // GLES can not use render texture's depth buffer with the color buffer of the backbuffer
             // in such case we create a color texture for it too.
-            if (SystemInfo.graphicsDeviceType != GraphicsDeviceType.Vulkan)
+            // If Vulkan PreTransform is enabled we can't mix backbuffer and intermediate render target due to screen orientation mismatch
+            if (SystemInfo.graphicsDeviceType != GraphicsDeviceType.Vulkan || m_VulkanEnablePreTransform)
                 createColorTexture |= createDepthTexture;
 #endif
             // If there is any scaling, the color and depth need to be the same resolution and the target texture
