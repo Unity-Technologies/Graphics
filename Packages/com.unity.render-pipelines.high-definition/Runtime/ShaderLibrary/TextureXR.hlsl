04508488a5a7343ffaa485b71966e669cd108701
Gregory Mitrano <gregory.mitrano@unity3d.com>
2023-12-09 04:53:15 +0000

    Add TextureXR.hlsl Redirect File in HDRP
    This change adds a redirection HLSL file in HDRP to prevent projects that were using the file from breaking because the file has been moved to the SRP Core package.

---
+++ Packages/com.unity.render-pipelines.high-definition/Runtime/ShaderLibrary/TextureXR.hlsl
@@ -0,0 +1,8 @@
+#ifndef UNITY_HD_TEXTURE_XR_INCLUDED
+#define UNITY_HD_TEXTURE_XR_INCLUDED
+
+// TextureXR.hlsl has been moved to the SRP Core package.
+// This file redirects projects that were previously using the file to the updated location.
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/TextureXR.hlsl"
+
+#endif // UNITY_HD_TEXTURE_XR_INCLUDED
