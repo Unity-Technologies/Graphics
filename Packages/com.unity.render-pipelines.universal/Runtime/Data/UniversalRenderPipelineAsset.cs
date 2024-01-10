230906e9b070b4dccff62565407096d650f17482
Oleksandr Kokoshyn <oleksandr.kokoshyn@unity3d.com>
2024-01-10 12:49:36 +0000

    Updated the console message for the Compatibility Mode as suggested by Oliver Schnabel.
    Updated the console message for the Compatibility Mode as suggested by Oliver Schnabel.

--- Packages/com.unity.render-pipelines.universal/Runtime/Data/UniversalRenderPipelineAsset.cs
+++ Packages/com.unity.render-pipelines.universal/Runtime/Data/UniversalRenderPipelineAsset.cs
@@ -427,7 +427,7 @@ namespace UnityEngine.Rendering.Universal
     internal struct DeprecationMessage
     {
         internal const string CompatibilityScriptingAPIObsolete = "This rendering path is for compatibility mode only (when Render Graph is disabled). Use Render Graph API instead.";
-        internal const string CompatibilityScriptingAPIConsoleWarning = "Currently using rendering compatibility mode, support for this will be removed in the next version. Please use Render Graph instead.";
+        internal const string CompatibilityScriptingAPIConsoleWarning = "The project currently uses the compatibility mode where the Render Graph API is disabled. Support for this mode will be removed in future Unity versions. Migrate existing ScriptableRenderPasses to the new RenderGraph API. After the migration, disable the compatibility mode in Edit > Projects Settings > Graphics > Render Graph.";
     }
 
 #if UNITY_EDITOR
