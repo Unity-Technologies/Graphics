9c851eb61e36c8a87eff1877255c32d98fcdc876
Kenny Tan <kenny.tan@unity3d.com>
2024-01-24 21:06:54 +0000

    [23.3][UUM-46391] Fixed mem leak caused by missing dispose
    Fix mem leak for ShadowCaster2D

--- Packages/com.unity.render-pipelines.universal/Runtime/2D/Shadows/ShadowProvider/ShadowUtility.cs
+++ Packages/com.unity.render-pipelines.universal/Runtime/2D/Shadows/ShadowProvider/ShadowUtility.cs
@@ -360,6 +360,12 @@ namespace UnityEngine.Rendering.Universal
 
             outStartIndex = indexOffset;
             outIndexCount = tessOutIndexCount;
+
+            tessInEdges.Dispose();
+            tessInVertices.Dispose();
+            tessOutIndices.Dispose();
+            tessOutVertices.Dispose();
+            tessOutEdges.Dispose();
         }
 
         //inEdges is expected to be contiguous
