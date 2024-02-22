09abf1212e744c385fa75071f12c9732602a231a
Giorgos Petkakis <giorgos.petkakis@unity3d.com>
2024-02-22 13:36:40 +0000

    Add package publication jobs for internal packages
    Adds package publication jobs to unity CI for publishing internal packages.
    https://jira.unity3d.com/browse/GSDET-2
    
    This is in the context of creating performance testing CI for the demo projects such as the URP sample and Fantasy Kingdom.
    
    We are adding these as part of publishing a particular package, but have made the recipes flexible enough to create pack/test/publish jobs for any package under the unity repository, simply by adding a path under `PackageCollection.cs`.
    
    1. I have added `com.unity.render-pipelines.core` as a package dependency. This was necessary as the package was already depending on it, but only implicitly. It just happened that every project that this package was in ALSO included the core RP package. Publishing the package means testing it in isolation, so I had to include the dependency explicitly.
    2. Adding the RP core package means locking in a version for it. Since we're working off trunk, I added the current version and matched the different metadata to be at the current trunk level. This means that if we want to publish this package for an earlier version, we can still do it.
    3. The 'Package Validation' test suite for this package contains only a dummy test. It is outside the scope of this PR to test the package fully, we only intend to publish it at this point. https://jira.unity3d.com/browse/GSDET-7 is tracking this task as technical debt.
    4. The `NDA Filename Validation` tests are ignored for the graphics-performance package. I have reviewed the files and they do not seem to contain any NDA information. The package is also only going to the internal registry, not anywhere further.
    5. I have changed the package dependency version of `com.unity.shaderanalysis` from `10.0.0-preview.1` to `1.0.0`. I am not quite sure what had happened here, but I think the `10.0.0` version never existed and package manager was just implicitly using the local version and never emitting any errors.
    6. I have added triggers for package publication to run automatically if the package is edited and the changes land in trunk, as well as for tests to run on open PRs with changes to package code.

---
+++ Tests/SRPTests/Packages/com.unity.testing.graphics-performance/Tests/PackageValidation/PackageValidationTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+[Category("PackageValidation")]
+public class PackageValidationTests
+{
+    [Test]
+    public void DummyTest()
+    {
+        Assert.Pass();
+    }
+}
