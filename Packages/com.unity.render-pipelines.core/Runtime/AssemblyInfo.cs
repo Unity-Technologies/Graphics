342ed55ea5d867f66fd7b13ca26fa3f967136b12
Raquel Peces <raquel.peces@unity3d.com>
2024-03-18 15:09:58 +0000

    URP performance test for 22LTS
    Jira ticket: GFXRPF-209 - https://jira.unity3d.com/browse/GFXRPF-209
    Create performance test project for LTS 2022. Port current Universal_PerformanceTests to execute the same performance metrics in 2022.
    The tests creates an artifact with the output images too.
    This is not a graphics tests, so the results between iterations will not be checked

--- Packages/com.unity.render-pipelines.core/Runtime/AssemblyInfo.cs
+++ Packages/com.unity.render-pipelines.core/Runtime/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.Core.Editor")]
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.Core.Runtime.Tests")]
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Runtime")]
+[assembly: InternalsVisibleTo("Unity.GraphicTests.Performance.Universal.Runtime")] // access to internal ProfileIds
