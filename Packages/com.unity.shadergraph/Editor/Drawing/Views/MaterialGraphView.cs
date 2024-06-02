354bff1fd954332f5115a5f44b5715e74c1d1c3d
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2024-06-02 10:29:02 +0200

    isolate possible problem with assembly.GetName() call made for all assemblies in the appdomain

--- Packages/com.unity.shadergraph/Editor/Drawing/Views/MaterialGraphView.cs
+++ Packages/com.unity.shadergraph/Editor/Drawing/Views/MaterialGraphView.cs
@@ -40,10 +40,15 @@ namespace UnityEditor.ShaderGraph.Drawing
             Assembly graphViewAssembly = null;
             foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
             {
-                var assemblyName = assembly.GetName().ToString();
-                if (assemblyName.Contains("GraphView"))
+                try
                 {
-                    graphViewAssembly = assembly;
+                    if (assembly.GetName().ToString().Contains("GraphView"))
+                        graphViewAssembly = assembly;
+                }
+                catch (Exception)
+                {
+                    // ignored, some user assemblies may fail here
+                    // https://youtrack.jetbrains.com/issue/RIDER-112939
                 }
             }
 
