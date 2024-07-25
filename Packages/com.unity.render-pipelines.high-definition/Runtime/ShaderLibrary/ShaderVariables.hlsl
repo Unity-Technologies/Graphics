25e3de15ea7497ddcbca13871bdc1b2043ad492c
Antoine Lelièvre <antoinel@unity3d.com>
2024-07-25 16:43:18 +0000

    Fix missing matrix defines
    Fix missing defines reported here: https://discussions.unity.com/t/unity_matrix_it_mv/950241

--- Packages/com.unity.render-pipelines.high-definition/Runtime/ShaderLibrary/ShaderVariables.hlsl
+++ Packages/com.unity.render-pipelines.high-definition/Runtime/ShaderLibrary/ShaderVariables.hlsl
@@ -536,6 +536,10 @@ float4x4 GetRawUnityPrevWorldToObject() { return unity_MatrixPreviousMI; }
 #define UNITY_MATRIX_I_M       ApplyCameraTranslationToInverseMatrix(GetRawUnityWorldToObject())
 #define UNITY_PREV_MATRIX_M    ApplyCameraTranslationToMatrix(GetRawUnityPrevObjectToWorld())
 #define UNITY_PREV_MATRIX_I_M  ApplyCameraTranslationToInverseMatrix(GetRawUnityPrevWorldToObject())
+#define UNITY_MATRIX_MV        mul(UNITY_MATRIX_V, UNITY_MATRIX_M)
+#define UNITY_MATRIX_T_MV      transpose(UNITY_MATRIX_MV)
+#define UNITY_MATRIX_IT_MV     transpose(mul(UNITY_MATRIX_I_M, UNITY_MATRIX_I_V))
+#define UNITY_MATRIX_MVP       mul(UNITY_MATRIX_VP, UNITY_MATRIX_M)
 
 #endif
 
