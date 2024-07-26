c66468218c81369c38336b4ee10e24df4889a4b1
Andrés Valencia <andresvalenciatellez@unity3d.com>
2024-07-26 19:44:49 +0000

    [UUM-73236] [URP] Set no-op matrix for empty shadow-rendering.
    This PR fixes an issue where some leftover shadow-rendering uniforms aren't properly cleared when using the `Unlit Draw Mode` in the scene-view, leading to some visible artifacts when sampling the empty shadow-map set for this mode.

--- Packages/com.unity.render-pipelines.universal/Runtime/Passes/MainLightShadowCasterPass.cs
+++ Packages/com.unity.render-pipelines.universal/Runtime/Passes/MainLightShadowCasterPass.cs
@@ -120,6 +120,11 @@ namespace UnityEngine.Rendering.Universal.Internal
             if (!shadowData.mainLightShadowsEnabled)
                 return false;
 
+#if UNITY_EDITOR
+            if (CoreUtils.IsSceneLightingDisabled(cameraData.camera))
+                return false;
+#endif
+
             using var profScope = new ProfilingScope(m_ProfilingSetupSampler);
 
             if (!shadowData.supportsMainLightShadows)
@@ -188,7 +193,7 @@ namespace UnityEngine.Rendering.Universal.Internal
         [Obsolete(DeprecationMessage.CompatibilityScriptingAPIObsolete, false)]
         public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
         {
-             
+
             if (m_CreateEmptyShadowmap && !m_EmptyShadowmapNeedsClear)
             {
                 // UUM-63146 - glClientWaitSync: Expected application to have kicked everything until job: 96089 (possibly by calling glFlush)" are thrown in the Android Player on some devices with PowerVR Rogue GE8320
