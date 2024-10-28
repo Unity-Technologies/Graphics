16a24cac0c18dfe227106d1c2ddcc37de25f0245
Rémi Chapelain <remi.chapelain@unity3d.com>
2024-10-28 11:13:24 +0000

    [HDRP] Add rolling wave sample scene to water sample
    Following this PR : https://github.cds.internal.unity3d.com/unity/unity/pull/52584
    
    
    https://media.github.cds.internal.unity3d.com/user/1764/files/8365e966-618c-41d8-a7f2-ffd7babc730b
    
    
    
    We wanted to give at least one example on how to setup properly a rolling wave given that it's the main reason of this fix. 
    This is exactly what this PR does, basically showing one way starting from a modeled mesh in blender and baking that mesh deformation into a texture to be able to read it in the shader graph.

---
+++ Packages/com.unity.render-pipelines.high-definition/Samples~/WaterSamples/Scripts/MoveWave.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+using UnityEditor;
+using UnityEngine.Rendering.HighDefinition;
+
+[ExecuteInEditMode]
+public class MoveWave : MonoBehaviour
+{
+
+    public bool autoplay = true;
+
+    [Range(0, 1)]
+    public float stateOverride;
+    
+    //Time the wave should take to go from start to end. 
+    public float speedWave = 1;
+
+    //X is the starting X position, Y is the end X position for the wave
+    public Vector2 positions = new Vector2(-100,0);
+    public Material deformer = null;
+    public VisualEffect vfxLip = null;
+    
+    
+    internal float startTime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.realtimeSinceStartup;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float stateWave = GetState();
+
+        if(vfxLip != null)
+            vfxLip.SetFloat("_factorWave", stateWave);
+
+        if (deformer != null)
+            deformer.SetFloat("_State", stateWave * 2);
+
+        // Lerping the X position of the wave. 
+        this.transform.position = new Vector3(Mathf.Lerp(positions.x, positions.y, stateWave), 0.0f, 0.0f);
+        
+        // If the state goes above one, we need to restart the wave. 
+        if(stateWave > 1) 
+            startTime = Time.realtimeSinceStartup;
+    }
+
+    public float GetState()
+	{
+        if (!autoplay) return stateOverride;
+
+        return (Time.realtimeSinceStartup - startTime)/speedWave;
+	}
+}
+
+[CustomEditor(typeof(MoveWave))]
+public class MoveWaveEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.DrawDefaultInspector();
+
+        var anim = (target as MoveWave);
+
+        if (anim.autoplay && GUILayout.Button("Reset"))
+        {
+            anim.startTime = Time.realtimeSinceStartup;
+        }
+    }
+}
+
