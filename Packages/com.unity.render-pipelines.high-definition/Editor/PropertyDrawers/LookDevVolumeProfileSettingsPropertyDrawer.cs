90c8d90ed5b81deb7153805b7e7c0504d3b86c25
Reach Platform Support <svc-reach-platform-support@unity3d.com>
2024-12-16 23:14:11 +0000

    [Port] [6000.0] [SRP][Volume]Multiple fixes for being able to properly perform reset
    Fix [UUM-77760](https://jira.unity3d.com/browse/UUM-77760) and additional issue around reset of default VolumeProfile:
    - Fixed Reset attempt crash when resetting for pipeline not in use
    - Fixed Reset nullifying when resetting for pipeline not in use instead of set back default values
    - Fixed additional separators for HDRP version due to LookDev volume
    
    To fix all of this, UITK conversion of the ImGUI VolumeProfile foldout field is done here.
    Each SRPGraphicsSettings (URP, HDRP) have a Reset implementation to not nullify anymore.
    On URP side, for the Reset to work, introduction of a new editor resource that load a default profile from package in Editor. The default vlume used is the one produced in the 3D template.

--- Packages/com.unity.render-pipelines.high-definition/Editor/PropertyDrawers/LookDevVolumeProfileSettingsPropertyDrawer.cs
+++ Packages/com.unity.render-pipelines.high-definition/Editor/PropertyDrawers/LookDevVolumeProfileSettingsPropertyDrawer.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
@@ -15,6 +16,7 @@ namespace UnityEditor.Rendering.HighDefinition
         SerializedObject m_SettingsSerializedObject;
         SerializedProperty m_VolumeProfileSerializedProperty;
         EditorPrefBool m_DefaultVolumeProfileFoldoutExpanded;
+        VisualElement m_EditorContainer;
 
         const int k_DefaultVolumeLabelWidth = 260;
         static readonly GUIContent k_LookDevVolumeProfileAssetLabel = EditorGUIUtility.TrTextContent("LookDev Profile");
@@ -35,13 +37,14 @@ namespace UnityEditor.Rendering.HighDefinition
             m_VolumeProfileSerializedProperty = property.FindPropertyRelative("m_VolumeProfile");
             m_DefaultVolumeProfileFoldoutExpanded = new EditorPrefBool($"{GetType()}.LookDevVolumeProfileFoldoutExpanded", false);
 
+            m_EditorContainer = new VisualElement();
             var assetFieldUI = CreateAssetFieldUI();
             assetFieldUI.style.marginBottom = k_AssetFieldBottomMargin;
             m_Root.Add(assetFieldUI);
 
-            var volumeEditorUI = CreateVolumeProfileEditorUI();
-            volumeEditorUI.style.marginLeft = -k_ImguiContainerLeftMargin;
-            m_Root.Add(volumeEditorUI);
+            m_EditorContainer.Add(CreateVolumeProfileEditorUI());
+            m_EditorContainer.style.marginLeft = -k_ImguiContainerLeftMargin;
+            m_Root.Add(m_EditorContainer);
 
             return m_Root;
         }
@@ -54,46 +57,74 @@ namespace UnityEditor.Rendering.HighDefinition
 
         VisualElement CreateAssetFieldUI()
         {
-            IMGUIContainer container = null;
-            container = new IMGUIContainer(() =>
+            var lookDevVolumeProfileSettings = GraphicsSettings.GetRenderPipelineSettings<LookDevVolumeProfileSettings>();
+
+            VisualElement profileLine = new();
+            var toggle = new Toggle();
+            toggle.AddToClassList(Foldout.toggleUssClassName);
+            var checkmark = toggle.Q(className: Toggle.checkmarkUssClassName);
+            checkmark.AddToClassList(Foldout.checkmarkUssClassName);
+            var field = new ObjectField(k_LookDevVolumeProfileAssetLabel.text)
             {
-                using var indentLevelScope = new EditorGUI.IndentLevelScope();
-                using var changeScope = new EditorGUI.ChangeCheckScope();
-                
-                /* values adapted to the ProjectSettings > Graphics */
-                var minWidth = 91;
-                var indent = 94;
-                var ratio = 0.45f;
-                EditorGUIUtility.labelWidth = Mathf.Max(minWidth, (int)((container.worldBound.width - indent) * ratio));
+                tooltip = k_LookDevVolumeProfileAssetLabel.tooltip,
+                objectType = typeof(VolumeProfile),
+                value = m_VolumeProfileSerializedProperty.objectReferenceValue as VolumeProfile,
+            };
+            field.AddToClassList("unity-base-field__aligned"); //Align with other BaseField<T>
+            field.Q<Label>().RegisterCallback<ClickEvent>(evt => toggle.value ^= true);
 
-                var lookDevVolumeProfileSettings = GraphicsSettings.GetRenderPipelineSettings<LookDevVolumeProfileSettings>();
-
-                bool expanded = m_DefaultVolumeProfileFoldoutExpanded.value;
-                VolumeProfile lookDevAsset = RenderPipelineGlobalSettingsUI.DrawVolumeProfileAssetField(
-                    m_VolumeProfileSerializedProperty,
-                    k_LookDevVolumeProfileAssetLabel,
-                    getOrCreateVolumeProfile: () =>
-                    {
-                        if (lookDevVolumeProfileSettings.volumeProfile == null)
-                        {
-                            lookDevVolumeProfileSettings.volumeProfile = VolumeUtils.CopyVolumeProfileFromResourcesToAssets(GraphicsSettings
-                                .GetRenderPipelineSettings<HDRenderPipelineEditorAssets>().lookDevVolumeProfile);
-                        }
-
-                        // When the built-in Reset context action is used, the asset becomes null outside of this scope.
-                        // This is required to apply the new value to the serialized property.
-                        GUI.changed = true;
-
-                        return lookDevVolumeProfileSettings.volumeProfile;
-                    },
-                    ref expanded
-                );
-                m_DefaultVolumeProfileFoldoutExpanded.value = expanded;
-
-                if (changeScope.changed)
-                    m_SettingsSerializedObject.ApplyModifiedProperties();
+            toggle.RegisterValueChangedCallback(evt =>
+            {
+                m_EditorContainer.style.display = evt.newValue ? DisplayStyle.Flex : DisplayStyle.None;
+                m_DefaultVolumeProfileFoldoutExpanded.value = evt.newValue;
             });
-            return container;
+            toggle.SetValueWithoutNotify(m_DefaultVolumeProfileFoldoutExpanded.value);
+            m_EditorContainer.style.display = m_DefaultVolumeProfileFoldoutExpanded.value ? DisplayStyle.Flex : DisplayStyle.None;
+
+            profileLine.Add(toggle);
+            profileLine.Add(field);
+            profileLine.style.flexDirection = FlexDirection.Row;
+            field.style.flexGrow = 1;
+
+            field.RegisterValueChangedCallback(evt =>
+            {
+                if (evt.newValue == evt.previousValue)
+                    return;
+
+                if (evt.newValue == null)
+                {
+                    if (evt.previousValue != null)
+                    {
+                        field.SetValueWithoutNotify(evt.previousValue);
+                        Debug.Log("This Volume Profile Asset cannot be null. Rolling back to previous value.");
+                        return;
+                    }
+                    else
+                    {
+                        if (RenderPipelineManager.currentPipeline is not HDRenderPipeline)
+                        {
+                            m_VolumeProfileSerializedProperty.objectReferenceValue = null;
+                        }
+                        else
+                        {
+                            var lookDevVolumeProfileSettings = GraphicsSettings.GetRenderPipelineSettings<LookDevVolumeProfileSettings>();
+                            if (lookDevVolumeProfileSettings.volumeProfile == null)
+                            {
+                                lookDevVolumeProfileSettings.volumeProfile = VolumeUtils.CopyVolumeProfileFromResourcesToAssets(
+                                    GraphicsSettings.GetRenderPipelineSettings<HDRenderPipelineEditorAssets>().defaultVolumeProfile);
+                            }
+
+                            m_VolumeProfileSerializedProperty.objectReferenceValue = lookDevVolumeProfileSettings.volumeProfile;
+                        }
+                    }
+                }
+                else
+                    m_VolumeProfileSerializedProperty.objectReferenceValue = evt.newValue;
+
+                m_VolumeProfileSerializedProperty.serializedObject.ApplyModifiedProperties();
+            });
+
+            return profileLine;
         }
 
         static Lazy<GUIStyle> s_ImguiContainerScopeStyle = new(() => new GUIStyle
@@ -124,28 +155,6 @@ namespace UnityEditor.Rendering.HighDefinition
                         EditorGUILayout.HelpBox("HDRISky is not modifiable and will be overridden by the LookDev", MessageType.Warning);
                 }
             });
-        }
-
-        public class LookDevVolumeProfileSettingsContextMenu : IRenderPipelineGraphicsSettingsContextMenu<LookDevVolumeProfileSettings>
-        {
-            public void PopulateContextMenu(LookDevVolumeProfileSettings setting, PropertyDrawer drawer, ref GenericMenu menu)
-            {
-                menu.AddSeparator("");
-
-                LookDevVolumeProfileSettingsPropertyDrawer lookDevDrawer = drawer as LookDevVolumeProfileSettingsPropertyDrawer;
-                VolumeProfileEditor lookDevVolumeProfileEditor = lookDevDrawer?.m_LookDevVolumeProfileEditor as VolumeProfileEditor;
-                var componentEditors = lookDevVolumeProfileEditor != null ? lookDevVolumeProfileEditor.componentList.editors : null;
-                VolumeProfileUtils.AddVolumeProfileContextMenuItems(ref menu,
-                    setting.volumeProfile,
-                    componentEditors,
-                    overrideStateOnReset: true,
-                    defaultVolumeProfilePath: $"Assets/{HDProjectSettings.projectSettingsFolderPath}/LookDevProfile_Default.asset",
-                    onNewVolumeProfileCreated: createdProfile =>
-                    {
-                        lookDevDrawer.m_VolumeProfileSerializedProperty.objectReferenceValue = createdProfile;
-                        lookDevDrawer.m_VolumeProfileSerializedProperty.serializedObject.ApplyModifiedProperties();
-                    });
-            }
         }
     }
 }
