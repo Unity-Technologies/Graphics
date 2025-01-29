7609e85d87bf03ae621fbf2a27885af37a3088d5
Evergreen <evergreen@unity3d.com>
2025-01-29 03:59:47 +0000

    Fixed the generation of new artifact ID of ShaderGraph on every reimport.

--- Tests/SRPTests/Packages/com.unity.testing.visualeffectgraph/Tests/Editor/VFXCodeGenerationTest.cs
+++ Tests/SRPTests/Packages/com.unity.testing.visualeffectgraph/Tests/Editor/VFXCodeGenerationTest.cs
@@ -46,6 +46,24 @@ namespace UnityEditor.VFX.Test
         }
 
         [Test]
+        public void ArtifactHashUnchanged_OnReimporting_ShaderGraph_With_VfxTarget()
+        {
+            var path = "Packages/com.unity.testing.visualeffectgraph/Tests/Editor/Data/SG_Lit_VfxTarget.shadergraph";
+
+            AssetDatabase.ImportAsset(path);
+            AssetDatabase.Refresh();
+
+            var initialHash = AssetDatabase.GetAssetDependencyHash(path);
+
+            AssetDatabase.ImportAsset(path);
+            AssetDatabase.Refresh();
+
+            var finalHash = AssetDatabase.GetAssetDependencyHash(path);
+
+            Assert.AreEqual(initialHash, finalHash);
+        }
+
+        [Test]
         public void Migration_ShaderGraph_Output_To_Composed()
         {
             var packagePath = "Packages/com.unity.testing.visualeffectgraph/Tests/Editor/Data/VFXShaderGraphOutput_Migration.unitypackage";
