5576dbc3a935d4baed18343ffbb456e85c2a57b7
Joseph Savell <joseph.savell@unity3d.com>
2025-01-31 22:25:24 +0000

    [UUM-84357] Fix issues when resizing Main Preview subwindow

--- Packages/com.unity.shadergraph/Editor/Drawing/Interfaces/IResizable.cs
+++ Packages/com.unity.shadergraph/Editor/Drawing/Interfaces/IResizable.cs
@@ -6,5 +6,8 @@ namespace UnityEditor.ShaderGraph.Drawing.Interfaces
     {
         // Depending on the return value, the ElementResizer either allows resizing past parent view edge (like in case of StickyNote) or clamps the size at the edges of parent view (like for GraphSubWindows)
         bool CanResizePastParentBounds();
+
+        // If true, element will be kept square when resizing. Otherwise, both axes can be resized freely.
+        bool KeepSquareAspect() => false;
     }
 }
