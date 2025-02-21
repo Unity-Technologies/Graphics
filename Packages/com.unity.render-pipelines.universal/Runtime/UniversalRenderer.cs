b528e7da0ed6b8f5a62ac5288042dec704a91581
Layla Arab <layla.arab@unity3d.com>
2025-02-21 08:29:22 +0000

    [Port] [6000.1] Check if transparent objects receive shadows when setting up ShadowCaster pass

--- Packages/com.unity.render-pipelines.universal/Runtime/UniversalRenderer.cs
+++ Packages/com.unity.render-pipelines.universal/Runtime/UniversalRenderer.cs
@@ -215,6 +215,7 @@ namespace UnityEngine.Rendering.Universal
         internal DeferredLights deferredLights { get => m_DeferredLights; }
         internal LayerMask opaqueLayerMask { get; set; }
         internal LayerMask transparentLayerMask { get; set; }
+        internal bool shadowTransparentReceive { get; set; }
 
         internal GraphicsFormat cameraDepthTextureFormat { get => (m_CameraDepthTextureFormat != DepthFormat.Default) ? (GraphicsFormat)m_CameraDepthTextureFormat : CoreUtils.GetDefaultDepthStencilFormat(); }
         internal GraphicsFormat cameraDepthAttachmentFormat { get => (m_CameraDepthAttachmentFormat != DepthFormat.Default) ? (GraphicsFormat)m_CameraDepthAttachmentFormat : CoreUtils.GetDefaultDepthStencilFormat(); }
@@ -266,6 +267,7 @@ namespace UnityEngine.Rendering.Universal
             m_IntermediateTextureMode = data.intermediateTextureMode;
             opaqueLayerMask = data.opaqueLayerMask;
             transparentLayerMask = data.transparentLayerMask;
+            shadowTransparentReceive = data.shadowTransparentReceive;
 
             if (UniversalRenderPipeline.asset?.supportsLightCookies ?? false)
             {
