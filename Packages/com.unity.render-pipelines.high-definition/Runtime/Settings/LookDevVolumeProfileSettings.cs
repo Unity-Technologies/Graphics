577803a07590ec69a51c5bc44064d71b24817d45
Rémi Slysz <remi@unity3d.com>
2025-04-25 15:33:59 +0000

    Update LookDevVolumeProfileSettings.cs

--- Packages/com.unity.render-pipelines.high-definition/Runtime/Settings/LookDevVolumeProfileSettings.cs
+++ Packages/com.unity.render-pipelines.high-definition/Runtime/Settings/LookDevVolumeProfileSettings.cs
@@ -70,13 +70,12 @@ namespace UnityEngine.Rendering.HighDefinition
             if (UnityEditor.Rendering.EditorGraphicsSettings.TryGetRenderPipelineSettingsForPipeline<HDRenderPipelineEditorAssets, HDRenderPipeline>(out var rpgs))
             {
                 //UUM-100350
-                //For some reason, when the one in the HDRP template is modified, all its components are nullified instead of replaced.
-                //Removing it fully and creating it solve the issue.
-                string path = VolumeUtils.BuildDefaultNameForVolumeProfile(rpgs.lookDevVolumeProfile);
-                if (UnityEditor.AssetDatabase.AssetPathExists(path))
-                    UnityEditor.AssetDatabase.DeleteAsset(path);
-
-                volumeProfile = VolumeUtils.CopyVolumeProfileFromResourcesToAssets(rpgs.lookDevVolumeProfile);
+                //When opening the new HDRP project from the template the first time, the global settings is created and the population of IRenderPipelineGraphicsSettings
+                //will call this Reset() method. At this time, the copied item will appear ok but will be seen as null soon after. This lead to errors when opening the 
+                //inspector of the LookDev's VolumeProfile (at the creation of Editors for VolumeComponent). Closing and opening the project would make this issue disappear.
+                //This asset data base manipulation issue disappear if we delay it.
+                UnityEditor.EditorApplication.delayCall += () =>
+                    volumeProfile = VolumeUtils.CopyVolumeProfileFromResourcesToAssets(rpgs.lookDevVolumeProfile);
             }
 #endif
         }
