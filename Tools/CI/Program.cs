857ed612e6d753f97518453fabb7397926913b65
Ben Grater <ben.grater@unity3d.com>
2025-06-16 22:57:37 -0700

    Add wrench cookbook project
    (cherry picked from commit 2ebd3d4b90648b3ee418a3f231937875d2d14819)

---
+++ Tools/CI/Program.cs
@@ -0,0 +1,21 @@
+﻿using Graphics.Cookbook.Settings;
+using RecipeEngine;
+using RecipeEngine.Modules.Wrench.Helpers;
+
+
+// ReSharper disable once CheckNamespace
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        var settings = new GraphicsSettings();
+
+        // ReSharper disable once UnusedVariable
+        var engine = EngineFactory
+            .Create()
+            .ScanAll()
+            .WithWrenchModule(settings.Wrench)
+            .GenerateAsync().Result;
+        return engine;
+    }
+}
---
+++ Tools/CI/regenerate.bat
@@ -0,0 +1,2 @@
+cd %~dp0../../
+dotnet run --project Tools\CI\Graphics.Cookbook.csproj
