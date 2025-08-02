ddbc4c5e9b875d3e90bb3b0b44f0e20648970aec
Reach Platform Support <svc-reach-platform-support@unity3d.com>
2025-08-02 14:15:39 +0000

    [Port] [6000.1] [SRP] Fix Volume icon for light theme

--- Packages/com.unity.render-pipelines.core/Runtime/Volume/Volume.cs
+++ Packages/com.unity.render-pipelines.core/Runtime/Volume/Volume.cs
@@ -10,6 +10,7 @@ namespace UnityEngine.Rendering
     [CurrentPipelineHelpURL("Volumes")]
     [ExecuteAlways]
     [AddComponentMenu("Miscellaneous/Volume")]
+    [Icon("Packages/com.unity.render-pipelines.core/Editor/Icons/Processed/Volume Icon.asset")]
     public class Volume : MonoBehaviour, IVolume
     {
         [SerializeField, FormerlySerializedAs("isGlobal")]
