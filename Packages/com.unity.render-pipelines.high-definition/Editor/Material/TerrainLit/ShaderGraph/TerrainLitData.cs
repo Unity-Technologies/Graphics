e93f532462748a7c381df8fe8584c92105a4b7e8
Jonathan Brodsky <jonathan.brodsky@unity3d.com>
2025-08-28 20:51:29 +0000

    Graphics/sg/terrain shadergraph UI fixes

--- Packages/com.unity.render-pipelines.high-definition/Editor/Material/TerrainLit/ShaderGraph/TerrainLitData.cs
+++ Packages/com.unity.render-pipelines.high-definition/Editor/Material/TerrainLit/ShaderGraph/TerrainLitData.cs
@@ -5,22 +5,6 @@ namespace UnityEditor.Rendering.HighDefinition.ShaderGraph
     class TerrainLitData : HDTargetData
     {
         [SerializeField]
-        private bool m_EnableHeightBlened;
-        public bool enableHeightBlend
-        {
-            get => m_EnableHeightBlened;
-            set => m_EnableHeightBlened = value;
-        }
-
-        [SerializeField]
-        private float m_HeightTransition;
-        public float heightTransition
-        {
-            get => m_HeightTransition;
-            set => m_HeightTransition = value;
-        }
-
-        [SerializeField]
         private bool m_EnableInstancedPerPixelNormal = true;
         public bool enableInstancedPerPixelNormal
         {
