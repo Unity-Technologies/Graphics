2c96b7b44bb76de6466b117f39dc1eb00bf5155d
Reach Platform Support <svc-reach-platform-support@unity3d.com>
2025-09-03 22:23:59 +0000

    [Port] [6000.3] [UUM-115779] Fix artifacts caused by tiled deferred lighting after disabling path tracing in HDRP.

--- Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.RenderGraph.cs
+++ Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.RenderGraph.cs
@@ -146,6 +146,8 @@ namespace UnityEngine.Rendering.HighDefinition
                 }
                 else if (hdCamera.frameSettings.IsEnabled(FrameSettingsField.RayTracing) && pathTracing.enable.value && hdCamera.camera.cameraType != CameraType.Preview && GetRayTracingState() && GetRayTracingClusterState())
                 {
+                    m_TileAndClusterData.listsAreInitialized = false;
+
                     // We only request the light cluster if we are gonna use it for debug mode
                     if (FullScreenDebugMode.LightCluster == m_CurrentDebugDisplaySettings.data.fullScreenDebugMode && GetRayTracingClusterState())
                     {
