10318978ca62dd8ccc7265ae99022e58a01a6420
Nicola Cerone <nicola.cerone@unity3d.com>
2025-09-25 02:53:27 +0000

    Improve Element Texture Size node.

--- Packages/com.unity.shadergraph/Editor/Data/Nodes/Input/UI/ElementTextureSizeNode.cs
+++ Packages/com.unity.shadergraph/Editor/Data/Nodes/Input/UI/ElementTextureSizeNode.cs
@@ -8,9 +8,15 @@ namespace UnityEditor.ShaderGraph
     [SubTargetFilter(typeof(IUISubTarget))]
     class ElementTextureUVSize : AbstractMaterialNode, IGeneratesBodyCode, IMayRequireUITK
     {
-        public const int TextureSizeSlotId = 0;
+        public const int TextureWidthSlotId = 0;
+        public const int TextureHeightSlotId = 1;
+        public const int TexelWidthSlotId = 2;
+        public const int TexelHeightSlotId = 3;
 
-        private const string kTextureSizeName = "Texture Size";
+        private const string kTextureWidthName = "Width";
+        private const string kTextureHeightName = "Height";
+        private const string kTexelWidthName = "Texel Width";
+        private const string kTexelHeightName = "Texel Height";
 
         public override bool hasPreview { get { return false; } }
 
@@ -23,17 +29,29 @@ namespace UnityEditor.ShaderGraph
 
         public override void UpdateNodeAfterDeserialization()
         {
-            AddSlot(new Vector4MaterialSlot(TextureSizeSlotId, kTextureSizeName, kTextureSizeName, SlotType.Output, Vector4.zero));
-            RemoveSlotsNameNotMatching(new[] { TextureSizeSlotId });
+            AddSlot(new Vector1MaterialSlot(TextureWidthSlotId, kTextureWidthName, kTextureWidthName, SlotType.Output, 0.0f));
+            AddSlot(new Vector1MaterialSlot(TextureHeightSlotId, kTextureHeightName, kTextureHeightName, SlotType.Output, 0.0f));
+            AddSlot(new Vector1MaterialSlot(TexelWidthSlotId, kTexelWidthName, kTexelWidthName, SlotType.Output, 0.0f));
+            AddSlot(new Vector1MaterialSlot(TexelHeightSlotId, kTexelHeightName, kTexelHeightName, SlotType.Output, 0.0f));
+
+            RemoveSlotsNameNotMatching(new[] { TextureWidthSlotId, TextureHeightSlotId, TexelWidthSlotId, TexelHeightSlotId });
         }
 
         public void GenerateNodeCode(ShaderStringBuilder sb, GenerationMode generationMode)
         {
-            if (GetInputNodeFromSlot(TextureSizeSlotId) != null) sb.AppendLine(string.Format("TextureInfo UITKNodeOutput_{0} = GetTextureInfo(IN.typeTexSettings.y);", GetVariableNameForSlot(TextureSizeSlotId)));
-            if (GetInputNodeFromSlot(TextureSizeSlotId) != null) sb.AppendLine(string.Format("$precision4 {0} = float4(UITKNodeOutput_{1}.textureSize, UITKNodeOutput_{2}.texelSize);",
-                GetVariableNameForSlot(TextureSizeSlotId),
-                GetVariableNameForSlot(TextureSizeSlotId),
-                GetVariableNameForSlot(TextureSizeSlotId)));
+            if ((GetInputNodeFromSlot(TextureWidthSlotId) != null) ||
+                (GetInputNodeFromSlot(TextureHeightSlotId) != null) ||
+                (GetInputNodeFromSlot(TexelWidthSlotId) != null) ||
+                (GetInputNodeFromSlot(TexelHeightSlotId) != null))
+            {
+                string variableName = string.Format("TextureInfo_{0}", objectId.ToString());
+
+                sb.AppendLine(string.Format("TextureInfo {0} = GetTextureInfo(IN.typeTexSettings.y);", variableName));
+                if (GetInputNodeFromSlot(TextureWidthSlotId) != null) sb.AppendLine(string.Format("$precision1 {0} = {1}.textureSize.x;", GetVariableNameForSlot(TextureWidthSlotId), variableName));
+                if (GetInputNodeFromSlot(TextureHeightSlotId) != null) sb.AppendLine(string.Format("$precision1 {0} = {1}.textureSize.y;", GetVariableNameForSlot(TextureHeightSlotId), variableName));
+                if (GetInputNodeFromSlot(TexelWidthSlotId) != null) sb.AppendLine(string.Format("$precision1 {0} = {1}.texelSize.x;", GetVariableNameForSlot(TexelWidthSlotId), variableName));
+                if (GetInputNodeFromSlot(TexelHeightSlotId) != null) sb.AppendLine(string.Format("$precision1 {0} = {1}.texelSize.y;", GetVariableNameForSlot(TexelHeightSlotId), variableName));
+            }
         }
 
         public bool RequiresUITK(ShaderStageCapability stageCapability)
