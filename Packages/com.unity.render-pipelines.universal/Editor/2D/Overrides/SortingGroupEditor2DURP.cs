ab142601756d7613dbea8a1be5092ca400cad3aa
Chris Chu <chris.chu@unity3d.com>
2025-10-05 23:20:31 +0000

    [UUM-117676] Fix for Sort As 2D when using Prefabs

--- Packages/com.unity.render-pipelines.universal/Editor/2D/Overrides/SortingGroupEditor2DURP.cs
+++ Packages/com.unity.render-pipelines.universal/Editor/2D/Overrides/SortingGroupEditor2DURP.cs
@@ -12,23 +12,25 @@ namespace UnityEditor
     {
         private static class Styles
         {
-            public static GUIContent flattenGroup = EditorGUIUtility.TrTextContent("Sort 3D as 2D", "Clears z values on 3D meshes affected by a Sorting Group allowing them to sort with other 2D objects and Sort 3D as 2D sorting groups.");
+            public static GUIContent sort3DAs2D = EditorGUIUtility.TrTextContent("Sort 3D as 2D", "Clears z values on 3D meshes affected by a Sorting Group allowing them to sort with other 2D objects and Sort 3D as 2D sorting groups.");
         }
 
         private SerializedProperty m_SortingOrder;
         private SerializedProperty m_SortingLayerID;
+        private SerializedProperty m_Sort3DAs2D;
 
         public virtual void OnEnable()
         {
             alwaysAllowExpansion = true;
-            m_SortingOrder   = serializedObject.FindProperty("m_SortingOrder");
+            m_SortingOrder = serializedObject.FindProperty("m_SortingOrder");
             m_SortingLayerID = serializedObject.FindProperty("m_SortingLayerID");
+            m_Sort3DAs2D = serializedObject.FindProperty("m_Sort3DAs2D");
         }
 
         public RenderAs2D TryToFindCreatedRenderAs2D(SortingGroup sortingGroup)
         {
             RenderAs2D[] renderAs2Ds = sortingGroup.GetComponents<RenderAs2D>();
-            foreach(RenderAs2D renderAs2D in renderAs2Ds)
+            foreach (RenderAs2D renderAs2D in renderAs2Ds)
             {
                 if (renderAs2D.IsOwner(sortingGroup))
                     return renderAs2D;
@@ -49,39 +51,30 @@ namespace UnityEditor
             UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(UnityEngine.SceneManagement.SceneManager.GetActiveScene());
         }
 
-        void RenderFlattening()
+        void RenderSort3DAs2D()
         {
-
-            bool tryToFlatten = TryToFindCreatedRenderAs2D(serializedObject.targetObject as SortingGroup) != null;
-            bool result = DrawToggleWithLayout(tryToFlatten, Styles.flattenGroup);
-
-            if (tryToFlatten != result)
+            EditorGUILayout.PropertyField(m_Sort3DAs2D, Styles.sort3DAs2D);
+            foreach (var target in targets)
             {
-                tryToFlatten = result;
-                foreach (Object targetObject in serializedObject.targetObjects)
+                SortingGroup sortingGroup = (SortingGroup)target;
+                if (sortingGroup.sort3DAs2D)
                 {
-                    SortingGroup sortingGroup = targetObject as SortingGroup;
-                    RenderAs2D renderAs2D = TryToFindCreatedRenderAs2D(sortingGroup);
+                    GameObject go = sortingGroup.gameObject;
+                    go.TryGetComponent<RenderAs2D>(out RenderAs2D renderAs2D);
 
-                    if (tryToFlatten)
+                    if (renderAs2D != null && !renderAs2D.IsOwner(sortingGroup))
                     {
-                        if (!renderAs2D)
-                        {
-                            Material mat = AssetDatabase.LoadAssetAtPath<Material>("Packages/com.unity.render-pipelines.universal/Runtime/Materials/RenderAs2D-Flattening.mat");
-                            RenderAs2D newRenderAs2D = sortingGroup.gameObject.AddComponent<RenderAs2D>();
-                            newRenderAs2D.Init(sortingGroup);
-                            newRenderAs2D.material = mat;
-                            newRenderAs2D.hideFlags = HideFlags.HideInInspector | HideFlags.HideInHierarchy;
-                            DirtyScene();
-                        }
+                        Component.DestroyImmediate(renderAs2D, true);
+                        renderAs2D = null;
                     }
-                    else
+
+                    if(renderAs2D == null)
                     {
-                        if (renderAs2D)
-                        {
-                            Component.DestroyImmediate(renderAs2D);
-                            DirtyScene();
-                        }
+                        Material mat = AssetDatabase.LoadAssetAtPath<Material>("Packages/com.unity.render-pipelines.universal/Runtime/Materials/RenderAs2D-Flattening.mat");
+                        renderAs2D = go.AddComponent<RenderAs2D>();
+                        renderAs2D.Init(sortingGroup);
+                        renderAs2D.material = mat;
+                        EditorUtility.SetDirty(sortingGroup.gameObject);
                     }
                 }
             }
@@ -95,7 +88,7 @@ namespace UnityEditor
             if (rpAsset != null && (rpAsset.scriptableRenderer is Renderer2D))
             {
                 SortingLayerEditorUtility.RenderSortingLayerFields(m_SortingOrder, m_SortingLayerID);
-                RenderFlattening();
+                RenderSort3DAs2D();
             }
             else
                 base.OnInspectorGUI();
