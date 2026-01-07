41f88b27944c98f9e49be639ba616cbd7064b2aa
Aljosha Demeulemeester <aljosha@unity3d.com>
2026-01-07 22:50:18 +0000

    Fix issue with depth priming where intermediate textures are not requested when needing a depth copy

--- Packages/com.unity.render-pipelines.universal/Runtime/UniversalRendererRenderGraph.cs
+++ Packages/com.unity.render-pipelines.universal/Runtime/UniversalRendererRenderGraph.cs
@@ -675,7 +675,7 @@ namespace UnityEngine.Rendering.Universal
             bool requirePrepass = requirePrepassForTextures || useDepthPriming;
 
             // Only use a depth format when we do a prepass directly the cameraDepthTexture. If we do depth priming (ie, prepass to the activeCameraDepth), we don't do a prepass to the texture. Instead, we do a copy from the primed attachment.
-            bool prepassToCameraDepthTexture = requirePrepassForTextures && !usesDeferredLighting;
+            bool prepassToCameraDepthTexture = requirePrepassForTextures && !usesDeferredLighting && !useDepthPriming;
             bool depthTextureIsDepthFormat = prepassToCameraDepthTexture;
             bool requireCopyFromDepth = renderPassInputs.requiresDepthTexture && !prepassToCameraDepthTexture;
 
