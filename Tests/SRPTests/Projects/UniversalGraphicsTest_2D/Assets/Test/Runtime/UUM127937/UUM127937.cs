a145130bc4982be5b66645f20eb96b43db53d571
Reach Platform Support <svc-reach-platform-support@unity3d.com>
2026-01-16 07:39:07 +0000

    [Port] [6000.4] 2d/bugfix/uum 127937

---
+++ Tests/SRPTests/Projects/UniversalGraphicsTest_2D/Assets/Test/Runtime/UUM127937/UUM127937.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace SpriteRendererTests
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class UUM127937: MonoBehaviour
+    {
+        private static readonly int Saturation = Shader.PropertyToID("_Saturation");
+        private static readonly int AnotherProp = Shader.PropertyToID("_AnotherProp");
+
+        public static readonly float sDefault = 0.2f;
+
+        private void Start()
+        {
+            SpriteRenderer ren = this.GetComponent<SpriteRenderer>();
+            MaterialPropertyBlock propertyBlock = new();
+            ren.GetPropertyBlock(propertyBlock);
+            propertyBlock.SetFloat(Saturation, sDefault);
+            propertyBlock.SetFloat(AnotherProp, sDefault);
+            ren.SetPropertyBlock(propertyBlock);
+        }
+
+        public float GetSaturation()
+        {
+            SpriteRenderer ren = this.GetComponent<SpriteRenderer>();
+            MaterialPropertyBlock propertyBlock = new();
+            ren.GetPropertyBlock(propertyBlock);
+            var saturation = propertyBlock.GetFloat(Saturation);
+            return saturation;
+        }
+    }
+}
