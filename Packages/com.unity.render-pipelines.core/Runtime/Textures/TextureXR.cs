22a8fdc275eee9cb09e884353120280864e14b3f
Rémi Slysz <remi@unity3d.com>
2026-02-18 23:57:23 +0000

    Fixing [AGGREGATE] Monitored SRPCore

--- Packages/com.unity.render-pipelines.core/Runtime/Textures/TextureXR.cs
+++ Packages/com.unity.render-pipelines.core/Runtime/Textures/TextureXR.cs
@@ -8,7 +8,7 @@ namespace UnityEngine.Rendering
     public static class TextureXR
     {
         // Property set by XRSystem
-        private static int m_MaxViews = 1;
+        private static int s_MaxViews = 1;
         /// <summary>
         /// Maximum number of views handled by the XR system.
         /// </summary>
@@ -16,7 +16,7 @@ namespace UnityEngine.Rendering
         {
             set
             {
-                m_MaxViews = value;
+                s_MaxViews = value;
             }
         }
 
@@ -24,7 +24,7 @@ namespace UnityEngine.Rendering
         /// <summary>
         /// Number of slices used by the XR system.
         /// </summary>
-        public static int slices { get => m_MaxViews; }
+        public static int slices { get => s_MaxViews; }
 
         // Must be in sync with shader define in TextureXR.hlsl
         /// <summary>
@@ -65,66 +65,71 @@ namespace UnityEngine.Rendering
         }
 
         // Need to keep both the Texture and the RTHandle in order to be able to track lifetime properly.
-        static Texture m_BlackUIntTexture2DArray;
-        static Texture m_BlackUIntTexture;
-        static RTHandle m_BlackUIntTexture2DArrayRTH;
-        static RTHandle m_BlackUIntTextureRTH;
+        static Texture s_BlackUIntTexture2DArray;
+        static Texture s_BlackUIntTexture;
+        static RTHandle s_BlackUIntTexture2DArrayRTH;
+        static RTHandle s_BlackUIntTextureRTH;
         /// <summary>
         /// Default black unsigned integer texture.
         /// </summary>
         /// <returns>The default black unsigned integer texture.</returns>
-        public static RTHandle GetBlackUIntTexture() { return useTexArray ? m_BlackUIntTexture2DArrayRTH : m_BlackUIntTextureRTH; }
+        public static RTHandle GetBlackUIntTexture() { return useTexArray ? s_BlackUIntTexture2DArrayRTH : s_BlackUIntTextureRTH; }
 
-        static Texture2DArray m_ClearTexture2DArray;
-        static Texture2D m_ClearTexture;
-        static RTHandle m_ClearTexture2DArrayRTH;
-        static RTHandle m_ClearTextureRTH;
+        static Texture2DArray s_ClearTexture2DArray;
+        static Texture2D s_ClearTexture;
+        static RTHandle s_ClearTexture2DArrayRTH;
+        static RTHandle s_ClearTextureRTH;
         /// <summary>
         /// Default clear color (0, 0, 0, 1) texture.
         /// </summary>
         /// <returns>The default clear color texture.</returns>
-        public static RTHandle GetClearTexture() { return useTexArray ? m_ClearTexture2DArrayRTH : m_ClearTextureRTH; }
+        public static RTHandle GetClearTexture() { return useTexArray ? s_ClearTexture2DArrayRTH : s_ClearTextureRTH; }
 
-        static Texture2DArray m_MagentaTexture2DArray;
-        static Texture2D m_MagentaTexture;
-        static RTHandle m_MagentaTexture2DArrayRTH;
-        static RTHandle m_MagentaTextureRTH;
+        static Texture2DArray s_MagentaTexture2DArray;
+        static Texture2D s_MagentaTexture;
+        static RTHandle s_MagentaTexture2DArrayRTH;
+        static RTHandle s_MagentaTextureRTH;
         /// <summary>
         /// Default magenta texture.
         /// </summary>
         /// <returns>The default magenta texture.</returns>
-        public static RTHandle GetMagentaTexture() { return useTexArray ? m_MagentaTexture2DArrayRTH : m_MagentaTextureRTH; }
+        public static RTHandle GetMagentaTexture() { return useTexArray ? s_MagentaTexture2DArrayRTH : s_MagentaTextureRTH; }
 
-        static Texture2D m_BlackTexture;
-        static Texture3D m_BlackTexture3D;
-        static Texture2DArray m_BlackTexture2DArray;
-        static RTHandle m_BlackTexture2DArrayRTH;
-        static RTHandle m_BlackTextureRTH;
-        static RTHandle m_BlackTexture3DRTH;
+        static Texture2D s_BlackTexture;
+        static Texture3D s_BlackTexture3D;
+        static Texture2DArray s_BlackTexture2DArray;
+        static RTHandle s_BlackTexture2DArrayRTH;
+        static RTHandle s_BlackTextureRTH;
+        static RTHandle s_BlackTexture3DRTH;
         /// <summary>
         /// Default black texture.
         /// </summary>
         /// <returns>The default black texture.</returns>
-        public static RTHandle GetBlackTexture() { return useTexArray ? m_BlackTexture2DArrayRTH : m_BlackTextureRTH; }
+        public static RTHandle GetBlackTexture() { return useTexArray ? s_BlackTexture2DArrayRTH : s_BlackTextureRTH; }
         /// <summary>
         /// Default black texture array.
         /// </summary>
         /// <returns>The default black texture array.</returns>
-        public static RTHandle GetBlackTextureArray() { return m_BlackTexture2DArrayRTH; }
+        public static RTHandle GetBlackTextureArray() { return s_BlackTexture2DArrayRTH; }
         /// <summary>
         /// Default black texture 3D.
         /// </summary>
         /// <returns>The default black texture 3D.</returns>
-        public static RTHandle GetBlackTexture3D() { return m_BlackTexture3DRTH; }
+        public static RTHandle GetBlackTexture3D() { return s_BlackTexture3DRTH; }
 
-        static Texture2DArray m_WhiteTexture2DArray;
-        static RTHandle m_WhiteTexture2DArrayRTH;
-        static RTHandle m_WhiteTextureRTH;
+        static Texture2DArray s_WhiteTexture2DArray;
+        static RTHandle s_WhiteTexture2DArrayRTH;
+        static RTHandle s_WhiteTextureRTH;
         /// <summary>
         /// Default white texture.
         /// </summary>
         /// <returns>The default white texture.</returns>
-        public static RTHandle GetWhiteTexture() { return useTexArray ? m_WhiteTexture2DArrayRTH : m_WhiteTextureRTH; }
+        public static RTHandle GetWhiteTexture() { return useTexArray ? s_WhiteTexture2DArrayRTH : s_WhiteTextureRTH; }
+
+        /// <summary>
+        /// Is TextureXR initialized?
+        /// </summary>
+        public static bool initialized => s_BlackUIntTexture2DArray != null; // We assume that everything is valid if one is valid.
 
         /// <summary>
         /// Initialize XR textures. Must be called at least once.
@@ -133,56 +138,89 @@ namespace UnityEngine.Rendering
         /// <param name="clearR32_UIntShader">Compute shader used to intitialize unsigned integer textures.</param>
         public static void Initialize(CommandBuffer cmd, ComputeShader clearR32_UIntShader)
         {
-            if (m_BlackUIntTexture2DArray == null) // We assume that everything is invalid if one is invalid.
-            {
-                // Black UINT
-                RTHandles.Release(m_BlackUIntTexture2DArrayRTH);
-                m_BlackUIntTexture2DArray = CreateBlackUIntTextureArray(cmd, clearR32_UIntShader);
-                m_BlackUIntTexture2DArrayRTH = RTHandles.Alloc(m_BlackUIntTexture2DArray);
-                RTHandles.Release(m_BlackUIntTextureRTH);
-                m_BlackUIntTexture = CreateBlackUintTexture(cmd, clearR32_UIntShader);
-                m_BlackUIntTextureRTH = RTHandles.Alloc(m_BlackUIntTexture);
+            if (initialized)
+                return;
+            
+            // Black UINT
+            s_BlackUIntTexture2DArray = CreateBlackUIntTextureArray(cmd, clearR32_UIntShader);
+            s_BlackUIntTexture2DArrayRTH = RTHandles.Alloc(s_BlackUIntTexture2DArray);
+            s_BlackUIntTexture = CreateBlackUintTexture(cmd, clearR32_UIntShader);
+            s_BlackUIntTextureRTH = RTHandles.Alloc(s_BlackUIntTexture);
 
-                // Clear
-                RTHandles.Release(m_ClearTextureRTH);
-                m_ClearTexture = new Texture2D(1, 1, GraphicsFormat.R8G8B8A8_SRGB, TextureCreationFlags.None) { name = "Clear Texture" };
-                m_ClearTexture.SetPixel(0, 0, Color.clear);
-                m_ClearTexture.Apply();
-                m_ClearTextureRTH = RTHandles.Alloc(m_ClearTexture);
-                RTHandles.Release(m_ClearTexture2DArrayRTH);
-                m_ClearTexture2DArray = CreateTexture2DArrayFromTexture2D(m_ClearTexture, "Clear Texture2DArray");
-                m_ClearTexture2DArrayRTH = RTHandles.Alloc(m_ClearTexture2DArray);
+            // Clear
+            s_ClearTexture = new Texture2D(1, 1, GraphicsFormat.R8G8B8A8_SRGB, TextureCreationFlags.None) { name = "Clear Texture" };
+            s_ClearTexture.SetPixel(0, 0, Color.clear);
+            s_ClearTexture.Apply();
+            s_ClearTextureRTH = RTHandles.Alloc(s_ClearTexture);
+            s_ClearTexture2DArray = CreateTexture2DArrayFromTexture2D(s_ClearTexture, "Clear Texture2DArray");
+            s_ClearTexture2DArrayRTH = RTHandles.Alloc(s_ClearTexture2DArray);
 
-                // Magenta
-                RTHandles.Release(m_MagentaTextureRTH);
-                m_MagentaTexture = new Texture2D(1, 1, GraphicsFormat.R8G8B8A8_SRGB, TextureCreationFlags.None) { name = "Magenta Texture" };
-                m_MagentaTexture.SetPixel(0, 0, Color.magenta);
-                m_MagentaTexture.Apply();
-                m_MagentaTextureRTH = RTHandles.Alloc(m_MagentaTexture);
-                RTHandles.Release(m_MagentaTexture2DArrayRTH);
-                m_MagentaTexture2DArray = CreateTexture2DArrayFromTexture2D(m_MagentaTexture, "Magenta Texture2DArray");
-                m_MagentaTexture2DArrayRTH = RTHandles.Alloc(m_MagentaTexture2DArray);
+            // Magenta
+            s_MagentaTexture = new Texture2D(1, 1, GraphicsFormat.R8G8B8A8_SRGB, TextureCreationFlags.None) { name = "Magenta Texture" };
+            s_MagentaTexture.SetPixel(0, 0, Color.magenta);
+            s_MagentaTexture.Apply();
+            s_MagentaTextureRTH = RTHandles.Alloc(s_MagentaTexture);
+            s_MagentaTexture2DArray = CreateTexture2DArrayFromTexture2D(s_MagentaTexture, "Magenta Texture2DArray");
+            s_MagentaTexture2DArrayRTH = RTHandles.Alloc(s_MagentaTexture2DArray);
 
-                // Black
-                RTHandles.Release(m_BlackTextureRTH);
-                m_BlackTexture = new Texture2D(1, 1, GraphicsFormat.R8G8B8A8_SRGB, TextureCreationFlags.None) { name = "Black Texture" };
-                m_BlackTexture.SetPixel(0, 0, Color.black);
-                m_BlackTexture.Apply();
-                m_BlackTextureRTH = RTHandles.Alloc(m_BlackTexture);
-                RTHandles.Release(m_BlackTexture2DArrayRTH);
-                m_BlackTexture2DArray = CreateTexture2DArrayFromTexture2D(m_BlackTexture, "Black Texture2DArray");
-                m_BlackTexture2DArrayRTH = RTHandles.Alloc(m_BlackTexture2DArray);
-                RTHandles.Release(m_BlackTexture3DRTH);
-                m_BlackTexture3D = CreateBlackTexture3D("Black Texture3D");
-                m_BlackTexture3DRTH = RTHandles.Alloc(m_BlackTexture3D);
+            // Black
+            s_BlackTexture = new Texture2D(1, 1, GraphicsFormat.R8G8B8A8_SRGB, TextureCreationFlags.None) { name = "Black Texture" };
+            s_BlackTexture.SetPixel(0, 0, Color.black);
+            s_BlackTexture.Apply();
+            s_BlackTextureRTH = RTHandles.Alloc(s_BlackTexture);
+            s_BlackTexture2DArray = CreateTexture2DArrayFromTexture2D(s_BlackTexture, "Black Texture2DArray");
+            s_BlackTexture2DArrayRTH = RTHandles.Alloc(s_BlackTexture2DArray);
+            s_BlackTexture3D = CreateBlackTexture3D("Black Texture3D");
+            s_BlackTexture3DRTH = RTHandles.Alloc(s_BlackTexture3D);
 
-                // White
-                RTHandles.Release(m_WhiteTextureRTH);
-                m_WhiteTextureRTH = RTHandles.Alloc(Texture2D.whiteTexture);
-                RTHandles.Release(m_WhiteTexture2DArrayRTH);
-                m_WhiteTexture2DArray = CreateTexture2DArrayFromTexture2D(Texture2D.whiteTexture, "White Texture2DArray");
-                m_WhiteTexture2DArrayRTH = RTHandles.Alloc(m_WhiteTexture2DArray);
-            }
+            // White
+            s_WhiteTextureRTH = RTHandles.Alloc(Texture2D.whiteTexture);
+            s_WhiteTexture2DArray = CreateTexture2DArrayFromTexture2D(Texture2D.whiteTexture, "White Texture2DArray");
+            s_WhiteTexture2DArrayRTH = RTHandles.Alloc(s_WhiteTexture2DArray);
+
+            //Auto cleanup if pipeline type is changed as all pipelines may not rely on TextureXR
+            //By time PipelineChange is raised though, it may be too late. So do it at each pipeline disposal instead.
+            RenderPipelineManager.activeRenderPipelineDisposed += Cleanup; 
+        }
+
+        static void Cleanup()
+        {
+            if (!initialized)
+                return;
+            
+            // Black UINT
+            RTHandles.Release(s_BlackUIntTexture2DArrayRTH);
+            s_BlackUIntTexture2DArray = null;
+            RTHandles.Release(s_BlackUIntTextureRTH);
+            s_BlackUIntTexture = null;
+            
+            // Clear
+            RTHandles.Release(s_ClearTextureRTH);
+            s_ClearTexture = null;
+            RTHandles.Release(s_ClearTexture2DArrayRTH);
+            s_ClearTexture2DArray = null;
+            
+            // Magenta
+            RTHandles.Release(s_MagentaTextureRTH);
+            s_MagentaTexture = null;
+            RTHandles.Release(s_MagentaTexture2DArrayRTH);
+            s_MagentaTexture2DArray = null;
+            
+            // Black
+            RTHandles.Release(s_BlackTextureRTH);
+            s_BlackTexture = null;
+            RTHandles.Release(s_BlackTexture2DArrayRTH);
+            s_BlackTexture2DArray = null;
+            RTHandles.Release(s_BlackTexture3DRTH);
+            s_BlackTexture3D = null;
+            
+            // White
+            RTHandles.Release(s_WhiteTextureRTH);
+            s_WhiteTextureRTH = null;
+            RTHandles.Release(s_WhiteTexture2DArrayRTH);
+            s_WhiteTexture2DArray = null;
+
+            RenderPipelineManager.activeRenderPipelineDisposed -= Cleanup;
         }
 
         static Texture2DArray CreateTexture2DArrayFromTexture2D(Texture2D source, string name)
