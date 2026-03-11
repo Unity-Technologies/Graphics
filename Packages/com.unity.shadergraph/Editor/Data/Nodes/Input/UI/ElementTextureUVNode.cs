c0be1c9eb7a5b0068fd9d9dfee64112cf7f783e9
Alexandre Thibodeau <alexandret@unity3d.com>
2026-03-11 10:06:52 +0000

    [Backport 6000.3] Fixed generated code for preview nodes for UI Toolkit shaders

--- Packages/com.unity.shadergraph/Editor/Data/Nodes/Input/UI/ElementTextureUVNode.cs
+++ Packages/com.unity.shadergraph/Editor/Data/Nodes/Input/UI/ElementTextureUVNode.cs
@@ -1,12 +1,13 @@
 using UnityEditor.Graphing;
 using UnityEditor.Rendering.UITK.ShaderGraph;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 namespace UnityEditor.ShaderGraph
 {
     [Title("Input", "UI", "Element Texture UV")]
     [SubTargetFilter(typeof(IUISubTarget))]
-    class ElementTextureUVNode : AbstractMaterialNode, IGeneratesBodyCode, IMayRequireUITK
+    class ElementTextureUVNode : AbstractMaterialNode, IGeneratesBodyCode, IMayRequireUITK, IMayRequireMeshUV
     {
         public const int TextureUVSlotId = 0;
 
@@ -29,12 +30,25 @@ namespace UnityEditor.ShaderGraph
 
         public void GenerateNodeCode(ShaderStringBuilder sb, GenerationMode generationMode)
         {
-            if (GetInputNodeFromSlot(TextureUVSlotId) != null) sb.AppendLine(string.Format("$precision2 {0} = IN.uvClip.xy;", GetVariableNameForSlot(TextureUVSlotId)));
+            if (generationMode == GenerationMode.Preview)
+            {
+                // In preview mode, use standard mesh UV0 (will visualize as color: red = u, green = v)
+                sb.AppendLine("$precision2 {0} = IN.uv0.xy;", GetVariableNameForSlot(TextureUVSlotId));
+                return;
+            }
+
+            if (GetInputNodeFromSlot(TextureUVSlotId) != null) sb.AppendLine("$precision2 {0} = IN.uvClip.xy;", GetVariableNameForSlot(TextureUVSlotId));
         }
 
         public bool RequiresUITK(ShaderStageCapability stageCapability)
         {
             return true;
+        }
+
+        public bool RequiresMeshUV(UVChannel channel, ShaderStageCapability stageCapability)
+        {
+            // Require UV0 in preview mode for visualization
+            return channel == UVChannel.UV0;
         }
     }
 }
