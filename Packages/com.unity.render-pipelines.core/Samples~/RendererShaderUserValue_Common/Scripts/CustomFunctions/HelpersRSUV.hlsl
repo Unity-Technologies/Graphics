9788dd74f834e9745fa77cf42ac524fc3d8a070f
Reach Platform Support <svc-reach-platform-support@unity3d.com>
2026-03-13 09:27:55 +0000

    [Port] [6000.5] [SRP] Update RSUV samples to leverage the new shader reflection API

--- Packages/com.unity.render-pipelines.core/Samples~/RendererShaderUserValue_Common/Scripts/CustomFunctions/HelpersRSUV.hlsl
+++ Packages/com.unity.render-pipelines.core/Samples~/RendererShaderUserValue_Common/Scripts/CustomFunctions/HelpersRSUV.hlsl
@@ -1,17 +1,58 @@
-bool GetBit(uint data, uint bitIndex)
+#include "ShaderApiReflectionSupport.hlsl"
+
+uint GetData()
 {
-    return ((data & (1 << bitIndex)) != 0);
+    return unity_RendererUserValue;
 }
 
-float DecodeBitsToInt(uint data, int bitOffset, int bitCount)
+///<funchints>
+///     <sg:ProviderKey>RSUVGetBit</sg:ProviderKey>
+///     <sg:DisplayName>RSUV Get Bit</sg:DisplayName>
+///     <sg:SearchName>RSUV Get Bit</sg:SearchName>
+///     <sg:SearchCategory>RSUV</sg:SearchCategory>
+///</funchints>
+///<paramhints name = "bitOffset">
+///    <sg:int/>
+///    <sg:Default>0</sg:Default>
+///</paramhints>
+UNITY_EXPORT_REFLECTION
+bool GetBit(int bitOffset)
+{
+    return ((GetData() & (1 << bitOffset)) != 0);
+}
+
+///<funchints>
+///     <sg:ProviderKey>RSUVGetInt</sg:ProviderKey>
+///     <sg:DisplayName>RSUV Get Int</sg:DisplayName>
+///     <sg:SearchName>RSUV Get Int</sg:SearchName>
+///     <sg:SearchCategory>RSUV</sg:SearchCategory>
+///</funchints>
+///<paramhints name = "bitOffset">
+///    <sg:int/>
+///    <sg:Default>0</sg:Default>
+///</paramhints>
+///<paramhints name = "bitCount">
+///    <sg:int/>
+///    <sg:Default>1</sg:Default>
+///</paramhints>
+UNITY_EXPORT_REFLECTION
+float DecodeBitsToInt(int bitOffset, int bitCount)
 {
     uint mask = (1u << bitCount) - 1u;        // Create bitCount-width mask (e.g., 0b000111 for 3 bits)
-    uint value = (data >> bitOffset) & mask;  // Shift down and apply mask
+    uint value = (GetData() >> bitOffset) & mask;  // Shift down and apply mask
     return (int)value;
 }
 
-float4 DecodeUintToFloat4(uint data)
+///<funchints>
+///     <sg:ProviderKey>RSUVGetFloat4</sg:ProviderKey>
+///     <sg:DisplayName>RSUV Get Float4</sg:DisplayName>
+///     <sg:SearchName>RSUV Get Float4</sg:SearchName>
+///     <sg:SearchCategory>RSUV</sg:SearchCategory>
+///</funchints>
+UNITY_EXPORT_REFLECTION
+float4 DecodeUintToFloat4()
 {
+    uint data = GetData();
     float a = ((data >> 24) & 0xFF) / 255.0;
     float r = ((data >> 16) & 0xFF) / 255.0;
     float g = ((data >> 8) & 0xFF) / 255.0;
@@ -20,7 +61,15 @@ float4 DecodeUintToFloat4(uint data)
     return float4(r, g, b, a);
 }
 
-uint GetData()
+///<funchints>
+///     <sg:ProviderKey>RSUVGetFloat</sg:ProviderKey>
+///     <sg:DisplayName>RSUV Get Float</sg:DisplayName>
+///     <sg:SearchName>RSUV Get Float</sg:SearchName>
+///     <sg:SearchCategory>RSUV</sg:SearchCategory>
+///</funchints>
+UNITY_EXPORT_REFLECTION
+float DecodeUintToFloat()
 {
-    return unity_RendererUserValue;
+    uint data = GetData();
+    return asfloat(data);
 }
