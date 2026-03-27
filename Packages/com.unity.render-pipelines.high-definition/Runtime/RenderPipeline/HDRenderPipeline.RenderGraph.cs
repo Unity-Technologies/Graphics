437bed062fd7b2d2891698fcc67838768cc44b46
Sumin Cho <sumin.cho@unity3d.com>
2026-03-27 10:34:10 +0000

    Fix dark screen HDR output and missing variants in HDRP Rendering Debugger

--- Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.RenderGraph.cs
+++ Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.RenderGraph.cs
@@ -143,6 +143,24 @@ namespace UnityEngine.Rendering.HighDefinition
 
                     colorBuffer = RenderDebugViewMaterial(m_RenderGraph, cullingResults, hdCamera, gpuLightListOutput, prepassOutput.dbuffer, prepassOutput.gbuffer, prepassOutput.depthBuffer, vtFeedbackBuffer);
                     colorBuffer = ResolveMSAAColor(m_RenderGraph, hdCamera, colorBuffer);
+
+                    bool rendersOffscreenUI = !m_OffscreenUIRenderedInCurrentFrame && HDROutputActiveForCameraType(hdCamera) && SupportedRenderingFeatures.active.rendersUIOverlay;
+                    if (rendersOffscreenUI)
+                    {
+                        uiBuffer = RenderHDROffscreenUI(m_RenderGraph, hdCamera, renderContext);
+                        m_OffscreenUIRenderedInCurrentFrame = true;
+                    }
+                    else
+                    {
+                        // We do not render offscreen ui for the rest of cameras.
+                        uiBuffer = m_OffscreenUIRenderedInCurrentFrame ? m_RenderGraph.ImportTexture(m_OffscreenUIColorBuffer.Value) : m_RenderGraph.defaultResources.blackTextureXR;
+                    }
+
+                    bool blitsOffscreenUICover = rendersOffscreenUI && m_RequireOffscreenUICoverPrepass;
+                    if (blitsOffscreenUICover)
+                    {
+                        BlitFullscreenUIToOffscreen(m_RenderGraph, colorBackBuffer, uiBuffer, hdCamera);
+                    }
                 }
                 else if (hdCamera.frameSettings.IsEnabled(FrameSettingsField.RayTracing) && pathTracing.enable.value && hdCamera.camera.cameraType != CameraType.Preview && GetRayTracingState() && GetRayTracingClusterState())
                 {
@@ -258,7 +276,7 @@ namespace UnityEngine.Rendering.HighDefinition
                     colorBuffer = RenderTransparency(m_RenderGraph, hdCamera, renderContext, colorBuffer, prepassOutput.resolvedNormalBuffer, vtFeedbackBuffer, currentColorPyramid, volumetricLighting, rayCountTexture, opticalFogTransmittance,
                         m_SkyManager.GetSkyReflection(hdCamera), gpuLightListOutput, transparentPrepass, ref prepassOutput, shadowResult, cullingResults, customPassCullingResults, aovRequest, aovCustomPassBuffers);
 
-                    bool rendersOffscreenUI = !m_OffscreenUIRenderedInCurrentFrame && HDROutputActiveForCameraType(hdCamera) && SupportedRenderingFeatures.active.rendersUIOverlay && !NeedHDRDebugMode(m_CurrentDebugDisplaySettings);
+                    bool rendersOffscreenUI = !m_OffscreenUIRenderedInCurrentFrame && HDROutputActiveForCameraType(hdCamera) && SupportedRenderingFeatures.active.rendersUIOverlay;
                     if (rendersOffscreenUI)
                     {
                         uiBuffer = RenderHDROffscreenUI(m_RenderGraph, hdCamera, renderContext);
@@ -515,6 +533,7 @@ namespace UnityEngine.Rendering.HighDefinition
             public Vector4 offscreenUIViewportParams;
             public bool applyAfterPP;
             public CubemapFace cubemapFace;
+            public bool postProcessEnabled;
 
             public TextureHandle uiTexture;
             public TextureHandle afterPostProcessTexture;
@@ -556,14 +575,17 @@ namespace UnityEngine.Rendering.HighDefinition
                     // Pick the right material based off XR rendering using texture arrays and if we are dealing with a single slice at the moment or processing all slices automatically.
                     passData.blitMaterial = (TextureXR.useTexArray && passData.srcTexArraySlice >= 0) ? m_FinalBlitWithOETFTexArraySingleSlice : m_FinalBlitWithOETF;
                     GetHDROutputParameters(HDRDisplayInformationForCamera(hdCamera), HDRDisplayColorGamutForCamera(hdCamera), m_Tonemapping, out passData.hdrOutputParmeters, out var unused);
+
                     GetOffscreenUIViewportParams(hdCamera, out passData.offscreenUIViewportParams);
                     passData.uiTexture = uiTexture;
                     builder.UseTexture(passData.uiTexture, AccessFlags.Read);
                     passData.applyAfterPP = hdCamera.frameSettings.IsEnabled(FrameSettingsField.AfterPostprocess) && !NeedHDRDebugMode(m_CurrentDebugDisplaySettings);
+                    passData.postProcessEnabled = m_PostProcessEnabled;
                 }
                 else
                 {
                     passData.hdrOutputParmeters = new Vector4(-1.0f, -1.0f, -1.0f, -1.0f);
+                    passData.postProcessEnabled = false;
                 }
 
                 builder.SetRenderFunc(
@@ -584,7 +606,12 @@ namespace UnityEngine.Rendering.HighDefinition
                             propertyBlock.SetVector(HDShaderIDs._OffscreenUIViewportParams, data.offscreenUIViewportParams);
                             propertyBlock.SetInt(HDShaderIDs._BlitTexArraySlice, data.srcTexArraySlice);
 
-                            HDROutputUtils.ConfigureHDROutput(data.blitMaterial, data.colorGamut, HDROutputUtils.Operation.ColorEncoding);
+                            HDROutputUtils.Operation hdrOperation = HDROutputUtils.Operation.ColorEncoding;
+                            if (!data.postProcessEnabled)
+                            {
+                                hdrOperation |= HDROutputUtils.Operation.ColorConversion;
+                            }
+                            HDROutputUtils.ConfigureHDROutput(data.blitMaterial, data.colorGamut, hdrOperation);
 
                             if (data.applyAfterPP)
                             {
