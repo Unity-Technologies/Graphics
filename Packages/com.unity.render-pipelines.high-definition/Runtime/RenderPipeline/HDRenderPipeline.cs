0e90fddb6f347f62e4c32bd0d45866cbea5b1043
Reach Platform Support <svc-reach-platform-support@unity3d.com>
2026-04-01 15:02:09 +0000

    [Port] [6000.5] Exposed serialized m_ShouldUseConservativeEnclosingSphere

--- Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.cs
+++ Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.cs
@@ -3091,6 +3091,7 @@ namespace UnityEngine.Rendering.HighDefinition
                 frozenCullingParamAvailable = false;
             }
 
+            cullingParams.conservativeEnclosingSphere = currentAsset.m_ShouldUseConservativeEnclosingSphere;
             LightLoopUpdateCullingParameters(ref cullingParams, hdCamera);
 
             // If we don't use environment light (like when rendering reflection probes)
