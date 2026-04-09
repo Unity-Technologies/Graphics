45731448494a33dee3b56d02b6debce932f2d61a
Reach Platform Support <svc-reach-platform-support@unity3d.com>
2026-04-09 22:25:48 +0000

    [Port] [6000.4] Fixed Default Volume profile bugs

--- Tests/SRPTests/Projects/SRP_SmokeTest/Assets/Tests/Editor/Volumes/VolumeComponentTests.cs
+++ Tests/SRPTests/Projects/SRP_SmokeTest/Assets/Tests/Editor/Volumes/VolumeComponentTests.cs
@@ -310,19 +310,6 @@ namespace UnityEditor.Rendering.Tests
 
         #region Decorators Handling Test
 
-        [HideInInspector]
-        class VolumeComponentDecorators : VolumeComponent
-        {
-            [Tooltip("Increase to make the noise texture appear bigger and less")]
-            public FloatParameter _NoiseTileSize = new FloatParameter(25.0f);
-
-            [InspectorName("Color")]
-            public ColorParameter _FogColor = new ColorParameter(Color.grey);
-
-            [InspectorName("Size and occurrence"), Tooltip("Increase to make patches SMALLER, and frequent")]
-            public ClampedFloatParameter _HighNoiseSpaceFreq = new ClampedFloatParameter(0.1f, 0.1f, 1f);
-        }
-
         readonly (string displayName, string tooltip)[] k_ExpectedResults =
         {
             (string.Empty, "Increase to make the noise texture appear bigger and less"),
