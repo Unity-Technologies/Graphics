3626fd76d6f1c6dc7b519d8473497db102b1190a
Reach Platform Support <svc-reach-platform-support@unity3d.com>
2026-04-15 17:49:05 +0000

    [Port] [6000.5] [Adaptive Performance] clamp cameraTargetDescriptor value to 1

--- Packages/com.unity.render-pipelines.universal/Runtime/UniversalRenderPipeline.cs
+++ Packages/com.unity.render-pipelines.universal/Runtime/UniversalRenderPipeline.cs
@@ -2596,17 +2596,17 @@ namespace UnityEngine.Rendering.Universal
             // TODO
             if (!cameraData.xr.enabled)
             {
-                cameraData.cameraTargetDescriptor.width = (int)(cameraData.camera.pixelWidth * cameraData.renderScale);
-                cameraData.cameraTargetDescriptor.height = (int)(cameraData.camera.pixelHeight * cameraData.renderScale);
+                cameraData.cameraTargetDescriptor.width = Mathf.Max(1, (int)(cameraData.pixelWidth * cameraData.renderScale));
+                cameraData.cameraTargetDescriptor.height = Mathf.Max(1, (int)(cameraData.pixelHeight * cameraData.renderScale));
 #if ENABLE_UPSCALER_FRAMEWORK
                 IUpscaler activeUpscaler = upscaling.activeUpscaler;
                 if (activeUpscaler != null) // An IUpscaler is active.
                 {
                     // It might want to change the pre-upscale resolution. Negotiate with it.
-                    Vector2Int res = new Vector2Int(cameraData.cameraTargetDescriptor.width, cameraData.scaledHeight);
+                    Vector2Int res = new Vector2Int(cameraData.cameraTargetDescriptor.width, cameraData.cameraTargetDescriptor.height);
                     activeUpscaler.NegotiatePreUpscaleResolution(ref res, new Vector2Int(cameraData.pixelWidth, cameraData.pixelHeight));
-                    cameraData.cameraTargetDescriptor.width = res.x;
-                    cameraData.cameraTargetDescriptor.height = res.y;
+                    cameraData.cameraTargetDescriptor.width = Mathf.Max(1, res.x);
+                    cameraData.cameraTargetDescriptor.height = Mathf.Max(1, res.y);
                 }
 #endif
                 cameraData.scaledWidth = cameraData.cameraTargetDescriptor.width;
