6af07f4fa891f470b874b7ce6e0d03c3d84bc46e
Pema Malling <pema.malling@unity3d.com>
2026-05-04 00:55:22 +0000

    Fix light cookies in URP when using multiple directional lights and deferred rendering.

--- Packages/com.unity.render-pipelines.universal/Runtime/DeferredLights.cs
+++ Packages/com.unity.render-pipelines.universal/Runtime/DeferredLights.cs
@@ -796,6 +796,7 @@ namespace UnityEngine.Rendering.Universal.Internal
             // Also separate shadow caster lights from non-shadow caster.
             int lastLightCookieIndex = -1;
             bool isFirstLight = true;
+            bool isFirstAdditionalLight = true;
             bool lastLightCookieKeywordState = false;
             bool lastShadowsKeywordState = false;
             bool lastSoftShadowsKeywordState = false;
@@ -829,7 +830,8 @@ namespace UnityEngine.Rendering.Universal.Internal
                     int shadowLightIndex = hasAdditionalLightPass ? m_AdditionalLightsShadowCasterPass.GetShadowLightIndexFromLightIndex(visLightIndex) : -1;
                     hasDeferredShadows = light && light.shadows != LightShadows.None && shadowLightIndex >= 0;
                     cmd.SetGlobalInt(ShaderConstants._ShadowLightIndex, shadowLightIndex);
-                    SetLightCookiesKeyword(cmd, visLightIndex, hasLightCookieManager, isFirstLight, ref lastLightCookieKeywordState, ref lastLightCookieIndex);
+                    SetLightCookiesKeyword(cmd, visLightIndex, hasLightCookieManager, isFirstAdditionalLight, ref lastLightCookieKeywordState, ref lastLightCookieIndex);
+                    isFirstAdditionalLight = false;
                 }
 
                 // Update keywords states
