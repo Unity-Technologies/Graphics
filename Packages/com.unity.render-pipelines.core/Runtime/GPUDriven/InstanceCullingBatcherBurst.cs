4cf13c1891ccce284ee3e9f2c752c9099bbee521
Sven Santema <svens@unity3d.com>
2026-05-19 16:52:07 +0000

    [Port] [6000.0] Disable indirect draws for non-tessellated quad topology in GRD.

--- Packages/com.unity.render-pipelines.core/Runtime/GPUDriven/InstanceCullingBatcherBurst.cs
+++ Packages/com.unity.render-pipelines.core/Runtime/GPUDriven/InstanceCullingBatcherBurst.cs
@@ -176,6 +176,7 @@ namespace UnityEngine.Rendering
             Span<GPUDrivenPackedMaterialData> packedMaterialDatas = stackalloc GPUDrivenPackedMaterialData[materialsCount];
 
             var supportsIndirect = true;
+            var isMetal = SystemInfo.graphicsDeviceType == GraphicsDeviceType.Metal;
             for (int matIndex = 0; matIndex < materialsCount; ++matIndex)
             {
                 if (matIndex >= submeshCount)
@@ -197,7 +198,14 @@ namespace UnityEngine.Rendering
                     bool isFound = packedMaterialDataHash.TryGetValue(materialID, out packedMaterialData);
                     Assert.IsTrue(isFound, "Packed material data not found.");
                 }
-                supportsIndirect &= packedMaterialData.isIndirectSupported;
+
+                var submeshIndex = startSubMesh + matIndex;
+                var subMesh = rendererData.subMeshDesc[subMeshDescOffset + submeshIndex];
+
+                // The indirect path does not support topology adjustment; use the direct path when this is required.
+                // Concretely, for quads, only use the indirect path if we allow quads natively (e.g. tessellation shaders).
+                supportsIndirect &= subMesh.topology != MeshTopology.Quads || packedMaterialData.hasTessellation;
+                supportsIndirect &= !isMetal || !packedMaterialData.hasTessellation;
 
                 packedMaterialDatas[matIndex] = packedMaterialData;
             }
