afc1e12c8e2d6af0c286e1e41da94fedd24413bf
Robin Bradley <robinb@unity3d.com>
2026-05-21 21:16:06 +0000

    [content automatically redacted] touching PlatformDependent folder

--- Packages/com.unity.render-pipelines.universal/Editor/ShaderGraph/Targets/UniversalTarget.cs
+++ Packages/com.unity.render-pipelines.universal/Editor/ShaderGraph/Targets/UniversalTarget.cs
@@ -1773,6 +1773,7 @@ namespace UnityEditor.Rendering.Universal.ShaderGraph
         const string kFog = "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Fog.hlsl";
         const string kRenderingLayers = "Packages/com.unity.render-pipelines.universal/ShaderLibrary/RenderingLayers.hlsl";
         const string kProbeVolumes = "Packages/com.unity.render-pipelines.universal/ShaderLibrary/ProbeVolumeVariants.hlsl";
+        const string kGbufferOutputFormat = "Packages/com.unity.render-pipelines.universal/ShaderLibrary/GBufferOutputFormat.hlsl";
 
         public static readonly IncludeCollection CorePregraph = new IncludeCollection
         {
@@ -1912,6 +1913,11 @@ namespace UnityEditor.Rendering.Universal.ShaderGraph
         public static readonly IncludeCollection LODCrossFade = new IncludeCollection
         {
             { kLODCrossFade, IncludeLocation.Pregraph }
+        };
+
+        public static readonly IncludeCollection GBufferOutputFormat = new IncludeCollection
+        {
+            { kGbufferOutputFormat, IncludeLocation.Postgraph, true }
         };
     }
     #endregion
--- Packages/com.unity.render-pipelines.universal/Shaders/Unlit.shader
+++ Packages/com.unity.render-pipelines.universal/Shaders/Unlit.shader
@@ -139,6 +139,7 @@ Shader "Universal Render Pipeline/Unlit"
             // Includes
             #include "Packages/com.unity.render-pipelines.universal/Shaders/UnlitInput.hlsl"
             #include "Packages/com.unity.render-pipelines.universal/Shaders/UnlitGBufferPass.hlsl"
+            #include_with_pragmas "Packages/com.unity.render-pipelines.universal/ShaderLibrary/GBufferOutputFormat.hlsl"
             ENDHLSL
         }
 
