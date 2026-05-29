23919f83adf315386709ec454eeb63ce9af6abf7
Evergreen <evergreen@unity3d.com>
2026-05-29 10:43:06 +0000

    [Port] [6000.3] [UUM-143038] Fix NRE in ReAllocateOffscreenUIColorBufferIfNeeded after build target switch

--- Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.cs
+++ Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.cs
@@ -2182,11 +2182,11 @@ namespace UnityEngine.Rendering.HighDefinition
             m_RequireOffscreenUICoverPrepass = HDROutputForMainDisplayIsActive() && SupportedRenderingFeatures.active.rendersUIOverlay && !CoreUtils.IsScreenFullyCoveredByCameras(cameras);
             m_OffscreenUIRenderedInCurrentFrame = false;
 
-            // Reallocate the offscreen UI buffer when the resolution changes.
-            ReAllocateOffscreenUIColorBufferIfNeeded();
-
             if (!m_ValidAPI || cameraCount == 0)
                 return;
+
+            // Reallocate the offscreen UI buffer when the resolution changes.
+            ReAllocateOffscreenUIColorBufferIfNeeded();
 
             GPUResidentDrawer.ReinitializeIfNeeded();
 
@@ -3469,7 +3469,7 @@ namespace UnityEngine.Rendering.HighDefinition
 
         void ReAllocateOffscreenUIColorBufferIfNeeded()
         {
-            if (m_OffscreenUIColorBuffer.IsValueCreated)
+            if (m_OffscreenUIColorBuffer != null && m_OffscreenUIColorBuffer.IsValueCreated)
             {
                 if (Screen.width != m_OffscreenUIColorBuffer.Value.rt.width || Screen.height != m_OffscreenUIColorBuffer.Value.rt.height)
                 {
